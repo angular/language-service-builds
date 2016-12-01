77c250a5dbae819d1b58faf3e4a3653536da9f4e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 00:00:52 +0000

    c066281 docs(changelog): add changelog for 2.3.0-rc.0

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-beta.0-d420080
+ * @license Angular v2.3.0-rc.0-c066281
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13491,7 +13491,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('2.3.0-beta.0-d420080');
+	var /** @type {?} */ VERSION$1 = new Version('2.3.0-rc.0-c066281');
 
 	/**
 	 * @license
@@ -39760,7 +39760,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$2 = new Version('2.3.0-beta.0-d420080');
+	var /** @type {?} */ VERSION$2 = new Version('2.3.0-rc.0-c066281');
 
 	/**
 	 * @license
@@ -45094,7 +45094,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('2.3.0-beta.0-d420080');
+	var VERSION = new Version('2.3.0-rc.0-c066281');
 
 	exports.VERSION = VERSION;
 	exports['default'] = LanguageServicePlugin;
