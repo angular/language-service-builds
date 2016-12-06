e2d738b692a915bdc49e6335994a73c4663ac9c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:54:21 +0000

    2893c2c fix(router): validate nested routes (#13224)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { LanguageServicePlugin } from './src/ts_plugin';
 /**
  * @stable
  */
-export var VERSION = new Version('2.3.0-rc.0-f31c947');
+export var VERSION = new Version('2.3.0-rc.0-2893c2c');
 export { createLanguageService } from './src/language_service';
 export { TypeScriptServiceHost, createLanguageServiceFromTypescript } from './src/typescript_host';
 export default LanguageServicePlugin;
