81e8e55dbbb9a315c136bf604c2b7dc117f39a1b
Victor Berchet <victor@suumit.com>
2016-12-09 23:50:36 +0000

    a006c14 feat(router): routerLink add tabindex attribute (#13094)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-aaf6e05
+ * @license Angular v2.3.0-a006c14
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -997,7 +997,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('2.3.0-aaf6e05');
+	var /** @type {?} */ VERSION = new Version('2.3.0-a006c14');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -25784,7 +25784,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('2.3.0-aaf6e05');
+	var /** @type {?} */ VERSION$1 = new Version('2.3.0-a006c14');
 
 	/**
 	 * @return {?}
@@ -44191,7 +44191,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('2.3.0-aaf6e05');
+	var VERSION$3 = new Version('2.3.0-a006c14');
 
 	/**
 	 * @license
@@ -45548,7 +45548,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('2.3.0-aaf6e05');
+	var VERSION$4 = new Version('2.3.0-a006c14');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
