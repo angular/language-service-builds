5dbfe9f54b898438954537079ba5259e8e2c9f13
Victor Berchet <victor@suumit.com>
2016-12-12 19:29:04 +0000

    56dce0e feat(common): export NgLocaleLocalization (#13367)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-8a8c532
+ * @license Angular v2.3.0-56dce0e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -997,7 +997,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('2.3.0-8a8c532');
+	var /** @type {?} */ VERSION = new Version('2.3.0-56dce0e');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -25784,7 +25784,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('2.3.0-8a8c532');
+	var /** @type {?} */ VERSION$1 = new Version('2.3.0-56dce0e');
 
 	/**
 	 * @return {?}
@@ -44191,7 +44191,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('2.3.0-8a8c532');
+	var VERSION$3 = new Version('2.3.0-56dce0e');
 
 	/**
 	 * @license
@@ -45548,7 +45548,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('2.3.0-8a8c532');
+	var VERSION$4 = new Version('2.3.0-56dce0e');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
