6c2eadaa34b28ce6664c1e3c83356fcc34028a88
Victor Berchet <victor@suumit.com>
2016-12-13 19:46:23 +0000

    714f2af ci(browser providers): update browsers in SL and BS (#13431)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-2b90cd5
+ * @license Angular v2.3.0-714f2af
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -997,7 +997,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('2.3.0-2b90cd5');
+	var /** @type {?} */ VERSION = new Version('2.3.0-714f2af');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -25825,7 +25825,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('2.3.0-2b90cd5');
+	var /** @type {?} */ VERSION$1 = new Version('2.3.0-714f2af');
 
 	/**
 	 * @return {?}
@@ -44243,7 +44243,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('2.3.0-2b90cd5');
+	var VERSION$3 = new Version('2.3.0-714f2af');
 
 	/**
 	 * @license
@@ -45614,7 +45614,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('2.3.0-2b90cd5');
+	var VERSION$4 = new Version('2.3.0-714f2af');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
