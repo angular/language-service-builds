3ceac0d62e05befc9b35f181a03e19e7b6f7c695
Victor Berchet <victor@suumit.com>
2016-12-14 17:07:20 +0000

    821b8f0 fix(forms): ensure select[multiple] retains selections

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-7b0a867
+ * @license Angular v2.3.0-821b8f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -997,7 +997,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('2.3.0-7b0a867');
+	var /** @type {?} */ VERSION = new Version('2.3.0-821b8f0');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -25826,7 +25826,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('2.3.0-7b0a867');
+	var /** @type {?} */ VERSION$1 = new Version('2.3.0-821b8f0');
 
 	/**
 	 * @return {?}
@@ -44244,7 +44244,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('2.3.0-7b0a867');
+	var VERSION$3 = new Version('2.3.0-821b8f0');
 
 	/**
 	 * @license
@@ -45641,7 +45641,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('2.3.0-7b0a867');
+	var VERSION$4 = new Version('2.3.0-821b8f0');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
