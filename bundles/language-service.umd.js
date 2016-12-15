511503a7210dedac6930c24c1f53080e7b9850e0
Chuck Jazdzewski <chuckj@google.com>
2016-12-15 22:32:45 +0000

    0c19898 fix(animations): allow players to be destroyed before initialized (#13346)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-5b6e8ea
+ * @license Angular v4.0.0-beta.0-0c19898
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1700,7 +1700,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-5b6e8ea');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-0c19898');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -26078,7 +26078,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.0-5b6e8ea');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.0-0c19898');
 
 	/**
 	 * @return {?}
@@ -45151,7 +45151,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.0-5b6e8ea');
+	var VERSION$3 = new Version('4.0.0-beta.0-0c19898');
 
 	/**
 	 * @license
@@ -46567,7 +46567,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.0-5b6e8ea');
+	var VERSION$4 = new Version('4.0.0-beta.0-0c19898');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
