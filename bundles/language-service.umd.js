3a7d47adb92641bf6820ad4a0f29754581490ce6
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 00:31:05 +0000

    9da4c25 feat(upgrade): support the `$doCheck()` lifecycle hook in `UpgradeComponent` (#13015)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-383adc9
+ * @license Angular v4.0.0-beta.0-9da4c25
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1700,7 +1700,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-383adc9');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-9da4c25');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -26124,7 +26124,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.0-383adc9');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.0-9da4c25');
 
 	/**
 	 * @return {?}
@@ -45232,7 +45232,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.0-383adc9');
+	var VERSION$3 = new Version('4.0.0-beta.0-9da4c25');
 
 	/**
 	 * @license
@@ -46648,7 +46648,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.0-383adc9');
+	var VERSION$4 = new Version('4.0.0-beta.0-9da4c25');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
