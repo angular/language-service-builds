7125ff99b634bfbe00b771178af4ae1a8408e89f
Hans <hans@hansl.ca>
2016-12-22 20:49:48 +0000

    e7ece6c fixed minor typo (#13626)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-f114e40
+ * @license Angular v4.0.0-beta.1-e7ece6c
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1700,7 +1700,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-f114e40');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-e7ece6c');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -26132,7 +26132,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.1-f114e40');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.1-e7ece6c');
 
 	/**
 	 * @return {?}
@@ -45242,7 +45242,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.1-f114e40');
+	var VERSION$3 = new Version('4.0.0-beta.1-e7ece6c');
 
 	/**
 	 * @license
@@ -46658,7 +46658,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.1-f114e40');
+	var VERSION$4 = new Version('4.0.0-beta.1-e7ece6c');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
