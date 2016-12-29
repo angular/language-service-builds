1a717e6a965d7e14857b500756a939584e6c0940
Hans <hans@hansl.ca>
2016-12-29 17:55:13 +0000

    bb0d23f Typo (#13698)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-1e6440e
+ * @license Angular v4.0.0-beta.1-bb0d23f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1602,7 +1602,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-1e6440e');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-bb0d23f');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26162,7 +26162,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.1-1e6440e');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.1-bb0d23f');
 
 	/**
 	 * @return {?}
@@ -45601,7 +45601,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.1-1e6440e');
+	var VERSION$3 = new Version('4.0.0-beta.1-bb0d23f');
 
 	/**
 	 * @license
@@ -47018,7 +47018,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.1-1e6440e');
+	var VERSION$4 = new Version('4.0.0-beta.1-bb0d23f');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
