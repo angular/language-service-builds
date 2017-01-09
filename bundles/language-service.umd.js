41723ca6ffea093586f06fa96a60769b6eda571c
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 23:21:38 +0000

    6164eb2 fix(compiler-cli): add support for more than 2 levels of nested lazy routes

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-7dc12b9
+ * @license Angular v2.4.2-6164eb2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1654,7 +1654,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('2.4.2-7dc12b9');
+	var /** @type {?} */ VERSION = new Version('2.4.2-6164eb2');
 
 	/**
 	 *  Allows to refer to references which are not yet defined.
@@ -26156,7 +26156,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('2.4.2-7dc12b9');
+	var /** @type {?} */ VERSION$1 = new Version('2.4.2-6164eb2');
 
 	/**
 	 * @return {?}
@@ -38527,6 +38527,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	                        if (value_1 && (depth != 0 || value_1.__symbolic != 'error')) {
 	                            var /** @type {?} */ parameters = targetFunction['parameters'];
 	                            var /** @type {?} */ defaults = targetFunction.defaults;
+	                            args = args.map(function (arg) { return simplifyInContext(context, arg, depth + 1); });
 	                            if (defaults && defaults.length > args.length) {
 	                                args.push.apply(args, defaults.slice(args.length).map(function (value) { return simplify(value); }));
 	                            }
@@ -38728,15 +38729,15 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	                                        return context;
 	                                    }
 	                                    var /** @type {?} */ argExpressions = expression['arguments'] || [];
-	                                    var /** @type {?} */ args = argExpressions.map(function (arg) { return simplifyInContext(context, arg, depth + 1); });
 	                                    var /** @type {?} */ converter = self.conversionMap.get(staticSymbol);
 	                                    if (converter) {
+	                                        var /** @type {?} */ args = argExpressions.map(function (arg) { return simplifyInContext(context, arg, depth + 1); });
 	                                        return converter(context, args);
 	                                    }
 	                                    else {
 	                                        // Determine if the function is one we can simplify.
 	                                        var /** @type {?} */ targetFunction = resolveReferenceValue(staticSymbol);
-	                                        return simplifyCall(staticSymbol, targetFunction, args);
+	                                        return simplifyCall(staticSymbol, targetFunction, argExpressions);
 	                                    }
 	                                }
 	                                break;
@@ -45294,7 +45295,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('2.4.2-7dc12b9');
+	var VERSION$3 = new Version('2.4.2-6164eb2');
 
 	/**
 	 * @license
@@ -46696,7 +46697,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('2.4.2-7dc12b9');
+	var VERSION$4 = new Version('2.4.2-6164eb2');
 
 	exports['default'] = LanguageServicePlugin;
 	exports.createLanguageService = createLanguageService;
