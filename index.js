a62e41a58f2101e47a94965db6fc21f949bc0050
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 23:56:24 +0000

    aeed737 fix(compiler-cli): avoid handling functions in loadChildren as lazy load routes paths

--- index.js
+++ index.js
@@ -5,9 +5,13 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { LanguageServicePlugin } from './src/ts_plugin';
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the language service package.
+ */
 export { createLanguageService } from './src/language_service';
+export { create } from './src/ts_plugin';
 export { TypeScriptServiceHost, createLanguageServiceFromTypescript } from './src/typescript_host';
 export { VERSION } from './src/version';
-export default LanguageServicePlugin;
 //# sourceMappingURL=index.js.map
