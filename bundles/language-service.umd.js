e4d5a1c93b43c22ba8a053c98f1d156ee1575452
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 21:38:10 +0000

    f20d1a8 ci: add @license to tools/validate-commit-message

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-e21e9c5
+ * @license Angular v4.0.0-beta.4-f20d1a8
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-e21e9c5');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-f20d1a8');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26499,7 +26499,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-e21e9c5');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-f20d1a8');
 
 	/**
 	 * @return {?}
@@ -45804,7 +45804,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.4-e21e9c5');
+	var VERSION$3 = new Version('4.0.0-beta.4-f20d1a8');
 
 	/**
 	 * @license
@@ -47268,7 +47268,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.4-e21e9c5');
+	var VERSION$4 = new Version('4.0.0-beta.4-f20d1a8');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
