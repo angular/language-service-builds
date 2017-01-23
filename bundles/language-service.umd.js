4e68c885a2cfd854e2825542ef59a59e49edf409
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:42:38 +0000

    e21e9c5 feat(upgrade): Support ng-model in downgraded components (#10578)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-d3a3a8e');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-e21e9c5');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26499,7 +26499,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-d3a3a8e');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-e21e9c5');
 
 	/**
 	 * @return {?}
