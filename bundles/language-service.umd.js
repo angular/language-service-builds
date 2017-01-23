2bb24a7ed01b1355cbed7b22b7fe64a6cbed4a41
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:42:03 +0000

    d3a3a8e fix(core): fix not declared variable in view engine (#14045)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-dff6ee3');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-d3a3a8e');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26499,7 +26499,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-dff6ee3');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-d3a3a8e');
 
 	/**
 	 * @return {?}
