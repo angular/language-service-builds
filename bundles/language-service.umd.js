fbe8a762e7344e3ab55994747b94e97f494a65e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 20:09:57 +0000

    4931a61 docs(core): add docs for `AnimationStyles` and `AnimationKeyframe` (#14107)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-a733444');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-4931a61');
 
 	/**
 	 * Inject decorator and metadata.
@@ -11860,7 +11860,14 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/**
-	 * \@experimental
+	 * `AnimationKeyframe` consists of a series of styles (contained within {\@link AnimationStyles
+	 * `AnimationStyles`})
+	 * and an offset value indicating when those styles are applied within the `duration/delay/easing`
+	 * timings.
+	 * `AnimationKeyframe` is mostly an internal class which is designed to be used alongside {\@link
+	 * Renderer#animate-anchor `Renderer.animate`}.
+	 *
+	 * \@experimental Animation support is experimental
 	 */
 	var AnimationKeyframe = (function () {
 	    /**
@@ -13046,7 +13053,21 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	}
 
 	/**
-	 * \@experimental
+	 * @license
+	 * Copyright Google Inc. All Rights Reserved.
+	 *
+	 * Use of this source code is governed by an MIT-style license that can be
+	 * found in the LICENSE file at https://angular.io/license
+	 */
+	/**
+	 * `AnimationStyles` consists of a collection of key/value maps containing CSS-based style data
+	 * that can either be used as initial styling data or apart of a series of keyframes within an
+	 * animation.
+	 * This class is mostly internal, and it is designed to be used alongside
+	 * {\@link AnimationKeyframe `AnimationKeyframe`} and {\@link Renderer#animate-anchor
+	 * `Renderer.animate`}.
+	 *
+	 * \@experimental Animation support is experimental
 	 */
 	var AnimationStyles = (function () {
 	    /**
@@ -26815,7 +26836,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-a733444');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.4-4931a61');
 
 	/**
 	 * @return {?}
