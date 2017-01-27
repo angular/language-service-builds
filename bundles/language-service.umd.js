19790831984764b1f2b616bc8f822c6bfd7388e6
Igor Minar <igor@angularjs.org>
2017-01-27 08:06:31 +0000

    b141a22 ci(aio): run docs-tests in travis (#14097)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-1e729d7');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-b141a22');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26839,7 +26839,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-1e729d7');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-b141a22');
 
 	/**
 	 * @return {?}
