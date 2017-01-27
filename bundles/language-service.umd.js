c1399b649163535120e967dee91d06c2689416a2
Igor Minar <iminar@google.com>
2017-01-27 22:13:53 +0000

    8960d49 test(aio): import zone.js in tests

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-c37af2a');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-8960d49');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26346,7 +26346,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-c37af2a');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-8960d49');
 
 	var CompilerConfig = (function () {
 	    /**
