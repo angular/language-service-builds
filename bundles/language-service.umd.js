86a4f32d05dcb6ab21eb32127aa2428fa88be7dd
Igor Minar <igor@angularjs.org>
2017-01-27 08:05:37 +0000

    b141a22 ci(aio): run docs-tests in travis (#14097)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-1e729d7
+ * @license Angular v4.0.0-beta.5-b141a22
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-1e729d7');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-b141a22');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26839,7 +26839,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-1e729d7');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-b141a22');
 
 	/**
 	 * @return {?}
@@ -46201,7 +46201,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.5-1e729d7');
+	var VERSION$3 = new Version('4.0.0-beta.5-b141a22');
 
 	/**
 	 * @license
@@ -47665,7 +47665,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.5-1e729d7');
+	var VERSION$4 = new Version('4.0.0-beta.5-b141a22');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
