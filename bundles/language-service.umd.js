b68f955314ca4a1ea27f6144ee54a70e167530eb
Miško Hevery <misko@hevery.com>
2017-01-28 04:46:16 +0000

    df7f5fc style: fix typo in CHANGELOG.md (#14009)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-24ea3f0');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-df7f5fc');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26346,7 +26346,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-24ea3f0');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-df7f5fc');
 
 	var CompilerConfig = (function () {
 	    /**
