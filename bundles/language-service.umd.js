2f4920c11d557e74f1d50263b3984286dfc98204
Miško Hevery <misko@hevery.com>
2017-01-28 04:45:24 +0000

    df7f5fc style: fix typo in CHANGELOG.md (#14009)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-24ea3f0
+ * @license Angular v4.0.0-beta.5-df7f5fc
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-24ea3f0');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-df7f5fc');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26346,7 +26346,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-24ea3f0');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-df7f5fc');
 
 	var CompilerConfig = (function () {
 	    /**
@@ -45649,7 +45649,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$3 = new Version('4.0.0-beta.5-24ea3f0');
+	var VERSION$3 = new Version('4.0.0-beta.5-df7f5fc');
 
 	/**
 	 * @license
@@ -47113,7 +47113,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.5-24ea3f0');
+	var VERSION$4 = new Version('4.0.0-beta.5-df7f5fc');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
