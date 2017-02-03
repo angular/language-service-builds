c723b2797a587686a56dfee5b22ce637c7047cf4
Misko Hevery <misko@hevery.com>
2017-02-03 05:42:08 +0000

    559cf9d docs(changelog): add changelog for 4.0.0-beta.6

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1650,7 +1650,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-69a4bb0');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-559cf9d');
 
 	/**
 	 * Inject decorator and metadata.
@@ -26373,7 +26373,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.5-69a4bb0');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.6-559cf9d');
 
 	var CompilerConfig = (function () {
 	    /**
