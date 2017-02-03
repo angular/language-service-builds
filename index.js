7590a2f7f528333e75b8dca081adc343eaeb2be8
Misko Hevery <misko@hevery.com>
2017-02-03 05:12:08 +0000

    343ee8a docs(changelog): add changelog for 2.4.6

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { LanguageServicePlugin } from './src/ts_plugin';
+export { createLanguageService } from './src/language_service';
+export { TypeScriptServiceHost, createLanguageServiceFromTypescript } from './src/typescript_host';
+export { VERSION } from './src/version';
+export default LanguageServicePlugin;
+//# sourceMappingURL=index.js.map
