b69d9202889f36302a2954cdca9682039e45ade6
Misko Hevery <misko@hevery.com>
2017-02-11 05:26:19 +0000

    2769370 ci: fix validate-commit-messages

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1654,7 +1654,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-d3f174a');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-2769370');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32061,7 +32061,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-d3f174a');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-2769370');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
