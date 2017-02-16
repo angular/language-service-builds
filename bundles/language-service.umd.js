19182a1d173d8b482b2899ab4fc44b637f0855cd
Igor Minar <iminar@google.com>
2017-02-16 18:46:58 +0000

    57461e9 docs(router): fix guards API docs (#14528)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-612f120
+ * @license Angular v4.0.0-beta.7-57461e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-612f120');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-57461e9');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32282,7 +32282,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-612f120');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-57461e9');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
@@ -54101,7 +54101,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.7-612f120');
+	var VERSION$4 = new Version('4.0.0-beta.7-57461e9');
 
 	var ROUTER_MODULE_PATH = '@angular/router';
 	var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -55784,7 +55784,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new Version('4.0.0-beta.7-612f120');
+	var VERSION$5 = new Version('4.0.0-beta.7-57461e9');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
