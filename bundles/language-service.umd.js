cf9ced3942c56d10ca2d53773db49774615651b7
Igor Minar <iminar@google.com>
2017-02-16 20:04:48 +0000

    3dbd9a0 fix(aio): fix staging build travis config (#14539)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-b36f60c
+ * @license Angular v4.0.0-beta.7-3dbd9a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-b36f60c');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-3dbd9a0');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32282,7 +32282,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-b36f60c');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-3dbd9a0');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
@@ -54101,7 +54101,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.7-b36f60c');
+	var VERSION$4 = new Version('4.0.0-beta.7-3dbd9a0');
 
 	var ROUTER_MODULE_PATH = '@angular/router';
 	var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -55784,7 +55784,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new Version('4.0.0-beta.7-b36f60c');
+	var VERSION$5 = new Version('4.0.0-beta.7-3dbd9a0');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
