6ddbfbfa6d59cee185220416eb9643f283d8526e
Igor Minar <iminar@google.com>
2017-02-16 20:05:50 +0000

    3dbd9a0 fix(aio): fix staging build travis config (#14539)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1656,7 +1656,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-b36f60c');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-3dbd9a0');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32282,7 +32282,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-b36f60c');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-3dbd9a0');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
