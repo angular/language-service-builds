095021172e249ca57229fea074bf6fd0356ad43b
Igor Minar <iminar@google.com>
2017-02-16 23:08:27 +0000

    88eb3b2 fix(aio): fix prod build (#14541)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1656,7 +1656,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-0fa3895');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-88eb3b2');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32230,7 +32230,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-0fa3895');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-88eb3b2');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
