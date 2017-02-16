bc5918edd1744ac96ead5094a27b5b62a1c8f7c4
Igor Minar <iminar@google.com>
2017-02-16 19:52:42 +0000

    b36f60c docs(router): fix broken link (#14431)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-57461e9
+ * @license Angular v4.0.0-beta.7-b36f60c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-57461e9');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-b36f60c');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32282,7 +32282,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-57461e9');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-b36f60c');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
@@ -54101,7 +54101,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.7-57461e9');
+	var VERSION$4 = new Version('4.0.0-beta.7-b36f60c');
 
 	var ROUTER_MODULE_PATH = '@angular/router';
 	var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -55784,7 +55784,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new Version('4.0.0-beta.7-57461e9');
+	var VERSION$5 = new Version('4.0.0-beta.7-b36f60c');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
