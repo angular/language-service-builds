e72ec26741c8fe9711541d771c5a7aa59ee84907
Igor Minar <iminar@google.com>
2017-02-16 18:39:05 +0000

    612f120 fix(platform-server): default unspecified sections of the url to empty string (#14512)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-4a56b6e
+ * @license Angular v4.0.0-beta.7-612f120
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-4a56b6e');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-612f120');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32282,7 +32282,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-4a56b6e');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-612f120');
 
 	/**
 	 * Temporal switch for the compiler to use the new view engine,
@@ -54101,7 +54101,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.7-4a56b6e');
+	var VERSION$4 = new Version('4.0.0-beta.7-612f120');
 
 	var ROUTER_MODULE_PATH = '@angular/router';
 	var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -55784,7 +55784,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new Version('4.0.0-beta.7-4a56b6e');
+	var VERSION$5 = new Version('4.0.0-beta.7-612f120');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
