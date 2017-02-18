4454dc01df0b13a9fba09c1425b3e9400cb8fc1b
Igor Minar <iminar@google.com>
2017-02-18 22:52:08 +0000

    5129e8e docs(router): fix typo in canLoad interface doc comment (#14519)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-0cf753b
+ * @license Angular v4.0.0-beta.7-5129e8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -949,7 +949,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-0cf753b');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-5129e8e');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32589,7 +32589,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-0cf753b');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.7-5129e8e');
 
 	var __extends$47 = (this && this.__extends) || function (d, b) {
 	    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
@@ -54392,7 +54392,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.7-0cf753b');
+	var VERSION$4 = new Version('4.0.0-beta.7-5129e8e');
 
 	var ROUTER_MODULE_PATH = '@angular/router';
 	var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -56075,7 +56075,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new Version('4.0.0-beta.7-0cf753b');
+	var VERSION$5 = new Version('4.0.0-beta.7-5129e8e');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
