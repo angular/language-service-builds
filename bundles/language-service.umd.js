d299acef534055aea1956e57d00af4c43ea097a9
Victor Berchet <victor@suumit.com>
2017-02-21 00:38:06 +0000

    601fd3e fix(platform-webworker): integrate review feedback

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -949,7 +949,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-1bdf706');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-601fd3e');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32589,7 +32589,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.8-1bdf706');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.8-601fd3e');
 
 	var __extends$47 = (this && this.__extends) || function (d, b) {
 	    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
