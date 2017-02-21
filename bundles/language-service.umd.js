b4249a3e31d8d9ac7f0888e644949d7198770bac
GitHub <noreply@github.com>
2017-02-21 02:56:54 +0000

    2a191ca fix(router): do not finish bootstrap until all the routes are resolved (#14608)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-c2e0f71
+ * @license Angular v4.0.0-beta.8-2a191ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -949,7 +949,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-c2e0f71');
+	var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-2a191ca');
 
 	/**
 	 * Inject decorator and metadata.
@@ -32592,7 +32592,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.8-c2e0f71');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-beta.8-2a191ca');
 
 	var __extends$47 = (this && this.__extends) || function (d, b) {
 	    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
@@ -54398,7 +54398,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$4 = new Version('4.0.0-beta.8-c2e0f71');
+	var VERSION$4 = new Version('4.0.0-beta.8-2a191ca');
 
 	var ROUTER_MODULE_PATH = '@angular/router';
 	var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -56081,7 +56081,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new Version('4.0.0-beta.8-c2e0f71');
+	var VERSION$5 = new Version('4.0.0-beta.8-2a191ca');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
