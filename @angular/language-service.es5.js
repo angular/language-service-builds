8b4354a9b7e779fbbc741df581da80d27fe1baa9
Igor Minar <iminar@google.com>
2017-02-22 19:49:50 +0000

    88755b0 build(aio): update dgeni-packages to 0.16.8 (#14597)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -15,7 +15,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
- * @license Angular v4.0.0-beta.8-fcc1d17
+ * @license Angular v4.0.0-beta.8-88755b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5136,6 +5136,6 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-beta.8-fcc1d17');
+var VERSION = new Version('4.0.0-beta.8-88755b0');
 
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
