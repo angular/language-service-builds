7dad348593537887f4e199be6fe4fa7bfb34e8a6
GitHub <noreply@github.com>
2017-02-23 06:22:31 +0000

    6b7937f test: fix flaky e2e inbox-app test due to net::ERR_SSL_PROTOCOL_ERROR from fetching google fonts (#14665)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -15,7 +15,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
- * @license Angular v4.0.0-beta.8-175dbce
+ * @license Angular v4.0.0-beta.8-6b7937f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5136,6 +5136,6 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-beta.8-175dbce');
+var VERSION = new Version('4.0.0-beta.8-6b7937f');
 
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
