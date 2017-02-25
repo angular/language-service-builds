90638c4102d1666e9e2f42d829a39929553b2a0a
Igor Minar <iminar@google.com>
2017-02-25 00:49:24 +0000

    2602b03 fix(animations): ignore triggers that are registered multiple times (#14723)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -821,7 +821,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION$1 = new Version('4.0.0-beta.8-9186068');
+  var VERSION$1 = new Version('4.0.0-beta.8-2602b03');
 
   /**
    * Inject decorator and metadata.
@@ -13987,7 +13987,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION = new Version('4.0.0-beta.8-9186068');
+  var VERSION = new Version('4.0.0-beta.8-2602b03');
 
   /**
    * @license
@@ -40495,7 +40495,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION$4 = new Version('4.0.0-beta.8-9186068');
+  var VERSION$4 = new Version('4.0.0-beta.8-2602b03');
 
   var ROUTER_MODULE_PATH = '@angular/router';
   var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -42179,7 +42179,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION$5 = new Version('4.0.0-beta.8-9186068');
+  var VERSION$5 = new Version('4.0.0-beta.8-2602b03');
 
   exports.createLanguageService = createLanguageService;
   exports.create = create;
