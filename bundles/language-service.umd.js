6b6ce58518044c3f53d54d34a273a1522a25f06e
Igor Minar <iminar@google.com>
2017-03-02 08:17:54 +0000

    e6e8123 build: update to yarn 0.21.3 (#14805)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -787,7 +787,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION$1 = new Version('4.0.0-rc.2-49aa508');
+  var VERSION$1 = new Version('4.0.0-rc.2-e6e8123');
 
   /**
    * Inject decorator and metadata.
@@ -11130,7 +11130,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION = new Version('4.0.0-rc.2-49aa508');
+  var VERSION = new Version('4.0.0-rc.2-e6e8123');
 
   /**
    * @license
@@ -33945,7 +33945,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION$4 = new Version('4.0.0-rc.2-49aa508');
+  var VERSION$4 = new Version('4.0.0-rc.2-e6e8123');
 
   var ROUTER_MODULE_PATH = '@angular/router';
   var ROUTER_ROUTES_SYMBOL_NAME = 'ROUTES';
@@ -35629,7 +35629,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
   /**
    * @stable
    */
-  var VERSION$5 = new Version('4.0.0-rc.2-49aa508');
+  var VERSION$5 = new Version('4.0.0-rc.2-e6e8123');
 
   exports.createLanguageService = createLanguageService;
   exports.create = create;
