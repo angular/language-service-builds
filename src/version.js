0ac0d75703177e23bb9f22455117a18efffcd194
Chuck Jazdzewski <chuckj@google.com>
2017-03-08 21:29:11 +0000

    5ad5301 feat(aio): add API list page (#14899)

--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-rc.2-174d4c8');
+export var VERSION = new Version('4.0.0-rc.2-5ad5301');
 //# sourceMappingURL=version.js.map
