5b2f04de8938196e2167f3572314091a71cb2219
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:19:08 +0000

    e7dab7e feat(aio): hard code a "Home" image link into the toolbar

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-893652a
+ * @license Angular v4.0.0-rc.3-e7dab7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4044,6 +4044,6 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-893652a');
+var VERSION = new Version('4.0.0-rc.3-e7dab7e');
 
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
