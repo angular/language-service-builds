649511de615c0bc212457ec4d5f9129a5b3b8f00
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:57 +0000

    80649ea fix(platform-server): correctly implement get href in parse5 adapter (#15022)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-778f7d6
+ * @license Angular v4.0.0-rc.3-80649ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4061,5 +4061,5 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-778f7d6');
+var VERSION = new Version('4.0.0-rc.3-80649ea');
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
