be33b0ad8b0037e6e2b3d71cf4e608215868a869
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:18:00 +0000

    6497633 feat(aio): improve search results functionality

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-8850098
+ * @license Angular v4.0.0-rc.3-6497633
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2794,7 +2794,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-8850098');
+	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-6497633');
 	/**
 	 * Inject decorator and metadata.
 	 *
@@ -15278,7 +15278,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-8850098');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-6497633');
 	/**
 	 * @license
 	 * Copyright Google Inc. All Rights Reserved.
@@ -41753,7 +41753,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new core_1.Version('4.0.0-rc.3-8850098');
+	var VERSION$5 = new core_1.Version('4.0.0-rc.3-6497633');
 
 	var __moduleExports$38 = {
 		VERSION: VERSION$5
@@ -46106,7 +46106,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('4.0.0-rc.3-8850098');
+	var VERSION = new Version('4.0.0-rc.3-6497633');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
