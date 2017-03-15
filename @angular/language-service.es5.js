4145bad81877313dec8dde9b1697efbf7780580b
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:46:44 +0000

    62d5543 fix(animations): make sure easing values are applied to an empty animate() step (#15174)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f1b33ab
+ * @license Angular v4.0.0-rc.3-62d5543
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4061,5 +4061,5 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-f1b33ab');
+var VERSION = new Version('4.0.0-rc.3-62d5543');
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
