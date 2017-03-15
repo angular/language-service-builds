7d9a8fa42ad2323da1465eb8c33b98b0e585e8c7
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 20:56:35 +0000

    80112a9 fix: fix path locally to empty.js (#15073)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-795638e
+ * @license Angular v4.0.0-rc.3-80112a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4061,5 +4061,5 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-795638e');
+var VERSION = new Version('4.0.0-rc.3-80112a9');
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
