be6a2c3ff88f9c6b077c8411b74107b57666f100
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:45:17 +0000

    f1b33ab fix(http): Make ResponseOptionsArgs an interface (#14607) (#14623)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4061,5 +4061,5 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-029d0f2');
+var VERSION = new Version('4.0.0-rc.3-f1b33ab');
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2794,7 +2794,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-029d0f2');
+	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-f1b33ab');
 	/**
 	 * Inject decorator and metadata.
 	 *
@@ -15495,7 +15495,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-029d0f2');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-f1b33ab');
 	/**
 	 * @license
 	 * Copyright Google Inc. All Rights Reserved.
@@ -42980,7 +42980,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new core_1.Version('4.0.0-rc.3-029d0f2');
+	var VERSION$5 = new core_1.Version('4.0.0-rc.3-f1b33ab');
 
 	var __moduleExports$38 = {
 		VERSION: VERSION$5
@@ -47333,7 +47333,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('4.0.0-rc.3-029d0f2');
+	var VERSION = new Version('4.0.0-rc.3-f1b33ab');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
