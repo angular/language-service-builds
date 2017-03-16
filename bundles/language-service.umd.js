c71e7c5348ce64ccd3051c2f2d41e73344fddaf7
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:04 +0000

    6e98757 fix(core): use presence of .subscribe to detect observables rather then Symbol.observable (#15171)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3131581
+ * @license Angular v4.0.0-rc.3-6e98757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2794,7 +2794,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-3131581');
+	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-6e98757');
 	/**
 	 * Inject decorator and metadata.
 	 *
@@ -4684,6 +4684,13 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	    return res;
 	}
 	/**
+	 * @license
+	 * Copyright Google Inc. All Rights Reserved.
+	 *
+	 * Use of this source code is governed by an MIT-style license that can be
+	 * found in the LICENSE file at https://angular.io/license
+	 */
+	/**
 	 * Determine if the argument is shaped like a Promise
 	 * @param {?} obj
 	 * @return {?}
@@ -4699,7 +4706,8 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	 * @return {?}
 	 */
 	function ɵisObservable(obj) {
-	    return !!(obj && obj[$$observable]);
+	    // TODO use Symbol.observable when https://github.com/ReactiveX/rxjs/issues/2415 will be resolved
+	    return !!obj && typeof obj.subscribe === 'function';
 	}
 	/**
 	 * @param {?} m1
@@ -15497,7 +15505,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-3131581');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-6e98757');
 	/**
 	 * @license
 	 * Copyright Google Inc. All Rights Reserved.
@@ -42982,7 +42990,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new core_1.Version('4.0.0-rc.3-3131581');
+	var VERSION$5 = new core_1.Version('4.0.0-rc.3-6e98757');
 
 	var __moduleExports$38 = {
 		VERSION: VERSION$5
@@ -47335,7 +47343,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('4.0.0-rc.3-3131581');
+	var VERSION = new Version('4.0.0-rc.3-6e98757');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
