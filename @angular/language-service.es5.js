2ea2e8c80d403eb1e4fddc3995559197de1cb4cd
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 16:31:07 +0000

    41f61b0 test: integration test that Angular apps compile with TS 2.2 (#15153)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f92d644
+ * @license Angular v4.0.0-rc.3-41f61b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4061,5 +4061,5 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-f92d644');
+var VERSION = new Version('4.0.0-rc.3-41f61b0');
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f92d644
+ * @license Angular v4.0.0-rc.3-41f61b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4044,6 +4044,6 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-f92d644');
+var VERSION = new Version('4.0.0-rc.3-41f61b0');
 
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
