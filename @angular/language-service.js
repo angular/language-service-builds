5a280d38e9bac2d77194cd774890eb04eac83d66
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:14:16 +0000

    992aa17 build: add source maps into NPM distribution (#15159)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a4076c7
+ * @license Angular v4.0.0-rc.3-992aa17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4044,6 +4044,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-a4076c7');
+var VERSION = new Version('4.0.0-rc.3-992aa17');
 
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
+//# sourceMappingURL=language-service.js.map
