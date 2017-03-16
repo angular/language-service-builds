5a3e6826dc12478796be2077614e2238d682f5ce
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 17:31:20 +0000

    26d4ce2 fix(forms): ensure observable validators are properly canceled (#15132)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-41f61b0
+ * @license Angular v4.0.0-rc.3-26d4ce2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2794,7 +2794,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-41f61b0');
+	var /** @type {?} */ VERSION$2 = new Version('4.0.0-rc.3-26d4ce2');
 	/**
 	 * Inject decorator and metadata.
 	 *
@@ -15505,7 +15505,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-41f61b0');
+	var /** @type {?} */ VERSION$1 = new Version('4.0.0-rc.3-26d4ce2');
 	/**
 	 * @license
 	 * Copyright Google Inc. All Rights Reserved.
@@ -42990,7 +42990,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new core_1.Version('4.0.0-rc.3-41f61b0');
+	var VERSION$5 = new core_1.Version('4.0.0-rc.3-26d4ce2');
 
 	var __moduleExports$38 = {
 		VERSION: VERSION$5
@@ -47343,7 +47343,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('4.0.0-rc.3-41f61b0');
+	var VERSION = new Version('4.0.0-rc.3-26d4ce2');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
