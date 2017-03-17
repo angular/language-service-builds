31c1730e7377b89e71642844c61a133925bcaaa6
GitHub <noreply@github.com>
2017-03-17 17:01:48 +0000

    7fb4528 fix(compiler-cli): update the tsc-wrapped dependency version (#15226)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.4-b7212f5
+ * @license Angular v4.0.0-rc.4-7fb4528
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4061,6 +4061,6 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.4-b7212f5');
+var VERSION = new Version('4.0.0-rc.4-7fb4528');
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
 //# sourceMappingURL=language-service.es5.js.map
