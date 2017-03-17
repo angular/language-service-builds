992841e9fcd23d22e17cbcd9d72917cb13e9e559
Miško Hevery <misko@hevery.com>
2017-03-17 21:33:12 +0000

    6e9264a fix(tsc-wrapped): emit flat module format correctly on Windows (#15215)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-f925910
+ * @license Angular v4.0.0-rc.5-6e9264a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2794,7 +2794,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * \@stable
 	 */
-	var VERSION$2 = new Version('4.0.0-rc.5-f925910');
+	var VERSION$2 = new Version('4.0.0-rc.5-6e9264a');
 	/**
 	 * Inject decorator and metadata.
 	 *
@@ -15625,7 +15625,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * \@stable
 	 */
-	var VERSION$1 = new Version('4.0.0-rc.5-f925910');
+	var VERSION$1 = new Version('4.0.0-rc.5-6e9264a');
 	/**
 	 * @license
 	 * Copyright Google Inc. All Rights Reserved.
@@ -43182,7 +43182,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new core_1.Version('4.0.0-rc.5-f925910');
+	var VERSION$5 = new core_1.Version('4.0.0-rc.5-6e9264a');
 
 	var __moduleExports$38 = {
 		VERSION: VERSION$5
@@ -47535,7 +47535,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('4.0.0-rc.5-f925910');
+	var VERSION = new Version('4.0.0-rc.5-6e9264a');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
