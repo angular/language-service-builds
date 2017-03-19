64fcbebbd86856d1fe6f0eb74a49d96b887d38cd
Miško Hevery <misko@hevery.com>
2017-03-19 17:36:08 +0000

    80075af fix(animations): only process element nodes through the animation engine (#15268)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2007,7 +2007,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2857,7 +2857,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.0.0-rc.5-bcc29ff');
+var VERSION$2 = new Version('4.0.0-rc.5-80075af');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16139,7 +16139,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16158,7 +16158,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.0.0-rc.5-bcc29ff');
+var VERSION$1 = new Version('4.0.0-rc.5-80075af');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44209,7 +44209,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.0.0-rc.5-bcc29ff');
+var VERSION$5 = new core_1.Version('4.0.0-rc.5-80075af');
 
 
 var version = {
@@ -44519,7 +44519,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48628,7 +48628,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.0.0-rc.5-bcc29ff');
+var VERSION$$1 = new Version('4.0.0-rc.5-80075af');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
