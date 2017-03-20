29ef42f650bb349e9b7ce57a45b16269230220e6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 15:03:27 +0000

    60c8368 build(aio): fix generated github links

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-6d4f5ce
+ * @license Angular v4.0.0-rc.5-60c8368
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4044,7 +4044,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-6d4f5ce');
+var VERSION = new Version('4.0.0-rc.5-60c8368');
 
 export { createLanguageService, create, TypeScriptServiceHost, createLanguageServiceFromTypescript, VERSION };
 //# sourceMappingURL=language-service.js.map
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-6d4f5ce
+ * @license Angular v4.0.0-rc.5-60c8368
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2794,7 +2794,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * \@stable
 	 */
-	var VERSION$2 = new Version('4.0.0-rc.5-6d4f5ce');
+	var VERSION$2 = new Version('4.0.0-rc.5-60c8368');
 	/**
 	 * Inject decorator and metadata.
 	 *
@@ -15625,7 +15625,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * \@stable
 	 */
-	var VERSION$1 = new Version('4.0.0-rc.5-6d4f5ce');
+	var VERSION$1 = new Version('4.0.0-rc.5-60c8368');
 	/**
 	 * @license
 	 * Copyright Google Inc. All Rights Reserved.
@@ -43182,7 +43182,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION$5 = new core_1.Version('4.0.0-rc.5-6d4f5ce');
+	var VERSION$5 = new core_1.Version('4.0.0-rc.5-60c8368');
 
 	var __moduleExports$38 = {
 		VERSION: VERSION$5
@@ -47535,7 +47535,7 @@ define(['exports', 'typescript', 'fs', 'path', 'reflect-metadata'], function (ex
 	/**
 	 * @stable
 	 */
-	var VERSION = new Version('4.0.0-rc.5-6d4f5ce');
+	var VERSION = new Version('4.0.0-rc.5-60c8368');
 
 	exports.createLanguageService = createLanguageService;
 	exports.create = create;
