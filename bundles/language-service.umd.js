ac85f866b43ec13c348156cbf51143cd2d038728
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 11:50:45 +0000

    1b9c809 refactor(aio): removed dead code

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2007,7 +2007,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2857,7 +2857,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.0.0-rc.5-b7ba331');
+var VERSION$2 = new Version('4.0.0-rc.5-1b9c809');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16160,7 +16160,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16179,7 +16179,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.0.0-rc.5-b7ba331');
+var VERSION$1 = new Version('4.0.0-rc.5-1b9c809');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44214,7 +44214,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.0.0-rc.5-b7ba331');
+var VERSION$5 = new core_1.Version('4.0.0-rc.5-1b9c809');
 
 
 var version = {
@@ -44524,7 +44524,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48633,7 +48633,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.0.0-rc.5-b7ba331');
+var VERSION$$1 = new Version('4.0.0-rc.5-1b9c809');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
