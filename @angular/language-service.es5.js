980d5e1904461b830dba12f3f0fc008e55d15856
Igor Minar <iminar@google.com>
2017-03-21 15:33:46 +0000

    81b6a5f build(aio): upgrade @angular/cli to 1.0.0-rc.4

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea979cf
+ * @license Angular v4.0.0-rc.5-81b6a5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4114,7 +4114,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-ea979cf');
+var VERSION = new Version('4.0.0-rc.5-81b6a5f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
