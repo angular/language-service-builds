bf7e3d3e05a4d3e2cec161aaa607e6045069ee90
Igor Minar <iminar@google.com>
2017-03-22 22:29:36 +0000

    a50d79d fix(compiler-cli): adding missing format xliff for the extractor (#15386)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4114,7 +4114,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-64285a2');
+var VERSION = new Version('4.0.0-rc.5-a50d79d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2007,7 +2007,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2860,7 +2860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.0.0-rc.5-64285a2');
+var VERSION$2 = new Version('4.0.0-rc.5-a50d79d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16202,7 +16202,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16221,7 +16221,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.0.0-rc.5-64285a2');
+var VERSION$1 = new Version('4.0.0-rc.5-a50d79d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44273,7 +44273,7 @@ var Extractor$2 = (function () {
         var format = (formatName || 'xlf').toLowerCase();
         if (format === 'xmb')
             return 'xmb';
-        if (format === 'xlf' || format === 'xlif')
+        if (format === 'xlf' || format === 'xlif' || format === 'xliff')
             return 'xlf';
         throw new Error('Unsupported format "${formatName}"');
     };
@@ -44307,7 +44307,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.0.0-rc.5-64285a2');
+var VERSION$5 = new core_1.Version('4.0.0-rc.5-a50d79d');
 
 
 var version = {
@@ -44617,7 +44617,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48726,7 +48726,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.0.0-rc.5-64285a2');
+var VERSION$$1 = new Version('4.0.0-rc.5-a50d79d');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
