536a151d57b2ba56f36ebaf0a573a610a1eebf20
Miško Hevery <misko@hevery.com>
2017-03-22 16:56:36 +0000

    c584997 fix(tsc-wrapped): use windows friendly path normalization in bundler (#15374)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-1bcbcfd
+ * @license Angular v4.0.0-rc.5-c584997
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-1bcbcfd');
+var VERSION = new Version('4.0.0-rc.5-c584997');
 
 /**
  * @license
