ecc8906f7560337189dc9205d3a53f28669a2bf5
Igor Minar <iminar@google.com>
2017-03-23 00:31:17 +0000

    26f6bd4 ci(travis): update excluded branch to g3 (#15391)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-ea49a95');
+var VERSION = new Version('4.0.0-rc.5-26f6bd4');
 
 /**
  * @license
