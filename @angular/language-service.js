73b3b629b4c6f2dc8e60c27e1527f9993865657f
Victor Berchet <victor@suumit.com>
2017-03-23 22:10:56 +0000

    b800a0c fix: prevent strictNullChecks support until #15432 is fixed (#15434)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-c8ab5cb
+ * @license Angular v4.0.0-rc.6-b800a0c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.6-c8ab5cb');
+var VERSION = new Version('4.0.0-rc.6-b800a0c');
 
 /**
  * @license
