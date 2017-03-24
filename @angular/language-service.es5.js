678bfd27556cca582f5dfc6a555a76a77700e7ad
GitHub <noreply@github.com>
2017-03-24 01:01:36 +0000

    c65b754 docs: add the 4.0.0 code name to the changelog

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-db0dca3
+ * @license Angular v4.0.0-c65b754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4114,7 +4114,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-db0dca3');
+var VERSION = new Version('4.0.0-c65b754');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
