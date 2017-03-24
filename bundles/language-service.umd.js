6d6aa66ef493be7b116de4cecea740059752d29c
Victor Berchet <victor@suumit.com>
2017-03-24 22:27:11 +0000

    53b89ec fix(compiler): allow single quotes into named interpolations (#15461)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c17b912
+ * @license Angular v4.0.0-53b89ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2007,7 +2007,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-c17b912
+ * @license Angular v4.0.0-53b89ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2860,7 +2860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.0.0-c17b912');
+var VERSION$2 = new Version('4.0.0-53b89ec');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16184,7 +16184,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-c17b912
+ * @license Angular v4.0.0-53b89ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16203,7 +16203,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.0.0-c17b912');
+var VERSION$1 = new Version('4.0.0-53b89ec');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -23447,13 +23447,13 @@ var _I18nVisitor = (function () {
     };
     return _I18nVisitor;
 }());
-var _CUSTOM_PH_EXP = /\/\/[\s\S]*i18n[\s\S]*\([\s\S]*ph[\s\S]*=[\s\S]*"([\s\S]*?)"[\s\S]*\)/g;
+var _CUSTOM_PH_EXP = /\/\/[\s\S]*i18n[\s\S]*\([\s\S]*ph[\s\S]*=[\s\S]*("|')([\s\S]*?)\1[\s\S]*\)/g;
 /**
  * @param {?} input
  * @return {?}
  */
 function _extractPlaceholderName(input) {
-    return input.split(_CUSTOM_PH_EXP)[1];
+    return input.split(_CUSTOM_PH_EXP)[2];
 }
 /**
  * @license
@@ -44335,7 +44335,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.0.0-c17b912');
+var VERSION$5 = new core_1.Version('4.0.0-53b89ec');
 
 
 var version = {
@@ -44645,7 +44645,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.0.0-c17b912
+ * @license Angular v4.0.0-53b89ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48754,7 +48754,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.0.0-c17b912');
+var VERSION$$1 = new Version('4.0.0-53b89ec');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
