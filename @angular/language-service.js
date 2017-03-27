8b1c49118ca6b202abc7fb1aabd507651621da57
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:30:54 +0000

    52ea193 build(aio): left align code regions

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-228238e');
+var VERSION = new Version('4.0.0-52ea193');
 
 /**
  * @license
