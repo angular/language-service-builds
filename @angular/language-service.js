e097226b809126b37e434f8f914da88c02facde0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:37:43 +0000

    a6fd22c docs(aio): fix typo

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-1616cae');
+var VERSION = new Version('4.0.0-a6fd22c');
 
 /**
  * @license
