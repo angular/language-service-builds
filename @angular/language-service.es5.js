2df5ef4aa1b7f2c0ccf3163b613a5ed4d024119e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 11:42:08 +0000

    837ed78 feat(aio): add code-example and code-tabs

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-52ea193
+ * @license Angular v4.0.0-837ed78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4114,7 +4114,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-52ea193');
+var VERSION = new Version('4.0.0-837ed78');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
