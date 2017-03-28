59d19180782fcb49964983bb27bdf63fc72fd39e
Victor Berchet <victor@suumit.com>
2017-03-28 16:59:08 +0000

    d05aa70 docs(core): fix typo and example in InjectionToken doc (#15449)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-eac99c1
+ * @license Angular v4.0.0-d05aa70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-eac99c1');
+var VERSION = new Version('4.0.0-d05aa70');
 
 /**
  * @license
