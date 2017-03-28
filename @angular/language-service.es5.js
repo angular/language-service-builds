3226b6d0bc272677b4eaf72263e2ab41d3370a05
Victor Berchet <victor@suumit.com>
2017-03-28 23:33:04 +0000

    0c36f23 perf(router): don't create new serializer every time UrlTree.toString is called (#15565)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a580f8c
+ * @license Angular v4.0.0-0c36f23
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4135,7 +4135,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-a580f8c');
+var VERSION = new Version('4.0.0-0c36f23');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
