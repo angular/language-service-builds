3c8918c1a37ebc5efd3e3fc7ad8bf296daf8c0bf
Victor Berchet <victor@suumit.com>
2017-03-28 00:54:49 +0000

    a9ae4da ci: add tbosch and vicb as approvers of the language service (#15530)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4102,7 +4102,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-15662ef');
+var VERSION = new Version('4.0.0-a9ae4da');
 
 /**
  * @license
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2007,7 +2007,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2860,7 +2860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.0.0-15662ef');
+var VERSION$2 = new Version('4.0.0-a9ae4da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16184,7 +16184,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16203,7 +16203,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.0.0-15662ef');
+var VERSION$1 = new Version('4.0.0-a9ae4da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44335,7 +44335,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.0.0-15662ef');
+var VERSION$5 = new core_1.Version('4.0.0-a9ae4da');
 
 
 var version = {
@@ -44645,7 +44645,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48754,7 +48754,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.0.0-15662ef');
+var VERSION$$1 = new Version('4.0.0-a9ae4da');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
