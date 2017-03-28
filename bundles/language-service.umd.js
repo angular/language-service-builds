6f9db53c774304209801b3f2028698db944def13
Victor Berchet <victor@suumit.com>
2017-03-28 23:07:33 +0000

    f368381 fix(router): should run CanActivate after CanDeactivate guards

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-93d48f1
+ * @license Angular v4.0.0-f368381
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2007,7 +2007,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-93d48f1
+ * @license Angular v4.0.0-f368381
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2860,7 +2860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.0.0-93d48f1');
+var VERSION$2 = new Version('4.0.0-f368381');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16159,7 +16159,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-93d48f1
+ * @license Angular v4.0.0-f368381
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16178,7 +16178,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.0.0-93d48f1');
+var VERSION$1 = new Version('4.0.0-f368381');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44339,7 +44339,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.0.0-93d48f1');
+var VERSION$5 = new core_1.Version('4.0.0-f368381');
 
 
 var version = {
@@ -44649,7 +44649,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.0.0-93d48f1
+ * @license Angular v4.0.0-f368381
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48779,7 +48779,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.0.0-93d48f1');
+var VERSION$$1 = new Version('4.0.0-f368381');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
