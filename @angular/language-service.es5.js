31a6d1195e9f5b034b0f4e16b02b35c13aa34834
Victor Berchet <victor@suumit.com>
2017-03-28 17:09:19 +0000

    8e03f65 refactor(router): improve flatten fn

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b7fa5de
+ * @license Angular v4.0.0-8e03f65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4114,7 +4114,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-b7fa5de');
+var VERSION = new Version('4.0.0-8e03f65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
