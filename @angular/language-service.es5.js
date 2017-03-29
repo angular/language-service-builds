84fc7db519805aa0d09886514b6c67fb36d6c5eb
Victor Berchet <victor@suumit.com>
2017-03-29 17:47:02 +0000

    9c77a7c fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-331b9f6
+ * @license Angular v4.0.0-9c77a7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4135,7 +4135,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-331b9f6');
+var VERSION = new Version('4.0.0-9c77a7c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
