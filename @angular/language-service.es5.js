775d35953089fb492db39ec45b1403ee3558a814
Victor Berchet <victor@suumit.com>
2017-03-29 20:54:24 +0000

    23bf348 refactor(router): cleanup & simplification (#15436)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-902bb2f
+ * @license Angular v4.0.0-23bf348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4135,7 +4135,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-902bb2f');
+var VERSION = new Version('4.0.0-23bf348');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
