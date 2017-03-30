e6cd1c244bfffb221221f1dce5b84ef223b70eca
Victor Berchet <victor@suumit.com>
2017-03-30 00:22:01 +0000

    67719f2 docs: add changelog for 4.1.0-beta.0

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1dda05a
+ * @license Angular v4.1.0-beta.0-67719f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4135,7 +4135,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-1dda05a');
+var VERSION = new Version('4.1.0-beta.0-67719f2');
 
 /**
  * @license
