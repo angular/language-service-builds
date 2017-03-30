7ae2f98d695ac577a87135cd803e835b9394b117
Igor Minar <iminar@google.com>
2017-03-30 07:13:16 +0000

    43a9619 feat(aio): overall margin, code, alert styles

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-3ccbe28
+ * @license Angular v4.1.0-beta.0-43a9619
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4135,7 +4135,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-3ccbe28');
+var VERSION = new Version('4.1.0-beta.0-43a9619');
 
 /**
  * @license
