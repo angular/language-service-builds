da95c92531407a5f268c87430ed98c485b967883
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 06:32:28 +0000

    e1c2e50 docs(aio): move bio images into content folder

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-08941aa
+ * @license Angular v4.1.0-beta.0-e1c2e50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4157,7 +4157,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-08941aa');
+var VERSION = new Version('4.1.0-beta.0-e1c2e50');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
