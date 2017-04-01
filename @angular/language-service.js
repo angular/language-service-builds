23abc48b81087a18e55a690ed80c378f5979148a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 17:37:26 +0000

    8c726ea fix(aio): preserve space after anchor elements

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-c8be396
+ * @license Angular v4.1.0-beta.0-8c726ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4145,7 +4145,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-c8be396');
+var VERSION = new Version('4.1.0-beta.0-8c726ea');
 
 /**
  * @license
