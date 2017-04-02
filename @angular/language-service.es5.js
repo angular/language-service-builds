0aa0614895e25e0f2e2806904ace46fb430d1cbb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 10:56:02 +0000

    34a1990 fix(aio): trim code blocks before rendering

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-e4c3882
+ * @license Angular v4.1.0-beta.0-34a1990
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4157,7 +4157,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-e4c3882');
+var VERSION = new Version('4.1.0-beta.0-34a1990');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
