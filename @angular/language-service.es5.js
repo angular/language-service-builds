1eda3d7c0aaa375a7003e8538e596f3df890c9e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 22:57:03 +0000

    0e38bf9 build(aio): new migration

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4157,7 +4157,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-309bae0');
+var VERSION = new Version('4.1.0-beta.0-0e38bf9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
