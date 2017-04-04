cbfb07d07d46ec368a94df4f43d1dbea8fd5594a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-04 10:36:11 +0000

    c6917d9 build(aio): update boilerplate to new folder structure

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-25132bf
+ * @license Angular v4.1.0-beta.0-c6917d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4145,7 +4145,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-25132bf');
+var VERSION = new Version('4.1.0-beta.0-c6917d9');
 
 /**
  * @license
