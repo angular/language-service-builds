0a5abd8ca47bdd8abc7394a8f9c8d7163f1e0f06
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 02:46:19 +0000

    f4f621a feat(animations): wip: introduce a wave of new animation features

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-c933b75
+ * @license Angular v4.1.0-beta.0-f4f621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-c933b75
+ * @license Angular v4.1.0-beta.0-f4f621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.0-beta.0-c933b75');
+var VERSION$2 = new Version('4.1.0-beta.0-f4f621a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16193,7 +16193,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-c933b75
+ * @license Angular v4.1.0-beta.0-f4f621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16212,7 +16212,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.0-beta.0-c933b75');
+var VERSION$1 = new Version('4.1.0-beta.0-f4f621a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44433,7 +44433,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.1.0-beta.0-c933b75');
+var VERSION$5 = new core_1.Version('4.1.0-beta.0-f4f621a');
 
 
 var version = {
@@ -44743,7 +44743,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.0-beta.0-c933b75
+ * @license Angular v4.1.0-beta.0-f4f621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48896,7 +48896,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.0-beta.0-c933b75');
+var VERSION$$1 = new Version('4.1.0-beta.0-f4f621a');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
