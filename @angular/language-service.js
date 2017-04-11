4fe53cbbc4a02d64891b49c710a6c472e9ac9899
Hans <hans@hansl.ca>
2017-04-11 18:31:43 +0000

    a487563 fix(router): fix query param parsing

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-46ce331
+ * @license Angular v4.1.0-beta.0-a487563
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4148,7 +4148,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-46ce331');
+var VERSION = new Version('4.1.0-beta.0-a487563');
 
 /**
  * @license
