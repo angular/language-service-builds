0f21f1ed288b38485d922b9afe42b5b4239d9bc4
Igor Minar <iminar@google.com>
2017-04-12 19:40:06 +0000

    28c2a71 docs(aio): revert file-trees back to original structure

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-540581d
+ * @license Angular v4.1.0-beta.0-28c2a71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-540581d');
+var VERSION = new Version('4.1.0-beta.0-28c2a71');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
