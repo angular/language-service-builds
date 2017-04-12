027e4ab2f556dd682c8d00ae567743555a304939
Hans Larsen <hans@hansl.ca>
2017-04-12 01:30:38 +0000

    38d75d4 feat(animations): Update types for TypeScript nullability support

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-04fb29b
+ * @license Angular v4.1.0-beta.0-38d75d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4160,7 +4160,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-04fb29b');
+var VERSION = new Version('4.1.0-beta.0-38d75d4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
