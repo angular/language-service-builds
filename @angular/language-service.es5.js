bfa330fdfc2d42748de127833a38b141b017cc4d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:18:17 +0000

    0614754 docs(aio): final migration from old site

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-8f1359d');
+var VERSION = new Version('4.1.0-beta.0-0614754');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
