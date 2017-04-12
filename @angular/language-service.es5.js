1f37feee8a00305920d01a15225eb55bdc1dc21c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 13:48:27 +0000

    2798c1b feat(aio): code block styles with header

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4160,7 +4160,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-38d75d4');
+var VERSION = new Version('4.1.0-beta.0-2798c1b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
