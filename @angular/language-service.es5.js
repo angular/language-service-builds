2d60b8369073c9e8618888385feccc067721f2ad
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 14:04:19 +0000

    075f3f8 feat(aio): search results layer fix and responsive table

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-2798c1b
+ * @license Angular v4.1.0-beta.0-075f3f8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4160,7 +4160,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-2798c1b');
+var VERSION = new Version('4.1.0-beta.0-075f3f8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
