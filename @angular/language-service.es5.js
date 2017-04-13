646cf1a376efe1ea14048ca10db9d2c6c90c3c2f
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:11:15 +0000

    70b1d6d fix(tsc-wrapped): collect new expressions with no arguments (#15908)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9394835
+ * @license Angular v4.1.0-beta.1-70b1d6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-9394835');
+var VERSION = new Version('4.1.0-beta.1-70b1d6d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
