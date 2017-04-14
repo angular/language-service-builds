999b4cb73879972667acdb5af60b44982c3a5708
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:18:25 +0000

    ce47d33 fix(compiler): ignore calls to unresolved symbols in metadata (#15970)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-2e47a0d');
+var VERSION = new Version('4.1.0-beta.1-ce47d33');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
