4a360e131230bd20e55480b3ff151bd9829528b3
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 18:36:16 +0000

    86396a4 Revert "fix(compiler): ignore calls to unresolved symbols in metadata (#15970)"

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-cb5a7ef
+ * @license Angular v4.1.0-beta.1-86396a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4153,7 +4153,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-cb5a7ef');
+var VERSION = new Version('4.1.0-beta.1-86396a4');
 
 /**
  * @license
