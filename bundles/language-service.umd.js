b663f102ca063fe507e2a78a2d5a6e6d705b4298
GitHub <noreply@github.com>
2017-04-14 01:19:07 +0000

    2e47a0d Revert "fix(forms): Update types for TypeScript nullability support (#15859)" (#15977)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-6a2e08d
+ * @license Angular v4.1.0-beta.1-2e47a0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-6a2e08d
+ * @license Angular v4.1.0-beta.1-2e47a0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.0-beta.1-6a2e08d');
+var VERSION$2 = new Version('4.1.0-beta.1-2e47a0d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16157,7 +16157,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-6a2e08d
+ * @license Angular v4.1.0-beta.1-2e47a0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16176,7 +16176,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.0-beta.1-6a2e08d');
+var VERSION$1 = new Version('4.1.0-beta.1-2e47a0d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44419,7 +44419,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.1.0-beta.1-6a2e08d');
+var VERSION$5 = new core_1.Version('4.1.0-beta.1-2e47a0d');
 
 
 var version = {
@@ -44729,7 +44729,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.0-beta.1-6a2e08d
+ * @license Angular v4.1.0-beta.1-2e47a0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -48889,7 +48889,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.0-beta.1-6a2e08d');
+var VERSION$$1 = new Version('4.1.0-beta.1-2e47a0d');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
