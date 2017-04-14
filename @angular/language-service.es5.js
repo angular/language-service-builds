e9425fa513d153e191cf4c6466c5fa7e205f0892
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:46:57 +0000

    56c46d7 fix(router): Update types for TypeScript nullability support

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c36ec9b
+ * @license Angular v4.1.0-beta.1-56c46d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-c36ec9b');
+var VERSION = new Version('4.1.0-beta.1-56c46d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
