bc0b57fb1a045fb94878af1f8d96fc0f7f1ef386
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:51:47 +0000

    16673fa build(aio): remove unused config code

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.0-beta.1-624b5a5');
+var VERSION$2 = new Version('4.1.0-beta.1-16673fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16150,7 +16150,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16169,7 +16169,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.0-beta.1-624b5a5');
+var VERSION$1 = new Version('4.1.0-beta.1-16673fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44893,7 +44893,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.1.0-beta.1-624b5a5');
+var VERSION$5 = new core_1.Version('4.1.0-beta.1-16673fa');
 
 
 var version = {
@@ -45203,7 +45203,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -49363,7 +49363,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.0-beta.1-624b5a5');
+var VERSION$$1 = new Version('4.1.0-beta.1-16673fa');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
