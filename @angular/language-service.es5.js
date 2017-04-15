38eb0f55adebb22896f98d31ec744594e1c8d28a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:37:54 +0000

    092f0df test(aio): remove dross from NavMenuComponent spec

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-dfc81c3
+ * @license Angular v4.1.0-beta.1-092f0df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-dfc81c3');
+var VERSION = new Version('4.1.0-beta.1-092f0df');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
