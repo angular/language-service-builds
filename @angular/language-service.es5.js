189ffe383654c21496a357c413a63453a6d10367
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:04 +0000

    8479e9e feat(aio): high res screen spacing

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-092f0df
+ * @license Angular v4.1.0-beta.1-8479e9e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-092f0df');
+var VERSION = new Version('4.1.0-beta.1-8479e9e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
