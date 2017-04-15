3380dae767eef4299ad67c627035243fcd26abe1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:44:45 +0000

    49dfc9f feat(aio): scroll bar styling

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49d97e1
+ * @license Angular v4.1.0-beta.1-49dfc9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-49d97e1');
+var VERSION = new Version('4.1.0-beta.1-49dfc9f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
