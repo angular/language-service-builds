f0f2ef3f00200ea1f9f5411db97402dd7d756be3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 20:43:32 +0000

    58f080a build(aio): add `doc-watch` yarn task for docs authors

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-be719e4
+ * @license Angular v4.1.0-beta.1-58f080a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4165,7 +4165,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-be719e4');
+var VERSION = new Version('4.1.0-beta.1-58f080a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
