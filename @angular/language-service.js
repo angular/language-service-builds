b8bdd4830a951d71a40a661ee2fc3612c14b359e
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 21:52:20 +0000

    38a7e0d fix(compiler): ignore calls to unresolved symbols in metadata

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a77b126
+ * @license Angular v4.1.0-beta.1-38a7e0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-a77b126');
+var VERSION = new Version('4.1.0-beta.1-38a7e0d');
 
 /**
  * @license
