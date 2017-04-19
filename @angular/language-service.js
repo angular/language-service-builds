f7d39626b849c65a1a430cbfd65c30dd2623b119
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 13:20:44 +0000

    79db6c3 feat(aio): about page style changes

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-76269f4
+ * @license Angular v4.1.0-beta.1-79db6c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4205,7 +4205,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-76269f4');
+var VERSION = new Version('4.1.0-beta.1-79db6c3');
 
 /**
  * @license
