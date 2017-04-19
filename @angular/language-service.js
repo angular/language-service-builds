f4ca333de2e5e583e69835029b63d8271a5e3368
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 07:53:34 +0000

    b668c2c build(aio): IDEs should only parse the `src` files

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-ecd0348
+ * @license Angular v4.1.0-beta.1-b668c2c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4205,7 +4205,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-ecd0348');
+var VERSION = new Version('4.1.0-beta.1-b668c2c');
 
 /**
  * @license
