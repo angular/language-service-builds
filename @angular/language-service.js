20635b2c14d5da3700076c057046ba2a078f4aa8
Miško Hevery <misko@hevery.com>
2017-04-20 04:30:46 +0000

    745731e fix(compiler): make I18NHtmlParser provider AoT-compliant (#15980)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-bfd5f27
+ * @license Angular v4.1.0-beta.1-745731e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4205,7 +4205,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-bfd5f27');
+var VERSION = new Version('4.1.0-beta.1-745731e');
 
 /**
  * @license
