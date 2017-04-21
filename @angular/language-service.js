f6bc482d278216a76dd969569a32f85f91d4878f
Matias Niemelä <matias@yearofmoo.com>
2017-04-21 04:29:16 +0000

    70384db feat(animations): wip: introduce a wave of new animation features

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-47acf3d
+ * @license Angular v4.1.0-beta.1-70384db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4205,7 +4205,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-47acf3d');
+var VERSION = new Version('4.1.0-beta.1-70384db');
 
 /**
  * @license
