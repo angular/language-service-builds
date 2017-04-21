f6f3ab1f3ec609e127fb5a8b631ee6196d7ec99c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:47:28 +0000

    87ec8d1 docs(aio): remove doble title from setup

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-b0c5d21
+ * @license Angular v4.1.0-rc.0-87ec8d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-b0c5d21
+ * @license Angular v4.1.0-rc.0-87ec8d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.0-rc.0-b0c5d21');
+var VERSION$2 = new Version('4.1.0-rc.0-87ec8d1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16150,7 +16150,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-b0c5d21
+ * @license Angular v4.1.0-rc.0-87ec8d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16169,7 +16169,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.0-rc.0-b0c5d21');
+var VERSION$1 = new Version('4.1.0-rc.0-87ec8d1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44918,7 +44918,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.1.0-rc.0-b0c5d21');
+var VERSION$5 = new core_1.Version('4.1.0-rc.0-87ec8d1');
 
 
 var version$1 = {
@@ -45228,7 +45228,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.0-rc.0-b0c5d21
+ * @license Angular v4.1.0-rc.0-87ec8d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -49441,7 +49441,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.0-rc.0-b0c5d21');
+var VERSION$$1 = new Version('4.1.0-rc.0-87ec8d1');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
