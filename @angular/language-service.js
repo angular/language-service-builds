4975ec4868480f0951ddaf3070c53b824bf548a8
Miško Hevery <misko@hevery.com>
2017-04-21 03:28:54 +0000

    5461182 docs(aio): make glossary links work again (#16201)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-cbe95f3
+ * @license Angular v4.1.0-beta.1-5461182
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4205,7 +4205,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-cbe95f3');
+var VERSION = new Version('4.1.0-beta.1-5461182');
 
 /**
  * @license
