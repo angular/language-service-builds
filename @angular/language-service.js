b67759c4f6fe89697b9e183f56c362886a5fdf75
Matias Niemelä <matias@yearofmoo.com>
2017-04-28 20:09:30 +0000

    a619991 feat(animations): introduce a wave of new animation features

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ed4eaf3
+ * @license Angular v4.1.0-a619991
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4205,7 +4205,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-ed4eaf3');
+var VERSION = new Version('4.1.0-a619991');
 
 /**
  * @license
