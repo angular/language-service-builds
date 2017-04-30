325c650e15feaac504616fc0f4fa6aafb99707a1
Miško Hevery <misko@hevery.com>
2017-04-30 16:28:33 +0000

    8d300ff fix(aio): header click should always toggle expand/collapse (#16439)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-065b76d
+ * @license Angular v4.1.0-8d300ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-065b76d');
+var VERSION = new Version('4.1.0-8d300ff');
 
 /**
  * @license
