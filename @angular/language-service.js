515230db2b664aba73ccfb78737a7f7fd6baab54
Miško Hevery <misko@hevery.com>
2017-04-30 16:31:43 +0000

    f32bfcd build(aio): convert triple backtick blocks to `code-example` tags

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-de8de84
+ * @license Angular v4.1.0-f32bfcd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-de8de84');
+var VERSION = new Version('4.1.0-f32bfcd');
 
 /**
  * @license
