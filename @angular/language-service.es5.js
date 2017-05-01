0d2302e6af8fb9106bb4687cdfbdad7fd74fe451
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:18:42 +0000

    ac220fc docs(aio): fix image size on homepage

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-d32bc5d
+ * @license Angular v4.1.0-ac220fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-d32bc5d');
+var VERSION = new Version('4.1.0-ac220fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-d32bc5d
+ * @license Angular v4.1.0-ac220fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-d32bc5d');
+var VERSION = new Version('4.1.0-ac220fc');
 
 /**
  * @license
