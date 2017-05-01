da8d51d72e6f52ab524208d0383cb14658337544
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:06:17 +0000

    65a8f7f fix(aio): search result styling

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-f32bfcd
+ * @license Angular v4.1.0-65a8f7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-f32bfcd');
+var VERSION = new Version('4.1.0-65a8f7f');
 
 /**
  * @license
