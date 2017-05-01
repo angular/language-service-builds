8f9f56d20788041df62e84f5e0eee063db02a663
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:08:32 +0000

    ff32c53 docs(aio): fix lots of broken links

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8fd904
+ * @license Angular v4.1.0-ff32c53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-c8fd904');
+var VERSION = new Version('4.1.0-ff32c53');
 
 /**
  * @license
