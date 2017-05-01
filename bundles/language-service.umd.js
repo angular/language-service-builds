2585a8401b65373db92caa54db2e9f801c1ae9ef
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:07:49 +0000

    c8fd904 fix(aio): remove max width on marketing pages and footer

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-aaa5628
+ * @license Angular v4.1.0-c8fd904
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-aaa5628
+ * @license Angular v4.1.0-c8fd904
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.0-aaa5628');
+var VERSION$2 = new Version('4.1.0-c8fd904');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16163,7 +16163,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-aaa5628
+ * @license Angular v4.1.0-c8fd904
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16182,7 +16182,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.0-aaa5628');
+var VERSION$1 = new Version('4.1.0-c8fd904');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44947,7 +44947,7 @@ var core_1$1 = require$$0$12;
 /**
  * @stable
  */
-var VERSION$5 = new core_1$1.Version('4.1.0-aaa5628');
+var VERSION$5 = new core_1$1.Version('4.1.0-c8fd904');
 
 
 var version$1 = {
@@ -45258,7 +45258,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.0-aaa5628
+ * @license Angular v4.1.0-c8fd904
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -49654,7 +49654,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.0-aaa5628');
+var VERSION$$1 = new Version('4.1.0-c8fd904');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
