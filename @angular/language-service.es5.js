c06b824cc1e39253f02343aba7f044bf0c1e8801
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 23:09:07 +0000

    c946a92 refactor(compiler): simplify AOT tests

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-21c96a5
+ * @license Angular v4.1.0-c946a92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-21c96a5');
+var VERSION = new Version('4.1.0-c946a92');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
