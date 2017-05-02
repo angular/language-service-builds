cc07334c368deb695c89d36dc5d40e9b9febde50
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:31:50 +0000

    1c8772a fix(aio): delete guide/index.md which is no longer used

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a0b9c23
+ * @license Angular v4.1.0-1c8772a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-a0b9c23');
+var VERSION = new Version('4.1.0-1c8772a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
