b09411d0a4f235acc41c528db18df22fa5102e79
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:31:02 +0000

    cb5bc76 feat(aio): move search results under search box

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-79ed0e7
+ * @license Angular v4.1.0-cb5bc76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-79ed0e7');
+var VERSION = new Version('4.1.0-cb5bc76');
 
 /**
  * @license
