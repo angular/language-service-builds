2b9e1b2bbda1f9c269906a688084700927240490
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:47:56 +0000

    8760bf7 build(aio): fix paths to template macros in overview-dump template

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8dc116
+ * @license Angular v4.1.0-8760bf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-c8dc116');
+var VERSION = new Version('4.1.0-8760bf7');
 
 /**
  * @license
