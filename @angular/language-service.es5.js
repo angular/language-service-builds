3e7a5ef97e354e16b18ea757240fc1823294d7f2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:39:09 +0000

    5ba8c14 docs(aio): remove unnecessary anchor links from styleguide

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ea9d8a6
+ * @license Angular v4.1.0-5ba8c14
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-ea9d8a6');
+var VERSION = new Version('4.1.0-5ba8c14');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
