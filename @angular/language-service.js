04fffb5720d8fc4231912ac900a8e5388d3c3914
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:07:06 +0000

    eb56ab3 fix(aio): remove top bar menu item focus jump

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-f29c6bb
+ * @license Angular v4.1.0-eb56ab3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-f29c6bb');
+var VERSION = new Version('4.1.0-eb56ab3');
 
 /**
  * @license
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-f29c6bb
+ * @license Angular v4.1.0-eb56ab3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-f29c6bb
+ * @license Angular v4.1.0-eb56ab3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.0-f29c6bb');
+var VERSION$2 = new Version('4.1.0-eb56ab3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16189,7 +16189,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-f29c6bb
+ * @license Angular v4.1.0-eb56ab3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16208,7 +16208,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.0-f29c6bb');
+var VERSION$1 = new Version('4.1.0-eb56ab3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -44973,7 +44973,7 @@ var core_1$1 = require$$0$12;
 /**
  * @stable
  */
-var VERSION$5 = new core_1$1.Version('4.1.0-f29c6bb');
+var VERSION$5 = new core_1$1.Version('4.1.0-eb56ab3');
 
 
 var version$1 = {
@@ -45284,7 +45284,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.0-f29c6bb
+ * @license Angular v4.1.0-eb56ab3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -49680,7 +49680,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.0-f29c6bb');
+var VERSION$$1 = new Version('4.1.0-eb56ab3');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
