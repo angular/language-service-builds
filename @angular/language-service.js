65df9553980344d4f0ae085a0049ae9d85c8477c
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:03:25 +0000

    f29c6bb docs(aio): fix missing title warnings

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-464701a');
+var VERSION = new Version('4.1.0-f29c6bb');
 
 /**
  * @license
