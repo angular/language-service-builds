f282f292dfd08986151278e8309eec2d1ec1b2e0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 07:22:53 +0000

    00dce16 docs(aio): grammatical fix. remove duplicate word 'our'

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c946a92
+ * @license Angular v4.1.0-00dce16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-c946a92');
+var VERSION = new Version('4.1.0-00dce16');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
