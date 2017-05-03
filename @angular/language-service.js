6bcc5c64f9a4b5dd524524a1a97b2f56347819ff
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:58:31 +0000

    5b96fb9 build(aio): rename `src/content` to `src/generated`

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1cfb263
+ * @license Angular v4.1.0-5b96fb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-1cfb263');
+var VERSION = new Version('4.1.0-5b96fb9');
 
 /**
  * @license
