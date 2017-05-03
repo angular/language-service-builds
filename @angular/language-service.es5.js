392464c1071269be7c9b1714e0742550ea01b009
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:47:36 +0000

    c8dc116 docs(aio): fixing new url on glossary date pipe

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9684d78
+ * @license Angular v4.1.0-c8dc116
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-9684d78');
+var VERSION = new Version('4.1.0-c8dc116');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
