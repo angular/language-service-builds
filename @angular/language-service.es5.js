aded52802c51e592ae6392c2443042017d2f7bc5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 13:04:49 +0000

    8931e71 build(aio): ensure that tutorial index arrives in the tutorial search area

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-895f47a
+ * @license Angular v4.1.0-8931e71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-895f47a');
+var VERSION = new Version('4.1.0-8931e71');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
