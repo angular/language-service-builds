fa460ccc06a96d9aa96e4d2a705ca660fdf2ea03
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:10:30 +0000

    0361052 docs(aio): remove hardcoded TOC

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-9449eff
+ * @license Angular v4.2.0-beta.0-0361052
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-9449eff');
+var VERSION = new Version('4.2.0-beta.0-0361052');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
