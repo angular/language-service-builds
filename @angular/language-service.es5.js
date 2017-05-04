cffd4e57f6a0b94e5947258006e8061bf42a9e9d
Martin Probst <martin@probst.io>
2017-05-04 18:12:27 +0000

    478d2ce fix(compiler): avoid a `...null` spread in extraction

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-83d9533');
+var VERSION = new Version('4.1.0-478d2ce');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
