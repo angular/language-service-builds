55d484785a22c233b1e13d00cecc072e9b6893a1
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 22:38:16 +0000

    cd28df6 docs: add changelog for 4.2.0-beta.0

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1092292
+ * @license Angular v4.2.0-beta.0-cd28df6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-1092292');
+var VERSION = new Version('4.2.0-beta.0-cd28df6');
 
 /**
  * @license
