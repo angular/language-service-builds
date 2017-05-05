e4bb6fc39795b30a9823032059c99dd03b49101e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:26:53 +0000

    2714644 docs(aio): fix broken links discovered May 4th

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-a8379a4
+ * @license Angular v4.2.0-beta.0-2714644
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-a8379a4');
+var VERSION = new Version('4.2.0-beta.0-2714644');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
