6b6e479263b6a4210b7ca46fb869bff2c1ec3e31
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:53:22 +0000

    5cf6426 docs(aio): fix sizing of marketing doc images

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-03513e9
+ * @license Angular v4.2.0-beta.0-5cf6426
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-03513e9');
+var VERSION = new Version('4.2.0-beta.0-5cf6426');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-03513e9
+ * @license Angular v4.2.0-beta.0-5cf6426
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-03513e9
+ * @license Angular v4.2.0-beta.0-5cf6426
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.0-beta.0-03513e9');
+var VERSION$2 = new Version('4.2.0-beta.0-5cf6426');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16224,7 +16224,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-03513e9
+ * @license Angular v4.2.0-beta.0-5cf6426
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16243,7 +16243,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.0-beta.0-03513e9');
+var VERSION$1 = new Version('4.2.0-beta.0-5cf6426');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -45364,7 +45364,7 @@ var core_1$1 = require$$0$12;
 /**
  * @stable
  */
-var VERSION$5 = new core_1$1.Version('4.2.0-beta.0-03513e9');
+var VERSION$5 = new core_1$1.Version('4.2.0-beta.0-5cf6426');
 
 
 var version$1 = {
@@ -45675,7 +45675,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.0-beta.0-03513e9
+ * @license Angular v4.2.0-beta.0-5cf6426
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -50071,7 +50071,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.0-beta.0-03513e9');
+var VERSION$$1 = new Version('4.2.0-beta.0-5cf6426');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
