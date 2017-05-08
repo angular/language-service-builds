8edda7a6f59cc672ece41d741e6aa3390c7825e4
Miško Hevery <misko@hevery.com>
2017-05-08 18:08:24 +0000

    c5ce040 docs(core): EventEmitter docs for isAsync defaults (#15780)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-98dd609
+ * @license Angular v4.2.0-beta.0-c5ce040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4388,7 +4388,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-98dd609');
+var VERSION = new Version('4.2.0-beta.0-c5ce040');
 
 /**
  * @license
