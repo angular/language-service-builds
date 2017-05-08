0a20faf8ea21a4029e29900daac1438a7e1cac35
Miško Hevery <misko@hevery.com>
2017-05-08 18:07:21 +0000

    98dd609 docs(animations): remove duplicate word (#16508)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-6dc6777
+ * @license Angular v4.2.0-beta.0-98dd609
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4401,7 +4401,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-6dc6777');
+var VERSION = new Version('4.2.0-beta.0-98dd609');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
