33a25773646e2f2365abedf2da7e3f6e3d559d46
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-10 23:52:31 +0000

    d28a3f7 docs(router): Change CanDeactivate to CanLoad (#16237)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-162dffb
+ * @license Angular v4.2.0-beta.1-d28a3f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2594,7 +2594,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-162dffb');
+var VERSION = new Version('4.2.0-beta.1-d28a3f7');
 
 /**
  * @license
