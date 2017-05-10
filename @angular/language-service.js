5447ebeaaffbc1f544453043f36aeed975156a26
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 11:12:37 +0000

    efa2928 docs(aio): add a progress reporter for the webpack guide

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f5335d1
+ * @license Angular v4.2.0-beta.0-efa2928
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2594,7 +2594,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-f5335d1');
+var VERSION = new Version('4.2.0-beta.0-efa2928');
 
 /**
  * @license
