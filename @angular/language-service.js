7a43b0f3c2555ecce68162a03451e711f19694d5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 12:10:13 +0000

    56833a6 docs(aio): more content fixes

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-efa2928
+ * @license Angular v4.2.0-beta.0-56833a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2594,7 +2594,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-efa2928');
+var VERSION = new Version('4.2.0-beta.0-56833a6');
 
 /**
  * @license
