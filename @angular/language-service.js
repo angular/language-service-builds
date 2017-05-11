d46c6cf1b550a777ce1af61f60f95a922cbfdd24
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-11 09:49:51 +0000

    c757e57 build(aio): serve gzipped content from the preview server

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-67dc970
+ * @license Angular v4.2.0-beta.1-c757e57
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2594,7 +2594,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-67dc970');
+var VERSION = new Version('4.2.0-beta.1-c757e57');
 
 /**
  * @license
