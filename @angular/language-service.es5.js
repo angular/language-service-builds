cea493aa44950c45a0b75d51e2ba176a9174372e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-12 13:47:32 +0000

    b016984 docs(aio): resize images

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-90bc5b2
+ * @license Angular v4.2.0-beta.1-b016984
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2601,7 +2601,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-90bc5b2');
+var VERSION = new Version('4.2.0-beta.1-b016984');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
