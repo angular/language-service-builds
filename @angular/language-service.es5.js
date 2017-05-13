3c9625413ac12b5b1b961648cb6eaf02ffea47d5
Igor Minar <iminar@google.com>
2017-05-13 09:01:24 +0000

    4a599ee ci: fix syntax error in `deploy.sh` (#16739)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-38c524d
+ * @license Angular v4.2.0-beta.1-4a599ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2601,7 +2601,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-38c524d');
+var VERSION = new Version('4.2.0-beta.1-4a599ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
