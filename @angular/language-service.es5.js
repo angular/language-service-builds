7ee05cfc10752fba1c26e35ad027059b0caa75e2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-15 10:14:05 +0000

    a9d9aa1 build(aio): automatically link code blocks to API docs

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-ecc356e
+ * @license Angular v4.2.0-beta.1-a9d9aa1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2601,7 +2601,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-ecc356e');
+var VERSION = new Version('4.2.0-beta.1-a9d9aa1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
