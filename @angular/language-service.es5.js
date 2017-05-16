d0e4d571b6d11f23c5e55944ab527687270dafc5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 09:03:41 +0000

    4419585 docs(aio): fix toh e2e test

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-39b92f7
+ * @license Angular v4.2.0-beta.1-4419585
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2601,7 +2601,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-39b92f7');
+var VERSION = new Version('4.2.0-beta.1-4419585');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
