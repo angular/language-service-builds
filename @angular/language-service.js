9f38d4f3b65be42ba8f1f7b9b9eb08ae7c21f568
Matias Niemelä <matias@yearofmoo.com>
2017-05-17 23:46:57 +0000

    712630c perf(animations): reduce size of animations bundle

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-6416e79
+ * @license Angular v4.2.0-beta.1-712630c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2626,7 +2626,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-6416e79');
+var VERSION = new Version('4.2.0-beta.1-712630c');
 
 /**
  * @license
