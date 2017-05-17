ea25468479a0346d4661da9980a08b4df174a933
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 16:00:16 +0000

    6416e79 build: do not create the bundles when updating the public API

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-238c523
+ * @license Angular v4.2.0-beta.1-6416e79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2626,7 +2626,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-238c523');
+var VERSION = new Version('4.2.0-beta.1-6416e79');
 
 /**
  * @license
