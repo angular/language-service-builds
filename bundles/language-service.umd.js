0e7e062af0efc41edbfcbfbf4a6bda563db53723
Jason Aden <jasonaden@google.com>
2017-05-18 00:47:21 +0000

    7edc5e9 docs: add changelog for 4.1.3

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2009,7 +2009,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2861,7 +2861,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.1.2-6f039d7');
+var VERSION$2 = new Version('4.1.3-7edc5e9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16348,7 +16348,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16367,7 +16367,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || function (d, b) {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.1.2-6f039d7');
+var VERSION$1 = new Version('4.1.3-7edc5e9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -45116,7 +45116,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-var VERSION$5 = new core_1.Version('4.1.2-6f039d7');
+var VERSION$5 = new core_1.Version('4.1.3-7edc5e9');
 
 
 var version$1 = {
@@ -45431,7 +45431,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -49693,7 +49693,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.1.2-6f039d7');
+var VERSION$$1 = new Version('4.1.3-7edc5e9');
 
 exports.createLanguageService = createLanguageService;
 exports.create = create;
