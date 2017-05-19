898d430a77c4c0deb344b9e46e6e23e093cd148c
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 20:12:11 +0000

    b10029c docs(animations): add documentation for new animation features

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-5d4f543
+ * @license Angular v4.2.0-beta.1-b10029c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2626,7 +2626,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-5d4f543');
+var VERSION = new Version('4.2.0-beta.1-b10029c');
 
 /**
  * @license
