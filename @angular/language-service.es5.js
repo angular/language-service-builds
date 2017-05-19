423a183ee2b639f55144039e351e85c1de15b56a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 10:43:56 +0000

    be9e8b9 fix(aio): support IE via conditionally loaded polyfills

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-8a0e565
+ * @license Angular v4.2.0-beta.1-be9e8b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2631,7 +2631,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-8a0e565');
+var VERSION = new Version('4.2.0-beta.1-be9e8b9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
