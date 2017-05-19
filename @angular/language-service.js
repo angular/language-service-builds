5d52558d384026649e1bf2503b9e36bb53f81839
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 13:30:12 +0000

    eed67dd feat(aio): update aio-shell class when sidenav opens or closes

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-be9e8b9
+ * @license Angular v4.2.0-beta.1-eed67dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2626,7 +2626,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-be9e8b9');
+var VERSION = new Version('4.2.0-beta.1-eed67dd');
 
 /**
  * @license
