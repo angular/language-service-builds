103b48f5458fcff689ee496e6f514cb75ca18935
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 21:04:16 +0000

    f1a9e3c feat(animations): introduce routeable animation support

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-b10029c
+ * @license Angular v4.2.0-beta.1-f1a9e3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2631,7 +2631,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-b10029c');
+var VERSION = new Version('4.2.0-beta.1-f1a9e3c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
