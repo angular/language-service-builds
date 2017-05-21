9cfa077f0550fdd1e1d9ecc3a9eb702520f52a5b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-21 14:55:17 +0000

    670771d build(aio): make the postinstall script more Windows-friendly

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-c8b08f3
+ * @license Angular v4.2.0-rc.0-670771d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2631,7 +2631,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-c8b08f3');
+var VERSION = new Version('4.2.0-rc.0-670771d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
