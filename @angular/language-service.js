3f75626704327d9f4098466bce9891711ae62353
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-22 13:08:52 +0000

    ce18fdb fix(aio): use the current version in the version picker

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-670771d
+ * @license Angular v4.2.0-rc.0-ce18fdb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2626,7 +2626,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-670771d');
+var VERSION = new Version('4.2.0-rc.0-ce18fdb');
 
 /**
  * @license
