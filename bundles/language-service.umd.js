da205ef575e5262cd48ed8c172aad5d1f1b714c3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-23 07:17:15 +0000

    3b28c75 test(aio): avoid warnings from Material during tests

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-f5b2ce0
+ * @license Angular v4.2.0-rc.0-3b28c75
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2014,7 +2014,7 @@ var __extends$2$1 = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-f5b2ce0
+ * @license Angular v4.2.0-rc.0-3b28c75
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2866,7 +2866,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.0-rc.0-f5b2ce0');
+var VERSION$2 = new Version('4.2.0-rc.0-3b28c75');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -17157,7 +17157,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-f5b2ce0
+ * @license Angular v4.2.0-rc.0-3b28c75
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17176,7 +17176,7 @@ var __extends$1$1 = (undefined && undefined.__extends) || (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.0-rc.0-f5b2ce0');
+var VERSION$1 = new Version('4.2.0-rc.0-3b28c75');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -46998,7 +46998,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-f5b2ce0');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-3b28c75');
 
 });
 
@@ -48999,7 +48999,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.0-rc.0-f5b2ce0
+ * @license Angular v4.2.0-rc.0-3b28c75
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51625,7 +51625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.0-rc.0-f5b2ce0');
+var VERSION$$1 = new Version('4.2.0-rc.0-3b28c75');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
