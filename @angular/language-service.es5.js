1d1dff22d71e92a58169343f473e369748b3fecc
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 00:14:37 +0000

    e7d9fd8 fix(animations): repair flicker issues with WA polyfill (#16937)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-08dfe91
+ * @license Angular v4.2.0-rc.0-e7d9fd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2631,7 +2631,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-08dfe91');
+var VERSION = new Version('4.2.0-rc.0-e7d9fd8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
