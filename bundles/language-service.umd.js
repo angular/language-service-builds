caa42a0ba0f459fcf0e45714b137eb9ebe5c3062
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-24 08:20:30 +0000

    19a509a fix(aio): use a special version of the TopBar if the screen is narrow

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-9e17a14
+ * @license Angular v4.2.0-rc.0-19a509a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2004,7 +2004,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.2.0-rc.0-9e17a14
+ * @license Angular v4.2.0-rc.0-19a509a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2796,7 +2796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.0-rc.0-9e17a14');
+var VERSION$2 = new Version('4.2.0-rc.0-19a509a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16982,7 +16982,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.2.0-rc.0-9e17a14
+ * @license Angular v4.2.0-rc.0-19a509a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17001,7 +17001,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.0-rc.0-9e17a14');
+var VERSION$1 = new Version('4.2.0-rc.0-19a509a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -46914,7 +46914,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-9e17a14');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-19a509a');
 
 });
 
@@ -48915,7 +48915,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.0-rc.0-9e17a14
+ * @license Angular v4.2.0-rc.0-19a509a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51540,7 +51540,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.0-rc.0-9e17a14');
+var VERSION$$1 = new Version('4.2.0-rc.0-19a509a');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
