308e6518e4bd727914069836a8d8172f7f21f289
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 20:58:33 +0000

    d108479 fix(animations): use a lightweight renderer for non-animation components (#17003)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-cd5bc64
+ * @license Angular v4.2.0-rc.0-d108479
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-cd5bc64');
+var VERSION = new Version('4.2.0-rc.0-d108479');
 
 /**
  * @license
