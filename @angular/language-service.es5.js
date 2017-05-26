a24364801b0cbb06750b6ba6aba91c9166f9c74a
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 19:46:33 +0000

    cd5bc64 docs(aio): update headers for toc (#16969)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-5f723cb
+ * @license Angular v4.2.0-rc.0-cd5bc64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-5f723cb');
+var VERSION = new Version('4.2.0-rc.0-cd5bc64');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
