77f10be3f673df2fe0b8228d68f3398e9eecfd02
Victor Berchet <victor@suumit.com>
2017-05-30 22:33:34 +0000

    0d894a1 fix(aio): fix ToC styling (#17110)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-e513808
+ * @license Angular v4.2.0-rc.1-0d894a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-e513808');
+var VERSION = new Version('4.2.0-rc.1-0d894a1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
