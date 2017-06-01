6ef0a0fa35b14bb9d740f3fdb7e2be8e5bf4306a
Victor Berchet <victor@suumit.com>
2017-06-01 16:57:31 +0000

    47e4fca fix(aio): swap feature headers for divs to remove anchors and fix feature image resizing (#17156)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-665e707
+ * @license Angular v4.2.0-rc.1-47e4fca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2004,7 +2004,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.2.0-rc.1-665e707
+ * @license Angular v4.2.0-rc.1-47e4fca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2796,7 +2796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.0-rc.1-665e707');
+var VERSION$2 = new Version('4.2.0-rc.1-47e4fca');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16982,7 +16982,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.2.0-rc.1-665e707
+ * @license Angular v4.2.0-rc.1-47e4fca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17001,7 +17001,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.0-rc.1-665e707');
+var VERSION$1 = new Version('4.2.0-rc.1-47e4fca');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -46982,7 +46982,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-665e707');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-47e4fca');
 
 });
 
@@ -48983,7 +48983,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.0-rc.1-665e707
+ * @license Angular v4.2.0-rc.1-47e4fca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51608,7 +51608,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.0-rc.1-665e707');
+var VERSION$$1 = new Version('4.2.0-rc.1-47e4fca');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
