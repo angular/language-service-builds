22cea2c2f57f19dd13f94cc42ba1bf21f74008ff
Victor Berchet <victor@suumit.com>
2017-06-01 16:59:14 +0000

    1338995 fix(aio): fix home learn more button alignment (#17154)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-47e4fca
+ * @license Angular v4.2.0-rc.1-1338995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-47e4fca');
+var VERSION = new Version('4.2.0-rc.1-1338995');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-47e4fca
+ * @license Angular v4.2.0-rc.1-1338995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-47e4fca');
+var VERSION = new Version('4.2.0-rc.1-1338995');
 
 /**
  * @license
