921bc029e315ce6513190d46230e493bc6f1a0a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-03 00:22:53 +0000

    819514a fix(animations): ensure web-animations understands a numeric CSS perspective value

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-b55adee
+ * @license Angular v4.2.0-rc.2-819514a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-b55adee');
+var VERSION = new Version('4.2.0-rc.2-819514a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
