6b752805621be4986301f88e1ef9937636928c38
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:38:12 +0000

    47c2a2e ci: switch CircleCI to 2.0

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-47c2a2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-7c78282');
+var VERSION = new Version('4.2.0-rc.2-47c2a2e');
 
 /**
  * @license
