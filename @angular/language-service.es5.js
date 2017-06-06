5756d10279d2fc6631b1f1f38a82af2c0b3f5149
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:27:43 +0000

    d4e1960 build(aio): update features and presskit marketing pages

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-3a121a6
+ * @license Angular v4.2.0-rc.2-d4e1960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-3a121a6');
+var VERSION = new Version('4.2.0-rc.2-d4e1960');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
