0d1e5e481a76873e04f637f357ce98a99a1416b1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:58:22 +0000

    c06f4fc docs(aio): remove tutorial from docs landing page

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-95f1ea2
+ * @license Angular v4.2.0-rc.2-c06f4fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-95f1ea2');
+var VERSION = new Version('4.2.0-rc.2-c06f4fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-95f1ea2
+ * @license Angular v4.2.0-rc.2-c06f4fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2004,7 +2004,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.2.0-rc.2-95f1ea2
+ * @license Angular v4.2.0-rc.2-c06f4fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2796,7 +2796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.0-rc.2-95f1ea2');
+var VERSION$2 = new Version('4.2.0-rc.2-c06f4fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16982,7 +16982,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.2.0-rc.2-95f1ea2
+ * @license Angular v4.2.0-rc.2-c06f4fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17001,7 +17001,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.0-rc.2-95f1ea2');
+var VERSION$1 = new Version('4.2.0-rc.2-c06f4fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47024,7 +47024,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-95f1ea2');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-c06f4fc');
 
 });
 
@@ -49025,7 +49025,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.0-rc.2-95f1ea2
+ * @license Angular v4.2.0-rc.2-c06f4fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51650,7 +51650,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.0-rc.2-95f1ea2');
+var VERSION$$1 = new Version('4.2.0-rc.2-c06f4fc');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
