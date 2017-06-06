5bcf35c8935eacc2d8c47943b102d567d16e3fec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 21:28:12 +0000

    269bbe0 fix(upgrade): call setInterval outside the Angular zone

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bb2fc6b
+ * @license Angular v4.2.0-rc.2-269bbe0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-bb2fc6b');
+var VERSION = new Version('4.2.0-rc.2-269bbe0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
