af4508553e0b84516b5db65134cdce60d9e24b28
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:29:04 +0000

    b8979c8 fix(aio): fix and clean up topbar styling

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-d4e1960
+ * @license Angular v4.2.0-rc.2-b8979c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-d4e1960');
+var VERSION = new Version('4.2.0-rc.2-b8979c8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-d4e1960
+ * @license Angular v4.2.0-rc.2-b8979c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-d4e1960');
+var VERSION = new Version('4.2.0-rc.2-b8979c8');
 
 /**
  * @license
