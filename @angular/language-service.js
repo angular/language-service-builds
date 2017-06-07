8e03340d2a9066c994e57f72a9525fc9d481eaea
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 13:11:42 +0000

    4759975 fix(aio): adjust SideNav top to avoid extra whitespace

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-078a4b0
+ * @license Angular v4.2.0-rc.2-4759975
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-078a4b0');
+var VERSION = new Version('4.2.0-rc.2-4759975');
 
 /**
  * @license
