ac40f4c64492586d7600b007373f898d51aef0c2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 14:01:52 +0000

    d837bfc fix(aio): fix scrolling to an element

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4759975
+ * @license Angular v4.2.0-rc.2-d837bfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-4759975');
+var VERSION = new Version('4.2.0-rc.2-d837bfc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
