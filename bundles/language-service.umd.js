63be9d373fc8693b613a68c2e7b26fe4cda76ae8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 18:59:43 +0000

    bb46f54 refactor(aio): use the SelectMenuComponent for all select menus

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2004,7 +2004,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2796,7 +2796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.0-rc.2-a39f7d6');
+var VERSION$2 = new Version('4.2.0-rc.2-bb46f54');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16982,7 +16982,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17001,7 +17001,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.0-rc.2-a39f7d6');
+var VERSION$1 = new Version('4.2.0-rc.2-bb46f54');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47033,7 +47033,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-a39f7d6');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-bb46f54');
 
 });
 
@@ -49034,7 +49034,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51659,7 +51659,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.0-rc.2-a39f7d6');
+var VERSION$$1 = new Version('4.2.0-rc.2-bb46f54');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
