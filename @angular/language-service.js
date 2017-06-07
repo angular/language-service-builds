70266f0a694cba480db5fc3456344f57ebc1df30
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 18:51:45 +0000

    3ddd28d fix(aio): update contrast on mediumgray color

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-fe6b39d
+ * @license Angular v4.2.0-rc.2-3ddd28d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-fe6b39d');
+var VERSION = new Version('4.2.0-rc.2-3ddd28d');
 
 /**
  * @license
