348812986b45d6a33b004a2123deb5bc422e4367
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 22:55:35 +0000

    cf0a9e0 docs: add changelog for 4.2.0

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-576d6d8
+ * @license Angular v4.2.0-cf0a9e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-576d6d8');
+var VERSION = new Version('4.2.0-cf0a9e0');
 
 /**
  * @license
