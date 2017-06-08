a4c4c4ebcd3ed0a207d75905c1ad8927fb120001
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 16:54:35 +0000

    022835b ci: update Chromium to 464641 (Chrome v59)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-33ba3e3
+ * @license Angular v4.2.0-rc.2-022835b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-33ba3e3');
+var VERSION = new Version('4.2.0-rc.2-022835b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
