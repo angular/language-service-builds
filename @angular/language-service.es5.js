5c6984be3fa61d2d192f4307a2b4c4c49c277a16
Igor Minar <iminar@google.com>
2017-06-08 08:14:05 +0000

    8be2e4c fix(aio): make the ToC left hand-side bar shorter (#17280)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a4a2901
+ * @license Angular v4.2.0-rc.2-8be2e4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-a4a2901');
+var VERSION = new Version('4.2.0-rc.2-8be2e4c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
