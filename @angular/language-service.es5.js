ea40d25a5d4efbf8d2f3d94d7b9415b9fa5abf95
Igor Minar <iminar@google.com>
2017-06-08 19:26:12 +0000

    5364b51 fix(aio): scroll to top immediately when doc changes (#17310)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-680128b
+ * @license Angular v4.2.0-rc.2-5364b51
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-680128b');
+var VERSION = new Version('4.2.0-rc.2-5364b51');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
