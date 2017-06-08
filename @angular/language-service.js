5cc819aee0ca94e4f123163bb8ceb1981c5ccf57
Igor Minar <iminar@google.com>
2017-06-08 19:35:12 +0000

    4d2ee51 fix(aio): remove glossary l-sub-section class (#17305)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-0940e6d
+ * @license Angular v4.2.0-rc.2-4d2ee51
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-0940e6d');
+var VERSION = new Version('4.2.0-rc.2-4d2ee51');
 
 /**
  * @license
