7490f97ac6014fba4b401e2ebffdf93d482bc75c
Igor Minar <iminar@google.com>
2017-06-08 19:46:55 +0000

    75a311f fix(aio): style and fix hover color for features cta button (#17342)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4d2ee51
+ * @license Angular v4.2.0-rc.2-75a311f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-4d2ee51');
+var VERSION = new Version('4.2.0-rc.2-75a311f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
