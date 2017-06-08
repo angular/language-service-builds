443ed74344ba1c617bcaf58eef3f23caf325db8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:17:05 +0000

    f370fd3 docs: "TypeScript to JavaScript" code tab renamed “ES6 + Decorators”

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-6a46cab
+ * @license Angular v4.2.0-rc.2-f370fd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-6a46cab');
+var VERSION = new Version('4.2.0-rc.2-f370fd3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
