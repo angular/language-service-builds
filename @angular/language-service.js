18b51965301863762aff976881da421e9c6ff5a0
GitHub <noreply@github.com>
2017-06-08 22:47:30 +0000

    576d6d8 feat(aio): update the home page and docs landing page content (#17325)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a222c3e
+ * @license Angular v4.2.0-rc.2-576d6d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-a222c3e');
+var VERSION = new Version('4.2.0-rc.2-576d6d8');
 
 /**
  * @license
