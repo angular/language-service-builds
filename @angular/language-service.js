07b5215dfc15d326f1f9a52cde0b1136418ed2d2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 07:38:59 +0000

    f7422a9 fix(aio): animate hamburger in/out

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-cf0a9e0
+ * @license Angular v4.2.0-f7422a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-cf0a9e0');
+var VERSION = new Version('4.2.0-f7422a9');
 
 /**
  * @license
