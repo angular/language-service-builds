78afe222a2ac2c2aa6902837d3dede0cbbfab277
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 22:04:12 +0000

    c2d31fb fix(http): move destructuring inside {Request,Response}Options ctor

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-db5e506
+ * @license Angular v4.2.0-c2d31fb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-db5e506');
+var VERSION = new Version('4.2.0-c2d31fb');
 
 /**
  * @license
