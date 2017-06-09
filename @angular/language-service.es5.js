6255e48c543ea437bde6d25969d0493e7d934ce9
Igor Minar <iminar@google.com>
2017-06-09 20:56:15 +0000

    3a99af2 docs(packaging): update homepage in @angular/tsc-wrapped (#17352)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-503fd1f
+ * @license Angular v4.2.0-3a99af2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-503fd1f');
+var VERSION = new Version('4.2.0-3a99af2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
