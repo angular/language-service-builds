ef27611aba5e87de9a03a5a6a43700db1316f47b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 18:05:59 +0000

    d961911 docs(aio): fix broken example in upgrade guide

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-e543272
+ * @license Angular v4.2.1-d961911
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-e543272');
+var VERSION = new Version('4.2.1-d961911');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
