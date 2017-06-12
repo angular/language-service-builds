d0b2952879d16bea532f92971695869f8238d947
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:20:19 +0000

    0268818 build(aio): update Google Analytics property id for the prod environment

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-234268e
+ * @license Angular v4.2.1-0268818
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-234268e');
+var VERSION = new Version('4.2.1-0268818');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
