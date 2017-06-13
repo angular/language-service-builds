824a1f6deece58d3a993515cb44afb26917c52e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 22:57:31 +0000

    11dfb68 ci: update github templates (#17466)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2004,7 +2004,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2796,7 +2796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.2-4164369');
+var VERSION$2 = new Version('4.2.2-11dfb68');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16982,7 +16982,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17001,7 +17001,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.2-4164369');
+var VERSION$1 = new Version('4.2.2-11dfb68');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -21653,7 +21653,7 @@ var Parser = (function () {
         for (var /** @type {?} */ i = 0; i < split.expressions.length; ++i) {
             var /** @type {?} */ expressionText = split.expressions[i];
             var /** @type {?} */ sourceToLex = this._stripComments(expressionText);
-            var /** @type {?} */ tokens = this._lexer.tokenize(this._stripComments(split.expressions[i]));
+            var /** @type {?} */ tokens = this._lexer.tokenize(sourceToLex);
             var /** @type {?} */ ast = new _ParseAST(input, location, tokens, sourceToLex.length, false, this.errors, split.offsets[i] + (expressionText.length - sourceToLex.length))
                 .parseChain();
             expressions.push(ast);
@@ -47037,7 +47037,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-4164369');
+exports.VERSION = new core_1.Version('4.2.2-11dfb68');
 
 });
 
@@ -49043,7 +49043,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51668,7 +51668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.2-4164369');
+var VERSION$$1 = new Version('4.2.2-11dfb68');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
