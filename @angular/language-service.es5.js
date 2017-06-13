f288a9e27359c31086d928b3e6a1fa003bd377eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 17:47:26 +0000

    98308cd docs(aio): update typescript for `examples/webpack` to same as cli

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-052331f
+ * @license Angular v4.2.2-98308cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-052331f');
+var VERSION = new Version('4.2.2-98308cd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
