eae657a61d458d769a64b229c7b1c144bd213638
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:06:44 +0000

    fa81c8e ci: use npm_install for bazel

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-98308cd
+ * @license Angular v4.2.2-fa81c8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-98308cd');
+var VERSION = new Version('4.2.2-fa81c8e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-98308cd
+ * @license Angular v4.2.2-fa81c8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-98308cd');
+var VERSION = new Version('4.2.2-fa81c8e');
 
 /**
  * @license
