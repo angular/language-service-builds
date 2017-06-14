0328072fd2e14293f4dc1b65ecc084ac58ed23b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-14 17:12:03 +0000

    a8afa65 fix(aio): specify large image for PWA splash-screen

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d378a29
+ * @license Angular v4.2.2-a8afa65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-d378a29');
+var VERSION = new Version('4.2.2-a8afa65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d378a29
+ * @license Angular v4.2.2-a8afa65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-d378a29');
+var VERSION = new Version('4.2.2-a8afa65');
 
 /**
  * @license
