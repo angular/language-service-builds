e8d5a34f166e33ab9370d1a6fe205ea8b2a44fe3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:13:36 +0000

    2c3e948 docs(aio): update about page

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-ba0f6de
+ * @license Angular v4.2.2-2c3e948
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-ba0f6de');
+var VERSION = new Version('4.2.2-2c3e948');
 
 /**
  * @license
