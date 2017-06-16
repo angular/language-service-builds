ecc77af9ae3fd800214377497a1e0eb0b3679bcc
Hans <hans@hansl.ca>
2017-06-16 18:41:50 +0000

    9315ab8 test(compiler): fix typo "mamespace"

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1ddbddb
+ * @license Angular v4.2.2-9315ab8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-1ddbddb');
+var VERSION = new Version('4.2.2-9315ab8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
