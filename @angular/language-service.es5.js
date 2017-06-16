a7107af0fbbd299a670a892a91c75b918826511f
Hans Larsen <hans@hansl.ca>
2017-06-16 21:13:56 +0000

    ab81c1c build(aio): update @angular/service-worker to 1.0.0-beta.16

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-71a8ef5');
+var VERSION = new Version('4.2.3-ab81c1c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
