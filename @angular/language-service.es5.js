c9028443b8624a7558869445274b80972fd0c077
Hans <hans@hansl.ca>
2017-06-16 19:40:14 +0000

    5ac3919 feat(core): update zone.js to 0.8.12

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fe126cb
+ * @license Angular v4.2.2-5ac3919
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fe126cb');
+var VERSION = new Version('4.2.2-5ac3919');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
