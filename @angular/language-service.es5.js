cf7bcf4cb6a6b8140ea6ecee3410172604a7c1a5
Hans <hans@hansl.ca>
2017-06-19 18:04:40 +0000

    4f37f86 fix(aio): switch from `innerText` to `textContent` to support older browsers

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-3093c55');
+var VERSION = new Version('4.2.2-4f37f86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-3093c55');
+var VERSION = new Version('4.2.2-4f37f86');
 
 /**
  * @license
