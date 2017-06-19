41330c8aa48752c3e32c0998c16e7c2754b9d633
Hans Larsen <hans@hansl.ca>
2017-06-19 22:12:57 +0000

    09512c7 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3607a48
+ * @license Angular v4.2.3-09512c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-3607a48');
+var VERSION = new Version('4.2.3-09512c7');
 
 /**
  * @license
