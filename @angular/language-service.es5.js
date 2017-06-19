2e153904e89b246fad84415743b14f11f96eb368
Hans Larsen <hans@hansl.ca>
2017-06-19 19:00:41 +0000

    3607a48 docs: remove aio from changelog

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-ab81c1c
+ * @license Angular v4.2.3-3607a48
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-ab81c1c');
+var VERSION = new Version('4.2.3-3607a48');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
