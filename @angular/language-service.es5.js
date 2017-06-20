0179bdf1900c4d6c6d56b42f4fe8c0e47c9be8fd
Hans <hans@hansl.ca>
2017-06-20 19:20:38 +0000

    4e6be15 fix(language-service): rollup `tslib` into the language service package

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3e685f9
+ * @license Angular v4.2.2-4e6be15
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-3e685f9');
+var VERSION = new Version('4.2.2-4e6be15');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
