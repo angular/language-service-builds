3b8649ae41edfd375053c06c222677dacfc7ef4a
Hans <hans@hansl.ca>
2017-06-20 20:25:39 +0000

    009651e refactor(core): remove toString() method from DefaultKeyValueDiffer

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-c59c390
+ * @license Angular v4.2.2-009651e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-c59c390');
+var VERSION = new Version('4.2.2-009651e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-c59c390
+ * @license Angular v4.2.2-009651e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-c59c390');
+var VERSION = new Version('4.2.2-009651e');
 
 /**
  * @license
