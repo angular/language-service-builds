c4412d1903d39a8249b3675a6df07aa316c01558
Hans Larsen <hans@hansl.ca>
2017-06-20 21:53:34 +0000

    59299de fix(compiler-cli): find lazy routes in nested module import arrays

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-f997937
+ * @license Angular v4.2.3-59299de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-f997937');
+var VERSION = new Version('4.2.3-59299de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
