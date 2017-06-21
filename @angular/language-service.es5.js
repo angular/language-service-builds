bc5b967dd3004b6facaeba76b651d25e38002b84
Hans <hans@hansl.ca>
2017-06-21 18:56:51 +0000

    0034bb2 fix(aio): restore component-styles/exclude hidden children

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d168528
+ * @license Angular v4.2.2-0034bb2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-d168528');
+var VERSION = new Version('4.2.2-0034bb2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
