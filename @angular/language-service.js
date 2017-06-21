3f26edcfa38edb934e2c6addd430703101027343
Hans <hans@hansl.ca>
2017-06-21 21:12:10 +0000

    1d93cf2 build(aio): ensure all doc tests are run

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0034bb2
+ * @license Angular v4.2.2-1d93cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-0034bb2');
+var VERSION = new Version('4.2.2-1d93cf2');
 
 /**
  * @license
