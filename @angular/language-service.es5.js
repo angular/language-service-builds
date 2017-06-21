a295b5811f77f554b49c2a8c3ee0abb15d9c4a81
Hans <hans@hansl.ca>
2017-06-21 22:00:35 +0000

    34cc3f2 feat(aio): display “Searching ..." while building search index

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-97fd248
+ * @license Angular v4.2.2-34cc3f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-97fd248');
+var VERSION = new Version('4.2.2-34cc3f2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
