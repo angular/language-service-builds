1db8e27e4f4d3074ac958880bd7b15df090d7842
Hans Larsen <hans@hansl.ca>
2017-06-22 00:43:52 +0000

    8a09015 docs: add changelog for 4.2.4

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-d203136
+ * @license Angular v4.2.4-8a09015
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-d203136');
+var VERSION = new Version('4.2.4-8a09015');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-d203136
+ * @license Angular v4.2.4-8a09015
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-d203136');
+var VERSION = new Version('4.2.4-8a09015');
 
 /**
  * @license
