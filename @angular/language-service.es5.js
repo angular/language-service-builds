3c4ee0c13f0c0720a5916122637c531873ef79d0
Hans Larsen <hans@hansl.ca>
2017-06-22 00:17:27 +0000

    d203136 revert: feat(core): update zone.js to 0.8.12

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-74954f1
+ * @license Angular v4.2.3-d203136
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-74954f1');
+var VERSION = new Version('4.2.3-d203136');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-74954f1
+ * @license Angular v4.2.3-d203136
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-74954f1');
+var VERSION = new Version('4.2.3-d203136');
 
 /**
  * @license
