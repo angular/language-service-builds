4f416e9605d66b17cf78876cf2f5418e220843a7
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:40:40 +0000

    5dd5bfd docs(aio): remove "_" from private property name

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-1a947e4
+ * @license Angular v4.3.0-beta.0-5dd5bfd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-1a947e4');
+var VERSION = new Version('4.3.0-beta.0-5dd5bfd');
 
 /**
  * @license
