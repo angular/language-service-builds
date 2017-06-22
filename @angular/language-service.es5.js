842d1a9bb0f4d3fa5d3acebe306bee7a23f05795
Hans Larsen <hans@hansl.ca>
2017-06-22 00:29:12 +0000

    02a38d3 docs: add changelog for 4.2.4

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-37cdc4f');
+var VERSION = new Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
