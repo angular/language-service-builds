ddf9a9317a1e888ce5428210833bbe3a4e690d94
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:33:45 +0000

    400486c docs(aio): correct typos in Tour of Heroes `HeroSearchService`

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-133b5e6
+ * @license Angular v4.3.0-beta.0-400486c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-133b5e6');
+var VERSION = new Version('4.3.0-beta.0-400486c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
