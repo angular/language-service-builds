cc3ed03fbb1face3f63216043c368df1983cdc19
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:06:41 +0000

    8c129d7 docs: Fix some non-code being formatted as code in the router docs

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-5c576d3
+ * @license Angular v4.3.0-beta.0-8c129d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-5c576d3');
+var VERSION = new Version('4.3.0-beta.0-8c129d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
