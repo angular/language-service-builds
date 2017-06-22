d720ee66d466388f94ef89f65c040e463655bf17
Hans Larsen <hans@hansl.ca>
2017-06-22 00:48:29 +0000

    1fc0d05 docs: add changelog for 4.3.0-beta.0

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-02a38d3
+ * @license Angular v4.3.0-beta.0-1fc0d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-02a38d3');
+var VERSION = new Version('4.3.0-beta.0-1fc0d05');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
