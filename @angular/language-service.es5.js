61fc8baf63927c1b0d6c56d5d2c9ce543b7447c7
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:08:49 +0000

    494a0d0 fix(aio): fix topbar nav-item focus style

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-7ba720a
+ * @license Angular v4.3.0-beta.0-494a0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-7ba720a');
+var VERSION = new Version('4.3.0-beta.0-494a0d0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
