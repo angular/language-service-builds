4578d317ac61e4ccadf64e9783f19c0587e64b3f
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:33:15 +0000

    353de51 docs(aio): replace “Angular 1” in upgrade phonecat script

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-8a09015
+ * @license Angular v4.2.4-353de51
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.4-8a09015');
+var VERSION = new Version('4.2.4-353de51');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-8a09015
+ * @license Angular v4.2.4-353de51
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.4-8a09015');
+var VERSION = new Version('4.2.4-353de51');
 
 /**
  * @license
