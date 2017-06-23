99ef736c2c356d8a1d7822a0047e9bb198898b12
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:14:32 +0000

    81734cf refactor(compiler): add tracking comment for self-import detection

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-b754e60
+ * @license Angular v4.3.0-beta.0-81734cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-b754e60');
+var VERSION = new Version('4.3.0-beta.0-81734cf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
