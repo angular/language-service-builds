498c44e634cacceb0fb91532bf8baf4ff9ad5a9e
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:49:49 +0000

    08ecfd8 build: No longer need to bazel build twice

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-3ce9d51
+ * @license Angular v4.3.0-beta.0-08ecfd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-3ce9d51');
+var VERSION = new Version('4.3.0-beta.0-08ecfd8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
