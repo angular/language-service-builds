420c3c731b986ac8f4238518e19755a99d82c411
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:47:12 +0000

    7ba720a docs(aio): resources.json - replace “Angular 2” in titles/descriptions

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-eacc36b
+ * @license Angular v4.3.0-beta.0-7ba720a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-eacc36b');
+var VERSION = new Version('4.3.0-beta.0-7ba720a');
 
 /**
  * @license
