e2a834cfdbe70464d37f2c3eb9bc3de40fc40a48
Matias Niemelä <matias@yearofmoo.com>
2017-06-24 00:53:32 +0000

    263c1a1 refactor: remove unused imports of the deprecated Renderer

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-43c187b
+ * @license Angular v4.3.0-beta.0-263c1a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-43c187b');
+var VERSION = new Version('4.3.0-beta.0-263c1a1');
 
 /**
  * @license
