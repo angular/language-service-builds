c1b886368c985d5c1d70794c6f36bd2ca467010c
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 16:54:33 +0000

    4268c82 feat(aio): use shorter URLs for previews

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-af14b1e
+ * @license Angular v4.3.0-beta.0-4268c82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-af14b1e');
+var VERSION = new Version('4.3.0-beta.0-4268c82');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
