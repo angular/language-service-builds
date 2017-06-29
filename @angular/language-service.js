fed31622ed2c563b2da5d41f584646f48fe38628
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:05:50 +0000

    96b1703 feat(aio): cross reference readme and docs styleguide

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-e47a77f');
+var VERSION = new Version('4.3.0-beta.0-96b1703');
 
 /**
  * @license
