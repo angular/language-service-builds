01fc73ef217519b8f3af95f3dea984194aef5489
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 01:02:24 +0000

    61e6618 docs: add changelog for 4.2.5

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-f31b0d6
+ * @license Angular v4.2.5-61e6618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2030,7 +2030,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.2.4-f31b0d6
+ * @license Angular v4.2.5-61e6618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2822,7 +2822,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.2.4-f31b0d6');
+var VERSION$2 = new Version('4.2.5-61e6618');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16981,7 +16981,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.2.4-f31b0d6
+ * @license Angular v4.2.5-61e6618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17000,7 +17000,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.2.4-f31b0d6');
+var VERSION$1 = new Version('4.2.5-61e6618');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47035,7 +47035,7 @@ var core_1 = require$$0$12;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.4-f31b0d6');
+exports.VERSION = new core_1.Version('4.2.5-61e6618');
 
 });
 
@@ -49047,7 +49047,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.2.4-f31b0d6
+ * @license Angular v4.2.5-61e6618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51672,7 +51672,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.2.4-f31b0d6');
+var VERSION$$1 = new Version('4.2.5-61e6618');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
