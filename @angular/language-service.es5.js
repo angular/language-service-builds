adafb4defae62028b3350b2c44f9276421fa7cd1
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 00:36:49 +0000

    40921bb docs: add changelog for 4.2.5

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-dfcca66
+ * @license Angular v4.3.0-beta.1-40921bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-dfcca66');
+var VERSION = new Version('4.3.0-beta.1-40921bb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
