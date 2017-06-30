1e76854ca17e5c8247fe749d42cdb1df967443f9
Stephen Fluin <stephen.fluin@gmail.com>
2017-06-30 19:12:43 +0000

    69ec333 docs(aio): Add resources contributor info

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-67519eb
+ * @license Angular v4.3.0-beta.1-69ec333
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-67519eb');
+var VERSION = new Version('4.3.0-beta.1-69ec333');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
