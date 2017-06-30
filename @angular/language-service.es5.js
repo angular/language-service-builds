32b7289423dd5a0530ee9ff52aa528ef515b9fd6
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:53:00 +0000

    27b87e8 docs(aio): fix quickstart-cli bad HTML escape chars

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-7080fcc
+ * @license Angular v4.2.5-27b87e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-7080fcc');
+var VERSION = new Version('4.2.5-27b87e8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
