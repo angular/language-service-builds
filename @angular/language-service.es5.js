e8fa4ced967b569dddd99101e1fdaef726e5d602
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:16:04 +0000

    54f96bf fix(aio): do not include hidden content in window title

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-61e6618
+ * @license Angular v4.2.5-54f96bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-61e6618');
+var VERSION = new Version('4.2.5-54f96bf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
