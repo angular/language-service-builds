1cf7129167b2c2837bd3e093a24acbc476eebabb
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:06:39 +0000

    c460757 docs(aio): Add resources contributor info

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-1a476b8
+ * @license Angular v4.2.5-c460757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-1a476b8');
+var VERSION = new Version('4.2.5-c460757');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
