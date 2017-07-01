0c87b6d06ff124ee2327c16d36b1551ba8144208
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:09:37 +0000

    8e00161 docs(aio): Add resources contributor info

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-01d4eae
+ * @license Angular v4.3.0-beta.1-8e00161
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-01d4eae');
+var VERSION = new Version('4.3.0-beta.1-8e00161');
 
 /**
  * @license
