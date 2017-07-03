3d235881fceb92a947cfd86e02df6dcb949c687e
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:45:27 +0000

    571adbe docs(aio): fix visual-studio-2015 typo

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-10bfa8a
+ * @license Angular v4.2.5-571adbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-10bfa8a');
+var VERSION = new Version('4.2.5-571adbe');
 
 /**
  * @license
