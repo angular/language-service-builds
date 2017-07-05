91b0549c222c6610ac47c6234d333a02889a674c
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:02:19 +0000

    c6b75b0 docs: add animations package to CONTRIBUTING.md (#15413)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-0d7e1a9
+ * @license Angular v4.3.0-beta.1-c6b75b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-0d7e1a9');
+var VERSION = new Version('4.3.0-beta.1-c6b75b0');
 
 /**
  * @license
