45cda9d337f417b78e766a5a55c2c3f8f6ca703d
Jason Aden <jasonaden@angular.io>
2017-07-05 22:57:23 +0000

    578bdeb fix(router): export missing UrlMatcher and UrlMatchResult types

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b51697c
+ * @license Angular v4.3.0-beta.1-578bdeb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-b51697c');
+var VERSION = new Version('4.3.0-beta.1-578bdeb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
