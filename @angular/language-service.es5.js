75d5951007c670ffddd1ff3f9c9abc2169f28ef2
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 21:58:35 +0000

    0d7e1a9 docs(changelog): Correct typos (#16966)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-9d15d85
+ * @license Angular v4.3.0-beta.1-0d7e1a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-9d15d85');
+var VERSION = new Version('4.3.0-beta.1-0d7e1a9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
