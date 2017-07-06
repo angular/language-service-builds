21040740753fdb2418cae9b6d65e4d4754025ab6
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-06 14:20:39 +0000

    670f2e5 ci(aio): raise polyfill payload limits

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-408bb89
+ * @license Angular v4.2.5-670f2e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-408bb89');
+var VERSION = new Version('4.2.5-670f2e5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
