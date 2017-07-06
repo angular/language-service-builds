25781ce85ad1722b14c80e16d2ec826db2b1f38a
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:19:52 +0000

    f2f61c9 ci: allow chuck to approve animations (#17940)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-ec58246
+ * @license Angular v4.3.0-beta.1-f2f61c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-ec58246');
+var VERSION = new Version('4.3.0-beta.1-f2f61c9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
