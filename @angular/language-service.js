9b8ab047e95cbc2f12f3e950e157030a051f3866
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:54:53 +0000

    9dd550f ci(aio): only deploy latest commits to staging/production (#17988)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-abbac4b
+ * @license Angular v4.3.0-beta.1-9dd550f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-abbac4b');
+var VERSION = new Version('4.3.0-beta.1-9dd550f');
 
 /**
  * @license
