4ee6e62a335d655d45222a5b7d97e590f06b5d20
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 22:14:55 +0000

    c81ad9d feat(common): two missing features in HttpClient (#17996)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-37797e2
+ * @license Angular v4.3.0-beta.1-c81ad9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-37797e2');
+var VERSION = new Version('4.3.0-beta.1-c81ad9d');
 
 /**
  * @license
