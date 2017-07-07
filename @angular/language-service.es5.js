74cfcae98a9da2b9bd9eea1ce31fa0c6b7b6e9ee
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:05:18 +0000

    3b2d2c4 fix(language-service): do not crash when hovering over a label definitions (#17974)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-227dbbc
+ * @license Angular v4.3.0-beta.1-3b2d2c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-227dbbc');
+var VERSION = new Version('4.3.0-beta.1-3b2d2c4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
