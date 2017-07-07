1453e4712778091f4a7b2b734cbde2cd93e6f6fd
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:38:23 +0000

    504500d fix(aio): activate ServiceWorker updates asap (#17699)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-e1174f3
+ * @license Angular v4.3.0-beta.1-504500d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.0-beta.1-e1174f3
+ * @license Angular v4.3.0-beta.1-504500d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.0-beta.1-e1174f3');
+var VERSION$2 = new Version('4.3.0-beta.1-504500d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16924,7 +16924,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.0-beta.1-e1174f3
+ * @license Angular v4.3.0-beta.1-504500d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16943,7 +16943,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.0-beta.1-e1174f3');
+var VERSION$1 = new Version('4.3.0-beta.1-504500d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47042,7 +47042,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-e1174f3');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-504500d');
 
 });
 
@@ -50492,7 +50492,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.0-beta.1-e1174f3
+ * @license Angular v4.3.0-beta.1-504500d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53121,7 +53121,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.0-beta.1-e1174f3');
+var VERSION$$1 = new Version('4.3.0-beta.1-504500d');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
