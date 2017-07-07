7f8178a1fba2f396c47188dd5d5b6414559cfa6e
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:14:01 +0000

    6bae73c refactor(common): replace `Object.assign` with the spread operator (#17982)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-11db3bd');
+var VERSION = new Version('4.3.0-beta.1-6bae73c');
 
 /**
  * @license
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.0-beta.1-11db3bd');
+var VERSION$2 = new Version('4.3.0-beta.1-6bae73c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16924,7 +16924,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16943,7 +16943,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.0-beta.1-11db3bd');
+var VERSION$1 = new Version('4.3.0-beta.1-6bae73c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47042,7 +47042,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-11db3bd');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-6bae73c');
 
 });
 
@@ -50492,7 +50492,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53121,7 +53121,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.0-beta.1-11db3bd');
+var VERSION$$1 = new Version('4.3.0-beta.1-6bae73c');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
