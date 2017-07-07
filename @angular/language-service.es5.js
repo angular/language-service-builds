0b100bc4211e925e20da7b8e5d734d4b0ef29eec
Jason Aden <jasonaden@angular.io>
2017-07-07 16:13:11 +0000

    11db3bd fix(upgrade): bring the dynamic version closer to the static one (#17971)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-3b2d2c4
+ * @license Angular v4.3.0-beta.1-11db3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-3b2d2c4');
+var VERSION = new Version('4.3.0-beta.1-11db3bd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
