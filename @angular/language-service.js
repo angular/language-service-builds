953bf39150e4075eaaffae1f6470296ed684a0c9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:53:33 +0000

    b3d9036 fix(aio): add new common/http package into API list

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-cb76091
+ * @license Angular v4.3.0-rc.0-b3d9036
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-cb76091');
+var VERSION = new Version('4.3.0-rc.0-b3d9036');
 
 /**
  * @license
