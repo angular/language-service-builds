7310d08229d54e02b1168c6391ede806ed464e3d
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:53:49 +0000

    ab13f1f build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-b69f0fa');
+var VERSION = new Version('4.2.6-ab13f1f');
 
 /**
  * @license
