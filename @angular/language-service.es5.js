c28cb754369e897d5557419f256216f888f7b89b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 12:02:19 +0000

    2055634 feat(aio): add API endpoint for notifying about PR updates

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b3d9036
+ * @license Angular v4.3.0-rc.0-2055634
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-b3d9036');
+var VERSION = new Version('4.3.0-rc.0-2055634');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
