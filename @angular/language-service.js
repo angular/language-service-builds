7577003f0cdd60fbb8c88e3a9eb9c025ca30b1cf
Igor Minar <iminar@google.com>
2017-07-11 22:31:37 +0000

    26b9492 docs(aio): Added resource link to amexio-ng-extensions [duplicate of #17785] (#17790)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-20127c1
+ * @license Angular v4.3.0-rc.0-26b9492
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-20127c1');
+var VERSION = new Version('4.3.0-rc.0-26b9492');
 
 /**
  * @license
