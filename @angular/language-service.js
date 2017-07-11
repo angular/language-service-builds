c8403dcb4fd781736bd41c22d0582e524e8e0a15
Igor Minar <iminar@google.com>
2017-07-11 19:09:16 +0000

    4d45fe6 fix(aio): fix tab animations on CodeTabsComponent

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e131f6b
+ * @license Angular v4.3.0-rc.0-4d45fe6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-e131f6b');
+var VERSION = new Version('4.3.0-rc.0-4d45fe6');
 
 /**
  * @license
