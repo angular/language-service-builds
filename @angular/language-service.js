966e2e343c4a82296e56fc4da26ec3614cdd98c2
Igor Minar <iminar@google.com>
2017-07-11 19:17:25 +0000

    d9b03be test: git ignore e2e test folders (#17984)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b6aad07
+ * @license Angular v4.3.0-rc.0-d9b03be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-b6aad07');
+var VERSION = new Version('4.3.0-rc.0-d9b03be');
 
 /**
  * @license
