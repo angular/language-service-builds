b1135e67199301a2d613674ef242b15784ab3d12
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:13:50 +0000

    c6aacf5 build(aio): remove unused script and GitHub token

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-bd4ec5c
+ * @license Angular v4.2.6-c6aacf5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-bd4ec5c');
+var VERSION = new Version('4.2.6-c6aacf5');
 
 /**
  * @license
