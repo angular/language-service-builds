f6b5553cde850c1525d3632e072e7d87225d5070
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:25:41 +0000

    3f29348 ci: add aio/content to aio group

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2625,7 +2625,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-09261e6');
+var VERSION = new Version('4.2.6-3f29348');
 
 /**
  * @license
