ea0cfcabebae221683dc53535bb71fd7a123a8d4
Igor Minar <iminar@google.com>
2017-07-11 19:14:34 +0000

    b6aad07 build: remove duplicate dependency on TypeScript

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4d45fe6
+ * @license Angular v4.3.0-rc.0-b6aad07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-4d45fe6');
+var VERSION = new Version('4.3.0-rc.0-b6aad07');
 
 /**
  * @license
