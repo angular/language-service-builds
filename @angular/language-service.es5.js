8a14c7e4f103d0345b921dab4077cc8cc47adcf9
GitHub <noreply@github.com>
2017-07-11 23:17:36 +0000

    edb8375 docs: adjust the release schedule with the 4.3.0-rc.0 release date

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e110a80
+ * @license Angular v4.3.0-rc.0-edb8375
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-e110a80');
+var VERSION = new Version('4.3.0-rc.0-edb8375');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
