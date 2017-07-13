c501d0807533105b14e39351f0cbfe0d54881b9e
Igor Minar <igor@angularjs.org>
2017-07-13 20:09:32 +0000

    e8b98a7 docs(aio): add Dart as a language option (#18080)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-afd5d72
+ * @license Angular v4.2.6-e8b98a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-afd5d72');
+var VERSION = new Version('4.2.6-e8b98a7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
