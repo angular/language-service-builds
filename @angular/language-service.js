d3ce57eb94b9cf94ef1422dde0a34bad83ef59ab
Igor Minar <iminar@google.com>
2017-07-13 19:49:33 +0000

    bc3b2ac docs(aio): Updated resources to UI Components (#18067)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4f1e4ff
+ * @license Angular v4.3.0-rc.0-bc3b2ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-4f1e4ff');
+var VERSION = new Version('4.3.0-rc.0-bc3b2ac');
 
 /**
  * @license
