ef42ee16fe4972cee74e1bd3dd380113a9b89390
Igor Minar <iminar@google.com>
2017-07-13 22:28:54 +0000

    db96c96 fix(aio): add <hr> in between overloads

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-ce0f4f0
+ * @license Angular v4.3.0-rc.0-db96c96
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-ce0f4f0');
+var VERSION = new Version('4.3.0-rc.0-db96c96');
 
 /**
  * @license
