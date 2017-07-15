c2981a9cf0be6017fe28e04df5ad746f11b11da4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:54:55 +0000

    95698d9 fix(aio): remove title from callout

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c649da9
+ * @license Angular v4.3.0-95698d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-c649da9');
+var VERSION = new Version('4.3.0-95698d9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
