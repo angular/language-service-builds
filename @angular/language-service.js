78d767ed70ada82f996b49ae718338e7fedca4dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 22:08:44 +0000

    375d598 docs(aio): add George's fixes

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-9f99f4f
+ * @license Angular v4.3.0-375d598
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-9f99f4f');
+var VERSION = new Version('4.3.0-375d598');
 
 /**
  * @license
