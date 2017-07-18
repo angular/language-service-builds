68a15bf350ce1f5ea6a34353465c1d81ced216dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:39:10 +0000

    4ce29f3 fix(platform-server): provide XhrFactory for HttpClient

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-f19bd5f
+ * @license Angular v4.3.0-4ce29f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-f19bd5f');
+var VERSION = new Version('4.3.0-4ce29f3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
