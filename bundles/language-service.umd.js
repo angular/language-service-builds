91c87662437abe70e1fd215e19474d50076003b9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:13:00 +0000

    c453b7b build(aio): fail doc-gen if referenced images are missing

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.0-3f331b5');
+var VERSION$2 = new Version('4.3.0-c453b7b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16989,7 +16989,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17008,7 +17008,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.0-3f331b5');
+var VERSION$1 = new Version('4.3.0-c453b7b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47186,7 +47186,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-3f331b5');
+exports.VERSION = new core_1.Version('4.3.0-c453b7b');
 
 });
 
@@ -50669,7 +50669,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53298,7 +53298,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.0-3f331b5');
+var VERSION$$1 = new Version('4.3.0-c453b7b');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
