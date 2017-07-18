b909e49dec784f4dffefbf9e781913f447ebb98e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 23:54:10 +0000

    5344be5 fix(animations): make sure @.disabled works in non-animation components

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-23146c9
+ * @license Angular v4.3.0-5344be5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-23146c9');
+var VERSION = new Version('4.3.0-5344be5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
