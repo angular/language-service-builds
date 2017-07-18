d116e6f6e778d6c647ff3ae4e2048f285037d917
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:11:29 +0000

    36faba1 fix(aio): add quote to module

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-92179bc
+ * @license Angular v4.3.0-36faba1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-92179bc');
+var VERSION = new Version('4.3.0-36faba1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
