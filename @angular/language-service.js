1dd70d3f15ccb255576ef62f2356cf0711519d9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:36:55 +0000

    ad3029e fix(router): should run resolvers for the same route concurrently

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-2a2fe11
+ * @license Angular v4.3.0-ad3029e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-2a2fe11');
+var VERSION = new Version('4.3.0-ad3029e');
 
 /**
  * @license
