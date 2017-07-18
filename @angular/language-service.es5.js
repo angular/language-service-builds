9bc7fce2426a36ccd3640c93fe362e6af24ceed1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:00:28 +0000

    95635c1 fix(compiler): ensure jit external id arguments names are unique

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e20cfe1
+ * @license Angular v4.3.0-95635c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-e20cfe1');
+var VERSION = new Version('4.3.0-95635c1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e20cfe1
+ * @license Angular v4.3.0-95635c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-e20cfe1');
+var VERSION = new Version('4.3.0-95635c1');
 
 /**
  * @license
