4a4a1ecbcf7fe8e98c6bfc01f6f312d07d9b8217
Miško Hevery <misko@hevery.com>
2017-07-20 16:56:24 +0000

    31cb418 docs(aio): Fix http guide

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8de44cf
+ * @license Angular v5.0.0-beta.0-31cb418
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-8de44cf');
+var VERSION = new Version('5.0.0-beta.0-31cb418');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8de44cf
+ * @license Angular v5.0.0-beta.0-31cb418
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-8de44cf');
+var VERSION = new Version('5.0.0-beta.0-31cb418');
 
 /**
  * @license
