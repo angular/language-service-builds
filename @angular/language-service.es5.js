b1daf1685c476ca475612fdf3e16f3c39ee4add8
Miško Hevery <misko@hevery.com>
2017-07-20 23:27:39 +0000

    54e0244 Revert "docs: Remove unneeded file (#18106)"

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8d28191
+ * @license Angular v5.0.0-beta.0-54e0244
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-8d28191');
+var VERSION = new Version('5.0.0-beta.0-54e0244');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
