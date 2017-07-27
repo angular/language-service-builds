f2252d60a6b33bd5f3f125e64f918e99fdee06eb
Miško Hevery <misko@hevery.com>
2017-07-27 18:49:19 +0000

    a7f2468 Revert "fix(router): should throw when lazy loaded module doesn't define any routes (#15001)"

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-fae47d8
+ * @license Angular v5.0.0-beta.0-a7f2468
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-fae47d8');
+var VERSION = new Version('5.0.0-beta.0-a7f2468');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
