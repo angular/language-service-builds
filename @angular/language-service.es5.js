24cc04be457f7a655b818d3a110592ef87adb02d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 23:02:17 +0000

    d71ae27 fix(aio): fix links to source for paths with symlinks

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-4620753
+ * @license Angular v5.0.0-beta.1-d71ae27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-4620753');
+var VERSION = new Version('5.0.0-beta.1-d71ae27');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-4620753
+ * @license Angular v5.0.0-beta.1-d71ae27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.1-4620753
+ * @license Angular v5.0.0-beta.1-d71ae27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3104,7 +3104,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('5.0.0-beta.1-4620753');
+var VERSION$2 = new Version('5.0.0-beta.1-d71ae27');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -16995,7 +16995,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v5.0.0-beta.1-4620753
+ * @license Angular v5.0.0-beta.1-d71ae27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17018,7 +17018,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-beta.1-4620753');
+var VERSION$1 = new Version('5.0.0-beta.1-d71ae27');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -47421,7 +47421,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.1-4620753');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-d71ae27');
 
 });
 
@@ -50881,7 +50881,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v5.0.0-beta.1-4620753
+ * @license Angular v5.0.0-beta.1-d71ae27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53510,7 +53510,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('5.0.0-beta.1-4620753');
+var VERSION$$1 = new Version('5.0.0-beta.1-d71ae27');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
