a08323979be8f0653be91564183dfe99aa7f407b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 22:54:40 +0000

    416ed69 docs(aio): fix URLSearchParams interface link to MDN

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-cf62846
+ * @license Angular v5.0.0-beta.1-416ed69
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-cf62846');
+var VERSION = new Version('5.0.0-beta.1-416ed69');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
