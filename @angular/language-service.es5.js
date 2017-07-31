fbf5c32cc5ebb27bb813767b27c9f4e8754f5b96
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 21:35:40 +0000

    cc2a4c4 build(aio): fix warning about missing `<h1>`

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-a11542a
+ * @license Angular v5.0.0-beta.1-cc2a4c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-a11542a');
+var VERSION = new Version('5.0.0-beta.1-cc2a4c4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
