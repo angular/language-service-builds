524309871cc42836d3e0534a793d7f1d6fd6ad53
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-01 18:05:31 +0000

    49cd851 feat(router): add events tracking activation of individual routes

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-82b067f
+ * @license Angular v5.0.0-beta.1-49cd851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-82b067f');
+var VERSION = new Version('5.0.0-beta.1-49cd851');
 
 /**
  * @license
