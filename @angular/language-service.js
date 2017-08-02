a8cfccfb566d7eb888eec846b6e33de0938db205
Victor Berchet <victor@suumit.com>
2017-08-02 22:50:39 +0000

    dbc5c58 docs: improve github labels by introducing "PR target" labels (#18436)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-baf4ce0
+ * @license Angular v4.3.3-dbc5c58
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.3-baf4ce0');
+var VERSION = new Version('4.3.3-dbc5c58');
 
 /**
  * @license
