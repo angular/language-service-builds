4f40b5575c853adbfc7e8d78b4dd6c9818d9aeef
Victor Berchet <victor@suumit.com>
2017-08-02 23:24:56 +0000

    1d09838 ci: remove chromium fold reference (#18445)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5cc9913
+ * @license Angular v5.0.0-beta.2-1d09838
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-5cc9913');
+var VERSION = new Version('5.0.0-beta.2-1d09838');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
