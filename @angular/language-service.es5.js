ce3b5d79d88935c7a528f8e5da14e4d7140984b9
Victor Berchet <victor@suumit.com>
2017-08-02 23:57:18 +0000

    3923c30 docs(aio): fix missing anchor-open in i18n documentation (#18476)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-99017bf
+ * @license Angular v5.0.0-beta.2-3923c30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-99017bf');
+var VERSION = new Version('5.0.0-beta.2-3923c30');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
