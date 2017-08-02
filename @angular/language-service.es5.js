4b0b88c9a958f25750f731d00d80d174f51851d7
Victor Berchet <victor@suumit.com>
2017-08-02 23:42:38 +0000

    4d117fa test(common): skip some DatePipe tests in old Chrome where Intl is buggy (#15784)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-1d09838
+ * @license Angular v5.0.0-beta.2-4d117fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-1d09838');
+var VERSION = new Version('5.0.0-beta.2-4d117fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-1d09838
+ * @license Angular v5.0.0-beta.2-4d117fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.2-1d09838
+ * @license Angular v5.0.0-beta.2-4d117fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3104,7 +3104,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('5.0.0-beta.2-1d09838');
+var VERSION$2 = new Version('5.0.0-beta.2-4d117fa');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -16995,7 +16995,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v5.0.0-beta.2-1d09838
+ * @license Angular v5.0.0-beta.2-4d117fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17018,7 +17018,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-beta.2-1d09838');
+var VERSION$1 = new Version('5.0.0-beta.2-4d117fa');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -47437,7 +47437,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-1d09838');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-4d117fa');
 
 });
 
@@ -51050,7 +51050,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v5.0.0-beta.2-1d09838
+ * @license Angular v5.0.0-beta.2-4d117fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53679,7 +53679,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('5.0.0-beta.2-1d09838');
+var VERSION$$1 = new Version('5.0.0-beta.2-4d117fa');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
