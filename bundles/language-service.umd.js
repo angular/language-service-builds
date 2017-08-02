a60f8aab70c0512ccee4e808bcb536468c6933d9
GitHub <noreply@github.com>
2017-08-02 22:43:08 +0000

    baf4ce0 build: enable TSLint on the packages folder (#18459)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-24db1ed
+ * @license Angular v4.3.3-baf4ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.3-24db1ed
+ * @license Angular v4.3.3-baf4ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.3-24db1ed');
+var VERSION$2 = new Version('4.3.3-baf4ce0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16991,7 +16991,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.3-24db1ed
+ * @license Angular v4.3.3-baf4ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17010,7 +17010,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.3-24db1ed');
+var VERSION$1 = new Version('4.3.3-baf4ce0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47187,7 +47187,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-24db1ed');
+exports.VERSION = new core_1.Version('4.3.3-baf4ce0');
 
 });
 
@@ -48873,6 +48873,13 @@ var EmitFlags;
 
 var module_filename_resolver = createCommonjsModule(function (module, exports) {
 "use strict";
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 var path = require$$2__default;
 var ts = require$$0__default;
@@ -49336,9 +49343,7 @@ var TypeCheckingHost = (function () {
     TypeCheckingHost.prototype.getDefaultLibFileName = function (options) {
         return this.host.getDefaultLibFileName(options);
     };
-    TypeCheckingHost.prototype.getCurrentDirectory = function () {
-        return this.host.getCurrentDirectory();
-    };
+    TypeCheckingHost.prototype.getCurrentDirectory = function () { return this.host.getCurrentDirectory(); };
     TypeCheckingHost.prototype.getDirectories = function (path) { return this.host.getDirectories(path); };
     TypeCheckingHost.prototype.getCanonicalFileName = function (fileName) {
         return this.host.getCanonicalFileName(fileName);
@@ -49762,7 +49767,6 @@ var ts = require$$0__default;
 var compiler_host_1 = compiler_host;
 var check_types_1 = check_types;
 var api_1 = api;
-var api_2 = api;
 var node_emitter_transform_1 = node_emitter_transform;
 var GENERATED_FILES = /\.ngfactory\.js$|\.ngstyle\.js$|\.ngsummary\.js$/;
 var SUMMARY_JSON_FILES = /\.ngsummary.json$/;
@@ -49828,10 +49832,10 @@ var AngularCompilerProgram = (function () {
     AngularCompilerProgram.prototype.getLazyRoutes = function (cancellationToken) { return {}; };
     AngularCompilerProgram.prototype.emit = function (_a) {
         var _this = this;
-        var _b = _a.emitFlags, emitFlags = _b === void 0 ? api_2.EmitFlags.Default : _b, cancellationToken = _a.cancellationToken;
+        var _b = _a.emitFlags, emitFlags = _b === void 0 ? api_1.EmitFlags.Default : _b, cancellationToken = _a.cancellationToken;
         var emitMap = new Map();
         var result = this.programWithStubs.emit(
-        /* targetSourceFile */ undefined, createWriteFileCallback(emitFlags, this.host, this.collector, this.options, emitMap), cancellationToken, (emitFlags & (api_2.EmitFlags.DTS | api_2.EmitFlags.JS)) == api_2.EmitFlags.DTS, {
+        /* targetSourceFile */ undefined, createWriteFileCallback(emitFlags, this.host, this.collector, this.options, emitMap), cancellationToken, (emitFlags & (api_1.EmitFlags.DTS | api_1.EmitFlags.JS)) == api_1.EmitFlags.DTS, {
             after: this.options.skipTemplateCodegen ? [] : [node_emitter_transform_1.getAngularEmitterTransformFactory(this.generatedFiles)]
         });
         this.generatedFiles.forEach(function (file) {
@@ -50022,7 +50026,7 @@ function createWriteFileCallback(emitFlags, host, collector$$1, ngOptions, emitM
             emitMap.set(sourceFiles[0].fileName, fileName);
         }
     };
-    return (emitFlags & api_2.EmitFlags.Metadata) != 0 ? withMetadata : withoutMetadata;
+    return (emitFlags & api_1.EmitFlags.Metadata) != 0 ? withMetadata : withoutMetadata;
 }
 function getNgOptionDiagnostics(options) {
     if (options.annotationsAs) {
@@ -50097,6 +50101,13 @@ function createProgramWithStubsHost(generatedFiles, originalProgram, originalHos
 
 var entry_points = createCommonjsModule(function (module, exports) {
 "use strict";
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 var module_filename_resolver_1 = module_filename_resolver;
 exports.createModuleFilenameResolver = module_filename_resolver_1.createModuleFilenameResolver;
@@ -50653,7 +50664,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.3-24db1ed
+ * @license Angular v4.3.3-baf4ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53282,7 +53293,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.3-24db1ed');
+var VERSION$$1 = new Version('4.3.3-baf4ce0');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
