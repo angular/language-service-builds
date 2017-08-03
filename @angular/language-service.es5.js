586d129088765738ea08a39c6215587e5989667c
Victor Berchet <victor@suumit.com>
2017-08-03 00:41:53 +0000

    ea07856 refactor(upgrade): compile upgrade cleanly with TypeScript 2.4 (#18461)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-7c47b62
+ * @license Angular v5.0.0-beta.2-ea07856
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-7c47b62');
+var VERSION = new Version('5.0.0-beta.2-ea07856');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
