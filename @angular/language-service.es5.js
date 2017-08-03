77ed5be8b6233910492c87e7d546cbf7eb7e61d0
GitHub <noreply@github.com>
2017-08-03 22:00:02 +0000

    ca695e0 fix(compiler-cli): disable buggy expression lowering (#18513)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-9b015a9');
+var VERSION = new Version('5.0.0-beta.2-ca695e0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3104,7 +3104,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('5.0.0-beta.2-9b015a9');
+var VERSION$2 = new Version('5.0.0-beta.2-ca695e0');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -16995,7 +16995,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17018,7 +17018,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-beta.2-9b015a9');
+var VERSION$1 = new Version('5.0.0-beta.2-ca695e0');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -47437,7 +47437,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-9b015a9');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-ca695e0');
 
 });
 
@@ -50303,7 +50303,8 @@ var AngularCompilerProgram = (function () {
         var before = [];
         var after = [];
         if (!this.options.disableExpressionLowering) {
-            before.push(lower_expressions_1.getExpressionLoweringTransformFactory(this.metadataCache));
+            // TODO(chuckj): fix and re-enable + tests - see https://github.com/angular/angular/pull/18388
+            // before.push(getExpressionLoweringTransformFactory(this.metadataCache));
         }
         if (!this.options.skipTemplateCodegen) {
             after.push(node_emitter_transform_1.getAngularEmitterTransformFactory(this.generatedFiles));
@@ -51051,7 +51052,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53680,7 +53681,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('5.0.0-beta.2-9b015a9');
+var VERSION$$1 = new Version('5.0.0-beta.2-ca695e0');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
