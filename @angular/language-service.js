7aedb5183c6b8e79ded1c65f82a659671fb4997f
Victor Berchet <victor@suumit.com>
2017-08-03 01:16:20 +0000

    3a227a1 refactor(router): compile router cleanly with TypeScript 2.4 (#18465)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-81cb5bc
+ * @license Angular v5.0.0-beta.2-3a227a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-81cb5bc');
+var VERSION = new Version('5.0.0-beta.2-3a227a1');
 
 /**
  * @license
