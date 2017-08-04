3762496bbe396f754124cf506a9e24eb53bdd4fe
Victor Berchet <victor@suumit.com>
2017-08-04 16:39:24 +0000

    10897d6 ci(aio): compute AIO deployment mode

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-ca695e0
+ * @license Angular v5.0.0-beta.2-10897d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.2-ca695e0
+ * @license Angular v5.0.0-beta.2-10897d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3104,7 +3104,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('5.0.0-beta.2-ca695e0');
+var VERSION$2 = new Version('5.0.0-beta.2-10897d6');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -16995,7 +16995,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v5.0.0-beta.2-ca695e0
+ * @license Angular v5.0.0-beta.2-10897d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17018,7 +17018,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-beta.2-ca695e0');
+var VERSION$1 = new Version('5.0.0-beta.2-10897d6');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
@@ -47437,7 +47437,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-ca695e0');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-10897d6');
 
 });
 
@@ -51052,7 +51052,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v5.0.0-beta.2-ca695e0
+ * @license Angular v5.0.0-beta.2-10897d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53681,7 +53681,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('5.0.0-beta.2-ca695e0');
+var VERSION$$1 = new Version('5.0.0-beta.2-10897d6');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
