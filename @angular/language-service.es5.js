d7a7ea663480101f6ba5a381825e48b8e3619fe2
Victor Berchet <victor@suumit.com>
2017-08-07 23:06:46 +0000

    f69561b feat(forms): add updateOn submit option to FormControls (#18514)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-fcadbf4
+ * @license Angular v5.0.0-beta.2-f69561b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-fcadbf4');
+var VERSION = new Version('5.0.0-beta.2-f69561b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
