ddcdd115bebdbcebb1b966e5a8b197afbfa2ad47
Victor Berchet <victor@suumit.com>
2017-08-07 17:41:49 +0000

    f9f8924 docs(aio): typo & update the bio (#18559)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-10897d6
+ * @license Angular v5.0.0-beta.2-f9f8924
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-10897d6');
+var VERSION = new Version('5.0.0-beta.2-f9f8924');
 
 /**
  * @license
