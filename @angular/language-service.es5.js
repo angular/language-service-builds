5b5be8ed763737ed588d13fd70d444cf565e5a0f
Victor Berchet <victor@suumit.com>
2017-08-09 22:03:17 +0000

    7f2037f test(aio): fix running docs examples against local builds (#18520)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-fd6ae57
+ * @license Angular v5.0.0-beta.2-7f2037f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-fd6ae57');
+var VERSION = new Version('5.0.0-beta.2-7f2037f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
