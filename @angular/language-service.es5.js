8699507afb8edc4a6389bc6ad75f7cec4889114d
Hans <hans@hansl.ca>
2017-08-11 20:51:06 +0000

    088532b perf(aio): update to new version of build-optimizer

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-cac130e
+ * @license Angular v5.0.0-beta.3-088532b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-cac130e');
+var VERSION = new Version('5.0.0-beta.3-088532b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
