33159999804b0178f0b5b93120ce9af2471b6d15
Hans <hans@hansl.ca>
2017-08-15 23:11:35 +0000

    856278c docs(core): correct code examples for ChangeDetectorRef

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-5e840e1
+ * @license Angular v5.0.0-beta.3-856278c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-5e840e1');
+var VERSION = new Version('5.0.0-beta.3-856278c');
 
 /**
  * @license
