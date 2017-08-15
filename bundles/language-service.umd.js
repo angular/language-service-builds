2a1be544e86cd8a1419b67c93c7c0ef67604a647
Hans Larsen <hans@hansl.ca>
2017-08-15 23:06:46 +0000

    a618d6e docs(forms): add api docs for AbstractControlDirective

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-b315a84
+ * @license Angular v4.3.4-a618d6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.4-b315a84
+ * @license Angular v4.3.4-a618d6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.4-b315a84');
+var VERSION$2 = new Version('4.3.4-a618d6e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -16998,7 +16998,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.4-b315a84
+ * @license Angular v4.3.4-a618d6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17017,7 +17017,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.4-b315a84');
+var VERSION$1 = new Version('4.3.4-a618d6e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47194,7 +47194,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.4-b315a84');
+exports.VERSION = new core_1.Version('4.3.4-a618d6e');
 
 });
 
@@ -50671,7 +50671,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.4-b315a84
+ * @license Angular v4.3.4-a618d6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53300,7 +53300,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.4-b315a84');
+var VERSION$$1 = new Version('4.3.4-a618d6e');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
