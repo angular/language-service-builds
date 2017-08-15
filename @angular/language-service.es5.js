30fca804756b1b8d3d5418d79e48e4dc4fc56bd6
Hans Larsen <hans@hansl.ca>
2017-08-15 23:22:42 +0000

    1cb6076 build(aio): switch from `@angular/http` to `@angular/common/http`

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-259fc91
+ * @license Angular v4.3.4-1cb6076
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-259fc91');
+var VERSION = new Version('4.3.4-1cb6076');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
