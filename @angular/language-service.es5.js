e5a14d89605d73e2211f1cfb3f459fa335c0e118
Hans Larsen <hans@hansl.ca>
2017-08-15 19:58:08 +0000

    b315a84 docs(aio): add Metadata guide based on Chuck’s docs

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-972538b
+ * @license Angular v4.3.4-b315a84
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-972538b');
+var VERSION = new Version('4.3.4-b315a84');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
