c8dfc0ff179123df38b0030184171fee9c70f07b
Hans <hans@hansl.ca>
2017-08-15 23:25:22 +0000

    38addac build(aio): switch from `@angular/http` to `@angular/common/http`

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-856278c
+ * @license Angular v5.0.0-beta.3-38addac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-856278c');
+var VERSION = new Version('5.0.0-beta.3-38addac');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
