55b21e665627407f1a7ded731925cf1a770adcf5
Hans Larsen <hans@hansl.ca>
2017-08-16 18:14:36 +0000

    7e94405 docs: add changelog for 4.3.5

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-a1624f2
+ * @license Angular v4.3.5-7e94405
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-a1624f2');
+var VERSION = new Version('4.3.5-7e94405');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
