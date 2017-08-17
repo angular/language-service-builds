c4fc61ea05d0b2821e29a1df600beb6807d1be65
Miško Hevery <misko@hevery.com>
2017-08-17 04:00:16 +0000

    56a5b02 test: add cli integration test (#18738)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-0cc77b4
+ * @license Angular v5.0.0-beta.4-56a5b02
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-0cc77b4');
+var VERSION = new Version('5.0.0-beta.4-56a5b02');
 
 /**
  * @license
