282e8469225b2eee6773d7b1d0fd9e647588b6be
Miško Hevery <misko@hevery.com>
2017-08-17 03:15:10 +0000

    0cc77b4 refactor(compiler): split compiler and core (#18683)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-3a50098
+ * @license Angular v5.0.0-beta.4-0cc77b4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2635,7 +2635,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-3a50098');
+var VERSION = new Version('5.0.0-beta.4-0cc77b4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
