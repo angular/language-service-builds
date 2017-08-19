857b6c3ab91409c1bf355875141cf65169afdfe6
Miško Hevery <misko@hevery.com>
2017-08-19 04:38:06 +0000

    e228f2c fix(compiler-cli): use forward slashes for ts.resolveModuleName (#18784)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-f2a2a6b
+ * @license Angular v5.0.0-beta.4-e228f2c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-f2a2a6b');
+var VERSION = new Version('5.0.0-beta.4-e228f2c');
 
 /**
  * @license
