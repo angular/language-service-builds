50fdde4c60310fdc6fb9d415d110b35ddc18f536
Miško Hevery <misko@hevery.com>
2017-08-21 22:22:37 +0000

    55d151a refactor(common): remove usage of deprecated Renderer (#17528)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-5a1b9a3
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-5a1b9a3');
+var VERSION = new Version('5.0.0-beta.4-55d151a');
 
 /**
  * @license
