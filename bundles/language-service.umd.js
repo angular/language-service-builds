8dcf4ce183d79bfdc1c6125088e7448601e10204
Miško Hevery <misko@hevery.com>
2017-08-21 22:27:46 +0000

    14d34c9 style(animations): format integration spec (#18805)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-e1f45a3
+ * @license Angular v4.3.5-14d34c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.5-e1f45a3
+ * @license Angular v4.3.5-14d34c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.5-e1f45a3');
+var VERSION$2 = new Version('4.3.5-14d34c9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -17005,7 +17005,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.5-e1f45a3
+ * @license Angular v4.3.5-14d34c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17024,7 +17024,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.5-e1f45a3');
+var VERSION$1 = new Version('4.3.5-14d34c9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47201,7 +47201,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-e1f45a3');
+exports.VERSION = new core_1.Version('4.3.5-14d34c9');
 
 });
 
@@ -50678,7 +50678,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.5-e1f45a3
+ * @license Angular v4.3.5-14d34c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53307,7 +53307,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.5-e1f45a3');
+var VERSION$$1 = new Version('4.3.5-14d34c9');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
