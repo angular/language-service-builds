67fe5f6c124a9d4ce5a6ef3d6a5093812eb4610c
Miško Hevery <misko@hevery.com>
2017-08-22 01:53:37 +0000

    1f43713 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.5-14d34c9');
+var VERSION = new Version('4.3.5-1f43713');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.5-14d34c9');
+var VERSION$2 = new Version('4.3.5-1f43713');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -17005,7 +17005,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17024,7 +17024,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.5-14d34c9');
+var VERSION$1 = new Version('4.3.5-1f43713');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47201,7 +47201,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-14d34c9');
+exports.VERSION = new core_1.Version('4.3.5-1f43713');
 
 });
 
@@ -50678,7 +50678,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53307,7 +53307,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.5-14d34c9');
+var VERSION$$1 = new Version('4.3.5-1f43713');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
