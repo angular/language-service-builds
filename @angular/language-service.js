dad43c4bd4aea6f5ea51370e0a0db5dfe7c02fef
Miško Hevery <misko@hevery.com>
2017-08-22 02:02:22 +0000

    ec56760 refactor(common): remove deprecated `NgFor` (#18758)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-ec56760
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-55d151a');
+var VERSION = new Version('5.0.0-beta.4-ec56760');
 
 /**
  * @license
