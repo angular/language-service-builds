c6ad531e6fce431f87b62135215274d8a31e4001
Miško Hevery <misko@hevery.com>
2017-08-28 21:46:30 +0000

    655b495 build: reenable npm install for air builds

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-92dbaf3
+ * @license Angular v5.0.0-beta.4-655b495
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-92dbaf3');
+var VERSION = new Version('5.0.0-beta.4-655b495');
 
 /**
  * @license
