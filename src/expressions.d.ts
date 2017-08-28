d751368645fb978e5f3f6c90416d23832b89d0c9
Miško Hevery <misko@hevery.com>
2017-08-28 18:09:07 +0000

    d64c935 ci: do not run npm tools for AIO builds (#18904)

--- src/expressions.d.ts
+++ src/expressions.d.ts
@@ -6,22 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { AST } from '@angular/compiler';
-import { TemplateInfo } from './common';
-import { TemplateAstPath } from './template_path';
-import { DiagnosticKind, Span, Symbol, SymbolQuery, SymbolTable } from './types';
-export interface ExpressionDiagnosticsContext {
-    event?: boolean;
-}
-export declare function getExpressionDiagnostics(scope: SymbolTable, ast: AST, query: SymbolQuery, context?: ExpressionDiagnosticsContext): TypeDiagnostic[];
+import { Span, Symbol, SymbolQuery, SymbolTable } from './types';
 export declare function getExpressionCompletions(scope: SymbolTable, ast: AST, position: number, query: SymbolQuery): Symbol[] | undefined;
 export declare function getExpressionSymbol(scope: SymbolTable, ast: AST, position: number, query: SymbolQuery): {
     symbol: Symbol;
     span: Span;
 } | undefined;
-export declare class TypeDiagnostic {
-    kind: DiagnosticKind;
-    message: string;
-    ast: AST;
-    constructor(kind: DiagnosticKind, message: string, ast: AST);
-}
-export declare function getExpressionScope(info: TemplateInfo, path: TemplateAstPath, includeEvent: boolean): SymbolTable;
