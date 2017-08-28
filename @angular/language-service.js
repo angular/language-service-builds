57c3dda42180d512660dd14ce94ec349f336d9d6
Miško Hevery <misko@hevery.com>
2017-08-28 23:49:40 +0000

    2e714f9 release: cut the 5.0.0-beta.5 release

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-655b495
+ * @license Angular v5.0.0-beta.5-2e714f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-655b495');
+var VERSION = new Version('5.0.0-beta.5-2e714f9');
 
 /**
  * @license
