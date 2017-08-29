412dc7177d25e719cc2ddfd5a8578c1039fb2a5e
Miško Hevery <misko@hevery.com>
2017-08-29 20:24:01 +0000

    24faaf4 docs: add changelog for 5.0.0-beta.5

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-90bd984
+ * @license Angular v5.0.0-beta.5-24faaf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-90bd984');
+var VERSION = new Version('5.0.0-beta.5-24faaf4');
 
 /**
  * @license
