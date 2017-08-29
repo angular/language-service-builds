b259f3edfe9dc6769d2bf37b85448aca3b7c1128
Victor Berchet <victor@suumit.com>
2017-08-29 19:25:14 +0000

    90bd984 refactor(common): simplify closure-locale by removing imports (#18926)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-4878936
+ * @license Angular v5.0.0-beta.5-90bd984
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2635,7 +2635,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-4878936');
+var VERSION = new Version('5.0.0-beta.5-90bd984');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
