ab58b118298809c96e7c9c281687f5e20510c76e
Miško Hevery <misko@hevery.com>
2017-08-29 00:15:38 +0000

    dfe2bad build: Add GitHub scripts for rebasing PRs (#18359)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-f09a266');
+var VERSION = new Version('4.3.6-dfe2bad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
