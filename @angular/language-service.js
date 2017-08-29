8e7da229567352b3db071e9836a270ff53c29430
Miško Hevery <misko@hevery.com>
2017-08-29 16:55:35 +0000

    4878936 feat(common): generate `closure-locale.ts` to tree shake locale data (#18907)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-72c7b6e
+ * @license Angular v5.0.0-beta.5-4878936
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-72c7b6e');
+var VERSION = new Version('5.0.0-beta.5-4878936');
 
 /**
  * @license
