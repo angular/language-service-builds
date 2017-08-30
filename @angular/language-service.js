19b6f0d42d93ba5ad605be5f3764ceb60efec2d7
Jason Aden <jasonaden@google.com>
2017-08-30 00:47:17 +0000

    a203a95 fix(common): fix improper packaging for @angular/common/http (#18613)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-8821723');
+var VERSION = new Version('4.3.6-a203a95');
 
 /**
  * @license
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.6-8821723');
+var VERSION$2 = new Version('4.3.6-a203a95');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -17010,7 +17010,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17029,7 +17029,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.6-8821723');
+var VERSION$1 = new Version('4.3.6-a203a95');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47206,7 +47206,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.6-8821723');
+exports.VERSION = new core_1.Version('4.3.6-a203a95');
 
 });
 
@@ -50683,7 +50683,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53312,7 +53312,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.6-8821723');
+var VERSION$$1 = new Version('4.3.6-a203a95');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
