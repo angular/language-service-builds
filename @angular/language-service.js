0296d3d1124a616b62825f08f460f84c81b3b4fb
Jason Aden <jasonaden@google.com>
2017-08-31 01:41:06 +0000

    ba7d70e build: fix changelog to remove AIO line items (#18956)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-497e017');
+var VERSION = new Version('4.3.6-ba7d70e');
 
 /**
  * @license
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.3.6-497e017');
+var VERSION$2 = new Version('4.3.6-ba7d70e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -17010,7 +17010,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17029,7 +17029,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.3.6-497e017');
+var VERSION$1 = new Version('4.3.6-ba7d70e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47207,7 +47207,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.6-497e017');
+exports.VERSION = new core_1.Version('4.3.6-ba7d70e');
 
 });
 
@@ -50684,7 +50684,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53313,7 +53313,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.3.6-497e017');
+var VERSION$$1 = new Version('4.3.6-ba7d70e');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
