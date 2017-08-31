924ad51b61b2371b3f4f8ddb049d216a91a73957
Jason Aden <jasonaden@google.com>
2017-08-31 22:58:41 +0000

    ee04217 refactor(compiler-cli): expose ngtools api separately (#18978)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-a1293b2
+ * @license Angular v5.0.0-beta.5-ee04217
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2635,7 +2635,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-a1293b2');
+var VERSION = new Version('5.0.0-beta.5-ee04217');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
