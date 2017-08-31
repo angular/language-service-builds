5c6842273c7b7117261d763a6d661dbdd1e9a500
Olivier Combe <olivier.combe@gmail.com>
2017-08-31 09:30:42 +0000

    e1dc9bf style(changelog): improving readability

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-3725535
+ * @license Angular v5.0.0-beta.5-e1dc9bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2630,7 +2630,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-3725535');
+var VERSION = new Version('5.0.0-beta.5-e1dc9bf');
 
 /**
  * @license
