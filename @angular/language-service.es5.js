e9ffa0485f36848a4a1b79411ae23b59315180ba
Victor Berchet <victor@suumit.com>
2017-08-31 01:06:54 +0000

    30d53a8 fix(common): use v4 plurals when importing `DeprecatedI18NPipesModule` (#18955)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-043f104
+ * @license Angular v5.0.0-beta.5-30d53a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2635,7 +2635,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-043f104');
+var VERSION = new Version('5.0.0-beta.5-30d53a8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
