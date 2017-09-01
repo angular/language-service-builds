d45e5c416dfd5bde16ad5779608e4b67ab461adf
Miško Hevery <misko@hevery.com>
2017-09-01 21:51:19 +0000

    185a941 test: support debugging unit tests in node (#18987)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -392,7 +392,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION$1 = new Version('5.0.0-beta.5-6af2396');
+var VERSION$1 = new Version('5.0.0-beta.5-185a941');
 
 /**
  * @license
@@ -25833,7 +25833,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -26244,7 +26244,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-beta.5-6af2396');
+var VERSION$2 = new Version$1('5.0.0-beta.5-185a941');
 
 /**
  * Inject decorator and metadata.
@@ -37556,7 +37556,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -40127,7 +40127,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-beta.5-6af2396');
+var VERSION = new Version$1('5.0.0-beta.5-185a941');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2578,7 +2578,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-6af2396');
+var VERSION = new Version('5.0.0-beta.5-185a941');
 
 /**
  * @license
