217bc47192d44a060ebedfaa9687e9a7dc6e5d2b
Misko Hevery <misko@hevery.com>
2017-09-02 05:48:05 +0000

    aeb98db docs: add changelog for 4.4.0-RC.0

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-8036d05
+ * @license Angular v4.4.0-RC.0-aeb98db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.0-RC.0-8036d05');
+var VERSION = new Version('4.4.0-RC.0-aeb98db');
 
 /**
  * @license
