051e4e761ccd29e7a096db23b74bd252c0908e19
Misko Hevery <misko@hevery.com>
2017-09-02 16:34:39 +0000

    fa6b802 docs: add changelog for 5.0.0-beta.6

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-6ab4966
+ * @license Angular v5.0.0-beta.6-fa6b802
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2578,7 +2578,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-6ab4966');
+var VERSION = new Version('5.0.0-beta.6-fa6b802');
 
 /**
  * @license
