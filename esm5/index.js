b93b2f776a1bb24b15981f3688b3444fe7005b66
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 23:51:32 +0000

    112e777 build(aio): update karma & systemjs config for HttpClient

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-a176cd6
+ * @license Angular v5.0.0-beta.6-112e777
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2578,7 +2578,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.6-a176cd6');
+var VERSION = new Version('5.0.0-beta.6-112e777');
 
 /**
  * @license
