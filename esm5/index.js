f0587efbf8c362ac18c5b0601c8d44a9b317565e
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 21:30:39 +0000

    42c69d3 docs: add changelog for 5.0.0-beta.7

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-9b571a1
+ * @license Angular v5.0.0-beta.7-42c69d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2613,7 +2613,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.6-9b571a1');
+var VERSION = new Version('5.0.0-beta.7-42c69d3');
 
 /**
  * @license
