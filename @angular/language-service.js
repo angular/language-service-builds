8cfc0bcccd4f5b732e7c627379c3a740067726dd
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:37:05 +0000

    b20c5d2 fix(upgrade): remove code setting id attribute. (#19182)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-0bafd03
+ * @license Angular v4.4.0-RC.0-b20c5d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.0-RC.0-0bafd03');
+var VERSION = new Version('4.4.0-RC.0-b20c5d2');
 
 /**
  * @license
