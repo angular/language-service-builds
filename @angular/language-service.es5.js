0ca54dd7b66291e0c8eb8b8425e00c9d950032a2
Igor Minar <igor@angularjs.org>
2017-09-19 22:59:47 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.2-0371538');
+var VERSION = new Version('4.4.3-e2e8ba6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
