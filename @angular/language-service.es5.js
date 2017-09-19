5747f4ff01af882dd00a170bd5a378fb8af4c538
Igor Minar <igor@angularjs.org>
2017-09-19 21:46:32 +0000

    0371538 fix(tsc-wrapped): deduplicate metadata only when the module is the same (#19261)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-351331a
+ * @license Angular v4.4.2-0371538
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.2-351331a');
+var VERSION = new Version('4.4.2-0371538');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
