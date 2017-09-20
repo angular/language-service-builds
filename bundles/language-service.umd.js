af882bb67d58ae28413b1cbd97ddc02ad868506a
Igor Minar <igor@angularjs.org>
2017-09-20 23:45:22 +0000

    9e84719 test: fix cli-integration tests that got broken by yarn update (#19297)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -380,7 +380,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION$1 = new Version('5.0.0-beta.7-2c4107c');
+var VERSION$1 = new Version('5.0.0-beta.7-9e84719');
 
 /**
  * @license
@@ -27691,7 +27691,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28080,7 +28080,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-beta.7-2c4107c');
+var VERSION$2 = new Version$1('5.0.0-beta.7-9e84719');
 
 /**
  * @fileoverview added by tsickle
@@ -41230,7 +41230,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -43831,7 +43831,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-beta.7-2c4107c');
+var VERSION = new Version$1('5.0.0-beta.7-9e84719');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
