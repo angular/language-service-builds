af369d84918525899a17f31260160d3f1a2979b3
Victor Berchet <victor@suumit.com>
2017-09-25 19:34:49 +0000

    5c99b01 build(aio): run the upload server as a non-previleged user

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2033,7 +2033,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2825,7 +2825,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version('4.4.3-04bc525');
+var VERSION$2 = new Version('4.4.3-5c99b01');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -17028,7 +17028,7 @@ var core_es5 = Object.freeze({
 });
 
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17047,7 +17047,7 @@ var core_es5 = Object.freeze({
 /**
  * \@stable
  */
-var VERSION$1 = new Version('4.4.3-04bc525');
+var VERSION$1 = new Version('4.4.3-5c99b01');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -47405,7 +47405,7 @@ var core_1 = require$$0$13;
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.3-04bc525');
+exports.VERSION = new core_1.Version('4.4.3-5c99b01');
 
 });
 
@@ -50882,7 +50882,7 @@ var ModuleResolutionHostAdapter = index.ModuleResolutionHostAdapter;
 var CompilerHost = index.CompilerHost;
 
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -53511,7 +53511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION$$1 = new Version('4.4.3-04bc525');
+var VERSION$$1 = new Version('4.4.3-5c99b01');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
