c35e71b904389c81603abb03c31cfbbf4a3e1654
Victor Berchet <victor@suumit.com>
2017-09-25 20:01:24 +0000

    e224e3d fix(compiler-cli): update ngtools2 EmitFlags (#19375)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9ad4b3b
+ * @license Angular v5.0.0-beta.7-e224e3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-9ad4b3b');
+var VERSION = new Version('5.0.0-beta.7-e224e3d');
 
 /**
  * @license
