345680a14d6943c6f974d89eb1e71e8587c777d5
Victor Berchet <victor@suumit.com>
2017-09-25 20:03:21 +0000

    4767902 fix(compiler-cli): do not validate metadata from declaration files (#19324)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-e224e3d');
+var VERSION = new Version('5.0.0-beta.7-4767902');
 
 /**
  * @license
