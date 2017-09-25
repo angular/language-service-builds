7b0705fa73c6bf0ce989e4c477250b73219c4ef1
Victor Berchet <victor@suumit.com>
2017-09-25 19:21:23 +0000

    4ae546b build(aio): ensure decorators with shared interface types are found (#19361)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -671,7 +671,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-beta.7-adb0b76');
+var VERSION$1 = new Version('5.0.0-beta.7-4ae546b');
 
 /**
  * @fileoverview added by tsickle
@@ -40516,7 +40516,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -40905,7 +40905,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-beta.7-adb0b76');
+var VERSION$2 = new Version$1('5.0.0-beta.7-4ae546b');
 
 /**
  * @fileoverview added by tsickle
@@ -54122,7 +54122,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -56696,7 +56696,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-beta.7-adb0b76');
+var VERSION = new Version$1('5.0.0-beta.7-4ae546b');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
