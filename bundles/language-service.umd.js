c93f99ab435e7bc0c903a1fa1a92e4563e4dd41e
Victor Berchet <victor@suumit.com>
2017-09-26 16:50:52 +0000

    27c6638 fix(compiler-cli): set source file ranges in node emitter (#19348)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -671,7 +671,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-beta.7-3f100eb');
+var VERSION$1 = new Version('5.0.0-beta.7-27c6638');
 
 /**
  * @fileoverview added by tsickle
@@ -40532,7 +40532,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -40921,7 +40921,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-beta.7-3f100eb');
+var VERSION$2 = new Version$1('5.0.0-beta.7-27c6638');
 
 /**
  * @fileoverview added by tsickle
@@ -54138,7 +54138,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -56712,7 +56712,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-beta.7-3f100eb');
+var VERSION = new Version$1('5.0.0-beta.7-27c6638');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
