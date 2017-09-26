2de5c4e5126c73838641d25a6acca924665ffa2b
Victor Berchet <victor@suumit.com>
2017-09-26 18:44:21 +0000

    e889c68 fix(compiler-cli): don't join errors with comma (#19331)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2e1fec1
+ * @license Angular v5.0.0-beta.7-e889c68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-2e1fec1');
+var VERSION = new Version('5.0.0-beta.7-e889c68');
 
 /**
  * @license
