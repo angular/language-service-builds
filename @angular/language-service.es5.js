2f2138806a1519077dee13a0c50ff861b05a9633
Victor Berchet <victor@suumit.com>
2017-09-26 20:44:31 +0000

    bc81fbd fix(animations): properly support the query limit option value (#19419)

--- @angular/language-service.es5.js
+++ @angular/language-service.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-c7aa8a1
+ * @license Angular v4.4.3-bc81fbd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2634,7 +2634,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.3-c7aa8a1');
+var VERSION = new Version('4.4.3-bc81fbd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
