d71cba76a3c7931ffffa8c149dc7d8bb797fe94d
Victor Berchet <victor@suumit.com>
2017-09-27 21:05:08 +0000

    48c6ece docs(aio): remove links to top of documents (#16971)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8d8995f
+ * @license Angular v5.0.0-beta.7-48c6ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-8d8995f');
+var VERSION = new Version('5.0.0-beta.7-48c6ece');
 
 /**
  * @license
