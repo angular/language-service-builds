4ad48e0a4d4be0169da1d93240c1a7d62ab8be78
Victor Berchet <victor@suumit.com>
2017-09-28 19:43:13 +0000

    bed8ac7 docs: add changelog for 4.4.4

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-a9d32a3
+ * @license Angular v5.0.0-beta.7-bed8ac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-a9d32a3');
+var VERSION = new Version('5.0.0-beta.7-bed8ac7');
 
 /**
  * @license
