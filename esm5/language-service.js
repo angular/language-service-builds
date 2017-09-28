6892b91cc3701581101a1d2fcc5048b68735d7fa
Victor Berchet <victor@suumit.com>
2017-09-28 16:48:52 +0000

    5406a49 docs: add 'bazel' as an Angular component (#19346)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8dbe2af
+ * @license Angular v5.0.0-beta.7-5406a49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-8dbe2af');
+var VERSION = new Version('5.0.0-beta.7-5406a49');
 
 /**
  * @license
