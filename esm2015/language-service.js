bff3b682a93ed5287978ed9208733fa3d521978c
Victor Berchet <victor@suumit.com>
2017-09-28 17:01:58 +0000

    2b84b86 refactor(core): Removed readonly getters and changed to readonly (#19225)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-c1b029a
+ * @license Angular v5.0.0-beta.7-2b84b86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2420,7 +2420,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.7-c1b029a');
+const VERSION = new Version('5.0.0-beta.7-2b84b86');
 
 /**
  * @license
