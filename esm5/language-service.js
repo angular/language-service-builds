3f51398b0c765f26dfa92dfe2e53a8d6160a5b37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:05:54 +0000

    d4d9009 docs(aio): fix broken link in toh-pt5

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-0f43299
+ * @license Angular v5.0.0-rc.0-d4d9009
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.0-0f43299');
+var VERSION = new Version('5.0.0-rc.0-d4d9009');
 
 /**
  * @license
