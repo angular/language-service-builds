d19b0e17ec452467a54e5fba77af70112da5efa9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-05 20:49:21 +0000

    f10f8db fix(service-worker): several misc fixes for corner cases

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-b804d4b
+ * @license Angular v5.0.0-rc.0-f10f8db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.0-b804d4b
+ * @license Angular v5.0.0-rc.0-f10f8db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -671,7 +671,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-rc.0-b804d4b');
+var VERSION$1 = new Version('5.0.0-rc.0-f10f8db');
 
 /**
  * @fileoverview added by tsickle
@@ -40915,7 +40915,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-rc.0-b804d4b
+ * @license Angular v5.0.0-rc.0-f10f8db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41304,7 +41304,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-rc.0-b804d4b');
+var VERSION$2 = new Version$1('5.0.0-rc.0-f10f8db');
 
 /**
  * @fileoverview added by tsickle
@@ -54437,7 +54437,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-rc.0-b804d4b
+ * @license Angular v5.0.0-rc.0-f10f8db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57011,7 +57011,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-rc.0-b804d4b');
+var VERSION = new Version$1('5.0.0-rc.0-f10f8db');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-b804d4b
+ * @license Angular v5.0.0-rc.0-f10f8db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.0-b804d4b');
+var VERSION = new Version('5.0.0-rc.0-f10f8db');
 
 /**
  * @license
