8a552848dff23262266d9bf94c2037121cb383bf
Chuck Jazdzewski <chuckj@google.com>
2017-10-07 01:37:41 +0000

    b1ca5d4 Revert "perf(animations): reduce size of bundle by removing AST classes (#19539)"

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-a8920eb
+ * @license Angular v5.0.0-rc.1-b1ca5d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-a8920eb');
+var VERSION = new Version('5.0.0-rc.1-b1ca5d4');
 
 /**
  * @license
