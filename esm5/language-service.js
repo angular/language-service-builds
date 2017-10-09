3750dc45565cf32c1b3678e14b2a780d9eec1729
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 22:08:36 +0000

    f393d86 build: add a Git .mailmap with my new name (#19550)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-6651541
+ * @license Angular v5.0.0-rc.1-f393d86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-6651541');
+var VERSION = new Version('5.0.0-rc.1-f393d86');
 
 /**
  * @license
