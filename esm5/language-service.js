710b861e6695e93a2215d53fd98f723d8d7c7be2
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 19:19:21 +0000

    566606e ci: freeze yarn lockfile when installing dependencies (#19616)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-46992b4
+ * @license Angular v5.0.0-rc.1-566606e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-46992b4');
+var VERSION = new Version('5.0.0-rc.1-566606e');
 
 /**
  * @license
