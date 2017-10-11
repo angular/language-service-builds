73954c58a874842604cfe022a50133c961534c0a
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 21:59:01 +0000

    3e3f918 ci: freeze yarn lockfile when installing dependencies (#19616)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-734378c
+ * @license Angular v4.4.4-3e3f918
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.4-734378c');
+var VERSION = new Version('4.4.4-3e3f918');
 
 /**
  * @license
