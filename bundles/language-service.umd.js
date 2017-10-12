fe9e24760614a77e17b9dba9116f2b3c7b8275b5
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:55:12 +0000

    6b7cead Revert "Revert "perf(compiler): skip type check and emit in bazel in some cases. (#19646)""

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -671,7 +671,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-rc.1-055b802');
+var VERSION$1 = new Version('5.0.0-rc.1-6b7cead');
 
 /**
  * @fileoverview added by tsickle
@@ -40915,7 +40915,7 @@ function share() {
 var share_2 = share;
 
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41304,7 +41304,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-rc.1-055b802');
+var VERSION$2 = new Version$1('5.0.0-rc.1-6b7cead');
 
 /**
  * @fileoverview added by tsickle
@@ -54465,7 +54465,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57039,7 +57039,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-rc.1-055b802');
+var VERSION = new Version$1('5.0.0-rc.1-6b7cead');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-055b802
+ * @license Angular v5.0.0-rc.1-6b7cead
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-055b802');
+var VERSION = new Version('5.0.0-rc.1-6b7cead');
 
 /**
  * @license
