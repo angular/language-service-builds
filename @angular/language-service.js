60b765d8ad839bf8f25aa78b956a06f9de80dd78
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 20:55:49 +0000

    32a814b docs: removing unnecessary commits from change log

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-912068e
+ * @license Angular v4.4.5-32a814b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-912068e');
+var VERSION = new Version('4.4.5-32a814b');
 
 /**
  * @license
