88929127a810c7d28db0e179276617d6ab1e904d
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:59:42 +0000

    18f1b01 build(aio): consider `devDependencies` when overwriting dependencies of local Angular packages (#19687)

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-591dcc2');
+var VERSION = new Version('4.4.5-18f1b01');
 
 /**
  * @license
