8eccc8a55191c01ddcf84547681419c2bb984f3a
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 22:25:51 +0000

    af8c2fa build: don’t make BrowserStack required as it fails with “Access denied”

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-0789601
+ * @license Angular v4.4.5-af8c2fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-0789601');
+var VERSION = new Version('4.4.5-af8c2fa');
 
 /**
  * @license
