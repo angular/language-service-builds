3f695cb46152949dbc2f4fdb2892df8b2351c1b2
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 22:18:16 +0000

    64b3619 build: don’t make BrowserStack required as it fails with “Access denied”

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-507290d
+ * @license Angular v5.0.0-rc.2-64b3619
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.2-507290d');
+var VERSION = new Version('5.0.0-rc.2-64b3619');
 
 /**
  * @license
