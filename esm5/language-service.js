19356acff0bdf5dcea217f1173da64e660ce90bf
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:41:09 +0000

    81173b0 fix(aio): make tests less flaky (#19784) PR Close #19784

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-b0c7ea8
+ * @license Angular v5.0.0-rc.2-81173b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.2-b0c7ea8');
+var VERSION = new Version('5.0.0-rc.2-81173b0');
 
 /**
  * @license
