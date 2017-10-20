451b46a9c7c675781720e787b34ad86104b017d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:41:00 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.3-820bb7b');
+var VERSION = new Version('5.0.0-rc.3-910735d');
 
 /**
  * @license
