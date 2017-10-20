77966302f95125cda6cb597c7aea6c3ccf23bfa8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:01:51 +0000

    fc86352 build(aio): use http push for navigation.json

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-230b98d
+ * @license Angular v5.0.0-rc.3-fc86352
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.3-230b98d');
+var VERSION = new Version('5.0.0-rc.3-fc86352');
 
 /**
  * @license
