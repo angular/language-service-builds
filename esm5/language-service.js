dd48618b8396801b6ce5d14cb9515f638a99c366
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 19:39:33 +0000

    90d1423 fix(service-worker): include versionedFiles in the manifest hashTable (#19837)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-910735d
+ * @license Angular v5.0.0-rc.3-90d1423
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2581,7 +2581,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.3-910735d');
+var VERSION = new Version('5.0.0-rc.3-90d1423');
 
 /**
  * @license
