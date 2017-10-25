adf7b5b488979c7735a91d563ee5cb512531e81f
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 18:51:27 +0000

    ef08330 fix(compiler): automatically set `emitDecoratorMetadata` when `"annotationsAs": "static fields”` (#19927)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-rc.5-6cc042e');
+var VERSION$1 = new Version('5.0.0-rc.5-ef08330');
 
 /**
  * @fileoverview added by tsickle
@@ -41120,7 +41120,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41509,7 +41509,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-rc.5-6cc042e');
+var VERSION$2 = new Version$1('5.0.0-rc.5-ef08330');
 
 /**
  * @fileoverview added by tsickle
@@ -54670,7 +54670,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57259,7 +57259,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-rc.5-6cc042e');
+var VERSION = new Version$1('5.0.0-rc.5-ef08330');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
