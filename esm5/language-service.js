e9c4589183cc1cc5c606f8641bb247759299b09d
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 22:15:45 +0000

    703fcda docs: add changelog for 5.0.0-rc.6

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.5-fde5f2f');
+var VERSION = new Version('5.0.0-rc.6-703fcda');
 
 /**
  * @license
