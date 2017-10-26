8dbf4ae33cb14e3918c33c71e90c9142b8de8b6d
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 22:42:48 +0000

    14016c7 fix(service-worker): fix improper call of Observable.merge (#19962)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-rc.6-47caebf');
+var VERSION$1 = new Version('5.0.0-rc.6-14016c7');
 
 /**
  * @fileoverview added by tsickle
@@ -41120,7 +41120,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41509,7 +41509,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-rc.6-47caebf');
+var VERSION$2 = new Version$1('5.0.0-rc.6-14016c7');
 
 /**
  * @fileoverview added by tsickle
@@ -54670,7 +54670,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57259,7 +57259,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-rc.6-47caebf');
+var VERSION = new Version$1('5.0.0-rc.6-14016c7');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.6-47caebf');
+var VERSION = new Version('5.0.0-rc.6-14016c7');
 
 /**
  * @license
