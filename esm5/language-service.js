32e65d175aacef6346ebc98badbad4868435ca50
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:50:12 +0000

    f1248b6 refactor: make all rollup config ES5 compatible (#20028)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-812786f
+ * @license Angular v5.0.0-rc.8-f1248b6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.8-812786f');
+var VERSION = new Version('5.0.0-rc.8-f1248b6');
 
 /**
  * @license
