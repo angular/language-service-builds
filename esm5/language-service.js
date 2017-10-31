5b6a0e7233618bca7b64edceaa96d129b89e0296
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:39:23 +0000

    420852e fix(compiler): reexport less symbols in `.ngfactory.ts` files (#19884)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-04eb80c
+ * @license Angular v5.0.0-rc.8-420852e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.8-04eb80c');
+var VERSION = new Version('5.0.0-rc.8-420852e');
 
 /**
  * @license
