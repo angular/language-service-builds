bfb9a4fbb36d712a2e513747430cee91f32304ad
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:52:11 +0000

    04eb80c fix(compiler): always use relative paths to refer to generated code

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-rc.8-308fc8e');
+var VERSION$1 = new Version('5.0.0-rc.8-04eb80c');
 
 /**
  * @fileoverview added by tsickle
@@ -41156,7 +41156,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41545,7 +41545,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-rc.8-308fc8e');
+var VERSION$2 = new Version$1('5.0.0-rc.8-04eb80c');
 
 /**
  * @fileoverview added by tsickle
@@ -54706,7 +54706,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57295,7 +57295,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-rc.8-308fc8e');
+var VERSION = new Version$1('5.0.0-rc.8-04eb80c');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
