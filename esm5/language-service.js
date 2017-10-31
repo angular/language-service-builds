099759be3e36beaeaa04c24efd736b67e48bf324
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 06:06:37 +0000

    5225fdb build(aio): add i18n boilerplate type (#20004)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-f5b7f2b
+ * @license Angular v5.0.0-rc.9-5225fdb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.9-f5b7f2b');
+var VERSION = new Version('5.0.0-rc.9-5225fdb');
 
 /**
  * @license
