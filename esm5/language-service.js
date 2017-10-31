fb90337da2e0759f5f096ef0233e35d615ef4bdd
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:07:05 +0000

    cf5fce8 build: update rollup lint rule from bad merge (#20047)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-f1248b6
+ * @license Angular v5.0.0-rc.8-cf5fce8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.8-f1248b6');
+var VERSION = new Version('5.0.0-rc.8-cf5fce8');
 
 /**
  * @license
