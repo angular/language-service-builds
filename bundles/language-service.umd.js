a883b27b8d4f054ce0f87ca570e6f3d338b85103
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:44:21 +0000

    951bd33 build: add `release` as a valid commit message subject (#19955)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.0-rc.8-420852e');
+var VERSION$1 = new Version('5.0.0-rc.8-951bd33');
 
 /**
  * @fileoverview added by tsickle
@@ -41239,7 +41239,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41628,7 +41628,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.0-rc.8-420852e');
+var VERSION$2 = new Version$1('5.0.0-rc.8-951bd33');
 
 /**
  * @fileoverview added by tsickle
@@ -54789,7 +54789,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57378,7 +57378,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.0-rc.8-420852e');
+var VERSION = new Version$1('5.0.0-rc.8-951bd33');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
