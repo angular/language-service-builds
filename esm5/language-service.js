c136afe480c6ef1c964380c766850e41a2e7ccf6
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:03:21 +0000

    27ae0f9 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-280dada
+ * @license Angular v5.0.0-27ae0f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2603,7 +2603,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-280dada');
+var VERSION = new Version('5.0.0-27ae0f9');
 
 /**
  * @license
