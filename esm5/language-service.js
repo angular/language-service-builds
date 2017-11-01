f491af07e6582161c9c1d9d3a1dbc610d9e25e80
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:29:38 +0000

    e1fb65c docs(packaging): fix typo

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-5225fdb
+ * @license Angular v5.0.0-e1fb65c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.9-5225fdb');
+var VERSION = new Version('5.0.0-e1fb65c');
 
 /**
  * @license
