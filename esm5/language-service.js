07b49a947a628c221ad1cfede26ba99328bb7a49
Victor Berchet <victor@suumit.com>
2017-11-02 23:16:19 +0000

    37a740b build: remove local yarn (#19981)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-067e926
+ * @license Angular v5.0.0-37a740b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-067e926');
+var VERSION = new Version('5.0.0-37a740b');
 
 /**
  * @license
