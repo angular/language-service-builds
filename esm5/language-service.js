54b05c8f43595f82b66cfd5f30ecf5b7f0b79610
Victor Berchet <victor@suumit.com>
2017-11-03 19:57:24 +0000

    e4cd3b0 test(aio): make e2e tests less flakey

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-9811bef');
+var VERSION = new Version('5.0.0-e4cd3b0');
 
 /**
  * @license
