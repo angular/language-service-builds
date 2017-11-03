bb22af04f745e16a056446a38367dd57b1c91316
Victor Berchet <victor@suumit.com>
2017-11-03 22:41:24 +0000

    200d92d fix(core): should support event.stopImmediatePropagation (#19222)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dbec3ca
+ * @license Angular v5.1.0-beta.0-200d92d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2448,7 +2448,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-dbec3ca');
+const VERSION = new Version('5.1.0-beta.0-200d92d');
 
 /**
  * @license
