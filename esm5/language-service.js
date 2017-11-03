5acd9763e60d6503c3b64ae4813b919fdf305ff7
Victor Berchet <victor@suumit.com>
2017-11-03 23:14:16 +0000

    3997d97 revert: feat(elements): implement `@angular/elements` #19469 (#20152)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2603,7 +2603,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-200d92d');
+var VERSION = new Version('5.1.0-beta.0-3997d97');
 
 /**
  * @license
