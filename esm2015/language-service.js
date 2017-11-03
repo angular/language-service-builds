f47e561ae99f9764962f59f2b1c82626c6bc25c1
Victor Berchet <victor@suumit.com>
2017-11-03 22:40:32 +0000

    dbec3ca docs(aio): update pipes for CLI (#19553)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2448,7 +2448,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-f7c9b94');
+const VERSION = new Version('5.1.0-beta.0-dbec3ca');
 
 /**
  * @license
