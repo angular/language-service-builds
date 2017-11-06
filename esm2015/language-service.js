4423dc410559283f0ff6708d98c69747b441bda9
Victor Berchet <victor@suumit.com>
2017-11-06 18:23:32 +0000

    8b50ed0 docs(aio): fixed case-typo for code (#20196)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2448,7 +2448,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-3997d97');
+const VERSION = new Version('5.1.0-beta.0-8b50ed0');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2603,7 +2603,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-3997d97');
+var VERSION = new Version('5.1.0-beta.0-8b50ed0');
 
 /**
  * @license
