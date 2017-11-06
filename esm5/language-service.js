c19ba444c9c6770367d8b4bd9908c8d227dbfc0e
Victor Berchet <victor@suumit.com>
2017-11-06 18:42:32 +0000

    6b74883 build(aio): upgrade to @anglar/core@5.0.0 and cli@1.5.0

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2603,7 +2603,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-049c896');
+var VERSION = new Version('5.1.0-beta.0-6b74883');
 
 /**
  * @license
