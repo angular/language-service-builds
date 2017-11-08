b00a03116fdb870fb100a749a2f28a945fac9e17
Victor Berchet <victor@suumit.com>
2017-11-08 22:36:41 +0000

    119034c docs: add changelog for 5.0.1

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-beta.0-78ba39b');
+var VERSION$1 = new Version('5.1.0-beta.0-119034c');
 
 /**
  * @fileoverview added by tsickle
@@ -41240,7 +41240,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41629,7 +41629,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-beta.0-78ba39b');
+var VERSION$2 = new Version$1('5.1.0-beta.0-119034c');
 
 /**
  * @fileoverview added by tsickle
@@ -54821,7 +54821,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57417,7 +57417,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-beta.0-78ba39b');
+var VERSION = new Version$1('5.1.0-beta.0-119034c');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2603,7 +2603,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-78ba39b');
+var VERSION = new Version('5.1.0-beta.0-119034c');
 
 /**
  * @license
