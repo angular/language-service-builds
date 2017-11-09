453e9276e63c255d51fda5111491414fe6a49ad1
Victor Berchet <victor@suumit.com>
2017-11-09 00:17:41 +0000

    28985cb test(compiler): do not use `a` as a content selector

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-9de45fa
+ * @license Angular v5.0.1-28985cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.1-9de45fa');
+var VERSION = new Version('5.0.1-28985cb');
 
 /**
  * @license
