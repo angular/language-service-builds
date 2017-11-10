0f7b6e9ff5b886a3bbd89cd154077b7373652cf0
Jason Aden <jasonaden@google.com>
2017-11-10 19:32:24 +0000

    7830d74 docs(aio): empty line between HTML tag and content (#20341) PR Close #20341

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3257fcd
+ * @license Angular v5.1.0-beta.0-7830d74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-3257fcd
+ * @license Angular v5.1.0-beta.0-7830d74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-beta.0-3257fcd');
+var VERSION$1 = new Version('5.1.0-beta.0-7830d74');
 
 /**
  * @fileoverview added by tsickle
@@ -41255,7 +41255,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-beta.0-3257fcd
+ * @license Angular v5.1.0-beta.0-7830d74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41644,7 +41644,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-beta.0-3257fcd');
+var VERSION$2 = new Version$1('5.1.0-beta.0-7830d74');
 
 /**
  * @fileoverview added by tsickle
@@ -54836,7 +54836,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-beta.0-3257fcd
+ * @license Angular v5.1.0-beta.0-7830d74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57432,7 +57432,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-beta.0-3257fcd');
+var VERSION = new Version$1('5.1.0-beta.0-7830d74');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
