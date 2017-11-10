7d17e2149e4b93814904c99686a9f08074a772dc
Jason Aden <jasonaden@google.com>
2017-11-10 23:06:14 +0000

    24cf8b3 fix(core): ensure init lifecycle events are called (#20258)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-161f88f
+ * @license Angular v5.1.0-beta.0-24cf8b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2451,7 +2451,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-161f88f');
+const VERSION = new Version('5.1.0-beta.0-24cf8b3');
 
 /**
  * @license
