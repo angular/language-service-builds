f4112ee3590a93123f8ea48085a6259b8e3d9baa
Jason Aden <jasonaden@google.com>
2017-11-10 23:05:41 +0000

    612f508 refactor(animations): uses a loop instead Array.map() which creates and (#19910) returns a new array that is discarded.

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.1-aa3d75b');
+var VERSION$1 = new Version('5.0.1-612f508');
 
 /**
  * @fileoverview added by tsickle
@@ -41256,7 +41256,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41645,7 +41645,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.1-aa3d75b');
+var VERSION$2 = new Version$1('5.0.1-612f508');
 
 /**
  * @fileoverview added by tsickle
@@ -54808,7 +54808,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57397,7 +57397,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.1-aa3d75b');
+var VERSION = new Version$1('5.0.1-612f508');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
