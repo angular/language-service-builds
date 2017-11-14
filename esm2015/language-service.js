a2c6a9a9ee60580cec9ab2293781cdf93d745e83
Jason Aden <jasonaden@google.com>
2017-11-14 22:07:43 +0000

    91efc7f docs(core): change from deprecated renderer to renderer2 (#19920)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-7acbc19
+ * @license Angular v5.0.1-91efc7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2441,7 +2441,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.1-7acbc19');
+const VERSION = new Version('5.0.1-91efc7f');
 
 /**
  * @license
