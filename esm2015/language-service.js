dead6a369a72084f73540b02e20544e7d0aaf36f
Jason Aden <jasonaden@google.com>
2017-11-15 20:18:45 +0000

    2d6126e style(core): fix comment format for linter

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2441,7 +2441,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.1-bc4b4b5');
+const VERSION = new Version('5.0.1-2d6126e');
 
 /**
  * @license
