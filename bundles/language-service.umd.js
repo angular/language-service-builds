e340f83bcda2e70ff80c5be817315afe2de42f26
Miško Hevery <misko@hevery.com>
2017-11-22 15:13:57 +0000

    ac93f12 ci: Update 1% payload size test (#20524)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-7cf5e95
+ * @license Angular v5.1.0-beta.1-ac93f12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.1-7cf5e95
+ * @license Angular v5.1.0-beta.1-ac93f12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-beta.1-7cf5e95');
+var VERSION$1 = new Version('5.1.0-beta.1-ac93f12');
 
 /**
  * @fileoverview added by tsickle
@@ -41656,7 +41656,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-beta.1-7cf5e95
+ * @license Angular v5.1.0-beta.1-ac93f12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42087,7 +42087,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-beta.1-7cf5e95');
+var VERSION$2 = new Version$1('5.1.0-beta.1-ac93f12');
 
 /**
  * @fileoverview added by tsickle
@@ -55475,7 +55475,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-beta.1-7cf5e95
+ * @license Angular v5.1.0-beta.1-ac93f12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58074,7 +58074,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-beta.1-7cf5e95');
+var VERSION = new Version$1('5.1.0-beta.1-ac93f12');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
