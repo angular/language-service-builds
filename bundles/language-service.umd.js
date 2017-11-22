ecf2acc72f9fbb053cfc2ad14e6d04f1ef6eac73
Miško Hevery <misko@hevery.com>
2017-11-22 20:09:49 +0000

    ffb6dbe fix(animations): always fire start and done callbacks in order for noop animations (#20570)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-beta.1-0e012c9');
+var VERSION$1 = new Version('5.1.0-beta.1-ffb6dbe');
 
 /**
  * @fileoverview added by tsickle
@@ -41656,7 +41656,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42087,7 +42087,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-beta.1-0e012c9');
+var VERSION$2 = new Version$1('5.1.0-beta.1-ffb6dbe');
 
 /**
  * @fileoverview added by tsickle
@@ -55475,7 +55475,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58074,7 +58074,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-beta.1-0e012c9');
+var VERSION = new Version$1('5.1.0-beta.1-ffb6dbe');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2606,7 +2606,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.1-0e012c9');
+var VERSION = new Version('5.1.0-beta.1-ffb6dbe');
 
 /**
  * @license
