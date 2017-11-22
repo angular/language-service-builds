96814ad69cb18f477189b1a3bb9c486236191c0b
Miško Hevery <misko@hevery.com>
2017-11-22 04:45:09 +0000

    7cf5e95 ci: update pullapprove (#20540)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-2a9d2ba
+ * @license Angular v5.1.0-beta.1-7cf5e95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2606,7 +2606,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.1-2a9d2ba');
+var VERSION = new Version('5.1.0-beta.1-7cf5e95');
 
 /**
  * @license
