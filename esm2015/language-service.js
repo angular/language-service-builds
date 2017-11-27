18541f5eddc0befac04a24bd2c627f6f57665f16
Miško Hevery <misko@hevery.com>
2017-11-27 01:29:36 +0000

    330bb2a release: cut the 5.1.0-beta.2 release

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0444e13
+ * @license Angular v5.1.0-beta.2-330bb2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2451,7 +2451,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.1-0444e13');
+const VERSION = new Version('5.1.0-beta.2-330bb2a');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0444e13
+ * @license Angular v5.1.0-beta.2-330bb2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2606,7 +2606,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.1-0444e13');
+var VERSION = new Version('5.1.0-beta.2-330bb2a');
 
 /**
  * @license
