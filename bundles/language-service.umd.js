9a6789acd7dc5d19df61b8cb284dd47fda15295f
Miško Hevery <misko@hevery.com>
2017-11-29 04:45:01 +0000

    c2b3792 fix(animations): ensure multi-level leave animations work (#19455)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-beta.2-8bb42df');
+var VERSION$1 = new Version('5.1.0-beta.2-c2b3792');
 
 /**
  * @fileoverview added by tsickle
@@ -42008,7 +42008,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42439,7 +42439,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-beta.2-8bb42df');
+var VERSION$2 = new Version$1('5.1.0-beta.2-c2b3792');
 
 /**
  * @fileoverview added by tsickle
@@ -55828,7 +55828,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58453,7 +58453,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-beta.2-8bb42df');
+var VERSION = new Version$1('5.1.0-beta.2-c2b3792');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
