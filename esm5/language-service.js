1f686556c92a4816d0818651d003e944f15239c2
Miško Hevery <misko@hevery.com>
2017-11-29 22:47:36 +0000

    647ca64 docs(aio): Updating with Ignite UI for Angular (#20663)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.3-c7b211c');
+var VERSION = new Version('5.0.3-647ca64');
 
 /**
  * @license
