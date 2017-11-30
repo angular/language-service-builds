9e9f45288901e2e79812706e0a8d5759d21eb45a
Miško Hevery <misko@hevery.com>
2017-11-30 18:24:42 +0000

    47addd1 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.2-ba6af2a');
+var VERSION = new Version('5.1.0-beta.2-47addd1');
 
 /**
  * @license
