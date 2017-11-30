974142eee3424028f44613590a2a8d45bcf5708b
Misko Hevery <misko@hevery.com>
2017-11-30 04:48:58 +0000

    ba6af2a style: broken build due to missing new lines

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-7a2550a
+ * @license Angular v5.1.0-beta.2-ba6af2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.2-7a2550a');
+var VERSION = new Version('5.1.0-beta.2-ba6af2a');
 
 /**
  * @license
