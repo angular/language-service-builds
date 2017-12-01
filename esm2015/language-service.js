e335079b74a3942f3f7dc89a3acdaa7a41783903
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:10:33 +0000

    d507057 docs: add changelog for 5.0.5

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f582620
+ * @license Angular v5.1.0-rc.0-d507057
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-rc.0-f582620');
+const VERSION = new Version('5.1.0-rc.0-d507057');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f582620
+ * @license Angular v5.1.0-rc.0-d507057
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-rc.0-f582620');
+var VERSION = new Version('5.1.0-rc.0-d507057');
 
 /**
  * @license
