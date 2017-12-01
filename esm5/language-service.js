e822f2a605519ed8b20aecbe5c77b25dcb76a5e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:45:48 +0000

    f582620 fix(service-worker): use relative path for ngsw.json

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f841fbe
+ * @license Angular v5.1.0-rc.0-f582620
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-rc.0-f841fbe');
+var VERSION = new Version('5.1.0-rc.0-f582620');
 
 /**
  * @license
