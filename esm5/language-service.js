50feccd8d4fa9f9f55d88105a18bb292032db7fd
Misko Hevery <misko@hevery.com>
2017-12-01 05:55:34 +0000

    cbd93fe docs: add changelog for 5.0.4

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-e95b5d7
+ * @license Angular v5.0.4-cbd93fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2596,7 +2596,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.3-e95b5d7');
+var VERSION = new Version('5.0.4-cbd93fe');
 
 /**
  * @license
