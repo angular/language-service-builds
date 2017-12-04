1629df45ed5101190686b1708ddd5e024005867e
Igor Minar <igor@angularjs.org>
2017-12-04 23:16:02 +0000

    7e7ff2e ci: fix the payload-size checking scripts (#20683)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-rc.0-d507057');
+var VERSION$1 = new Version('5.1.0-rc.1-7e7ff2e');
 
 /**
  * @fileoverview added by tsickle
@@ -42012,7 +42012,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42443,7 +42443,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-rc.0-d507057');
+var VERSION$2 = new Version$1('5.1.0-rc.1-7e7ff2e');
 
 /**
  * @fileoverview added by tsickle
@@ -55832,7 +55832,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58474,7 +58474,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-rc.0-d507057');
+var VERSION = new Version$1('5.1.0-rc.1-7e7ff2e');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-rc.0-d507057');
+var VERSION = new Version('5.1.0-rc.1-7e7ff2e');
 
 /**
  * @license
