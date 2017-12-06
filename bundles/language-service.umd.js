eaa7c3f0f336c24971afc980b77cf3d09dd8008b
Igor Minar <igor@angularjs.org>
2017-12-06 15:15:35 +0000

    c91c0d4 fix(service-worker): esm2015 points to wrong path (#20800)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.0.5-9efb5da');
+var VERSION$1 = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
@@ -41301,7 +41301,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -41690,7 +41690,7 @@ var Version$1 = (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.0.5-9efb5da');
+var VERSION$2 = new Version$1('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
@@ -54854,7 +54854,7 @@ var NgModuleFactory_ = (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -57443,7 +57443,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.0.5-9efb5da');
+var VERSION = new Version$1('5.0.5-c91c0d4');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
