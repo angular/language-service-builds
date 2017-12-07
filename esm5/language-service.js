07aed6e9babbd80aa4d59ca735e0d35be3030f2f
Jason Aden <jasonaden@google.com>
2017-12-07 02:01:18 +0000

    6727336 fix(compiler-cli): Fix swallowed Error messages (#20846)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c2dbc55
+ * @license Angular v5.1.0-6727336
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-c2dbc55');
+var VERSION = new Version('5.1.0-6727336');
 
 /**
  * @license
