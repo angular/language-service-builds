78db253ae7dae962f01eb775c70eb6d9ca870a6b
Jason Aden <jasonaden@google.com>
2017-12-07 21:48:53 +0000

    9dd60a5 build: upgrade bazel rules to latest (#20768)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-6727336
+ * @license Angular v5.1.0-9dd60a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-6727336');
+var VERSION = new Version('5.1.0-9dd60a5');
 
 /**
  * @license
