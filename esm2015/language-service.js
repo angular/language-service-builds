c44d301c5025a1ea8dd1a62d90b1d879d9fc0044
Jason Aden <jasonaden@google.com>
2017-12-08 18:28:53 +0000

    225baf4 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-46aa0a1
+ * @license Angular v5.1.0-225baf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-46aa0a1');
+const VERSION = new Version('5.1.0-225baf4');
 
 /**
  * @license
