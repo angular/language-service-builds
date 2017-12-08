8aeea8b0656904077f7198a475dbbac55b0cdb9c
Jason Aden <jasonaden@google.com>
2017-12-08 01:29:34 +0000

    d8cc09b fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-ef534c0
+ * @license Angular v5.1.0-d8cc09b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-ef534c0');
+var VERSION = new Version('5.1.0-d8cc09b');
 
 /**
  * @license
