cdb47b3f843a7704b0fd4940208ff47c69a06013
Jason Aden <jasonaden@google.com>
2017-12-11 19:56:29 +0000

    7363b3d build: remove bazel option --noshow_results (#20943)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.0-13e663c');
+var VERSION$1 = new Version('5.1.0-7363b3d');
 
 /**
  * @fileoverview added by tsickle
@@ -42048,7 +42048,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42479,7 +42479,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.0-13e663c');
+var VERSION$2 = new Version$1('5.1.0-7363b3d');
 
 /**
  * @fileoverview added by tsickle
@@ -55854,7 +55854,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58496,7 +58496,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.0-13e663c');
+var VERSION = new Version$1('5.1.0-7363b3d');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
