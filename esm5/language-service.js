0611194a5919cc93cc6dc7272ac73baefe59166b
Jason Aden <jasonaden@google.com>
2017-12-12 00:28:25 +0000

    add3589 ci: use container version in cache key (#20952)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-81d497c
+ * @license Angular v5.1.0-add3589
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-81d497c');
+var VERSION = new Version('5.1.0-add3589');
 
 /**
  * @license
