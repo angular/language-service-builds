50fe194790b3a56cc880be355d5b606691585b5e
Jason Aden <jasonaden@google.com>
2017-12-12 00:26:15 +0000

    81d497c build: pin ChromeDriver version (#20940)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-70cd124');
+var VERSION = new Version('5.1.0-81d497c');
 
 /**
  * @license
