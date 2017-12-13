e8d83fd40494fdf0cd3743e6e760eb932d4a41d4
GitHub <noreply@github.com>
2017-12-13 19:43:46 +0000

    7fd441e ci: fix invalid bazel query expression

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c8010f4
+ * @license Angular v5.1.0-7fd441e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-c8010f4');
+var VERSION = new Version('5.1.0-7fd441e');
 
 /**
  * @license
