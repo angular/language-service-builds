212ca58a2250062c747aa8f2da080a14b1b89a48
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 17:29:47 +0000

    49c45f3 ci(aio): move e2e tests to non-optional job

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.1-e3140ae');
+var VERSION$1 = new Version('5.1.1-49c45f3');
 
 /**
  * @fileoverview added by tsickle
@@ -41931,7 +41931,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42362,7 +42362,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.1-e3140ae');
+var VERSION$2 = new Version$1('5.1.1-49c45f3');
 
 /**
  * @fileoverview added by tsickle
@@ -55805,7 +55805,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58447,7 +58447,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.1-e3140ae');
+var VERSION = new Version$1('5.1.1-49c45f3');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
