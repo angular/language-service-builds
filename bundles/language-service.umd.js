c31915265251b0e9075007122e40b72492ae3a73
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:38:55 +0000

    c011ffa test(aio): correct usage of fakeAsync and inject together in test

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.1-756dd34');
+var VERSION$1 = new Version('5.1.1-c011ffa');
 
 /**
  * @fileoverview added by tsickle
@@ -41938,7 +41938,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42369,7 +42369,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.1-756dd34');
+var VERSION$2 = new Version$1('5.1.1-c011ffa');
 
 /**
  * @fileoverview added by tsickle
@@ -55812,7 +55812,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58454,7 +58454,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.1-756dd34');
+var VERSION = new Version$1('5.1.1-c011ffa');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
