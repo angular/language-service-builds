45777d8dcc1adea749e6cf15575be87df7fcec0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 21:58:52 +0000

    6353b77 docs: add changelog for 5.1.2

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-a36dfd4
+ * @license Angular v5.1.2-6353b77
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.1-a36dfd4');
+const VERSION = new Version('5.1.2-6353b77');
 
 /**
  * @license
