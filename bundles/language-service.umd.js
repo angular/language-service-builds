b7a8985f3d64b986f5947103d268dc7c6d8b21b8
Igor Minar <igor@angularjs.org>
2017-12-21 21:35:39 +0000

    0487a9f build: update pullapprove: (#21117)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-871ece6
+ * @license Angular v5.2.0-beta.1-0487a9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-beta.1-871ece6
+ * @license Angular v5.2.0-beta.1-0487a9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.0-beta.1-871ece6');
+var VERSION$1 = new Version('5.2.0-beta.1-0487a9f');
 
 /**
  * @fileoverview added by tsickle
@@ -42109,7 +42109,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.0-beta.1-871ece6
+ * @license Angular v5.2.0-beta.1-0487a9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42540,7 +42540,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.0-beta.1-871ece6');
+var VERSION$2 = new Version$1('5.2.0-beta.1-0487a9f');
 
 /**
  * @fileoverview added by tsickle
@@ -55935,7 +55935,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.2.0-beta.1-871ece6
+ * @license Angular v5.2.0-beta.1-0487a9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58577,7 +58577,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.0-beta.1-871ece6');
+var VERSION = new Version$1('5.2.0-beta.1-0487a9f');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
