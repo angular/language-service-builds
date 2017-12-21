4b02c81528e2175870083f3dcb0c8580c170fc0b
Igor Minar <igor@angularjs.org>
2017-12-21 20:24:36 +0000

    d1f4500 fix(animations): renaming issue with DOMAnimation. (#21125)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-6353b77
+ * @license Angular v5.1.2-d1f4500
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-6353b77');
+var VERSION = new Version('5.1.2-d1f4500');
 
 /**
  * @license
