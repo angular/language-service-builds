78eb36159b58ccbce487be30e161fd9fd7ebdd74
Igor Minar <igor@angularjs.org>
2017-12-22 04:31:16 +0000

    6a58184 build: make umd.min.js source map paths relative (#21147)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-bc66d27
+ * @license Angular v5.2.0-beta.1-6a58184
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-beta.1-bc66d27');
+var VERSION = new Version('5.2.0-beta.1-6a58184');
 
 /**
  * @license
