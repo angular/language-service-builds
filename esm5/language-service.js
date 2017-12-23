53199467d4b0ae38b836c5c033c7f9dee87278bd
Igor Minar <igor@angular.io>
2017-12-23 05:58:37 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-5de91fe');
+var VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @license
