cead79781a7441a360428990a2b8128773e07803
Igor Minar <igor@angular.io>
2017-12-23 05:55:06 +0000

    d787b55 build(common): generate ts declarations for i18n locale files (#21127)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.2-3e34fa8');
+const VERSION = new Version('5.1.2-d787b55');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-3e34fa8');
+var VERSION = new Version('5.1.2-d787b55');
 
 /**
  * @license
