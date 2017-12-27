8602d3bb77d98ad0e2153f767f5d64f161904c75
Igor Minar <igor@angular.io>
2017-12-27 21:35:26 +0000

    66043e0 docs(aio): Sort in the api type dropdown (#21030) (#21176)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.2-12702b2');
+var VERSION$1 = new Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
@@ -41960,7 +41960,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42391,7 +42391,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.2-12702b2');
+var VERSION$2 = new Version$1('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
@@ -55834,7 +55834,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58476,7 +58476,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.2-12702b2');
+var VERSION = new Version$1('5.1.2-66043e0');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-12702b2');
+var VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @license
