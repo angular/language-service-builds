d0439e2563f9481584febadfdb2a99be82de33a7
Igor Minar <igor@angular.io>
2017-12-27 21:43:31 +0000

    d8abf70 aio: Sort in the api type dropdown (#21176)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-efd9c09
+ * @license Angular v5.2.0-beta.1-d8abf70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-beta.1-efd9c09');
+var VERSION = new Version('5.2.0-beta.1-d8abf70');
 
 /**
  * @license
