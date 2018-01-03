4f78af79440c1ff8649578a7e04aca578fb0611f
Misko Hevery <misko@hevery.com>
2018-01-03 22:03:43 +0000

    a7a7bd3 build: update rules_typescript (#21290)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-67630f8
+ * @license Angular v5.1.2-a7a7bd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.2-67630f8');
+const VERSION = new Version('5.1.2-a7a7bd3');
 
 /**
  * @license
