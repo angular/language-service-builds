4ed608183c1c47aecbfe2ddbff09a5c044bc84c1
Kara Erickson <karakara@google.com>
2018-01-04 23:53:28 +0000

    03d16fc docs(aio): add missing imports to interceptor example (#21259)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v5.2.0-rc.0-b5934fc
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-03d16fc
+ * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 
@@ -59,8 +59,8 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-b5934fc
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-03d16fc
+ * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 /**
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.0-rc.0-b5934fc');
+var VERSION$1 = new Version('5.2.0-rc.0-03d16fc');
 
 /**
  * @fileoverview added by tsickle
@@ -42087,8 +42087,8 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.0-rc.0-b5934fc
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-03d16fc
+ * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 /**
@@ -42518,7 +42518,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.0-rc.0-b5934fc');
+var VERSION$2 = new Version$1('5.2.0-rc.0-03d16fc');
 
 /**
  * @fileoverview added by tsickle
@@ -56713,8 +56713,8 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.0-rc.0-b5934fc
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-03d16fc
+ * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 /**
@@ -59357,7 +59357,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.0-rc.0-b5934fc');
+var VERSION = new Version$1('5.2.0-rc.0-03d16fc');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
