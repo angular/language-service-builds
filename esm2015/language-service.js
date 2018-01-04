8607c39ccfa3fc11264c696e71e09c8c3e99a2d2
Kara Erickson <karakara@google.com>
2018-01-04 22:58:44 +0000

    b7307c9 fix(aio): fix test message's mistake. (#21254)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-cc0b0a4
+ * @license Angular v5.1.3-b7307c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.3-cc0b0a4');
+const VERSION = new Version('5.1.3-b7307c9');
 
 /**
  * @license
