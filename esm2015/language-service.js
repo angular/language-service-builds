4121f634d7ca3e922ad20ac21785bdd203f40973
Misko Hevery <misko@hevery.com>
2018-01-04 00:21:27 +0000

    d138b38 docs: add changelog for 5.1.3

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.2-00308fc');
+const VERSION = new Version('5.1.3-d138b38');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-00308fc');
+var VERSION = new Version('5.1.3-d138b38');
 
 /**
  * @license
