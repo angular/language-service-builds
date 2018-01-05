ae6edc3d5bdca77798642dfb373204cd6cbbb05c
Kara Erickson <karakara@google.com>
2018-01-05 20:28:24 +0000

    e726d81 build: force fetch PR in merge script (#21295)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-6cd4b1b
+ * @license Angular v5.2.0-rc.0-e726d81
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-rc.0-6cd4b1b');
+var VERSION = new Version('5.2.0-rc.0-e726d81');
 
 /**
  * @license
