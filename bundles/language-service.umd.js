ecba800765ab00ab9c486d4ea9d42fc2d611eb75
Kara Erickson <karakara@google.com>
2018-01-05 20:24:43 +0000

    6cd4b1b build(aio): add `@nodoc` alias to the `@internal` tag (#21024)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.0-rc.0-24df08e');
+var VERSION$1 = new Version('5.2.0-rc.0-6cd4b1b');
 
 /**
  * @fileoverview added by tsickle
@@ -42087,7 +42087,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42518,7 +42518,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.0-rc.0-24df08e');
+var VERSION$2 = new Version$1('5.2.0-rc.0-6cd4b1b');
 
 /**
  * @fileoverview added by tsickle
@@ -56713,7 +56713,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.0-rc.0-24df08e
+ * @license Angular v5.2.0-rc.0-6cd4b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59357,7 +59357,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.0-rc.0-24df08e');
+var VERSION = new Version$1('5.2.0-rc.0-6cd4b1b');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
