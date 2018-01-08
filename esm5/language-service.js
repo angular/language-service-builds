7576524b51a9ca0f35ccf04ed5565217c6ee8e3f
Kara Erickson <karakara@google.com>
2018-01-08 18:54:48 +0000

    1bd03a4 docs(aio): document the template compiler options (#21333)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8b197b2
+ * @license Angular v5.1.3-1bd03a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.3-8b197b2');
+var VERSION = new Version('5.1.3-1bd03a4');
 
 /**
  * @license
