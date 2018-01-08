d47b7c2a1f6e940bdc04a578d31cdc5264830d1a
Kara Erickson <karakara@google.com>
2018-01-08 21:29:38 +0000

    6040ee3 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-74dbf7b
+ * @license Angular v5.2.0-rc.0-6040ee3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-rc.0-74dbf7b');
+var VERSION = new Version('5.2.0-rc.0-6040ee3');
 
 /**
  * @license
