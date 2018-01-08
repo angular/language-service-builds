b80b28aed4f9ca47d0e7ffe1d4957daefb6410b4
Kara Erickson <karakara@google.com>
2018-01-08 19:33:17 +0000

    7269fe5 ci: reformat itegration/_payload-limits.json to be easier to read (#21344)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-e994752
+ * @license Angular v5.2.0-rc.0-7269fe5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-rc.0-e994752');
+var VERSION = new Version('5.2.0-rc.0-7269fe5');
 
 /**
  * @license
