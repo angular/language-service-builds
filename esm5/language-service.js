2f758eed3ec2d043d60f14ede7ff76cdf56eb635
Kara Erickson <karakara@google.com>
2018-01-08 21:27:56 +0000

    fda3815 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-4f7c13d
+ * @license Angular v5.1.3-fda3815
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.3-4f7c13d');
+var VERSION = new Version('5.1.3-fda3815');
 
 /**
  * @license
