f21ac692d126aff80e627de50976cab63a3f6844
Kara Erickson <karakara@google.com>
2018-01-09 19:12:39 +0000

    84f6b14 fix(bazel): Give correct module names for ES6 output (#21320)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-39f81b2
+ * @license Angular v5.1.3-84f6b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.3-39f81b2');
+const VERSION = new Version('5.1.3-84f6b14');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-39f81b2
+ * @license Angular v5.1.3-84f6b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.3-39f81b2');
+var VERSION = new Version('5.1.3-84f6b14');
 
 /**
  * @license
