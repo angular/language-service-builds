f0940520fed343b93910162a7b33e0961edc1582
Kara Erickson <karakara@google.com>
2018-01-09 00:29:45 +0000

    a73b0b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.3-fda3815');
+const VERSION = new Version('5.1.3-a73b0b6');
 
 /**
  * @license
