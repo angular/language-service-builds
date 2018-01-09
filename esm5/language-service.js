2df216399e5ad0c6a6e2bcc87e91ffe145bf27b3
Kara Erickson <karakara@google.com>
2018-01-09 20:42:49 +0000

    96efb4b ci: temporary work-around for Travis issue (#21422)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-7c34807
+ * @license Angular v5.1.3-96efb4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2649,7 +2649,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.3-7c34807');
+var VERSION = new Version('5.1.3-96efb4b');
 
 /**
  * @license
