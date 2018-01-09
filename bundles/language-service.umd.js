0503cffa9f6b2cfc05db6977255201a590b93fd6
Kara Erickson <karakara@google.com>
2018-01-09 18:50:35 +0000

    39f81b2 build: upgrade Bazel to 0.9.0 (#21335)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -696,7 +696,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.1.3-0602aaa');
+var VERSION$1 = new Version('5.1.3-39f81b2');
 
 /**
  * @fileoverview added by tsickle
@@ -41960,7 +41960,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42391,7 +42391,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.1.3-0602aaa');
+var VERSION$2 = new Version$1('5.1.3-39f81b2');
 
 /**
  * @fileoverview added by tsickle
@@ -55834,7 +55834,7 @@ var NgModuleFactory_ = /** @class */ (function (_super) {
 }(NgModuleFactory));
 
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -58476,7 +58476,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.1.3-0602aaa');
+var VERSION = new Version$1('5.1.3-39f81b2');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2494,7 +2494,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.3-0602aaa');
+const VERSION = new Version('5.1.3-39f81b2');
 
 /**
  * @license
