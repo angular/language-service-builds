6fd1673e2055c554f96122c63ab1b15f2309b1c7
Alex Eagle <alexeagle@google.com>
2018-01-10 23:40:21 +0000

    aad1126 docs: update the release schedule with v6 info (#21435)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7b463df
+ * @license Angular v5.2.0-aad1126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-7b463df
+ * @license Angular v5.2.0-aad1126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.0-7b463df');
+var VERSION$1 = new Version('5.2.0-aad1126');
 
 /**
  * @fileoverview added by tsickle
@@ -42098,7 +42098,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.0-7b463df
+ * @license Angular v5.2.0-aad1126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42529,7 +42529,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.0-7b463df');
+var VERSION$2 = new Version$1('5.2.0-aad1126');
 
 /**
  * @fileoverview added by tsickle
@@ -56727,7 +56727,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.0-7b463df
+ * @license Angular v5.2.0-aad1126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59371,7 +59371,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.0-7b463df');
+var VERSION = new Version$1('5.2.0-aad1126');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
