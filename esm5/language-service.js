d9e4628bfea96e26df5910b5c058c613190be9dc
Alex Eagle <alexeagle@google.com>
2018-01-10 23:50:45 +0000

    a33ff2c docs(ivy): add Ivy as a recognized label (#21428)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-92984ba');
+var VERSION = new Version('5.2.0-a33ff2c');
 
 /**
  * @license
