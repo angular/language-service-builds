a9dda1fa16ce5d71800277478bcd8b48d4d912e1
Alex Eagle <alexeagle@google.com>
2018-01-10 21:25:14 +0000

    c4f02e2 build: move repeated tsconfig attributes to a macro (#20964)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f15ca6c
+ * @license Angular v5.2.0-c4f02e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-f15ca6c');
+var VERSION = new Version('5.2.0-c4f02e2');
 
 /**
  * @license
