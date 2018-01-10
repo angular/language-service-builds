88a9e5d57ec4625702d8979c0da5ca6bf8267fe9
Alex Eagle <alexeagle@google.com>
2018-01-10 22:14:22 +0000

    2ad92ed build(bazel): fix merge conflict between c4f02e2 and ef956a2 (#21453)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-e3e2fc0
+ * @license Angular v5.2.0-2ad92ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-e3e2fc0');
+var VERSION = new Version('5.2.0-2ad92ed');
 
 /**
  * @license
