32e50f9bd5e6d4d6cc50e02c3c68b957b18771a5
Alex Eagle <alexeagle@google.com>
2018-01-10 21:12:09 +0000

    f15ca6c fix(aio): support multibyte character in heading (#21414)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0de219a
+ * @license Angular v5.2.0-f15ca6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-0de219a');
+var VERSION = new Version('5.2.0-f15ca6c');
 
 /**
  * @license
