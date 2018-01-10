025d52ab4b805f1c788d21f8d50128181f7619fd
Alex Eagle <alexeagle@google.com>
2018-01-10 20:58:38 +0000

    ff6047c build(aio): upgrade to uglify-es@3.3.5 (#21350)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a76ab1f
+ * @license Angular v5.2.0-ff6047c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2651,7 +2651,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-a76ab1f');
+var VERSION = new Version('5.2.0-ff6047c');
 
 /**
  * @license
