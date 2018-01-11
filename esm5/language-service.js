0cf31b79c8967b1d1bda1ae358b79e71e344bcd0
Alex Eagle <alexeagle@google.com>
2018-01-11 15:22:47 +0000

    b49d54e docs(forms): clarify note in reactive forms doc (#21134)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2652,7 +2652,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-55fd82e');
+var VERSION = new Version('5.2.0-b49d54e');
 
 /**
  * @license
