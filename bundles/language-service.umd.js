3f4ab886a5a7525ce0e5b72e2381d3dde54a56c9
Alex Eagle <alexeagle@google.com>
2018-01-12 22:56:17 +0000

    2d44a2a docs(compiler): document the $any type cast function (#20968)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.0-c5c6d84');
+var VERSION$1 = new Version('5.2.0-2d44a2a');
 
 /**
  * @fileoverview added by tsickle
@@ -42100,7 +42100,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42531,7 +42531,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.0-c5c6d84');
+var VERSION$2 = new Version$1('5.2.0-2d44a2a');
 
 /**
  * @fileoverview added by tsickle
@@ -57322,7 +57322,7 @@ function assertDataInRange(index, arr) {
 }
 
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59967,7 +59967,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.0-c5c6d84');
+var VERSION = new Version$1('5.2.0-2d44a2a');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
