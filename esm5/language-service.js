211e57b839f4ecd14d0533359342d58b40a23235
Alex Eagle <alexeagle@google.com>
2018-01-16 15:30:42 +0000

    d6ba9f9 build(aio): move file cleaning to later in the doc gen (#21540)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-54bf179
+ * @license Angular v5.2.0-d6ba9f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-54bf179');
+var VERSION = new Version('5.2.0-d6ba9f9');
 
 /**
  * @license
