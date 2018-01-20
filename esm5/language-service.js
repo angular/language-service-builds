17116ccd550db884a9139690fdb344ca13d14c86
Misko Hevery <misko@hevery.com>
2018-01-20 05:01:07 +0000

    d7dbdc5 docs: fix stray div and reformat paragraph (#21676)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-554129d
+ * @license Angular v5.2.1-d7dbdc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.1-554129d');
+var VERSION = new Version('5.2.1-d7dbdc5');
 
 /**
  * @license
