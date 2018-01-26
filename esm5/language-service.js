f2821fc348c3905a6d6f596cad66962ee321ea72
Misko Hevery <misko@hevery.com>
2018-01-26 06:44:25 +0000

    56f3e18 fix(forms): allow FormBuilder to create controls with any formState type (#20917)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-27ecd07
+ * @license Angular v5.2.2-56f3e18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.2-27ecd07');
+var VERSION = new Version('5.2.2-56f3e18');
 
 /**
  * @license
