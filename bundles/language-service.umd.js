61b9152f98aee491da5215af209b1551fa385047
Miško Hevery <misko@hevery.com>
2018-01-26 18:51:10 +0000

    108fa15 fix(aio): close SideNav on non-sidenav doc on wide screen (#21538)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -698,7 +698,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.1-e608052');
+var VERSION$1 = new Version('6.0.0-beta.1-108fa15');
 
 /**
  * @fileoverview added by tsickle
@@ -43216,7 +43216,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -43647,7 +43647,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.1-e608052');
+var VERSION$2 = new Version$1('6.0.0-beta.1-108fa15');
 
 /**
  * @fileoverview added by tsickle
@@ -58973,7 +58973,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -61622,7 +61622,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.1-e608052');
+var VERSION = new Version$1('6.0.0-beta.1-108fa15');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
