819843a1514bf13d16672b2c97f14614c5700b8a
Misko Hevery <misko@hevery.com>
2018-01-27 19:09:58 +0000

    6245637 fix(ivy): correct query read logic after merges (#21749)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-ab69f12
+ * @license Angular v6.0.0-beta.1-6245637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.1-ab69f12');
+const VERSION = new Version('6.0.0-beta.1-6245637');
 
 /**
  * @license
