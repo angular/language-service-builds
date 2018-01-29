bc57a411d3040bbc6052692fb8faa3d3ab4ee834
Jason Aden <jasonaden@google.com>
2018-01-29 20:07:42 +0000

    18174e5 feat(ivy): support ng-content projection in the ivy compiler (#21764)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-72265f7
+ * @license Angular v6.0.0-beta.1-18174e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.1-72265f7');
+const VERSION = new Version('6.0.0-beta.1-18174e5');
 
 /**
  * @license
