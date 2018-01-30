1a06b3bdadf1bee937a48c9bfe4c8b81a78bae0b
GitHub <noreply@github.com>
2018-01-30 23:05:32 +0000

    27a2515 ci: mark PRs with rejection as not green

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import * as ts from 'typescript';
-export declare function getExternalFiles(project: any): string[] | undefined;
-export declare function create(info: any): ts.LanguageService;
+export * from './language-service';
