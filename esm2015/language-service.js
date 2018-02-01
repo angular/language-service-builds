44cdfb1a862bcf7c85b289cff62eaa45f9d45959
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 17:06:18 +0000

    65cf1ad fix(ivy): remove unnecessary parameter of NgOnChangesFeature (#21879)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-8b14488
+ * @license Angular v6.0.0-beta.2-65cf1ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.2-8b14488');
+const VERSION = new Version('6.0.0-beta.2-65cf1ad');
 
 /**
  * @license
