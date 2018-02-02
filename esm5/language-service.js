c9ea36de2674aa1e0e49fefdd0ed53bda287900c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:56:32 +0000

    102d06b docs: consistency fix in describing a custom tag (#21747)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.3-aa9ba7f');
+var VERSION = new Version('5.2.3-102d06b');
 
 /**
  * @license
