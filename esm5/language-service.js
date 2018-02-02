eefe6235fe86e3b2fd2805d2285a30f0351f087b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:14:51 +0000

    75eecdc docs: add missing underline (#21892) PR Close #21892

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.3-965eecc');
+var VERSION = new Version('5.2.3-75eecdc');
 
 /**
  * @license
