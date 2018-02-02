22fbc07b13733799c77c2ceb32f396f7a0eedc54
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:32:07 +0000

    447783e docs: add docs for IE (#21824)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-743d8bc
+ * @license Angular v6.0.0-beta.2-447783e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.2-743d8bc
+ * @license Angular v6.0.0-beta.2-447783e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -698,7 +698,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.2-743d8bc');
+var VERSION$1 = new Version('6.0.0-beta.2-447783e');
 
 /**
  * @fileoverview added by tsickle
@@ -43357,7 +43357,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.2-743d8bc
+ * @license Angular v6.0.0-beta.2-447783e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -43794,7 +43794,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.2-743d8bc');
+var VERSION$2 = new Version$1('6.0.0-beta.2-447783e');
 
 /**
  * @fileoverview added by tsickle
@@ -59324,7 +59324,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.2-743d8bc
+ * @license Angular v6.0.0-beta.2-447783e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -61973,7 +61973,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.2-743d8bc');
+var VERSION = new Version$1('6.0.0-beta.2-447783e');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
