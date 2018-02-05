db4896ac2920a9731e4a39c641568dac9fd5b91d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:32:23 +0000

    b155ae1 ci: add config for g3 status (#21996)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-d38e088
+ * @license Angular v5.2.3-b155ae1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.3-d38e088');
+var VERSION = new Version('5.2.3-b155ae1');
 
 /**
  * @license
