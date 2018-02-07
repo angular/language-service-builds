3cdf9ceeab452b84089103bddd064395a3608ade
Miško Hevery <misko@hevery.com>
2018-02-07 20:39:33 +0000

    941e88f feat(forms): multiple validators for array method (#20766)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7e928db
+ * @license Angular v6.0.0-beta.3-941e88f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-7e928db
+ * @license Angular v6.0.0-beta.3-941e88f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -698,7 +698,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.3-7e928db');
+var VERSION$1 = new Version('6.0.0-beta.3-941e88f');
 
 /**
  * @fileoverview added by tsickle
@@ -43357,7 +43357,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.3-7e928db
+ * @license Angular v6.0.0-beta.3-941e88f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -43794,7 +43794,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.3-7e928db');
+var VERSION$2 = new Version$1('6.0.0-beta.3-941e88f');
 
 /**
  * @fileoverview added by tsickle
@@ -59343,7 +59343,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.3-7e928db
+ * @license Angular v6.0.0-beta.3-941e88f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -61992,7 +61992,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.3-7e928db');
+var VERSION = new Version$1('6.0.0-beta.3-941e88f');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
