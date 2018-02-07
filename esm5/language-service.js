9dc20cb065ef4486278183f86869b89d1aab85b1
Miško Hevery <misko@hevery.com>
2018-02-07 20:58:05 +0000

    56b9591 fix(forms): prevent event emission on enable/disable when emitEvent is false (#12366) (#21018)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-103727a
+ * @license Angular v5.2.4-56b9591
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.4-103727a');
+var VERSION = new Version('5.2.4-56b9591');
 
 /**
  * @license
