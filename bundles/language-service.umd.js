3e3a4f1d2f28fee68b127cb383fd8a1e7c971cb9
Miško Hevery <misko@hevery.com>
2018-02-07 21:32:19 +0000

    e56de10 fix(core): ensure initial value of QueryList length (#21980) (#21982)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-54a1431
+ * @license Angular v6.0.0-beta.3-e56de10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-54a1431
+ * @license Angular v6.0.0-beta.3-e56de10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -698,7 +698,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.3-54a1431');
+var VERSION$1 = new Version('6.0.0-beta.3-e56de10');
 
 /**
  * @fileoverview added by tsickle
@@ -43357,7 +43357,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.3-54a1431
+ * @license Angular v6.0.0-beta.3-e56de10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -43794,7 +43794,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.3-54a1431');
+var VERSION$2 = new Version$1('6.0.0-beta.3-e56de10');
 
 /**
  * @fileoverview added by tsickle
@@ -49086,6 +49086,7 @@ var QueryList = /** @class */ (function () {
         this.dirty = true;
         this._results = [];
         this.changes = new EventEmitter();
+        this.length = 0;
     }
     /**
      * See
@@ -59343,7 +59344,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.3-54a1431
+ * @license Angular v6.0.0-beta.3-e56de10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -61992,7 +61993,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.3-54a1431');
+var VERSION = new Version$1('6.0.0-beta.3-e56de10');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
