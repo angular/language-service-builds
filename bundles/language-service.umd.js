6183896fedcec8171a0f1ac264ae0ae57b872448
Miško Hevery <misko@hevery.com>
2018-02-08 22:59:08 +0000

    f80b9da Revert "docs(common): add `HttpParamsOptions` to the public API (#20332)"

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-c5ec8d9
+ * @license Angular v5.2.4-f80b9da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-c5ec8d9
+ * @license Angular v5.2.4-f80b9da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.4-c5ec8d9');
+var VERSION$1 = new Version('5.2.4-f80b9da');
 
 /**
  * @fileoverview added by tsickle
@@ -42103,7 +42103,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.4-c5ec8d9
+ * @license Angular v5.2.4-f80b9da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42534,7 +42534,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.4-c5ec8d9');
+var VERSION$2 = new Version$1('5.2.4-f80b9da');
 
 /**
  * @fileoverview added by tsickle
@@ -56776,7 +56776,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.4-c5ec8d9
+ * @license Angular v5.2.4-f80b9da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59425,7 +59425,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.4-c5ec8d9');
+var VERSION = new Version$1('5.2.4-f80b9da');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
