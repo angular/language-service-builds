e68c8ac523952731fc9418b51d710d1e3dbf320b
Miško Hevery <misko@hevery.com>
2018-02-10 01:48:47 +0000

    7e9b120 build: update to latest bazel rules (#22127)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b081dfe
+ * @license Angular v6.0.0-beta.3-7e9b120
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.3-b081dfe');
+var VERSION = new Version('6.0.0-beta.3-7e9b120');
 
 /**
  * @license
