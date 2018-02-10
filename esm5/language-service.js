8d09cede9a3766721175281dda8fedac0535aa26
Miško Hevery <misko@hevery.com>
2018-02-10 01:37:09 +0000

    b081dfe fix(bazel): allow TS to read ambient typings (#21876)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-c878d55
+ * @license Angular v6.0.0-beta.3-b081dfe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.3-c878d55');
+var VERSION = new Version('6.0.0-beta.3-b081dfe');
 
 /**
  * @license
