40268e9f1fb905938218e6025f201a21d8e845d9
Miško Hevery <misko@hevery.com>
2018-02-12 23:05:25 +0000

    1a897e4 docs(aio): add angular-playground to resources (#22042)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-603e50d
+ * @license Angular v5.2.4-1a897e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.4-603e50d');
+var VERSION = new Version('5.2.4-1a897e4');
 
 /**
  * @license
