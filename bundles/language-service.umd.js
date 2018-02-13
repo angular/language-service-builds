e131546258555fc77438e8931f964efa6bfd6204
Miško Hevery <misko@hevery.com>
2018-02-13 21:31:25 +0000

    72f8abd fix(compiler): make unary plus operator consistent to JavaScript (#22154)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-20a900b
+ * @license Angular v6.0.0-beta.3-72f8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-20a900b
+ * @license Angular v6.0.0-beta.3-72f8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.3-20a900b');
+var VERSION$1 = new Version('6.0.0-beta.3-72f8abd');
 
 /**
  * @fileoverview added by tsickle
@@ -6847,7 +6847,8 @@ var _ParseAST = /** @class */ (function () {
             switch (operator) {
                 case '+':
                     this.advance();
-                    return this.parsePrefix();
+                    result = this.parsePrefix();
+                    return new Binary(this.span(start), '-', result, new LiteralPrimitive(new ParseSpan(start, start), 0));
                 case '-':
                     this.advance();
                     result = this.parsePrefix();
@@ -43686,7 +43687,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.3-20a900b
+ * @license Angular v6.0.0-beta.3-72f8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45430,7 +45431,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.3-20a900b');
+var VERSION$2 = new Version$1('6.0.0-beta.3-72f8abd');
 
 /**
  * @fileoverview added by tsickle
@@ -59930,7 +59931,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.3-20a900b
+ * @license Angular v6.0.0-beta.3-72f8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62579,7 +62580,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.3-20a900b');
+var VERSION = new Version$1('6.0.0-beta.3-72f8abd');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
