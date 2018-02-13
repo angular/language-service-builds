a757e9fc652a82bd5cd2eb3eb46ae5c570cb97e6
Miško Hevery <misko@hevery.com>
2018-02-13 18:58:11 +0000

    2de0d4c perf(ivy): use official build optimizer rollup plugin in int test (#22121)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-8ec21fc
+ * @license Angular v6.0.0-beta.3-2de0d4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-8ec21fc
+ * @license Angular v6.0.0-beta.3-2de0d4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.3-8ec21fc');
+var VERSION$1 = new Version('6.0.0-beta.3-2de0d4c');
 
 /**
  * @fileoverview added by tsickle
@@ -43686,7 +43686,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.3-8ec21fc
+ * @license Angular v6.0.0-beta.3-2de0d4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45430,7 +45430,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.3-8ec21fc');
+var VERSION$2 = new Version$1('6.0.0-beta.3-2de0d4c');
 
 /**
  * @fileoverview added by tsickle
@@ -58930,8 +58930,7 @@ function createTView() {
         contentCheckHooks: null,
         viewHooks: null,
         viewCheckHooks: null,
-        destroyHooks: null,
-        objectLiterals: null
+        destroyHooks: null
     };
 }
 /**
@@ -59157,6 +59156,11 @@ function initBindings() {
  * @template T
  * @param {?} index
  * @param {?=} value
+ * @return {?}
+ */
+
+/**
+ * Gets the binding at the current bindingIndex
  * @return {?}
  */
 
@@ -59926,7 +59930,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.3-8ec21fc
+ * @license Angular v6.0.0-beta.3-2de0d4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62575,7 +62579,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.3-8ec21fc');
+var VERSION = new Version$1('6.0.0-beta.3-2de0d4c');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
