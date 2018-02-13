0de399c2fdc5b821e0991f147a47475e2cc5f6e7
Miško Hevery <misko@hevery.com>
2018-02-13 00:31:08 +0000

    e97afae docs(aio): fix typo in "preserveWhitespaces" example (#22182)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-c6bdc83
+ * @license Angular v5.2.4-e97afae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.4-c6bdc83');
+var VERSION = new Version('5.2.4-e97afae');
 
 /**
  * @license
