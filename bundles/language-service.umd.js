85bf650498eb04d60512da212856013265dbc717
Miško Hevery <misko@hevery.com>
2018-02-13 18:48:10 +0000

    8ec21fc ci: enable bazel remote caching on CircleCI (#21784)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-eb48750
+ * @license Angular v6.0.0-beta.3-8ec21fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-eb48750
+ * @license Angular v6.0.0-beta.3-8ec21fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.3-eb48750');
+var VERSION$1 = new Version('6.0.0-beta.3-8ec21fc');
 
 /**
  * @fileoverview added by tsickle
@@ -43686,7 +43686,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.3-eb48750
+ * @license Angular v6.0.0-beta.3-8ec21fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45430,7 +45430,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.3-eb48750');
+var VERSION$2 = new Version$1('6.0.0-beta.3-8ec21fc');
 
 /**
  * @fileoverview added by tsickle
@@ -59926,7 +59926,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.3-eb48750
+ * @license Angular v6.0.0-beta.3-8ec21fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62575,7 +62575,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.3-eb48750');
+var VERSION = new Version$1('6.0.0-beta.3-8ec21fc');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
