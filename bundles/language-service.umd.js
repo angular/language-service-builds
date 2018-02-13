e9b7af6e26a5fd8b4e95e0ee355395a0c373c14e
Miško Hevery <misko@hevery.com>
2018-02-13 19:58:48 +0000

    16d1700 fix(core): add stacktrace in log when error during cleanup component in TestBed (#22162)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b75cf3f
+ * @license Angular v6.0.0-beta.3-16d1700
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-b75cf3f
+ * @license Angular v6.0.0-beta.3-16d1700
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.3-b75cf3f');
+var VERSION$1 = new Version('6.0.0-beta.3-16d1700');
 
 /**
  * @fileoverview added by tsickle
@@ -43686,7 +43686,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.3-b75cf3f
+ * @license Angular v6.0.0-beta.3-16d1700
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45430,7 +45430,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.3-b75cf3f');
+var VERSION$2 = new Version$1('6.0.0-beta.3-16d1700');
 
 /**
  * @fileoverview added by tsickle
@@ -59930,7 +59930,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.3-b75cf3f
+ * @license Angular v6.0.0-beta.3-16d1700
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62579,7 +62579,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.3-b75cf3f');
+var VERSION = new Version$1('6.0.0-beta.3-16d1700');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
