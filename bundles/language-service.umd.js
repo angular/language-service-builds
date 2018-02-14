f6528c400ff88254fd440a821a8b6095c7b5a31d
Miško Hevery <misko@hevery.com>
2018-02-14 05:21:57 +0000

    c4f841f fix(core): add stacktrace in log when error during cleanup component in TestBed (#22162)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.4-1b8ea10');
+var VERSION$1 = new Version('5.2.4-c4f841f');
 
 /**
  * @fileoverview added by tsickle
@@ -6815,8 +6815,7 @@ var _ParseAST = /** @class */ (function () {
             switch (operator) {
                 case '+':
                     this.advance();
-                    result = this.parsePrefix();
-                    return new Binary(this.span(start), '-', result, new LiteralPrimitive(new ParseSpan(start, start), 0));
+                    return this.parsePrefix();
                 case '-':
                     this.advance();
                     result = this.parsePrefix();
@@ -42104,7 +42103,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42535,7 +42534,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.4-1b8ea10');
+var VERSION$2 = new Version$1('5.2.4-c4f841f');
 
 /**
  * @fileoverview added by tsickle
@@ -56777,7 +56776,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59426,7 +59425,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.4-1b8ea10');
+var VERSION = new Version$1('5.2.4-c4f841f');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
