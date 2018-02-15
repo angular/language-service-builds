40ecb4eed2bac8385d75305f40295ca32547769b
Victor Berchet <victor@suumit.com>
2018-02-15 22:40:56 +0000

    5f52ea3 feat(bazel): ng_module produces bundle index (#22176)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-6a57264
+ * @license Angular v5.2.5-5f52ea3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.5-6a57264
+ * @license Angular v5.2.5-5f52ea3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.5-6a57264');
+var VERSION$1 = new Version('5.2.5-5f52ea3');
 
 /**
  * @fileoverview added by tsickle
@@ -39224,6 +39224,8 @@ var CompilerHostAdapter = /** @class */ (function () {
         this.collector = new collector.MetadataCollector();
     }
     CompilerHostAdapter.prototype.getMetadataFor = function (fileName) {
+        if (!this.host.fileExists(fileName + '.ts'))
+            return undefined;
         var sourceFile = this.host.getSourceFile(fileName + '.ts', ts__default.ScriptTarget.Latest);
         return sourceFile && this.collector.getMetadata(sourceFile);
     };
@@ -42104,7 +42106,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.5-6a57264
+ * @license Angular v5.2.5-5f52ea3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42535,7 +42537,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.5-6a57264');
+var VERSION$2 = new Version$1('5.2.5-5f52ea3');
 
 /**
  * @fileoverview added by tsickle
@@ -56777,7 +56779,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.5-6a57264
+ * @license Angular v5.2.5-5f52ea3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59426,7 +59428,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.5-6a57264');
+var VERSION = new Version$1('5.2.5-5f52ea3');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
