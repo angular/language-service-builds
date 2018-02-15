0ff6ca565322ead05b6b495351e2d34a25ba7f35
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 00:26:51 +0000

    1e3e0fa fix(aio): improve printing styles (#19651)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-0c88d5d
+ * @license Angular v5.2.5-1e3e0fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.5-0c88d5d');
+var VERSION = new Version('5.2.5-1e3e0fa');
 
 /**
  * @license
