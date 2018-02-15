7586769c6392636e9477dc251252a58b6bccfbb2
Victor Berchet <victor@suumit.com>
2018-02-15 22:38:33 +0000

    029dbf0 feat(bazel): ng_module produces bundle index (#22176)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-03d93c9
+ * @license Angular v6.0.0-beta.4-029dbf0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-03d93c9
+ * @license Angular v6.0.0-beta.4-029dbf0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.4-03d93c9');
+var VERSION$1 = new Version('6.0.0-beta.4-029dbf0');
 
 /**
  * @fileoverview added by tsickle
@@ -40797,6 +40797,8 @@ var CompilerHostAdapter = /** @class */ (function () {
         this.collector = new collector.MetadataCollector();
     }
     CompilerHostAdapter.prototype.getMetadataFor = function (fileName) {
+        if (!this.host.fileExists(fileName + '.ts'))
+            return undefined;
         var sourceFile = this.host.getSourceFile(fileName + '.ts', ts__default.ScriptTarget.Latest);
         return sourceFile && this.collector.getMetadata(sourceFile);
     };
@@ -43687,7 +43689,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.4-03d93c9
+ * @license Angular v6.0.0-beta.4-029dbf0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45431,7 +45433,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.4-03d93c9');
+var VERSION$2 = new Version$1('6.0.0-beta.4-029dbf0');
 
 /**
  * @fileoverview added by tsickle
@@ -59712,7 +59714,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.4-03d93c9
+ * @license Angular v6.0.0-beta.4-029dbf0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62361,7 +62363,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.4-03d93c9');
+var VERSION = new Version$1('6.0.0-beta.4-029dbf0');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
