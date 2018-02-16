9c28fa99e57202b08f9087cd425ca9758cf4d2f2
Victor Berchet <victor@suumit.com>
2018-02-16 23:42:47 +0000

    dfa2fb9 test(ivy): clean the imported renderer2 (#22255)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-2639b4b
+ * @license Angular v6.0.0-beta.4-dfa2fb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-2639b4b
+ * @license Angular v6.0.0-beta.4-dfa2fb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.4-2639b4b');
+var VERSION$1 = new Version('6.0.0-beta.4-dfa2fb9');
 
 /**
  * @fileoverview added by tsickle
@@ -43688,7 +43688,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.4-2639b4b
+ * @license Angular v6.0.0-beta.4-dfa2fb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45432,7 +45432,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.4-2639b4b');
+var VERSION$2 = new Version$1('6.0.0-beta.4-dfa2fb9');
 
 /**
  * @fileoverview added by tsickle
@@ -59753,7 +59753,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.4-2639b4b
+ * @license Angular v6.0.0-beta.4-dfa2fb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62402,7 +62402,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.4-2639b4b');
+var VERSION = new Version$1('6.0.0-beta.4-dfa2fb9');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
