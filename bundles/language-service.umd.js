a6bb3aa4beb23060eed124fd0b0acc82f888340d
Victor Berchet <victor@suumit.com>
2018-02-18 23:36:55 +0000

    28b23f9 docs(aio): add angular-buch to resources (#22163)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-c30d329
+ * @license Angular v6.0.0-beta.4-28b23f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-c30d329
+ * @license Angular v6.0.0-beta.4-28b23f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.4-c30d329');
+var VERSION$1 = new Version('6.0.0-beta.4-28b23f9');
 
 /**
  * @fileoverview added by tsickle
@@ -43688,7 +43688,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.4-c30d329
+ * @license Angular v6.0.0-beta.4-28b23f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45432,7 +45432,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.4-c30d329');
+var VERSION$2 = new Version$1('6.0.0-beta.4-28b23f9');
 
 /**
  * @fileoverview added by tsickle
@@ -59802,7 +59802,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.4-c30d329
+ * @license Angular v6.0.0-beta.4-28b23f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62451,7 +62451,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.4-c30d329');
+var VERSION = new Version$1('6.0.0-beta.4-28b23f9');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
