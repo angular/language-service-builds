54b156e9b855c0bc83680579d84cea3bb978cc37
Victor Berchet <victor@suumit.com>
2018-02-18 21:28:06 +0000

    f4845fa build: use authenticated mode for the merge script (#22269)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.4-f693be3');
+var VERSION$1 = new Version('6.0.0-beta.4-f4845fa');
 
 /**
  * @fileoverview added by tsickle
@@ -43688,7 +43688,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45432,7 +45432,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.4-f693be3');
+var VERSION$2 = new Version$1('6.0.0-beta.4-f4845fa');
 
 /**
  * @fileoverview added by tsickle
@@ -59765,7 +59765,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62414,7 +62414,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.4-f693be3');
+var VERSION = new Version$1('6.0.0-beta.4-f4845fa');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
