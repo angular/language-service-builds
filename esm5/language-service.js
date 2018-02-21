0b0b6794abac94d000a38e85d2afe6ba949d2aee
Victor Berchet <victor@suumit.com>
2018-02-21 00:03:41 +0000

    d20a08b docs(aio): update installed mobile tool list (#22331) PR Close #22331

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5bdb3ac
+ * @license Angular v5.2.5-d20a08b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.5-5bdb3ac');
+var VERSION = new Version('5.2.5-d20a08b');
 
 /**
  * @license
