f4964467539d86e6c4dea27372c4be5ee41c97eb
Victor Berchet <victor@suumit.com>
2018-02-21 00:02:45 +0000

    5bdb3ac build: update tsickle dep from compiler-cli (#22295) PR Close #22295

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.5-a346d28');
+var VERSION$1 = new Version('5.2.5-5bdb3ac');
 
 /**
  * @fileoverview added by tsickle
@@ -42106,7 +42106,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42537,7 +42537,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.5-a346d28');
+var VERSION$2 = new Version$1('5.2.5-5bdb3ac');
 
 /**
  * @fileoverview added by tsickle
@@ -56779,7 +56779,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59428,7 +59428,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.5-a346d28');
+var VERSION = new Version$1('5.2.5-5bdb3ac');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.5-a346d28');
+var VERSION = new Version('5.2.5-5bdb3ac');
 
 /**
  * @license
