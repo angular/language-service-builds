98085e3852a0a3f6e5e50224bc48e52f0c02a6c1
Victor Berchet <victor@suumit.com>
2018-02-21 00:32:21 +0000

    3b692a5 docs(aio): fix incorrect quote mark usage (#22335) PR Close #22335

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.4-69a0578');
+var VERSION$1 = new Version('6.0.0-beta.4-3b692a5');
 
 /**
  * @fileoverview added by tsickle
@@ -44211,7 +44211,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45955,7 +45955,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.4-69a0578');
+var VERSION$2 = new Version$1('6.0.0-beta.4-3b692a5');
 
 /**
  * @fileoverview added by tsickle
@@ -60326,7 +60326,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.4-69a0578
+ * @license Angular v6.0.0-beta.4-3b692a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62975,7 +62975,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.4-69a0578');
+var VERSION = new Version$1('6.0.0-beta.4-3b692a5');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
