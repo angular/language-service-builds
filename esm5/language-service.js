b8dfec70c96f2fc6dcc0077365054b6149beef85
Victor Berchet <victor@suumit.com>
2018-02-21 00:41:53 +0000

    ee53577 docs: add ngStyle to cheat sheet (#22070)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-94756eb
+ * @license Angular v5.2.5-ee53577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.5-94756eb');
+var VERSION = new Version('5.2.5-ee53577');
 
 /**
  * @license
