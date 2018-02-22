aabe4552d4e9fd4ee1eb8e6380ff511d6c3c0aa1
Victor Berchet <victor@suumit.com>
2018-02-22 22:11:54 +0000

    b3ffeaa fix(ivy): OnDestroy hook should not be called twice for a directive on an element (#22350)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.5-f194d00');
+var VERSION$1 = new Version('6.0.0-beta.5-b3ffeaa');
 
 /**
  * @fileoverview added by tsickle
@@ -44211,7 +44211,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45959,7 +45959,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.5-f194d00');
+var VERSION$2 = new Version$1('6.0.0-beta.5-b3ffeaa');
 
 /**
  * @fileoverview added by tsickle
@@ -60330,7 +60330,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62979,7 +62979,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.5-f194d00');
+var VERSION = new Version$1('6.0.0-beta.5-b3ffeaa');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
