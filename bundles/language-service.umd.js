9bc929ee0f1a21778c5b76a6417a8ecf01034acd
Victor Berchet <victor@suumit.com>
2018-02-22 18:50:54 +0000

    90e9c59 Revert "feat(core): support metadata reflection for native class types (#22356)"

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-ca06af4
+ * @license Angular v6.0.0-beta.5-90e9c59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-ca06af4
+ * @license Angular v6.0.0-beta.5-90e9c59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.5-ca06af4');
+var VERSION$1 = new Version('6.0.0-beta.5-90e9c59');
 
 /**
  * @fileoverview added by tsickle
@@ -44211,7 +44211,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.5-ca06af4
+ * @license Angular v6.0.0-beta.5-90e9c59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44494,11 +44494,9 @@ function makeParamDecorator(name, props, parentClass) {
  * found in the LICENSE file at https://angular.io/license
  */
 /**
- * Attention: These regex has to hold even if the code is minified!
+ * Attention: This regex has to hold even if the code is minified!
  */
 var DELEGATE_CTOR = /^function\s+\S+\(\)\s*{[\s\S]+\.apply\(this,\s*arguments\)/;
-var INHERITED_CLASS = /^class\s+[A-Za-z\d$_]*\s*extends\s+[A-Za-z\d$_]+\s*{/;
-var INHERITED_CLASS_WITH_CTOR = /^class\s+[A-Za-z\d$_]*\s*extends\s+[A-Za-z\d$_]+\s*{[\s\S]*constructor\s*\(/;
 var ReflectionCapabilities = /** @class */ (function () {
     function ReflectionCapabilities(reflect) {
         this._reflect = reflect || _global['Reflect'];
@@ -44578,7 +44576,6 @@ var ReflectionCapabilities = /** @class */ (function () {
      * @return {?}
      */
     function (type, parentCtor) {
-        var /** @type {?} */ typeStr = type.toString();
         // If we have no decorators, we only have function.length as metadata.
         // In that case, to detect whether a child class declared an own constructor or not,
         // we need to look inside of that constructor to check whether it is
@@ -44586,8 +44583,7 @@ var ReflectionCapabilities = /** @class */ (function () {
         // This also helps to work around for https://github.com/Microsoft/TypeScript/issues/12439
         // that sets 'design:paramtypes' to []
         // if a class inherits from another class but has no ctor declared itself.
-        if (DELEGATE_CTOR.exec(typeStr) ||
-            (INHERITED_CLASS.exec(typeStr) && !INHERITED_CLASS_WITH_CTOR.exec(typeStr))) {
+        if (DELEGATE_CTOR.exec(type.toString())) {
             return null;
         }
         // Prefer the direct API.
@@ -45959,7 +45955,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.5-ca06af4');
+var VERSION$2 = new Version$1('6.0.0-beta.5-90e9c59');
 
 /**
  * @fileoverview added by tsickle
@@ -60330,7 +60326,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.5-ca06af4
+ * @license Angular v6.0.0-beta.5-90e9c59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62979,7 +62975,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.5-ca06af4');
+var VERSION = new Version$1('6.0.0-beta.5-90e9c59');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
