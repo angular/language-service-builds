fe54ea919b0885ea0e8fa1c6bbd2fd4ef1e3ffe7
Victor Berchet <victor@suumit.com>
2018-02-22 19:41:29 +0000

    da1b4d5 docs: fix ngmodules-jsmodules pre-req (#22316)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.6-aa100f6');
+var VERSION = new Version('5.2.6-da1b4d5');
 
 /**
  * @license
