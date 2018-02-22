7b17b3945ede16bf7440f6e44683ad4ff1172791
Victor Berchet <victor@suumit.com>
2018-02-22 00:21:21 +0000

    3e6a86f fix(forms): set state before emitting a value from ngModelChange (#21514)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-a7ebf5a
+ * @license Angular v6.0.0-beta.4-3e6a86f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.4-a7ebf5a');
+var VERSION = new Version('6.0.0-beta.4-3e6a86f');
 
 /**
  * @license
