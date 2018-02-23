f5525e1f796ad18bdc915f0d401abebc9bb261bb
Victor Berchet <victor@suumit.com>
2018-02-23 19:39:43 +0000

    cf91906 docs(aio): Essential JS 2 url updated (#19739) PR Close #19739

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-2b71889
+ * @license Angular v5.2.6-cf91906
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.6-2b71889');
+var VERSION = new Version('5.2.6-cf91906');
 
 /**
  * @license
