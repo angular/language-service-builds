02a0461fbbb3e570e326d8c8a80ddc0070519f97
Victor Berchet <victor@suumit.com>
2018-02-23 23:53:11 +0000

    4180912 feat(common): export functions to format numbers, percents, currencies & dates (#22423)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-094666d
+ * @license Angular v6.0.0-beta.5-4180912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-094666d
+ * @license Angular v6.0.0-beta.5-4180912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.5-094666d');
+var VERSION$1 = new Version('6.0.0-beta.5-4180912');
 
 /**
  * @fileoverview added by tsickle
@@ -44211,7 +44211,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.5-094666d
+ * @license Angular v6.0.0-beta.5-4180912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45959,7 +45959,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.5-094666d');
+var VERSION$2 = new Version$1('6.0.0-beta.5-4180912');
 
 /**
  * @fileoverview added by tsickle
@@ -60330,7 +60330,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.5-094666d
+ * @license Angular v6.0.0-beta.5-4180912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -62979,7 +62979,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.5-094666d');
+var VERSION = new Version$1('6.0.0-beta.5-4180912');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
