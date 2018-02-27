65a367c77408cc1e4140447940abd6a159e06c98
Alex Eagle <alexeagle@google.com>
2018-02-27 19:47:17 +0000

    f2fa7a2 docs(aio): add Observable and Rx docs (#21423)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('5.2.6-e1fbe20');
+var VERSION$1 = new Version('5.2.6-f2fa7a2');
 
 /**
  * @fileoverview added by tsickle
@@ -42106,7 +42106,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -42537,7 +42537,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('5.2.6-e1fbe20');
+var VERSION$2 = new Version$1('5.2.6-f2fa7a2');
 
 /**
  * @fileoverview added by tsickle
@@ -56783,7 +56783,7 @@ function initViewStaticData(viewIndex, parent) {
 var NO_CHANGE = /** @type {?} */ ({});
 
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59432,7 +59432,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('5.2.6-e1fbe20');
+var VERSION = new Version$1('5.2.6-f2fa7a2');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
