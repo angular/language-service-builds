43d8acf7c90ad36fab12103f80d645030862bf80
Alex Eagle <alexeagle@google.com>
2018-03-01 17:21:53 +0000

    ab790f3 build: Add support for bazelOptions.maxCacheSizeMb in ngc-wrapped. (#22511)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-dd53447
+ * @license Angular v6.0.0-beta.6-ab790f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.6-dd53447
+ * @license Angular v6.0.0-beta.6-ab790f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.6-dd53447');
+var VERSION$1 = new Version('6.0.0-beta.6-ab790f3');
 
 /**
  * @fileoverview added by tsickle
@@ -44212,7 +44212,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.6-dd53447
+ * @license Angular v6.0.0-beta.6-ab790f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -45960,7 +45960,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.6-dd53447');
+var VERSION$2 = new Version$1('6.0.0-beta.6-ab790f3');
 
 /**
  * @fileoverview added by tsickle
@@ -60415,7 +60415,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.6-dd53447
+ * @license Angular v6.0.0-beta.6-ab790f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -63064,7 +63064,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.6-dd53447');
+var VERSION = new Version$1('6.0.0-beta.6-ab790f3');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
