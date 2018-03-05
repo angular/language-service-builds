077e8554f7f82e280727d77230b1df94ad2eb490
Alex Eagle <alexeagle@google.com>
2018-03-05 18:32:46 +0000

    51ca643 test(ivy): add injectAttribute spec (#22510)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4f60968
+ * @license Angular v6.0.0-beta.6-51ca643
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.6-4f60968');
+const VERSION = new Version('6.0.0-beta.6-51ca643');
 
 /**
  * @license
