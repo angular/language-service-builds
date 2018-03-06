9ea074141094a133ec1e73896cbb293cfe648807
Alex Eagle <alexeagle@google.com>
2018-03-06 05:47:55 +0000

    f86d8ae docs(compiler): fix a line about ivy library (#22579) PR Close #22579

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.6-94bc277');
+var VERSION$1 = new Version('6.0.0-beta.6-f86d8ae');
 
 /**
  * @fileoverview added by tsickle
@@ -44649,7 +44649,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46397,7 +46397,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.6-94bc277');
+var VERSION$2 = new Version$1('6.0.0-beta.6-f86d8ae');
 
 /**
  * @fileoverview added by tsickle
@@ -60852,7 +60852,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -63501,7 +63501,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.6-94bc277');
+var VERSION = new Version$1('6.0.0-beta.6-f86d8ae');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
