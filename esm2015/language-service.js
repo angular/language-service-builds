ea6f4cc07bb72d6f955c9f3ecd4bf0ef37b7baed
Alex Eagle <alexeagle@google.com>
2018-03-06 23:27:03 +0000

    5876fb0 docs(aio): update deprecated Http reference to HttpClientModule, remove Http reference because another context is used (#21984)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5b7b208
+ * @license Angular v5.2.7-5876fb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.7-5b7b208');
+const VERSION = new Version('5.2.7-5876fb0');
 
 /**
  * @license
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5b7b208
+ * @license Angular v5.2.7-5876fb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.7-5b7b208');
+var VERSION = new Version('5.2.7-5876fb0');
 
 /**
  * @license
