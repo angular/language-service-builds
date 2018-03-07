acd917558adc5472b444ee05c87b3d378d0dbf19
Alex Eagle <alexeagle@google.com>
2018-03-07 22:56:29 +0000

    7816791 build(common): export locale data as commonjs instead of es2015 (#20624)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4f744cc
+ * @license Angular v6.0.0-beta.6-7816791
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.6-4f744cc');
+var VERSION = new Version('6.0.0-beta.6-7816791');
 
 /**
  * @license
