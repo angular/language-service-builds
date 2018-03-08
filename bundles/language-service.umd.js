cdfe46239dc1f91d00b202976b05defa8212faaf
Kara Erickson <karakara@google.com>
2018-03-08 20:31:47 +0000

    2fee5cc test(ivy): add injection canonical specs (#22595)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f13f4db
+ * @license Angular v6.0.0-beta.7-2fee5cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-f13f4db
+ * @license Angular v6.0.0-beta.7-2fee5cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-f13f4db');
+var VERSION$1 = new Version('6.0.0-beta.7-2fee5cc');
 
 /**
  * @fileoverview added by tsickle
@@ -44743,7 +44743,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-f13f4db
+ * @license Angular v6.0.0-beta.7-2fee5cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46491,7 +46491,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-f13f4db');
+var VERSION$2 = new Version$1('6.0.0-beta.7-2fee5cc');
 
 /**
  * @fileoverview added by tsickle
@@ -61118,7 +61118,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-f13f4db
+ * @license Angular v6.0.0-beta.7-2fee5cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -63767,7 +63767,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-f13f4db');
+var VERSION = new Version$1('6.0.0-beta.7-2fee5cc');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
