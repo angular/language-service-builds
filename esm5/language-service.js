49c4a7706fc52784f930292cd4444c163d6e670a
Kara Erickson <karakara@google.com>
2018-03-08 02:43:06 +0000

    3112320 docs: add HeroService to code tabs and fix headers (#22373)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.7-4aeb04d');
+var VERSION = new Version('5.2.8-3112320');
 
 /**
  * @license
