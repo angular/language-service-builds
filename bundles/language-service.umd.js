2efdbd366e6476265e36f2ec25707da803a19d81
Kara Erickson <karakara@google.com>
2018-03-08 05:20:39 +0000

    f64ee15 feat(ivy): implement pipes (#22254)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-5d4fa7f
+ * @license Angular v6.0.0-beta.7-f64ee15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-5d4fa7f
+ * @license Angular v6.0.0-beta.7-f64ee15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-5d4fa7f');
+var VERSION$1 = new Version('6.0.0-beta.7-f64ee15');
 
 /**
  * @fileoverview added by tsickle
@@ -44649,7 +44649,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-5d4fa7f
+ * @license Angular v6.0.0-beta.7-f64ee15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46397,7 +46397,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-5d4fa7f');
+var VERSION$2 = new Version$1('6.0.0-beta.7-f64ee15');
 
 /**
  * @fileoverview added by tsickle
@@ -59705,7 +59705,7 @@ var currentQueries;
  */
 var creationMode;
 /**
- * An array of nodes (text, element, container, etc), their bindings, and
+ * An array of nodes (text, element, container, etc), pipes, their bindings, and
  * any local variables that need to be stored between invocations.
  */
 var data;
@@ -60954,7 +60954,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-5d4fa7f
+ * @license Angular v6.0.0-beta.7-f64ee15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -63603,7 +63603,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-5d4fa7f');
+var VERSION = new Version$1('6.0.0-beta.7-f64ee15');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
