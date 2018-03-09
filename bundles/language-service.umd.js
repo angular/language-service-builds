c432d7036b6a9a66a9e9c3c4c94cd6867ea6f3f9
Kara Erickson <karakara@google.com>
2018-03-09 21:30:57 +0000

    0d8deb0 fix(compiler-cli): generate proper exports.* identifiers in cjs output (#22564)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e8326e6
+ * @license Angular v6.0.0-beta.7-0d8deb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-e8326e6
+ * @license Angular v6.0.0-beta.7-0d8deb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-e8326e6');
+var VERSION$1 = new Version('6.0.0-beta.7-0d8deb0');
 
 /**
  * @fileoverview added by tsickle
@@ -44798,7 +44798,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-e8326e6
+ * @license Angular v6.0.0-beta.7-0d8deb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46546,7 +46546,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-e8326e6');
+var VERSION$2 = new Version$1('6.0.0-beta.7-0d8deb0');
 
 /**
  * @fileoverview added by tsickle
@@ -61205,7 +61205,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-e8326e6
+ * @license Angular v6.0.0-beta.7-0d8deb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -63854,7 +63854,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-e8326e6');
+var VERSION = new Version$1('6.0.0-beta.7-0d8deb0');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
