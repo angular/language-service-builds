58a2aef2c1c2cbedabb7ab32d17668b237cfdab8
Kara Erickson <karakara@google.com>
2018-03-12 05:40:50 +0000

    40315be fix(compiler-cli): enableResourceInlining handles both styles and styleUrls (#22688)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-fa451bc');
+var VERSION$1 = new Version('6.0.0-beta.7-40315be');
 
 /**
  * @fileoverview added by tsickle
@@ -44798,7 +44798,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46546,7 +46546,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-fa451bc');
+var VERSION$2 = new Version$1('6.0.0-beta.7-40315be');
 
 /**
  * @fileoverview added by tsickle
@@ -61817,7 +61817,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -64466,7 +64466,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-fa451bc');
+var VERSION = new Version$1('6.0.0-beta.7-40315be');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
