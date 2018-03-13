058e7368403b05c2e67acc02fdec60fa3c8620b6
Kara Erickson <karakara@google.com>
2018-03-13 19:35:24 +0000

    34e355a build(bazel): ng_package rxjs/operators rollup config (#22744)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-58b94e6');
+var VERSION$1 = new Version('6.0.0-beta.7-34e355a');
 
 /**
  * @fileoverview added by tsickle
@@ -44538,7 +44538,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46295,7 +46295,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-58b94e6');
+var VERSION$2 = new Version$1('6.0.0-beta.7-34e355a');
 
 /**
  * @fileoverview added by tsickle
@@ -61646,7 +61646,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -64307,7 +64307,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-58b94e6');
+var VERSION = new Version$1('6.0.0-beta.7-34e355a');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
