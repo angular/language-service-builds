cfaaa976e81b18766e550028740e5f1a869fc6c1
Kara Erickson <karakara@google.com>
2018-03-14 20:36:38 +0000

    112431d test(ivy): add canonical compiler spec for class/style (#22719)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f5a98f4
+ * @license Angular v6.0.0-beta.7-112431d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-f5a98f4
+ * @license Angular v6.0.0-beta.7-112431d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-f5a98f4');
+var VERSION$1 = new Version('6.0.0-beta.7-112431d');
 
 /**
  * @fileoverview added by tsickle
@@ -30169,8 +30169,8 @@ var Identifiers$1 = /** @class */ (function () {
     Identifiers.elementEnd = { name: 'ɵe', moduleName: CORE$1 };
     Identifiers.elementProperty = { name: 'ɵp', moduleName: CORE$1 };
     Identifiers.elementAttribute = { name: 'ɵa', moduleName: CORE$1 };
-    Identifiers.elementClass = { name: 'ɵk', moduleName: CORE$1 };
-    Identifiers.elementStyle = { name: 'ɵs', moduleName: CORE$1 };
+    Identifiers.elementClassNamed = { name: 'ɵkn', moduleName: CORE$1 };
+    Identifiers.elementStyleNamed = { name: 'ɵsn', moduleName: CORE$1 };
     Identifiers.containerCreate = { name: 'ɵC', moduleName: CORE$1 };
     Identifiers.containerEnd = { name: 'ɵc', moduleName: CORE$1 };
     Identifiers.containerRefreshStart = { name: 'ɵcR', moduleName: CORE$1 };
@@ -30401,8 +30401,8 @@ function unsupported(feature) {
 var BINDING_INSTRUCTION_MAP = (_a = {},
     _a[PropertyBindingType.Property] = Identifiers$1.elementProperty,
     _a[PropertyBindingType.Attribute] = Identifiers$1.elementAttribute,
-    _a[PropertyBindingType.Class] = Identifiers$1.elementClass,
-    _a[PropertyBindingType.Style] = Identifiers$1.elementStyle,
+    _a[PropertyBindingType.Class] = Identifiers$1.elementClassNamed,
+    _a[PropertyBindingType.Style] = Identifiers$1.elementStyleNamed,
     _a);
 /**
  * @param {?} args
@@ -44538,7 +44538,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-f5a98f4
+ * @license Angular v6.0.0-beta.7-112431d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46295,7 +46295,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-f5a98f4');
+var VERSION$2 = new Version$1('6.0.0-beta.7-112431d');
 
 /**
  * @fileoverview added by tsickle
@@ -61871,7 +61871,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-f5a98f4
+ * @license Angular v6.0.0-beta.7-112431d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -64532,7 +64532,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-f5a98f4');
+var VERSION = new Version$1('6.0.0-beta.7-112431d');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
