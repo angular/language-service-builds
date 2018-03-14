81d1a8ae7b30474ebfa22f02a2fb6038e00294df
Kara Erickson <karakara@google.com>
2018-03-14 18:13:45 +0000

    fe96caf fix(aio): constrain error logging to improve reporting (#22713)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-ad674da
+ * @license Angular v5.2.8-fe96caf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.8-ad674da');
+var VERSION = new Version('5.2.8-fe96caf');
 
 /**
  * @license
