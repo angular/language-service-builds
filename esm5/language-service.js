b763a13d8359f8fd41c6c294d5a549bdb4c54b8c
Kara Erickson <karakara@google.com>
2018-03-14 05:07:45 +0000

    4ac606b docs(compiler): fix spelling errors (#22704)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-51027d7
+ * @license Angular v6.0.0-beta.7-4ac606b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.7-51027d7');
+var VERSION = new Version('6.0.0-beta.7-4ac606b');
 
 /**
  * @license
