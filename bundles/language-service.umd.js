9c248c480b9fdda08d96077db9fc223e3702ebd6
Kara Erickson <karakara@google.com>
2018-03-14 21:46:11 +0000

    d509bd6 build(aio): improve the API Pipe pages (#22702)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-cd2ebd2
+ * @license Angular v6.0.0-beta.7-d509bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59,7 +59,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-cd2ebd2
+ * @license Angular v6.0.0-beta.7-d509bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-cd2ebd2');
+var VERSION$1 = new Version('6.0.0-beta.7-d509bd6');
 
 /**
  * @fileoverview added by tsickle
@@ -44538,7 +44538,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-cd2ebd2
+ * @license Angular v6.0.0-beta.7-d509bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46295,7 +46295,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-cd2ebd2');
+var VERSION$2 = new Version$1('6.0.0-beta.7-d509bd6');
 
 /**
  * @fileoverview added by tsickle
@@ -61871,7 +61871,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-cd2ebd2
+ * @license Angular v6.0.0-beta.7-d509bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -64532,7 +64532,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-cd2ebd2');
+var VERSION = new Version$1('6.0.0-beta.7-d509bd6');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-cd2ebd2
+ * @license Angular v6.0.0-beta.7-d509bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.7-cd2ebd2');
+var VERSION = new Version('6.0.0-beta.7-d509bd6');
 
 /**
  * @license
