1a008c5be80f2f2f06c4a4940dbcbc5a100cd2a5
Miško Hevery <misko@hevery.com>
2018-03-16 04:40:49 +0000

    b149424 build(bazel): make ng_package auto generate package.json for secondary entry-points (#22806)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f9247e4
+ * @license Angular v6.0.0-beta.7-b149424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-f9247e4');
+const VERSION = new Version('6.0.0-beta.7-b149424');
 
 /**
  * @license
