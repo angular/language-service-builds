4b44075819ba33f464de4c069439f704458f8637
Miško Hevery <misko@hevery.com>
2018-03-16 04:42:41 +0000

    269c3a1 build(compiler-cli): fix tsconfig.json circularity issue (#22722)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-b149424');
+var VERSION$1 = new Version('6.0.0-beta.7-269c3a1');
 
 /**
  * @fileoverview added by tsickle
@@ -44797,7 +44797,7 @@ function share() {
 var share_3 = share;
 
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -46557,7 +46557,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-b149424');
+var VERSION$2 = new Version$1('6.0.0-beta.7-269c3a1');
 
 /**
  * @fileoverview added by tsickle
@@ -62198,7 +62198,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -64859,7 +64859,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-b149424');
+var VERSION = new Version$1('6.0.0-beta.7-269c3a1');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
