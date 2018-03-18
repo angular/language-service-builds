4be8e93315a884dd86454871bb692d2a803b95e9
Igor Minar <igor@angular.io>
2018-03-18 16:54:56 +0000

    ce63dc6 feat: update the package output of build.sh to APF v6 (#22808)

---
+++ esm5/src/types.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { BuiltinType } from '@angular/compiler-cli/src/language_services';
+export { BuiltinType };
+/**
+ * The kind of diagnostic message.
+ *
+ * @experimental
+ */
+export var DiagnosticKind;
+(function (DiagnosticKind) {
+    DiagnosticKind[DiagnosticKind["Error"] = 0] = "Error";
+    DiagnosticKind[DiagnosticKind["Warning"] = 1] = "Warning";
+})(DiagnosticKind || (DiagnosticKind = {}));
+//# sourceMappingURL=types.js.map
