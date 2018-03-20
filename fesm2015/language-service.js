6b3b0133fc80f10022ef715c9fb36384b48e2064
Igor Minar <igor@angular.io>
2018-03-20 20:52:01 +0000

    67f570c ci: update ngbot config for g3 (#22882)

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4648597
+ * @license Angular v6.0.0-beta.7-67f570c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-4648597');
+const VERSION = new Version('6.0.0-beta.7-67f570c');
 
 /**
  * @license
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4648597
+ * @license Angular v6.0.0-beta.7-67f570c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.7-4648597');
+var VERSION = new Version('6.0.0-beta.7-67f570c');
 
 /**
  * @license
