1287d97bcafbd63e81199732345533e981a825c2
Igor Minar <igor@angular.io>
2018-03-20 23:16:28 +0000

    17fb983 fix(ivy): fix type error in newer version of TS (#22897)

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-3cc5c2e
+ * @license Angular v6.0.0-beta.7-17fb983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-3cc5c2e');
+const VERSION = new Version('6.0.0-beta.7-17fb983');
 
 /**
  * @license
