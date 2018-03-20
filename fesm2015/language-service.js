61fa6405254f16503386ffde3c6b8cfb49766f80
Igor Minar <igor@angular.io>
2018-03-20 20:57:06 +0000

    7d095b9 fix: correct several esm2015 entry-points in package.jsons (#22892)

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-689f351
+ * @license Angular v6.0.0-beta.7-7d095b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-689f351');
+const VERSION = new Version('6.0.0-beta.7-7d095b9');
 
 /**
  * @license
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-689f351
+ * @license Angular v6.0.0-beta.7-7d095b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.7-689f351');
+var VERSION = new Version('6.0.0-beta.7-7d095b9');
 
 /**
  * @license
