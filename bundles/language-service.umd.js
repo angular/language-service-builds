4f8bbea9e9da8332d20034019aaf976f69f78d8d
Igor Minar <igor@angular.io>
2018-03-21 01:03:02 +0000

    623d769 fix(router): make locationSyncBootstrapListener public due to change in output after TS 2.7 update in #22669 (#22896)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-17fb983
+ * @license Angular v6.0.0-beta.7-623d769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-beta.7-17fb983
+ * @license Angular v6.0.0-beta.7-623d769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-beta.7-17fb983');
+var VERSION$1 = new Version('6.0.0-beta.7-623d769');
 
 /**
  * @fileoverview added by tsickle
@@ -58605,7 +58605,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_68 = index$4.share;
 
 /**
- * @license Angular v6.0.0-beta.7-17fb983
+ * @license Angular v6.0.0-beta.7-623d769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60508,7 +60508,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-beta.7-17fb983');
+var VERSION$2 = new Version$1('6.0.0-beta.7-623d769');
 
 /**
  * @fileoverview added by tsickle
@@ -76209,7 +76209,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-beta.7-17fb983
+ * @license Angular v6.0.0-beta.7-623d769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -78870,7 +78870,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-beta.7-17fb983');
+var VERSION = new Version$1('6.0.0-beta.7-623d769');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-17fb983
+ * @license Angular v6.0.0-beta.7-623d769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-beta.7-17fb983');
+var VERSION = new Version('6.0.0-beta.7-623d769');
 
 /**
  * @license
