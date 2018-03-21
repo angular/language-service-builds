17c19993a2bceaf86ad83238dad437a60c14afaf
Igor Minar <igor@angular.io>
2018-03-21 20:36:14 +0000

    8c2a578 fix(service-worker): fix LruList bugs (#22769)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-4f2c51f
+ * @license Angular v6.0.0-rc.0-8c2a578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.0-4f2c51f
+ * @license Angular v6.0.0-rc.0-8c2a578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-rc.0-4f2c51f');
+var VERSION$1 = new Version('6.0.0-rc.0-8c2a578');
 
 /**
  * @fileoverview added by tsickle
@@ -58605,7 +58605,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_68 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.0-4f2c51f
+ * @license Angular v6.0.0-rc.0-8c2a578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60508,7 +60508,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-rc.0-4f2c51f');
+var VERSION$2 = new Version$1('6.0.0-rc.0-8c2a578');
 
 /**
  * @fileoverview added by tsickle
@@ -76209,7 +76209,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.0-4f2c51f
+ * @license Angular v6.0.0-rc.0-8c2a578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -78870,7 +78870,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-rc.0-4f2c51f');
+var VERSION = new Version$1('6.0.0-rc.0-8c2a578');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
