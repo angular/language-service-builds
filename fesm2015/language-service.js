0954678428b26846a23c6cb42ae67a812a040d2e
Igor Minar <igor@angular.io>
2018-03-21 17:17:36 +0000

    4f2c51f docs: switch over to the preview url for the rxjs upgrade guide in changelog

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-7cd8e8d
+ * @license Angular v6.0.0-rc.0-4f2c51f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-rc.0-7cd8e8d');
+const VERSION = new Version('6.0.0-rc.0-4f2c51f');
 
 /**
  * @license
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-7cd8e8d
+ * @license Angular v6.0.0-rc.0-4f2c51f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.0-7cd8e8d');
+var VERSION = new Version('6.0.0-rc.0-4f2c51f');
 
 /**
  * @license
