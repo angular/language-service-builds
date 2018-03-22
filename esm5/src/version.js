1d0f7a7e11a504eb6d9e5bd10f5056133cb884a9
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 19:49:44 +0000

    32105c8 test: temporarily disable ng_package test (#22933)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-f461f43');
+export var VERSION = new Version('6.0.0-rc.0-32105c8');
 //# sourceMappingURL=version.js.map
