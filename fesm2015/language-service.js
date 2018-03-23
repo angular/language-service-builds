b189ad555648cb1183844ef7ab21be4f32978426
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:32:11 +0000

    16f021c docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-3a30f5d
+ * @license Angular v6.0.0-rc.0-16f021c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-rc.0-3a30f5d');
+const VERSION = new Version('6.0.0-rc.0-16f021c');
 
 /**
  * @license
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-3a30f5d
+ * @license Angular v6.0.0-rc.0-16f021c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.0-3a30f5d');
+var VERSION = new Version('6.0.0-rc.0-16f021c');
 
 /**
  * @license
