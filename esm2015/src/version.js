5fa75d41684557efdce30823be78b68a3e9d3211
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:13:58 +0000

    7966744 fix(common): titlecase pipe (#22600)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-d9a0a8f');
+export const VERSION = new Version('6.0.0-rc.0-7966744');
 //# sourceMappingURL=version.js.map
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-d9a0a8f
+ * @license Angular v6.0.0-rc.0-7966744
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.0-d9a0a8f');
+var VERSION = new Version('6.0.0-rc.0-7966744');
 
 /**
  * @license
