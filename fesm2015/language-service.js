991549f1af1c23d1179aae4bf8fdcb5749fa6d04
Miško Hevery <misko@hevery.com>
2018-03-23 20:29:22 +0000

    4f0cae0 build: allow bazel build //... (#22168)

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-65f296a
+ * @license Angular v6.0.0-rc.0-4f0cae0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2511,7 +2511,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-rc.0-65f296a');
+const VERSION = new Version('6.0.0-rc.0-4f0cae0');
 
 /**
  * @license
