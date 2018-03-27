666952bd049acc849cfad3029dccff5a62021c17
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 02:56:24 +0000

    27e14b2 feat(bazel): prefix private-export (barred-latin-o) symbols (#23007)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-2388f24
+ * @license Angular v6.0.0-rc.0-27e14b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2668,7 +2668,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.0-2388f24');
+var VERSION = new Version('6.0.0-rc.0-27e14b2');
 
 /**
  * @license
