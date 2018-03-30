5e0e6e2dbd37f27f136ca8ac9772be8b151033f0
Igor Minar <igor@angular.io>
2018-03-30 03:59:01 +0000

    439030f fix(bazel): complete the rollup globals list for all angular entrypoints (#23080)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-481b22e
+ * @license Angular v6.0.0-rc.0-439030f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.0-481b22e');
+var VERSION = new Version('6.0.0-rc.0-439030f');
 
 /**
  * @license
