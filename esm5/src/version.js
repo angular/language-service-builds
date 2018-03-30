81ce637cb21f55eb00c3dd46599980d0cd262105
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 22:54:51 +0000

    0c4e371 build(bazel): Emit metadata.json under Blaze (#23049)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.1-6880766');
+export var VERSION = new Version('6.0.0-rc.1-0c4e371');
 //# sourceMappingURL=version.js.map
