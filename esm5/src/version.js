f67c9a859b80750abd1dc4aefda727f215edb5e5
Igor Minar <igor@angular.io>
2018-03-30 22:33:17 +0000

    6880766 build: update release check scripts

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.1-a30728c');
+export var VERSION = new Version('6.0.0-rc.1-6880766');
 //# sourceMappingURL=version.js.map
