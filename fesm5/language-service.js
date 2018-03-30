6e942b1fd93ca99f7221430d3182c9a1ac04badb
Igor Minar <igor@angular.io>
2018-03-30 21:26:43 +0000

    1527878 release(bazel): change publish-next script to publish bazel artifacts (#23097)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-22cb2c9
+ * @license Angular v6.0.0-rc.0-1527878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.0-22cb2c9');
+var VERSION = new Version('6.0.0-rc.0-1527878');
 
 /**
  * @license
