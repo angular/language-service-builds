a8e061a2fe60b66f6d5bd90049caaa046d5e23fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:35:51 +0000

    ff34d5e docs: add custom elements documentation (#22966)

--- fesm2015/language-service.js
+++ fesm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-85d3b59
+ * @license Angular v6.0.0-rc.1-ff34d5e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2456,7 +2456,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-rc.1-85d3b59');
+const VERSION = new Version('6.0.0-rc.1-ff34d5e');
 
 /**
  * @license
