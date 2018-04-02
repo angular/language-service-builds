05bceeec4f97e0807fa2dd139a5a49aa5a39d8ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 17:12:13 +0000

    85d3b59 refactor(ivy): generate pipe names instead of defs (#23104)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-7a1c437
+ * @license Angular v6.0.0-rc.1-85d3b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.1-7a1c437
+ * @license Angular v6.0.0-rc.1-85d3b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-rc.1-7a1c437');
+var VERSION$1 = new Version('6.0.0-rc.1-85d3b59');
 
 /**
  * @fileoverview added by tsickle
@@ -59120,7 +59120,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.1-7a1c437
+ * @license Angular v6.0.0-rc.1-85d3b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -61024,7 +61024,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-rc.1-7a1c437');
+var VERSION$2 = new Version$1('6.0.0-rc.1-85d3b59');
 
 /**
  * @fileoverview added by tsickle
@@ -76725,7 +76725,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.1-7a1c437
+ * @license Angular v6.0.0-rc.1-85d3b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79323,7 +79323,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-rc.1-7a1c437');
+var VERSION = new Version$1('6.0.0-rc.1-85d3b59');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
