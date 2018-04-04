8640de7c75c47d7d41a549992a4b46da31d8b46d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 21:55:56 +0000

    6dd8f6e docs(aio): remove invalid nav item (#23175)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-32a41bc
+ * @license Angular v6.0.0-rc.1-6dd8f6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.1-32a41bc
+ * @license Angular v6.0.0-rc.1-6dd8f6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-rc.1-32a41bc');
+var VERSION$1 = new Version('6.0.0-rc.1-6dd8f6e');
 
 /**
  * @fileoverview added by tsickle
@@ -58911,7 +58911,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.1-32a41bc
+ * @license Angular v6.0.0-rc.1-6dd8f6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60815,7 +60815,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-rc.1-32a41bc');
+var VERSION$2 = new Version$1('6.0.0-rc.1-6dd8f6e');
 
 /**
  * @fileoverview added by tsickle
@@ -76597,7 +76597,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.1-32a41bc
+ * @license Angular v6.0.0-rc.1-6dd8f6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79195,7 +79195,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-rc.1-32a41bc');
+var VERSION = new Version$1('6.0.0-rc.1-6dd8f6e');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-32a41bc
+ * @license Angular v6.0.0-rc.1-6dd8f6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-32a41bc');
+var VERSION = new Version('6.0.0-rc.1-6dd8f6e');
 
 /**
  * @license
