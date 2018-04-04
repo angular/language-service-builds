20f22c57f1227f0991bce745985c72a4afb16faa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:52:45 +0000

    f48e215 build(service-worker): properly build npm_package with Bazel (#23090)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-f99cb5c
+ * @license Angular v6.0.0-rc.1-f48e215
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-f99cb5c');
+var VERSION = new Version('6.0.0-rc.1-f48e215');
 
 /**
  * @license
