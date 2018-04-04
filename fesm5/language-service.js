b822dac5a89a4c6e9e8d0f111ade8f8d88e28b12
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:54:14 +0000

    23cc3ef fix(forms): improve error message for invalid value accessors (#22731)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-550433a
+ * @license Angular v6.0.0-rc.1-23cc3ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-550433a');
+var VERSION = new Version('6.0.0-rc.1-23cc3ef');
 
 /**
  * @license
