e1f2d4c863c829cf90ffc6bec4560eab0bb128cb
Igor Minar <igor@angular.io>
2018-04-05 17:46:36 +0000

    524e5d8 refactor(ivy): adjust types (#23167)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-79cecf9
+ * @license Angular v6.0.0-rc.1-524e5d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-79cecf9');
+var VERSION = new Version('6.0.0-rc.1-524e5d8');
 
 /**
  * @license
