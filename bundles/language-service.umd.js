622d1d6e337858b3359a5c3f789c69d39ef440c8
Igor Minar <igor@angular.io>
2018-04-05 22:09:15 +0000

    5cd36c7 build: improve the publish-next script (#23206)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-rc.2-f3d2623');
+var VERSION$1 = new Version('6.0.0-rc.2-5cd36c7');
 
 /**
  * @fileoverview added by tsickle
@@ -58911,7 +58911,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60817,7 +60817,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-rc.2-f3d2623');
+var VERSION$2 = new Version$1('6.0.0-rc.2-5cd36c7');
 
 /**
  * @fileoverview added by tsickle
@@ -76601,7 +76601,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79199,7 +79199,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-rc.2-f3d2623');
+var VERSION = new Version$1('6.0.0-rc.2-5cd36c7');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.2-f3d2623');
+var VERSION = new Version('6.0.0-rc.2-5cd36c7');
 
 /**
  * @license
