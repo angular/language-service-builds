e069a6af44d0ffaa456414bb15434e3a074f9e61
Igor Minar <igor@angular.io>
2018-04-05 18:00:44 +0000

    3b607e6 refactor(aio): remove unused images (#23018)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ce43e96
+ * @license Angular v6.0.0-rc.1-3b607e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-ce43e96');
+var VERSION = new Version('6.0.0-rc.1-3b607e6');
 
 /**
  * @license
