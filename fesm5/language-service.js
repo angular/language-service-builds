08231700301cefabbb36a058e4b2717f249d0936
Igor Minar <igor@angular.io>
2018-04-05 23:54:29 +0000

    eb9968a docs(elements): use Angular Elements naming in nav (#23213)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-b5f41f2
+ * @license Angular v6.0.0-rc.2-eb9968a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.2-b5f41f2');
+var VERSION = new Version('6.0.0-rc.2-eb9968a');
 
 /**
  * @license
