b7666c445be2c85d52f40ced52400b434f0f0683
Igor Minar <igor@angular.io>
2018-04-05 17:02:55 +0000

    9141424 docs(router): remove unnecessary `{@link Injector}` jsdoc tags (#23187)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-92821e3
+ * @license Angular v6.0.0-rc.1-9141424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-92821e3');
+var VERSION = new Version('6.0.0-rc.1-9141424');
 
 /**
  * @license
