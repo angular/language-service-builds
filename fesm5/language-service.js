a68b6d92adb0496f12e9c7bcefdd907a17e377be
Igor Minar <igor@angular.io>
2018-04-05 17:38:43 +0000

    79cecf9 fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ac316be
+ * @license Angular v6.0.0-rc.1-79cecf9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-ac316be');
+var VERSION = new Version('6.0.0-rc.1-79cecf9');
 
 /**
  * @license
