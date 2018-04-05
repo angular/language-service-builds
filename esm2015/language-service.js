bf22b199c899c4c9413f9dcb56513466bedff694
Igor Minar <igor@angular.io>
2018-04-05 23:51:16 +0000

    a1231be docs: fixed live example for the lifecycle hooks. (#23201)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-73172dd
+ * @license Angular v5.2.9-a1231be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.9-73172dd');
+const VERSION = new Version('5.2.9-a1231be');
 
 /**
  * @license
