fef46f3902f683f5065153e0dc5904cabc34a2b5
Igor Minar <igor@angular.io>
2018-04-05 17:46:55 +0000

    ce43e96 ci(aio): upload the preview before checking the bundle sizes (#23123)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-524e5d8
+ * @license Angular v6.0.0-rc.1-ce43e96
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.1-524e5d8');
+var VERSION = new Version('6.0.0-rc.1-ce43e96');
 
 /**
  * @license
