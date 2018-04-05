d103fbe991f5342610b304414a8d202dabd98b03
Igor Minar <igor@angular.io>
2018-04-05 16:51:27 +0000

    8ea15b4 docs(core): remove unnecessary `{@link Injector}` jsdoc tag (#23185)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-5db9ab1
+ * @license Angular v6.0.0-rc.1-8ea15b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.1-5db9ab1
+ * @license Angular v6.0.0-rc.1-8ea15b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-rc.1-5db9ab1');
+var VERSION$1 = new Version('6.0.0-rc.1-8ea15b4');
 
 /**
  * @fileoverview added by tsickle
@@ -58911,7 +58911,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.1-5db9ab1
+ * @license Angular v6.0.0-rc.1-8ea15b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -59432,9 +59432,9 @@ var Pipe$1 = makeDecorator('Pipe', function (p) { return (__assign({ pure: true 
  * found in the LICENSE file at https://angular.io/license
  */
 /**
- * \@whatItDoes Represents a type that a Component or other object is instances of.
- *
  * \@description
+ *
+ * Represents a type that a Component or other object is instances of.
  *
  * An example of a `Type` is `MyCustomComponent` class, which in JavaScript is be represented by
  * the `MyCustomComponent` constructor function.
@@ -60168,14 +60168,16 @@ var NullInjector = /** @class */ (function () {
     return NullInjector;
 }());
 /**
- * \@whatItDoes Injector interface
- * \@howToUse
+ * \@usageNotes
  * ```
  * const injector: Injector = ...;
  * injector.get(...);
  * ```
  *
  * \@description
+ *
+ * Concrete injectors implement this interface.
+ *
  * For more details, see the {\@linkDocs guide/dependency-injection "Dependency Injection Guide"}.
  *
  * ### Example
@@ -60799,7 +60801,7 @@ ViewEncapsulation$1[ViewEncapsulation$1.None] = "None";
  * found in the LICENSE file at https://angular.io/license
  */
 /**
- * \@whatItDoes Represents the version of Angular
+ * \@description Represents the version of Angular
  *
  * \@stable
  */
@@ -60815,7 +60817,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-rc.1-5db9ab1');
+var VERSION$2 = new Version$1('6.0.0-rc.1-8ea15b4');
 
 /**
  * @fileoverview added by tsickle
@@ -60883,9 +60885,9 @@ function defaultErrorLogger(console) {
  * found in the LICENSE file at https://angular.io/license
  */
 /**
- * \@whatItDoes Provides a hook for centralized exception handling.
  *
  * \@description
+ * Provides a hook for centralized exception handling.
  *
  * The default implementation of `ErrorHandler` prints error messages to the `console`. To
  * intercept error handling, write a custom exception handler that replaces this default as
@@ -76597,7 +76599,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.1-5db9ab1
+ * @license Angular v6.0.0-rc.1-8ea15b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79195,7 +79197,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-rc.1-5db9ab1');
+var VERSION = new Version$1('6.0.0-rc.1-8ea15b4');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.1-5db9ab1');
+export const VERSION = new Version('6.0.0-rc.1-8ea15b4');
 //# sourceMappingURL=version.js.map
