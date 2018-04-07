4a20c129ba99de1db05124fa4c33ff3363ecfbc4
Igor Minar <igor@angular.io>
2018-04-07 05:16:47 +0000

    1fac5f4 docs: recommend best practice for providers (#22934)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.3-33f8120');
+export const VERSION = new Version('6.0.0-rc.3-1fac5f4');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.3-33f8120');
+export var VERSION = new Version('6.0.0-rc.3-1fac5f4');
 //# sourceMappingURL=version.js.map
