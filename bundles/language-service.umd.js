d5f038adaa7cf0ab37c3f46cd8a757d8f35a1acb
Igor Minar <igor@angular.io>
2018-04-09 22:40:34 +0000

    3e8eef6 fix(bazel): set rollup output.name and amd.id (#23274)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-1fac5f4
+ * @license Angular v6.0.0-rc.3-3e8eef6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.3-1fac5f4
+ * @license Angular v6.0.0-rc.3-3e8eef6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -886,7 +886,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$1 = new Version('6.0.0-rc.3-1fac5f4');
+var VERSION$1 = new Version('6.0.0-rc.3-3e8eef6');
 
 /**
  * @fileoverview added by tsickle
@@ -58911,7 +58911,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.3-1fac5f4
+ * @license Angular v6.0.0-rc.3-3e8eef6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60817,7 +60817,7 @@ var Version$1 = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION$2 = new Version$1('6.0.0-rc.3-1fac5f4');
+var VERSION$2 = new Version$1('6.0.0-rc.3-3e8eef6');
 
 /**
  * @fileoverview added by tsickle
@@ -76619,7 +76619,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.3-1fac5f4
+ * @license Angular v6.0.0-rc.3-3e8eef6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79217,7 +79217,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version$1('6.0.0-rc.3-1fac5f4');
+var VERSION = new Version$1('6.0.0-rc.3-3e8eef6');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
