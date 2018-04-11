bbb4a0e7a36f77ce3f37dd59bb31ef0811753fed
Igor Minar <igor@angular.io>
2018-04-11 06:22:53 +0000

    b3a10e0 build: update Bazel dependency to 0.11.1 (#23297)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -899,7 +899,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION$1 = new Version('6.0.0-rc.3-0cb4f12');
+var VERSION$1 = new Version('6.0.0-rc.3-b3a10e0');
 
 /**
  * @fileoverview added by tsickle
@@ -59000,7 +59000,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60906,7 +60906,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.3-0cb4f12');
+var VERSION$2 = new Version$1('6.0.0-rc.3-b3a10e0');
 
 /**
  * @fileoverview added by tsickle
@@ -76676,7 +76676,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79274,7 +79274,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version$1('6.0.0-rc.3-0cb4f12');
+var VERSION = new Version$1('6.0.0-rc.3-b3a10e0');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
