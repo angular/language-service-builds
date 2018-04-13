755b43745d6966c3d0e7e73c05b5a7a1147b22fd
Igor Minar <igor@angular.io>
2018-04-13 07:26:38 +0000

    3cf5719 docs: lock version of in-memory API (#23242)

--- esm2015/language-service.js
+++ esm2015/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-47229fa
+ * @license Angular v5.2.9-3cf5719
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2503,7 +2503,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.9-47229fa');
+const VERSION = new Version('5.2.9-3cf5719');
 
 /**
  * @license
