8691a5e22353e91a4edc57ce613486b64033b634
Igor Minar <igor@angular.io>
2018-04-13 06:39:12 +0000

    993eeab docs: fix typo in injected variable name (#23315)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-b8f4e43
+ * @license Angular v6.0.0-rc.4-993eeab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-b8f4e43');
+var VERSION = new Version('6.0.0-rc.4-993eeab');
 
 /**
  * @license
