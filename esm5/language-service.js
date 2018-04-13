72a5203ad268bfca87da56a3b5daf2cfc8e861e6
Igor Minar <igor@angular.io>
2018-04-13 20:46:31 +0000

    21f3301 refactor: ensure all 'TODO's are consistent (#23252)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a3204f8
+ * @license Angular v5.2.9-21f3301
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.9-a3204f8');
+var VERSION = new Version('5.2.9-21f3301');
 
 /**
  * @license
