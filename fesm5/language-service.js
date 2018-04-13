123629022255c59763061e96455afb1691f8c9e3
Igor Minar <igor@angular.io>
2018-04-13 07:39:58 +0000

    2e270bb build: include tslib in umd bundles (#23354)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-a025f7e
+ * @license Angular v6.0.0-rc.4-2e270bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-a025f7e');
+var VERSION = new Version('6.0.0-rc.4-2e270bb');
 
 /**
  * @license
