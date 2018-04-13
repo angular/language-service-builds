86a0d0bbb0618efc93aa64ca3d86baa0969e8086
Igor Minar <igor@angular.io>
2018-04-13 23:52:19 +0000

    12f90ef build: move ngcontainer sources to angular/angular (#23374)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-7e581da
+ * @license Angular v6.0.0-rc.4-12f90ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-7e581da');
+var VERSION = new Version('6.0.0-rc.4-12f90ef');
 
 /**
  * @license
