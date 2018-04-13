8e4114b172e8a796bacadb29e4c49af8123395d7
Igor Minar <igor@angular.io>
2018-04-13 21:42:58 +0000

    490772e docs(aio): Update file tree to match Angular CLI V6 structure (#23367)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -899,7 +899,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION$1 = new Version('6.0.0-rc.4-e7ef027');
+var VERSION$1 = new Version('6.0.0-rc.4-490772e');
 
 /**
  * @fileoverview added by tsickle
@@ -58998,7 +58998,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60904,7 +60904,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.4-e7ef027');
+var VERSION$2 = new Version$1('6.0.0-rc.4-490772e');
 
 /**
  * @fileoverview added by tsickle
@@ -76739,7 +76739,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79337,7 +79337,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version$1('6.0.0-rc.4-e7ef027');
+var VERSION = new Version$1('6.0.0-rc.4-490772e');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
