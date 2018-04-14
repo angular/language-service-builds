a83cc70fe58f568056195e347a5c901f30636aea
Igor Minar <igor@angular.io>
2018-04-14 11:12:21 +0000

    815ae29 build: add '@angular/elements' the framework package group

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-8f690c9');
+export const VERSION = new Version('6.0.0-rc.5-815ae29');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-8f690c9');
+export var VERSION = new Version('6.0.0-rc.5-815ae29');
 //# sourceMappingURL=version.js.map
