496ba7fec8a034531c6e289ca3f77e4784c08c2d
Igor Minar <igor@angular.io>
2018-04-16 06:47:03 +0000

    7b23983 fix(service-worker): add badge to NOTIFICATION_OPTION_NAMES (#23241)

--- esm5/language-service.js
+++ esm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-21f3301
+ * @license Angular v5.2.9-7b23983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2656,7 +2656,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.9-21f3301');
+var VERSION = new Version('5.2.9-7b23983');
 
 /**
  * @license
