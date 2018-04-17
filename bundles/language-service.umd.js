7e0aeb92a0c01f4cb582bbf1afb26694a318ea6c
Jason Aden <jasonaden@google.com>
2018-04-17 21:41:58 +0000

    41b5149 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-06f8656
+ * @license Angular v6.0.0-rc.5-41b5149
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.5-06f8656
+ * @license Angular v6.0.0-rc.5-41b5149
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -906,7 +906,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION$1 = new Version('6.0.0-rc.5-06f8656');
+var VERSION$1 = new Version('6.0.0-rc.5-41b5149');
 
 /**
  * @fileoverview added by tsickle
@@ -59028,7 +59028,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.5-06f8656
+ * @license Angular v6.0.0-rc.5-41b5149
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60943,7 +60943,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5-06f8656');
+var VERSION$2 = new Version$1('6.0.0-rc.5-41b5149');
 
 /**
  * @fileoverview added by tsickle
@@ -76780,7 +76780,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.5-06f8656
+ * @license Angular v6.0.0-rc.5-41b5149
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79378,7 +79378,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version$1('6.0.0-rc.5-06f8656');
+var VERSION = new Version$1('6.0.0-rc.5-41b5149');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
