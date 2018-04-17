1e51642ce8e43a2d8e079d2c6b0bf37b50cc7a5e
Jason Aden <jasonaden@google.com>
2018-04-17 21:09:32 +0000

    99f8e10 ci(aio): fix `aio-monitoring` tests (#23390)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-010a4ef
+ * @license Angular v6.0.0-rc.5-99f8e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.5-010a4ef
+ * @license Angular v6.0.0-rc.5-99f8e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -906,7 +906,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION$1 = new Version('6.0.0-rc.5-010a4ef');
+var VERSION$1 = new Version('6.0.0-rc.5-99f8e10');
 
 /**
  * @fileoverview added by tsickle
@@ -31458,7 +31458,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
             var /** @type {?} */ instruction = BINDING_INSTRUCTION_MAP[input.type];
             if (instruction) {
                 // TODO(chuckj): runtime: security context?
-                this.instruction(this._bindingMode, input.sourceSpan, instruction, literal(elementIndex), literal(input.name), convertedBinding);
+                this.instruction(this._bindingMode, input.sourceSpan, instruction, literal(elementIndex), literal(input.name), importExpr(Identifiers$1.bind).callFn([convertedBinding]));
             }
             else {
                 this.unsupported("binding " + PropertyBindingType[input.type]);
@@ -59022,7 +59022,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.5-010a4ef
+ * @license Angular v6.0.0-rc.5-99f8e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60937,7 +60937,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5-010a4ef');
+var VERSION$2 = new Version$1('6.0.0-rc.5-99f8e10');
 
 /**
  * @fileoverview added by tsickle
@@ -76774,7 +76774,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.5-010a4ef
+ * @license Angular v6.0.0-rc.5-99f8e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79372,7 +79372,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version$1('6.0.0-rc.5-010a4ef');
+var VERSION = new Version$1('6.0.0-rc.5-99f8e10');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-010a4ef');
+export const VERSION = new Version('6.0.0-rc.5-99f8e10');
 //# sourceMappingURL=version.js.map
