5e82012918dba0f45a3524b29d7a61e27619f2b7
Victor Berchet <victor@suumit.com>
2018-04-18 22:13:24 +0000

    acf6781 test(core): add a symbols test for renderer2 code (#23436)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-fd48e53
+ * @license Angular v6.0.0-rc.5-acf6781
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-fd48e53');
+var VERSION = new Version('6.0.0-rc.5-acf6781');
 
 /**
  * @license
