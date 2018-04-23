8f83d42149e3ef5a2e7accfd8de1f8f59fc1ab76
Victor Berchet <victor@suumit.com>
2018-04-23 21:06:48 +0000

    60e5507 docs(aio): Add UpgradingAngularJS to education resources (#23169)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-4cfa571
+ * @license Angular v6.0.0-rc.5-60e5507
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-4cfa571');
+var VERSION = new Version('6.0.0-rc.5-60e5507');
 
 /**
  * @license
