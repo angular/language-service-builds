52487532abf7d0c71c474da681c8616886f363fe
Victor Berchet <victor@suumit.com>
2018-04-24 22:27:46 +0000

    8d0ee34 docs: corrected spelling of "ambient".

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+81.sha-43a49d3
+ * @license Angular v6.0.0-rc.5+82.sha-8d0ee34
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1165,7 +1165,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+81.sha-43a49d3');
+var VERSION = new Version('6.0.0-rc.5+82.sha-8d0ee34');
 
 /**
  * @license
@@ -24164,7 +24164,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5+81.sha-43a49d3');
+var VERSION$2 = new Version$1('6.0.0-rc.5+82.sha-8d0ee34');
 
 /**
  * @license
@@ -49639,7 +49639,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+81.sha-43a49d3');
+var VERSION$3 = new Version$1('6.0.0-rc.5+82.sha-8d0ee34');
 
 /**
  * @license
