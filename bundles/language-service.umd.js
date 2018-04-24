1c558faa594f127120faa99cb4b57789d23cfd0c
Victor Berchet <victor@suumit.com>
2018-04-24 21:21:11 +0000

    e1c4930 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+76.sha-b1d03fe
+ * @license Angular v6.0.0-rc.5+78.sha-e1c4930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1165,7 +1165,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+76.sha-b1d03fe');
+var VERSION = new Version('6.0.0-rc.5+78.sha-e1c4930');
 
 /**
  * @license
@@ -24164,7 +24164,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5+76.sha-b1d03fe');
+var VERSION$2 = new Version$1('6.0.0-rc.5+78.sha-e1c4930');
 
 /**
  * @license
@@ -49639,7 +49639,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+76.sha-b1d03fe');
+var VERSION$3 = new Version$1('6.0.0-rc.5+78.sha-e1c4930');
 
 /**
  * @license
