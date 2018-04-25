6ca5e73c1d764f60b34cb748eb45c5322264dea4
Igor Minar <igor@angular.io>
2018-04-25 23:16:35 +0000

    b4c252b build: serve ivy todo app with real http-server (#23446)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+85.sha-db77d8d
+ * @license Angular v6.0.0-rc.5+86.sha-b4c252b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1165,7 +1165,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+85.sha-db77d8d');
+var VERSION = new Version('6.0.0-rc.5+86.sha-b4c252b');
 
 /**
  * @license
@@ -24206,7 +24206,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5+85.sha-db77d8d');
+var VERSION$2 = new Version$1('6.0.0-rc.5+86.sha-b4c252b');
 
 /**
  * @license
@@ -49682,7 +49682,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+85.sha-db77d8d');
+var VERSION$3 = new Version$1('6.0.0-rc.5+86.sha-b4c252b');
 
 /**
  * @license
