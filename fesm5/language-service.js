0364066a0400a3734d8d5fefe42b1bb67560217b
Igor Minar <igor@angular.io>
2018-05-01 17:54:14 +0000

    31a435e docs: fix typo in tag name (`my-child` --> `app-child`) (#23606)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-3e92b22
+ * @license Angular v6.0.0-rc.6-31a435e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.6-3e92b22');
+var VERSION = new Version('6.0.0-rc.6-31a435e');
 
 /**
  * @license
