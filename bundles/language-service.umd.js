e5742b68f9cfaaa4f21311d71307c2ca80fe1097
Igor Minar <igor@angular.io>
2018-05-02 23:18:13 +0000

    5fc4299 refactor(aio): move right margin from `.home` image to `.home` anchor (#23624)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-91d4da0
+ * @license Angular v6.0.0-rc.6-5fc4299
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.6-91d4da0
+ * @license Angular v6.0.0-rc.6-5fc4299
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -895,7 +895,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION$1 = new Version('6.0.0-rc.6-91d4da0');
+var VERSION$1 = new Version('6.0.0-rc.6-5fc4299');
 
 /**
  * @fileoverview added by tsickle
@@ -59019,7 +59019,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.6-91d4da0
+ * @license Angular v6.0.0-rc.6-5fc4299
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60934,7 +60934,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.6-91d4da0');
+var VERSION$2 = new Version$1('6.0.0-rc.6-5fc4299');
 
 /**
  * @fileoverview added by tsickle
@@ -76774,7 +76774,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.6-91d4da0
+ * @license Angular v6.0.0-rc.6-5fc4299
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79372,7 +79372,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version$1('6.0.0-rc.6-91d4da0');
+var VERSION = new Version$1('6.0.0-rc.6-5fc4299');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-91d4da0
+ * @license Angular v6.0.0-rc.6-5fc4299
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.6-91d4da0');
+var VERSION = new Version('6.0.0-rc.6-5fc4299');
 
 /**
  * @license
