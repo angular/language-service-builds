46540de0acaf9a10b6dff179601a6db90b87a112
Igor Minar <igor@angular.io>
2018-05-03 20:08:06 +0000

    d0ccf5f release: cut the v6.0.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-ecde152
+ * @license Angular v6.0.0-d0ccf5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -227,7 +227,7 @@ var tslib_es6 = Object.freeze({
 });
 
 /**
- * @license Angular v6.0.0-rc.6-ecde152
+ * @license Angular v6.0.0-d0ccf5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -895,7 +895,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION$1 = new Version('6.0.0-rc.6-ecde152');
+var VERSION$1 = new Version('6.0.0-d0ccf5f');
 
 /**
  * @fileoverview added by tsickle
@@ -59038,7 +59038,7 @@ exports.zipAll = zipAll_1.zipAll;
 var index_71 = index$4.share;
 
 /**
- * @license Angular v6.0.0-rc.6-ecde152
+ * @license Angular v6.0.0-d0ccf5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -60953,7 +60953,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.6-ecde152');
+var VERSION$2 = new Version$1('6.0.0-d0ccf5f');
 
 /**
  * @fileoverview added by tsickle
@@ -76793,7 +76793,7 @@ var QueryList_ = /** @class */ (function () {
 }());
 
 /**
- * @license Angular v6.0.0-rc.6-ecde152
+ * @license Angular v6.0.0-d0ccf5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -79391,7 +79391,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version$1('6.0.0-rc.6-ecde152');
+var VERSION = new Version$1('6.0.0-d0ccf5f');
 
 exports.createLanguageService = createLanguageService;
 exports.TypeScriptServiceHost = TypeScriptServiceHost;
