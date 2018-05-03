7d3081eea9cb51e84693f0899353b4168c0a461a
Igor Minar <igor@angular.io>
2018-05-03 22:30:23 +0000

    b19216d fix(aio): correct project id for deployment of archive sites

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.7-84fc1a3
+ * @license Angular v4.4.7-b19216d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.7-84fc1a3');
+var VERSION = new Version('4.4.7-b19216d');
 
 /**
  * @license
