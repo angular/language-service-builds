04fff472b404b8a87a47b139ba8e45a2dba1a3fe
Igor Minar <igor@angular.io>
2018-05-04 22:53:33 +0000

    7e9649b build: update to latest nodejs bazel rules (#23683)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+135.sha-e3e1577
+ * @license Angular v6.0.0-rc.5+136.sha-7e9649b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1165,7 +1165,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+135.sha-e3e1577');
+var VERSION = new Version('6.0.0-rc.5+136.sha-7e9649b');
 
 /**
  * @license
@@ -24206,7 +24206,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5+135.sha-e3e1577');
+var VERSION$2 = new Version$1('6.0.0-rc.5+136.sha-7e9649b');
 
 /**
  * @license
@@ -49721,7 +49721,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+135.sha-e3e1577');
+var VERSION$3 = new Version$1('6.0.0-rc.5+136.sha-7e9649b');
 
 /**
  * @license
