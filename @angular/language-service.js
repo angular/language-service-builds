95612678e1f42f4c1a5e029becf7de90df6b7513
Igor Minar <igor@angular.io>
2018-05-04 22:32:42 +0000

    0187501 test: fix firebase deployment script test

--- @angular/language-service.js
+++ @angular/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.7-b19216d
+ * @license Angular v4.4.7-0187501
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2629,7 +2629,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.7-b19216d');
+var VERSION = new Version('4.4.7-0187501');
 
 /**
  * @license
