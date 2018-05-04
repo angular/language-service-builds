6083d25cd167d5b4c9f59f413e8e7023b81a3387
Igor Minar <igor@angular.io>
2018-05-04 22:47:14 +0000

    9b53a6e fix(aio): remove main background color when printing (#23538)

--- fesm5/language-service.js
+++ fesm5/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-3824e3f
+ * @license Angular v6.0.0-9b53a6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2605,7 +2605,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-3824e3f');
+var VERSION = new Version('6.0.0-9b53a6e');
 
 /**
  * @license
