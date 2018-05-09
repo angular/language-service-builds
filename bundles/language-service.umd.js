5a22de4f25c96f6652a7cffcce1c7c1788390aec
Igor Minar <igor@angular.io>
2018-05-09 19:27:02 +0000

    2e466f4 build(bazel): update to rules_typescript 0.12.3 (#23617)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+33.sha-7b06fa8
+ * @license Angular v6.0.0+34.sha-2e466f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -393,7 +393,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0+33.sha-7b06fa8');
+var VERSION = new Version('6.0.0+34.sha-2e466f4');
 
 /**
  * @license
@@ -24164,7 +24164,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0+33.sha-7b06fa8');
+var VERSION$2 = new Version$1('6.0.0+34.sha-2e466f4');
 
 /**
  * @license
@@ -49687,7 +49687,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0+33.sha-7b06fa8');
+var VERSION$3 = new Version$1('6.0.0+34.sha-2e466f4');
 
 /**
  * @license
