f79c238182448a204737029e1f0003a0321c4441
Igor Minar <igor@angular.io>
2018-05-09 19:18:25 +0000

    971e78d ci: Remove Chuck from pullapprove (#23798)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+144.sha-b0eca85
+ * @license Angular v6.0.0-rc.5+145.sha-971e78d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1165,7 +1165,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+144.sha-b0eca85');
+var VERSION = new Version('6.0.0-rc.5+145.sha-971e78d');
 
 /**
  * @license
@@ -24331,7 +24331,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5+144.sha-b0eca85');
+var VERSION$2 = new Version$1('6.0.0-rc.5+145.sha-971e78d');
 
 /**
  * @license
@@ -49859,7 +49859,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+144.sha-b0eca85');
+var VERSION$3 = new Version$1('6.0.0-rc.5+145.sha-971e78d');
 
 /**
  * @license
