e6f9981f46816ae0e5305211651646fe61b24bf3
Igor Minar <igor@angular.io>
2018-05-09 19:25:07 +0000

    6117085 build(aio): include `navigation.json` changes in docs-watch (#23698)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+146.sha-a800ccd
+ * @license Angular v6.0.0-rc.5+147.sha-6117085
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1165,7 +1165,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+146.sha-a800ccd');
+var VERSION = new Version('6.0.0-rc.5+147.sha-6117085');
 
 /**
  * @license
@@ -24331,7 +24331,7 @@ var Version$1 = /** @class */ (function () {
 /**
  *
  */
-var VERSION$2 = new Version$1('6.0.0-rc.5+146.sha-a800ccd');
+var VERSION$2 = new Version$1('6.0.0-rc.5+147.sha-6117085');
 
 /**
  * @license
@@ -49859,7 +49859,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
 /**
  *
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+146.sha-a800ccd');
+var VERSION$3 = new Version$1('6.0.0-rc.5+147.sha-6117085');
 
 /**
  * @license
