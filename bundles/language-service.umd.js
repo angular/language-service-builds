909b721363b844e2f97c317402e5dd6e9a6c8e2c
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 22:35:20 +0000

    d4b8b24 fix(elements): prevent closure renaming of platform properties (#23843)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+174.sha-cfde36d
+ * @license Angular v6.0.0-rc.5+175.sha-d4b8b24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+174.sha-cfde36d');
+var VERSION = new Version('6.0.0-rc.5+175.sha-d4b8b24');
 
 /**
  * @license
@@ -24325,7 +24325,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+174.sha-cfde36d');
+var VERSION$2 = new Version$1('6.0.0-rc.5+175.sha-d4b8b24');
 
 /**
  * @license
@@ -49836,7 +49836,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+174.sha-cfde36d');
+var VERSION$3 = new Version$1('6.0.0-rc.5+175.sha-d4b8b24');
 
 /**
  * @license
