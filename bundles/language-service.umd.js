06821f73a5f7ac23a46b52d45ec74f5a91ee7e22
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 21:49:17 +0000

    6a24c02 docs(aio): Remove Intertech with no courses scheduled (#22867) PR Close #22867

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+187.sha-b7c417f
+ * @license Angular v6.0.0-rc.5+188.sha-6a24c02
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+187.sha-b7c417f');
+var VERSION = new Version('6.0.0-rc.5+188.sha-6a24c02');
 
 /**
  * @license
@@ -24325,7 +24325,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+187.sha-b7c417f');
+var VERSION$2 = new Version$1('6.0.0-rc.5+188.sha-6a24c02');
 
 /**
  * @license
@@ -49847,7 +49847,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+187.sha-b7c417f');
+var VERSION$3 = new Version$1('6.0.0-rc.5+188.sha-6a24c02');
 
 /**
  * @license
