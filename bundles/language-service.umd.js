0970a36daea4ad8ba2809905d601629b94bc7916
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:49:15 +0000

    4226070 docs(aio): Add missing dependencies and files to testing zip file download (#23948)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+16.sha-95344d6
+ * @license Angular v6.0.2+18.sha-4226070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+16.sha-95344d6');
+var VERSION = new Version('6.0.2+18.sha-4226070');
 
 /**
  * @license
@@ -24158,7 +24158,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.2+16.sha-95344d6');
+var VERSION$2 = new Version$1('6.0.2+18.sha-4226070');
 
 /**
  * @license
@@ -49660,7 +49660,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.2+16.sha-95344d6');
+var VERSION$3 = new Version$1('6.0.2+18.sha-4226070');
 
 /**
  * @license
