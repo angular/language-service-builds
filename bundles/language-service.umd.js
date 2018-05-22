32044890c551dee9d4b764d80d71384735cb63eb
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 21:55:15 +0000

    23a98b9 docs: add doc to event-management api (#23656)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+214.sha-bd149e5
+ * @license Angular v6.0.0-rc.5+215.sha-23a98b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+214.sha-bd149e5');
+var VERSION = new Version('6.0.0-rc.5+215.sha-23a98b9');
 
 /**
  * @license
@@ -24404,7 +24404,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+214.sha-bd149e5');
+var VERSION$2 = new Version$1('6.0.0-rc.5+215.sha-23a98b9');
 
 /**
  * @license
@@ -49940,7 +49940,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+214.sha-bd149e5');
+var VERSION$3 = new Version$1('6.0.0-rc.5+215.sha-23a98b9');
 
 /**
  * @license
