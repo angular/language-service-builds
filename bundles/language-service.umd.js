5a3958d47cbc04a2925c8fd1650d4bcf1b5b1da5
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 19:32:18 +0000

    0ee5b7e fix(service-worker): check platformBrowser before accessing navigator.serviceWorker (#21231)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+19.sha-23f2a70
+ * @license Angular v6.0.2+21.sha-0ee5b7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+19.sha-23f2a70');
+var VERSION = new Version('6.0.2+21.sha-0ee5b7e');
 
 /**
  * @license
@@ -24158,7 +24158,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.2+19.sha-23f2a70');
+var VERSION$2 = new Version$1('6.0.2+21.sha-0ee5b7e');
 
 /**
  * @license
@@ -49660,7 +49660,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.2+19.sha-23f2a70');
+var VERSION$3 = new Version$1('6.0.2+21.sha-0ee5b7e');
 
 /**
  * @license
