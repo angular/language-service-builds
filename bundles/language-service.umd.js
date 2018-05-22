efed735abe08d052d78aa00ce6235787ebcf880d
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 17:58:51 +0000

    afff84c docs(aio): Remove outdated README.md from cli-quickstart zip (#23947)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+18.sha-4226070
+ * @license Angular v6.0.2+20.sha-afff84c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+18.sha-4226070');
+var VERSION = new Version('6.0.2+20.sha-afff84c');
 
 /**
  * @license
@@ -24158,7 +24158,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.2+18.sha-4226070');
+var VERSION$2 = new Version$1('6.0.2+20.sha-afff84c');
 
 /**
  * @license
@@ -49660,7 +49660,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.2+18.sha-4226070');
+var VERSION$3 = new Version$1('6.0.2+20.sha-afff84c');
 
 /**
  * @license
