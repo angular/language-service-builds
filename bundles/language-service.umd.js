fceef3a5b81da3993747193f0afd2fa03694c5a2
Victor Berchet <victor@suumit.com>
2018-05-30 17:35:00 +0000

    b87d650 refactor(ivy): rename `PipeDef.n` to `PipeDef.factory` (#23883)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+250.sha-f6f44ed
+ * @license Angular v6.0.0-rc.5+248.sha-b87d650
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+250.sha-f6f44ed');
+var VERSION = new Version('6.0.0-rc.5+248.sha-b87d650');
 
 /**
  * @license
@@ -24440,7 +24440,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+250.sha-f6f44ed');
+var VERSION$2 = new Version$1('6.0.0-rc.5+248.sha-b87d650');
 
 /**
  * @license
@@ -50059,7 +50059,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+250.sha-f6f44ed');
+var VERSION$3 = new Version$1('6.0.0-rc.5+248.sha-b87d650');
 
 /**
  * @license
