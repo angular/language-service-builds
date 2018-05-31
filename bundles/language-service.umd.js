2008418ed56325a905361e821459767aca728cb9
Victor Berchet <victor@suumit.com>
2018-05-31 23:35:05 +0000

    68a799e build(bazel): re-enable packages/upgrade/test:test_web test with static_files in ts_web_test_suite (#24214)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+292.sha-5f178f3
+ * @license Angular v6.0.0-rc.5+293.sha-68a799e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+292.sha-5f178f3');
+var VERSION = new Version('6.0.0-rc.5+293.sha-68a799e');
 
 /**
  * @license
@@ -24441,7 +24441,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+292.sha-5f178f3');
+var VERSION$2 = new Version$1('6.0.0-rc.5+293.sha-68a799e');
 
 /**
  * @license
@@ -50086,7 +50086,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+292.sha-5f178f3');
+var VERSION$3 = new Version$1('6.0.0-rc.5+293.sha-68a799e');
 
 /**
  * @license
