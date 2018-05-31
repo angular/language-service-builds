c31a3db5c9caf41a11e64d2d9f0e4128f127963d
Victor Berchet <victor@suumit.com>
2018-05-31 00:27:10 +0000

    5d6074e docs: fix typo (#24210)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+275.sha-b492b9e
+ * @license Angular v6.0.0-rc.5+282.sha-5d6074e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+275.sha-b492b9e');
+var VERSION = new Version('6.0.0-rc.5+282.sha-5d6074e');
 
 /**
  * @license
@@ -24441,7 +24441,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+275.sha-b492b9e');
+var VERSION$2 = new Version$1('6.0.0-rc.5+282.sha-5d6074e');
 
 /**
  * @license
@@ -50079,7 +50079,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+275.sha-b492b9e');
+var VERSION$3 = new Version$1('6.0.0-rc.5+282.sha-5d6074e');
 
 /**
  * @license
