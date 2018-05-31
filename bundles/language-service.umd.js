7c760d26ec1e82c67ed2c9bb3f4bcbd41373d980
Victor Berchet <victor@suumit.com>
2018-05-31 00:03:13 +0000

    c9eb491 fix(animations): Fix browser detection logic (#24188)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+35.sha-a634a5a
+ * @license Angular v6.0.3+36.sha-c9eb491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+35.sha-a634a5a');
+var VERSION = new Version('6.0.3+36.sha-c9eb491');
 
 /**
  * @license
@@ -24158,7 +24158,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.3+35.sha-a634a5a');
+var VERSION$2 = new Version$1('6.0.3+36.sha-c9eb491');
 
 /**
  * @license
@@ -49660,7 +49660,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.3+35.sha-a634a5a');
+var VERSION$3 = new Version$1('6.0.3+36.sha-c9eb491');
 
 /**
  * @license
