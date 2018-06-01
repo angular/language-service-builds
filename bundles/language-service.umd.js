9c5d36defebfcf706f800eb3333151ecb04ef2a5
Victor Berchet <victor@suumit.com>
2018-06-01 15:55:58 +0000

    2991b1b fix(platform-server): avoid dependency cycle when using http interceptor (#24229)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+48.sha-b18cf21
+ * @license Angular v6.0.3+49.sha-2991b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+48.sha-b18cf21');
+var VERSION = new Version('6.0.3+49.sha-2991b1b');
 
 /**
  * @license
@@ -24158,7 +24158,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.3+48.sha-b18cf21');
+var VERSION$2 = new Version$1('6.0.3+49.sha-2991b1b');
 
 /**
  * @license
@@ -49660,7 +49660,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.3+48.sha-b18cf21');
+var VERSION$3 = new Version$1('6.0.3+49.sha-2991b1b');
 
 /**
  * @license
