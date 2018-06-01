a06e9a8cf194855b1a069c6d81ad2bea4794c2dc
Victor Berchet <victor@suumit.com>
2018-06-01 17:29:53 +0000

    08f943a test(platform-server): add a test for 'hidden' property (#24239)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+295.sha-f69ac67
+ * @license Angular v6.0.0-rc.5+296.sha-08f943a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+295.sha-f69ac67');
+var VERSION = new Version('6.0.0-rc.5+296.sha-08f943a');
 
 /**
  * @license
@@ -24385,7 +24385,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+295.sha-f69ac67');
+var VERSION$2 = new Version$1('6.0.0-rc.5+296.sha-08f943a');
 
 /**
  * @license
@@ -48653,7 +48653,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+295.sha-f69ac67');
+var VERSION$3 = new Version$1('6.0.0-rc.5+296.sha-08f943a');
 
 /**
  * @license
