fc22fc963a75c96ddafc31e309e82255f02a4968
Victor Berchet <victor@suumit.com>
2018-06-04 19:31:55 +0000

    255463e fix(aio): remove unnecessary scrollbar in `code-tabs` (#24207)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+305.sha-ccaa199
+ * @license Angular v6.0.0-rc.5+308.sha-255463e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+305.sha-ccaa199');
+var VERSION = new Version('6.0.0-rc.5+308.sha-255463e');
 
 /**
  * @license
@@ -24385,7 +24385,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+305.sha-ccaa199');
+var VERSION$2 = new Version$1('6.0.0-rc.5+308.sha-255463e');
 
 /**
  * @license
@@ -48644,7 +48644,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+305.sha-ccaa199');
+var VERSION$3 = new Version$1('6.0.0-rc.5+308.sha-255463e');
 
 /**
  * @license
