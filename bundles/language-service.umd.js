09baecc48f73266e3c22ddaa262c68fcb201c897
Victor Berchet <victor@suumit.com>
2018-06-05 21:05:30 +0000

    d814eaa build(bazel): ran format (#24279)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+312.sha-b750919
+ * @license Angular v6.0.0-rc.5+316.sha-d814eaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+312.sha-b750919');
+var VERSION = new Version('6.0.0-rc.5+316.sha-d814eaa');
 
 /**
  * @license
@@ -24385,7 +24385,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+312.sha-b750919');
+var VERSION$2 = new Version$1('6.0.0-rc.5+316.sha-d814eaa');
 
 /**
  * @license
@@ -48681,7 +48681,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+312.sha-b750919');
+var VERSION$3 = new Version$1('6.0.0-rc.5+316.sha-d814eaa');
 
 /**
  * @license
