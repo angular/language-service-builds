62ba8b4fff901692b933c90149612aa75e9ba3b9
Victor Berchet <victor@suumit.com>
2018-06-05 22:38:16 +0000

    20c463e feat(router): add navigation execution context info to activation hooks (#24204)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+317.sha-57eacf4
+ * @license Angular v6.0.0-rc.5+318.sha-20c463e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+317.sha-57eacf4');
+var VERSION = new Version('6.0.0-rc.5+318.sha-20c463e');
 
 /**
  * @license
@@ -24385,7 +24385,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+317.sha-57eacf4');
+var VERSION$2 = new Version$1('6.0.0-rc.5+318.sha-20c463e');
 
 /**
  * @license
@@ -48677,7 +48677,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+317.sha-57eacf4');
+var VERSION$3 = new Version$1('6.0.0-rc.5+318.sha-20c463e');
 
 /**
  * @license
