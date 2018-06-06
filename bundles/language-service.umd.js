53ffa7842980a1478840e26235553577e2eace8a
Victor Berchet <victor@suumit.com>
2018-06-06 01:50:53 +0000

    9367e91 fix(forms): properly handle special properties in FormGroup.get (#22249)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+319.sha-87b1671
+ * @license Angular v6.0.0-rc.5+320.sha-9367e91
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+319.sha-87b1671');
+var VERSION = new Version('6.0.0-rc.5+320.sha-9367e91');
 
 /**
  * @license
@@ -24385,7 +24385,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.0-rc.5+319.sha-87b1671');
+var VERSION$2 = new Version$1('6.0.0-rc.5+320.sha-9367e91');
 
 /**
  * @license
@@ -48677,7 +48677,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.0-rc.5+319.sha-87b1671');
+var VERSION$3 = new Version$1('6.0.0-rc.5+320.sha-9367e91');
 
 /**
  * @license
