8f00d722bdf0a1e796057214d920e5778dfd3a2e
Miško Hevery <misko@hevery.com>
2018-06-08 21:32:04 +0000

    1135563 fix(ivy): compute transitive scopes from NgModuleDef only (#24334)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+21.sha-8be6892
+ * @license Angular v6.1.0-beta.0+23.sha-1135563
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+21.sha-8be6892');
+var VERSION = new Version('6.1.0-beta.0+23.sha-1135563');
 
 /**
  * @license
@@ -24388,7 +24388,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.0+21.sha-8be6892');
+var VERSION$2 = new Version$1('6.1.0-beta.0+23.sha-1135563');
 
 /**
  * @license
@@ -49371,7 +49371,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.0+21.sha-8be6892');
+var VERSION$3 = new Version$1('6.1.0-beta.0+23.sha-1135563');
 
 /**
  * @license
