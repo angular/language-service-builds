338631df86336f47e623db9870ae33ea6eb19a98
Miško Hevery <misko@hevery.com>
2018-06-11 16:50:41 +0000

    5e8bf2f build(docs-infra): ensure `dist/` directory is cleaned before running `tsc --watch` (#24372)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+30.sha-29eb24b
+ * @license Angular v6.1.0-beta.0+32.sha-5e8bf2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+30.sha-29eb24b');
+var VERSION = new Version('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @license
@@ -24393,7 +24393,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.0+30.sha-29eb24b');
+var VERSION$2 = new Version$1('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @license
@@ -49402,7 +49402,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.0+30.sha-29eb24b');
+var VERSION$3 = new Version$1('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @license
