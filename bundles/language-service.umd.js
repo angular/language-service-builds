e5c098f76a86b6d84ad22c5cf6e0a5ede4df1d85
Miško Hevery <misko@hevery.com>
2018-06-12 19:19:38 +0000

    c2b5ebf build: update buildifier to latest (#24296)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+40.sha-1316024
+ * @license Angular v6.1.0-beta.0+42.sha-c2b5ebf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+40.sha-1316024');
+var VERSION = new Version('6.1.0-beta.0+42.sha-c2b5ebf');
 
 /**
  * @license
@@ -41276,7 +41276,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.0+40.sha-1316024');
+var VERSION$2 = new Version$1('6.1.0-beta.0+42.sha-c2b5ebf');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54234,7 +54234,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.0+40.sha-1316024');
+var VERSION$3 = new Version$1('6.1.0-beta.0+42.sha-c2b5ebf');
 
 /**
  * @license
