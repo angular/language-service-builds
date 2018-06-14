c2645d7db75a017d9ba0dc6f8c076a8403e4d867
Miško Hevery <misko@hevery.com>
2018-06-14 00:15:18 +0000

    03f93b3 Revert "build: update to Bazel 0.14.0 (#24296)" (#24492)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1
+ * @license Angular v6.1.0-beta.1+1.sha-03f93b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1');
+var VERSION = new Version('6.1.0-beta.1+1.sha-03f93b3');
 
 /**
  * @license
@@ -41274,7 +41274,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.1');
+var VERSION$2 = new Version$1('6.1.0-beta.1+1.sha-03f93b3');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54232,7 +54232,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.1');
+var VERSION$3 = new Version$1('6.1.0-beta.1+1.sha-03f93b3');
 
 /**
  * @license
