8658e5a1407be8c50b81d0607dee63b0b1f31142
Miško Hevery <misko@hevery.com>
2018-06-14 17:33:31 +0000

    5aa12c7 build: update to Bazel 0.14.0 (#24512)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+23.sha-d8f7b29
+ * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+23.sha-d8f7b29');
+var VERSION = new Version('6.1.0-beta.1+24.sha-5aa12c7');
 
 /**
  * @license
@@ -41262,7 +41262,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.1+23.sha-d8f7b29');
+var VERSION$2 = new Version$1('6.1.0-beta.1+24.sha-5aa12c7');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54227,7 +54227,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.1+23.sha-d8f7b29');
+var VERSION$3 = new Version$1('6.1.0-beta.1+24.sha-5aa12c7');
 
 /**
  * @license
