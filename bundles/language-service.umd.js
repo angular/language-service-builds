50345eb46a949455f30550b00e5083608236412a
Miško Hevery <misko@hevery.com>
2018-06-15 00:21:24 +0000

    f3625e4 test(common): rename keyvalue e2e test (#24489)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+31.sha-ccbda9d
+ * @license Angular v6.1.0-beta.1+32.sha-f3625e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+31.sha-ccbda9d');
+var VERSION = new Version('6.1.0-beta.1+32.sha-f3625e4');
 
 /**
  * @license
@@ -24375,7 +24375,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.1+31.sha-ccbda9d');
+var VERSION$2 = new Version$1('6.1.0-beta.1+32.sha-f3625e4');
 
 /**
  * @license
@@ -49381,7 +49381,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.1+31.sha-ccbda9d');
+var VERSION$3 = new Version$1('6.1.0-beta.1+32.sha-f3625e4');
 
 /**
  * @license
