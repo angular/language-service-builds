e05897b48996a7cc3e4a7dba72c27cfa14b181d8
Miško Hevery <misko@hevery.com>
2018-06-21 00:21:44 +0000

    def354d release: cut the v6.1.0-beta.2 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+43.sha-9782736
+ * @license Angular v6.1.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+43.sha-9782736');
+var VERSION = new Version('6.1.0-beta.2');
 
 /**
  * @license
@@ -24375,7 +24375,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.1+43.sha-9782736');
+var VERSION$2 = new Version$1('6.1.0-beta.2');
 
 /**
  * @license
@@ -49381,7 +49381,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.1+43.sha-9782736');
+var VERSION$3 = new Version$1('6.1.0-beta.2');
 
 /**
  * @license
