b4f5a6e6d210be58bc20356570faf2ac401f641e
Miško Hevery <misko@hevery.com>
2018-06-21 20:48:50 +0000

    10da6a4 refactor(ivy): first pass at extracting ReflectionHost for abstract reflection (#24541)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+7.sha-84272e2
+ * @license Angular v6.1.0-beta.2+8.sha-10da6a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
+var VERSION = new Version('6.1.0-beta.2+8.sha-10da6a4');
 
 /**
  * @license
@@ -41648,7 +41648,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+7.sha-84272e2');
+var VERSION$2 = new Version$1('6.1.0-beta.2+8.sha-10da6a4');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54616,7 +54616,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+7.sha-84272e2');
+var VERSION$3 = new Version$1('6.1.0-beta.2+8.sha-10da6a4');
 
 /**
  * @license
