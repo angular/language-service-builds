d5acf6d1bc3a7e1af22b86ddb291f5a97e2b45ac
Miško Hevery <misko@hevery.com>
2018-06-21 20:44:42 +0000

    84eff42 docs: update `lowercase` pipe example in "AngularJS to Angular" guide (#24588)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+3.sha-c11e84e
+ * @license Angular v6.0.6+4.sha-84eff42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.6+3.sha-c11e84e');
+var VERSION = new Version('6.0.6+4.sha-84eff42');
 
 /**
  * @license
@@ -24081,7 +24081,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.6+3.sha-c11e84e');
+var VERSION$2 = new Version$1('6.0.6+4.sha-84eff42');
 
 /**
  * @license
@@ -48218,7 +48218,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.6+3.sha-c11e84e');
+var VERSION$3 = new Version$1('6.0.6+4.sha-84eff42');
 
 /**
  * @license
