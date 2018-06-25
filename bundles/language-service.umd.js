0c9a304fb48eaf2fcb0ab59974c6722a77262e73
Miško Hevery <misko@hevery.com>
2018-06-25 19:07:14 +0000

    166d90d ci: fix broken build

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+28.sha-7d31874
+ * @license Angular v6.1.0-beta.2+29.sha-166d90d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+28.sha-7d31874');
+var VERSION = new Version('6.1.0-beta.2+29.sha-166d90d');
 
 /**
  * @license
@@ -41829,7 +41829,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+28.sha-7d31874');
+var VERSION$2 = new Version$1('6.1.0-beta.2+29.sha-166d90d');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54797,7 +54797,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+28.sha-7d31874');
+var VERSION$3 = new Version$1('6.1.0-beta.2+29.sha-166d90d');
 
 /**
  * @license
