b0bd52564aa07bc37322e7fff0321dfd5d460989
Miško Hevery <misko@hevery.com>
2018-06-25 16:10:30 +0000

    1e139d4 refactor(ivy): rename, limit usage of global vars (#24604)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+19.sha-fba3f10
+ * @license Angular v6.1.0-beta.2+20.sha-1e139d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
+var VERSION = new Version('6.1.0-beta.2+20.sha-1e139d4');
 
 /**
  * @license
@@ -24375,7 +24375,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+19.sha-fba3f10');
+var VERSION$2 = new Version$1('6.1.0-beta.2+20.sha-1e139d4');
 
 /**
  * @license
@@ -46042,15 +46042,16 @@ function leaveView(newView, creationOnly) {
 }
 /**
  * Refreshes the view, executing the following steps in that order:
- * triggers init hooks, refreshes dynamic children, triggers content hooks, sets host bindings,
+ * triggers init hooks, refreshes dynamic embedded views, triggers content hooks, sets host
+ * bindings,
  * refreshes child components.
  * Note: view hooks are triggered later when leaving the view.
- * */
+ */
 function refreshView() {
     if (!checkNoChangesMode) {
         executeInitHooks(viewData, tView, creationMode);
     }
-    refreshDynamicChildren();
+    refreshDynamicEmbeddedViews(viewData);
     if (!checkNoChangesMode) {
         executeHooks(directives, tView.contentHooks, tView.contentCheckHooks, creationMode);
     }
@@ -46688,8 +46689,12 @@ function baseDirectiveCreate(index, directive, directiveDef) {
  * Marking the end of LContainerNode is the time when to child Views get inserted or removed.
  */
 
-function refreshDynamicChildren() {
-    for (var current = getLViewChild(viewData); current !== null; current = current[NEXT]) {
+/**
+ * Goes over dynamic embedded views (ones created through ViewContainerRef APIs) and refreshes them
+ * by executing an associated template function.
+ */
+function refreshDynamicEmbeddedViews(lViewData) {
+    for (var current = getLViewChild(lViewData); current !== null; current = current[NEXT]) {
         // Note: current can be an LViewData or an LContainer instance, but here we are only interested
         // in LContainer. We can tell it's an LContainer because its length is less than the LViewData
         // header.
@@ -49480,7 +49485,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+19.sha-fba3f10');
+var VERSION$3 = new Version$1('6.1.0-beta.2+20.sha-1e139d4');
 
 /**
  * @license
