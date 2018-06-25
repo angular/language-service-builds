44eb7e854eeed679b4d1eab09aa9118f95cd1433
Miško Hevery <misko@hevery.com>
2018-06-25 16:12:20 +0000

    6e20e0a fix(animations): set animations styles properly on platform-server (#24624)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+20.sha-1e139d4
+ * @license Angular v6.1.0-beta.2+21.sha-6e20e0a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+20.sha-1e139d4');
+var VERSION = new Version('6.1.0-beta.2+21.sha-6e20e0a');
 
 /**
  * @license
@@ -41650,7 +41650,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+20.sha-1e139d4');
+var VERSION$2 = new Version$1('6.1.0-beta.2+21.sha-6e20e0a');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54618,7 +54618,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+20.sha-1e139d4');
+var VERSION$3 = new Version$1('6.1.0-beta.2+21.sha-6e20e0a');
 
 /**
  * @license
