494a9e91a2278bf444b039c728c406a7db5117ad
Jason Aden <jasonaden@google.com>
2018-06-25 17:16:15 +0000

    941d2cd test(aio): fix upgrade-phonecat examples e2e tests (#24583)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+26.sha-d244523
+ * @license Angular v6.1.0-beta.2+25.sha-941d2cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+26.sha-d244523');
+var VERSION = new Version('6.1.0-beta.2+25.sha-941d2cd');
 
 /**
  * @license
@@ -41729,7 +41729,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+26.sha-d244523');
+var VERSION$2 = new Version$1('6.1.0-beta.2+25.sha-941d2cd');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54697,7 +54697,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+26.sha-d244523');
+var VERSION$3 = new Version$1('6.1.0-beta.2+25.sha-941d2cd');
 
 /**
  * @license
