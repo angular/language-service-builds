d6553212e4d1820ed1649b80c6ddfd6b58edceed
Jason Aden <jasonaden@google.com>
2018-06-25 17:13:56 +0000

    65744e4 docs: test api doc for pipes (#24141)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+10.sha-56bc869
+ * @license Angular v6.0.6+11.sha-65744e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -390,7 +390,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.6+10.sha-56bc869');
+var VERSION = new Version('6.0.6+11.sha-65744e4');
 
 /**
  * @license
@@ -24081,7 +24081,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.6+10.sha-56bc869');
+var VERSION$2 = new Version$1('6.0.6+11.sha-65744e4');
 
 /**
  * @license
@@ -48218,7 +48218,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.6+10.sha-56bc869');
+var VERSION$3 = new Version$1('6.0.6+11.sha-65744e4');
 
 /**
  * @license
