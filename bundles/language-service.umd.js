f6bfce12ef97188afce2bb54908e45c99f737d14
Miško Hevery <misko@hevery.com>
2018-06-25 16:10:18 +0000

    fba3f10 docs: add guide-angular.wishtack.io to education resources (#24585)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+17.sha-39c7769
+ * @license Angular v6.1.0-beta.2+19.sha-fba3f10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+17.sha-39c7769');
+var VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
 
 /**
  * @license
@@ -24375,7 +24375,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+17.sha-39c7769');
+var VERSION$2 = new Version$1('6.1.0-beta.2+19.sha-fba3f10');
 
 /**
  * @license
@@ -49480,7 +49480,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+17.sha-39c7769');
+var VERSION$3 = new Version$1('6.1.0-beta.2+19.sha-fba3f10');
 
 /**
  * @license
