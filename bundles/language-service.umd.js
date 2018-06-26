afb537fafd733d926e50db1db4a317641427a83e
Jason Aden <jasonaden@google.com>
2018-06-26 18:34:06 +0000

    cf0968f build(docs-infra): support hiding constructors of injectable classes (#24529)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+33.sha-cf0968f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+var VERSION = new Version('6.1.0-beta.2+33.sha-cf0968f');
 
 /**
  * @license
@@ -41872,7 +41872,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+32.sha-855e8ad');
+var VERSION$2 = new Version$1('6.1.0-beta.2+33.sha-cf0968f');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54840,7 +54840,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+32.sha-855e8ad');
+var VERSION$3 = new Version$1('6.1.0-beta.2+33.sha-cf0968f');
 
 /**
  * @license
