f8840b4624995aeac49afe1466bf9dc14430b127
Jason Aden <jasonaden@google.com>
2018-06-27 22:14:57 +0000

    8dec381 docs: fix unit tests in toh-pt6 (#24491)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+42.sha-32da3e1
+ * @license Angular v6.1.0-beta.2+43.sha-8dec381
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+42.sha-32da3e1');
+var VERSION = new Version('6.1.0-beta.2+43.sha-8dec381');
 
 /**
  * @license
@@ -41885,7 +41885,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+42.sha-32da3e1');
+var VERSION$2 = new Version$1('6.1.0-beta.2+43.sha-8dec381');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54853,7 +54853,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+42.sha-32da3e1');
+var VERSION$3 = new Version$1('6.1.0-beta.2+43.sha-8dec381');
 
 /**
  * @license
