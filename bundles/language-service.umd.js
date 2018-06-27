daf700e8f383b7690502c500d31b3b5f98e9411f
Jason Aden <jasonaden@google.com>
2018-06-27 22:39:23 +0000

    39c8bae fix(common): use correct ICU plural for locale mk (#24659) PR Close #24659

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+53.sha-abed2cd
+ * @license Angular v6.1.0-beta.2+54.sha-39c8bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+53.sha-abed2cd');
+var VERSION = new Version('6.1.0-beta.2+54.sha-39c8bae');
 
 /**
  * @license
@@ -24480,7 +24480,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+53.sha-abed2cd');
+var VERSION$2 = new Version$1('6.1.0-beta.2+54.sha-39c8bae');
 
 /**
  * @license
@@ -49602,7 +49602,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+53.sha-abed2cd');
+var VERSION$3 = new Version$1('6.1.0-beta.2+54.sha-39c8bae');
 
 /**
  * @license
