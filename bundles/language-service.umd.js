61744d2d9b953cca97b80c3843eaada5b0ab02a5
Jason Aden <jasonaden@google.com>
2018-06-27 22:13:29 +0000

    32da3e1 docs: add explanation for `enableResourceInlining` (#24644)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+41.sha-6d68f3e
+ * @license Angular v6.1.0-beta.2+42.sha-32da3e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+41.sha-6d68f3e');
+var VERSION = new Version('6.1.0-beta.2+42.sha-32da3e1');
 
 /**
  * @license
@@ -24480,7 +24480,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+41.sha-6d68f3e');
+var VERSION$2 = new Version$1('6.1.0-beta.2+42.sha-32da3e1');
 
 /**
  * @license
@@ -49602,7 +49602,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+41.sha-6d68f3e');
+var VERSION$3 = new Version$1('6.1.0-beta.2+42.sha-32da3e1');
 
 /**
  * @license
