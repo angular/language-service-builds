6fd82bf4c3fb8ddb9056702e06d8f59e9a165b90
Jason Aden <jasonaden@google.com>
2018-06-28 01:23:47 +0000

    1dc7d0d release: cut the v6.0.7 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+54.sha-39c8bae
+ * @license Angular v6.1.0-beta.2+55.sha-1dc7d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1162,7 +1162,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+54.sha-39c8bae');
+var VERSION = new Version('6.1.0-beta.2+55.sha-1dc7d0d');
 
 /**
  * @license
@@ -24480,7 +24480,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+54.sha-39c8bae');
+var VERSION$2 = new Version$1('6.1.0-beta.2+55.sha-1dc7d0d');
 
 /**
  * @license
@@ -49602,7 +49602,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+54.sha-39c8bae');
+var VERSION$3 = new Version$1('6.1.0-beta.2+55.sha-1dc7d0d');
 
 /**
  * @license
