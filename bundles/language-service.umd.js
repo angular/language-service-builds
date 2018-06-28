41e5a97cc69f32c302954c7b0f32a8f7096c559e
Jason Aden <jasonaden@google.com>
2018-06-28 02:02:25 +0000

    c57b491 release: cut the v6.1.0-beta.3 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+55.sha-1dc7d0d
+ * @license Angular v6.1.0-beta.2+56.sha-c57b491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1204,7 +1204,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+55.sha-1dc7d0d');
+var VERSION = new Version('6.1.0-beta.2+56.sha-c57b491');
 
 /**
  * @license
@@ -41885,7 +41885,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.2+55.sha-1dc7d0d');
+var VERSION$2 = new Version$1('6.1.0-beta.2+56.sha-c57b491');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -54853,7 +54853,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.2+55.sha-1dc7d0d');
+var VERSION$3 = new Version$1('6.1.0-beta.2+56.sha-c57b491');
 
 /**
  * @license
