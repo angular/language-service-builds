5fd6bd0d22184ac808d72ba996fb6650d71ddbab
Miško Hevery <misko@hevery.com>
2018-07-03 21:03:18 +0000

    0c3738a feat(ivy): support templateUrl for ngtsc (#24704)

--- language-service.d.ts
+++ language-service.d.ts
@@ -1,3 +1,4 @@
+/// <amd-module name="@angular/language-service/language-service" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -5,7 +6,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="@angular/language-service/language-service" />
 /**
  * @module
  * @description
--- src/hover.d.ts
+++ src/hover.d.ts
@@ -1,3 +1,4 @@
+/// <amd-module name="@angular/language-service/src/hover" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -5,7 +6,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="@angular/language-service/src/hover" />
 import { TemplateInfo } from './common';
 import { Hover } from './types';
 export declare function getHover(info: TemplateInfo): Hover | undefined;
