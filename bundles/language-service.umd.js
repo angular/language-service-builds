fdb11a88814634790cb2b922863c0a50a2485490
Miško Hevery <misko@hevery.com>
2018-07-03 16:26:30 +0000

    0922228 fix(core): use addCustomEqualityTester instead of overriding toEqual (#22983)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
+ * @license Angular v6.1.0-beta.3+28.sha-0922228
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+27.sha-c94a2c9');
+var VERSION = new Version('6.1.0-beta.3+28.sha-0922228');
 
 /**
  * @license
@@ -42103,7 +42103,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.1.0-beta.3+27.sha-c94a2c9');
+var VERSION$2 = new Version$1('6.1.0-beta.3+28.sha-0922228');
 
 var __extends$34 = (undefined && undefined.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
@@ -55071,7 +55071,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.1.0-beta.3+27.sha-c94a2c9');
+var VERSION$3 = new Version$1('6.1.0-beta.3+28.sha-0922228');
 
 /**
  * @license
