b07b20b5c4fd795734151b5b6c871251828d6810
Miško Hevery <misko@hevery.com>
2018-07-03 21:01:05 +0000

    e3064d5 feat: typescript 2.9 support (#24652)

--- src/diagnostics.d.ts
+++ src/diagnostics.d.ts
@@ -1,4 +1,3 @@
-/// <amd-module name="@angular/language-service/src/diagnostics" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -6,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <amd-module name="@angular/language-service/src/diagnostics" />
 import { NgAnalyzedModules } from '@angular/compiler';
 import { AstResult } from './common';
 import { Declarations, Diagnostics, TemplateSource } from './types';
