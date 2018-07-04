d251e4c1e9b60ee29f307e4d97d79413f871201d
Miško Hevery <misko@hevery.com>
2018-07-04 00:13:51 +0000

    b6af870 feat(ivy): AOT support for compilation of @Pipes (#24703)

--- src/expressions.d.ts
+++ src/expressions.d.ts
@@ -1,4 +1,3 @@
-/// <amd-module name="@angular/language-service/src/expressions" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -6,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <amd-module name="@angular/language-service/src/expressions" />
 import { AST } from '@angular/compiler';
 import { Span, Symbol, SymbolQuery, SymbolTable } from './types';
 export declare function getExpressionCompletions(scope: SymbolTable, ast: AST, position: number, query: SymbolQuery): Symbol[] | undefined;
