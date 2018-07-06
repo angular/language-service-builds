c3454568b58553e0103db5e8215218713bb7a9e7
Miško Hevery <misko@hevery.com>
2018-07-06 21:33:12 +0000

    f936b8c docs: refactored ng-container code (#22742)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+47.sha-1e74ea9
+ * @license Angular v6.1.0-beta.3+56.sha-f936b8c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1185,7 +1185,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+47.sha-1e74ea9');
+    var VERSION = new Version('6.1.0-beta.3+56.sha-f936b8c');
 
     /**
      * @license
@@ -40426,7 +40426,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+47.sha-1e74ea9');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+56.sha-f936b8c');
 
     var __extends$y = (undefined && undefined.__extends) || (function () {
         var extendStatics = Object.setPrototypeOf ||
@@ -50376,7 +50376,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+47.sha-1e74ea9');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+56.sha-f936b8c');
 
     /**
      * @license
