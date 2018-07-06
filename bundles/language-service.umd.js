0e03770970029ae12db5d6ef29e8e06ad58fc3be
Miško Hevery <misko@hevery.com>
2018-07-06 18:19:10 +0000

    bc2bf18 docs: describe rounding behaviour of 'DecimalPipe' (#24303)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+18.sha-25b532e
+ * @license Angular v6.0.7+20.sha-bc2bf18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -391,7 +391,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+18.sha-25b532e');
+var VERSION = new Version('6.0.7+20.sha-bc2bf18');
 
 /**
  * @license
@@ -24198,7 +24198,7 @@ var Version$1 = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION$2 = new Version$1('6.0.7+18.sha-25b532e');
+var VERSION$2 = new Version$1('6.0.7+20.sha-bc2bf18');
 
 /**
  * @license
@@ -48335,7 +48335,7 @@ function create(info /* ts.server.PluginCreateInfo */) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION$3 = new Version$1('6.0.7+18.sha-25b532e');
+var VERSION$3 = new Version$1('6.0.7+20.sha-bc2bf18');
 
 /**
  * @license
