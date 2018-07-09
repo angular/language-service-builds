a660a8788898df1501ed8efde2bca68868e984b2
Miško Hevery <misko@hevery.com>
2018-07-09 23:00:52 +0000

    ddb792d build: remove unnecessary `internal-angular` karma reporter (#24803)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+64.sha-3d20c50
+ * @license Angular v6.1.0-beta.3+66.sha-ddb792d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+64.sha-3d20c50');
+    var VERSION = new Version('6.1.0-beta.3+66.sha-ddb792d');
 
     /**
      * @license
@@ -24197,7 +24197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+64.sha-3d20c50');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+66.sha-ddb792d');
 
     /**
      * @license
@@ -43952,7 +43952,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+64.sha-3d20c50');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+66.sha-ddb792d');
 
     /**
      * @license
