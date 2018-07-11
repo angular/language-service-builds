1675cf7c9aa7e7e484f38a760fb1ce8f51076ada
Miško Hevery <misko@hevery.com>
2018-07-11 03:07:23 +0000

    81a9db2 docs(forms): added missing backtick (#24451)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+87.sha-05e3e4d
+ * @license Angular v6.1.0-beta.3+86.sha-81a9db2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1186,7 +1186,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+87.sha-05e3e4d');
+    var VERSION = new Version('6.1.0-beta.3+86.sha-81a9db2');
 
     /**
      * @license
@@ -41036,7 +41036,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+87.sha-05e3e4d');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+86.sha-81a9db2');
 
     var __extends$y = (undefined && undefined.__extends) || (function () {
         var extendStatics = Object.setPrototypeOf ||
@@ -50986,7 +50986,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+87.sha-05e3e4d');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+86.sha-81a9db2');
 
     /**
      * @license
