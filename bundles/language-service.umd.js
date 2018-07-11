c9c737a44589e8214818e32a8d51e564fcf2205c
Miško Hevery <misko@hevery.com>
2018-07-11 02:55:37 +0000

    a527c69 fix(common): do not round factional seconds (#24831)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+79.sha-8057664
+ * @license Angular v6.1.0-beta.3+80.sha-a527c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1186,7 +1186,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+79.sha-8057664');
+    var VERSION = new Version('6.1.0-beta.3+80.sha-a527c69');
 
     /**
      * @license
@@ -41036,7 +41036,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+79.sha-8057664');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+80.sha-a527c69');
 
     var __extends$y = (undefined && undefined.__extends) || (function () {
         var extendStatics = Object.setPrototypeOf ||
@@ -50986,7 +50986,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+79.sha-8057664');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+80.sha-a527c69');
 
     /**
      * @license
