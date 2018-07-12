6ffbb02dd74ea49cb9dc78326187ed55b81d1110
Miško Hevery <misko@hevery.com>
2018-07-12 16:50:23 +0000

    52d11f6 release: cut the v6.0.9 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+89.sha-0b4d85e
+ * @license Angular v6.1.0-beta.3+91.sha-52d11f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+89.sha-0b4d85e');
+    var VERSION = new Version('6.1.0-beta.3+91.sha-52d11f6');
 
     /**
      * @license
@@ -24197,7 +24197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+89.sha-0b4d85e');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+91.sha-52d11f6');
 
     /**
      * @license
@@ -43958,7 +43958,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+89.sha-0b4d85e');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+91.sha-52d11f6');
 
     /**
      * @license
