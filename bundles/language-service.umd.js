b8f8fe0454545ab79cd78b7f9b1f167ef4ae9127
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 20:36:33 +0000

    9be8abd build: disable IE web worker tests (#24908)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+115.sha-74b250b
+ * @license Angular v6.1.0-beta.3+116.sha-9be8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+115.sha-74b250b');
+    var VERSION = new Version('6.1.0-beta.3+116.sha-9be8abd');
 
     /**
      * @license
@@ -24197,7 +24197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+115.sha-74b250b');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+116.sha-9be8abd');
 
     /**
      * @license
@@ -43966,7 +43966,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+115.sha-74b250b');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+116.sha-9be8abd');
 
     /**
      * @license
