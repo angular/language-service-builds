70184114c7d0f5613522fddba5ce89fc3b9c0841
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 21:48:30 +0000

    c8ad965 fix(compiler): i18n_extractor now outputs the correct source file name (#24885) for non-inline templates

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+108.sha-80a74b4
+ * @license Angular v6.1.0-beta.3+117.sha-c8ad965
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+108.sha-80a74b4');
+    var VERSION = new Version('6.1.0-beta.3+117.sha-c8ad965');
 
     /**
      * @license
@@ -24197,7 +24197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+108.sha-80a74b4');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+117.sha-c8ad965');
 
     /**
      * @license
@@ -43966,7 +43966,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+108.sha-80a74b4');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+117.sha-c8ad965');
 
     /**
      * @license
