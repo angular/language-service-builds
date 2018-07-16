a7a72257af723250a2e194191cbf6724551f0084
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 13:09:19 +0000

    ae04929 feat(ivy): implement initial ngcc package transformer

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-31b4043
+ * @license Angular v6.1.0-beta.3+129.sha-ae04929
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
+    var VERSION = new Version('6.1.0-beta.3+129.sha-ae04929');
 
     /**
      * @license
@@ -24197,7 +24197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+130.sha-31b4043');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+129.sha-ae04929');
 
     /**
      * @license
@@ -43966,7 +43966,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+130.sha-31b4043');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+129.sha-ae04929');
 
     /**
      * @license
