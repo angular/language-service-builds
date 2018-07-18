5d3bca614485cdd028d87e24eb1c1a30f8aa1ff6
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 22:54:25 +0000

    d76531d fix(animations): @internal must use JSDoc tags. (#24928)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
+ * @license Angular v6.1.0-beta.3+122.sha-d76531d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1186,7 +1186,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-beta.3+121.sha-23dc9a9');
+    var VERSION = new Version('6.1.0-beta.3+122.sha-d76531d');
 
     /**
      * @license
@@ -41276,7 +41276,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-beta.3+121.sha-23dc9a9');
+    var VERSION$2 = new Version$1('6.1.0-beta.3+122.sha-d76531d');
 
     var __extends$y = (undefined && undefined.__extends) || (function () {
         var extendStatics = Object.setPrototypeOf ||
@@ -51226,7 +51226,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-beta.3+121.sha-23dc9a9');
+    var VERSION$3 = new Version$1('6.1.0-beta.3+122.sha-d76531d');
 
     /**
      * @license
