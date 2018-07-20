e52be10778a9e31f90d7c78e0a5fccd8abdb8b98
Victor Berchet <victor@suumit.com>
2018-07-20 18:39:05 +0000

    2aab1c9 ci: remove Tina from pullaprove (#25006)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3
+ * @license Angular v6.1.0-rc.3+1.sha-2aab1c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-rc.3');
+    var VERSION = new Version('6.1.0-rc.3+1.sha-2aab1c9');
 
     /**
      * @license
@@ -24196,7 +24196,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-rc.3');
+    var VERSION$2 = new Version$1('6.1.0-rc.3+1.sha-2aab1c9');
 
     /**
      * @license
@@ -43972,7 +43972,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-rc.3');
+    var VERSION$3 = new Version$1('6.1.0-rc.3+1.sha-2aab1c9');
 
     /**
      * @license
