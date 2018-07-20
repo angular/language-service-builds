dfaa0d5bf8f4531f63855bdb448071ad5928f7d9
Victor Berchet <victor@suumit.com>
2018-07-20 19:24:48 +0000

    2a5a05b fix(docs-infra): fix table header layout in API pages (#24919)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+25.sha-4a29100
+ * @license Angular v6.0.9+26.sha-2a5a05b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -371,7 +371,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.0.9+25.sha-4a29100');
+    var VERSION = new Version('6.0.9+26.sha-2a5a05b');
 
     /**
      * @license
@@ -23998,7 +23998,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.0.9+25.sha-4a29100');
+    var VERSION$2 = new Version$1('6.0.9+26.sha-2a5a05b');
 
     /**
      * @license
@@ -42853,7 +42853,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.0.9+25.sha-4a29100');
+    var VERSION$3 = new Version$1('6.0.9+26.sha-2a5a05b');
 
     /**
      * @license
