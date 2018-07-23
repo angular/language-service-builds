eb193b1498dee29bb4990bd46241b222f720163f
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-23 07:18:18 +0000

    0650f32 fixup! build: add dependencies to be used by ngcc

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+70.sha-8a7b0e9
+ * @license Angular v6.1.0-rc.3+71.sha-0650f32
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-rc.3+70.sha-8a7b0e9');
+    var VERSION = new Version('6.1.0-rc.3+71.sha-0650f32');
 
     /**
      * @license
@@ -24253,7 +24253,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-rc.3+70.sha-8a7b0e9');
+    var VERSION$2 = new Version$1('6.1.0-rc.3+71.sha-0650f32');
 
     /**
      * @license
@@ -44110,7 +44110,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-rc.3+70.sha-8a7b0e9');
+    var VERSION$3 = new Version$1('6.1.0-rc.3+71.sha-0650f32');
 
     /**
      * @license
