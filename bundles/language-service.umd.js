15a749253de20e3286615bfc1e217e6eaed06857
Victor Berchet <victor@suumit.com>
2018-07-25 04:30:46 +0000

    76fb905 docs: replaced old angular/http example (#25065) PR Close #25065

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+45.sha-93d325c
+ * @license Angular v6.0.9+46.sha-76fb905
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -371,7 +371,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.0.9+45.sha-93d325c');
+    var VERSION = new Version('6.0.9+46.sha-76fb905');
 
     /**
      * @license
@@ -24009,7 +24009,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.0.9+45.sha-93d325c');
+    var VERSION$2 = new Version$1('6.0.9+46.sha-76fb905');
 
     /**
      * @license
@@ -42940,7 +42940,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.0.9+45.sha-93d325c');
+    var VERSION$3 = new Version$1('6.0.9+46.sha-76fb905');
 
     /**
      * @license
