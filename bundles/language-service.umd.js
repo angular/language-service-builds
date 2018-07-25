ec8a1845eecc01ca4dd32b9b3c5f4215349dd21a
Victor Berchet <victor@suumit.com>
2018-07-25 22:29:23 +0000

    48d7205 release: cut the v6.1.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
+ * @license Angular v6.1.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0-rc.3+75.sha-e1c6fd5');
+    var VERSION = new Version('6.1.0');
 
     /**
      * @license
@@ -24262,7 +24262,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0-rc.3+75.sha-e1c6fd5');
+    var VERSION$2 = new Version$1('6.1.0');
 
     /**
      * @license
@@ -44303,7 +44303,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0-rc.3+75.sha-e1c6fd5');
+    var VERSION$3 = new Version$1('6.1.0');
 
     /**
      * @license
