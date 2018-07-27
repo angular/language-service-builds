0376c78658819f1325064a17bdf6e3e20bc5a378
Igor Minar <igor@angular.io>
2018-07-27 17:55:24 +0000

    a3cf61b docs: refactor feature modules example (#25069)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+14.sha-a1b185b
+ * @license Angular v6.1.0+15.sha-a3cf61b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+14.sha-a1b185b');
+    var VERSION = new Version('6.1.0+15.sha-a3cf61b');
 
     /**
      * @license
@@ -45903,7 +45903,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+14.sha-a1b185b');
+    var VERSION$2 = new Version$1('6.1.0+15.sha-a3cf61b');
 
     /**
      * @license
@@ -50621,7 +50621,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+14.sha-a1b185b');
+    var VERSION$3 = new Version$1('6.1.0+15.sha-a3cf61b');
 
     /**
      * @license
