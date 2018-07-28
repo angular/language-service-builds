75413cafff00a39c4926b4cfedeca80d0dd0c429
Igor Minar <igor@angular.io>
2018-07-28 00:55:22 +0000

    a87d44c refactor(ivy): do not deep import from ngtsc into ngcc (#24897)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+25.sha-8e65891
+ * @license Angular v6.1.0+40.sha-a87d44c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+25.sha-8e65891');
+    var VERSION = new Version('6.1.0+40.sha-a87d44c');
 
     /**
      * @license
@@ -24268,7 +24268,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+25.sha-8e65891');
+    var VERSION$2 = new Version$1('6.1.0+40.sha-a87d44c');
 
     /**
      * @license
@@ -42672,7 +42672,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+25.sha-8e65891');
+    var VERSION$3 = new Version$1('6.1.0+40.sha-a87d44c');
 
     /**
      * @license
