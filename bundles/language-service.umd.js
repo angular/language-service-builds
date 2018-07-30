a99f0f82b4a253eaf15933e4d1f86ba203363650
Igor Minar <igor@angular.io>
2018-07-30 22:21:58 +0000

    9a6d26e docs: refactor pipe example to use the HttpClient (#22741)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+43.sha-6a797d5
+ * @license Angular v6.1.0+44.sha-9a6d26e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+43.sha-6a797d5');
+    var VERSION = new Version('6.1.0+44.sha-9a6d26e');
 
     /**
      * @license
@@ -46166,7 +46166,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+43.sha-6a797d5');
+    var VERSION$2 = new Version$1('6.1.0+44.sha-9a6d26e');
 
     /**
      * @license
@@ -50613,7 +50613,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+43.sha-6a797d5');
+    var VERSION$3 = new Version$1('6.1.0+44.sha-9a6d26e');
 
     /**
      * @license
