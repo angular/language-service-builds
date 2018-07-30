69f8cc2ea63419cb4d9654eb8f40af4f33b6779e
Igor Minar <igor@angular.io>
2018-07-30 23:40:59 +0000

    4d5167e docs: update bootstrapping and entry component guide to use httpclient (#25178)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+47.sha-2ef777b
+ * @license Angular v6.1.0+49.sha-4d5167e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+47.sha-2ef777b');
+    var VERSION = new Version('6.1.0+49.sha-4d5167e');
 
     /**
      * @license
@@ -46351,7 +46351,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+47.sha-2ef777b');
+    var VERSION$2 = new Version$1('6.1.0+49.sha-4d5167e');
 
     /**
      * @license
@@ -50798,7 +50798,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+47.sha-2ef777b');
+    var VERSION$3 = new Version$1('6.1.0+49.sha-4d5167e');
 
     /**
      * @license
