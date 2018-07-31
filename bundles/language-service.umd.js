158173da9598faddf4512ae796e21edd4c2387e9
Igor Minar <igor@angular.io>
2018-07-31 21:24:19 +0000

    4cb1074 docs(aio): add short description for entryComponents (#21360)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+58.sha-5254d34
+ * @license Angular v6.1.0+61.sha-4cb1074
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+58.sha-5254d34');
+    var VERSION = new Version('6.1.0+61.sha-4cb1074');
 
     /**
      * @license
@@ -24349,7 +24349,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+58.sha-5254d34');
+    var VERSION$2 = new Version$1('6.1.0+61.sha-4cb1074');
 
     /**
      * @license
@@ -43520,7 +43520,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+58.sha-5254d34');
+    var VERSION$3 = new Version$1('6.1.0+61.sha-4cb1074');
 
     /**
      * @license
