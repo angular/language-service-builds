ee2bbbdd8fc310be4c2ea0ce6bae0c53c2bcfec6
Igor Minar <igor@angular.io>
2018-07-31 00:07:03 +0000

    efc6684 docs: fix typo in dependency injection guide (#24972)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+49.sha-4d5167e
+ * @license Angular v6.1.0+48.sha-efc6684
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+49.sha-4d5167e');
+    var VERSION = new Version('6.1.0+48.sha-efc6684');
 
     /**
      * @license
@@ -24335,7 +24335,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+49.sha-4d5167e');
+    var VERSION$2 = new Version$1('6.1.0+48.sha-efc6684');
 
     /**
      * @license
@@ -42745,7 +42745,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+49.sha-4d5167e');
+    var VERSION$3 = new Version$1('6.1.0+48.sha-efc6684');
 
     /**
      * @license
