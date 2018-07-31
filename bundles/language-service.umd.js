b08a47fe2522996495fbc5319359649f0d0e9744
Igor Minar <igor@angular.io>
2018-07-31 19:51:39 +0000

    875efa8 docs(docs-infra): fix topnav layout for smaller screens (#25181)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+17.sha-74964bd
+ * @license Angular v6.1.0+18.sha-875efa8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+17.sha-74964bd');
+    var VERSION = new Version('6.1.0+18.sha-875efa8');
 
     /**
      * @license
@@ -45895,7 +45895,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+17.sha-74964bd');
+    var VERSION$2 = new Version$1('6.1.0+18.sha-875efa8');
 
     /**
      * @license
@@ -50613,7 +50613,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+17.sha-74964bd');
+    var VERSION$3 = new Version$1('6.1.0+18.sha-875efa8');
 
     /**
      * @license
