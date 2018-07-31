cd5d7a0df3453faae5664f0c06a176810351e062
Igor Minar <igor@angular.io>
2018-07-31 00:52:46 +0000

    3664829 refactor(ivy): misc (#25174)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+52.sha-e99d860
+ * @license Angular v6.1.0+54.sha-3664829
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+52.sha-e99d860');
+    var VERSION = new Version('6.1.0+54.sha-3664829');
 
     /**
      * @license
@@ -28615,7 +28615,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             null,
             null,
             context,
-            viewData && viewData[INJECTOR$1],
+            viewData ? viewData[INJECTOR$1] : null,
             renderer,
             sanitizer || null,
             null,
@@ -39128,19 +39128,66 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             lContainerNode.tNode = hostTNode.dynamicContainerNode;
             vcRefHost.dynamicLContainerNode = lContainerNode;
             addToViewTree(vcRefHost.view, hostTNode.index, lContainer);
-            di.viewContainerRef = new ViewContainerRef$1(lContainerNode);
+            di.viewContainerRef = new ViewContainerRef$1(lContainerNode, vcRefHost);
         }
         return di.viewContainerRef;
     }
+    var NodeInjector = /** @class */ (function () {
+        function NodeInjector(_lInjector) {
+            this._lInjector = _lInjector;
+        }
+        NodeInjector.prototype.get = function (token) {
+            if (token === TemplateRef) {
+                return getOrCreateTemplateRef(this._lInjector);
+            }
+            if (token === ViewContainerRef) {
+                return getOrCreateContainerRef(this._lInjector);
+            }
+            if (token === ElementRef) {
+                return getOrCreateElementRef(this._lInjector);
+            }
+            if (token === ChangeDetectorRef) {
+                return getOrCreateChangeDetectorRef(this._lInjector, null);
+            }
+            return getOrCreateInjectable(this._lInjector, token);
+        };
+        return NodeInjector;
+    }());
     /**
      * A ref to a container that enables adding and removing views from that container
      * imperatively.
      */
     var ViewContainerRef$1 = /** @class */ (function () {
-        function ViewContainerRef$$1(_lContainerNode) {
+        function ViewContainerRef$$1(_lContainerNode, _hostNode) {
             this._lContainerNode = _lContainerNode;
+            this._hostNode = _hostNode;
             this._viewRefs = [];
         }
+        Object.defineProperty(ViewContainerRef$$1.prototype, "element", {
+            get: function () {
+                var injector = getOrCreateNodeInjectorForNode(this._hostNode);
+                return getOrCreateElementRef(injector);
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ViewContainerRef$$1.prototype, "injector", {
+            get: function () {
+                var injector = getOrCreateNodeInjectorForNode(this._hostNode);
+                return new NodeInjector(injector);
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(ViewContainerRef$$1.prototype, "parentInjector", {
+            /** @deprecated No replacement */
+            get: function () {
+                var parentLInjector = getParentLNode(this._hostNode).nodeInjector;
+                return parentLInjector ? new NodeInjector(parentLInjector) : new NullInjector();
+            },
+            enumerable: true,
+            configurable: true
+        });
         ViewContainerRef$$1.prototype.clear = function () {
             var lContainer = this._lContainerNode.data;
             while (lContainer[VIEWS].length) {
@@ -39167,7 +39214,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         ViewContainerRef$$1.prototype.createComponent = function (componentFactory, index, injector, projectableNodes, ngModuleRef) {
             var contextInjector = injector || this.parentInjector;
             if (!ngModuleRef && contextInjector) {
-                ngModuleRef = contextInjector.get(NgModuleRef);
+                ngModuleRef = contextInjector.get(NgModuleRef, null);
             }
             var componentRef = componentFactory.create(contextInjector, projectableNodes, undefined, ngModuleRef);
             this.insert(componentRef.hostView, index);
@@ -46365,7 +46412,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+52.sha-e99d860');
+    var VERSION$2 = new Version$1('6.1.0+54.sha-3664829');
 
     /**
      * @license
@@ -47332,6 +47379,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             configurable: true
         });
         Object.defineProperty(ViewContainerRef_.prototype, "parentInjector", {
+            /** @deprecated No replacement */
             get: function () {
                 var view = this._view;
                 var elDef = this._elDef.parent;
@@ -50812,7 +50860,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+52.sha-e99d860');
+    var VERSION$3 = new Version$1('6.1.0+54.sha-3664829');
 
     /**
      * @license
