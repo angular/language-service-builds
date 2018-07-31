06af9e86d1a728def50b3e89bcb3e718cc5968b0
Igor Minar <igor@angular.io>
2018-07-31 00:37:18 +0000

    785fb5c docs(aio): add StrongBrew to the trainer list (#24891)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+14.sha-22ebd53
+ * @license Angular v6.1.0+16.sha-785fb5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+14.sha-22ebd53');
+    var VERSION = new Version('6.1.0+16.sha-785fb5c');
 
     /**
      * @license
@@ -24268,7 +24268,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+14.sha-22ebd53');
+    var VERSION$2 = new Version$1('6.1.0+16.sha-785fb5c');
 
     /**
      * @license
@@ -42672,7 +42672,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+14.sha-22ebd53');
+    var VERSION$3 = new Version$1('6.1.0+16.sha-785fb5c');
 
     /**
      * @license
