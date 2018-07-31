566527198f44703ef950055850de5a2302ac0fb7
Igor Minar <igor@angular.io>
2018-07-31 22:46:43 +0000

    d4bf2da refactor(core): remove withBody from public testing API (#25171)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+20.sha-fa59748
+ * @license Angular v6.1.0+22.sha-d4bf2da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+20.sha-fa59748');
+    var VERSION = new Version('6.1.0+22.sha-d4bf2da');
 
     /**
      * @license
@@ -45895,7 +45895,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+20.sha-fa59748');
+    var VERSION$2 = new Version$1('6.1.0+22.sha-d4bf2da');
 
     /**
      * @license
@@ -50613,7 +50613,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+20.sha-fa59748');
+    var VERSION$3 = new Version$1('6.1.0+22.sha-d4bf2da');
 
     /**
      * @license
