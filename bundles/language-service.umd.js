8ecb4a04b514d2115f42f6101425b5b0bae83f35
Igor Minar <igor@angular.io>
2018-08-01 20:22:31 +0000

    a9ecf4b docs: refactor lazy loading modules example (#25071)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+75.sha-01e6dab
+ * @license Angular v6.1.0+74.sha-a9ecf4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+75.sha-01e6dab');
+    var VERSION = new Version('6.1.0+74.sha-a9ecf4b');
 
     /**
      * @license
@@ -24429,7 +24429,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+75.sha-01e6dab');
+    var VERSION$2 = new Version$1('6.1.0+74.sha-a9ecf4b');
 
     /**
      * @license
@@ -43608,7 +43608,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+75.sha-01e6dab');
+    var VERSION$3 = new Version$1('6.1.0+74.sha-a9ecf4b');
 
     /**
      * @license
