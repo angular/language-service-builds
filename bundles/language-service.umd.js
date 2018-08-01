9d10de883d10cda34d04b095adf0c3d900c4111a
Igor Minar <igor@angular.io>
2018-08-01 21:23:10 +0000

    7a1fdde build(bazel): entry point file couldn't be resolved [ts-api-guardian] (#25052)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+39.sha-cbc2ea1
+ * @license Angular v6.1.0+40.sha-7a1fdde
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+39.sha-cbc2ea1');
+    var VERSION = new Version('6.1.0+40.sha-7a1fdde');
 
     /**
      * @license
@@ -47529,7 +47529,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+39.sha-cbc2ea1');
+    var VERSION$2 = new Version$1('6.1.0+40.sha-7a1fdde');
 
     /**
      * @license
@@ -52247,7 +52247,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+39.sha-cbc2ea1');
+    var VERSION$3 = new Version$1('6.1.0+40.sha-7a1fdde');
 
     /**
      * @license
