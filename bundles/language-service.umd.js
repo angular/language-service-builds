656bc0b2fa1fdfbe9a84ebbca91b3dda48a19490
Igor Minar <igor@angular.io>
2018-08-01 20:12:25 +0000

    d5b7383 refactor(animations): do not use short parameter names (#25198)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+69.sha-7075c41
+ * @license Angular v6.1.0+70.sha-d5b7383
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+69.sha-7075c41');
+    var VERSION = new Version('6.1.0+70.sha-d5b7383');
 
     /**
      * @license
@@ -46536,7 +46536,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+69.sha-7075c41');
+    var VERSION$2 = new Version$1('6.1.0+70.sha-d5b7383');
 
     /**
      * @license
@@ -50984,7 +50984,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+69.sha-7075c41');
+    var VERSION$3 = new Version$1('6.1.0+70.sha-d5b7383');
 
     /**
      * @license
