691bc0a84cd125f89eafcf3e48f7db3bc9be53a1
Igor Minar <igor@angular.io>
2018-08-01 20:48:38 +0000

    5342aea docs(aio): update Kendo UI description in resource.json (#24845)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+80.sha-1dd2eaa
+ * @license Angular v6.1.0+81.sha-5342aea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+80.sha-1dd2eaa');
+    var VERSION = new Version('6.1.0+81.sha-5342aea');
 
     /**
      * @license
@@ -46533,7 +46533,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+80.sha-1dd2eaa');
+    var VERSION$2 = new Version$1('6.1.0+81.sha-5342aea');
 
     /**
      * @license
@@ -50981,7 +50981,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+80.sha-1dd2eaa');
+    var VERSION$3 = new Version$1('6.1.0+81.sha-5342aea');
 
     /**
      * @license
