3e10f5bea4f6113905df10d0d11431874bec54da
Igor Minar <igor@angular.io>
2018-08-01 20:17:45 +0000

    3d3a1a4 docs(aio): add Kevin Yang to GDE resources (#24791)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+31.sha-0452712
+ * @license Angular v6.1.0+33.sha-3d3a1a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+31.sha-0452712');
+    var VERSION = new Version('6.1.0+33.sha-3d3a1a4');
 
     /**
      * @license
@@ -24268,7 +24268,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+31.sha-0452712');
+    var VERSION$2 = new Version$1('6.1.0+33.sha-3d3a1a4');
 
     /**
      * @license
@@ -42672,7 +42672,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+31.sha-0452712');
+    var VERSION$3 = new Version$1('6.1.0+33.sha-3d3a1a4');
 
     /**
      * @license
