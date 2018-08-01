fd94f48ce57eab1a7cd794afbeb5e52c808262ee
Igor Minar <igor@angular.io>
2018-08-01 20:21:57 +0000

    01e6dab fix(compiler-cli): correct realPath to realpath. (#25023)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+72.sha-1e53278
+ * @license Angular v6.1.0+75.sha-01e6dab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+72.sha-1e53278');
+    var VERSION = new Version('6.1.0+75.sha-01e6dab');
 
     /**
      * @license
@@ -24429,7 +24429,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+72.sha-1e53278');
+    var VERSION$2 = new Version$1('6.1.0+75.sha-01e6dab');
 
     /**
      * @license
@@ -43608,7 +43608,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+72.sha-1e53278');
+    var VERSION$3 = new Version$1('6.1.0+75.sha-01e6dab');
 
     /**
      * @license
