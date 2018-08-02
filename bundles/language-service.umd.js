d6be330db7ea42ce4e73e53c39f5837797514671
Igor Minar <igor@angular.io>
2018-08-02 16:28:43 +0000

    0a6434b test(common): TokenExtractor should extend HttpXsrfTokenExtractor in xsrf spec (#24649)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+48.sha-9ddf269
+ * @license Angular v6.1.0+51.sha-0a6434b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+48.sha-9ddf269');
+    var VERSION = new Version('6.1.0+51.sha-0a6434b');
 
     /**
      * @license
@@ -47529,7 +47529,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+48.sha-9ddf269');
+    var VERSION$2 = new Version$1('6.1.0+51.sha-0a6434b');
 
     /**
      * @license
@@ -52247,7 +52247,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+48.sha-9ddf269');
+    var VERSION$3 = new Version$1('6.1.0+51.sha-0a6434b');
 
     /**
      * @license
