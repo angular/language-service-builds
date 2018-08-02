572587f626349ddfb0df82c2ea7dd220e5851a92
Igor Minar <igor@angular.io>
2018-08-02 17:47:22 +0000

    183757d fix(core): fix tests for uninitialized @Output error (#19116)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+98.sha-74bce18
+ * @license Angular v6.1.0+100.sha-183757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+98.sha-74bce18');
+    var VERSION = new Version('6.1.0+100.sha-183757d');
 
     /**
      * @license
@@ -45243,6 +45243,14 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         // It's up to the caller to ensure that obj.then conforms to the spec
         return !!obj && typeof obj.then === 'function';
     }
+    /**
+     * Determine if the argument is an Observable
+     */
+    function isObservable$1(obj) {
+        // TODO: use isObservable once we update pass rxjs 6.1
+        // https://github.com/ReactiveX/rxjs/blob/master/CHANGELOG.md#610-2018-05-03
+        return !!obj && typeof obj.subscribe === 'function';
+    }
 
     /**
      * @license
@@ -48170,7 +48178,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+98.sha-74bce18');
+    var VERSION$2 = new Version$1('6.1.0+100.sha-183757d');
 
     /**
      * @license
@@ -49516,8 +49524,14 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         if (def.outputs.length) {
             for (var i = 0; i < def.outputs.length; i++) {
                 var output = def.outputs[i];
-                var subscription = instance[output.propName].subscribe(eventHandlerClosure(view, def.parent.nodeIndex, output.eventName));
-                view.disposables[def.outputIndex + i] = subscription.unsubscribe.bind(subscription);
+                var outputObservable = instance[output.propName];
+                if (isObservable$1(outputObservable)) {
+                    var subscription = outputObservable.subscribe(eventHandlerClosure(view, def.parent.nodeIndex, output.eventName));
+                    view.disposables[def.outputIndex + i] = subscription.unsubscribe.bind(subscription);
+                }
+                else {
+                    throw new Error("@Output " + output.propName + " not initialized in '" + instance.constructor.name + "'.");
+                }
             }
         }
         return instance;
@@ -52618,7 +52632,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+98.sha-74bce18');
+    var VERSION$3 = new Version$1('6.1.0+100.sha-183757d');
 
     /**
      * @license
