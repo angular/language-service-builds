44e34b256fb5a10e92835cc7ac0f16d7cd3fe7ca
Igor Minar <igor@angular.io>
2018-08-02 22:05:11 +0000

    6bacd32 release: cut the v7.0.0-beta.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+100.sha-183757d
+ * @license Angular v7.0.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+100.sha-183757d');
+    var VERSION = new Version('7.0.0-beta.0');
 
     /**
      * @license
@@ -48178,7 +48178,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+100.sha-183757d');
+    var VERSION$2 = new Version$1('7.0.0-beta.0');
 
     /**
      * @license
@@ -52632,7 +52632,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+100.sha-183757d');
+    var VERSION$3 = new Version$1('7.0.0-beta.0');
 
     /**
      * @license
