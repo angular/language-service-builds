dc7a7ec5d296dea7a8d503142e5ccfae56d132c5
Igor Minar <igor@angular.io>
2018-08-02 16:13:56 +0000

    9ddf269 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+45.sha-1ef2eae');
+    var VERSION = new Version('6.1.0+48.sha-9ddf269');
 
     /**
      * @license
@@ -47529,7 +47529,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+45.sha-1ef2eae');
+    var VERSION$2 = new Version$1('6.1.0+48.sha-9ddf269');
 
     /**
      * @license
@@ -52247,7 +52247,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+45.sha-1ef2eae');
+    var VERSION$3 = new Version$1('6.1.0+48.sha-9ddf269');
 
     /**
      * @license
