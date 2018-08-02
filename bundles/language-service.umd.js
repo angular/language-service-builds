335ceec2fff00531e7a01a4f5e67412dd9982456
Igor Minar <igor@angular.io>
2018-08-02 00:42:26 +0000

    d46a961 docs(ivy): API doc tweaks (#25258)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+88.sha-20b4530
+ * @license Angular v6.1.0+89.sha-d46a961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.0+88.sha-20b4530');
+    var VERSION = new Version('6.1.0+89.sha-d46a961');
 
     /**
      * @license
@@ -48170,7 +48170,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.0+88.sha-20b4530');
+    var VERSION$2 = new Version$1('6.1.0+89.sha-d46a961');
 
     /**
      * @license
@@ -52618,7 +52618,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.0+88.sha-20b4530');
+    var VERSION$3 = new Version$1('6.1.0+89.sha-d46a961');
 
     /**
      * @license
