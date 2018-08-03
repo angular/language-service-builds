d2a87e55a5b0e4b8449c7e6ef5e96fcc56ece779
Kara Erickson <karakara@google.com>
2018-08-03 06:54:49 +0000

    728d98d fix(ivy): add bound proerties name to template (#25272)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+4.sha-1fb7111
+ * @license Angular v7.0.0-beta.0+8.sha-728d98d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.0+4.sha-1fb7111');
+    var VERSION = new Version('7.0.0-beta.0+8.sha-728d98d');
 
     /**
      * @license
@@ -24429,7 +24429,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.0+4.sha-1fb7111');
+    var VERSION$2 = new Version$1('7.0.0-beta.0+8.sha-728d98d');
 
     /**
      * @license
@@ -45288,7 +45288,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.0+4.sha-1fb7111');
+    var VERSION$3 = new Version$1('7.0.0-beta.0+8.sha-728d98d');
 
     /**
      * @license
