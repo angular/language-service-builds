73bb9a60069cba58f458b1ff7b262d8133c51536
Kara Erickson <karakara@google.com>
2018-08-06 19:43:31 +0000

    4eb8ac6 build: update Bazel to 0.16 (#25316)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
+ * @license Angular v7.0.0-beta.0+24.sha-4eb8ac6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.0+23.sha-ba1e25f');
+    var VERSION = new Version('7.0.0-beta.0+24.sha-4eb8ac6');
 
     /**
      * @license
@@ -48299,7 +48299,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.0+23.sha-ba1e25f');
+    var VERSION$2 = new Version$1('7.0.0-beta.0+24.sha-4eb8ac6');
 
     /**
      * @license
@@ -52751,7 +52751,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.0+23.sha-ba1e25f');
+    var VERSION$3 = new Version$1('7.0.0-beta.0+24.sha-4eb8ac6');
 
     /**
      * @license
