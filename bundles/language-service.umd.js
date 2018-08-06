badee02a02b528d6fb0eb567e6202f4a2be4764f
Kara Erickson <karakara@google.com>
2018-08-06 22:29:28 +0000

    6e7d5f0 docs(core): fix tree-shakable spelling (#24057)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+17.sha-66547d8
+ * @license Angular v6.1.1+16.sha-6e7d5f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.1+17.sha-66547d8');
+    var VERSION = new Version('6.1.1+16.sha-6e7d5f0');
 
     /**
      * @license
@@ -24270,7 +24270,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.1+17.sha-66547d8');
+    var VERSION$2 = new Version$1('6.1.1+16.sha-6e7d5f0');
 
     /**
      * @license
@@ -44308,7 +44308,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.1+17.sha-66547d8');
+    var VERSION$3 = new Version$1('6.1.1+16.sha-6e7d5f0');
 
     /**
      * @license
