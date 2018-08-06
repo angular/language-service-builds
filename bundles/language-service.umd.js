9aa9a42004b8aebc8c0186228f04e9cb75cc5c4f
Kara Erickson <karakara@google.com>
2018-08-06 22:30:08 +0000

    c158702 docs: add missing word in outputs description. (#25330)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+27.sha-2b906f6
+ * @license Angular v7.0.0-beta.0+28.sha-c158702
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.0+27.sha-2b906f6');
+    var VERSION = new Version('7.0.0-beta.0+28.sha-c158702');
 
     /**
      * @license
@@ -48299,7 +48299,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.0+27.sha-2b906f6');
+    var VERSION$2 = new Version$1('7.0.0-beta.0+28.sha-c158702');
 
     /**
      * @license
@@ -52751,7 +52751,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.0+27.sha-2b906f6');
+    var VERSION$3 = new Version$1('7.0.0-beta.0+28.sha-c158702');
 
     /**
      * @license
