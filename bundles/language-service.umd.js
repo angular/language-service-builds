36d344ce52a019eefbf3fc9baf764575c44ff572
Kara Erickson <karakara@google.com>
2018-08-06 22:25:34 +0000

    a54c049 docs: replace npm with yarn in lockfile readme (#25309)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+11.sha-88f01f5
+ * @license Angular v6.1.1+13.sha-a54c049
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.1+11.sha-88f01f5');
+    var VERSION = new Version('6.1.1+13.sha-a54c049');
 
     /**
      * @license
@@ -47529,7 +47529,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.1+11.sha-88f01f5');
+    var VERSION$2 = new Version$1('6.1.1+13.sha-a54c049');
 
     /**
      * @license
@@ -52247,7 +52247,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.1+11.sha-88f01f5');
+    var VERSION$3 = new Version$1('6.1.1+13.sha-a54c049');
 
     /**
      * @license
