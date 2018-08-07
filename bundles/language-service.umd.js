812e7c8b93b1d62c3ac0adf9bf6246a7442e5222
Kara Erickson <karakara@google.com>
2018-08-07 19:15:22 +0000

    2fe05ab docs: update resources to include UI-jar (#21200)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+41.sha-2505c07
+ * @license Angular v7.0.0-beta.0+42.sha-2fe05ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.0+41.sha-2505c07');
+    var VERSION = new Version('7.0.0-beta.0+42.sha-2fe05ab');
 
     /**
      * @license
@@ -48317,7 +48317,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.0+41.sha-2505c07');
+    var VERSION$2 = new Version$1('7.0.0-beta.0+42.sha-2fe05ab');
 
     /**
      * @license
@@ -52769,7 +52769,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.0+41.sha-2505c07');
+    var VERSION$3 = new Version$1('7.0.0-beta.0+42.sha-2fe05ab');
 
     /**
      * @license
