94b08ccd89a0d7e8a5c36fd0882b2aa5ca9ead84
Kara Erickson <karakara@google.com>
2018-08-07 20:03:08 +0000

    2f23533 docs(aio): Angular course in Portuguese #21836

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+26.sha-054fbbe
+ * @license Angular v6.1.1+27.sha-2f23533
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.1+26.sha-054fbbe');
+    var VERSION = new Version('6.1.1+27.sha-2f23533');
 
     /**
      * @license
@@ -24278,7 +24278,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.1+26.sha-054fbbe');
+    var VERSION$2 = new Version$1('6.1.1+27.sha-2f23533');
 
     /**
      * @license
@@ -44316,7 +44316,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.1+26.sha-054fbbe');
+    var VERSION$3 = new Version$1('6.1.1+27.sha-2f23533');
 
     /**
      * @license
