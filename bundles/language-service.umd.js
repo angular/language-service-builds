d88548cc0ead77272aaba66335cd90d9c749ab22
Kara Erickson <karakara@google.com>
2018-08-07 21:37:52 +0000

    732026c feat(core): add DoBootstrap interface. (#24558)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+45.sha-fefc860
+ * @license Angular v7.0.0-beta.0+50.sha-732026c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.0+45.sha-fefc860');
+    var VERSION = new Version('7.0.0-beta.0+50.sha-732026c');
 
     /**
      * @license
@@ -30499,6 +30499,16 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         // Since the projection would than move it to its final destination.
         return false;
     }
+    /**
+     * We might delay insertion of children for a given view if it is disconnected.
+     * This might happen for 2 main reason:
+     * - view is not inserted into any container (view was created but not iserted yet)
+     * - view is inserted into a container but the container itself is not inserted into the DOM
+     * (container might be part of projection or child of a view that is not inserted yet).
+     *
+     * In other words we can insert children of a given view this view was inserted into a container and
+     * the container itself has it render parent determined.
+     */
     function canInsertNativeChildOfView(parent) {
         ngDevMode && assertNodeType(parent, 2 /* View */);
         // Because we are inserting into a `View` the `View` may be disconnected.
@@ -30600,6 +30610,9 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             else if (parent.tNode.type === 4 /* ElementContainer */) {
                 var beforeNode = parent.native;
                 var grandParent = getParentLNode(parent);
+                while (grandParent.tNode.type === 4 /* ElementContainer */) {
+                    grandParent = getParentLNode(grandParent);
+                }
                 if (grandParent.tNode.type === 2 /* View */) {
                     var renderParent = getRenderParent(grandParent);
                     nativeInsertBefore(renderer, renderParent.native, child, beforeNode);
@@ -30637,6 +30650,13 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             var views = lContainer[VIEWS];
             for (var i = 0; i < views.length; i++) {
                 addRemoveViewFromContainer(node, views[i], true, node.native);
+            }
+        }
+        else if (node.tNode.type === 4 /* ElementContainer */) {
+            var ngContainerChild = getChildLNode(node);
+            while (ngContainerChild) {
+                appendProjectedNode(ngContainerChild, currentParent, currentView, renderParent);
+                ngContainerChild = getNextLNode(ngContainerChild);
             }
         }
         if (node.dynamicLContainerNode) {
@@ -48340,7 +48360,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.0+45.sha-fefc860');
+    var VERSION$2 = new Version$1('7.0.0-beta.0+50.sha-732026c');
 
     /**
      * @license
@@ -52792,7 +52812,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.0+45.sha-fefc860');
+    var VERSION$3 = new Version$1('7.0.0-beta.0+50.sha-732026c');
 
     /**
      * @license
