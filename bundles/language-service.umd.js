90c8c67db59aba02e085d53f990740827771edf4
Kara Erickson <karakara@google.com>
2018-08-08 21:52:16 +0000

    a3f1e2c style: format .bzl files with buildifier (#23544)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+1.sha-1039bea
+ * @license Angular v7.0.0-beta.1+7.sha-a3f1e2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.1+1.sha-1039bea');
+    var VERSION = new Version('7.0.0-beta.1+7.sha-a3f1e2c');
 
     /**
      * @license
@@ -24440,7 +24440,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.1+1.sha-1039bea');
+    var VERSION$2 = new Version$1('7.0.0-beta.1+7.sha-a3f1e2c');
 
     /**
      * @license
@@ -45339,7 +45339,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.1+1.sha-1039bea');
+    var VERSION$3 = new Version$1('7.0.0-beta.1+7.sha-a3f1e2c');
 
     /**
      * @license
