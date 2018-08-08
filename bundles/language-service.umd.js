6eaf8b0de539f947b53310376b793d755c9f8ddd
Kara Erickson <karakara@google.com>
2018-08-08 21:43:59 +0000

    a2593cb release: cut the v7.0.0-beta.1 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+51.sha-8434794
+ * @license Angular v7.0.0-beta.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.0+51.sha-8434794');
+    var VERSION = new Version('7.0.0-beta.1');
 
     /**
      * @license
@@ -24440,7 +24440,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.0+51.sha-8434794');
+    var VERSION$2 = new Version$1('7.0.0-beta.1');
 
     /**
      * @license
@@ -45339,7 +45339,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.0+51.sha-8434794');
+    var VERSION$3 = new Version$1('7.0.0-beta.1');
 
     /**
      * @license
