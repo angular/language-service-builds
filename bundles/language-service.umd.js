5e92ccaa9a3b0ab86ec7f7596fc02a6228edfbcf
Ben Lesh <ben@benlesh.com>
2018-08-14 21:25:55 +0000

    c13901f build(docs-infra): remove stability labels from API docs (#25453)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+37.sha-0c4209f
+ * @license Angular v7.0.0-beta.1+43.sha-c13901f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.1+37.sha-0c4209f');
+    var VERSION = new Version('7.0.0-beta.1+43.sha-c13901f');
 
     /**
      * @license
@@ -46243,7 +46243,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.1+37.sha-0c4209f');
+    var VERSION$2 = new Version$1('7.0.0-beta.1+43.sha-c13901f');
 
     /**
      * @license
@@ -53148,7 +53148,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.1+37.sha-0c4209f');
+    var VERSION$3 = new Version$1('7.0.0-beta.1+43.sha-c13901f');
 
     /**
      * @license
