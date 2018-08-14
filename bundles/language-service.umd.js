36c4d4e0a493377e5108b033182324e06ef1cb49
Ben Lesh <ben@benlesh.com>
2018-08-14 20:12:40 +0000

    e906bf4 docs: add Accelebrate to resources (#23204)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+12.sha-f1ed022
+ * @license Angular v6.1.2+14.sha-e906bf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.2+12.sha-f1ed022');
+    var VERSION = new Version('6.1.2+14.sha-e906bf4');
 
     /**
      * @license
@@ -47539,7 +47539,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.2+12.sha-f1ed022');
+    var VERSION$2 = new Version$1('6.1.2+14.sha-e906bf4');
 
     /**
      * @license
@@ -52257,7 +52257,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.2+12.sha-f1ed022');
+    var VERSION$3 = new Version$1('6.1.2+14.sha-e906bf4');
 
     /**
      * @license
