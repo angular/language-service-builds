38cd1b850c1a633b777863065f44a2dd8d396e69
Ben Lesh <ben@benlesh.com>
2018-08-15 00:51:44 +0000

    ed2a47f build(bazel): update to rules_typescript 0.16.0 & update to tagged rules_webtesting 0.2.1 (#25486)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+19.sha-cdee9ad
+ * @license Angular v6.1.2+20.sha-ed2a47f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.2+19.sha-cdee9ad');
+    var VERSION = new Version('6.1.2+20.sha-ed2a47f');
 
     /**
      * @license
@@ -47539,7 +47539,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.2+19.sha-cdee9ad');
+    var VERSION$2 = new Version$1('6.1.2+20.sha-ed2a47f');
 
     /**
      * @license
@@ -52257,7 +52257,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.2+19.sha-cdee9ad');
+    var VERSION$3 = new Version$1('6.1.2+20.sha-ed2a47f');
 
     /**
      * @license
