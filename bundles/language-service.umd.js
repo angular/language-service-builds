4af18d84a5f532b622b82a4f9732f9de7affbcb8
Jason Aden <jasonaden@google.com>
2018-08-17 00:13:54 +0000

    116946f style: tslint enforces no debugger statements left behind (#25532)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+18.sha-503905c
+ * @license Angular v7.0.0-beta.2+19.sha-116946f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.2+18.sha-503905c');
+    var VERSION = new Version('7.0.0-beta.2+19.sha-116946f');
 
     /**
      * @license
@@ -23846,6 +23846,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
     }
     function throwError(msg) {
+        // tslint:disable-next-line
         debugger; // Left intentionally for better debugger experience.
         throw new Error("ASSERTION ERROR: " + msg);
     }
@@ -33296,7 +33297,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.2+18.sha-503905c');
+    var VERSION$2 = new Version$1('7.0.0-beta.2+19.sha-116946f');
 
     /**
      * @license
@@ -33310,10 +33311,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     *
     * @Annotation
     */
-    var Injectable = makeDecorator('Injectable', undefined, undefined, undefined, function (type, meta) {
-        debugger;
-        return R3_COMPILE_INJECTABLE$1(type, meta);
-    });
+    var Injectable = makeDecorator('Injectable', undefined, undefined, undefined, function (type, meta) { return R3_COMPILE_INJECTABLE$1(type, meta); });
 
     /**
      * @license
@@ -45651,7 +45649,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.2+18.sha-503905c');
+    var VERSION$3 = new Version$1('7.0.0-beta.2+19.sha-116946f');
 
     /**
      * @license
