28ddf886d678920fc01db9193094d1885fdcfe65
Jason Aden <jasonaden@google.com>
2018-08-17 18:45:21 +0000

    e9026a5 docs: fix script path reference (#25552)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
+ * @license Angular v7.0.0-beta.2+23.sha-e9026a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.2+21.sha-31f0f5b');
+    var VERSION = new Version('7.0.0-beta.2+23.sha-e9026a5');
 
     /**
      * @license
@@ -40726,7 +40726,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.2+21.sha-31f0f5b');
+    var VERSION$2 = new Version$1('7.0.0-beta.2+23.sha-e9026a5');
 
     /**
      * @license
@@ -53147,7 +53147,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.2+21.sha-31f0f5b');
+    var VERSION$3 = new Version$1('7.0.0-beta.2+23.sha-e9026a5');
 
     /**
      * @license
