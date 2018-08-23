7a9692ac3586c83dea676dc4717de87766b7dc77
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 03:05:35 +0000

    cc0d0a9 fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+1.sha-82f26fe
+ * @license Angular v6.1.4+2.sha-cc0d0a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.4+1.sha-82f26fe');
+    var VERSION = new Version('6.1.4+2.sha-cc0d0a9');
 
     /**
      * @license
@@ -14902,7 +14902,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
                 }
             }
         };
-        /* @internal */
+        /** @internal */
         StaticSymbolResolver.prototype.ignoreErrorsFor = function (cb) {
             var recorder = this.errorRecorder;
             this.errorRecorder = function () { };
@@ -24270,7 +24270,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.4+1.sha-82f26fe');
+    var VERSION$2 = new Version$1('6.1.4+2.sha-cc0d0a9');
 
     /**
      * @license
@@ -44308,7 +44308,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.4+1.sha-82f26fe');
+    var VERSION$3 = new Version$1('6.1.4+2.sha-cc0d0a9');
 
     /**
      * @license
