6315ca56e9a4509fee0408b3fe373833e7522dd4
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:41:13 +0000

    366195e build(bazel): esm5_outputs_aspect to work with targets such as ts_proto_library with no replay_params attribute (#25605)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+60.sha-a081d20
+ * @license Angular v7.0.0-beta.3+62.sha-366195e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.3+60.sha-a081d20');
+    var VERSION = new Version('7.0.0-beta.3+62.sha-366195e');
 
     /**
      * @license
@@ -41404,7 +41404,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.3+60.sha-a081d20');
+    var VERSION$2 = new Version$1('7.0.0-beta.3+62.sha-366195e');
 
     /**
      * @license
@@ -53825,7 +53825,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.3+60.sha-a081d20');
+    var VERSION$3 = new Version$1('7.0.0-beta.3+62.sha-366195e');
 
     /**
      * @license
