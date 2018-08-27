6cc4804d48d686697d825178dd6944a25b01e2e0
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:24:29 +0000

    b1902db build(docs-infra): render all overloads if they are abstract (#25670)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+58.sha-6a0f78f
+ * @license Angular v7.0.0-beta.3+61.sha-b1902db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.3+58.sha-6a0f78f');
+    var VERSION = new Version('7.0.0-beta.3+61.sha-b1902db');
 
     /**
      * @license
@@ -41404,7 +41404,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.3+58.sha-6a0f78f');
+    var VERSION$2 = new Version$1('7.0.0-beta.3+61.sha-b1902db');
 
     /**
      * @license
@@ -53825,7 +53825,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.3+58.sha-6a0f78f');
+    var VERSION$3 = new Version$1('7.0.0-beta.3+61.sha-b1902db');
 
     /**
      * @license
