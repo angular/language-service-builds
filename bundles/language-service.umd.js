1849d7149088d859e7ee0707db0fd2db52043947
Matias Niemelä <matias@yearofmoo.com>
2018-08-28 18:41:31 +0000

    b069514 fix(docs-infra): temporary SW fix (for online mode) (#25692)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+67.sha-0024d68
+ * @license Angular v7.0.0-beta.3+68.sha-b069514
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.3+67.sha-0024d68');
+    var VERSION = new Version('7.0.0-beta.3+68.sha-b069514');
 
     /**
      * @license
@@ -41530,7 +41530,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.3+67.sha-0024d68');
+    var VERSION$2 = new Version$1('7.0.0-beta.3+68.sha-b069514');
 
     /**
      * @license
@@ -54129,7 +54129,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.3+67.sha-0024d68');
+    var VERSION$3 = new Version$1('7.0.0-beta.3+68.sha-b069514');
 
     /**
      * @license
