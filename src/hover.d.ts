67b305ea322d27c7034d047eca104b229080549d
GitHub <noreply@github.com>
2018-08-29 19:04:40 +0000

    693c387 docs: add pwa keyword to service worker page

--- src/hover.d.ts
+++ src/hover.d.ts
@@ -1,4 +1,3 @@
-/// <amd-module name="@angular/language-service/src/hover" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -6,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <amd-module name="@angular/language-service/src/hover" />
 import { TemplateInfo } from './common';
 import { Hover } from './types';
 export declare function getHover(info: TemplateInfo): Hover | undefined;
