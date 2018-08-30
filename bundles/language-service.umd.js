8cea871ca0bdd3f65d7c9f87d7102717c9b38c19
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:35:44 +0000

    4a8d56a release: cut the v6.1.6 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.5+2.sha-0a3dd87
+ * @license Angular v6.1.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1144,7 +1144,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.5+2.sha-0a3dd87');
+    var VERSION = new Version('6.1.6');
 
     /**
      * @license
@@ -24270,7 +24270,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.5+2.sha-0a3dd87');
+    var VERSION$2 = new Version$1('6.1.6');
 
     /**
      * @license
@@ -44308,7 +44308,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.5+2.sha-0a3dd87');
+    var VERSION$3 = new Version$1('6.1.6');
 
     /**
      * @license
