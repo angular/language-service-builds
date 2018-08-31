72d62ba305b74ad07e2d0154f86518ab8b6b3223
Misko Hevery <misko@hevery.com>
2018-08-31 18:10:36 +0000

    a469c2c feat(ivy): produce contextual diagnostics in ngtsc mode (#25647)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+21.sha-b424b31
+ * @license Angular v7.0.0-beta.4+23.sha-a469c2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.4+21.sha-b424b31');
+    var VERSION = new Version('7.0.0-beta.4+23.sha-a469c2c');
 
     /**
      * @license
@@ -41602,7 +41602,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.4+21.sha-b424b31');
+    var VERSION$2 = new Version$1('7.0.0-beta.4+23.sha-a469c2c');
 
     /**
      * @license
@@ -54196,7 +54196,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.4+21.sha-b424b31');
+    var VERSION$3 = new Version$1('7.0.0-beta.4+23.sha-a469c2c');
 
     /**
      * @license
