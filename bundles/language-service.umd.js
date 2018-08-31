56a482906ce3afc55c03bddfc17ce2514a76b0c6
Misko Hevery <misko@hevery.com>
2018-08-31 05:45:04 +0000

    ed6b68b fix(bazel): protractor rule should include *.e2e-spec.js (#25701)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+1.sha-2e09115
+ * @license Angular v6.1.6+2.sha-ed6b68b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1192,7 +1192,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.6+1.sha-2e09115');
+    var VERSION = new Version('6.1.6+2.sha-ed6b68b');
 
     /**
      * @license
@@ -47531,7 +47531,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.6+1.sha-2e09115');
+    var VERSION$2 = new Version$1('6.1.6+2.sha-ed6b68b');
 
     /**
      * @license
@@ -52249,7 +52249,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.6+1.sha-2e09115');
+    var VERSION$3 = new Version$1('6.1.6+2.sha-ed6b68b');
 
     /**
      * @license
