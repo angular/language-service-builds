e2bdfc3e6ee88953317b4cfeb2330f801752655e
Misko Hevery <misko@hevery.com>
2018-09-05 19:23:14 +0000

    234661b feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+46.sha-a417b2b
+ * @license Angular v7.0.0-beta.4+48.sha-234661b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.4+46.sha-a417b2b');
+    var VERSION = new Version('7.0.0-beta.4+48.sha-234661b');
 
     /**
      * @license
@@ -33496,7 +33496,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.4+46.sha-a417b2b');
+    var VERSION$2 = new Version$1('7.0.0-beta.4+48.sha-234661b');
 
     /**
      * @license
@@ -46090,7 +46090,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.4+46.sha-a417b2b');
+    var VERSION$3 = new Version$1('7.0.0-beta.4+48.sha-234661b');
 
     /**
      * @license
