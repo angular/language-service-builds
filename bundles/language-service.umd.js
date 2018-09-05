bb84018f1678c268b5000e1527d37e1598b7d6a4
Misko Hevery <misko@hevery.com>
2018-09-05 19:24:54 +0000

    6f7df8a feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+17.sha-92298e5
+ * @license Angular v6.1.6+18.sha-6f7df8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.6+17.sha-92298e5');
+    var VERSION = new Version('6.1.6+18.sha-6f7df8a');
 
     /**
      * @license
@@ -24296,7 +24296,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.6+17.sha-92298e5');
+    var VERSION$2 = new Version$1('6.1.6+18.sha-6f7df8a');
 
     /**
      * @license
@@ -44334,7 +44334,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.6+17.sha-92298e5');
+    var VERSION$3 = new Version$1('6.1.6+18.sha-6f7df8a');
 
     /**
      * @license
