546fa489b85918e0e16bd05eee694ca9dc725344
Miško Hevery <misko@hevery.com>
2018-09-06 02:30:22 +0000

    d1063c6 build(docs-infra): restore correct dependency versions in `package.json` (#25827)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+25.sha-2c1f550
+ * @license Angular v6.1.6+28.sha-d1063c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.6+25.sha-2c1f550');
+    var VERSION = new Version('6.1.6+28.sha-d1063c6');
 
     /**
      * @license
@@ -24296,7 +24296,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.6+25.sha-2c1f550');
+    var VERSION$2 = new Version$1('6.1.6+28.sha-d1063c6');
 
     /**
      * @license
@@ -44334,7 +44334,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.6+25.sha-2c1f550');
+    var VERSION$3 = new Version$1('6.1.6+28.sha-d1063c6');
 
     /**
      * @license
