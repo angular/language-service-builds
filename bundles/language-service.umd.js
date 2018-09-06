5844b75add89e637f6f21763cabfe581c7efc20b
Igor Minar <igor@angular.io>
2018-09-06 23:21:54 +0000

    b9a5ce1 docs(service-worker): update http-server command (#25845) PR Close #25845

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+28.sha-d1063c6
+ * @license Angular v6.1.7+8.sha-b9a5ce1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.6+28.sha-d1063c6');
+    var VERSION = new Version('6.1.7+8.sha-b9a5ce1');
 
     /**
      * @license
@@ -47557,7 +47557,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.6+28.sha-d1063c6');
+    var VERSION$2 = new Version$1('6.1.7+8.sha-b9a5ce1');
 
     /**
      * @license
@@ -52275,7 +52275,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.6+28.sha-d1063c6');
+    var VERSION$3 = new Version$1('6.1.7+8.sha-b9a5ce1');
 
     /**
      * @license
