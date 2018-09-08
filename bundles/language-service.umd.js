0364989f28314eb582827a74660d76dc9ef5ed74
Igor Minar <igor@angular.io>
2018-09-08 00:09:33 +0000

    96eb79b build: add support for running builds outside of sandbox on Mac. (#25870)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+9.sha-83a1334
+ * @license Angular v7.0.0-beta.5+10.sha-96eb79b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.5+9.sha-83a1334');
+    var VERSION = new Version('7.0.0-beta.5+10.sha-96eb79b');
 
     /**
      * @license
@@ -33832,7 +33832,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.5+9.sha-83a1334');
+    var VERSION$2 = new Version$1('7.0.0-beta.5+10.sha-96eb79b');
 
     /**
      * @license
@@ -46426,7 +46426,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.5+9.sha-83a1334');
+    var VERSION$3 = new Version$1('7.0.0-beta.5+10.sha-96eb79b');
 
     /**
      * @license
