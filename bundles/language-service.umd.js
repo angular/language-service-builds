b72af72ea6e30cea4392f1203a818738cc29388f
Igor Minar <igor@angular.io>
2018-09-10 22:07:41 +0000

    e363388 feat(ivy): support animation @triggers in templates (#25849)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+13.sha-ed266da
+ * @license Angular v7.0.0-beta.5+14.sha-e363388
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.5+13.sha-ed266da');
+    var VERSION = new Version('7.0.0-beta.5+14.sha-e363388');
 
     /**
      * @license
@@ -16930,10 +16930,11 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         switch (type) {
             case 0 /* Property */:
                 return Identifiers$1.elementProperty;
-            case 1 /* Attribute */:
-                return Identifiers$1.elementAttribute;
             case 2 /* Class */:
                 return Identifiers$1.elementClassProp;
+            case 1 /* Attribute */:
+            case 4 /* Animation */:
+                return Identifiers$1.elementAttribute;
             default:
                 return undefined;
         }
@@ -17317,7 +17318,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             var hasStylingInstructions = initialStyleDeclarations.length || styleInputs.length ||
                 initialClassDeclarations.length || classInputs.length;
             // add attributes for directive matching purposes
-            attributes.push.apply(attributes, __spread(this.prepareSelectOnlyAttrs(allOtherInputs, element.outputs)));
+            attributes.push.apply(attributes, __spread(this.prepareSyntheticAndSelectOnlyAttrs(allOtherInputs, element.outputs)));
             parameters.push(this.toAttrsParam(attributes));
             // local refs (ex.: <div #foo #bar="baz">)
             parameters.push(this.prepareRefsParameter(element.references));
@@ -17448,23 +17449,31 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             }
             // Generate element input bindings
             allOtherInputs.forEach(function (input) {
-                if (input.type === 4 /* Animation */) {
-                    console.error('warning: animation bindings not yet supported');
-                    return;
-                }
                 var instruction = mapBindingToInstruction(input.type);
-                if (instruction) {
+                if (input.type === 4 /* Animation */) {
+                    var value_2 = input.value.visit(_this._valueConverter);
+                    // setAttribute without a value doesn't make any sense
+                    if (value_2.name || value_2.value) {
+                        var name_2 = prepareSyntheticAttributeName(input.name);
+                        _this.updateInstruction(input.sourceSpan, Identifiers$1.elementAttribute, function () {
+                            return [
+                                literal(elementIndex), literal(name_2), _this.convertPropertyBinding(implicit, value_2)
+                            ];
+                        });
+                    }
+                }
+                else if (instruction) {
                     var params_2 = [];
                     var sanitizationRef = resolveSanitizationFn(input, input.securityContext);
                     if (sanitizationRef)
                         params_2.push(sanitizationRef);
-                    // TODO(chuckj): runtime: security context?
-                    var value_2 = input.value.visit(_this._valueConverter);
-                    _this.allocateBindingSlots(value_2);
+                    // TODO(chuckj): runtime: security context
+                    var value_3 = input.value.visit(_this._valueConverter);
+                    _this.allocateBindingSlots(value_3);
                     _this.updateInstruction(input.sourceSpan, instruction, function () {
                         return __spread([
                             literal(elementIndex), literal(input.name),
-                            _this.convertPropertyBinding(implicit, value_2)
+                            _this.convertPropertyBinding(implicit, value_3)
                         ], params_2);
                     });
                 }
@@ -17508,7 +17517,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             // prepare attributes parameter (including attributes used for directive matching)
             var attrsExprs = [];
             template.attributes.forEach(function (a) { attrsExprs.push(asLiteral(a.name), asLiteral(a.value)); });
-            attrsExprs.push.apply(attrsExprs, __spread(this.prepareSelectOnlyAttrs(template.inputs, template.outputs)));
+            attrsExprs.push.apply(attrsExprs, __spread(this.prepareSyntheticAndSelectOnlyAttrs(template.inputs, template.outputs)));
             parameters.push(this.toAttrsParam(attrsExprs));
             // local refs (ex.: <ng-template #foo>)
             if (template.references && template.references.length) {
@@ -17630,12 +17639,28 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             elOrTpl.outputs.forEach(function (o) { attributesMap[o.name] = ''; });
             return attributesMap;
         };
-        TemplateDefinitionBuilder.prototype.prepareSelectOnlyAttrs = function (inputs, outputs) {
+        TemplateDefinitionBuilder.prototype.prepareSyntheticAndSelectOnlyAttrs = function (inputs, outputs) {
             var attrExprs = [];
-            if (inputs.length || outputs.length) {
+            var nonSyntheticInputs = [];
+            if (inputs.length) {
+                var EMPTY_STRING_EXPR_1 = asLiteral('');
+                inputs.forEach(function (input) {
+                    if (input.type === 4 /* Animation */) {
+                        // @attributes are for Renderer2 animation @triggers, but this feature
+                        // may be supported differently in future versions of angular. However,
+                        // @triggers should always just be treated as regular attributes (it's up
+                        // to the renderer to detect and use them in a special way).
+                        attrExprs.push(asLiteral(prepareSyntheticAttributeName(input.name)), EMPTY_STRING_EXPR_1);
+                    }
+                    else {
+                        nonSyntheticInputs.push(input);
+                    }
+                });
+            }
+            if (nonSyntheticInputs.length || outputs.length) {
                 attrExprs.push(literal(1 /* SelectOnly */));
-                inputs.forEach(function (i) { attrExprs.push(asLiteral(i.name)); });
-                outputs.forEach(function (o) { attrExprs.push(asLiteral(o.name)); });
+                nonSyntheticInputs.forEach(function (i) { return attrExprs.push(asLiteral(i.name)); });
+                outputs.forEach(function (o) { return attrExprs.push(asLiteral(o.name)); });
             }
             return attrExprs;
         };
@@ -18090,6 +18115,9 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
                 return true;
         }
         return false;
+    }
+    function prepareSyntheticAttributeName(name) {
+        return '@' + name;
     }
 
     /**
@@ -42005,7 +42033,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.5+13.sha-ed266da');
+    var VERSION$2 = new Version$1('7.0.0-beta.5+14.sha-e363388');
 
     /**
      * @license
@@ -54609,7 +54637,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.5+13.sha-ed266da');
+    var VERSION$3 = new Version$1('7.0.0-beta.5+14.sha-e363388');
 
     /**
      * @license
