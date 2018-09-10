ad80df9368f44a5b05d8868515cd181ab990ede7
Igor Minar <igor@angular.io>
2018-09-10 22:06:26 +0000

    7ba0cb7 refactor(ivy): remove superfluous Array check (#25894)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+14.sha-e363388
+ * @license Angular v7.0.0-beta.5+16.sha-7ba0cb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.5+14.sha-e363388');
+    var VERSION = new Version('7.0.0-beta.5+16.sha-7ba0cb7');
 
     /**
      * @license
@@ -33836,7 +33836,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.5+14.sha-e363388');
+    var VERSION$2 = new Version$1('7.0.0-beta.5+16.sha-7ba0cb7');
 
     /**
      * @license
@@ -46430,7 +46430,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.5+14.sha-e363388');
+    var VERSION$3 = new Version$1('7.0.0-beta.5+16.sha-7ba0cb7');
 
     /**
      * @license
