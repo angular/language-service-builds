ae0abe028a98ecb453cd63c84e916975bc2c07de
Igor Minar <igor@angular.io>
2018-09-11 20:42:23 +0000

    55a1ce7 docs: add `disableTypeScriptVersionCheck` documentation (#25537)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+9.sha-8f9aeaa
+ * @license Angular v6.1.7+11.sha-55a1ce7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.7+9.sha-8f9aeaa');
+    var VERSION = new Version('6.1.7+11.sha-55a1ce7');
 
     /**
      * @license
@@ -24311,7 +24311,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.7+9.sha-8f9aeaa');
+    var VERSION$2 = new Version$1('6.1.7+11.sha-55a1ce7');
 
     /**
      * @license
@@ -44418,7 +44418,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.7+9.sha-8f9aeaa');
+    var VERSION$3 = new Version$1('6.1.7+11.sha-55a1ce7');
 
     /**
      * @license
