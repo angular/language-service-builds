f6dec2d6186bb2095b2e968c9540ca0a394b6257
Igor Minar <igor@angular.io>
2018-09-11 15:12:43 +0000

    86a3be8 docs(ivy): add explanation of LViewData (#25779)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+21.sha-d5bd86a
+ * @license Angular v7.0.0-beta.5+22.sha-86a3be8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.5+21.sha-d5bd86a');
+    var VERSION = new Version('7.0.0-beta.5+22.sha-86a3be8');
 
     /**
      * @license
@@ -42059,7 +42059,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.5+21.sha-d5bd86a');
+    var VERSION$2 = new Version$1('7.0.0-beta.5+22.sha-86a3be8');
 
     /**
      * @license
@@ -54663,7 +54663,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.5+21.sha-d5bd86a');
+    var VERSION$3 = new Version$1('7.0.0-beta.5+22.sha-86a3be8');
 
     /**
      * @license
