35ccaf91816ff4bdbf9bc597e4602b719a2e44dc
Igor Minar <igor@angular.io>
2018-09-12 00:43:08 +0000

    8d7fbb6 docs(router): change description of parameter usage for navigation functions (#21840)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+11.sha-55a1ce7
+ * @license Angular v6.1.7+13.sha-8d7fbb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.7+11.sha-55a1ce7');
+    var VERSION = new Version('6.1.7+13.sha-8d7fbb6');
 
     /**
      * @license
@@ -47557,7 +47557,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.7+11.sha-55a1ce7');
+    var VERSION$2 = new Version$1('6.1.7+13.sha-8d7fbb6');
 
     /**
      * @license
@@ -52275,7 +52275,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.7+11.sha-55a1ce7');
+    var VERSION$3 = new Version$1('6.1.7+13.sha-8d7fbb6');
 
     /**
      * @license
