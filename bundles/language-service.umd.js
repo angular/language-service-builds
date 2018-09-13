f520f5b5ac19140a0bcdcf9aa5a9df8daf9fe9fc
Ben Lesh <ben@benlesh.com>
2018-09-13 23:59:50 +0000

    076374b docs: delete old comments from example (#25931)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+12.sha-a31cfc5
+ * @license Angular v6.1.7+15.sha-076374b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.7+12.sha-a31cfc5');
+    var VERSION = new Version('6.1.7+15.sha-076374b');
 
     /**
      * @license
@@ -47557,7 +47557,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.7+12.sha-a31cfc5');
+    var VERSION$2 = new Version$1('6.1.7+15.sha-076374b');
 
     /**
      * @license
@@ -52275,7 +52275,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.7+12.sha-a31cfc5');
+    var VERSION$3 = new Version$1('6.1.7+15.sha-076374b');
 
     /**
      * @license
