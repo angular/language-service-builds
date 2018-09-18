bd71b16ff9f4d2ab48f41e7c1b5ea5c5282d57fe
Ben Lesh <ben@benlesh.com>
2018-09-18 21:11:35 +0000

    56283ed build: update .nvmrc file to correct node version (#25992)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+50.sha-96ee898
+ * @license Angular v7.0.0-beta.5+46.sha-56283ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.5+50.sha-96ee898');
+    var VERSION = new Version('7.0.0-beta.5+46.sha-56283ed');
 
     /**
      * @license
@@ -34117,7 +34117,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.5+50.sha-96ee898');
+    var VERSION$2 = new Version$1('7.0.0-beta.5+46.sha-56283ed');
 
     /**
      * @license
@@ -46712,7 +46712,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.5+50.sha-96ee898');
+    var VERSION$3 = new Version$1('7.0.0-beta.5+46.sha-56283ed');
 
     /**
      * @license
