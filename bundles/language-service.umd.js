bb565e55d9c058e4f985561a2e5b3a514a89945b
Kara Erickson <karakara@google.com>
2018-09-20 18:31:05 +0000

    549de1e fix(core): add missing `peerDependency ` to `@angular/compiler` (#26033)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+13.sha-48e73c1
+ * @license Angular v7.0.0-beta.6+14.sha-549de1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.6+13.sha-48e73c1');
+    var VERSION = new Version('7.0.0-beta.6+14.sha-549de1e');
 
     /**
      * @license
@@ -34116,7 +34116,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.6+13.sha-48e73c1');
+    var VERSION$2 = new Version$1('7.0.0-beta.6+14.sha-549de1e');
 
     /**
      * @license
@@ -46723,7 +46723,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.6+13.sha-48e73c1');
+    var VERSION$3 = new Version$1('7.0.0-beta.6+14.sha-549de1e');
 
     /**
      * @license
