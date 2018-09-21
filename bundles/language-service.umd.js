0113b0d1f0d6866eac1a62c114de959e169efc68
Kara Erickson <karakara@google.com>
2018-09-21 01:24:11 +0000

    64aa670 refactor(router): cleanup to navigation stream for readability and documentation (#25740)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+14.sha-549de1e
+ * @license Angular v7.0.0-beta.6+25.sha-64aa670
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.6+14.sha-549de1e');
+    var VERSION = new Version('7.0.0-beta.6+25.sha-64aa670');
 
     /**
      * @license
@@ -42074,7 +42074,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.6+14.sha-549de1e');
+    var VERSION$2 = new Version$1('7.0.0-beta.6+25.sha-64aa670');
 
     /**
      * @license
@@ -54690,7 +54690,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.6+14.sha-549de1e');
+    var VERSION$3 = new Version$1('7.0.0-beta.6+25.sha-64aa670');
 
     /**
      * @license
