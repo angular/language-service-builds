d444c139d77d0cbbcd39cae3ad15b5757522f33f
Kara Erickson <karakara@google.com>
2018-09-21 18:03:05 +0000

    e42c1b0 ci: correct github robot size check configuration (#26057)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+27.sha-e7ade38
+ * @license Angular v7.0.0-beta.6+28.sha-e42c1b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.6+27.sha-e7ade38');
+    var VERSION = new Version('7.0.0-beta.6+28.sha-e42c1b0');
 
     /**
      * @license
@@ -34062,7 +34062,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.6+27.sha-e7ade38');
+    var VERSION$2 = new Version$1('7.0.0-beta.6+28.sha-e42c1b0');
 
     /**
      * @license
@@ -46669,7 +46669,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.6+27.sha-e7ade38');
+    var VERSION$3 = new Version$1('7.0.0-beta.6+28.sha-e42c1b0');
 
     /**
      * @license
