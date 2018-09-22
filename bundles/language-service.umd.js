680fedab63261b5fd9b767b3e6244b5b6c52073b
Kara Erickson <karakara@google.com>
2018-09-22 00:40:33 +0000

    7f1cace build(docs-infra): sort NgModule exports by id (#26051)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+35.sha-82a14dc
+ * @license Angular v7.0.0-beta.6+37.sha-7f1cace
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.6+35.sha-82a14dc');
+    var VERSION = new Version('7.0.0-beta.6+37.sha-7f1cace');
 
     /**
      * @license
@@ -34079,7 +34079,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.6+35.sha-82a14dc');
+    var VERSION$2 = new Version$1('7.0.0-beta.6+37.sha-7f1cace');
 
     /**
      * @license
@@ -46694,7 +46694,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.6+35.sha-82a14dc');
+    var VERSION$3 = new Version$1('7.0.0-beta.6+37.sha-7f1cace');
 
     /**
      * @license
