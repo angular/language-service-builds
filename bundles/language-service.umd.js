1dd2c2fa4ffa092bcfdb94d50342cb1846a7779f
Kara Erickson <karakara@google.com>
2018-09-26 02:22:25 +0000

    c7e2930 docs: fix issues related to tutorial. (#24445)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+60.sha-6a62ed2
+ * @license Angular v7.0.0-beta.6+61.sha-c7e2930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.6+60.sha-6a62ed2');
+    var VERSION = new Version('7.0.0-beta.6+61.sha-c7e2930');
 
     /**
      * @license
@@ -33427,7 +33427,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.6+60.sha-6a62ed2');
+    var VERSION$2 = new Version$1('7.0.0-beta.6+61.sha-c7e2930');
 
     /**
      * @license
@@ -46159,7 +46159,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.6+60.sha-6a62ed2');
+    var VERSION$3 = new Version$1('7.0.0-beta.6+61.sha-c7e2930');
 
     /**
      * @license
