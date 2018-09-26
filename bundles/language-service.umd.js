f1f80725450e79df92fe4837a5ee1f4ade041f4d
Kara Erickson <karakara@google.com>
2018-09-26 02:29:40 +0000

    d191085 docs: cleanup minor changes for forms overview (#25663)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+61.sha-c7e2930
+ * @license Angular v7.0.0-beta.6+79.sha-d191085
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.6+61.sha-c7e2930');
+    var VERSION = new Version('7.0.0-beta.6+79.sha-d191085');
 
     /**
      * @license
@@ -33427,7 +33427,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.6+61.sha-c7e2930');
+    var VERSION$2 = new Version$1('7.0.0-beta.6+79.sha-d191085');
 
     /**
      * @license
@@ -46159,7 +46159,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.6+61.sha-c7e2930');
+    var VERSION$3 = new Version$1('7.0.0-beta.6+79.sha-d191085');
 
     /**
      * @license
