15b13f26142e06f9fae2619acf9657a6f738611c
Kara Erickson <karakara@google.com>
2018-09-26 23:06:53 +0000

    e1990a5 docs: firefox web components info (#26118)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7
+ * @license Angular v7.0.0-beta.7+1.sha-e1990a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.7');
+    var VERSION = new Version('7.0.0-beta.7+1.sha-e1990a5');
 
     /**
      * @license
@@ -42039,7 +42039,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.7');
+    var VERSION$2 = new Version$1('7.0.0-beta.7+1.sha-e1990a5');
 
     /**
      * @license
@@ -54727,7 +54727,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.7');
+    var VERSION$3 = new Version$1('7.0.0-beta.7+1.sha-e1990a5');
 
     /**
      * @license
