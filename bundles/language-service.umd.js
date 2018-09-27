b940d89fccf4b578916a1d629767161920ea6a02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 19:54:13 +0000

    63b795a refactor(ivy): make sure that test bed symbols are imported from ivy_switch (#26121)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+29.sha-5f6900e
+ * @license Angular v7.0.0-beta.7+30.sha-63b795a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.7+29.sha-5f6900e');
+    var VERSION = new Version('7.0.0-beta.7+30.sha-63b795a');
 
     /**
      * @license
@@ -40775,7 +40775,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.7+29.sha-5f6900e');
+    var VERSION$2 = new Version$1('7.0.0-beta.7+30.sha-63b795a');
 
     /**
      * @license
@@ -53140,7 +53140,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.7+29.sha-5f6900e');
+    var VERSION$3 = new Version$1('7.0.0-beta.7+30.sha-63b795a');
 
     /**
      * @license
