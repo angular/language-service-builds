fcf7c7242d309274225422dd36b2cdaa4e0036a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 20:24:22 +0000

    04a023c build: pass `stripExportPattern` as an array of `RegExp` (#26012)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+18.sha-c12e553
+ * @license Angular v6.1.9+19.sha-04a023c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.9+18.sha-c12e553');
+    var VERSION = new Version('6.1.9+19.sha-04a023c');
 
     /**
      * @license
@@ -47602,7 +47602,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.9+18.sha-c12e553');
+    var VERSION$2 = new Version$1('6.1.9+19.sha-04a023c');
 
     /**
      * @license
@@ -52332,7 +52332,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.9+18.sha-c12e553');
+    var VERSION$3 = new Version$1('6.1.9+19.sha-04a023c');
 
     /**
      * @license
