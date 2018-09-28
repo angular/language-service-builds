f511816abb1b442eb157df340ad939d1747ef347
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 17:53:36 +0000

    22dc8ad build: use separate tags for ivy builds in `publish-build-artifacts.sh` (#26159)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+19.sha-04a023c
+ * @license Angular v6.1.9+20.sha-22dc8ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.9+19.sha-04a023c');
+    var VERSION = new Version('6.1.9+20.sha-22dc8ad');
 
     /**
      * @license
@@ -47602,7 +47602,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.9+19.sha-04a023c');
+    var VERSION$2 = new Version$1('6.1.9+20.sha-22dc8ad');
 
     /**
      * @license
@@ -52332,7 +52332,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.9+19.sha-04a023c');
+    var VERSION$3 = new Version$1('6.1.9+20.sha-22dc8ad');
 
     /**
      * @license
