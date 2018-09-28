d710e71aa3f1c80d44aaa53648ae558cdf976b54
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 23:17:28 +0000

    2ad1bb4 release: cut the v7.0.0-rc.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+51.sha-b807106
+ * @license Angular v7.0.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-beta.7+51.sha-b807106');
+    var VERSION = new Version('7.0.0-rc.0');
 
     /**
      * @license
@@ -32156,7 +32156,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-beta.7+51.sha-b807106');
+    var VERSION$2 = new Version$1('7.0.0-rc.0');
 
     /**
      * @license
@@ -44504,7 +44504,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-beta.7+51.sha-b807106');
+    var VERSION$3 = new Version$1('7.0.0-rc.0');
 
     /**
      * @license
