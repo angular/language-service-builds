667d4e7d121149eda4efe5d83a49d7ae951fb3a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 23:10:35 +0000

    aaaa340 fix(ivy): sanitize tag name while generating listener function name (#26237)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+26.sha-7306799
+ * @license Angular v7.0.0-rc.0+27.sha-aaaa340
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.0+26.sha-7306799');
+    var VERSION = new Version('7.0.0-rc.0+27.sha-aaaa340');
 
     /**
      * @license
@@ -32316,7 +32316,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.0+26.sha-7306799');
+    var VERSION$2 = new Version$1('7.0.0-rc.0+27.sha-aaaa340');
 
     /**
      * @license
@@ -44690,7 +44690,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.0+26.sha-7306799');
+    var VERSION$3 = new Version$1('7.0.0-rc.0+27.sha-aaaa340');
 
     /**
      * @license
