768c46664a9a28acd19f4c7a809389eb1dcf4d6b
Jason Aden <jasonaden@google.com>
2018-10-05 23:17:57 +0000

    55d54c7 fix(platform-browser): fix #22155, destroy hammer manager when off (#22156)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+62.sha-ccceff5
+ * @license Angular v7.0.0-rc.0+63.sha-55d54c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.0+62.sha-ccceff5');
+    var VERSION = new Version('7.0.0-rc.0+63.sha-55d54c7');
 
     /**
      * @license
@@ -40776,7 +40776,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.0+62.sha-ccceff5');
+    var VERSION$2 = new Version$1('7.0.0-rc.0+63.sha-55d54c7');
 
     /**
      * @license
@@ -53160,7 +53160,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.0+62.sha-ccceff5');
+    var VERSION$3 = new Version$1('7.0.0-rc.0+63.sha-55d54c7');
 
     /**
      * @license
