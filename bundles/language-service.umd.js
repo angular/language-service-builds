fce22490bf6bbdf9ba72539be7fbae6a1a217fb1
Jason Aden <jasonaden@google.com>
2018-10-05 21:14:28 +0000

    bfeed84 docs(service-worker): improve API docs for `SwPush`/`SwUpdate` (#23138)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+49.sha-3cce4af
+ * @license Angular v7.0.0-rc.0+53.sha-bfeed84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.0+49.sha-3cce4af');
+    var VERSION = new Version('7.0.0-rc.0+53.sha-bfeed84');
 
     /**
      * @license
@@ -40776,7 +40776,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.0+49.sha-3cce4af');
+    var VERSION$2 = new Version$1('7.0.0-rc.0+53.sha-bfeed84');
 
     /**
      * @license
@@ -53160,7 +53160,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.0+49.sha-3cce4af');
+    var VERSION$3 = new Version$1('7.0.0-rc.0+53.sha-bfeed84');
 
     /**
      * @license
