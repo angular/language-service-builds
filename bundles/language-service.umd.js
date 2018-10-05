afd491ab5382a91f21358f0ccefbd490fb6be535
Jason Aden <jasonaden@google.com>
2018-10-05 21:26:40 +0000

    8f25321 refactor(ivy): update context discovery to prep for dir merge (#26262)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+54.sha-245b85f
+ * @license Angular v7.0.0-rc.0+48.sha-8f25321
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.0+54.sha-245b85f');
+    var VERSION = new Version('7.0.0-rc.0+48.sha-8f25321');
 
     /**
      * @license
@@ -32266,7 +32266,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.0+54.sha-245b85f');
+    var VERSION$2 = new Version$1('7.0.0-rc.0+48.sha-8f25321');
 
     /**
      * @license
@@ -44633,7 +44633,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.0+54.sha-245b85f');
+    var VERSION$3 = new Version$1('7.0.0-rc.0+48.sha-8f25321');
 
     /**
      * @license
