179f1bc85bbb9920c23801c4baa6442757eae201
Jason Aden <jasonaden@google.com>
2018-10-05 21:49:56 +0000

    b4421bb docs: fix wording (#26207)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+62.sha-ecb28bf
+ * @license Angular v6.1.9+63.sha-b4421bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.9+62.sha-ecb28bf');
+    var VERSION = new Version('6.1.9+63.sha-b4421bb');
 
     /**
      * @license
@@ -47602,7 +47602,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.9+62.sha-ecb28bf');
+    var VERSION$2 = new Version$1('6.1.9+63.sha-b4421bb');
 
     /**
      * @license
@@ -52332,7 +52332,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.9+62.sha-ecb28bf');
+    var VERSION$3 = new Version$1('6.1.9+63.sha-b4421bb');
 
     /**
      * @license
