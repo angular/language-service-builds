75af02f5936086dcc0e18de39cb5c3bc934f39c6
Jason Aden <jasonaden@google.com>
2018-10-09 00:03:33 +0000

    83302d1 fix(ivy): ensure ngcc compiles `@angular/core` with correct internal imports (#26236)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+63.sha-55d54c7
+ * @license Angular v7.0.0-rc.0+79.sha-83302d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.0+63.sha-55d54c7');
+    var VERSION = new Version('7.0.0-rc.0+79.sha-83302d1');
 
     /**
      * @license
@@ -32266,7 +32266,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.0+63.sha-55d54c7');
+    var VERSION$2 = new Version$1('7.0.0-rc.0+79.sha-83302d1');
 
     /**
      * @license
@@ -44633,7 +44633,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.0+63.sha-55d54c7');
+    var VERSION$3 = new Version$1('7.0.0-rc.0+79.sha-83302d1');
 
     /**
      * @license
