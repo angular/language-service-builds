4d89038364eebf32489396ea4f437e3a9b6c6989
Miško Hevery <misko@hevery.com>
2018-10-11 21:49:28 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('6.1.10+2.sha-65555fe');
+    var VERSION = new Version('6.1.10+4.sha-f1e14a3');
 
     /**
      * @license
@@ -24311,7 +24311,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('6.1.10+2.sha-65555fe');
+    var VERSION$2 = new Version$1('6.1.10+4.sha-f1e14a3');
 
     /**
      * @license
@@ -44723,7 +44723,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('6.1.10+2.sha-65555fe');
+    var VERSION$3 = new Version$1('6.1.10+4.sha-f1e14a3');
 
     /**
      * @license
