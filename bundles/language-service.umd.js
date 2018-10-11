53722db0b2e1302ef23162ea7fe90edb674a0674
Miško Hevery <misko@hevery.com>
2018-10-11 21:05:43 +0000

    7d0e175 ci(docs-infra): allow `aio_local` builds to fail on Travis (#26366)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+5.sha-053bf27
+ * @license Angular v7.0.0-rc.1+6.sha-7d0e175
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+5.sha-053bf27');
+    var VERSION = new Version('7.0.0-rc.1+6.sha-7d0e175');
 
     /**
      * @license
@@ -41231,7 +41231,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+5.sha-053bf27');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+6.sha-7d0e175');
 
     /**
      * @license
@@ -53641,7 +53641,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+5.sha-053bf27');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+6.sha-7d0e175');
 
     /**
      * @license
