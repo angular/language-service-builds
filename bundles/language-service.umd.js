5fe04209e846876d650d60696f64358617a573b0
Miško Hevery <misko@hevery.com>
2018-10-11 21:51:01 +0000

    6a64ac4 fix(ivy): Renderer2 should not use a special injection fn (#26369)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+22.sha-0a3f817
+ * @license Angular v7.0.0-rc.1+21.sha-6a64ac4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+22.sha-0a3f817');
+    var VERSION = new Version('7.0.0-rc.1+21.sha-6a64ac4');
 
     /**
      * @license
@@ -32485,7 +32485,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+22.sha-0a3f817');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+21.sha-6a64ac4');
 
     /**
      * @license
@@ -44886,7 +44886,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+22.sha-0a3f817');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+21.sha-6a64ac4');
 
     /**
      * @license
