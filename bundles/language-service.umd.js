f32f0979b46d642a11753978b8f7ae8bc21a17a6
Miško Hevery <misko@hevery.com>
2018-10-12 17:16:01 +0000

    ff15043 fix(core): allow null value for renderer setElement(…) (#17065)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+27.sha-071934e
+ * @license Angular v7.0.0-rc.1+34.sha-ff15043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
+    var VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
 
     /**
      * @license
@@ -32492,7 +32492,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+27.sha-071934e');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+34.sha-ff15043');
 
     /**
      * @license
@@ -44893,7 +44893,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+27.sha-071934e');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+34.sha-ff15043');
 
     /**
      * @license
