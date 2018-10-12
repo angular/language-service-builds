83477e2bb1ba72f3fd7259ba59a64c06c676b1e0
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:59 +0000

    a19b690 docs: move cli commands above api in nav (#26405)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+38.sha-9e32dc7
+ * @license Angular v7.0.0-rc.1+37.sha-a19b690
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+38.sha-9e32dc7');
+    var VERSION = new Version('7.0.0-rc.1+37.sha-a19b690');
 
     /**
      * @license
@@ -41237,7 +41237,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+38.sha-9e32dc7');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+37.sha-a19b690');
 
     /**
      * @license
@@ -53655,7 +53655,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+38.sha-9e32dc7');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+37.sha-a19b690');
 
     /**
      * @license
