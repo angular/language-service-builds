ef1cd593a51cf156938db7ec53431520cd65bbbb
Miško Hevery <misko@hevery.com>
2018-10-12 21:37:35 +0000

    3daeadd docs: visual studio removed from nav and updated to refer to getting started instead of quickstart repo (#26376)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+34.sha-ff15043
+ * @license Angular v7.0.0-rc.1+35.sha-3daeadd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
+    var VERSION = new Version('7.0.0-rc.1+35.sha-3daeadd');
 
     /**
      * @license
@@ -32492,7 +32492,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+34.sha-ff15043');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+35.sha-3daeadd');
 
     /**
      * @license
@@ -44893,7 +44893,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+34.sha-ff15043');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+35.sha-3daeadd');
 
     /**
      * @license
