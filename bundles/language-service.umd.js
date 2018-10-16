21d012aa3d61aaab8dccc4628961c90fc1436044
Miško Hevery <misko@hevery.com>
2018-10-16 22:10:44 +0000

    b115374 build: fix deps for running modules/benchmarks/src/largetable/render3:perf (#26482)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+51.sha-7d82053
+ * @license Angular v7.0.0-rc.1+52.sha-b115374
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+51.sha-7d82053');
+    var VERSION = new Version('7.0.0-rc.1+52.sha-b115374');
 
     /**
      * @license
@@ -32463,7 +32463,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+51.sha-7d82053');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+52.sha-b115374');
 
     /**
      * @license
@@ -44864,7 +44864,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+51.sha-7d82053');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+52.sha-b115374');
 
     /**
      * @license
