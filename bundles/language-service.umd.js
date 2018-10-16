9f1ba5e613fcd7829445ad944caddb51d346edc4
Miško Hevery <misko@hevery.com>
2018-10-16 00:17:25 +0000

    dd8a851 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+48.sha-164f79a
+ * @license Angular v7.0.0-rc.1+49.sha-dd8a851
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+48.sha-164f79a');
+    var VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
 
     /**
      * @license
@@ -41221,7 +41221,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+48.sha-164f79a');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+49.sha-dd8a851');
 
     /**
      * @license
@@ -53639,7 +53639,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+48.sha-164f79a');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+49.sha-dd8a851');
 
     /**
      * @license
