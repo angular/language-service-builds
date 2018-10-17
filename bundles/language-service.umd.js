4651f00ce65321107c716df55c7d3c21b2c24e0b
Miško Hevery <misko@hevery.com>
2018-10-17 20:42:36 +0000

    665627e fix(aio): add relative to app level routes section (#26504)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+86.sha-a609bf5
+ * @license Angular v7.0.0-rc.1+87.sha-665627e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+86.sha-a609bf5');
+    var VERSION = new Version('7.0.0-rc.1+87.sha-665627e');
 
     /**
      * @license
@@ -32478,7 +32478,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+86.sha-a609bf5');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+87.sha-665627e');
 
     /**
      * @license
@@ -44879,7 +44879,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+86.sha-a609bf5');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+87.sha-665627e');
 
     /**
      * @license
