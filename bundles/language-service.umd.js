3d5d8d6019dd37f526a4354aa87ee8352f719e19
Misko Hevery <misko@hevery.com>
2018-10-17 04:08:45 +0000

    948f507 docs(forms): change headings (#25900)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-fa8e633
+ * @license Angular v7.0.0-rc.1+67.sha-948f507
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+64.sha-fa8e633');
+    var VERSION = new Version('7.0.0-rc.1+67.sha-948f507');
 
     /**
      * @license
@@ -32470,7 +32470,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+64.sha-fa8e633');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+67.sha-948f507');
 
     /**
      * @license
@@ -44871,7 +44871,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+64.sha-fa8e633');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+67.sha-948f507');
 
     /**
      * @license
