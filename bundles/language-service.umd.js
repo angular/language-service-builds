d25baaccb81994ad8bfa946ddd405a48993b4306
Miško Hevery <misko@hevery.com>
2018-10-17 21:09:38 +0000

    f8195e5 fix(ivy): stub TestBed.compileComponents implementation (#26506)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+71.sha-8a3fd58
+ * @license Angular v7.0.0-rc.1+85.sha-f8195e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+71.sha-8a3fd58');
+    var VERSION = new Version('7.0.0-rc.1+85.sha-f8195e5');
 
     /**
      * @license
@@ -32478,7 +32478,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+71.sha-8a3fd58');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+85.sha-f8195e5');
 
     /**
      * @license
@@ -44879,7 +44879,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+71.sha-8a3fd58');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+85.sha-f8195e5');
 
     /**
      * @license
