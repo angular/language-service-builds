cd668f441d2b999e3af4fa58eabf910744237cd9
Igor Minar <igor@angular.io>
2018-10-18 20:04:57 +0000

    bbf96db feat(docs-infra): add v6 to the aio version picker

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+99.sha-72c2578
+ * @license Angular v7.0.0-rc.1+100.sha-bbf96db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+99.sha-72c2578');
+    var VERSION = new Version('7.0.0-rc.1+100.sha-bbf96db');
 
     /**
      * @license
@@ -41645,7 +41645,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+99.sha-72c2578');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+100.sha-bbf96db');
 
     /**
      * @license
@@ -54063,7 +54063,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+99.sha-72c2578');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+100.sha-bbf96db');
 
     /**
      * @license
