29d2ca21d74b230741256991b22d9585e74ca6dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:46:01 +0000

    d0a1e42 ci: add forms-overview guide to forms approval list (#26549)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+2.sha-32c018c
+ * @license Angular v7.0.0+6.sha-d0a1e42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0+2.sha-32c018c');
+    var VERSION = new Version('7.0.0+6.sha-d0a1e42');
 
     /**
      * @license
@@ -41221,7 +41221,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0+2.sha-32c018c');
+    var VERSION$2 = new Version$1('7.0.0+6.sha-d0a1e42');
 
     /**
      * @license
@@ -53639,7 +53639,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0+2.sha-32c018c');
+    var VERSION$3 = new Version$1('7.0.0+6.sha-d0a1e42');
 
     /**
      * @license
