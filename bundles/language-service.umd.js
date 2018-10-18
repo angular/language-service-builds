31d9b1b53261e6ae87e03fe37651e3d5209b6a9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:17 +0000

    a403c4f docs: type fix (#26386)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0
+ * @license Angular v7.0.0+1.sha-a403c4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0');
+    var VERSION = new Version('7.0.0+1.sha-a403c4f');
 
     /**
      * @license
@@ -41221,7 +41221,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0');
+    var VERSION$2 = new Version$1('7.0.0+1.sha-a403c4f');
 
     /**
      * @license
@@ -53639,7 +53639,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0');
+    var VERSION$3 = new Version$1('7.0.0+1.sha-a403c4f');
 
     /**
      * @license
