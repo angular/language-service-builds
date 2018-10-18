78916ccf7d2143b311ad0a8367eb09666658f17e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:45:15 +0000

    32c018c docs: removing errant double-quote (#26483) PR Close #26483

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+4.sha-8d28fe9
+ * @license Angular v7.0.0+2.sha-32c018c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0+4.sha-8d28fe9');
+    var VERSION = new Version('7.0.0+2.sha-32c018c');
 
     /**
      * @license
@@ -41221,7 +41221,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0+4.sha-8d28fe9');
+    var VERSION$2 = new Version$1('7.0.0+2.sha-32c018c');
 
     /**
      * @license
@@ -53639,7 +53639,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0+4.sha-8d28fe9');
+    var VERSION$3 = new Version$1('7.0.0+2.sha-32c018c');
 
     /**
      * @license
