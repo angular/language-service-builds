e1fbd82310e3b3e933a83e8b8adbe6773bf6ceee
Miško Hevery <misko@hevery.com>
2018-10-18 19:46:09 +0000

    72c2578 docs: fix incomplete sentence in CHANGELOG.md

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+97.sha-7634c1c
+ * @license Angular v7.0.0-rc.1+99.sha-72c2578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+97.sha-7634c1c');
+    var VERSION = new Version('7.0.0-rc.1+99.sha-72c2578');
 
     /**
      * @license
@@ -41645,7 +41645,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+97.sha-7634c1c');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+99.sha-72c2578');
 
     /**
      * @license
@@ -54063,7 +54063,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+97.sha-7634c1c');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+99.sha-72c2578');
 
     /**
      * @license
