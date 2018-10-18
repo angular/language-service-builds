54ce027d52cf891b9ad90569733abf986501684b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:46:54 +0000

    4f14a09 docs: fix links to browserlist (#26531)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+6.sha-d0a1e42
+ * @license Angular v7.0.0+5.sha-4f14a09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0+6.sha-d0a1e42');
+    var VERSION = new Version('7.0.0+5.sha-4f14a09');
 
     /**
      * @license
@@ -41221,7 +41221,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0+6.sha-d0a1e42');
+    var VERSION$2 = new Version$1('7.0.0+5.sha-4f14a09');
 
     /**
      * @license
@@ -53639,7 +53639,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0+6.sha-d0a1e42');
+    var VERSION$3 = new Version$1('7.0.0+5.sha-4f14a09');
 
     /**
      * @license
