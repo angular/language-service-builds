235c5852809d8abe165c2274b7840f30f39bc220
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:19:56 +0000

    b89bc37 fix(docs-infra): correct the version of @angular-devkit/build-angular (#26555)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+112.sha-716d887
+ * @license Angular v7.0.0-rc.1+114.sha-b89bc37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+112.sha-716d887');
+    var VERSION = new Version('7.0.0-rc.1+114.sha-b89bc37');
 
     /**
      * @license
@@ -41528,7 +41528,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0-rc.1+112.sha-716d887');
+    var VERSION$2 = new Version$1('7.0.0-rc.1+114.sha-b89bc37');
 
     /**
      * @license
@@ -54016,7 +54016,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0-rc.1+112.sha-716d887');
+    var VERSION$3 = new Version$1('7.0.0-rc.1+114.sha-b89bc37');
 
     /**
      * @license
