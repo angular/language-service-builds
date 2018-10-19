d58f077bc784827cd3b7b12de570b1c2e61417b2
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:20:38 +0000

    d972501 docs: fix release info navigation (#26558)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+27.sha-078a928
+ * @license Angular v7.0.0+33.sha-d972501
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1197,7 +1197,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0+27.sha-078a928');
+    var VERSION = new Version('7.0.0+33.sha-d972501');
 
     /**
      * @license
@@ -41645,7 +41645,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0+27.sha-078a928');
+    var VERSION$2 = new Version$1('7.0.0+33.sha-d972501');
 
     /**
      * @license
@@ -54063,7 +54063,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0+27.sha-078a928');
+    var VERSION$3 = new Version$1('7.0.0+33.sha-d972501');
 
     /**
      * @license
