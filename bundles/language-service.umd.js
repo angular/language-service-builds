d92195fbf052de924d63eb63bc895963d50f3557
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:12:44 +0000

    5d90aed docs(service-worker): updated browser support for Service Worker (#26408)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+22.sha-6c48455
+ * @license Angular v7.0.0+26.sha-5d90aed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0+22.sha-6c48455');
+    var VERSION = new Version('7.0.0+26.sha-5d90aed');
 
     /**
      * @license
@@ -32478,7 +32478,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         }
         return Version;
     }());
-    var VERSION$2 = new Version$1('7.0.0+22.sha-6c48455');
+    var VERSION$2 = new Version$1('7.0.0+26.sha-5d90aed');
 
     /**
      * @license
@@ -44879,7 +44879,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.0+22.sha-6c48455');
+    var VERSION$3 = new Version$1('7.0.0+26.sha-5d90aed');
 
     /**
      * @license
