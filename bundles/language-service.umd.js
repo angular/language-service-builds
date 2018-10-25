850a12430090564981764bb41a5936c47fd01704
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:12:52 +0000

    13a803d build(docs-infra): fix parameter type rendering (#26688)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+5.sha-0cc9842
+ * @license Angular v7.1.0-beta.0+2.sha-13a803d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+5.sha-0cc9842');
+    var VERSION = new Version('7.1.0-beta.0+2.sha-13a803d');
 
     /**
      * @license
@@ -32469,7 +32469,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+5.sha-0cc9842');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+2.sha-13a803d');
 
     /**
      * @license
@@ -44931,7 +44931,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+5.sha-0cc9842');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+2.sha-13a803d');
 
     /**
      * @license
