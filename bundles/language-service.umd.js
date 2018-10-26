17f2a2738175e4c905b17c74f87ee01ac1c72e6c
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:45:00 +0000

    da4a28e docs: fix indentation (#26623)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+39.sha-07509da
+ * @license Angular v7.1.0-beta.0+41.sha-da4a28e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1182,7 +1182,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+39.sha-07509da');
+    var VERSION = new Version('7.1.0-beta.0+41.sha-da4a28e');
 
     /**
      * @license
@@ -42345,7 +42345,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+39.sha-07509da');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+41.sha-da4a28e');
 
     /**
      * @license
@@ -54777,7 +54777,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+39.sha-07509da');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+41.sha-da4a28e');
 
     /**
      * @license
