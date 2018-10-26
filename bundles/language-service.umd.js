f5933f8ebe05f804a9ace7d309bba93a1ae213b8
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 21:45:34 +0000

    19fcfc3 fix(compiler): generate inputs with aliases properly (#26774)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+34.sha-c048358
+ * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+34.sha-c048358');
+    var VERSION = new Version('7.1.0-beta.0+35.sha-19fcfc3');
 
     /**
      * @license
@@ -32827,7 +32827,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+34.sha-c048358');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+35.sha-19fcfc3');
 
     /**
      * @license
@@ -45242,7 +45242,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+34.sha-c048358');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+35.sha-19fcfc3');
 
     /**
      * @license
