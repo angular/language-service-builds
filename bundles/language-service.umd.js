b76fdb4f853612eafd72c1643355cf2aa356b325
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:32:32 +0000

    ef6f160 ci(docs-infra): notify caretaker about `aio_monitoring` failures (#26649)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+44.sha-95993e1
+ * @license Angular v7.1.0-beta.0+53.sha-ef6f160
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1182,7 +1182,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+44.sha-95993e1');
+    var VERSION = new Version('7.1.0-beta.0+53.sha-ef6f160');
 
     /**
      * @license
@@ -42357,7 +42357,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+44.sha-95993e1');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+53.sha-ef6f160');
 
     /**
      * @license
@@ -54789,7 +54789,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+44.sha-95993e1');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+53.sha-ef6f160');
 
     /**
      * @license
