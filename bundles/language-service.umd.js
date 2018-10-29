bef297994479fe002258a52bcf74f4305b83048f
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 18:22:48 +0000

    2a86927 fix(ivy): move HostListeners generation to factory function (#26480)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+55.sha-c0bf222
+ * @license Angular v7.1.0-beta.0+56.sha-2a86927
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+55.sha-c0bf222');
+    var VERSION = new Version('7.1.0-beta.0+56.sha-2a86927');
 
     /**
      * @license
@@ -32837,7 +32837,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+55.sha-c0bf222');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+56.sha-2a86927');
 
     /**
      * @license
@@ -45252,7 +45252,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+55.sha-c0bf222');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+56.sha-2a86927');
 
     /**
      * @license
