2e43b8b3e9130630d039608006230cfd2d13d3a3
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:53:51 +0000

    87a7f2d docs: fix typo (#26773) PR Close #26773

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+69.sha-8fc4ae5
+ * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+69.sha-8fc4ae5');
+    var VERSION = new Version('7.1.0-beta.0+70.sha-87a7f2d');
 
     /**
      * @license
@@ -32846,7 +32846,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+69.sha-8fc4ae5');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+70.sha-87a7f2d');
 
     /**
      * @license
@@ -45261,7 +45261,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+69.sha-8fc4ae5');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+70.sha-87a7f2d');
 
     /**
      * @license
