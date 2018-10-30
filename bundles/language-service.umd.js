1afb7104bbbab230b80f51c567ef69bd27eff962
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:26:33 +0000

    23648b0 docs: configuration file reference (#26484)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+61.sha-91d2368
+ * @license Angular v7.1.0-beta.0+62.sha-23648b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+61.sha-91d2368');
+    var VERSION = new Version('7.1.0-beta.0+62.sha-23648b0');
 
     /**
      * @license
@@ -32846,7 +32846,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+61.sha-91d2368');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+62.sha-23648b0');
 
     /**
      * @license
@@ -45261,7 +45261,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+61.sha-91d2368');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+62.sha-23648b0');
 
     /**
      * @license
