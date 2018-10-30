d5e20752f2db1456b30134b0e58fac252861aedb
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:47:20 +0000

    8fc4ae5 build: use bazel version from node modules (#26691)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+67.sha-b95089d
+ * @license Angular v7.1.0-beta.0+69.sha-8fc4ae5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1182,7 +1182,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+67.sha-b95089d');
+    var VERSION = new Version('7.1.0-beta.0+69.sha-8fc4ae5');
 
     /**
      * @license
@@ -42446,7 +42446,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+67.sha-b95089d');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+69.sha-8fc4ae5');
 
     /**
      * @license
@@ -54878,7 +54878,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+67.sha-b95089d');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+69.sha-8fc4ae5');
 
     /**
      * @license
