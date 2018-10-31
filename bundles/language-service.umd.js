8d79a78fe80525505d126f248e1a9dfb59529fdc
GitHub <noreply@github.com>
2018-10-31 22:30:49 +0000

    f4e7920 fixup! docs: add `Function Inlining` rules to `PERF_NOTES.md`

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+80.sha-774f6fb
+ * @license Angular v7.1.0-beta.0+82.sha-f4e7920
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1164,7 +1164,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+80.sha-774f6fb');
+    var VERSION = new Version('7.1.0-beta.0+82.sha-f4e7920');
 
     /**
      * @license
@@ -32846,7 +32846,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.0+80.sha-774f6fb');
+    var VERSION$2 = new Version$1('7.1.0-beta.0+82.sha-f4e7920');
 
     /**
      * @license
@@ -45261,7 +45261,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.0+80.sha-774f6fb');
+    var VERSION$3 = new Version$1('7.1.0-beta.0+82.sha-f4e7920');
 
     /**
      * @license
