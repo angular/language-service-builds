309a62ed95ae58e71093368a950e1b89932da2bb
Kara Erickson <karakara@google.com>
2018-11-01 20:53:41 +0000

    aadbc8a fix(ivy): sync exported r3 symbol with compiler generated symbol (#26877)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+14.sha-2e7b5c5
+ * @license Angular v7.1.0-beta.1+15.sha-aadbc8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13334,7 +13334,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+14.sha-2e7b5c5');
+    var VERSION$1 = new Version('7.1.0-beta.1+15.sha-aadbc8a');
 
     /**
      * @license
@@ -37025,7 +37025,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.1+14.sha-2e7b5c5');
+    var VERSION$2 = new Version$1('7.1.0-beta.1+15.sha-aadbc8a');
 
     /**
      * @license
@@ -49440,7 +49440,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.1+14.sha-2e7b5c5');
+    var VERSION$3 = new Version$1('7.1.0-beta.1+15.sha-aadbc8a');
 
     /**
      * @license
