5d245c65a342acc54985bba1ccd37a3f81cde8c2
Kara Erickson <karakara@google.com>
2018-11-01 21:13:29 +0000

    eb5d308 build: update `canonical-path` dependency (#26719)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+18.sha-683d53d
+ * @license Angular v7.1.0-beta.1+19.sha-eb5d308
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13334,7 +13334,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+18.sha-683d53d');
+    var VERSION$1 = new Version('7.1.0-beta.1+19.sha-eb5d308');
 
     /**
      * @license
@@ -37021,7 +37021,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.1+18.sha-683d53d');
+    var VERSION$2 = new Version$1('7.1.0-beta.1+19.sha-eb5d308');
 
     /**
      * @license
@@ -49436,7 +49436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.1+18.sha-683d53d');
+    var VERSION$3 = new Version$1('7.1.0-beta.1+19.sha-eb5d308');
 
     /**
      * @license
