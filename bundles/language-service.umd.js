c1c7b4e2bdaadce0e011128fa987b5a215eaac69
Kara Erickson <karakara@google.com>
2018-11-05 18:15:48 +0000

    e6a0c45 style(compiler): typo fix (#26934) PR Close #26934

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+66.sha-7d2a746
+ * @license Angular v7.1.0-beta.1+67.sha-e6a0c45
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7472,8 +7472,8 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         };
         Token.prototype.isNumber = function () { return this.type == TokenType.Number; };
         Token.prototype.isString = function () { return this.type == TokenType.String; };
-        Token.prototype.isOperator = function (operater) {
-            return this.type == TokenType.Operator && this.strValue == operater;
+        Token.prototype.isOperator = function (operator) {
+            return this.type == TokenType.Operator && this.strValue == operator;
         };
         Token.prototype.isIdentifier = function () { return this.type == TokenType.Identifier; };
         Token.prototype.isKeyword = function () { return this.type == TokenType.Keyword; };
@@ -13334,7 +13334,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+66.sha-7d2a746');
+    var VERSION$1 = new Version('7.1.0-beta.1+67.sha-e6a0c45');
 
     /**
      * @license
@@ -37022,7 +37022,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.1+66.sha-7d2a746');
+    var VERSION$2 = new Version$1('7.1.0-beta.1+67.sha-e6a0c45');
 
     /**
      * @license
@@ -49437,7 +49437,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.1+66.sha-7d2a746');
+    var VERSION$3 = new Version$1('7.1.0-beta.1+67.sha-e6a0c45');
 
     /**
      * @license
