d4223e41ffac8fb6cd1debe845f6e492e47f378c
Kara Erickson <karakara@google.com>
2018-11-06 00:58:15 +0000

    7dbc103 fix(upgrade): improve downgrading-related error messages (#26217)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+74.sha-099d1a6
+ * @license Angular v7.1.0-beta.1+77.sha-7dbc103
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13319,7 +13319,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+74.sha-099d1a6');
+    var VERSION$1 = new Version('7.1.0-beta.1+77.sha-7dbc103');
 
     /**
      * @license
@@ -42658,7 +42658,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.1+74.sha-099d1a6');
+    var VERSION$2 = new Version$1('7.1.0-beta.1+77.sha-7dbc103');
 
     /**
      * @license
@@ -55129,7 +55129,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.1+74.sha-099d1a6');
+    var VERSION$3 = new Version$1('7.1.0-beta.1+77.sha-7dbc103');
 
     /**
      * @license
