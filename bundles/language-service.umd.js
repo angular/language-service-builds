542d17f1152f9af138e833dc9bf33611173edd55
Kara Erickson <karakara@google.com>
2018-11-06 23:32:30 +0000

    3ca1a57 test(ivy): run forms tests with ivy on ci (#26968)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+79.sha-297c54e
+ * @license Angular v7.1.0-beta.1+80.sha-3ca1a57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13319,7 +13319,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+79.sha-297c54e');
+    var VERSION$1 = new Version('7.1.0-beta.1+80.sha-3ca1a57');
 
     /**
      * @license
@@ -42663,7 +42663,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.1+79.sha-297c54e');
+    var VERSION$2 = new Version$1('7.1.0-beta.1+80.sha-3ca1a57');
 
     /**
      * @license
@@ -55134,7 +55134,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.1+79.sha-297c54e');
+    var VERSION$3 = new Version$1('7.1.0-beta.1+80.sha-3ca1a57');
 
     /**
      * @license
