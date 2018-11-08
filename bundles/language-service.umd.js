9640a576c4851ab71cf47d89eb3cbc0d6dc67673
Andrew Kushnir <akushnir@google.com>
2018-11-08 21:30:20 +0000

    6be6125 docs: add Angular subreddit to resources.json (#26976)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+4.sha-d284fa6
+ * @license Angular v7.0.3+6.sha-6be6125
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13305,7 +13305,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.0.3+4.sha-d284fa6');
+    var VERSION$1 = new Version('7.0.3+6.sha-6be6125');
 
     /**
      * @license
@@ -36654,7 +36654,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.0.3+4.sha-d284fa6');
+    var VERSION$2 = new Version$1('7.0.3+6.sha-6be6125');
 
     /**
      * @license
@@ -49076,7 +49076,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.3+4.sha-d284fa6');
+    var VERSION$3 = new Version$1('7.0.3+6.sha-6be6125');
 
     /**
      * @license
