fb9cf8ba2ea47d2cdb7afa475b9ccdd88cbeb5ad
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:29:52 +0000

    791d192 docs: correct https://angularconsole.com/ link (#26999) PR Close #26999

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+2.sha-15e671e
+ * @license Angular v7.1.0-beta.2+3.sha-791d192
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13336,7 +13336,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.2+2.sha-15e671e');
+    var VERSION$1 = new Version('7.1.0-beta.2+3.sha-791d192');
 
     /**
      * @license
@@ -42694,7 +42694,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.2+2.sha-15e671e');
+    var VERSION$2 = new Version$1('7.1.0-beta.2+3.sha-791d192');
 
     /**
      * @license
@@ -55166,7 +55166,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.2+2.sha-15e671e');
+    var VERSION$3 = new Version$1('7.1.0-beta.2+3.sha-791d192');
 
     /**
      * @license
