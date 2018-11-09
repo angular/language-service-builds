a9d4b351d98113ea4a1c4c2f0f6796023a7c56ef
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:17:38 +0000

    4efb460 docs(router): add RxJS filter pipe operator to example (#25686)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+13.sha-edb50a2
+ * @license Angular v7.1.0-beta.2+15.sha-4efb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13351,7 +13351,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.2+13.sha-edb50a2');
+    var VERSION$1 = new Version('7.1.0-beta.2+15.sha-4efb460');
 
     /**
      * @license
@@ -37095,7 +37095,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.2+13.sha-edb50a2');
+    var VERSION$2 = new Version$1('7.1.0-beta.2+15.sha-4efb460');
 
     /**
      * @license
@@ -49503,7 +49503,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.2+13.sha-edb50a2');
+    var VERSION$3 = new Version$1('7.1.0-beta.2+15.sha-4efb460');
 
     /**
      * @license
