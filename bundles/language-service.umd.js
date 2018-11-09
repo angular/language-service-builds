13e6cf1865432be5f5f166049df6905806e6bdc6
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:08:34 +0000

    b00588a docs: add missing instruction in HTTP section (#25715)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+10.sha-fb0d93d
+ * @license Angular v7.0.3+11.sha-b00588a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13305,7 +13305,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.0.3+10.sha-fb0d93d');
+    var VERSION$1 = new Version('7.0.3+11.sha-b00588a');
 
     /**
      * @license
@@ -36654,7 +36654,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.0.3+10.sha-fb0d93d');
+    var VERSION$2 = new Version$1('7.0.3+11.sha-b00588a');
 
     /**
      * @license
@@ -49076,7 +49076,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.3+10.sha-fb0d93d');
+    var VERSION$3 = new Version$1('7.0.3+11.sha-b00588a');
 
     /**
      * @license
