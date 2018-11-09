9e5dfa86700eaf832fd0fb415dada1d6ab57e79e
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:12:10 +0000

    1258ec0 docs: Fix links (#27027)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+10.sha-499e303
+ * @license Angular v7.1.0-beta.2+11.sha-1258ec0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13336,7 +13336,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.2+10.sha-499e303');
+    var VERSION$1 = new Version('7.1.0-beta.2+11.sha-1258ec0');
 
     /**
      * @license
@@ -42718,7 +42718,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.2+10.sha-499e303');
+    var VERSION$2 = new Version$1('7.1.0-beta.2+11.sha-1258ec0');
 
     /**
      * @license
@@ -55182,7 +55182,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.2+10.sha-499e303');
+    var VERSION$3 = new Version$1('7.1.0-beta.2+11.sha-1258ec0');
 
     /**
      * @license
