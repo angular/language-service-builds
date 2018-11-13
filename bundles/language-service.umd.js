6ade35c7ab169136b0ab0e5d8e6ad4e53c02a0df
Andrew Kushnir <akushnir@google.com>
2018-11-13 20:39:00 +0000

    e6e5904 fix(ivy): support forward refs in @Inject annotations (#27069)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+37.sha-7695dbd
+ * @license Angular v7.1.0-beta.2+39.sha-e6e5904
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13435,7 +13435,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.2+37.sha-7695dbd');
+    var VERSION$1 = new Version('7.1.0-beta.2+39.sha-e6e5904');
 
     /**
      * @license
@@ -37096,7 +37096,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.2+37.sha-7695dbd');
+    var VERSION$2 = new Version$1('7.1.0-beta.2+39.sha-e6e5904');
 
     /**
      * @license
@@ -49504,7 +49504,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.2+37.sha-7695dbd');
+    var VERSION$3 = new Version$1('7.1.0-beta.2+39.sha-e6e5904');
 
     /**
      * @license
