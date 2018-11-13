2797da4b558cd0dae5aea5246d283d27d15fda7a
Andrew Kushnir <akushnir@google.com>
2018-11-13 22:22:38 +0000

    c31e78f fix(compiler-cli): add missing tslib dependency (#27063)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+40.sha-bc652a2
+ * @license Angular v7.1.0-beta.2+41.sha-c31e78f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13435,7 +13435,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.2+40.sha-bc652a2');
+    var VERSION$1 = new Version('7.1.0-beta.2+41.sha-c31e78f');
 
     /**
      * @license
@@ -37096,7 +37096,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-beta.2+40.sha-bc652a2');
+    var VERSION$2 = new Version$1('7.1.0-beta.2+41.sha-c31e78f');
 
     /**
      * @license
@@ -49504,7 +49504,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-beta.2+40.sha-bc652a2');
+    var VERSION$3 = new Version$1('7.1.0-beta.2+41.sha-c31e78f');
 
     /**
      * @license
