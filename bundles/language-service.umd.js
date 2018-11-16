383351ccc0a37498ea9492be622697c959922cbe
Misko Hevery <misko@hevery.com>
2018-11-16 20:39:54 +0000

    41b5ea1 feat(bazel): Bazel workspace schematics (#26971)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.4+2.sha-597d14f
+ * @license Angular v7.0.4+3.sha-41b5ea1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13305,7 +13305,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.0.4+2.sha-597d14f');
+    var VERSION$1 = new Version('7.0.4+3.sha-41b5ea1');
 
     /**
      * @license
@@ -36571,7 +36571,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.0.4+2.sha-597d14f');
+    var VERSION$2 = new Version$1('7.0.4+3.sha-41b5ea1');
 
     /**
      * @license
@@ -48993,7 +48993,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.0.4+2.sha-597d14f');
+    var VERSION$3 = new Version$1('7.0.4+3.sha-41b5ea1');
 
     /**
      * @license
