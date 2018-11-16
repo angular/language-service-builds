177477dc4167d1ca6d8d5825751ac7d239b60185
Misko Hevery <misko@hevery.com>
2018-11-16 20:52:12 +0000

    a4934a7 docs: fix code examples style (#26573)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+10.sha-91bffa9
+ * @license Angular v7.1.0-rc.0+11.sha-a4934a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13424,7 +13424,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+10.sha-91bffa9');
+    var VERSION$1 = new Version('7.1.0-rc.0+11.sha-a4934a7');
 
     /**
      * @license
@@ -44015,7 +44015,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+10.sha-91bffa9');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+11.sha-a4934a7');
 
     /**
      * @license
@@ -56483,7 +56483,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+10.sha-91bffa9');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+11.sha-a4934a7');
 
     /**
      * @license
