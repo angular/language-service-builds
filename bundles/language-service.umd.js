a1fb7790f447ad7c512b7fc31f37f400df9d6273
Misko Hevery <misko@hevery.com>
2018-11-16 20:48:20 +0000

    91bffa9 fix(ivy): set encapsulation metadata (#27115) PR Close #27115

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+9.sha-60800da
+ * @license Angular v7.1.0-rc.0+10.sha-91bffa9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12952,6 +12952,10 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             var strings = styleValues.map(function (str) { return literal(str); });
             definitionMap.set('styles', literalArr(strings));
         }
+        // Only set view encapsulation if it's not the default value
+        if (meta.encapsulation !== null && meta.encapsulation !== ViewEncapsulation.Emulated) {
+            definitionMap.set('encapsulation', literal(meta.encapsulation));
+        }
         // e.g. `animations: [trigger('123', [])]`
         if (meta.animations !== null) {
             definitionMap.set('data', literalMap([{ key: 'animations', value: meta.animations, quoted: false }]));
@@ -13435,7 +13439,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+9.sha-60800da');
+    var VERSION$1 = new Version('7.1.0-rc.0+10.sha-91bffa9');
 
     /**
      * @license
@@ -37175,7 +37179,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+9.sha-60800da');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+10.sha-91bffa9');
 
     /**
      * @license
@@ -49587,7 +49591,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+9.sha-60800da');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+10.sha-91bffa9');
 
     /**
      * @license
