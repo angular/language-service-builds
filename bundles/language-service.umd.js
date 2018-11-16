f2ac6d0344d9822245c724aabb80aecc558df747
Misko Hevery <misko@hevery.com>
2018-11-16 20:47:30 +0000

    60800da fix(ivy): export elementContainerStart/End instructions (#27053)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+8.sha-b07bd30
+ * @license Angular v7.1.0-rc.0+9.sha-60800da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3557,8 +3557,8 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         Identifiers.elementProperty = { name: 'ɵelementProperty', moduleName: CORE$1 };
         Identifiers.elementAttribute = { name: 'ɵelementAttribute', moduleName: CORE$1 };
         Identifiers.elementClassProp = { name: 'ɵelementClassProp', moduleName: CORE$1 };
-        Identifiers.elementContainerStart = { name: 'ɵEC', moduleName: CORE$1 };
-        Identifiers.elementContainerEnd = { name: 'ɵeC', moduleName: CORE$1 };
+        Identifiers.elementContainerStart = { name: 'ɵelementContainerStart', moduleName: CORE$1 };
+        Identifiers.elementContainerEnd = { name: 'ɵelementContainerEnd', moduleName: CORE$1 };
         Identifiers.elementStyling = { name: 'ɵelementStyling', moduleName: CORE$1 };
         Identifiers.elementStylingMap = { name: 'ɵelementStylingMap', moduleName: CORE$1 };
         Identifiers.elementStyleProp = { name: 'ɵelementStyleProp', moduleName: CORE$1 };
@@ -13420,7 +13420,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+8.sha-b07bd30');
+    var VERSION$1 = new Version('7.1.0-rc.0+9.sha-60800da');
 
     /**
      * @license
@@ -43079,8 +43079,8 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         'ɵelementStart': elementStart,
         'ɵelementEnd': elementEnd,
         'ɵelement': element,
-        'ɵEC': elementContainerStart,
-        'ɵeC': elementContainerEnd,
+        'ɵelementContainerStart': elementContainerStart,
+        'ɵelementContainerEnd': elementContainerEnd,
         'ɵpureFunction0': pureFunction0,
         'ɵpureFunction1': pureFunction1,
         'ɵpureFunction2': pureFunction2,
@@ -44011,7 +44011,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+8.sha-b07bd30');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+9.sha-60800da');
 
     /**
      * @license
@@ -56479,7 +56479,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+8.sha-b07bd30');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+9.sha-60800da');
 
     /**
      * @license
