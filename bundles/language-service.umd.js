86aa233a5a132f052e9db73e828236f2e1e3933d
Misko Hevery <misko@hevery.com>
2018-11-16 20:46:16 +0000

    b07bd30 feat(bazel): Bazel workspace schematics (#26971)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+7.sha-4f965ad
+ * @license Angular v7.1.0-rc.0+8.sha-b07bd30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13435,7 +13435,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+7.sha-4f965ad');
+    var VERSION$1 = new Version('7.1.0-rc.0+8.sha-b07bd30');
 
     /**
      * @license
@@ -37175,7 +37175,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+7.sha-4f965ad');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+8.sha-b07bd30');
 
     /**
      * @license
@@ -49587,7 +49587,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+7.sha-4f965ad');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+8.sha-b07bd30');
 
     /**
      * @license
