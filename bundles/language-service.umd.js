76067b1baf13d98c101af9ccf3d60addb11133ff
Misko Hevery <misko@hevery.com>
2018-11-17 20:43:02 +0000

    81e975a build: update to latest Bazel rules_typescript (#27138)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+15.sha-20ea5b5
+ * @license Angular v7.1.0-rc.0+16.sha-81e975a.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15120,7 +15120,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+15.sha-20ea5b5');
+    var VERSION$1 = new Version('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
 
     /**
      * @license
@@ -44813,7 +44813,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+15.sha-20ea5b5');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
 
     /**
      * @license
@@ -57281,7 +57281,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+15.sha-20ea5b5');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
 
     /**
      * @license
