752a4add2ea9c991241a2256da280eb2aaf66626
Miško Hevery <misko@hevery.com>
2018-11-21 16:09:38 +0000

    a72250b build(bazel): Add Bazel builders (#27141)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+31.sha-026b7e3
+ * @license Angular v7.1.0-rc.0+32.sha-a72250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+31.sha-026b7e3');
+    var VERSION$1 = new Version('7.1.0-rc.0+32.sha-a72250b');
 
     /**
      * @license
@@ -33173,7 +33173,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+31.sha-026b7e3');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+32.sha-a72250b');
 
     /**
      * @license
@@ -50337,7 +50337,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+31.sha-026b7e3');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+32.sha-a72250b');
 
     /**
      * @license
