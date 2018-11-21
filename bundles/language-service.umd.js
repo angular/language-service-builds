438253e1210508b2df7fdeb8b77fadca74f51e23
Miško Hevery <misko@hevery.com>
2018-11-21 17:46:13 +0000

    0d9b27f fix(ivy): let ngcc transform @angular/core typings with relative imports (#27055)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+35.sha-8ce59a5
+ * @license Angular v7.1.0-rc.0+37.sha-0d9b27f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15127,7 +15127,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+35.sha-8ce59a5');
+    var VERSION$1 = new Version('7.1.0-rc.0+37.sha-0d9b27f');
 
     /**
      * @license
@@ -37312,7 +37312,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0-rc.0+35.sha-8ce59a5');
+    var VERSION$2 = new Version$1('7.1.0-rc.0+37.sha-0d9b27f');
 
     /**
      * @license
@@ -57384,7 +57384,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0-rc.0+35.sha-8ce59a5');
+    var VERSION$3 = new Version$1('7.1.0-rc.0+37.sha-0d9b27f');
 
     /**
      * @license
