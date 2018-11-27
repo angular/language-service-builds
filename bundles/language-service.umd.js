d42c712660cbfa98918c48693eb5b0c1d9368eff
Jason Aden <jasonaden@google.com>
2018-11-27 18:49:16 +0000

    a7ba05a fix(ivy): fix directive instantiation at slots above 2^12 (#27280)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+18.sha-d62da4d
+ * @license Angular v7.1.0+19.sha-a7ba05a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15133,7 +15133,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+18.sha-d62da4d');
+    var VERSION$1 = new Version('7.1.0+19.sha-a7ba05a');
 
     /**
      * @license
@@ -33208,7 +33208,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+18.sha-d62da4d');
+    var VERSION$2 = new Version$1('7.1.0+19.sha-a7ba05a');
 
     /**
      * @license
@@ -50324,7 +50324,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+18.sha-d62da4d');
+    var VERSION$3 = new Version$1('7.1.0+19.sha-a7ba05a');
 
     /**
      * @license
