137d29171c98f26396902aa43ff69f2f69d8dbd1
Igor Minar <igor@angular.io>
2018-11-30 06:10:03 +0000

    2d51579 docs: move Minko to Angular (#27339)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+50.sha-8ceea0f
+ * @license Angular v7.1.0+52.sha-2d51579
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15133,7 +15133,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+50.sha-8ceea0f');
+    var VERSION$1 = new Version('7.1.0+52.sha-2d51579');
 
     /**
      * @license
@@ -33268,7 +33268,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+50.sha-8ceea0f');
+    var VERSION$2 = new Version$1('7.1.0+52.sha-2d51579');
 
     /**
      * @license
@@ -50464,7 +50464,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+50.sha-8ceea0f');
+    var VERSION$3 = new Version$1('7.1.0+52.sha-2d51579');
 
     /**
      * @license
