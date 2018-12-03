a138beb54f5fccc2eb5c831e032f67c77287994b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:08:15 +0000

    06d4a0c feat(bazel): ng-new schematics with Bazel (#27277)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+112.sha-7ec05b4
+ * @license Angular v7.1.0+113.sha-06d4a0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15156,7 +15156,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+112.sha-7ec05b4');
+    var VERSION$1 = new Version('7.1.0+113.sha-06d4a0c');
 
     /**
      * @license
@@ -33235,7 +33235,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+112.sha-7ec05b4');
+    var VERSION$2 = new Version$1('7.1.0+113.sha-06d4a0c');
 
     /**
      * @license
@@ -50424,7 +50424,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+112.sha-7ec05b4');
+    var VERSION$3 = new Version$1('7.1.0+113.sha-06d4a0c');
 
     /**
      * @license
