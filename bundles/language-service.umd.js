73e7ba9ca4918b2efd9893738e290fee983e3934
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 17:43:25 +0000

    6f5c124 fix(ivy): proper accounting of host vars in case of inherited Directives (#27392)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+107.sha-931a636
+ * @license Angular v7.1.0+108.sha-6f5c124
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15147,7 +15147,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+107.sha-931a636');
+    var VERSION$1 = new Version('7.1.0+108.sha-6f5c124');
 
     /**
      * @license
@@ -33226,7 +33226,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+107.sha-931a636');
+    var VERSION$2 = new Version$1('7.1.0+108.sha-6f5c124');
 
     /**
      * @license
@@ -50422,7 +50422,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+107.sha-931a636');
+    var VERSION$3 = new Version$1('7.1.0+108.sha-6f5c124');
 
     /**
      * @license
