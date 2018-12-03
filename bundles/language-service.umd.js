32333a058830b702286a8701be3da653957e78b3
Igor Minar <igor@angular.io>
2018-12-03 16:52:43 +0000

    a082f64 fix(ivy): fix style prop instructions to account for zero-based values (#27270)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+105.sha-d847051
+ * @license Angular v7.1.0+106.sha-a082f64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15147,7 +15147,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+105.sha-d847051');
+    var VERSION$1 = new Version('7.1.0+106.sha-a082f64');
 
     /**
      * @license
@@ -33226,7 +33226,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+105.sha-d847051');
+    var VERSION$2 = new Version$1('7.1.0+106.sha-a082f64');
 
     /**
      * @license
@@ -50422,7 +50422,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+105.sha-d847051');
+    var VERSION$3 = new Version$1('7.1.0+106.sha-a082f64');
 
     /**
      * @license
