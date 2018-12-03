6e2787860ed52989d9dc27a398c2f81d83a08b8d
Filipe Silva <filipematossilva@gmail.com>
2018-12-03 19:56:23 +0000

    2f888b8 ci: test ts-api-guardian on windows

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+103.sha-4fd3402
+ * @license Angular v7.1.0+103.sha-2f888b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15147,7 +15147,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+103.sha-4fd3402');
+    var VERSION$1 = new Version('7.1.0+103.sha-2f888b8');
 
     /**
      * @license
@@ -33226,7 +33226,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+103.sha-4fd3402');
+    var VERSION$2 = new Version$1('7.1.0+103.sha-2f888b8');
 
     /**
      * @license
@@ -50422,7 +50422,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+103.sha-4fd3402');
+    var VERSION$3 = new Version$1('7.1.0+103.sha-2f888b8');
 
     /**
      * @license
