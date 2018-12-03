236dad8ddab4912ea2840afca50db85c1e3be9a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 19:29:36 +0000

    2a39425 fix(ivy): temporary hack to set host styles and static classes (#27385)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+109.sha-7d89cff
+ * @license Angular v7.1.0+110.sha-2a39425
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15147,7 +15147,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+109.sha-7d89cff');
+    var VERSION$1 = new Version('7.1.0+110.sha-2a39425');
 
     /**
      * @license
@@ -33226,7 +33226,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+109.sha-7d89cff');
+    var VERSION$2 = new Version$1('7.1.0+110.sha-2a39425');
 
     /**
      * @license
@@ -50415,7 +50415,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+109.sha-7d89cff');
+    var VERSION$3 = new Version$1('7.1.0+110.sha-2a39425');
 
     /**
      * @license
