d661d87c9887ab772e0eb38c94c45c07c726f398
Igor Minar <igor@angular.io>
2018-12-04 22:33:32 +0000

    8087b6b fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+18.sha-0378792
+ * @license Angular v7.1.1+23.sha-8087b6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.1+18.sha-0378792');
+    var VERSION$1 = new Version('7.1.1+23.sha-8087b6b');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.1+18.sha-0378792');
+    var VERSION$2 = new Version$1('7.1.1+23.sha-8087b6b');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.1+18.sha-0378792');
+    var VERSION$3 = new Version$1('7.1.1+23.sha-8087b6b');
 
     /**
      * @license
