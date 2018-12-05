c4b68f7eaa14b1b3ffee7c3d8855a4bb464faf96
Igor Minar <igor@angular.io>
2018-12-05 04:40:24 +0000

    9b4d959 fix(bazel): do not throw if ts compile action does not create esm5 outputs (#27401)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+23.sha-8087b6b
+ * @license Angular v7.1.1+24.sha-9b4d959
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.1+23.sha-8087b6b');
+    var VERSION$1 = new Version('7.1.1+24.sha-9b4d959');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.1+23.sha-8087b6b');
+    var VERSION$2 = new Version$1('7.1.1+24.sha-9b4d959');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.1+23.sha-8087b6b');
+    var VERSION$3 = new Version$1('7.1.1+24.sha-9b4d959');
 
     /**
      * @license
