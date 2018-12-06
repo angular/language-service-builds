73123ac58e2f20e9d97a248bd5aee4df7105a7ff
Igor Minar <igor@angular.io>
2018-12-06 22:12:21 +0000

    8fa7e93 test(ivy): enable router tests with pending timers (FW-768) (#27509)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+207.sha-cad6714
+ * @license Angular v7.1.0+208.sha-8fa7e93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15165,7 +15165,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+207.sha-cad6714');
+    var VERSION$1 = new Version('7.1.0+208.sha-8fa7e93');
 
     /**
      * @license
@@ -37708,7 +37708,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+207.sha-cad6714');
+    var VERSION$2 = new Version$1('7.1.0+208.sha-8fa7e93');
 
     /**
      * @license
@@ -57947,7 +57947,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+207.sha-cad6714');
+    var VERSION$3 = new Version$1('7.1.0+208.sha-8fa7e93');
 
     /**
      * @license
