6cb396fba6be908e65a2e550c9cdf31c9e1771a2
Igor Minar <igor@angular.io>
2018-12-06 05:19:23 +0000

    6740d0d build: remove obsolete gulp build task (#27386)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+185.sha-a315ded
+ * @license Angular v7.1.0+194.sha-6740d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15176,7 +15176,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+185.sha-a315ded');
+    var VERSION$1 = new Version('7.1.0+194.sha-6740d0d');
 
     /**
      * @license
@@ -33479,7 +33479,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+185.sha-a315ded');
+    var VERSION$2 = new Version$1('7.1.0+194.sha-6740d0d');
 
     /**
      * @license
@@ -50685,7 +50685,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+185.sha-a315ded');
+    var VERSION$3 = new Version$1('7.1.0+194.sha-6740d0d');
 
     /**
      * @license
