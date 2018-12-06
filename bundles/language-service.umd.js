5e88a4f5ca1d9b6f870f092917170ad7662837de
Igor Minar <igor@angular.io>
2018-12-06 18:01:04 +0000

    7c52d10 Revert "ci: test ts-api-guardian on windows (#27205)"

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+198.sha-160cace
+ * @license Angular v7.1.0+199.sha-7c52d10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15176,7 +15176,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+198.sha-160cace');
+    var VERSION$1 = new Version('7.1.0+199.sha-7c52d10');
 
     /**
      * @license
@@ -33479,7 +33479,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+198.sha-160cace');
+    var VERSION$2 = new Version$1('7.1.0+199.sha-7c52d10');
 
     /**
      * @license
@@ -50685,7 +50685,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+198.sha-160cace');
+    var VERSION$3 = new Version$1('7.1.0+199.sha-7c52d10');
 
     /**
      * @license
