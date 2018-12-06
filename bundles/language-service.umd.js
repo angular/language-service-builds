992cf3b7988190d7086ae622dd9ec180d87cceb3
Igor Minar <igor@angular.io>
2018-12-06 17:47:25 +0000

    84f2928 fix(compiler-cli): ngtsc shim files not being generated on case-insensitive platforms (#27466)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+53.sha-d7c72fc
+ * @license Angular v7.1.1+56.sha-84f2928
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.1+53.sha-d7c72fc');
+    var VERSION$1 = new Version('7.1.1+56.sha-84f2928');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.1+53.sha-d7c72fc');
+    var VERSION$2 = new Version$1('7.1.1+56.sha-84f2928');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.1+53.sha-d7c72fc');
+    var VERSION$3 = new Version$1('7.1.1+56.sha-84f2928');
 
     /**
      * @license
