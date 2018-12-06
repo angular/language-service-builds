233add77c0debde5383bc69ba137db86d3ae9eac
Igor Minar <igor@angular.io>
2018-12-06 22:12:04 +0000

    cad6714 fix(ivy): "select" attribute on <ng-content> should not be case-sensitive (FW-789) (#27500)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+209.sha-913563a
+ * @license Angular v7.1.0+207.sha-cad6714
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15165,7 +15165,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+209.sha-913563a');
+    var VERSION$1 = new Version('7.1.0+207.sha-cad6714');
 
     /**
      * @license
@@ -37708,7 +37708,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.0+209.sha-913563a');
+    var VERSION$2 = new Version$1('7.1.0+207.sha-cad6714');
 
     /**
      * @license
@@ -57947,7 +57947,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.0+209.sha-913563a');
+    var VERSION$3 = new Version$1('7.1.0+207.sha-cad6714');
 
     /**
      * @license
