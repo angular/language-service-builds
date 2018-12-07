93c269c5c86d40bc134cd42e60500413575dac7d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:56:20 +0000

    74346e9 style: run latest buildifier binary (#27489)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2
+ * @license Angular v7.1.2+3.sha-74346e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.2');
+    var VERSION$1 = new Version('7.1.2+3.sha-74346e9');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.2');
+    var VERSION$2 = new Version$1('7.1.2+3.sha-74346e9');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.2');
+    var VERSION$3 = new Version$1('7.1.2+3.sha-74346e9');
 
     /**
      * @license
