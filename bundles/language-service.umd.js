98b72eb2c083cb26d344f71480250fed538f145c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:03:13 +0000

    055dc2d docs: file new issue needs to have opener choose type from template (#27400)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+11.sha-2a35471
+ * @license Angular v7.2.0-beta.1+10.sha-055dc2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15185,7 +15185,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.1+11.sha-2a35471');
+    var VERSION$1 = new Version('7.2.0-beta.1+10.sha-055dc2d');
 
     /**
      * @license
@@ -33499,7 +33499,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.1+11.sha-2a35471');
+    var VERSION$2 = new Version$1('7.2.0-beta.1+10.sha-055dc2d');
 
     /**
      * @license
@@ -50736,7 +50736,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.1+11.sha-2a35471');
+    var VERSION$3 = new Version$1('7.2.0-beta.1+10.sha-055dc2d');
 
     /**
      * @license
