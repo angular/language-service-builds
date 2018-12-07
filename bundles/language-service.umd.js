fdcde13e3a31b0647b4bdcb274000c2a0ced1115
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:48:23 +0000

    aa48810 feat(ivy): generate flat module index files (#27497)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+1.sha-6bd28e6
+ * @license Angular v7.2.0-beta.1+3.sha-aa48810
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15165,7 +15165,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.1+1.sha-6bd28e6');
+    var VERSION$1 = new Version('7.2.0-beta.1+3.sha-aa48810');
 
     /**
      * @license
@@ -37708,7 +37708,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.1+1.sha-6bd28e6');
+    var VERSION$2 = new Version$1('7.2.0-beta.1+3.sha-aa48810');
 
     /**
      * @license
@@ -57947,7 +57947,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.1+1.sha-6bd28e6');
+    var VERSION$3 = new Version$1('7.2.0-beta.1+3.sha-aa48810');
 
     /**
      * @license
