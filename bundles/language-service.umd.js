dd5180750d042dbe02854ede330d34cd8274d317
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 00:59:35 +0000

    50687e1 build: fix type-check errors introduced during rules_ts 0.21 (#27586)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+22.sha-05cdfb9
+ * @license Angular v7.2.0-beta.1+24.sha-50687e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15170,7 +15170,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.1+22.sha-05cdfb9');
+    var VERSION$1 = new Version('7.2.0-beta.1+24.sha-50687e1');
 
     /**
      * @license
@@ -37703,7 +37703,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.1+22.sha-05cdfb9');
+    var VERSION$2 = new Version$1('7.2.0-beta.1+24.sha-50687e1');
 
     /**
      * @license
@@ -57942,7 +57942,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.1+22.sha-05cdfb9');
+    var VERSION$3 = new Version$1('7.2.0-beta.1+24.sha-50687e1');
 
     /**
      * @license
