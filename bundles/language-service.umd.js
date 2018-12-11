4e88f5098863d797d52fdc5458b239f27aa213bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:59:50 +0000

    3cb6dad docs: update npm scripts to support angular 7 (#27334)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+29.sha-ceb14de
+ * @license Angular v7.2.0-beta.1+28.sha-3cb6dad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14796,9 +14796,8 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         };
         if (bindings) {
             var hostVarsCountFn = function (numSlots) {
-                var originalVarsCount = totalHostVarsCount;
                 totalHostVarsCount += numSlots;
-                return originalVarsCount;
+                return hostVarsCount;
             };
             var valueConverter = new ValueConverter(constantPool, 
             /* new nodes are illegal here */ function () { return error('Unexpected node'); }, hostVarsCountFn, 
@@ -15171,7 +15170,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.1+29.sha-ceb14de');
+    var VERSION$1 = new Version('7.2.0-beta.1+28.sha-3cb6dad');
 
     /**
      * @license
@@ -37704,7 +37703,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.1+29.sha-ceb14de');
+    var VERSION$2 = new Version$1('7.2.0-beta.1+28.sha-3cb6dad');
 
     /**
      * @license
@@ -57943,7 +57942,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.1+29.sha-ceb14de');
+    var VERSION$3 = new Version$1('7.2.0-beta.1+28.sha-3cb6dad');
 
     /**
      * @license
