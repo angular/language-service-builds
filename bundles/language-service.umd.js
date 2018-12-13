0fbf6559d391363148fd8aa21951947bd5ac1916
Miško Hevery <misko@hevery.com>
2018-12-13 19:45:26 +0000

    e3a2ca5 fix(docs-infra): fix top menu item clickable area (#27633)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
+ * @license Angular v7.2.0-beta.2+15.sha-e3a2ca5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15187,7 +15187,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+14.sha-1cc08b4');
+    var VERSION$1 = new Version('7.2.0-beta.2+15.sha-e3a2ca5');
 
     /**
      * @license
@@ -33492,7 +33492,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+14.sha-1cc08b4');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+15.sha-e3a2ca5');
 
     /**
      * @license
@@ -50729,7 +50729,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+14.sha-1cc08b4');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+15.sha-e3a2ca5');
 
     /**
      * @license
