611a7e7e4e6ec8c13d1fc5fbe18b70bfe6f0f570
Miško Hevery <misko@hevery.com>
2018-12-13 19:46:29 +0000

    1cc08b4 fix(bazel): fix major/minor semver check between @angular/bazel npm packager version and angular bazel repo version (#27635)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+12.sha-f00af3e
+ * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15172,7 +15172,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+12.sha-f00af3e');
+    var VERSION$1 = new Version('7.2.0-beta.2+14.sha-1cc08b4');
 
     /**
      * @license
@@ -37709,7 +37709,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+12.sha-f00af3e');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+14.sha-1cc08b4');
 
     /**
      * @license
@@ -57948,7 +57948,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+12.sha-f00af3e');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+14.sha-1cc08b4');
 
     /**
      * @license
