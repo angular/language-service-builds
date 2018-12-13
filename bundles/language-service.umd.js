71461a84d61fc63c7474c30d8ec4856c1d0acd5a
Miško Hevery <misko@hevery.com>
2018-12-13 19:48:55 +0000

    8313ffc fix(bazel): Load http_archive and rules_nodejs dependencies (#27609)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+13.sha-e318081
+ * @license Angular v7.2.0-beta.2+18.sha-8313ffc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15172,7 +15172,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+13.sha-e318081');
+    var VERSION$1 = new Version('7.2.0-beta.2+18.sha-8313ffc');
 
     /**
      * @license
@@ -37709,7 +37709,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+13.sha-e318081');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+18.sha-8313ffc');
 
     /**
      * @license
@@ -57948,7 +57948,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+13.sha-e318081');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+18.sha-8313ffc');
 
     /**
      * @license
