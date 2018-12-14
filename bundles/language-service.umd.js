a461602fa004faf73a9563b0d1e53e62d5c9d0ac
Miško Hevery <misko@hevery.com>
2018-12-14 19:05:33 +0000

    a8ebc83 fix(ivy): support complex type nodes in ModuleWithProviders (#27562)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+39.sha-d0d351c
+ * @license Angular v7.2.0-beta.2+37.sha-a8ebc83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15196,7 +15196,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+39.sha-d0d351c');
+    var VERSION$1 = new Version('7.2.0-beta.2+37.sha-a8ebc83');
 
     /**
      * @license
@@ -33504,7 +33504,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+39.sha-d0d351c');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+37.sha-a8ebc83');
 
     /**
      * @license
@@ -50741,7 +50741,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+39.sha-d0d351c');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+37.sha-a8ebc83');
 
     /**
      * @license
