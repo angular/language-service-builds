4ee74f2a10c3576dc8a4f172589a11f182529172
Miško Hevery <misko@hevery.com>
2018-12-14 23:58:51 +0000

    78bd3c7 docs(animations): rename animateChild() to animate() (#26184)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+29.sha-b224057
+ * @license Angular v7.1.3+30.sha-78bd3c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+29.sha-b224057');
+    var VERSION$1 = new Version('7.1.3+30.sha-78bd3c7');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.3+29.sha-b224057');
+    var VERSION$2 = new Version$1('7.1.3+30.sha-78bd3c7');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.3+29.sha-b224057');
+    var VERSION$3 = new Version$1('7.1.3+30.sha-78bd3c7');
 
     /**
      * @license
