124b014b604c6d7de05d24f7651999999441a74c
Miško Hevery <misko@hevery.com>
2018-12-14 18:55:46 +0000

    63d395c build: restrict visibility of npm_package targets (#27611)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+22.sha-fe8c6b0
+ * @license Angular v7.1.3+21.sha-63d395c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+22.sha-fe8c6b0');
+    var VERSION$1 = new Version('7.1.3+21.sha-63d395c');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.3+22.sha-fe8c6b0');
+    var VERSION$2 = new Version$1('7.1.3+21.sha-63d395c');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.3+22.sha-fe8c6b0');
+    var VERSION$3 = new Version$1('7.1.3+21.sha-63d395c');
 
     /**
      * @license
