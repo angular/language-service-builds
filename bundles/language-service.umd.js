e4ff3d65f4a6bbbbb208150ead5557b85ec5f88d
Miško Hevery <misko@hevery.com>
2018-12-14 18:50:04 +0000

    f5e7208 build: re-enable saucelabs non-verbose logging (#27657)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+15.sha-52fd4a2
+ * @license Angular v7.1.3+20.sha-f5e7208
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+15.sha-52fd4a2');
+    var VERSION$1 = new Version('7.1.3+20.sha-f5e7208');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.3+15.sha-52fd4a2');
+    var VERSION$2 = new Version$1('7.1.3+20.sha-f5e7208');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.3+15.sha-52fd4a2');
+    var VERSION$3 = new Version$1('7.1.3+20.sha-f5e7208');
 
     /**
      * @license
