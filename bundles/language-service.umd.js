9f08d31280472766ed96851b986c8f590561f83a
Miško Hevery <misko@hevery.com>
2018-12-15 00:04:53 +0000

    b39efdd fix(common): KeyValuePipe should return empty array for empty objects (#27258)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+42.sha-0b78f54
+ * @license Angular v7.2.0-beta.2+44.sha-b39efdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15181,7 +15181,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+42.sha-0b78f54');
+    var VERSION$1 = new Version('7.2.0-beta.2+44.sha-b39efdd');
 
     /**
      * @license
@@ -37721,7 +37721,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+42.sha-0b78f54');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+44.sha-b39efdd');
 
     /**
      * @license
@@ -57960,7 +57960,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+42.sha-0b78f54');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+44.sha-b39efdd');
 
     /**
      * @license
