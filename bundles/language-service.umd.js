a11204527334ec20a71d2862ff96d726023847c9
Miško Hevery <misko@hevery.com>
2018-12-15 00:06:25 +0000

    f4a797d fix(ivy): prevent ngcc from inadvertently dropping Ivy definitions (#27159)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+49.sha-4df82bd
+ * @license Angular v7.2.0-beta.2+50.sha-f4a797d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15196,7 +15196,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+49.sha-4df82bd');
+    var VERSION$1 = new Version('7.2.0-beta.2+50.sha-f4a797d');
 
     /**
      * @license
@@ -33530,7 +33530,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+49.sha-4df82bd');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+50.sha-f4a797d');
 
     /**
      * @license
@@ -50768,7 +50768,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+49.sha-4df82bd');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+50.sha-f4a797d');
 
     /**
      * @license
