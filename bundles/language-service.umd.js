5c99bc87f7094244a41c516b55560bb7d4ab5d6e
Miško Hevery <misko@hevery.com>
2018-12-15 00:05:24 +0000

    df123e0 fix(animations): mark actual descendant node as disabled (#26180)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+44.sha-b39efdd
+ * @license Angular v7.2.0-beta.2+46.sha-df123e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15181,7 +15181,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+44.sha-b39efdd');
+    var VERSION$1 = new Version('7.2.0-beta.2+46.sha-df123e0');
 
     /**
      * @license
@@ -37721,7 +37721,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+44.sha-b39efdd');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+46.sha-df123e0');
 
     /**
      * @license
@@ -57960,7 +57960,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+44.sha-b39efdd');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+46.sha-df123e0');
 
     /**
      * @license
