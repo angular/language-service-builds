bec761c37ff4bc805b3e09ef478b12f3326ce940
Miško Hevery <misko@hevery.com>
2018-12-17 18:07:57 +0000

    d1a14f6 docs: remove misplaced code in Universal guide (#26865)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+31.sha-ce4cff6
+ * @license Angular v7.1.3+32.sha-d1a14f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15142,7 +15142,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+31.sha-ce4cff6');
+    var VERSION$1 = new Version('7.1.3+32.sha-d1a14f6');
 
     /**
      * @license
@@ -33194,7 +33194,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.1.3+31.sha-ce4cff6');
+    var VERSION$2 = new Version$1('7.1.3+32.sha-d1a14f6');
 
     /**
      * @license
@@ -50358,7 +50358,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.1.3+31.sha-ce4cff6');
+    var VERSION$3 = new Version$1('7.1.3+32.sha-d1a14f6');
 
     /**
      * @license
