594faa235dd600bccab66c4815bffa08a461cf8c
Miško Hevery <misko@hevery.com>
2018-12-17 18:52:10 +0000

    d132bae test(ivy): add more test for content projection with ngIf / ngFor (#27705)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+61.sha-44ec66d
+ * @license Angular v7.2.0-beta.2+63.sha-d132bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15193,7 +15193,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+61.sha-44ec66d');
+    var VERSION$1 = new Version('7.2.0-beta.2+63.sha-d132bae');
 
     /**
      * @license
@@ -33527,7 +33527,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+61.sha-44ec66d');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+63.sha-d132bae');
 
     /**
      * @license
@@ -50765,7 +50765,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+61.sha-44ec66d');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+63.sha-d132bae');
 
     /**
      * @license
