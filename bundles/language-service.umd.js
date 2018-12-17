72c3349b6732800cf4ffedd08fb1bc07d46cd523
Miško Hevery <misko@hevery.com>
2018-12-17 18:16:32 +0000

    44ec66d test(ivy): enable running cli-hello-world-ivy integration test suite (#27438)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+55.sha-a726338
+ * @license Angular v7.2.0-beta.2+61.sha-44ec66d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15193,7 +15193,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+55.sha-a726338');
+    var VERSION$1 = new Version('7.2.0-beta.2+61.sha-44ec66d');
 
     /**
      * @license
@@ -33527,7 +33527,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+55.sha-a726338');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+61.sha-44ec66d');
 
     /**
      * @license
@@ -50765,7 +50765,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+55.sha-a726338');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+61.sha-44ec66d');
 
     /**
      * @license
