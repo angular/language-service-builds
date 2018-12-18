577802bf80e9e0307b0f9ee7cf80e9cf04aa4f5c
Miško Hevery <misko@hevery.com>
2018-12-18 18:02:34 +0000

    8d0a3ce build: use https link to editorconfig.org in .editorconfig (#27664)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+76.sha-f57916c
+ * @license Angular v7.2.0-beta.2+80.sha-8d0a3ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15193,7 +15193,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+76.sha-f57916c');
+    var VERSION$1 = new Version('7.2.0-beta.2+80.sha-8d0a3ce');
 
     /**
      * @license
@@ -33558,7 +33558,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+76.sha-f57916c');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+80.sha-8d0a3ce');
 
     /**
      * @license
@@ -50796,7 +50796,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+76.sha-f57916c');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+80.sha-8d0a3ce');
 
     /**
      * @license
