e2357e3222fb6e857249f932b28de08bf535c217
Miško Hevery <misko@hevery.com>
2018-12-18 21:47:59 +0000

    7901cd8 fix(ivy): add missing AttributeMarker export (#27485) PR Close #27485

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+89.sha-69198ba
+ * @license Angular v7.2.0-beta.2+90.sha-7901cd8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15210,7 +15210,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+89.sha-69198ba');
+    var VERSION$1 = new Version('7.2.0-beta.2+90.sha-7901cd8');
 
     /**
      * @license
@@ -33581,7 +33581,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+89.sha-69198ba');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+90.sha-7901cd8');
 
     /**
      * @license
@@ -50823,7 +50823,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+89.sha-69198ba');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+90.sha-7901cd8');
 
     /**
      * @license
