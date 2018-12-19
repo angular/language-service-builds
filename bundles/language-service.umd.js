bee51f2dfc1468b5198a5f4ea931d2e8a908da9a
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 21:33:28 +0000

    3ab25ab refactor: fix broken linting rules due to revert

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+4.sha-0604527
+ * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15210,7 +15210,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+4.sha-0604527');
+    var VERSION$1 = new Version('7.2.0-rc.0+5.sha-3ab25ab');
 
     /**
      * @license
@@ -33584,7 +33584,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+4.sha-0604527');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+5.sha-3ab25ab');
 
     /**
      * @license
@@ -50826,7 +50826,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+4.sha-0604527');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+5.sha-3ab25ab');
 
     /**
      * @license
