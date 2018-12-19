72465d537fa0aab289fceac284e18f7c0b17a61b
Igor Minar <igor@angular.io>
2018-12-19 18:24:52 +0000

    01a5c74 turns out that github silently converts '/ in team names to '-'

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-4d71c4c
+ * @license Angular v7.2.0-beta.2+78.sha-01a5c74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15193,7 +15193,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+77.sha-4d71c4c');
+    var VERSION$1 = new Version('7.2.0-beta.2+78.sha-01a5c74');
 
     /**
      * @license
@@ -33558,7 +33558,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-beta.2+77.sha-4d71c4c');
+    var VERSION$2 = new Version$1('7.2.0-beta.2+78.sha-01a5c74');
 
     /**
      * @license
@@ -50796,7 +50796,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-beta.2+77.sha-4d71c4c');
+    var VERSION$3 = new Version$1('7.2.0-beta.2+78.sha-01a5c74');
 
     /**
      * @license
