f216d0cc6505a8179e8f2d6303066e60eb519a99
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:43:56 +0000

    0604527 Revert "build: update gulp-clang-format dependency (#27712)" (#27759)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+3.sha-8f8572f
+ * @license Angular v7.2.0-rc.0+4.sha-0604527
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15210,7 +15210,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+3.sha-8f8572f');
+    var VERSION$1 = new Version('7.2.0-rc.0+4.sha-0604527');
 
     /**
      * @license
@@ -33584,7 +33584,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+3.sha-8f8572f');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+4.sha-0604527');
 
     /**
      * @license
@@ -50826,7 +50826,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+3.sha-8f8572f');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+4.sha-0604527');
 
     /**
      * @license
