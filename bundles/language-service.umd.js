f8ae3964d98ff122baefdcbc8e1c7e48a0d4d1bd
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:44:39 +0000

    e8f7241 build: fix our copy of Array#find typing (#27742)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+1.sha-a20b2f7
+ * @license Angular v7.2.0-rc.0+2.sha-e8f7241
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15195,7 +15195,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+1.sha-a20b2f7');
+    var VERSION$1 = new Version('7.2.0-rc.0+2.sha-e8f7241');
 
     /**
      * @license
@@ -37800,7 +37800,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+1.sha-a20b2f7');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+2.sha-e8f7241');
 
     /**
      * @license
@@ -58201,7 +58201,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+1.sha-a20b2f7');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+2.sha-e8f7241');
 
     /**
      * @license
