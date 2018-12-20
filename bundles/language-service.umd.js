0920d5fd13703bf6258c0dd0870c49c61ad5aa32
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:27:01 +0000

    4b70a4e feat(ivy): support NgModule metadata from calls that do not return ModuleWithProviders types (#27326)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+6.sha-b08f3ac
+ * @license Angular v7.2.0-rc.0+10.sha-4b70a4e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15195,7 +15195,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+6.sha-b08f3ac');
+    var VERSION$1 = new Version('7.2.0-rc.0+10.sha-4b70a4e');
 
     /**
      * @license
@@ -37815,7 +37815,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+6.sha-b08f3ac');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+10.sha-4b70a4e');
 
     /**
      * @license
@@ -58216,7 +58216,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+6.sha-b08f3ac');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+10.sha-4b70a4e');
 
     /**
      * @license
