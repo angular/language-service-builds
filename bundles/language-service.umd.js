0586d29aa4c49c3f9df1c1fc3043717f25e3886a
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 21:06:08 +0000

    509aa61 fix(ivy): apply all overrides from TestBed, not the last one only (#27734)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+23.sha-c5ce4e6
+ * @license Angular v7.2.0-rc.0+22.sha-509aa61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15227,7 +15227,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+23.sha-c5ce4e6');
+    var VERSION$1 = new Version('7.2.0-rc.0+22.sha-509aa61');
 
     /**
      * @license
@@ -33601,7 +33601,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+23.sha-c5ce4e6');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+22.sha-509aa61');
 
     /**
      * @license
@@ -50843,7 +50843,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+23.sha-c5ce4e6');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+22.sha-509aa61');
 
     /**
      * @license
