1a7441aebee315c256dca44bffcadbe4e995e4c1
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:44:54 +0000

    bba5e26 docs(forms): clarify the pattern validator behavior (#27560)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+21.sha-cdd737e
+ * @license Angular v7.2.0-rc.0+24.sha-bba5e26
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15212,7 +15212,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+21.sha-cdd737e');
+    var VERSION$1 = new Version('7.2.0-rc.0+24.sha-bba5e26');
 
     /**
      * @license
@@ -37832,7 +37832,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+21.sha-cdd737e');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+24.sha-bba5e26');
 
     /**
      * @license
@@ -58233,7 +58233,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+21.sha-cdd737e');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+24.sha-bba5e26');
 
     /**
      * @license
