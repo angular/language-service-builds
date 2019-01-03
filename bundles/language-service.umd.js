b5f7b5e8c86722d1b285d3d5469537cfddf62591
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:46 +0000

    a0a3648 docs: add more info on what is being cached (#27343)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+43.sha-a0a3648
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15370,7 +15370,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+40.sha-755c809');
+    var VERSION$1 = new Version('7.2.0-rc.0+43.sha-a0a3648');
 
     /**
      * @license
@@ -33771,7 +33771,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+40.sha-755c809');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+43.sha-a0a3648');
 
     /**
      * @license
@@ -51013,7 +51013,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+40.sha-755c809');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+43.sha-a0a3648');
 
     /**
      * @license
