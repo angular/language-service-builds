cdd6ebdd2126b801e18655dffb563bc7c36561ec
Ben Lesh <ben@benlesh.com>
2019-01-03 18:18:40 +0000

    1dc95c4 docs: fix typo in TOH http section (#26127)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+41.sha-5638c1d
+ * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15370,7 +15370,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+41.sha-5638c1d');
+    var VERSION$1 = new Version('7.2.0-rc.0+44.sha-1dc95c4');
 
     /**
      * @license
@@ -33771,7 +33771,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+41.sha-5638c1d');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+44.sha-1dc95c4');
 
     /**
      * @license
@@ -51013,7 +51013,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+41.sha-5638c1d');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+44.sha-1dc95c4');
 
     /**
      * @license
