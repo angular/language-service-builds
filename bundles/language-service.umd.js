480fcf35266306fc286fdbf4aab6f99a9cc867f9
Ben Lesh <ben@benlesh.com>
2019-01-03 18:21:08 +0000

    880b4aa fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+47.sha-5fee444
+ * @license Angular v7.2.0-rc.0+48.sha-880b4aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15370,7 +15370,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+47.sha-5fee444');
+    var VERSION$1 = new Version('7.2.0-rc.0+48.sha-880b4aa');
 
     /**
      * @license
@@ -33771,7 +33771,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+47.sha-5fee444');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+48.sha-880b4aa');
 
     /**
      * @license
@@ -51013,7 +51013,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+47.sha-5fee444');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+48.sha-880b4aa');
 
     /**
      * @license
