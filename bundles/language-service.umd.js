46dd4abcd95b5dd7a09ebb675743c8ec38ec4a45
Ben Lesh <ben@benlesh.com>
2019-01-03 18:19:14 +0000

    5638c1d docs: fix npm description in architecture modules guide (#27818)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+41.sha-5638c1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15355,7 +15355,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+40.sha-755c809');
+    var VERSION$1 = new Version('7.2.0-rc.0+41.sha-5638c1d');
 
     /**
      * @license
@@ -38383,7 +38383,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+40.sha-755c809');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+41.sha-5638c1d');
 
     /**
      * @license
@@ -58784,7 +58784,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+40.sha-755c809');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+41.sha-5638c1d');
 
     /**
      * @license
