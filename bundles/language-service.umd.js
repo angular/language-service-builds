aa04ec75a5b2b5be69a4c45f4f6337dd7df01651
Kara Erickson <karakara@google.com>
2019-01-04 20:28:47 +0000

    8a05199 test: remove bower and polymer benchmarks (#27931)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+52.sha-51a0bd2
+ * @license Angular v7.2.0-rc.0+53.sha-8a05199
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15397,7 +15397,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+52.sha-51a0bd2');
+    var VERSION$1 = new Version('7.2.0-rc.0+53.sha-8a05199');
 
     /**
      * @license
@@ -33799,7 +33799,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+52.sha-51a0bd2');
+    var VERSION$2 = new Version$1('7.2.0-rc.0+53.sha-8a05199');
 
     /**
      * @license
@@ -51041,7 +51041,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+52.sha-51a0bd2');
+    var VERSION$3 = new Version$1('7.2.0-rc.0+53.sha-8a05199');
 
     /**
      * @license
