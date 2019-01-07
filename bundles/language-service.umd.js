29bb3a26984814497fa773b8195022be04de259a
Kara Erickson <karakara@google.com>
2019-01-07 17:08:14 +0000

    e94e7ee docs: add Alyle UI to resources (#27954) PR Close #27954

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0
+ * @license Angular v7.2.0+2.sha-e94e7ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15421,7 +15421,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0');
+    var VERSION$1 = new Version('7.2.0+2.sha-e94e7ee');
 
     /**
      * @license
@@ -38476,7 +38476,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0');
+    var VERSION$2 = new Version$1('7.2.0+2.sha-e94e7ee');
 
     /**
      * @license
@@ -58880,7 +58880,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0');
+    var VERSION$3 = new Version$1('7.2.0+2.sha-e94e7ee');
 
     /**
      * @license
