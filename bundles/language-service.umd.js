35a9150be34ace40e11e4aaa61e2d4e4f673a738
Kara Erickson <karakara@google.com>
2019-01-07 16:26:15 +0000

    0efbb37 release: cut the v7.2.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
+ * @license Angular v7.2.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15421,7 +15421,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+71.sha-6b0c1a7');
+    var VERSION$1 = new Version('7.2.0');
 
     /**
      * @license
@@ -38476,7 +38476,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0-rc.0+71.sha-6b0c1a7');
+    var VERSION$2 = new Version$1('7.2.0');
 
     /**
      * @license
@@ -58880,7 +58880,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0-rc.0+71.sha-6b0c1a7');
+    var VERSION$3 = new Version$1('7.2.0');
 
     /**
      * @license
