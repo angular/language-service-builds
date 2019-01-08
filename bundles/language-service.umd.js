a4964acc5fa89bf09f18e7d866c75adf5240af1f
Kara Erickson <karakara@google.com>
2019-01-08 18:47:07 +0000

    917c09c ci: update recommended buildkite VM (#27728)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+32.sha-f547110
+ * @license Angular v7.2.0+33.sha-917c09c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15421,7 +15421,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+32.sha-f547110');
+    var VERSION$1 = new Version('7.2.0+33.sha-917c09c');
 
     /**
      * @license
@@ -38427,7 +38427,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+32.sha-f547110');
+    var VERSION$2 = new Version$1('7.2.0+33.sha-917c09c');
 
     /**
      * @license
@@ -58748,7 +58748,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+32.sha-f547110');
+    var VERSION$3 = new Version$1('7.2.0+33.sha-917c09c');
 
     /**
      * @license
