03a4ed2fb7ecc34b0b0d63c15401d910a781f159
Kara Erickson <karakara@google.com>
2019-01-08 22:21:23 +0000

    1a7f92c test(ivy): re-enable passing animation tests (#27997)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+43.sha-a084024
+ * @license Angular v7.2.0+44.sha-1a7f92c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15454,7 +15454,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+43.sha-a084024');
+    var VERSION$1 = new Version('7.2.0+44.sha-1a7f92c');
 
     /**
      * @license
@@ -33845,7 +33845,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+43.sha-a084024');
+    var VERSION$2 = new Version$1('7.2.0+44.sha-1a7f92c');
 
     /**
      * @license
@@ -51005,7 +51005,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+43.sha-a084024');
+    var VERSION$3 = new Version$1('7.2.0+44.sha-1a7f92c');
 
     /**
      * @license
