66b7230f91d839330f603e7c143f7330b200562d
Kara Erickson <karakara@google.com>
2019-01-09 01:31:58 +0000

    1de4031 docs: use static zip function as an Observable creator. (#26790)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+54.sha-1c39ad3
+ * @license Angular v7.2.0+55.sha-1de4031
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15454,7 +15454,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+54.sha-1c39ad3');
+    var VERSION$1 = new Version('7.2.0+55.sha-1de4031');
 
     /**
      * @license
@@ -33845,7 +33845,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+54.sha-1c39ad3');
+    var VERSION$2 = new Version$1('7.2.0+55.sha-1de4031');
 
     /**
      * @license
@@ -51024,7 +51024,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+54.sha-1c39ad3');
+    var VERSION$3 = new Version$1('7.2.0+55.sha-1de4031');
 
     /**
      * @license
