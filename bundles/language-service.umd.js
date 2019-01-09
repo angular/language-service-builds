9a7888906540ce20586151fc7aa7938c6edd1f90
Andrew Kushnir <akushnir@google.com>
2019-01-09 19:28:28 +0000

    d6cfe2e build: use cross platform workspace_status_command (#27431)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+67.sha-d6cfe2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15485,7 +15485,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+56.sha-c3aa24c');
+    var VERSION$1 = new Version('7.2.0+67.sha-d6cfe2e');
 
     /**
      * @license
@@ -33876,7 +33876,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+56.sha-c3aa24c');
+    var VERSION$2 = new Version$1('7.2.0+67.sha-d6cfe2e');
 
     /**
      * @license
@@ -51055,7 +51055,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+56.sha-c3aa24c');
+    var VERSION$3 = new Version$1('7.2.0+67.sha-d6cfe2e');
 
     /**
      * @license
