2c6ea7a7e5fd6e0d31580270b66f70a89eaa6a1e
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:32:23 +0000

    ee60c76 test(ivy): turn on passing i18n test in CI (#28035)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+92.sha-65e72e9
+ * @license Angular v7.2.0+94.sha-ee60c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15497,7 +15497,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+92.sha-65e72e9');
+    var VERSION$1 = new Version('7.2.0+94.sha-ee60c76');
 
     /**
      * @license
@@ -33885,7 +33885,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+92.sha-65e72e9');
+    var VERSION$2 = new Version$1('7.2.0+94.sha-ee60c76');
 
     /**
      * @license
@@ -51064,7 +51064,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+92.sha-65e72e9');
+    var VERSION$3 = new Version$1('7.2.0+94.sha-ee60c76');
 
     /**
      * @license
