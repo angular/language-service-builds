37d8a0634fdec1abfe52be82af5068a34984f64d
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:25:42 +0000

    aebec4b refactor(core): improve type for EventEmitter (#27228)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+105.sha-9277142
+ * @license Angular v7.2.0+106.sha-aebec4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15505,7 +15505,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+105.sha-9277142');
+    var VERSION$1 = new Version('7.2.0+106.sha-aebec4b');
 
     /**
      * @license
@@ -33909,7 +33909,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+105.sha-9277142');
+    var VERSION$2 = new Version$1('7.2.0+106.sha-aebec4b');
 
     /**
      * @license
@@ -51117,7 +51117,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+105.sha-9277142');
+    var VERSION$3 = new Version$1('7.2.0+106.sha-aebec4b');
 
     /**
      * @license
