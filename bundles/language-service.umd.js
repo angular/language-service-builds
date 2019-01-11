7734405a56025d4c313a9df4540c2161de7fd12b
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:40 +0000

    26a8c09 docs(router): fixed a typo in CanLoad title text (#27894)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+108.sha-acb2cac
+ * @license Angular v7.2.0+110.sha-26a8c09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15505,7 +15505,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+108.sha-acb2cac');
+    var VERSION$1 = new Version('7.2.0+110.sha-26a8c09');
 
     /**
      * @license
@@ -33909,7 +33909,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+108.sha-acb2cac');
+    var VERSION$2 = new Version$1('7.2.0+110.sha-26a8c09');
 
     /**
      * @license
@@ -51117,7 +51117,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+108.sha-acb2cac');
+    var VERSION$3 = new Version$1('7.2.0+110.sha-26a8c09');
 
     /**
      * @license
