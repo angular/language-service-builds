1bf9293c195860c7dcbec3793607be3497bde09a
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:09:30 +0000

    9277142 fix(ivy): support multiple exportAs (#27996)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+104.sha-b78351c
+ * @license Angular v7.2.0+105.sha-9277142
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14753,9 +14753,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         // e.g 'outputs: {a: 'a'}`
         definitionMap.set('outputs', conditionallyCreateMapObjectLiteral(meta.outputs));
         if (meta.exportAs !== null) {
-            // TODO: handle multiple exportAs values (currently only the first is taken).
-            var _a = __read(meta.exportAs, 1), exportAs = _a[0];
-            definitionMap.set('exportAs', literal(exportAs));
+            definitionMap.set('exportAs', literalArr(meta.exportAs.map(function (e) { return literal(e); })));
         }
         return { definitionMap: definitionMap, statements: result.statements };
     }
@@ -15017,7 +15015,6 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         return expressionType(importExpr(typeBase, [
             typeWithParameters(meta.type, meta.typeArgumentCount),
             stringAsType(selectorForType),
-            // TODO: handle multiple exportAs values (currently only the first is taken).
             meta.exportAs !== null ? stringArrayAsType(meta.exportAs) : NONE_TYPE,
             stringMapAsType(meta.inputs),
             stringMapAsType(meta.outputs),
@@ -15493,7 +15490,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+104.sha-b78351c');
+    var VERSION$1 = new Version('7.2.0+105.sha-9277142');
 
     /**
      * @license
@@ -35349,8 +35346,11 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     */
     function saveNameToExportMap(index, def, exportsMap) {
         if (exportsMap) {
-            if (def.exportAs)
-                exportsMap[def.exportAs] = index;
+            if (def.exportAs) {
+                for (var i = 0; i < def.exportAs.length; i++) {
+                    exportsMap[def.exportAs[i]] = index;
+                }
+            }
             if (def.template)
                 exportsMap[''] = index;
         }
@@ -38519,7 +38519,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+104.sha-b78351c');
+    var VERSION$2 = new Version$1('7.2.0+105.sha-9277142');
 
     /**
      * @license
@@ -58923,7 +58923,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+104.sha-b78351c');
+    var VERSION$3 = new Version$1('7.2.0+105.sha-9277142');
 
     /**
      * @license
