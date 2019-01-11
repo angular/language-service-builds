382c25c019b3e2579865737abacbc9c3d925e69c
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:36:24 +0000

    e31afb7 fix(ivy): ngcc - identify all ESM5 decorated classes (#27848)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+113.sha-d505468
+ * @license Angular v7.2.0+114.sha-e31afb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15505,7 +15505,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+113.sha-d505468');
+    var VERSION$1 = new Version('7.2.0+114.sha-e31afb7');
 
     /**
      * @license
@@ -33909,7 +33909,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+113.sha-d505468');
+    var VERSION$2 = new Version$1('7.2.0+114.sha-e31afb7');
 
     /**
      * @license
@@ -51117,7 +51117,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+113.sha-d505468');
+    var VERSION$3 = new Version$1('7.2.0+114.sha-e31afb7');
 
     /**
      * @license
