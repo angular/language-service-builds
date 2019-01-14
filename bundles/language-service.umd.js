983b2f24eb66f5ecbf4176b36ff027ea9c90693c
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:22:29 +0000

    48a03fc test(docs-infra): increase timeout for all redirection tests (#28103)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+146.sha-6fff74e
+ * @license Angular v7.2.0+147.sha-48a03fc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15429,7 +15429,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+146.sha-6fff74e');
+    var VERSION$1 = new Version('7.2.0+147.sha-48a03fc');
 
     /**
      * @license
@@ -39969,7 +39969,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+146.sha-6fff74e');
+    var VERSION$2 = new Version$1('7.2.0+147.sha-48a03fc');
 
     /**
      * @license
@@ -59078,7 +59078,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+146.sha-6fff74e');
+    var VERSION$3 = new Version$1('7.2.0+147.sha-48a03fc');
 
     /**
      * @license
