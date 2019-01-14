1301fa97ececcb89448381a8ffed36711378e0cc
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:31:17 +0000

    e8495b4 refactor(bazel): use web_package rule for index.html (#27995)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+158.sha-1ec01d6
+ * @license Angular v7.2.0+161.sha-e8495b4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15444,7 +15444,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+158.sha-1ec01d6');
+    var VERSION$1 = new Version('7.2.0+161.sha-e8495b4');
 
     /**
      * @license
@@ -35282,7 +35282,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+158.sha-1ec01d6');
+    var VERSION$2 = new Version$1('7.2.0+161.sha-e8495b4');
 
     /**
      * @license
@@ -51276,7 +51276,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+158.sha-1ec01d6');
+    var VERSION$3 = new Version$1('7.2.0+161.sha-e8495b4');
 
     /**
      * @license
