fc6f46530a220c1dba6434afcb59ba84fab89fca
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:30:48 +0000

    4bb5f63 docs: fix typo in dependency injection guide (#27616)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+154.sha-d6aca79
+ * @license Angular v7.2.0+157.sha-4bb5f63
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15429,7 +15429,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+154.sha-d6aca79');
+    var VERSION$1 = new Version('7.2.0+157.sha-4bb5f63');
 
     /**
      * @license
@@ -39969,7 +39969,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+154.sha-d6aca79');
+    var VERSION$2 = new Version$1('7.2.0+157.sha-4bb5f63');
 
     /**
      * @license
@@ -59078,7 +59078,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+154.sha-d6aca79');
+    var VERSION$3 = new Version$1('7.2.0+157.sha-4bb5f63');
 
     /**
      * @license
