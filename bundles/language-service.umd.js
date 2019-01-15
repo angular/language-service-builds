75e222aa5bf8be745678a4be1c5fa30c5f915253
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:10:24 +0000

    850b867 docs: add api doc for switch directives (#27378)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+183.sha-a241200
+ * @license Angular v7.2.0+184.sha-850b867
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15449,7 +15449,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+183.sha-a241200');
+    var VERSION$1 = new Version('7.2.0+184.sha-850b867');
 
     /**
      * @license
@@ -35414,7 +35414,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+183.sha-a241200');
+    var VERSION$2 = new Version$1('7.2.0+184.sha-850b867');
 
     /**
      * @license
@@ -51237,7 +51237,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+183.sha-a241200');
+    var VERSION$3 = new Version$1('7.2.0+184.sha-850b867');
 
     /**
      * @license
