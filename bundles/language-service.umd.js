10c2397de3eca30f0e0c1b3f0a70531a245a2cdf
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:37:14 +0000

    bc02e31 fix(ivy): normalize summary and factory shim files paths (#28006)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+186.sha-6072ca8
+ * @license Angular v7.2.0+187.sha-bc02e31
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15434,7 +15434,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+186.sha-6072ca8');
+    var VERSION$1 = new Version('7.2.0+187.sha-bc02e31');
 
     /**
      * @license
@@ -40872,7 +40872,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+186.sha-6072ca8');
+    var VERSION$2 = new Version$1('7.2.0+187.sha-bc02e31');
 
     /**
      * @license
@@ -59199,7 +59199,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+186.sha-6072ca8');
+    var VERSION$3 = new Version$1('7.2.0+187.sha-bc02e31');
 
     /**
      * @license
