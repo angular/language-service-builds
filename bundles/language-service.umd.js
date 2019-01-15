2128830b15a7495153d41d3a0ce62df1068f4620
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:12:38 +0000

    9460218 docs(router): add clarification for Router config (#28159)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+184.sha-850b867
+ * @license Angular v7.2.0+185.sha-9460218
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15434,7 +15434,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+184.sha-850b867');
+    var VERSION$1 = new Version('7.2.0+185.sha-9460218');
 
     /**
      * @license
@@ -40872,7 +40872,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+184.sha-850b867');
+    var VERSION$2 = new Version$1('7.2.0+185.sha-9460218');
 
     /**
      * @license
@@ -59199,7 +59199,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+184.sha-850b867');
+    var VERSION$3 = new Version$1('7.2.0+185.sha-9460218');
 
     /**
      * @license
