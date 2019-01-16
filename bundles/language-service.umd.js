b079447974a9fff37daae19c6ced948609dfc550
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:57:39 +0000

    1e5e62e docs: release notes for the v7.2.1 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+198.sha-bac71ef
+ * @license Angular v7.2.0+199.sha-1e5e62e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15451,7 +15451,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+198.sha-bac71ef');
+    var VERSION$1 = new Version('7.2.0+199.sha-1e5e62e');
 
     /**
      * @license
@@ -35414,7 +35414,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+198.sha-bac71ef');
+    var VERSION$2 = new Version$1('7.2.0+199.sha-1e5e62e');
 
     /**
      * @license
@@ -51141,7 +51141,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+198.sha-bac71ef');
+    var VERSION$3 = new Version$1('7.2.0+199.sha-1e5e62e');
 
     /**
      * @license
