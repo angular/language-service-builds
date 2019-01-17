0948b59eb4ab183c81539c0adb6a9b8e96ddf0d6
Andrew Kushnir <akushnir@google.com>
2019-01-17 00:43:39 +0000

    a570fdf fix(bazel): Fix integration test after v8 bump (#28194)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+199.sha-1e5e62e
+ * @license Angular v8.0.0-beta.0+1.sha-a570fdf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+199.sha-1e5e62e');
+    var VERSION$1 = new Version('8.0.0-beta.0+1.sha-a570fdf');
 
     /**
      * @license
@@ -40872,7 +40872,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.0+199.sha-1e5e62e');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+1.sha-a570fdf');
 
     /**
      * @license
@@ -59114,7 +59114,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.0+199.sha-1e5e62e');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+1.sha-a570fdf');
 
     /**
      * @license
