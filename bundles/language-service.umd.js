3a632d532c52f838019e0f39802fdd47aa90a9a4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:01:12 +0000

    8a08ff1 docs: fix comments in services example (#26194)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+5.sha-80967ce
+ * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15451,7 +15451,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+5.sha-80967ce');
+    var VERSION$1 = new Version('8.0.0-beta.0+7.sha-8a08ff1');
 
     /**
      * @license
@@ -35414,7 +35414,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+5.sha-80967ce');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+7.sha-8a08ff1');
 
     /**
      * @license
@@ -51141,7 +51141,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+5.sha-80967ce');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+7.sha-8a08ff1');
 
     /**
      * @license
