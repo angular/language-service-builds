6d057f058ec0f1d84c775f9df1be4ccb62ce5529
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:11:26 +0000

    2b9cc85 fix(ivy): Ensure proper namespace is used to create elements in JIT (#28144)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+18.sha-a58fd21
+ * @license Angular v8.0.0-beta.0+19.sha-2b9cc85
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15447,7 +15447,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+18.sha-a58fd21');
+    var VERSION$1 = new Version('8.0.0-beta.0+19.sha-2b9cc85');
 
     /**
      * @license
@@ -35411,7 +35411,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+18.sha-a58fd21');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+19.sha-2b9cc85');
 
     /**
      * @license
@@ -51134,7 +51134,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+18.sha-a58fd21');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+19.sha-2b9cc85');
 
     /**
      * @license
