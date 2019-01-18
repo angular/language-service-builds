b38d3521402754f8aeb88d4f290102b511aac670
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:26:40 +0000

    45bf911 feat(forms): add `markAllAsTouched()` to `AbstractControl` (#26812)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+20.sha-ab2bf83
+ * @license Angular v8.0.0-beta.0+21.sha-45bf911
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15447,7 +15447,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+20.sha-ab2bf83');
+    var VERSION$1 = new Version('8.0.0-beta.0+21.sha-45bf911');
 
     /**
      * @license
@@ -35416,7 +35416,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+20.sha-ab2bf83');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+21.sha-45bf911');
 
     /**
      * @license
@@ -51141,7 +51141,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+20.sha-ab2bf83');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+21.sha-45bf911');
 
     /**
      * @license
