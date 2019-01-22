8f64a5010eb618a7946c78fe2850b8e30161175b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:35:41 +0000

    54565ed docs: fix crisis-detail milestone files (#27025)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+14.sha-ab08385
+ * @license Angular v7.2.1+15.sha-54565ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1+14.sha-ab08385');
+    var VERSION$1 = new Version('7.2.1+15.sha-54565ed');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.1+14.sha-ab08385');
+    var VERSION$2 = new Version$1('7.2.1+15.sha-54565ed');
 
     /**
      * @license
@@ -51075,7 +51075,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.1+14.sha-ab08385');
+    var VERSION$3 = new Version$1('7.2.1+15.sha-54565ed');
 
     /**
      * @license
