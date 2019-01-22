d77547acd8e17d142ccfa0ecdfbcb2f326c9fe6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:44:10 +0000

    ea1b5c1 fix(ivy): not applying camelCased style properties (#28276)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+62.sha-18a9afc
+ * @license Angular v8.0.0-beta.0+63.sha-ea1b5c1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15432,7 +15432,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+62.sha-18a9afc');
+    var VERSION$1 = new Version('8.0.0-beta.0+63.sha-ea1b5c1');
 
     /**
      * @license
@@ -36411,13 +36411,13 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             ngDevMode && ngDevMode.rendererSetStyle++;
             isProceduralRenderer(renderer) ?
                 renderer.setStyle(native, prop, value, RendererStyleFlags3.DashCase) :
-                native['style'].setProperty(prop, value);
+                native.style[prop] = value;
         }
         else {
             ngDevMode && ngDevMode.rendererRemoveStyle++;
             isProceduralRenderer(renderer) ?
                 renderer.removeStyle(native, prop, RendererStyleFlags3.DashCase) :
-                native['style'].removeProperty(prop);
+                native.style[prop] = '';
         }
     }
     /**
@@ -40905,7 +40905,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+62.sha-18a9afc');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+63.sha-ea1b5c1');
 
     /**
      * @license
@@ -59146,7 +59146,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+62.sha-18a9afc');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+63.sha-ea1b5c1');
 
     /**
      * @license
