54c915cd152d6fd1baf962a11359d76f47109b2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:38:56 +0000

    197676a docs: rename "internal" access modifier to "protected" (#26627)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+38.sha-1fb6731
+ * @license Angular v8.0.0-beta.0+39.sha-197676a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15432,7 +15432,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+38.sha-1fb6731');
+    var VERSION$1 = new Version('8.0.0-beta.0+39.sha-197676a');
 
     /**
      * @license
@@ -40905,7 +40905,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+38.sha-1fb6731');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+39.sha-197676a');
 
     /**
      * @license
@@ -59146,7 +59146,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+38.sha-1fb6731');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+39.sha-197676a');
 
     /**
      * @license
