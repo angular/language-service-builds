9a3c35c4e85d04cb81872ad6e2d5c84a4edbd010
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:08:37 +0000

    cb93027 build: Fix gulp format for Node >= 10.14 (#28213)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+10.sha-f4ac96d
+ * @license Angular v7.2.1+12.sha-cb93027
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1+10.sha-f4ac96d');
+    var VERSION$1 = new Version('7.2.1+12.sha-cb93027');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.1+10.sha-f4ac96d');
+    var VERSION$2 = new Version$1('7.2.1+12.sha-cb93027');
 
     /**
      * @license
@@ -51075,7 +51075,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.1+10.sha-f4ac96d');
+    var VERSION$3 = new Version$1('7.2.1+12.sha-cb93027');
 
     /**
      * @license
