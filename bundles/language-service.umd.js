7564a0ee3cf306089664d954d75d7f5f579952cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:17:38 +0000

    351ef2a fix(docs-infra): removal of the use of the ChildNode.remove() method that it isn't supported by IE (#28188)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+31.sha-058aafc
+ * @license Angular v8.0.0-beta.0+33.sha-351ef2a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15432,7 +15432,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+31.sha-058aafc');
+    var VERSION$1 = new Version('8.0.0-beta.0+33.sha-351ef2a');
 
     /**
      * @license
@@ -40905,7 +40905,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+31.sha-058aafc');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+33.sha-351ef2a');
 
     /**
      * @license
@@ -59146,7 +59146,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+31.sha-058aafc');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+33.sha-351ef2a');
 
     /**
      * @license
