eb755e148c6a9322811722d2bad50b57a6192084
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:58:50 +0000

    5b08a88 style(docs-infra): Active tslint rule `semicolon` (#28282)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+26.sha-7db0358
+ * @license Angular v8.0.0-beta.0+27.sha-5b08a88
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15432,7 +15432,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+26.sha-7db0358');
+    var VERSION$1 = new Version('8.0.0-beta.0+27.sha-5b08a88');
 
     /**
      * @license
@@ -40905,7 +40905,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+26.sha-7db0358');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+27.sha-5b08a88');
 
     /**
      * @license
@@ -59146,7 +59146,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+26.sha-7db0358');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+27.sha-5b08a88');
 
     /**
      * @license
