571bd2f12c09dc18f4c474723e25eba94d31add4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:35:11 +0000

    1df8be5 docs: update link to post on redirects in router guide (#27256)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+36.sha-6324ad4
+ * @license Angular v8.0.0-beta.0+37.sha-1df8be5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15447,7 +15447,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+36.sha-6324ad4');
+    var VERSION$1 = new Version('8.0.0-beta.0+37.sha-1df8be5');
 
     /**
      * @license
@@ -35416,7 +35416,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+36.sha-6324ad4');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+37.sha-1df8be5');
 
     /**
      * @license
@@ -51141,7 +51141,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+36.sha-6324ad4');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+37.sha-1df8be5');
 
     /**
      * @license
