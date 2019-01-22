40f05b94e1c35d4dae0db3de82a187122eb1b290
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:36:20 +0000

    37f8263 docs: correct array to map (#27379)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
+ * @license Angular v8.0.0-beta.0+54.sha-37f8263
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15447,7 +15447,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+51.sha-e0fbe86');
+    var VERSION$1 = new Version('8.0.0-beta.0+54.sha-37f8263');
 
     /**
      * @license
@@ -35416,7 +35416,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.0+51.sha-e0fbe86');
+    var VERSION$2 = new Version$1('8.0.0-beta.0+54.sha-37f8263');
 
     /**
      * @license
@@ -51141,7 +51141,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.0+51.sha-e0fbe86');
+    var VERSION$3 = new Version$1('8.0.0-beta.0+54.sha-37f8263');
 
     /**
      * @license
