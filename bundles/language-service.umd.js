6c0e1a3feb1d0581e81dce1ba111d9659a3b3642
Jason Aden <jasonaden@google.com>
2019-01-23 23:14:23 +0000

    22a43cf fix(ivy): error when encountering an empty class attribute (#28321)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+13.sha-9098225
+ * @license Angular v8.0.0-beta.1+14.sha-22a43cf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15538,7 +15538,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+13.sha-9098225');
+    var VERSION$1 = new Version('8.0.0-beta.1+14.sha-22a43cf');
 
     /**
      * @license
@@ -35406,7 +35406,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.1+13.sha-9098225');
+    var VERSION$2 = new Version$1('8.0.0-beta.1+14.sha-22a43cf');
 
     /**
      * @license
@@ -51089,7 +51089,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.1+13.sha-9098225');
+    var VERSION$3 = new Version$1('8.0.0-beta.1+14.sha-22a43cf');
 
     /**
      * @license
