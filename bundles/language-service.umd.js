530e8c7d17c784179cfbad5a1b9b6d278ecc2f58
Jason Aden <jasonaden@google.com>
2019-01-24 18:32:05 +0000

    c1fb9c2 fix(ivy): save queries at the correct indices (#28327)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+20.sha-d4ecffe
+ * @license Angular v8.0.0-beta.1+21.sha-c1fb9c2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15538,7 +15538,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+20.sha-d4ecffe');
+    var VERSION$1 = new Version('8.0.0-beta.1+21.sha-c1fb9c2');
 
     /**
      * @license
@@ -35406,7 +35406,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.1+20.sha-d4ecffe');
+    var VERSION$2 = new Version$1('8.0.0-beta.1+21.sha-c1fb9c2');
 
     /**
      * @license
@@ -51091,7 +51091,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.1+20.sha-d4ecffe');
+    var VERSION$3 = new Version$1('8.0.0-beta.1+21.sha-c1fb9c2');
 
     /**
      * @license
