df79f22465abbe9b83933ae2f23cf8fe35322401
Jason Aden <jasonaden@google.com>
2019-01-25 21:25:18 +0000

    2da82db fix(ivy): proper i18n postprocessing in case of nested templates (#28209)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+39.sha-7496630
+ * @license Angular v8.0.0-beta.1+41.sha-2da82db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15538,7 +15538,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+39.sha-7496630');
+    var VERSION$1 = new Version('8.0.0-beta.1+41.sha-2da82db');
 
     /**
      * @license
@@ -35430,7 +35430,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.1+39.sha-7496630');
+    var VERSION$2 = new Version$1('8.0.0-beta.1+41.sha-2da82db');
 
     /**
      * @license
@@ -51199,7 +51199,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.1+39.sha-7496630');
+    var VERSION$3 = new Version$1('8.0.0-beta.1+41.sha-2da82db');
 
     /**
      * @license
