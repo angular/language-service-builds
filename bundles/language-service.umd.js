9b7dbd8adb58694781f7098af1943b5e7d97f545
Jason Aden <jasonaden@google.com>
2019-01-29 20:06:34 +0000

    b87bf39 fix(docs-infra): add hamburger button to CLI COMMANDS section (#28418)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+85.sha-227f7e4
+ * @license Angular v8.0.0-beta.1+86.sha-b87bf39
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15522,7 +15522,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+85.sha-227f7e4');
+    var VERSION$1 = new Version('8.0.0-beta.1+86.sha-b87bf39');
 
     /**
      * @license
@@ -41154,7 +41154,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.1+85.sha-227f7e4');
+    var VERSION$2 = new Version$1('8.0.0-beta.1+86.sha-b87bf39');
 
     /**
      * @license
@@ -59548,7 +59548,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.1+85.sha-227f7e4');
+    var VERSION$3 = new Version$1('8.0.0-beta.1+86.sha-b87bf39');
 
     /**
      * @license
