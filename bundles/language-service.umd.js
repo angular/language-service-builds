73674ff58d63d2a7e4ce017ee954ca28ee827a2f
Jason Aden <jasonaden@google.com>
2019-01-30 01:24:05 +0000

    ad49962 docs: fix typo for Browserslist in build guide (#28328)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+95.sha-41e68f7
+ * @license Angular v8.0.0-beta.1+100.sha-ad49962
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15537,7 +15537,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+95.sha-41e68f7');
+    var VERSION$1 = new Version('8.0.0-beta.1+100.sha-ad49962');
 
     /**
      * @license
@@ -35468,7 +35468,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.1+95.sha-41e68f7');
+    var VERSION$2 = new Version$1('8.0.0-beta.1+100.sha-ad49962');
 
     /**
      * @license
@@ -51237,7 +51237,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.1+95.sha-41e68f7');
+    var VERSION$3 = new Version$1('8.0.0-beta.1+100.sha-ad49962');
 
     /**
      * @license
