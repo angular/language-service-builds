fefb8e2f13f02c9ea36df44b5524cecc593eac03
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 21:12:12 +0000

    9efb39c test(ivy): ComponentFactoryResolver can resolve any component factory in Ivy (#28465)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+4.sha-c0b3835
+ * @license Angular v8.0.0-beta.2+6.sha-9efb39c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.2+4.sha-c0b3835');
+    var VERSION$1 = new Version('8.0.0-beta.2+6.sha-9efb39c');
 
     /**
      * @license
@@ -41179,7 +41179,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.2+4.sha-c0b3835');
+    var VERSION$2 = new Version$1('8.0.0-beta.2+6.sha-9efb39c');
 
     /**
      * @license
@@ -59598,7 +59598,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.2+4.sha-c0b3835');
+    var VERSION$3 = new Version$1('8.0.0-beta.2+6.sha-9efb39c');
 
     /**
      * @license
