bf426bb126f8a22c8ef66385381fb34ae8ceacc7
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:13:17 +0000

    ec6e730 perf: pngcrush all pngs (#28479)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+8.sha-1b6d8a7
+ * @license Angular v8.0.0-beta.2+10.sha-ec6e730
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.2+8.sha-1b6d8a7');
+    var VERSION$1 = new Version('8.0.0-beta.2+10.sha-ec6e730');
 
     /**
      * @license
@@ -41187,7 +41187,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.2+8.sha-1b6d8a7');
+    var VERSION$2 = new Version$1('8.0.0-beta.2+10.sha-ec6e730');
 
     /**
      * @license
@@ -59606,7 +59606,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.2+8.sha-1b6d8a7');
+    var VERSION$3 = new Version$1('8.0.0-beta.2+10.sha-ec6e730');
 
     /**
      * @license
