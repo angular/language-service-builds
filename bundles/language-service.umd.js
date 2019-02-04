21b5c668f51c4d0ac8c19207eefaa51b8b8439fc
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:12:24 +0000

    c1aa1bf build: `gulp format` only changed lines by default (#28411)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+19.sha-dad1bc7
+ * @license Angular v7.2.3+20.sha-c1aa1bf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.3+19.sha-dad1bc7');
+    var VERSION$1 = new Version('7.2.3+20.sha-c1aa1bf');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.3+19.sha-dad1bc7');
+    var VERSION$2 = new Version$1('7.2.3+20.sha-c1aa1bf');
 
     /**
      * @license
@@ -51075,7 +51075,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.3+19.sha-dad1bc7');
+    var VERSION$3 = new Version$1('7.2.3+20.sha-c1aa1bf');
 
     /**
      * @license
