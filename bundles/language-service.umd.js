7f5dca2fefe6a9b0bc4042479f16a0bb22429fd9
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:56:47 +0000

    7219639 fix(compiler-cli): base synthetic filepaths on input filepath (#28453)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+35.sha-f2621db
+ * @license Angular v8.0.0-beta.2+36.sha-7219639
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15566,7 +15566,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.2+35.sha-f2621db');
+    var VERSION$1 = new Version('8.0.0-beta.2+36.sha-7219639');
 
     /**
      * @license
@@ -35603,7 +35603,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.2+35.sha-f2621db');
+    var VERSION$2 = new Version$1('8.0.0-beta.2+36.sha-7219639');
 
     /**
      * @license
@@ -51281,7 +51281,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.2+35.sha-f2621db');
+    var VERSION$3 = new Version$1('8.0.0-beta.2+36.sha-7219639');
 
     /**
      * @license
