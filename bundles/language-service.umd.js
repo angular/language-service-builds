b6db4f9f9116c41959c9b220801e50ace0f88d63
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:54:14 +0000

    744b020 build: fix failing compiler-cli tests on windows (#28352)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
+ * @license Angular v8.0.0-beta.2+48.sha-744b020
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15566,7 +15566,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.2+39.sha-9ef8d2b');
+    var VERSION$1 = new Version('8.0.0-beta.2+48.sha-744b020');
 
     /**
      * @license
@@ -35629,7 +35629,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.2+39.sha-9ef8d2b');
+    var VERSION$2 = new Version$1('8.0.0-beta.2+48.sha-744b020');
 
     /**
      * @license
@@ -51307,7 +51307,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.2+39.sha-9ef8d2b');
+    var VERSION$3 = new Version$1('8.0.0-beta.2+48.sha-744b020');
 
     /**
      * @license
