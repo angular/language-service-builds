e1d8dd2df6d575921d23f83c6ff5f5e290b132c4
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:44:56 +0000

    a6fd118 build: switch to typescript's es2015 typings (#28134)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+24.sha-fe6e76c
+ * @license Angular v7.2.3+25.sha-a6fd118
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.3+24.sha-fe6e76c');
+    var VERSION$1 = new Version('7.2.3+25.sha-a6fd118');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.3+24.sha-fe6e76c');
+    var VERSION$2 = new Version$1('7.2.3+25.sha-a6fd118');
 
     /**
      * @license
@@ -51075,7 +51075,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.3+24.sha-fe6e76c');
+    var VERSION$3 = new Version$1('7.2.3+25.sha-a6fd118');
 
     /**
      * @license
