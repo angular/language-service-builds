25c59547423d3b0bde95208f2d684f4982103edb
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 18:40:27 +0000

    9ef8d2b ci: keep job alive when yarn request takes more than 10 minutes (#28458)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+38.sha-07fb4b5
+ * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15566,7 +15566,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.2+38.sha-07fb4b5');
+    var VERSION$1 = new Version('8.0.0-beta.2+39.sha-9ef8d2b');
 
     /**
      * @license
@@ -35629,7 +35629,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.2+38.sha-07fb4b5');
+    var VERSION$2 = new Version$1('8.0.0-beta.2+39.sha-9ef8d2b');
 
     /**
      * @license
@@ -51307,7 +51307,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.2+38.sha-07fb4b5');
+    var VERSION$3 = new Version$1('8.0.0-beta.2+39.sha-9ef8d2b');
 
     /**
      * @license
