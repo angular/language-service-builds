df5ce860d63c365ced1d036d40646ecf429ad15e
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:22:53 +0000

    a0e3a8e build: fix revert issue with bitmap.ts (#28576)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+11.sha-be8a403
+ * @license Angular v7.2.4+21.sha-a0e3a8e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+11.sha-be8a403');
+    var VERSION$1 = new Version('7.2.4+21.sha-a0e3a8e');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.4+11.sha-be8a403');
+    var VERSION$2 = new Version$1('7.2.4+21.sha-a0e3a8e');
 
     /**
      * @license
@@ -51075,7 +51075,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.4+11.sha-be8a403');
+    var VERSION$3 = new Version$1('7.2.4+21.sha-a0e3a8e');
 
     /**
      * @license
