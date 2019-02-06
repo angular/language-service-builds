82e05b2b5d8d0d42165caa5ade87f1fd6dcc6392
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:56:17 +0000

    baf103c fix(ivy): don't increment `expandoStartIndex` after directives are matched (#28424)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+5.sha-3f73dfa
+ * @license Angular v8.0.0-beta.3+6.sha-baf103c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+5.sha-3f73dfa');
+    var VERSION$1 = new Version('8.0.0-beta.3+6.sha-baf103c');
 
     /**
      * @license
@@ -36970,13 +36970,24 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * i18nApply() or ComponentFactory.create), we need to adjust the blueprint for future
      * template passes.
      */
-    function allocExpando(view) {
+    function allocExpando(view, numSlotsToAlloc) {
         var tView = view[TVIEW];
         if (tView.firstTemplatePass) {
-            tView.expandoStartIndex++;
-            tView.blueprint.push(null);
-            tView.data.push(null);
-            view.push(null);
+            for (var i = 0; i < numSlotsToAlloc; i++) {
+                tView.blueprint.push(null);
+                tView.data.push(null);
+                view.push(null);
+            }
+            // We should only increment the expando start index if there aren't already directives
+            // and injectors saved in the "expando" section
+            if (!tView.expandoInstructions) {
+                tView.expandoStartIndex += numSlotsToAlloc;
+            }
+            else {
+                // Since we're adding the dynamic nodes into the expando section, we need to let the host
+                // bindings know that they should skip x slots
+                tView.expandoInstructions.push(numSlotsToAlloc);
+            }
         }
     }
     /**
@@ -41354,7 +41365,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+5.sha-3f73dfa');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+6.sha-baf103c');
 
     /**
      * @license
@@ -44655,12 +44666,17 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             i18nStartFirstPass(tView, index, message, subTemplateIndex);
         }
     }
+    // Count for the number of vars that will be allocated for each i18n block.
+    // It is global because this is used in multiple functions that include loops and recursive calls.
+    // This is reset to 0 when `i18nStartFirstPass` is called.
+    var i18nVarsCount;
     /**
      * See `i18nStart` above.
      */
     function i18nStartFirstPass(tView, index, message, subTemplateIndex) {
         var viewData = getLView();
-        var expandoStartIndex = tView.blueprint.length - HEADER_OFFSET;
+        var startIndex = tView.blueprint.length - HEADER_OFFSET;
+        i18nVarsCount = 0;
         var previousOrParentTNode = getPreviousOrParentTNode();
         var parentTNode = getIsParent() ? getPreviousOrParentTNode() :
             previousOrParentTNode && previousOrParentTNode.parent;
@@ -44708,8 +44724,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
                     if (j & 1) {
                         // Odd indexes are ICU expressions
                         // Create the comment node that will anchor the ICU expression
-                        allocExpando(viewData);
-                        var icuNodeIndex = tView.blueprint.length - 1 - HEADER_OFFSET;
+                        var icuNodeIndex = startIndex + i18nVarsCount++;
                         createOpCodes.push(COMMENT_MARKER, ngDevMode ? "ICU " + icuNodeIndex : '', icuNodeIndex, parentIndex << 17 /* SHIFT_PARENT */ | 1 /* AppendChild */);
                         // Update codes for the ICU expression
                         var icuExpression = parts[j];
@@ -44728,22 +44743,21 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
                         // Even indexes are text (including bindings)
                         var hasBinding = text$$1.match(BINDING_REGEXP);
                         // Create text nodes
-                        allocExpando(viewData);
-                        var textNodeIndex = tView.blueprint.length - 1 - HEADER_OFFSET;
+                        var textNodeIndex = startIndex + i18nVarsCount++;
                         createOpCodes.push(
                         // If there is a binding, the value will be set during update
                         hasBinding ? '' : text$$1, textNodeIndex, parentIndex << 17 /* SHIFT_PARENT */ | 1 /* AppendChild */);
                         if (hasBinding) {
-                            addAllToArray(generateBindingUpdateOpCodes(text$$1, tView.blueprint.length - 1 - HEADER_OFFSET), updateOpCodes);
+                            addAllToArray(generateBindingUpdateOpCodes(text$$1, textNodeIndex), updateOpCodes);
                         }
                     }
                 }
             }
         }
+        allocExpando(viewData, i18nVarsCount);
         // NOTE: local var needed to properly assert the type of `TI18n`.
         var tI18n = {
-            vars: tView.blueprint.length - HEADER_OFFSET - expandoStartIndex,
-            expandoStartIndex: expandoStartIndex,
+            vars: i18nVarsCount,
             create: createOpCodes,
             update: updateOpCodes,
             icus: icuExpressions.length ? icuExpressions : null,
@@ -45677,18 +45691,15 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
         var tIcu = {
             type: icuExpression.type,
             vars: vars,
-            expandoStartIndex: expandoStartIndex + 1, childIcus: childIcus,
+            childIcus: childIcus,
             cases: icuExpression.cases,
             create: createCodes,
             remove: removeCodes,
             update: updateCodes
         };
         tIcus.push(tIcu);
-        var lView = getLView();
-        var worstCaseSize = Math.max.apply(Math, __spread(vars));
-        for (var i = 0; i < worstCaseSize; i++) {
-            allocExpando(lView);
-        }
+        // Adding the maximum possible of vars needed (based on the cases with the most vars)
+        i18nVarsCount += Math.max.apply(Math, __spread(vars));
     }
     /**
      * Transforms a string template into an HTML template and a list of instructions used to update
@@ -59680,7 +59691,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+5.sha-3f73dfa');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+6.sha-baf103c');
 
     /**
      * @license
