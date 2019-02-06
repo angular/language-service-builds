9404799809aa47bcd216cfb39a9917eff8dd463a
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 19:35:30 +0000

    6709db9 build: switch to typescript's es2015 typings (#28570)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+24.sha-43081a0
+ * @license Angular v8.0.0-beta.3+25.sha-6709db9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+24.sha-43081a0');
+    var VERSION$1 = new Version('8.0.0-beta.3+25.sha-6709db9');
 
     /**
      * @license
@@ -41364,7 +41364,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+24.sha-43081a0');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+25.sha-6709db9');
 
     /**
      * @license
@@ -59688,7 +59688,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+24.sha-43081a0');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+25.sha-6709db9');
 
     /**
      * @license
