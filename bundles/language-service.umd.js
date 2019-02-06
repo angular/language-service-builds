59f63c6fe685dc17848edf4a09a21fae3519bc77
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:46:28 +0000

    47f5b5f docs(service-worker): note about isStable/setInterval (#28102)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+21.sha-a0e3a8e
+ * @license Angular v7.2.4+23.sha-47f5b5f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+21.sha-a0e3a8e');
+    var VERSION$1 = new Version('7.2.4+23.sha-47f5b5f');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.4+21.sha-a0e3a8e');
+    var VERSION$2 = new Version$1('7.2.4+23.sha-47f5b5f');
 
     /**
      * @license
@@ -44306,6 +44306,94 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * A reference to an Angular application running on a page.
      *
+     * @usageNotes
+     *
+     * {@a is-stable-examples}
+     * ### isStable examples and caveats
+     *
+     * Note two important points about `isStable`, demonstrated in the examples below:
+     * - the application will never be stable if you start any kind
+     * of recurrent asynchronous task when the application starts
+     * (for example for a polling process, started with a `setInterval`, a `setTimeout`
+     * or using RxJS operators like `interval`);
+     * - the `isStable` Observable runs outside of the Angular zone.
+     *
+     * Let's imagine that you start a recurrent task
+     * (here incrementing a counter, using RxJS `interval`),
+     * and at the same time subscribe to `isStable`.
+     *
+     * ```
+     * constructor(appRef: ApplicationRef) {
+     *   appRef.isStable.pipe(
+     *      filter(stable => stable)
+     *   ).subscribe(() => console.log('App is stable now');
+     *   interval(1000).subscribe(counter => console.log(counter));
+     * }
+     * ```
+     * In this example, `isStable` will never emit `true`,
+     * and the trace "App is stable now" will never get logged.
+     *
+     * If you want to execute something when the app is stable,
+     * you have to wait for the application to be stable
+     * before starting your polling process.
+     *
+     * ```
+     * constructor(appRef: ApplicationRef) {
+     *   appRef.isStable.pipe(
+     *     first(stable => stable),
+     *     tap(stable => console.log('App is stable now')),
+     *     switchMap(() => interval(1000))
+     *   ).subscribe(counter => console.log(counter));
+     * }
+     * ```
+     * In this example, the trace "App is stable now" will be logged
+     * and then the counter starts incrementing every second.
+     *
+     * Note also that this Observable runs outside of the Angular zone,
+     * which means that the code in the subscription
+     * to this Observable will not trigger the change detection.
+     *
+     * Let's imagine that instead of logging the counter value,
+     * you update a field of your component
+     * and display it in its template.
+     *
+     * ```
+     * constructor(appRef: ApplicationRef) {
+     *   appRef.isStable.pipe(
+     *     first(stable => stable),
+     *     switchMap(() => interval(1000))
+     *   ).subscribe(counter => this.value = counter);
+     * }
+     * ```
+     * As the `isStable` Observable runs outside the zone,
+     * the `value` field will be updated properly,
+     * but the template will not be refreshed!
+     *
+     * You'll have to manually trigger the change detection to update the template.
+     *
+     * ```
+     * constructor(appRef: ApplicationRef, cd: ChangeDetectorRef) {
+     *   appRef.isStable.pipe(
+     *     first(stable => stable),
+     *     switchMap(() => interval(1000))
+     *   ).subscribe(counter => {
+     *     this.value = counter;
+     *     cd.detectChanges();
+     *   });
+     * }
+     * ```
+     *
+     * Or make the subscription callback run inside the zone.
+     *
+     * ```
+     * constructor(appRef: ApplicationRef, zone: NgZone) {
+     *   appRef.isStable.pipe(
+     *     first(stable => stable),
+     *     switchMap(() => interval(1000))
+     *   ).subscribe(counter => zone.run(() => this.value = counter));
+     * }
+     * ```
+     *
      * @publicApi
      */
     var ApplicationRef = /** @class */ (function () {
@@ -51075,7 +51163,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.4+21.sha-a0e3a8e');
+    var VERSION$3 = new Version$1('7.2.4+23.sha-47f5b5f');
 
     /**
      * @license
