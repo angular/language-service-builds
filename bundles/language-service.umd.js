d004ec0eff3d66145be3fb74cd101c53e0916335
Misko Hevery <misko@hevery.com>
2019-02-07 05:40:47 +0000

    1e58a21 fix(router): redirect to root url when returned as UrlTree from guard (#28271)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+28.sha-da81c9c
+ * @license Angular v7.2.4+29.sha-1e58a21
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+28.sha-da81c9c');
+    var VERSION$1 = new Version('7.2.4+29.sha-1e58a21');
 
     /**
      * @license
@@ -33833,7 +33833,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.4+28.sha-da81c9c');
+    var VERSION$2 = new Version$1('7.2.4+29.sha-1e58a21');
 
     /**
      * @license
@@ -51163,7 +51163,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.4+28.sha-da81c9c');
+    var VERSION$3 = new Version$1('7.2.4+29.sha-1e58a21');
 
     /**
      * @license
