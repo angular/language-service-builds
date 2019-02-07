62187a922be962d313d4e2e095b4016c347ea3c8
Miško Hevery <misko@hevery.com>
2019-02-07 20:56:22 +0000

    264ef72 build: update render3 section of integration test readme (#28372)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+35.sha-50732e1
+ * @license Angular v8.0.0-beta.3+42.sha-264ef72
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+35.sha-50732e1');
+    var VERSION$1 = new Version('8.0.0-beta.3+42.sha-264ef72');
 
     /**
      * @license
@@ -41364,7 +41364,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+35.sha-50732e1');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+42.sha-264ef72');
 
     /**
      * @license
@@ -59776,7 +59776,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+35.sha-50732e1');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+42.sha-264ef72');
 
     /**
      * @license
