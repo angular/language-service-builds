1595fa32b5448d382c57cdebf9fa080d8300dda4
Miško Hevery <misko@hevery.com>
2019-02-08 22:16:16 +0000

    fb9a4a6 fix(ivy): support web worker nodes in unknown property checks (#28610)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+68.sha-7115e7c
+ * @license Angular v8.0.0-beta.3+64.sha-fb9a4a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+68.sha-7115e7c');
+    var VERSION$1 = new Version('8.0.0-beta.3+64.sha-fb9a4a6');
 
     /**
      * @license
@@ -41389,7 +41389,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+68.sha-7115e7c');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+64.sha-fb9a4a6');
 
     /**
      * @license
@@ -59816,7 +59816,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+68.sha-7115e7c');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+64.sha-fb9a4a6');
 
     /**
      * @license
