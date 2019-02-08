a960f3b51d2c7c017252306817399c466462e440
Miško Hevery <misko@hevery.com>
2019-02-08 01:12:08 +0000

    6f9dd1b refactor(common): Remove deprecated FileType (#28583)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+48.sha-c3fadad
+ * @license Angular v8.0.0-beta.3+49.sha-6f9dd1b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+48.sha-c3fadad');
+    var VERSION$1 = new Version('8.0.0-beta.3+49.sha-6f9dd1b');
 
     /**
      * @license
@@ -41393,7 +41393,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+48.sha-c3fadad');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+49.sha-6f9dd1b');
 
     /**
      * @license
@@ -59805,7 +59805,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+48.sha-c3fadad');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+49.sha-6f9dd1b');
 
     /**
      * @license
