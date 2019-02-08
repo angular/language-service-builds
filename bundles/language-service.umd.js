3c72e472ff1f38a598fab541982e3aecc938a06d
Miško Hevery <misko@hevery.com>
2019-02-08 18:03:27 +0000

    6e4ef91 docs: update merge label meaning (#28574) PR Close #28574

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+60.sha-1ff3699
+ * @license Angular v8.0.0-beta.3+58.sha-6e4ef91
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15551,7 +15551,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+60.sha-1ff3699');
+    var VERSION$1 = new Version('8.0.0-beta.3+58.sha-6e4ef91');
 
     /**
      * @license
@@ -41387,7 +41387,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+60.sha-1ff3699');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+58.sha-6e4ef91');
 
     /**
      * @license
@@ -59814,7 +59814,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+60.sha-1ff3699');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+58.sha-6e4ef91');
 
     /**
      * @license
