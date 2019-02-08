4a38b699ee8b0272591f44630024e0e6f7bd1d2d
Miško Hevery <misko@hevery.com>
2019-02-08 17:34:38 +0000

    ceada77 ci: add all required statuses to the requiredStatuses list for angular-robot (#28613)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
+ * @license Angular v8.0.0-beta.3+57.sha-ceada77
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15566,7 +15566,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+56.sha-d91ea2c');
+    var VERSION$1 = new Version('8.0.0-beta.3+57.sha-ceada77');
 
     /**
      * @license
@@ -35670,7 +35670,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+56.sha-d91ea2c');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+57.sha-ceada77');
 
     /**
      * @license
@@ -51436,7 +51436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+56.sha-d91ea2c');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+57.sha-ceada77');
 
     /**
      * @license
