1b6fb0518fa66bf5ffd2e2d72dcbd5a9248a40cc
Miško Hevery <misko@hevery.com>
2019-02-13 20:51:40 +0000

    2f27a80 test(ivy): ivy change detection doesn't descend into CD-detached view trees (#28680)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+143.sha-77eee42
+ * @license Angular v8.0.0-beta.3+144.sha-2f27a80
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15852,7 +15852,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+143.sha-77eee42');
+    var VERSION$1 = new Version('8.0.0-beta.3+144.sha-2f27a80');
 
     /**
      * @license
@@ -35838,7 +35838,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+143.sha-77eee42');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+144.sha-2f27a80');
 
     /**
      * @license
@@ -51372,7 +51372,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+143.sha-77eee42');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+144.sha-2f27a80');
 
     /**
      * @license
