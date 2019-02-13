2c0f372b63364ed148be8da45d3c30dcba1b06f7
Miško Hevery <misko@hevery.com>
2019-02-13 18:19:10 +0000

    06ec95f fix(ivy): allow directive inheritance in strict mode (#28634)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+128.sha-fc8f4f8
+ * @license Angular v8.0.0-beta.3+130.sha-06ec95f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15850,7 +15850,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+128.sha-fc8f4f8');
+    var VERSION$1 = new Version('8.0.0-beta.3+130.sha-06ec95f');
 
     /**
      * @license
@@ -35819,7 +35819,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+128.sha-fc8f4f8');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+130.sha-06ec95f');
 
     /**
      * @license
@@ -51353,7 +51353,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+128.sha-fc8f4f8');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+130.sha-06ec95f');
 
     /**
      * @license
