9b7dc812d75c383c73af0c0cd433531def8c8de1
Miško Hevery <misko@hevery.com>
2019-02-13 20:54:34 +0000

    6fa4235 test(ivy): enable passing Material tests (#28687)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+152.sha-dba2a40
+ * @license Angular v8.0.0-beta.3+146.sha-6fa4235
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15852,7 +15852,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+152.sha-dba2a40');
+    var VERSION$1 = new Version('8.0.0-beta.3+146.sha-6fa4235');
 
     /**
      * @license
@@ -35842,7 +35842,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+152.sha-dba2a40');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+146.sha-6fa4235');
 
     /**
      * @license
@@ -51376,7 +51376,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+152.sha-dba2a40');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+146.sha-6fa4235');
 
     /**
      * @license
