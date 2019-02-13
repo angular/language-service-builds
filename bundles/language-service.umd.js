617b31650deb6e1999e2242147272c9d1ff4fbef
Miško Hevery <misko@hevery.com>
2019-02-13 04:51:50 +0000

    6050cd0 ci: downgrade to sauce-connect v4.5.1 (#28659)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+107.sha-0cb02d9
+ * @license Angular v8.0.0-beta.3+108.sha-6050cd0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15593,7 +15593,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+107.sha-0cb02d9');
+    var VERSION$1 = new Version('8.0.0-beta.3+108.sha-6050cd0');
 
     /**
      * @license
@@ -35559,7 +35559,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+107.sha-0cb02d9');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+108.sha-6050cd0');
 
     /**
      * @license
@@ -51097,7 +51097,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+107.sha-0cb02d9');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+108.sha-6050cd0');
 
     /**
      * @license
