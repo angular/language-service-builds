5ae62719088bc19be9a5606b788008514a79d820
Misko Hevery <misko@hevery.com>
2019-02-14 03:48:45 +0000

    36df905 docs(animations): fixed some grammar (#28708) PR Close #28708

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+160.sha-2c6a6f1
+ * @license Angular v8.0.0-beta.3+161.sha-36df905
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15864,7 +15864,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+160.sha-2c6a6f1');
+    var VERSION$1 = new Version('8.0.0-beta.3+161.sha-36df905');
 
     /**
      * @license
@@ -42075,7 +42075,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+160.sha-2c6a6f1');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+161.sha-36df905');
 
     /**
      * @license
@@ -60484,7 +60484,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+160.sha-2c6a6f1');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+161.sha-36df905');
 
     /**
      * @license
