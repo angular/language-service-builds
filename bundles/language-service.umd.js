3e0d789e2d1d1064624be986f0d714f77f48d4a9
Miško Hevery <misko@hevery.com>
2019-02-14 23:35:05 +0000

    8accc98 docs: incorporate review feedback (#26827)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+179.sha-80a5934
+ * @license Angular v8.0.0-beta.3+181.sha-8accc98
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15868,7 +15868,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+179.sha-80a5934');
+    var VERSION$1 = new Version('8.0.0-beta.3+181.sha-8accc98');
 
     /**
      * @license
@@ -42149,7 +42149,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+179.sha-80a5934');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+181.sha-8accc98');
 
     /**
      * @license
@@ -60559,7 +60559,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+179.sha-80a5934');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+181.sha-8accc98');
 
     /**
      * @license
