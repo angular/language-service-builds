53286bc3a3520abac66472ea92c0316a7c2a3a91
Miško Hevery <misko@hevery.com>
2019-02-14 19:51:49 +0000

    63e5d27 fix(ivy): rethrow event handler errors in tests (#28707)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+178.sha-7cbc36f
+ * @license Angular v8.0.0-beta.3+177.sha-63e5d27
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15879,7 +15879,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+178.sha-7cbc36f');
+    var VERSION$1 = new Version('8.0.0-beta.3+177.sha-63e5d27');
 
     /**
      * @license
@@ -35899,7 +35899,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.3+178.sha-7cbc36f');
+    var VERSION$2 = new Version$1('8.0.0-beta.3+177.sha-63e5d27');
 
     /**
      * @license
@@ -51431,7 +51431,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.3+178.sha-7cbc36f');
+    var VERSION$3 = new Version$1('8.0.0-beta.3+177.sha-63e5d27');
 
     /**
      * @license
