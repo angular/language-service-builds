4badbc7c48dd58c70eb60ec17c8e1cef739607c9
Igor Minar <igor@angular.io>
2019-02-19 20:44:18 +0000

    a17fd43 docs: display the beginning label in interpolation guide example (#28602)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+11.sha-0cc5804
+ * @license Angular v8.0.0-beta.4+12.sha-a17fd43
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15883,7 +15883,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+11.sha-0cc5804');
+    var VERSION$1 = new Version('8.0.0-beta.4+12.sha-a17fd43');
 
     /**
      * @license
@@ -35915,7 +35915,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.4+11.sha-0cc5804');
+    var VERSION$2 = new Version$1('8.0.0-beta.4+12.sha-a17fd43');
 
     /**
      * @license
@@ -51447,7 +51447,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.4+11.sha-0cc5804');
+    var VERSION$3 = new Version$1('8.0.0-beta.4+12.sha-a17fd43');
 
     /**
      * @license
