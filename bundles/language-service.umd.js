1079e5981e1dd9b80d6e8cbc26bfa70c69562fb2
Igor Minar <igor@angular.io>
2019-02-19 21:00:07 +0000

    cdcbb53 docs: capitalize karma (#28775)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+7.sha-1df6b16
+ * @license Angular v7.2.5+9.sha-cdcbb53
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15436,7 +15436,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.5+7.sha-1df6b16');
+    var VERSION$1 = new Version('7.2.5+9.sha-cdcbb53');
 
     /**
      * @license
@@ -33850,7 +33850,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.5+7.sha-1df6b16');
+    var VERSION$2 = new Version$1('7.2.5+9.sha-cdcbb53');
 
     /**
      * @license
@@ -51180,7 +51180,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.5+7.sha-1df6b16');
+    var VERSION$3 = new Version$1('7.2.5+9.sha-cdcbb53');
 
     /**
      * @license
