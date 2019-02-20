e22060503350d662ed5c2df9921f16555c07a9dd
Igor Minar <igor@angular.io>
2019-02-20 00:52:37 +0000

    71d0eeb feat(router): add hash-based navigation option to setUpLocationSync (#28609)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+36.sha-295a143
+ * @license Angular v8.0.0-beta.4+37.sha-71d0eeb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15870,7 +15870,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+36.sha-295a143');
+    var VERSION$1 = new Version('8.0.0-beta.4+37.sha-71d0eeb');
 
     /**
      * @license
@@ -42193,7 +42193,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.4+36.sha-295a143');
+    var VERSION$2 = new Version$1('8.0.0-beta.4+37.sha-71d0eeb');
 
     /**
      * @license
@@ -60668,7 +60668,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.4+36.sha-295a143');
+    var VERSION$3 = new Version$1('8.0.0-beta.4+37.sha-71d0eeb');
 
     /**
      * @license
