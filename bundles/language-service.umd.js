a652843f62a7dede016a26782cfb538f1f637cb4
Igor Minar <igor@angular.io>
2019-02-20 22:10:50 +0000

    cfb2d17 feat(ivy): convert [ngStyle] and [ngClass] to use ivy styling bindings (#28711)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+56.sha-a7e1c0c
+ * @license Angular v8.0.0-beta.5+2.sha-cfb2d17
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15885,7 +15885,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+56.sha-a7e1c0c');
+    var VERSION$1 = new Version('8.0.0-beta.5+2.sha-cfb2d17');
 
     /**
      * @license
@@ -35967,7 +35967,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.4+56.sha-a7e1c0c');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+2.sha-cfb2d17');
 
     /**
      * @license
@@ -46341,7 +46341,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.4+56.sha-a7e1c0c');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+2.sha-cfb2d17');
 
     /**
      * @license
