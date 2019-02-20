b1c41ceb2da1400b3808ea26408c34117e5d6742
Igor Minar <igor@angular.io>
2019-02-20 20:00:31 +0000

    68da465 test(ivy): update material blocklist to turn on passing tests (#28873)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+52.sha-b9eb662
+ * @license Angular v8.0.0-beta.4+53.sha-68da465
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15870,7 +15870,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+52.sha-b9eb662');
+    var VERSION$1 = new Version('8.0.0-beta.4+53.sha-68da465');
 
     /**
      * @license
@@ -42193,7 +42193,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.4+52.sha-b9eb662');
+    var VERSION$2 = new Version$1('8.0.0-beta.4+53.sha-68da465');
 
     /**
      * @license
@@ -55509,7 +55509,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.4+52.sha-b9eb662');
+    var VERSION$3 = new Version$1('8.0.0-beta.4+53.sha-68da465');
 
     /**
      * @license
