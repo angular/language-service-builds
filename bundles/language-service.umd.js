7f1c144af86d0a3805c5f2c4b2458a968de21629
Ben Lesh <ben@benlesh.com>
2019-02-21 02:47:23 +0000

    58436fd fix(ivy): unable to import shim factory files on case-insensitive platforms (#28831)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+7.sha-d0b6622
+ * @license Angular v8.0.0-beta.5+9.sha-58436fd
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15870,7 +15870,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+7.sha-d0b6622');
+    var VERSION$1 = new Version('8.0.0-beta.5+9.sha-58436fd');
 
     /**
      * @license
@@ -42220,7 +42220,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+7.sha-d0b6622');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+9.sha-58436fd');
 
     /**
      * @license
@@ -55536,7 +55536,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+7.sha-d0b6622');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+9.sha-58436fd');
 
     /**
      * @license
