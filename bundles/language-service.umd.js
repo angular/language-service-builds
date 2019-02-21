2205ea677eba84e96f6fb1f0b49081b34f6b8f37
Igor Minar <igor@angular.io>
2019-02-21 16:09:18 +0000

    ebffde7 build: update to rules_typescript 0.25.1 (#28625)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+16.sha-0ea216b
+ * @license Angular v8.0.0-beta.5+15.sha-ebffde7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15893,7 +15893,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+16.sha-0ea216b');
+    var VERSION$1 = new Version('8.0.0-beta.5+15.sha-ebffde7');
 
     /**
      * @license
@@ -35975,7 +35975,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+16.sha-0ea216b');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+15.sha-ebffde7');
 
     /**
      * @license
@@ -46349,7 +46349,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+16.sha-0ea216b');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+15.sha-ebffde7');
 
     /**
      * @license
