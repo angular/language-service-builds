9e93f5240b21d3eb72a56f23834bafc60d7d21d0
Igor Minar <igor@angular.io>
2019-02-21 20:58:28 +0000

    d8704a3 test(ivy): turn on passing Material tests for paginator (#28890)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+18.sha-64e6aaa
+ * @license Angular v8.0.0-beta.5+19.sha-d8704a3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15878,7 +15878,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+18.sha-64e6aaa');
+    var VERSION$1 = new Version('8.0.0-beta.5+19.sha-d8704a3');
 
     /**
      * @license
@@ -42236,7 +42236,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+18.sha-64e6aaa');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+19.sha-d8704a3');
 
     /**
      * @license
@@ -55555,7 +55555,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+18.sha-64e6aaa');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+19.sha-d8704a3');
 
     /**
      * @license
