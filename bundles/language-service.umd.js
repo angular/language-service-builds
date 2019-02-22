b615c64fe514792bab5dccbc9334a938ea4b174c
Ben Lesh <ben@benlesh.com>
2019-02-22 20:36:20 +0000

    fafabc0 build(bazel): use http_server rule from rules_nodejs (#28905)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+45.sha-43181ea.with-local-changes
+ * @license Angular v8.0.0-beta.5+47.sha-fafabc0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15880,7 +15880,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+47.sha-fafabc0.with-local-changes');
 
     /**
      * @license
@@ -27847,8 +27847,9 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             matcher.match(elementSelector, function (selector) {
                 var directive = selectorMap.get(selector);
                 if (directive) {
-                    attrs_1.push.apply(attrs_1, __spread(Object.keys(directive.inputs).map(function (name) { return ({ name: name, input: true }); })));
-                    attrs_1.push.apply(attrs_1, __spread(Object.keys(directive.outputs).map(function (name) { return ({ name: name, output: true }); })));
+                    var inputs_1 = directive.inputs, outputs_1 = directive.outputs;
+                    attrs_1.push.apply(attrs_1, __spread(Object.keys(inputs_1).map(function (name) { return ({ name: inputs_1[name], input: true }); })));
+                    attrs_1.push.apply(attrs_1, __spread(Object.keys(outputs_1).map(function (name) { return ({ name: outputs_1[name], output: true }); })));
                 }
             });
             // If a name shows up twice, fold it into a single value.
@@ -42245,7 +42246,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+47.sha-fafabc0.with-local-changes');
 
     /**
      * @license
@@ -55564,7 +55565,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+47.sha-fafabc0.with-local-changes');
 
     /**
      * @license
