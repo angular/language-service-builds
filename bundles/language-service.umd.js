1b3f358938a44ecf346f2c088a474c0c14893182
Ben Lesh <ben@benlesh.com>
2019-02-22 23:00:53 +0000

    395fb18 docs(router): reword relativeLinkResolution docs to not mention version numbers (#26991)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+58.sha-f0989b7.with-local-changes
+ * @license Angular v8.0.0-beta.5+59.sha-395fb18.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15880,7 +15880,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
 
     /**
      * @license
@@ -42315,7 +42315,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
 
     /**
      * @license
@@ -55653,7 +55653,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
 
     /**
      * @license
