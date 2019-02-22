3b19a7a773ef0f519cd77851ed1b4adaad065077
Ben Lesh <ben@benlesh.com>
2019-02-22 20:20:36 +0000

    43181ea fix(ivy): host listeners being inherited twice (#28902)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+44.sha-9dac04f.with-local-changes
+ * @license Angular v8.0.0-beta.5+45.sha-43181ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15895,7 +15895,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+44.sha-9dac04f.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
 
     /**
      * @license
@@ -29492,6 +29492,12 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
             }
             return propMetadata;
         };
+        ReflectionCapabilities.prototype.ownPropMetadata = function (typeOrFunc) {
+            if (!isType(typeOrFunc)) {
+                return {};
+            }
+            return this._ownPropMetadata(typeOrFunc, Object) || {};
+        };
         ReflectionCapabilities.prototype.hasLifecycleHook = function (type, lcProperty) {
             return type instanceof Type$2 && lcProperty in type.prototype;
         };
@@ -35974,7 +35980,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+44.sha-9dac04f.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
 
     /**
      * @license
@@ -41096,7 +41102,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             }
             var baseDef = constructor.ngBaseDef;
             var defProp = getProp(baseDef);
-            defProp[name] = args[0];
+            defProp[name] = args[0] || name;
         };
     };
     /**
@@ -46348,7 +46354,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+44.sha-9dac04f.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
 
     /**
      * @license
