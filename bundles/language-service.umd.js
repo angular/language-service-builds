3fbb311e40b91d44224d84ae35525fcbbd89ab82
Ben Lesh <ben@benlesh.com>
2019-02-22 17:21:17 +0000

    33f71e8 test(ivy): diagnose root causes of failures in Dialog and MatDialog (#28925)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes
+ * @license Angular v8.0.0-beta.5+37.sha-33f71e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15880,7 +15880,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+37.sha-33f71e8.with-local-changes');
 
     /**
      * @license
@@ -42250,7 +42250,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+37.sha-33f71e8.with-local-changes');
 
     /**
      * @license
@@ -55569,7 +55569,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+37.sha-33f71e8.with-local-changes');
 
     /**
      * @license
