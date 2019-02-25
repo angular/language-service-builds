abe19af04d732e58b2a2047ff8cb1060e6856cec
Ben Lesh <ben@benlesh.com>
2019-02-25 22:31:58 +0000

    1930e8a docs: add Leonardo Zizzamia to GDE resources (#25935)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+74.sha-7a11242.with-local-changes
+ * @license Angular v8.0.0-beta.5+75.sha-1930e8a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15895,7 +15895,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+74.sha-7a11242.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+75.sha-1930e8a.with-local-changes');
 
     /**
      * @license
@@ -36324,7 +36324,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+74.sha-7a11242.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.5+75.sha-1930e8a.with-local-changes');
 
     /**
      * @license
@@ -46706,7 +46706,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+74.sha-7a11242.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.5+75.sha-1930e8a.with-local-changes');
 
     /**
      * @license
