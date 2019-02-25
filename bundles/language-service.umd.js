67bdd213995079d190d32a33cdf0296cc8d880df
Ben Lesh <ben@benlesh.com>
2019-02-25 18:37:21 +0000

    7e1e00c docs: add ngVikings 2019 to events page (#28501)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+28.sha-481f4b7.with-local-changes
+ * @license Angular v7.2.6+29.sha-7e1e00c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15440,7 +15440,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.6+28.sha-481f4b7.with-local-changes');
+    var VERSION$1 = new Version('7.2.6+29.sha-7e1e00c.with-local-changes');
 
     /**
      * @license
@@ -33855,7 +33855,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('7.2.6+28.sha-481f4b7.with-local-changes');
+    var VERSION$2 = new Version$1('7.2.6+29.sha-7e1e00c.with-local-changes');
 
     /**
      * @license
@@ -46026,7 +46026,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('7.2.6+28.sha-481f4b7.with-local-changes');
+    var VERSION$3 = new Version$1('7.2.6+29.sha-7e1e00c.with-local-changes');
 
     /**
      * @license
