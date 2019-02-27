e8ee74f74bb929d0232645d057e53b54b1f1cdba
Ben Lesh <ben@benlesh.com>
2019-02-27 19:52:41 +0000

    34bdebc feat(ivy): add support for windows concrete types for paths (#28752)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+11.sha-034de06.with-local-changes
+ * @license Angular v8.0.0-beta.6+12.sha-34bdebc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15883,7 +15883,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+11.sha-034de06.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+12.sha-34bdebc.with-local-changes');
 
     /**
      * @license
@@ -42624,7 +42624,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+11.sha-034de06.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+12.sha-34bdebc.with-local-changes');
 
     /**
      * @license
@@ -55962,7 +55962,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+11.sha-034de06.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+12.sha-34bdebc.with-local-changes');
 
     /**
      * @license
