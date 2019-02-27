cc6e6521af5d19634cb676d39c61068b342a7ba1
Ben Lesh <ben@benlesh.com>
2019-02-27 01:06:50 +0000

    f380263 release: cut the v8.0.0-beta.6 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+86.sha-5a4c402.with-local-changes
+ * @license Angular v8.0.0-beta.6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15896,7 +15896,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+86.sha-5a4c402.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6.with-local-changes');
 
     /**
      * @license
@@ -36468,7 +36468,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.5+86.sha-5a4c402.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6.with-local-changes');
 
     /**
      * @license
@@ -46850,7 +46850,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.5+86.sha-5a4c402.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6.with-local-changes');
 
     /**
      * @license
