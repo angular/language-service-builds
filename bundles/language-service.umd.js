1a1041e53a46f08319e29a0db74e237aa6042e94
Andrew Kushnir <akushnir@google.com>
2019-02-28 20:25:10 +0000

    c532646 fix(bazel): ng serve should always watch (#29032)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+32.sha-cd83a43.with-local-changes
+ * @license Angular v8.0.0-beta.6+37.sha-c532646.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15938,7 +15938,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+32.sha-cd83a43.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+37.sha-c532646.with-local-changes');
 
     /**
      * @license
@@ -36510,7 +36510,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+32.sha-cd83a43.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+37.sha-c532646.with-local-changes');
 
     /**
      * @license
@@ -46892,7 +46892,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+32.sha-cd83a43.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+37.sha-c532646.with-local-changes');
 
     /**
      * @license
