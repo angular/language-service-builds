ba014363e77024f8f9f9123e3755935a770ad71f
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:41:26 +0000

    b1df9a3 fix(ivy): use the imported name of decorators for detection (#29061)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+46.sha-3e5c1bc.with-local-changes
+ * @license Angular v8.0.0-beta.6+47.sha-b1df9a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15901,7 +15901,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+46.sha-3e5c1bc.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+47.sha-b1df9a3.with-local-changes');
 
     /**
      * @license
@@ -42703,7 +42703,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+46.sha-3e5c1bc.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+47.sha-b1df9a3.with-local-changes');
 
     /**
      * @license
@@ -48310,7 +48310,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = Promise;
+            promiseCtor = config.Promise || Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -56041,7 +56041,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+46.sha-3e5c1bc.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+47.sha-b1df9a3.with-local-changes');
 
     /**
      * @license
