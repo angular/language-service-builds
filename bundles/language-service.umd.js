0a7a57ce50dfeef73801a663b00390b92fb817dc
Andrew Kushnir <akushnir@google.com>
2019-03-04 20:07:39 +0000

    95989a1 docs: fix and add decorator api doc (#28986)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+64.sha-48214e2.with-local-changes
+ * @license Angular v8.0.0-beta.6+63.sha-95989a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15901,7 +15901,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+64.sha-48214e2.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+63.sha-95989a1.with-local-changes');
 
     /**
      * @license
@@ -42710,7 +42710,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+64.sha-48214e2.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+63.sha-95989a1.with-local-changes');
 
     /**
      * @license
@@ -56048,7 +56048,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+64.sha-48214e2.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+63.sha-95989a1.with-local-changes');
 
     /**
      * @license
