91d3a058d394589df778f9551bd9da8cfa1325f2
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:07:48 +0000

    3063547 docs(router): align examples in interfaces docs (#29093)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
+ * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15926,7 +15926,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
 
     /**
      * @license
@@ -36608,7 +36608,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+74.sha-3063547.with-local-changes');
 
     /**
      * @license
@@ -46990,7 +46990,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+74.sha-3063547.with-local-changes');
 
     /**
      * @license
