408c1997449ae56b8851ca30212e885970b3b313
Andrew Kushnir <akushnir@google.com>
2019-03-05 00:19:56 +0000

    866d500 fix(ivy): copy top-level comments into generated factory shims (#29065)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+67.sha-04cf4ef.with-local-changes
+ * @license Angular v8.0.0-beta.6+68.sha-866d500.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15926,7 +15926,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+67.sha-04cf4ef.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+68.sha-866d500.with-local-changes');
 
     /**
      * @license
@@ -36608,7 +36608,7 @@ define(['exports', 'fs', 'path', 'typescript'], function (exports, fs, path, ts)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+67.sha-04cf4ef.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+68.sha-866d500.with-local-changes');
 
     /**
      * @license
@@ -46990,7 +46990,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+67.sha-04cf4ef.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+68.sha-866d500.with-local-changes');
 
     /**
      * @license
