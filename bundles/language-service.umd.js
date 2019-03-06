79a0a89f91c4c285e6bed37f2ff85303458b017b
Andrew Kushnir <akushnir@google.com>
2019-03-06 05:33:41 +0000

    84406e4 fix(ivy): avoid innerHTML usage in exports test (to make it work in IE11) (#29127)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+89.sha-c29d2a4.with-local-changes
+ * @license Angular v8.0.0-beta.6+90.sha-84406e4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15936,7 +15936,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+89.sha-c29d2a4.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
 
     /**
      * @license
@@ -36715,7 +36715,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.6+89.sha-c29d2a4.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
 
     /**
      * @license
@@ -47097,7 +47097,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.6+89.sha-c29d2a4.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
 
     /**
      * @license
