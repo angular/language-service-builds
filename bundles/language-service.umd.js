9681be1bf9b3731050dc54fa3c1457826651cb35
Kara Erickson <karakara@google.com>
2019-03-07 15:51:19 +0000

    29f57e3 build: enable bundle_dts for router package (#28833)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+15.sha-c866c11.with-local-changes
+ * @license Angular v8.0.0-beta.7+16.sha-29f57e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15921,7 +15921,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.7+15.sha-c866c11.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.7+16.sha-29f57e3.with-local-changes');
 
     /**
      * @license
@@ -42903,7 +42903,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.7+15.sha-c866c11.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.7+16.sha-29f57e3.with-local-changes');
 
     /**
      * @license
@@ -56216,7 +56216,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.7+15.sha-c866c11.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.7+16.sha-29f57e3.with-local-changes');
 
     /**
      * @license
