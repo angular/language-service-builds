deab1273911d534be1d6f5df3853ecdb2434eea5
Kara Erickson <karakara@google.com>
2019-03-12 19:45:46 +0000

    b7c17ff ci: run legacy saucelabs for every build (#29255)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+83.sha-c5daaa9.with-local-changes
+ * @license Angular v8.0.0-beta.7+84.sha-b7c17ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15990,7 +15990,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.7+83.sha-c5daaa9.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.7+84.sha-b7c17ff.with-local-changes');
 
     /**
      * @license
@@ -36766,7 +36766,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.7+83.sha-c5daaa9.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.7+84.sha-b7c17ff.with-local-changes');
 
     /**
      * @license
@@ -47148,7 +47148,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.7+83.sha-c5daaa9.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.7+84.sha-b7c17ff.with-local-changes');
 
     /**
      * @license
