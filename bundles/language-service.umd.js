99db139067aa25360cfc19f14f10c4d14123ede9
Kara Erickson <karakara@google.com>
2019-03-13 00:39:21 +0000

    940fbf7 fix(ivy): unable to inject class and style attributes (#29192)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8.with-local-changes
+ * @license Angular v8.0.0-beta.8+1.sha-940fbf7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15975,7 +15975,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+1.sha-940fbf7.with-local-changes');
 
     /**
      * @license
@@ -33821,15 +33821,42 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
         ngDevMode && assertDefined(tNode, 'expecting tNode');
         var attrs = tNode.attrs;
         if (attrs) {
-            for (var i = 0; i < attrs.length; i = i + 2) {
-                var attrName = attrs[i];
+            var attrsLength = attrs.length;
+            var i = 0;
+            while (i < attrsLength) {
+                var value = attrs[i];
                 // If we hit a `Bindings` or `Template` marker then we are done.
-                if (isNameOnlyAttributeMarker(attrName))
+                if (isNameOnlyAttributeMarker(value))
                     break;
-                // TODO(FW-1137): Skip namespaced attributes
-                // TODO(FW-1139): supports classes/styles in @Attribute injection
-                if (attrName == attrNameToInject) {
+                if (typeof value === 'number') {
+                    // Skip to the first value of the marked attribute.
+                    i++;
+                    if (value === 1 /* Classes */ && attrNameToInject === 'class') {
+                        var accumulatedClasses = '';
+                        while (i < attrsLength && typeof attrs[i] === 'string') {
+                            accumulatedClasses += ' ' + attrs[i++];
+                        }
+                        return accumulatedClasses.trim();
+                    }
+                    else if (value === 2 /* Styles */ && attrNameToInject === 'style') {
+                        var accumulatedStyles = '';
+                        while (i < attrsLength && typeof attrs[i] === 'string') {
+                            accumulatedStyles += attrs[i++] + ": " + attrs[i++] + "; ";
+                        }
+                        return accumulatedStyles.trim();
+                    }
+                    else {
+                        while (i < attrsLength && typeof attrs[i] === 'string') {
+                            i++;
+                        }
+                    }
+                }
+                else if (value === attrNameToInject) {
+                    // TODO(FW-1137): Skip namespaced attributes
                     return attrs[i + 1];
+                }
+                else {
+                    i = i + 2;
                 }
             }
         }
@@ -43099,7 +43126,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.8.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.8+1.sha-940fbf7.with-local-changes');
 
     /**
      * @license
@@ -56433,7 +56460,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.8.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.8+1.sha-940fbf7.with-local-changes');
 
     /**
      * @license
