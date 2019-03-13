6e98b362e6bf71904dbfb461367f663e6e926f05
Kara Erickson <karakara@google.com>
2019-03-13 17:58:22 +0000

    7b20cec build(bazel): fix tsconfig extends for //packages/bazel/test/protractor-2:ts_spec (#29280)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+8.sha-9a7f560.with-local-changes
+ * @license Angular v8.0.0-beta.8+11.sha-7b20cec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15995,7 +15995,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+8.sha-9a7f560.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+11.sha-7b20cec.with-local-changes');
 
     /**
      * @license
@@ -36782,7 +36782,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.8+8.sha-9a7f560.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.8+11.sha-7b20cec.with-local-changes');
 
     /**
      * @license
@@ -47164,7 +47164,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.8+8.sha-9a7f560.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.8+11.sha-7b20cec.with-local-changes');
 
     /**
      * @license
