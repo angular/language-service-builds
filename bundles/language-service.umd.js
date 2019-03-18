9f5469da5cbc143d40bb206687459028e481de31
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 19:24:48 +0000

    2ab194c test(ivy): remove tree tests from blocklist (#29377)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+57.sha-7b5d326.with-local-changes
+ * @license Angular v8.0.0-beta.8+58.sha-2ab194c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15953,7 +15953,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+57.sha-7b5d326.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+58.sha-2ab194c.with-local-changes');
 
     /**
      * @license
@@ -43139,7 +43139,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.8+57.sha-7b5d326.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.8+58.sha-2ab194c.with-local-changes');
 
     /**
      * @license
@@ -56392,7 +56392,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.8+57.sha-7b5d326.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.8+58.sha-2ab194c.with-local-changes');
 
     /**
      * @license
