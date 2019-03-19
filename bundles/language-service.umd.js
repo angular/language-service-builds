b0153897533b234cd5721eece985c0dd720d739d
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:45:53 +0000

    4605df8 fix(core): parse incorrect ML open tag as text (#29328)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes
+ * @license Angular v8.0.0-beta.8+78.sha-4605df8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10821,6 +10821,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
             var tagName;
             var prefix;
             var openTagToken;
+            var tokensBeforeTagOpen = this.tokens.length;
             var innerStart = this._cursor.clone();
             try {
                 if (!isAsciiLetter(this._cursor.peek())) {
@@ -10843,10 +10844,10 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
             }
             catch (e) {
                 if (e instanceof _ControlFlowError) {
-                    // When the start tag is invalid, assume we want a "<"
+                    // When the start tag is invalid (including invalid "attributes"), assume we want a "<"
                     this._cursor = innerStart;
                     if (openTagToken) {
-                        this.tokens.pop();
+                        this.tokens.length = tokensBeforeTagOpen;
                     }
                     // Back to back text tokens are merged at the end
                     this._beginToken(TokenType$1.TEXT, start);
@@ -10887,6 +10888,10 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
             return this._endToken(parts);
         };
         _Tokenizer.prototype._consumeAttributeName = function () {
+            var attrNameStart = this._cursor.peek();
+            if (attrNameStart === $SQ || attrNameStart === $DQ) {
+                throw this._createError(_unexpectedCharacterErrorMsg(attrNameStart), this._cursor.getSpan());
+            }
             this._beginToken(TokenType$1.ATTR_NAME);
             var prefixAndName = this._consumePrefixAndName();
             this._endToken(prefixAndName);
@@ -15953,7 +15958,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+78.sha-4605df8.with-local-changes');
 
     /**
      * @license
@@ -43152,7 +43157,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.8+78.sha-4605df8.with-local-changes');
 
     /**
      * @license
@@ -56425,7 +56430,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.8+78.sha-4605df8.with-local-changes');
 
     /**
      * @license
