b35904bb6ae08b661649244314f4b96f72ff2d2e
Miško Hevery <misko@hevery.com>
2019-03-27 20:00:38 +0000

    9745f55 feat: remove @angular/http dependency from @angular/platform-server (#29408)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+21.sha-96b800c.with-local-changes
+ * @license Angular v8.0.0-beta.10+25.sha-9745f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15977,7 +15977,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+21.sha-96b800c.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
 
     /**
      * @license
@@ -41706,7 +41706,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
         }
     }
     function wrapOnChanges() {
-        return function () {
+        return function wrapOnChangesHook_inPreviousChangesStorage() {
             var simpleChangesStore = getSimpleChangesStore(this);
             var current = simpleChangesStore && simpleChangesStore.current;
             if (current) {
@@ -43411,7 +43411,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.10+21.sha-96b800c.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
 
     /**
      * @license
@@ -52978,15 +52978,40 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
          */
         ApplicationRef.prototype.tick = function () {
             var _this = this;
+            var e_1, _a, e_2, _b;
             if (this._runningTick) {
                 throw new Error('ApplicationRef.tick is called recursively');
             }
             var scope = ApplicationRef._tickScope();
             try {
                 this._runningTick = true;
-                this._views.forEach(function (view) { return view.detectChanges(); });
+                try {
+                    for (var _c = __values(this._views), _d = _c.next(); !_d.done; _d = _c.next()) {
+                        var view = _d.value;
+                        view.detectChanges();
+                    }
+                }
+                catch (e_1_1) { e_1 = { error: e_1_1 }; }
+                finally {
+                    try {
+                        if (_d && !_d.done && (_a = _c.return)) _a.call(_c);
+                    }
+                    finally { if (e_1) throw e_1.error; }
+                }
                 if (this._enforceNoNewChanges) {
-                    this._views.forEach(function (view) { return view.checkNoChanges(); });
+                    try {
+                        for (var _e = __values(this._views), _f = _e.next(); !_f.done; _f = _e.next()) {
+                            var view = _f.value;
+                            view.checkNoChanges();
+                        }
+                    }
+                    catch (e_2_1) { e_2 = { error: e_2_1 }; }
+                    finally {
+                        try {
+                            if (_f && !_f.done && (_b = _e.return)) _b.call(_e);
+                        }
+                        finally { if (e_2) throw e_2.error; }
+                    }
                 }
             }
             catch (e) {
@@ -56850,7 +56875,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.10+21.sha-96b800c.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
 
     /**
      * @license
