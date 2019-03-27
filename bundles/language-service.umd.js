e31a9bb6da1adec22f0e8597497b7fca086e499e
Miško Hevery <misko@hevery.com>
2019-03-27 23:40:38 +0000

    931d356 Revert "feat(ivy): add property instruction (#29513)"

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+31.sha-e4c1c88.with-local-changes
+ * @license Angular v8.0.0-beta.10+32.sha-931d356.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15992,7 +15992,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+31.sha-e4c1c88.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+32.sha-931d356.with-local-changes');
 
     /**
      * @license
@@ -36835,7 +36835,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.10+31.sha-e4c1c88.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.10+32.sha-931d356.with-local-changes');
 
     /**
      * @license
@@ -47415,7 +47415,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.10+31.sha-e4c1c88.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.10+32.sha-931d356.with-local-changes');
 
     /**
      * @license
