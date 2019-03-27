7a36022595396eaba66916cfdf047c7606737a4c
Miško Hevery <misko@hevery.com>
2019-03-27 17:27:53 +0000

    ac3ce0d docs: replaced hero with heroes component property (#29487) PR Close #29487

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+16.sha-b17d1a9.with-local-changes
+ * @license Angular v8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15976,7 +15976,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
 
     /**
      * @license
@@ -43410,7 +43410,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
 
     /**
      * @license
@@ -56831,7 +56831,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
 
     /**
      * @license
