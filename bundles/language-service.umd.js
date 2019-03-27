2541a2a9c3aee4108ed238b4be199451c4fbbf33
Miško Hevery <misko@hevery.com>
2019-03-27 17:20:07 +0000

    3c11646 ci: update windows CI bazelrc (#29542)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+11.sha-6c33058.with-local-changes
+ * @license Angular v8.0.0-beta.10+3.sha-3c11646.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15991,7 +15991,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+11.sha-6c33058.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+3.sha-3c11646.with-local-changes');
 
     /**
      * @license
@@ -36834,7 +36834,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.10+11.sha-6c33058.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.10+3.sha-3c11646.with-local-changes');
 
     /**
      * @license
@@ -47371,7 +47371,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.10+11.sha-6c33058.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.10+3.sha-3c11646.with-local-changes');
 
     /**
      * @license
