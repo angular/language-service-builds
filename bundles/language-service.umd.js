3fe15fe7418682ee6c441b3a82419d9eea4cb293
Miško Hevery <misko@hevery.com>
2019-03-27 20:02:31 +0000

    c981006 ci: lock chrome and firefox versions for saucelabs (#29529)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+25.sha-9745f55.with-local-changes
+ * @license Angular v8.0.0-beta.10+24.sha-c981006.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15977,7 +15977,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+24.sha-c981006.with-local-changes');
 
     /**
      * @license
@@ -43411,7 +43411,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.10+24.sha-c981006.with-local-changes');
 
     /**
      * @license
@@ -56875,7 +56875,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.10+24.sha-c981006.with-local-changes');
 
     /**
      * @license
