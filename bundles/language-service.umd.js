96a4e2b4f5df7eea0f140ccf73dcfe7c475f6a4d
Igor Minar <igor@angular.io>
2019-04-04 21:44:41 +0000

    2bd9214 fix(bazel): remove karma-jasmine from ts_web_test_suite (#29695)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+13.sha-c3aea73.with-local-changes
+ * @license Angular v8.0.0-beta.11+17.sha-2bd9214.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16024,7 +16024,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.11+13.sha-c3aea73.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.11+17.sha-2bd9214.with-local-changes');
 
     /**
      * @license
@@ -43832,7 +43832,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.11+13.sha-c3aea73.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.11+17.sha-2bd9214.with-local-changes');
 
     /**
      * @license
@@ -57277,7 +57277,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.11+13.sha-c3aea73.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.11+17.sha-2bd9214.with-local-changes');
 
     /**
      * @license
