bccd2d5dcf57efbe448b5da90d2b0781255c5cda
Igor Minar <igor@angular.io>
2019-04-08 17:09:44 +0000

    cf40105 build: ignore packages/bazel/node_modules (#29738)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+40.sha-c65ac7f.with-local-changes
+ * @license Angular v8.0.0-beta.11+38.sha-cf40105.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16039,7 +16039,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.11+40.sha-c65ac7f.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.11+38.sha-cf40105.with-local-changes');
 
     /**
      * @license
@@ -37221,7 +37221,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.11+40.sha-c65ac7f.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.11+38.sha-cf40105.with-local-changes');
 
     /**
      * @license
@@ -47795,7 +47795,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.11+40.sha-c65ac7f.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.11+38.sha-cf40105.with-local-changes');
 
     /**
      * @license
