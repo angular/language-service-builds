743fa585a4901c6e6e7baffd08a7a0f067f2dc6e
Igor Minar <igor@angular.io>
2019-04-10 19:43:37 +0000

    17c4ed9 docs: update path mappings and exports (#29810)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+60.sha-0997512.with-local-changes
+ * @license Angular v8.0.0-beta.11+65.sha-17c4ed9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16024,7 +16024,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.11+60.sha-0997512.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.11+65.sha-17c4ed9.with-local-changes');
 
     /**
      * @license
@@ -35195,7 +35195,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
             for (var i = 1 /* ValuesStartPosition */; i < buffer.length; i += 3 /* Size */) {
                 var fn = buffer[i + 1 /* InstructionFnOffset */];
                 var args = buffer[i + 2 /* ParamsOffset */];
-                fn.apply(void 0, __spread(args));
+                fn.apply(this, args);
             }
             buffer.length = 1 /* ValuesStartPosition */;
         }
@@ -44250,7 +44250,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.11+60.sha-0997512.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.11+65.sha-17c4ed9.with-local-changes');
 
     /**
      * @license
@@ -57759,7 +57759,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.11+60.sha-0997512.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.11+65.sha-17c4ed9.with-local-changes');
 
     /**
      * @license
