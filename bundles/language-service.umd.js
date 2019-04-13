7c22025359629fdeb8d8a24cc861f82b343d4103
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-13 00:11:55 +0000

    6c01800 fix(ivy): keep i18n-annotated attributes in element attribute list (#29856)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+90.sha-0aa0f11.with-local-changes
+ * @license Angular v8.0.0-beta.11+91.sha-6c01800.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14227,10 +14227,14 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
                     else if (name_1 === 'class') {
                         stylingBuilder.registerClassAttr(value);
                     }
-                    else if (attr.i18n) {
-                        i18nAttrs.push(attr);
-                    }
                     else {
+                        if (attr.i18n) {
+                            // Place attributes into a separate array for i18n processing, but also keep such
+                            // attributes in the main list to make them available for directive matching at runtime.
+                            // TODO(FW-1248): prevent attributes duplication in `i18nAttributes` and `elementStart`
+                            // arguments
+                            i18nAttrs.push(attr);
+                        }
                         outputAttrs.push(attr);
                     }
                 }
@@ -14255,17 +14259,14 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
             element.inputs.forEach(function (input) {
                 var stylingInputWasSet = stylingBuilder.registerBoundInput(input);
                 if (!stylingInputWasSet) {
-                    if (input.type === 0 /* Property */) {
-                        if (input.i18n) {
-                            i18nAttrs.push(input);
-                        }
-                        else {
-                            allOtherInputs.push(input);
-                        }
+                    if (input.type === 0 /* Property */ && input.i18n) {
+                        // Place attributes into a separate array for i18n processing, but also keep such
+                        // attributes in the main list to make them available for directive matching at runtime.
+                        // TODO(FW-1248): prevent attributes duplication in `i18nAttributes` and `elementStart`
+                        // arguments
+                        i18nAttrs.push(input);
                     }
-                    else {
-                        allOtherInputs.push(input);
-                    }
+                    allOtherInputs.push(input);
                 }
             });
             outputAttrs.forEach(function (attr) {
@@ -14399,6 +14400,10 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
                     });
                 }
                 else if (instruction) {
+                    // we must skip attributes with associated i18n context, since these attributes are handled
+                    // separately and corresponding `i18nExp` and `i18nApply` instructions will be generated
+                    if (input.i18n)
+                        return;
                     var value_2 = input.value.visit(_this._valueConverter);
                     if (value_2 !== undefined) {
                         var params_2 = [];
@@ -16065,7 +16070,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.11+90.sha-0aa0f11.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.11+91.sha-6c01800.with-local-changes');
 
     /**
      * @license
@@ -37238,7 +37243,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.11+90.sha-0aa0f11.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.11+91.sha-6c01800.with-local-changes');
 
     /**
      * @license
@@ -47828,7 +47833,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.11+90.sha-0aa0f11.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.11+91.sha-6c01800.with-local-changes');
 
     /**
      * @license
