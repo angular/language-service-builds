31f7f8fa1a64725ef5e1f83c6c9e383f1c8c326e
Ben Lesh <ben@benlesh.com>
2019-04-18 00:39:59 +0000

    e1f51ea feat(compiler-cli): export tooling definitions (#29929)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+18.sha-b9251fd.with-local-changes
+ * @license Angular v8.0.0-beta.13+20.sha-e1f51ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16070,7 +16070,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+18.sha-b9251fd.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+20.sha-e1f51ea.with-local-changes');
 
     /**
      * @license
@@ -31473,8 +31473,10 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * The idea is that unless we are doing production build where we explicitly
      * set `ngDevMode == false` we should be helping the developer by providing
      * as much early warning and errors as possible.
+     *
+     * NOTE: changes to the `ngDevMode` name must be synced with `compiler-cli/src/tooling.ts`.
      */
-    if (typeof _global$1['ngDevMode'] === 'undefined' || _global$1['ngDevMode']) {
+    if (typeof ngDevMode === 'undefined' || ngDevMode) {
         ngDevModeResetPerfCounters();
     }
 
@@ -37243,7 +37245,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.13+18.sha-b9251fd.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.13+20.sha-e1f51ea.with-local-changes');
 
     /**
      * @license
@@ -40271,7 +40273,10 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    if (typeof _global$1['ngI18nClosureMode'] === 'undefined') {
+    /**
+     * NOTE: changes to the `ngI18nClosureMode` name must be synced with `compiler-cli/src/tooling.ts`.
+     */
+    if (typeof ngI18nClosureMode === 'undefined') {
         // Make sure to refer to ngI18nClosureMode as ['ngI18nClosureMode'] for closure.
         _global$1['ngI18nClosureMode'] =
             // TODO(FW-1250): validate that this actually, you know, works.
@@ -47840,7 +47845,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.13+18.sha-b9251fd.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.13+20.sha-e1f51ea.with-local-changes');
 
     /**
      * @license
