cd9ea6652565a80606fb40cc5ede4360f1524cd9
Ben Lesh <ben@benlesh.com>
2019-04-19 21:35:17 +0000

    6ae0084 fix(ivy): error when calling remove() or detach() on an empty view container (#29986)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+53.sha-2271f20.with-local-changes
+ * @license Angular v8.0.0-beta.13+54.sha-6ae0084.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16073,7 +16073,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+53.sha-2271f20.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+54.sha-6ae0084.with-local-changes');
 
     /**
      * @license
@@ -37248,7 +37248,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.13+53.sha-2271f20.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.13+54.sha-6ae0084.with-local-changes');
 
     /**
      * @license
@@ -47856,7 +47856,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.13+53.sha-2271f20.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.13+54.sha-6ae0084.with-local-changes');
 
     /**
      * @license
