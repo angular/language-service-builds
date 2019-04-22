d5c4fb98848a359c3bf9e454b5726312d2b69bac
Ben Lesh <ben@benlesh.com>
2019-04-22 16:12:29 +0000

    f53d0fd fix(ivy): classes should not mess up matching for bound dir attributes (#30002)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+67.sha-645e305.with-local-changes
+ * @license Angular v8.0.0-beta.13+66.sha-f53d0fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16060,7 +16060,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+67.sha-645e305.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+66.sha-f53d0fd.with-local-changes');
 
     /**
      * @license
@@ -44510,7 +44510,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.13+67.sha-645e305.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.13+66.sha-f53d0fd.with-local-changes');
 
     /**
      * @license
@@ -58103,7 +58103,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.13+67.sha-645e305.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.13+66.sha-f53d0fd.with-local-changes');
 
     /**
      * @license
