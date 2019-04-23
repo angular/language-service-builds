379f3d920884382fc109fd9aa034e4972d005d50
Ben Lesh <ben@benlesh.com>
2019-04-23 00:44:16 +0000

    0f9230d feat(ivy): generate ɵɵproperty in host bindings (#30009)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+78.sha-19dfadb.with-local-changes
+ * @license Angular v8.0.0-beta.13+79.sha-0f9230d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17160,9 +17160,17 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
                         sanitizerFn = resolveSanitizationFn(securityContexts[0], isAttribute);
                     }
                 }
-                var instructionParams = [
-                    elVarExp, literal(bindingName), importExpr(Identifiers$1.bind).callFn([bindingExpr.currValExpr])
-                ];
+                var isPropertyInstruction = instruction === Identifiers$1.property;
+                var instructionParams = isPropertyInstruction ?
+                    [
+                        literal(bindingName),
+                        bindingExpr.currValExpr,
+                    ] :
+                    [
+                        elVarExp,
+                        literal(bindingName),
+                        importExpr(Identifiers$1.bind).callFn([bindingExpr.currValExpr]),
+                    ];
                 if (sanitizerFn) {
                     instructionParams.push(sanitizerFn);
                 }
@@ -17253,7 +17261,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
                 instruction = Identifiers$1.componentHostSyntheticProperty;
             }
             else {
-                instruction = Identifiers$1.elementProperty;
+                instruction = Identifiers$1.property;
             }
         }
         return { bindingName: bindingName, instruction: instruction, isAttribute: !!attrMatches };
@@ -17635,7 +17643,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+78.sha-19dfadb.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+79.sha-0f9230d.with-local-changes');
 
     /**
      * @license
@@ -44959,7 +44967,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.13+78.sha-19dfadb.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.13+79.sha-0f9230d.with-local-changes');
 
     /**
      * @license
@@ -58562,7 +58570,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.13+78.sha-19dfadb.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.13+79.sha-0f9230d.with-local-changes');
 
     /**
      * @license
