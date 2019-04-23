ff1ff9fb309a0716bd1460fcf3fbffd9893f4339
Ben Lesh <ben@benlesh.com>
2019-04-23 20:19:17 +0000

    c4dd2d1 fix(ivy): let ngtsc's shim host delegate `resolveModuleNames` method (#30068)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+88.sha-8e8e89a.with-local-changes
+ * @license Angular v8.0.0-beta.13+90.sha-c4dd2d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17643,7 +17643,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+88.sha-8e8e89a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+90.sha-c4dd2d1.with-local-changes');
 
     /**
      * @license
@@ -44967,7 +44967,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.13+88.sha-8e8e89a.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.13+90.sha-c4dd2d1.with-local-changes');
 
     /**
      * @license
@@ -58570,7 +58570,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.13+88.sha-8e8e89a.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.13+90.sha-c4dd2d1.with-local-changes');
 
     /**
      * @license
