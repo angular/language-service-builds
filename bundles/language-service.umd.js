b7418614f0b282c09d1b09fea29e38672a396871
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:22:49 +0000

    28fd5ab fix(compiler): Fix compiler crash due to isSkipSelf of null (#30075)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+39.sha-c61df39.with-local-changes
+ * @license Angular v8.0.0-beta.14+40.sha-28fd5ab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17698,7 +17698,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.14+39.sha-c61df39.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.14+40.sha-28fd5ab.with-local-changes');
 
     /**
      * @license
@@ -20451,7 +20451,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
                 }
                 if (token == null) {
                     hasUnknownDeps = true;
-                    return null;
+                    return {};
                 }
                 return {
                     isAttribute: isAttribute,
@@ -20463,7 +20463,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
                 };
             });
             if (hasUnknownDeps) {
-                var depsTokens = dependenciesMetadata.map(function (dep) { return dep ? stringifyType(dep.token) : '?'; }).join(', ');
+                var depsTokens = dependenciesMetadata.map(function (dep) { return dep.token ? stringifyType(dep.token) : '?'; }).join(', ');
                 var message = "Can't resolve all parameters for " + stringifyType(typeOrFunc) + ": (" + depsTokens + ").";
                 if (throwOnUnknownDeps || this._config.strictInjectionParameters) {
                     this._reportError(syntaxError(message), typeOrFunc);
@@ -37370,7 +37370,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.14+39.sha-c61df39.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.14+40.sha-28fd5ab.with-local-changes');
 
     /**
      * @license
@@ -47957,7 +47957,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.14+39.sha-c61df39.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.14+40.sha-28fd5ab.with-local-changes');
 
     /**
      * @license
