79186945734c6b73dafc39d6c7bf4c4939bcb88e
Andrew Kushnir <akushnir@google.com>
2019-04-25 21:56:52 +0000

    61365a9 fix(bazel): transitive npm deps in ng_module (#30065)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+72.sha-3a9d247.with-local-changes
+ * @license Angular v8.0.0-beta.14+73.sha-61365a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17707,7 +17707,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.14+72.sha-3a9d247.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.14+73.sha-61365a9.with-local-changes');
 
     /**
      * @license
@@ -37387,7 +37387,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-beta.14+72.sha-3a9d247.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-beta.14+73.sha-61365a9.with-local-changes');
 
     /**
      * @license
@@ -47974,7 +47974,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-beta.14+72.sha-3a9d247.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-beta.14+73.sha-61365a9.with-local-changes');
 
     /**
      * @license
