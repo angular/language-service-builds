e4dee5a1760e2374cd8f0fde0a20f9ee05bfb21a
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:34:03 +0000

    876ceb3 docs: remove note about ivy being coupled to dynamic import (#30151)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+6.sha-b9f0720.with-local-changes
+ * @license Angular v8.0.0-rc.0+9.sha-876ceb3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17692,7 +17692,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+6.sha-b9f0720.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+9.sha-876ceb3.with-local-changes');
 
     /**
      * @license
@@ -45044,7 +45044,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+6.sha-b9f0720.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+9.sha-876ceb3.with-local-changes');
 
     /**
      * @license
@@ -58687,7 +58687,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+6.sha-b9f0720.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+9.sha-876ceb3.with-local-changes');
 
     /**
      * @license
