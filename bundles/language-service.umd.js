3f79fa1b8f0206cdbd4210c983c2cd6ca76b243a
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:48:35 +0000

    6067583 fix(bazel): update peerDep ranges (#30155)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+11.sha-ba2a359.with-local-changes
+ * @license Angular v8.0.0-rc.0+12.sha-6067583.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17766,7 +17766,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+11.sha-ba2a359.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+12.sha-6067583.with-local-changes');
 
     /**
      * @license
@@ -37446,7 +37446,7 @@ define(['exports', 'path', 'typescript', 'typescript/lib/tsserverlibrary', 'fs']
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+11.sha-ba2a359.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+12.sha-6067583.with-local-changes');
 
     /**
      * @license
@@ -48033,7 +48033,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+11.sha-ba2a359.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+12.sha-6067583.with-local-changes');
 
     /**
      * @license
