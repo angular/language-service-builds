05ab9f4c21388afd7b4b880945feb425287bd6c2
Kara Erickson <karakara@google.com>
2019-04-30 02:02:12 +0000

    e53cf81 build(docs-infra): upgrade material and cdk to 8.0.0-rc.0 (#30202)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+66.sha-68ff2cc.with-local-changes
+ * @license Angular v8.0.0-rc.0+68.sha-e53cf81.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17770,7 +17770,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+66.sha-68ff2cc.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+68.sha-e53cf81.with-local-changes');
 
     /**
      * @license
@@ -37450,7 +37450,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+66.sha-68ff2cc.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+68.sha-e53cf81.with-local-changes');
 
     /**
      * @license
@@ -48048,7 +48048,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+66.sha-68ff2cc.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+68.sha-e53cf81.with-local-changes');
 
     /**
      * @license
