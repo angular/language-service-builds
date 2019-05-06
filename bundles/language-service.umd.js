7d5740c1c9dc62983039de1942d50887e87373f5
Kara Erickson <karakara@google.com>
2019-05-06 21:27:56 +0000

    39f2c9f docs(forms): fix ControlValueAccessor registerOnChange code example (#30276)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+80.sha-30d1f29.with-local-changes
+ * @license Angular v8.0.0-rc.0+85.sha-39f2c9f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17770,7 +17770,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+80.sha-30d1f29.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+85.sha-39f2c9f.with-local-changes');
 
     /**
      * @license
@@ -37480,7 +37480,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+80.sha-30d1f29.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+85.sha-39f2c9f.with-local-changes');
 
     /**
      * @license
@@ -48078,7 +48078,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+80.sha-30d1f29.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+85.sha-39f2c9f.with-local-changes');
 
     /**
      * @license
