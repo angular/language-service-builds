f811d514f3beb2e2926bc211333dabf224c425d2
Kara Erickson <karakara@google.com>
2019-05-07 21:42:59 +0000

    48de6e8 docs: release notes for the v7.2.15 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+98.sha-3b551e0.with-local-changes
+ * @license Angular v8.0.0-rc.0+99.sha-48de6e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17777,7 +17777,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+98.sha-3b551e0.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+99.sha-48de6e8.with-local-changes');
 
     /**
      * @license
@@ -37489,7 +37489,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+98.sha-3b551e0.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+99.sha-48de6e8.with-local-changes');
 
     /**
      * @license
@@ -48087,7 +48087,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+98.sha-3b551e0.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+99.sha-48de6e8.with-local-changes');
 
     /**
      * @license
