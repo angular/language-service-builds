77ca799d169b15b9439ad47e38207ee43d587a2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 21:50:43 +0000

    452f121 fix: ensure strict mode when evaluating in JIT (#30122)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+119.sha-728db88.with-local-changes
+ * @license Angular v8.0.0-rc.0+120.sha-452f121.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6063,6 +6063,12 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
         JitEvaluator.prototype.evaluateStatements = function (sourceUrl, statements, reflector, createSourceMaps) {
             var converter = new JitEmitterVisitor(reflector);
             var ctx = EmitterVisitorContext.createRoot();
+            // Ensure generated code is in strict mode
+            if (statements.length > 0 && !isUseStrictStatement(statements[0])) {
+                statements = __spread([
+                    literal('use strict').toStmt()
+                ], statements);
+            }
             converter.visitAllStatements(statements, ctx);
             converter.createReturnStmt(ctx);
             return this.evaluateCode(sourceUrl, ctx, converter.getArgs(), createSourceMaps);
@@ -6173,6 +6179,9 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
         };
         return JitEmitterVisitor;
     }(AbstractJsEmitterVisitor));
+    function isUseStrictStatement(statement) {
+        return statement.isEquivalent(literal('use strict').toStmt());
+    }
 
     /**
      * @license
@@ -17765,7 +17774,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+119.sha-728db88.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+120.sha-452f121.with-local-changes');
 
     /**
      * @license
@@ -37480,7 +37489,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+119.sha-728db88.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+120.sha-452f121.with-local-changes');
 
     /**
      * @license
@@ -48078,7 +48087,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+119.sha-728db88.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+120.sha-452f121.with-local-changes');
 
     /**
      * @license
