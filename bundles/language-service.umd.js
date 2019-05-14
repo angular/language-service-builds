0c2e9273dce8fc5b742548ebb70a3b1618e52cb6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 17:57:51 +0000

    c7f9a95 test: fix tests in windows ci (#30451)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+166.sha-cc2f175.with-local-changes
+ * @license Angular v8.0.0-rc.0+170.sha-c7f9a95.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17734,7 +17734,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+166.sha-cc2f175.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+170.sha-c7f9a95.with-local-changes');
 
     /**
      * @license
@@ -44890,7 +44890,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+166.sha-cc2f175.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+170.sha-c7f9a95.with-local-changes');
 
     /**
      * @license
@@ -58553,7 +58553,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+166.sha-cc2f175.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+170.sha-c7f9a95.with-local-changes');
 
     /**
      * @license
