67ebd11fc9bce02323ec324a5a6175660a4f9f33
Jason Aden <jasonaden@google.com>
2019-05-15 21:47:37 +0000

    8555016 docs: fix the payload in event-binding sample code (#30429)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+186.sha-1a0e500.with-local-changes
+ * @license Angular v8.0.0-rc.0+188.sha-8555016.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17749,7 +17749,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+186.sha-1a0e500.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+188.sha-8555016.with-local-changes');
 
     /**
      * @license
@@ -37517,7 +37517,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+186.sha-1a0e500.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+188.sha-8555016.with-local-changes');
 
     /**
      * @license
@@ -48144,7 +48144,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+186.sha-1a0e500.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+188.sha-8555016.with-local-changes');
 
     /**
      * @license
