1ea19db9f6dbbe3b55702c47e22b3c4f99a92d1e
Jason Aden <jasonaden@google.com>
2019-05-16 19:06:35 +0000

    1b613c3 fix(ivy): evaluate external declaration usages as dynamic (#30247)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+199.sha-e9ead2b.with-local-changes
+ * @license Angular v8.0.0-rc.0+201.sha-1b613c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17734,7 +17734,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+199.sha-e9ead2b.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+201.sha-1b613c3.with-local-changes');
 
     /**
      * @license
@@ -44933,7 +44933,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+199.sha-e9ead2b.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+201.sha-1b613c3.with-local-changes');
 
     /**
      * @license
@@ -58595,7 +58595,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+199.sha-e9ead2b.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+201.sha-1b613c3.with-local-changes');
 
     /**
      * @license
