22589d072e1dad2f06bebbc490737aca6897a742
Jason Aden <jasonaden@google.com>
2019-05-17 20:52:45 +0000

    fd7dd4d refactor: clean up language service from unused code and imports (#30534)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+246.sha-01919fb.with-local-changes
+ * @license Angular v8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17767,7 +17767,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+246.sha-01919fb.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes');
 
     /**
      * @license
@@ -37871,7 +37871,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+246.sha-01919fb.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes');
 
     /**
      * @license
@@ -47649,8 +47649,6 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this.options = options;
             this.metadataReaderCache = createMetadataReaderCache();
             this.hostAdapter = new ReflectorModuleModuleResolutionHost(serviceHost, getProgram);
-            this.moduleResolutionCache =
-                ts.createModuleResolutionCache(serviceHost.getCurrentDirectory(), function (s) { return s; });
         }
         ReflectorHost.prototype.getMetadataFor = function (modulePath) {
             return readMetadata(modulePath, this.hostAdapter, this.metadataReaderCache);
@@ -48502,7 +48500,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+246.sha-01919fb.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes');
 
     /**
      * @license
