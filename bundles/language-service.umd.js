abbd7ba545289f46b981e884b3da5bfb9363c1c5
Jason Aden <jasonaden@google.com>
2019-05-17 17:17:23 +0000

    0937062 fix(ivy): type-checking should infer string type for interpolations (#30177)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+241.sha-f03475c.with-local-changes
+ * @license Angular v8.0.0-rc.0+243.sha-0937062.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17752,7 +17752,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+241.sha-f03475c.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+243.sha-0937062.with-local-changes');
 
     /**
      * @license
@@ -45715,7 +45715,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+241.sha-f03475c.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+243.sha-0937062.with-local-changes');
 
     /**
      * @license
@@ -59381,7 +59381,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+241.sha-f03475c.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+243.sha-0937062.with-local-changes');
 
     /**
      * @license
