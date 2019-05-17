ea2465e73e54f49dbcac61c4057441729eaab983
Jason Aden <jasonaden@google.com>
2019-05-17 20:54:49 +0000

    660a091 ci: increase codefresh memory limit to decrease flakyness of known big tests (#30472)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+248.sha-9d5abfb.with-local-changes
+ * @license Angular v8.0.0-rc.0+253.sha-660a091.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17767,7 +17767,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+248.sha-9d5abfb.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+253.sha-660a091.with-local-changes');
 
     /**
      * @license
@@ -37871,7 +37871,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+248.sha-9d5abfb.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+253.sha-660a091.with-local-changes');
 
     /**
      * @license
@@ -41656,7 +41656,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -48500,7 +48500,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+248.sha-9d5abfb.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+253.sha-660a091.with-local-changes');
 
     /**
      * @license
