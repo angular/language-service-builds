215cc59c77fd20758773b355f069b36eab27958b
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:23:21 +0000

    68cd0ca fix(ivy): don't throw on unmatched placeholder replacements in `i18nPostprocess` (#30632)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+342.sha-deb77bd.with-local-changes
+ * @license Angular v8.0.0-rc.0+340.sha-68cd0ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17807,7 +17807,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+342.sha-deb77bd.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+340.sha-68cd0ca.with-local-changes');
 
     /**
      * @license
@@ -41325,15 +41325,15 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
         }
         return tNode;
     }
-    function getBeforeNodeForView(viewIndexInContainer, lContainer) {
-        var nextViewIndex = CONTAINER_HEADER_OFFSET + viewIndexInContainer + 1;
-        if (nextViewIndex < lContainer.length) {
-            var lView = lContainer[nextViewIndex];
-            var tViewNodeChild = lView[T_HOST].child;
-            return tViewNodeChild !== null ? getNativeByTNode(tViewNodeChild, lView) : lContainer[NATIVE];
+    function getBeforeNodeForView(index, lContainer) {
+        var containerNative = lContainer[NATIVE];
+        if (index + 1 < lContainer.length - CONTAINER_HEADER_OFFSET) {
+            var view = lContainer[CONTAINER_HEADER_OFFSET + index + 1];
+            var viewTNode = view[T_HOST];
+            return viewTNode.child ? getNativeByTNode(viewTNode.child, view) : containerNative;
         }
         else {
-            return lContainer[NATIVE];
+            return containerNative;
         }
     }
     /**
@@ -46145,7 +46145,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+342.sha-deb77bd.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+340.sha-68cd0ca.with-local-changes');
 
     /**
      * @license
@@ -53755,12 +53755,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     }
     /**
      * Compiles and adds the `ngModuleDef` and `ngInjectorDef` properties to the module class.
-     *
-     * It's possible to compile a module via this API which will allow duplicate declarations in its
-     * root.
      */
-    function compileNgModuleDefs(moduleType, ngModule, allowDuplicateDeclarationsInRoot) {
-        if (allowDuplicateDeclarationsInRoot === void 0) { allowDuplicateDeclarationsInRoot = false; }
+    function compileNgModuleDefs(moduleType, ngModule) {
         ngDevMode && assertDefined(moduleType, 'Required value moduleType');
         ngDevMode && assertDefined(ngModule, 'Required value ngModule');
         var declarations = flatten$2(ngModule.declarations || EMPTY_ARRAY$3);
@@ -53791,7 +53787,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
         Object.defineProperty(moduleType, NG_INJECTOR_DEF, {
             get: function () {
                 if (ngInjectorDef === null) {
-                    ngDevMode && verifySemanticsOfNgModuleDef(moduleType, allowDuplicateDeclarationsInRoot);
+                    ngDevMode && verifySemanticsOfNgModuleDef(moduleType);
                     var meta = {
                         name: moduleType.name,
                         type: moduleType,
@@ -53810,7 +53806,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             configurable: !!ngDevMode,
         });
     }
-    function verifySemanticsOfNgModuleDef(moduleType, allowDuplicateDeclarationsInRoot) {
+    function verifySemanticsOfNgModuleDef(moduleType) {
         if (verifiedNgModule.get(moduleType))
             return;
         verifiedNgModule.set(moduleType, true);
@@ -53819,21 +53815,19 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
         var errors = [];
         var declarations = maybeUnwrapFn(ngModuleDef.declarations);
         var imports = maybeUnwrapFn(ngModuleDef.imports);
-        flatten$2(imports)
-            .map(unwrapModuleWithProvidersImports)
-            .forEach(function (mod) { return verifySemanticsOfNgModuleDef(mod, false); });
+        flatten$2(imports).map(unwrapModuleWithProvidersImports).forEach(verifySemanticsOfNgModuleDef);
         var exports = maybeUnwrapFn(ngModuleDef.exports);
         declarations.forEach(verifyDeclarationsHaveDefinitions);
         var combinedDeclarations = __spread(declarations.map(resolveForwardRef$1), flatten$2(imports.map(computeCombinedExports)).map(resolveForwardRef$1));
         exports.forEach(verifyExportsAreDeclaredOrReExported);
-        declarations.forEach(function (decl) { return verifyDeclarationIsUnique(decl, allowDuplicateDeclarationsInRoot); });
+        declarations.forEach(verifyDeclarationIsUnique);
         declarations.forEach(verifyComponentEntryComponentsIsPartOfNgModule);
         var ngModule = getAnnotation(moduleType, 'NgModule');
         if (ngModule) {
             ngModule.imports &&
                 flatten$2(ngModule.imports)
                     .map(unwrapModuleWithProvidersImports)
-                    .forEach(function (mod) { return verifySemanticsOfNgModuleDef(mod, false); });
+                    .forEach(verifySemanticsOfNgModuleDef);
             ngModule.bootstrap && ngModule.bootstrap.forEach(verifyCorrectBootstrapType);
             ngModule.bootstrap && ngModule.bootstrap.forEach(verifyComponentIsPartOfNgModule);
             ngModule.entryComponents && ngModule.entryComponents.forEach(verifyComponentIsPartOfNgModule);
@@ -53863,16 +53857,14 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
                 }
             }
         }
-        function verifyDeclarationIsUnique(type, suppressErrors) {
+        function verifyDeclarationIsUnique(type) {
             type = resolveForwardRef$1(type);
             var existingModule = ownerNgModule.get(type);
             if (existingModule && existingModule !== moduleType) {
-                if (!suppressErrors) {
-                    var modules = [existingModule, moduleType].map(stringifyForError).sort();
-                    errors.push("Type " + stringifyForError(type) + " is part of the declarations of 2 modules: " + modules[0] + " and " + modules[1] + "! " +
-                        ("Please consider moving " + stringifyForError(type) + " to a higher module that imports " + modules[0] + " and " + modules[1] + ". ") +
-                        ("You can also create a new NgModule that exports and includes " + stringifyForError(type) + " then import that NgModule in " + modules[0] + " and " + modules[1] + "."));
-                }
+                var modules = [existingModule, moduleType].map(stringifyForError).sort();
+                errors.push("Type " + stringifyForError(type) + " is part of the declarations of 2 modules: " + modules[0] + " and " + modules[1] + "! " +
+                    ("Please consider moving " + stringifyForError(type) + " to a higher module that imports " + modules[0] + " and " + modules[1] + ". ") +
+                    ("You can also create a new NgModule that exports and includes " + stringifyForError(type) + " then import that NgModule in " + modules[0] + " and " + modules[1] + "."));
             }
             else {
                 // Mark type as having owner.
@@ -53951,7 +53943,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
         return __spread(flatten$2(maybeUnwrapFn(ngModuleDef.exports).map(function (type) {
             var ngModuleDef = getNgModuleDef(type);
             if (ngModuleDef) {
-                verifySemanticsOfNgModuleDef(type, false);
+                verifySemanticsOfNgModuleDef(type);
                 return computeCombinedExports(type);
             }
             else {
@@ -59827,7 +59819,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+342.sha-deb77bd.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+340.sha-68cd0ca.with-local-changes');
 
     /**
      * @license
