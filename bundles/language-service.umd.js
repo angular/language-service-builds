4b58dc6ca9d9a1262bd170a289ee701baefc9ecb
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:22:26 +0000

    661c6e6 fix(ivy): fix PR collision with static: true and new test (#30666)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+343.sha-dc6406e.with-local-changes
+ * @license Angular v8.0.0-rc.0+345.sha-661c6e6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17827,7 +17827,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
 
     /**
      * @license
@@ -38277,7 +38277,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
 
     /**
      * @license
@@ -48907,7 +48907,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
 
     /**
      * @license
