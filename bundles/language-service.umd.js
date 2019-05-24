8c50314b12b923c20a1b7100f08c5966d3b46956
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:38:16 +0000

    41cf066 test: improve language service tests performance (#30585)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+345.sha-661c6e6.with-local-changes
+ * @license Angular v8.0.0-rc.0+346.sha-41cf066.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17812,7 +17812,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+346.sha-41cf066.with-local-changes');
 
     /**
      * @license
@@ -46639,7 +46639,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+346.sha-41cf066.with-local-changes');
 
     /**
      * @license
@@ -52313,7 +52313,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -60321,7 +60321,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+346.sha-41cf066.with-local-changes');
 
     /**
      * @license
