e0890422811d66c6d006a219f37e20fd2a3293d4
Igor Minar <igor@angular.io>
2019-05-28 16:57:51 +0000

    c61c14a docs: remove core.module.ts from file tree in style guide (#30679)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+6.sha-45718b3.with-local-changes
+ * @license Angular v8.0.0-rc.5+11.sha-c61c14a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17775,7 +17775,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.5+6.sha-45718b3.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.5+11.sha-c61c14a.with-local-changes');
 
     /**
      * @license
@@ -37462,7 +37462,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.5+6.sha-45718b3.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.5+11.sha-c61c14a.with-local-changes');
 
     /**
      * @license
@@ -41217,7 +41217,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = Promise;
+            promiseCtor = config.Promise || Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -48054,7 +48054,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.5+6.sha-45718b3.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.5+11.sha-c61c14a.with-local-changes');
 
     /**
      * @license
