c7012f54a2329c5cc40c96c94f746488a85e7796
Miško Hevery <misko@hevery.com>
2019-05-28 23:42:10 +0000

    51cb592 release: cut the v8.0.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+352.sha-a36caca.with-local-changes
+ * @license Angular v8.0.0-rc.0+353.sha-51cb592.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17812,7 +17812,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+352.sha-a36caca.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+353.sha-51cb592.with-local-changes');
 
     /**
      * @license
@@ -46639,7 +46639,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+352.sha-a36caca.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+353.sha-51cb592.with-local-changes');
 
     /**
      * @license
@@ -60321,7 +60321,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+352.sha-a36caca.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+353.sha-51cb592.with-local-changes');
 
     /**
      * @license
