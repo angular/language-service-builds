ce1646300fc4356eff4605cefba8da1716501dbd
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 21:28:11 +0000

    19bbf4b test(ivy): fix sanitization state leaking issue

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+385.sha-53c6b78.with-local-changes
+ * @license Angular v8.0.0-rc.0+386.sha-19bbf4b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17882,7 +17882,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+385.sha-53c6b78.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+386.sha-19bbf4b.with-local-changes');
 
     /**
      * @license
@@ -47112,7 +47112,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+385.sha-53c6b78.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+386.sha-19bbf4b.with-local-changes');
 
     /**
      * @license
@@ -60651,7 +60651,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+385.sha-53c6b78.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+386.sha-19bbf4b.with-local-changes');
 
     /**
      * @license
