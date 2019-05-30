34e6d19e5ea7f7e7dc780c1904b7a7aa73b5273d
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:31:24 +0000

    6a5cc8f docs: merge duplicated platform-webworker content in Deprecation Guide (#30651)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+371.sha-71be1da.with-local-changes
+ * @license Angular v8.0.0-rc.0+370.sha-6a5cc8f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17872,7 +17872,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+371.sha-71be1da.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+370.sha-6a5cc8f.with-local-changes');
 
     /**
      * @license
@@ -47097,7 +47097,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+371.sha-71be1da.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+370.sha-6a5cc8f.with-local-changes');
 
     /**
      * @license
@@ -52771,7 +52771,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = Promise;
+            promiseCtor = config.Promise || Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -60790,7 +60790,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+371.sha-71be1da.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+370.sha-6a5cc8f.with-local-changes');
 
     /**
      * @license
