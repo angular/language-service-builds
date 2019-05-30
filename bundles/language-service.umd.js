2e731590d57cadf101a44ed24a2d8f81be031a6c
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:21:59 +0000

    24ca8ba docs: fix the sequence of creating class in HTTP tutorial (#30566)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+361.sha-af4925f.with-local-changes
+ * @license Angular v8.0.0-rc.0+362.sha-24ca8ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17887,7 +17887,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+361.sha-af4925f.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+362.sha-24ca8ba.with-local-changes');
 
     /**
      * @license
@@ -38413,7 +38413,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0-rc.0+361.sha-af4925f.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0-rc.0+362.sha-24ca8ba.with-local-changes');
 
     /**
      * @license
@@ -49043,7 +49043,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0-rc.0+361.sha-af4925f.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0-rc.0+362.sha-24ca8ba.with-local-changes');
 
     /**
      * @license
