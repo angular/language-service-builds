a56d3d79fe80e8492c7e8abe3a479655b678b28e
Miško Hevery <misko@hevery.com>
2019-05-31 17:04:09 +0000

    cdf586d build(docs-infra): upgrade cli command docs sources to e8bdfe6b9 (#30777)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+17.sha-0f352b6.with-local-changes
+ * @license Angular v8.0.0+19.sha-cdf586d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17775,7 +17775,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0+17.sha-0f352b6.with-local-changes');
+    var VERSION$1 = new Version('8.0.0+19.sha-cdf586d.with-local-changes');
 
     /**
      * @license
@@ -37462,7 +37462,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0+17.sha-0f352b6.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0+19.sha-cdf586d.with-local-changes');
 
     /**
      * @license
@@ -48054,7 +48054,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0+17.sha-0f352b6.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0+19.sha-cdf586d.with-local-changes');
 
     /**
      * @license
