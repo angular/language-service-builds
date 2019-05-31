9bf60839872dd870a82f5b791e43f18997937eb5
Misko Hevery <misko@hevery.com>
2019-05-31 03:45:50 +0000

    09c57ec docs: add final release date and update future schedule (#30712)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0.with-local-changes
+ * @license Angular v8.1.0-beta.0+1.sha-09c57ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17882,7 +17882,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-beta.0.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-beta.0+1.sha-09c57ec.with-local-changes');
 
     /**
      * @license
@@ -47112,7 +47112,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-beta.0.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-beta.0+1.sha-09c57ec.with-local-changes');
 
     /**
      * @license
@@ -60651,7 +60651,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-beta.0.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-beta.0+1.sha-09c57ec.with-local-changes');
 
     /**
      * @license
