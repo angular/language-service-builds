765e746ebf3583dbd4b94f3ae54202022412ee8d
Misko Hevery <misko@hevery.com>
2019-06-03 16:39:10 +0000

    a981dd2 docs(core): fix typo in style bindings comment (#30661)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+21.sha-b4e6802.with-local-changes
+ * @license Angular v8.1.0-beta.0+22.sha-a981dd2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17884,7 +17884,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-beta.0+21.sha-b4e6802.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-beta.0+22.sha-a981dd2.with-local-changes');
 
     /**
      * @license
@@ -38428,7 +38428,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-beta.0+21.sha-b4e6802.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-beta.0+22.sha-a981dd2.with-local-changes');
 
     /**
      * @license
@@ -49165,7 +49165,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-beta.0+21.sha-b4e6802.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-beta.0+22.sha-a981dd2.with-local-changes');
 
     /**
      * @license
