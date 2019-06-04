23db86bc7a0d9f0f3f5c10d3ff73678362498458
Misko Hevery <misko@hevery.com>
2019-06-04 19:31:30 +0000

    b74df20 docs(service-worker): mention that HTTPS is required, unless on `localhost` (#30847)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+33.sha-4ecff42.with-local-changes
+ * @license Angular v8.1.0-beta.0+34.sha-b74df20.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17869,7 +17869,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-beta.0+33.sha-4ecff42.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-beta.0+34.sha-b74df20.with-local-changes');
 
     /**
      * @license
@@ -47311,7 +47311,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-beta.0+33.sha-4ecff42.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-beta.0+34.sha-b74df20.with-local-changes');
 
     /**
      * @license
@@ -52810,7 +52810,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = Promise;
+            promiseCtor = config.Promise || Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -60873,7 +60873,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-beta.0+33.sha-4ecff42.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-beta.0+34.sha-b74df20.with-local-changes');
 
     /**
      * @license
