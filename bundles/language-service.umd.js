cfa2feee3085726d165d54788787e25ec41c701d
Miško Hevery <misko@hevery.com>
2019-06-06 16:12:28 +0000

    f440bd1 build(docs-infra): fix CLI command github links (#30889)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+6.sha-3859bcc.with-local-changes
+ * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17882,7 +17882,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.1+6.sha-3859bcc.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
 
     /**
      * @license
@@ -47325,7 +47325,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.1+6.sha-3859bcc.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
 
     /**
      * @license
@@ -60897,7 +60897,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.1+6.sha-3859bcc.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
 
     /**
      * @license
