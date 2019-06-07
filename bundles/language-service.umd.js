b4699e9e8b2e2065eaa8bd82acdb307476d582f8
Misko Hevery <misko@hevery.com>
2019-06-07 16:16:46 +0000

    81c2a94 fix(service-worker): avoid uncaught rejection warning when registration fails (#30876)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+16.sha-7afd260.with-local-changes
+ * @license Angular v8.1.0-next.1+15.sha-81c2a94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17897,7 +17897,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.1+16.sha-7afd260.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.1+15.sha-81c2a94.with-local-changes');
 
     /**
      * @license
@@ -38609,7 +38609,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.1+16.sha-7afd260.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.1+15.sha-81c2a94.with-local-changes');
 
     /**
      * @license
@@ -49389,7 +49389,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.1+16.sha-7afd260.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.1+15.sha-81c2a94.with-local-changes');
 
     /**
      * @license
