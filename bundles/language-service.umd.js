8e8aec7666aca9e4ac3b9dea202bda281407177d
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:43:42 +0000

    c15e675 docs: fix a grammar mistake (#30949)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+41.sha-bb635c0.with-local-changes
+ * @license Angular v8.1.0-next.1+40.sha-c15e675.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17884,7 +17884,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.1+41.sha-bb635c0.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.1+40.sha-c15e675.with-local-changes');
 
     /**
      * @license
@@ -47338,7 +47338,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.1+41.sha-bb635c0.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.1+40.sha-c15e675.with-local-changes');
 
     /**
      * @license
@@ -60941,7 +60941,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.1+41.sha-bb635c0.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.1+40.sha-c15e675.with-local-changes');
 
     /**
      * @license
