c496257086079747f4c5a0e09bd98f735840c9ac
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:05:15 +0000

    4ca401c build(docs-infra): upgrade cli command docs sources to 7da10691d (#30999)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+65.sha-3dcd5eb.with-local-changes
+ * @license Angular v8.0.0+66.sha-4ca401c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17833,7 +17833,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0+65.sha-3dcd5eb.with-local-changes');
+    var VERSION$1 = new Version('8.0.0+66.sha-4ca401c.with-local-changes');
 
     /**
      * @license
@@ -37520,7 +37520,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.0+65.sha-3dcd5eb.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.0+66.sha-4ca401c.with-local-changes');
 
     /**
      * @license
@@ -44303,8 +44303,6 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var SWITCH_IVY_ENABLED__PRE_R3__ = false;
-    var ivyEnabled = SWITCH_IVY_ENABLED__PRE_R3__;
 
     /**
      * @license
@@ -44344,7 +44342,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            var legacyOfflineMode = this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -48110,7 +48108,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.0+65.sha-3dcd5eb.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.0+66.sha-4ca401c.with-local-changes');
 
     /**
      * @license
