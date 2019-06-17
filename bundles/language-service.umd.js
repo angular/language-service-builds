607af272e4b3572a878a4ed69e0f6a2a13f7635a
Andrew Kushnir <akushnir@google.com>
2019-06-17 23:59:59 +0000

    6a01fa532 docs: add mock heroes to the code review tabs for TOH pt. 2 (#31080)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+24.sha-53c6425.with-local-changes
+ * @license Angular v8.0.1+26.sha-6a01fa5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17833,7 +17833,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.1+24.sha-53c6425.with-local-changes');
+    var VERSION$1 = new Version('8.0.1+26.sha-6a01fa5.with-local-changes');
 
     /**
      * @license
@@ -37520,7 +37520,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.1+24.sha-53c6425.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.1+26.sha-6a01fa5.with-local-changes');
 
     /**
      * @license
@@ -41273,7 +41273,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -48100,7 +48100,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.1+24.sha-53c6425.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.1+26.sha-6a01fa5.with-local-changes');
 
     /**
      * @license
