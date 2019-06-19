dc570042062ad54035bf4efa9b966612e8b62eee
Kara Erickson <karakara@google.com>
2019-06-19 23:18:57 +0000

    c36edffe2 docs: fix bad tag (#31115)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+3.sha-b68f2d6.with-local-changes
+ * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17833,7 +17833,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.2+3.sha-b68f2d6.with-local-changes');
+    var VERSION$1 = new Version('8.0.2+7.sha-c36edff.with-local-changes');
 
     /**
      * @license
@@ -37520,7 +37520,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.0.2+3.sha-b68f2d6.with-local-changes');
+    var VERSION$2 = new Version$1('8.0.2+7.sha-c36edff.with-local-changes');
 
     /**
      * @license
@@ -48100,7 +48100,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.0.2+3.sha-b68f2d6.with-local-changes');
+    var VERSION$3 = new Version$1('8.0.2+7.sha-c36edff.with-local-changes');
 
     /**
      * @license
