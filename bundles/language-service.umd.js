2ceeafd236292dc79c1b700b0218db3b3e48c3da
Kara Erickson <karakara@google.com>
2019-06-19 22:24:57 +0000

    c34abf2cb refactor(docs-infra): move auto-link filters to dedicated directory (#31051)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3.with-local-changes
+ * @license Angular v8.1.0-next.3+2.sha-c34abf2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17946,7 +17946,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+2.sha-c34abf2.with-local-changes');
 
     /**
      * @license
@@ -47473,7 +47473,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.3.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.3+2.sha-c34abf2.with-local-changes');
 
     /**
      * @license
@@ -61073,7 +61073,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.3.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.3+2.sha-c34abf2.with-local-changes');
 
     /**
      * @license
