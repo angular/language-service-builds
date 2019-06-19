d9b6d47c7040b2c652e8f2a6e27b777adeb4f75e
Kara Erickson <karakara@google.com>
2019-06-19 23:10:37 +0000

    91008bd97 fix(ivy): improve error message when NgModule properties are not arrays (#30796)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+6.sha-f039583.with-local-changes
+ * @license Angular v8.1.0-next.3+7.sha-91008bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18001,7 +18001,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+6.sha-f039583.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+7.sha-91008bd.with-local-changes');
 
     /**
      * @license
@@ -38647,7 +38647,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.3+6.sha-f039583.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.3+7.sha-91008bd.with-local-changes');
 
     /**
      * @license
@@ -49511,7 +49511,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.3+6.sha-f039583.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.3+7.sha-91008bd.with-local-changes');
 
     /**
      * @license
