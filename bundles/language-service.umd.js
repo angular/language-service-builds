0655b9ebf8b3c61ce903cd909474b33e17a0f1e5
Kara Erickson <karakara@google.com>
2019-06-20 01:16:37 +0000

    75ac72484 fix(ivy): quote dots in directive inputs and outputs (#31146)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+13.sha-4c45aa3.with-local-changes
+ * @license Angular v8.1.0-next.3+14.sha-75ac724.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4736,7 +4736,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * TODO(FW-1136): this is a temporary solution, we need to come up with a better way of working with
      * inputs that contain potentially unsafe chars.
      */
-    var UNSAFE_OBJECT_KEY_NAME_REGEXP = /-/;
+    var UNSAFE_OBJECT_KEY_NAME_REGEXP = /[-.]/;
     /** Name of the temporary to use during data binding */
     var TEMPORARY_NAME = '_t';
     /** Name of the context parameter passed into a template function */
@@ -17986,7 +17986,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+13.sha-4c45aa3.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+14.sha-75ac724.with-local-changes');
 
     /**
      * @license
@@ -47424,7 +47424,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.3+13.sha-4c45aa3.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.3+14.sha-75ac724.with-local-changes');
 
     /**
      * @license
@@ -61114,7 +61114,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.3+13.sha-4c45aa3.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.3+14.sha-75ac724.with-local-changes');
 
     /**
      * @license
