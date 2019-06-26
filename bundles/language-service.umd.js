717ea027ae693baa675d0a2d6beb39e240e65d58
Kara Erickson <karakara@google.com>
2019-06-26 00:37:02 +0000

    7186f9c01 refactor(ivy): implement a virtual file-system layer in ngtsc + ngcc (#30921)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+80.sha-280e856.with-local-changes
+ * @license Angular v8.1.0-next.3+83.sha-7186f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18003,7 +18003,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+80.sha-280e856.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+83.sha-7186f9c.with-local-changes');
 
     /**
      * @license
@@ -38653,7 +38653,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-next.3+80.sha-280e856.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-next.3+83.sha-7186f9c.with-local-changes');
 
     /**
      * @license
@@ -49517,7 +49517,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-next.3+80.sha-280e856.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-next.3+83.sha-7186f9c.with-local-changes');
 
     /**
      * @license
