d675dad86f465bc9a2e09412b2c2be14641c9c93
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:37:59 +0000

    a29dc961a feat(bazel): allow passing a custom bazel compiler host to ngc compile (#31341)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+22.sha-0de5d79.with-local-changes
+ * @license Angular v8.1.0-rc.0+23.sha-a29dc96.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18017,7 +18017,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-rc.0+22.sha-0de5d79.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-rc.0+23.sha-a29dc96.with-local-changes');
 
     /**
      * @license
@@ -47502,7 +47502,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.0-rc.0+22.sha-0de5d79.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.0-rc.0+23.sha-a29dc96.with-local-changes');
 
     /**
      * @license
@@ -61201,7 +61201,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.0-rc.0+22.sha-0de5d79.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.0-rc.0+23.sha-a29dc96.with-local-changes');
 
     /**
      * @license
