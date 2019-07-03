4f3d7508b5cd8ff04d4a8a443509da06c364b5e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-03 16:13:35 +0000

    9515f171b ci: update gcp_token (#31405)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0.with-local-changes
+ * @license Angular v8.2.0-next.0+1.sha-9515f17.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18108,7 +18108,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.0.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.0+1.sha-9515f17.with-local-changes');
 
     /**
      * @license
@@ -38791,7 +38791,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.0.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.0+1.sha-9515f17.with-local-changes');
 
     /**
      * @license
@@ -42689,7 +42689,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -49660,7 +49660,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.0.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.0+1.sha-9515f17.with-local-changes');
 
     /**
      * @license
