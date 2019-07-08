add2c7553ea3a56b3dee6cd034727c373666550a
Jason Aden <jasonaden@google.com>
2019-07-08 17:50:15 +0000

    6aaca21c2 fix(compiler): give ASTWithSource its own visit method (#31347)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+9.sha-9a2d1fa.with-local-changes
+ * @license Angular v8.2.0-next.0+11.sha-6aaca21.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6987,6 +6987,9 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
         }
         ASTWithSource.prototype.visit = function (visitor, context) {
             if (context === void 0) { context = null; }
+            if (visitor.visitASTWithSource) {
+                return visitor.visitASTWithSource(this, context);
+            }
             return this.ast.visit(visitor, context);
         };
         ASTWithSource.prototype.toString = function () { return this.source + " in " + this.location; };
@@ -18084,7 +18087,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.0+9.sha-9a2d1fa.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
 
     /**
      * @license
@@ -48247,7 +48250,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.0+9.sha-9a2d1fa.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
 
     /**
      * @license
@@ -61950,7 +61953,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.0+9.sha-9a2d1fa.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
 
     /**
      * @license
