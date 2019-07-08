b71749be4cb8334850cbe53ede4f250742a48d85
Jason Aden <jasonaden@google.com>
2019-07-08 17:04:48 +0000

    4b05ebc80 docs: fix formatting in workspace config and add helpful info for universal (#31399)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+4.sha-e30f494.with-local-changes
+ * @license Angular v8.2.0-next.0+5.sha-4b05ebc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18084,7 +18084,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.0+4.sha-e30f494.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.0+5.sha-4b05ebc.with-local-changes');
 
     /**
      * @license
@@ -48235,7 +48235,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.0+4.sha-e30f494.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.0+5.sha-4b05ebc.with-local-changes');
 
     /**
      * @license
@@ -61944,7 +61944,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.0+4.sha-e30f494.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.0+5.sha-4b05ebc.with-local-changes');
 
     /**
      * @license
