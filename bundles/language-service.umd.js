65d6a6f26256e60325c6d124ca6708ad5c59f471
Jason Aden <jasonaden@google.com>
2019-07-09 21:12:49 +0000

    7724f7446 refactor(bazel): remove unused lockfile (#31458)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+15.sha-98a68ad.with-local-changes
+ * @license Angular v8.2.0-next.0+20.sha-7724f74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18102,7 +18102,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.0+15.sha-98a68ad.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.0+20.sha-7724f74.with-local-changes');
 
     /**
      * @license
@@ -38797,7 +38797,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.0+15.sha-98a68ad.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.0+20.sha-7724f74.with-local-changes');
 
     /**
      * @license
@@ -49662,7 +49662,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.0+15.sha-98a68ad.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.0+20.sha-7724f74.with-local-changes');
 
     /**
      * @license
