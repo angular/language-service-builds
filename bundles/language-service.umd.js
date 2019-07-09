ddd008940f45d502827d837d37282e461c510436
Jason Aden <jasonaden@google.com>
2019-07-09 18:02:44 +0000

    7f2330a96 perf(ivy): ngcc - add a cache to the FileSystem (#30525)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+11.sha-6aaca21.with-local-changes
+ * @license Angular v8.2.0-next.0+17.sha-7f2330a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18102,7 +18102,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.0+17.sha-7f2330a.with-local-changes');
 
     /**
      * @license
@@ -38797,7 +38797,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.0+17.sha-7f2330a.with-local-changes');
 
     /**
      * @license
@@ -49662,7 +49662,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.0+17.sha-7f2330a.with-local-changes');
 
     /**
      * @license
