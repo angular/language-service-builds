59af422dea0d831e5569f6f3dec84a212c71b1c4
Matias Niemelä <matias@yearofmoo.com>
2019-07-10 23:27:08 +0000

    53212db3e refactor(ivy): remove duplicate forward ref logic (#31423)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+3.sha-dee16a4.with-local-changes
+ * @license Angular v8.2.0-next.1+4.sha-53212db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18121,7 +18121,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+3.sha-dee16a4.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+4.sha-53212db.with-local-changes');
 
     /**
      * @license
@@ -48304,7 +48304,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.1+3.sha-dee16a4.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.1+4.sha-53212db.with-local-changes');
 
     /**
      * @license
@@ -58229,6 +58229,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var SWITCH_IVY_ENABLED__POST_R3__ = true;
+    var ivyEnabled = SWITCH_IVY_ENABLED__POST_R3__;
 
     var _SEPARATOR = '#';
     var FACTORY_CLASS_SUFFIX = 'NgFactory';
@@ -58261,7 +58263,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -62028,7 +62031,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.1+3.sha-dee16a4.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.1+4.sha-53212db.with-local-changes');
 
     /**
      * @license
