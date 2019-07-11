4ab215abb7219eab797c2b4cf82e85e5f3cb0e66
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:40:06 +0000

    4bb283cbb build: remove redundant `@types/source-map` dependency (#31468)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+27.sha-565a58e.with-local-changes
+ * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18121,7 +18121,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+27.sha-565a58e.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
 
     /**
      * @license
@@ -48313,7 +48313,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.1+27.sha-565a58e.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
 
     /**
      * @license
@@ -62040,7 +62040,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.1+27.sha-565a58e.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
 
     /**
      * @license
