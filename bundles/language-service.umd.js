a91f05073fb93406a9be638df06da0a8e5e5c177
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 16:55:47 +0000

    d6c80871f docs: remove and update resource list (#31469)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+47.sha-b65e11e.with-local-changes
+ * @license Angular v8.2.0-next.1+50.sha-d6c8087.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18122,7 +18122,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+50.sha-d6c8087.with-local-changes');
 
     /**
      * @license
@@ -48314,7 +48314,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.1+50.sha-d6c8087.with-local-changes');
 
     /**
      * @license
@@ -62041,7 +62041,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.1+50.sha-d6c8087.with-local-changes');
 
     /**
      * @license
