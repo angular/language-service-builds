0d7c52439f8fa09ea6803fac93c8f242d0df13c1
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 18:49:45 +0000

    1ac07757d refactor(ivy): cleanup implementation of container related instructions (#31142)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+62.sha-0110de2.with-local-changes
+ * @license Angular v8.2.0-next.1+63.sha-1ac0775.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18130,7 +18130,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+62.sha-0110de2.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+63.sha-1ac0775.with-local-changes');
 
     /**
      * @license
@@ -38824,7 +38824,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.1+62.sha-0110de2.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.1+63.sha-1ac0775.with-local-changes');
 
     /**
      * @license
@@ -49694,7 +49694,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.1+62.sha-0110de2.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.1+63.sha-1ac0775.with-local-changes');
 
     /**
      * @license
