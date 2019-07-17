bd6a652efa1989baa5aba76f41d4e3c715e8d70c
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:44:54 +0000

    32c07ceca docs(core): fix typo in event-binding code example (#31611)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+67.sha-12fd069.with-local-changes
+ * @license Angular v8.2.0-next.1+70.sha-32c07ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18132,7 +18132,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+67.sha-12fd069.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+70.sha-32c07ce.with-local-changes');
 
     /**
      * @license
@@ -38826,7 +38826,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.1+67.sha-12fd069.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.1+70.sha-32c07ce.with-local-changes');
 
     /**
      * @license
@@ -49696,7 +49696,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.1+67.sha-12fd069.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.1+70.sha-32c07ce.with-local-changes');
 
     /**
      * @license
