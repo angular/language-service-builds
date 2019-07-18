884ba29feb6bc7f2f69e567162fb44179cefc279
Miško Hevery <misko@hevery.com>
2019-07-18 21:46:34 +0000

    1b6e8411b docs: fix typo in workspace config section (#31630)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+24.sha-376ad9c.with-local-changes
+ * @license Angular v8.2.0-next.2+26.sha-1b6e841.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18135,7 +18135,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+24.sha-376ad9c.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+26.sha-1b6e841.with-local-changes');
 
     /**
      * @license
@@ -38858,7 +38858,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+24.sha-376ad9c.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+26.sha-1b6e841.with-local-changes');
 
     /**
      * @license
@@ -49697,7 +49697,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+24.sha-376ad9c.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+26.sha-1b6e841.with-local-changes');
 
     /**
      * @license
