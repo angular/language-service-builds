48ad27194c259f60fbe6daf1536fa1807b266234
Miško Hevery <misko@hevery.com>
2019-07-19 00:09:16 +0000

    ae3bcd858  docs: rephrased explanation to fork before save (#31384)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+8.sha-434b796.with-local-changes
+ * @license Angular v8.1.2+12.sha-ae3bcd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18008,7 +18008,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.2+8.sha-434b796.with-local-changes');
+    var VERSION$1 = new Version('8.1.2+12.sha-ae3bcd8.with-local-changes');
 
     /**
      * @license
@@ -38676,7 +38676,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.2+8.sha-434b796.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.2+12.sha-ae3bcd8.with-local-changes');
 
     /**
      * @license
@@ -49537,7 +49537,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.2+8.sha-434b796.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.2+12.sha-ae3bcd8.with-local-changes');
 
     /**
      * @license
