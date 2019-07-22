bf798c2ec8f9cf0075b33c325cad697f8bd7dcd6
Miško Hevery <misko@hevery.com>
2019-07-22 17:16:57 +0000

    8b26447c4 perf(ivy): avoid using array splice (#31656)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+42.sha-c845a7b.with-local-changes
+ * @license Angular v8.2.0-next.2+41.sha-8b26447.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18083,7 +18083,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+42.sha-c845a7b.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+41.sha-8b26447.with-local-changes');
 
     /**
      * @license
@@ -38676,7 +38676,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+42.sha-c845a7b.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+41.sha-8b26447.with-local-changes');
 
     /**
      * @license
@@ -45753,6 +45753,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var SWITCH_IVY_ENABLED__PRE_R3__ = false;
+    var ivyEnabled = SWITCH_IVY_ENABLED__PRE_R3__;
 
     /**
      * @license
@@ -45792,7 +45794,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = this._compiler instanceof Compiler;
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -49564,7 +49566,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+42.sha-c845a7b.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+41.sha-8b26447.with-local-changes');
 
     /**
      * @license
