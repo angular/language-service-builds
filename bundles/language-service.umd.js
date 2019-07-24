4b8096217f6659da41a73cc172cc53ca9b41b6b3
Miško Hevery <misko@hevery.com>
2019-07-24 21:53:14 +0000

    2bb9a6535 fix(zone.js): don't wrap uncaught promise error. (#31443)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+69.sha-b6aeace.with-local-changes
+ * @license Angular v8.2.0-next.2+72.sha-2bb9a65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18091,7 +18091,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+69.sha-b6aeace.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+72.sha-2bb9a65.with-local-changes');
 
     /**
      * @license
@@ -46797,7 +46797,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+69.sha-b6aeace.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+72.sha-2bb9a65.with-local-changes');
 
     /**
      * @license
@@ -49607,7 +49607,8 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
             _this.componentDef = componentDef;
             _this.ngModule = ngModule;
             _this.componentType = componentDef.type;
-            _this.selector = componentDef.selectors[0][0];
+            // default to 'div' in case this component has an attribute selector
+            _this.selector = componentDef.selectors[0][0] || 'div';
             _this.ngContentSelectors =
                 componentDef.ngContentSelectors ? componentDef.ngContentSelectors : [];
             _this.isBoundToModule = !!ngModule;
@@ -56729,6 +56730,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var SWITCH_IVY_ENABLED__POST_R3__ = true;
+    var ivyEnabled = SWITCH_IVY_ENABLED__POST_R3__;
 
     var _SEPARATOR = '#';
     var FACTORY_CLASS_SUFFIX = 'NgFactory';
@@ -56761,7 +56764,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -60539,7 +60543,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+69.sha-b6aeace.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+72.sha-2bb9a65.with-local-changes');
 
     /**
      * @license
