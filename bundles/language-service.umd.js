4ddd60887db5098f07cf88c1cbffa07e5586e2d0
Miško Hevery <misko@hevery.com>
2019-07-24 21:56:08 +0000

    0440dc948 fix(zone.js): don't wrap uncaught promise error. (#31443)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+22.sha-5407b43.with-local-changes
+ * @license Angular v8.1.2+25.sha-0440dc9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18019,7 +18019,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.2+22.sha-5407b43.with-local-changes');
+    var VERSION$1 = new Version('8.1.2+25.sha-0440dc9.with-local-changes');
 
     /**
      * @license
@@ -38687,7 +38687,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.2+22.sha-5407b43.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.2+25.sha-0440dc9.with-local-changes');
 
     /**
      * @license
@@ -49548,7 +49548,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.2+22.sha-5407b43.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.2+25.sha-0440dc9.with-local-changes');
 
     /**
      * @license
