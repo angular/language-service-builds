f3872b598ff9f14393b3fb8e7d92a6deeee4707a
Miško Hevery <misko@hevery.com>
2019-07-25 20:30:29 +0000

    6b67cd562 feat(ivy): index template references, variables, bound attributes/events (#31535)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+88.sha-0cd4c01.with-local-changes
+ * @license Angular v8.2.0-next.2+89.sha-6b67cd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18106,7 +18106,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+89.sha-6b67cd5.with-local-changes');
 
     /**
      * @license
@@ -38622,7 +38622,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+89.sha-6b67cd5.with-local-changes');
 
     /**
      * @license
@@ -49462,7 +49462,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+89.sha-6b67cd5.with-local-changes');
 
     /**
      * @license
