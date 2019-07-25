8b2f71b94898e29a5e83d72bb6373e2d9cd8718b
Misko Hevery <misko@hevery.com>
2019-07-25 23:24:09 +0000

    55fab0a5c build: lock material unit tests job to specific commit (#31569) (#31847)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+33.sha-fdf2f69.with-local-changes
+ * @license Angular v8.1.2+35.sha-55fab0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18019,7 +18019,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.2+33.sha-fdf2f69.with-local-changes');
+    var VERSION$1 = new Version('8.1.2+35.sha-55fab0a.with-local-changes');
 
     /**
      * @license
@@ -38687,7 +38687,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.1.2+33.sha-fdf2f69.with-local-changes');
+    var VERSION$2 = new Version$1('8.1.2+35.sha-55fab0a.with-local-changes');
 
     /**
      * @license
@@ -45725,6 +45725,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var SWITCH_IVY_ENABLED__PRE_R3__ = false;
+    var ivyEnabled = SWITCH_IVY_ENABLED__PRE_R3__;
 
     /**
      * @license
@@ -45764,7 +45766,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = this._compiler instanceof Compiler;
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -49546,7 +49548,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.1.2+33.sha-fdf2f69.with-local-changes');
+    var VERSION$3 = new Version$1('8.1.2+35.sha-55fab0a.with-local-changes');
 
     /**
      * @license
