68983da90c27ff96feeac9732ac60eb7407b88db
Miško Hevery <misko@hevery.com>
2019-07-25 20:30:30 +0000

    0cd4c019c build: lock material unit tests job to specific commit (#31569)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+86.sha-85d051f.with-local-changes
+ * @license Angular v8.2.0-next.2+88.sha-0cd4c01.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18091,7 +18091,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+86.sha-85d051f.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
 
     /**
      * @license
@@ -46797,7 +46797,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+86.sha-85d051f.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
 
     /**
      * @license
@@ -60544,7 +60544,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+86.sha-85d051f.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
 
     /**
      * @license
