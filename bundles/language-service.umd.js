e71cda1bf95f37a470b38020eba55e2ed40eccbb
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:42:37 +0000

    397d0ba9a test(ivy): fix broken testcase in Windows (#31860)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
+ * @license Angular v8.2.0-next.2+99.sha-397d0ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18111,7 +18111,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
 
     /**
      * @license
@@ -38627,7 +38627,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
 
     /**
      * @license
@@ -49467,7 +49467,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
 
     /**
      * @license
