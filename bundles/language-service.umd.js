ad8e1334ff2c4a8a1139b07953e8915d4fed8151
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:22:56 +0000

    4da805243 docs(core): clarify @ContentChild(ren) behavior (#31846)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+99.sha-397d0ba.with-local-changes
+ * @license Angular v8.2.0-next.2+101.sha-4da8052.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18096,7 +18096,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+101.sha-4da8052.with-local-changes');
 
     /**
      * @license
@@ -46802,7 +46802,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+101.sha-4da8052.with-local-changes');
 
     /**
      * @license
@@ -60549,7 +60549,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+101.sha-4da8052.with-local-changes');
 
     /**
      * @license
