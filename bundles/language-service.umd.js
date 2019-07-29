edaa93abd2e7d2f4e0c1b7f61352dcae00b8ab4f
Andrew Kushnir <akushnir@google.com>
2019-07-29 17:21:06 +0000

    5f0d5e9cc build: update to nodejs rules 0.34.0 and bazel 0.28.1 (#31824)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
+ * @license Angular v8.2.0-next.2+104.sha-5f0d5e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18111,7 +18111,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
 
     /**
      * @license
@@ -29794,14 +29794,12 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * @publicApi
      */
     function resolveForwardRef$1(type) {
-        var fn = type;
-        if (typeof fn === 'function' && fn.hasOwnProperty(__forward_ref__) &&
-            fn.__forward_ref__ === forwardRef) {
-            return fn();
-        }
-        else {
-            return type;
-        }
+        return isForwardRef(type) ? type() : type;
+    }
+    /** Checks whether a function is wrapped by a `forwardRef`. */
+    function isForwardRef(fn) {
+        return typeof fn === 'function' && fn.hasOwnProperty(__forward_ref__) &&
+            fn.__forward_ref__ === forwardRef;
     }
 
     /**
@@ -38627,7 +38625,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
 
     /**
      * @license
@@ -41655,6 +41653,10 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    /**
+     * The locale id that the application is using by default (for translations and ICU expressions).
+     */
+    var DEFAULT_LOCALE_ID = 'en-US';
 
     /**
      * @license
@@ -41668,7 +41670,6 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * This is the ivy version of `LOCALE_ID` that was defined as an injection token for the view engine
      * but is now defined as a global value.
      */
-    var DEFAULT_LOCALE_ID = 'en-US';
     var LOCALE_ID = DEFAULT_LOCALE_ID;
     /**
      * Sets the locale id that will be used for translations and ICU expressions.
@@ -41678,7 +41679,10 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * @param localeId
      */
     function setLocaleId(localeId) {
-        LOCALE_ID = localeId.toLowerCase().replace(/_/g, '-');
+        assertDefined(localeId, "Expected localeId to be defined");
+        if (typeof localeId === 'string') {
+            LOCALE_ID = localeId.toLowerCase().replace(/_/g, '-');
+        }
     }
 
     /**
@@ -44288,6 +44292,16 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var SWITCH_IVY_ENABLED__PRE_R3__ = false;
+    var ivyEnabled = SWITCH_IVY_ENABLED__PRE_R3__;
+
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
     function _throwError() {
         throw new Error("Runtime compiler is not loaded");
     }
@@ -45149,9 +45163,6 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
                 if (!exceptionHandler) {
                     throw new Error('No ErrorHandler. Is platform module (BrowserModule) included?');
                 }
-                // If the `LOCALE_ID` provider is defined at bootstrap we set the value for runtime i18n (ivy)
-                var localeId = moduleRef.injector.get(LOCALE_ID$1, DEFAULT_LOCALE_ID);
-                setLocaleId(localeId);
                 moduleRef.onDestroy(function () { return remove(_this._modules, moduleRef); });
                 ngZone.runOutsideAngular(function () { return ngZone.onError.subscribe({ next: function (error) { exceptionHandler.handleError(error); } }); });
                 return _callAndReportToErrorHandler(exceptionHandler, ngZone, function () {
@@ -45631,16 +45642,6 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var SWITCH_IVY_ENABLED__PRE_R3__ = false;
-    var ivyEnabled = SWITCH_IVY_ENABLED__PRE_R3__;
-
-    /**
-     * @license
-     * Copyright Google Inc. All Rights Reserved.
-     *
-     * Use of this source code is governed by an MIT-style license that can be
-     * found in the LICENSE file at https://angular.io/license
-     */
     var _SEPARATOR = '#';
     var FACTORY_CLASS_SUFFIX = 'NgFactory';
     /**
@@ -45672,7 +45673,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            var legacyOfflineMode = this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -46538,15 +46539,21 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     }
     function _localeFactory(locale) {
         if (locale) {
+            if (ivyEnabled) {
+                setLocaleId(locale);
+            }
             return locale;
         }
         // Use `goog.LOCALE` as default value for `LOCALE_ID` token for Closure Compiler.
         // Note: default `goog.LOCALE` value is `en`, when Angular used `en-US`. In order to preserve
         // backwards compatibility, we use Angular default value over Closure Compiler's one.
         if (ngI18nClosureMode && typeof goog !== 'undefined' && goog.LOCALE !== 'en') {
+            if (ivyEnabled) {
+                setLocaleId(goog.LOCALE);
+            }
             return goog.LOCALE;
         }
-        return 'en-US';
+        return DEFAULT_LOCALE_ID;
     }
     /**
      * A built-in [dependency injection token](guide/glossary#di-token)
@@ -49467,7 +49474,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
 
     /**
      * @license
