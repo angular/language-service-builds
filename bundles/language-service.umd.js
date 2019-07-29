795c80dcc149d2883757b8c50f01f2fe7fe28857
Andrew Kushnir <akushnir@google.com>
2019-07-29 17:27:24 +0000

    14dba72ae fix(core): DebugElement.listeners not cleared on destroy (#31820)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+104.sha-5f0d5e9.with-local-changes
+ * @license Angular v8.2.0-next.2+105.sha-14dba72.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18096,7 +18096,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+105.sha-14dba72.with-local-changes');
 
     /**
      * @license
@@ -46812,7 +46812,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-next.2+105.sha-14dba72.with-local-changes');
 
     /**
      * @license
@@ -56788,8 +56788,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -59375,7 +59374,11 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
         }
         DebugRenderer2.prototype.createDebugContext = function (nativeElement) { return this.debugContextFactory(nativeElement); };
         DebugRenderer2.prototype.destroyNode = function (node) {
-            removeDebugNodeFromIndex(getDebugNode(node));
+            var debugNode = getDebugNode(node);
+            removeDebugNodeFromIndex(debugNode);
+            if (debugNode instanceof DebugNode__PRE_R3__) {
+                debugNode.listeners.length = 0;
+            }
             if (this.delegate.destroyNode) {
                 this.delegate.destroyNode(node);
             }
@@ -60573,7 +60576,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-next.2+105.sha-14dba72.with-local-changes');
 
     /**
      * @license
