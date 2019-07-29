6cdc5a62ddc241e586a4c5d8e06da8dd46da73d8
Andrew Kushnir <akushnir@google.com>
2019-07-29 17:24:11 +0000

    46b160e92 fix(core): DebugElement.listeners not cleared on destroy (#31820)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+6.sha-dcbc28f.with-local-changes
+ * @license Angular v8.2.0-rc.0+7.sha-46b160e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18111,7 +18111,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-rc.0+6.sha-dcbc28f.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-rc.0+7.sha-46b160e.with-local-changes');
 
     /**
      * @license
@@ -38627,7 +38627,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0-rc.0+6.sha-dcbc28f.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0-rc.0+7.sha-46b160e.with-local-changes');
 
     /**
      * @license
@@ -48269,7 +48269,11 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
         }
         DebugRenderer2.prototype.createDebugContext = function (nativeElement) { return this.debugContextFactory(nativeElement); };
         DebugRenderer2.prototype.destroyNode = function (node) {
-            removeDebugNodeFromIndex(getDebugNode(node));
+            var debugNode = getDebugNode(node);
+            removeDebugNodeFromIndex(debugNode);
+            if (debugNode instanceof DebugNode__PRE_R3__) {
+                debugNode.listeners.length = 0;
+            }
             if (this.delegate.destroyNode) {
                 this.delegate.destroyNode(node);
             }
@@ -49467,7 +49471,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0-rc.0+6.sha-dcbc28f.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0-rc.0+7.sha-46b160e.with-local-changes');
 
     /**
      * @license
