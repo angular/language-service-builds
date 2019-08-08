f06c89c5f47ade57985c7ecb2f76758e705ac8d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:35:35 +0000

    a48376b26 docs(router): rename incorrect class names (#31815)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+28.sha-038d823.with-local-changes
+ * @license Angular v8.2.0+33.sha-a48376b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18111,7 +18111,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0+28.sha-038d823.with-local-changes');
+    var VERSION$1 = new Version('8.2.0+33.sha-a48376b.with-local-changes');
 
     /**
      * @license
@@ -38627,7 +38627,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.0+28.sha-038d823.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.0+33.sha-a48376b.with-local-changes');
 
     /**
      * @license
@@ -43458,11 +43458,15 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * found in the LICENSE file at https://angular.io/license
      */
     /**
-     * Use in directives and components to emit custom events synchronously
-     * or asynchronously, and register handlers for those events by subscribing
-     * to an instance.
+     * Use in components with the `@Output` directive to emit custom events
+     * synchronously or asynchronously, and register handlers for those events
+     * by subscribing to an instance.
      *
      * @usageNotes
+     *
+     * Extends
+     * [RxJS `Subject`](https://rxjs.dev/api/index/class/Subject)
+     * for Angular by adding the `emit()` method.
      *
      * In the following example, a component defines two output properties
      * that create event emitters. When the title is clicked, the emitter
@@ -43501,6 +43505,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * <zippy (open)="onOpen($event)" (close)="onClose($event)"></zippy>
      * ```
      *
+     * @see [Observables in Angular](guide/observables-in-angular)
      * @publicApi
      */
     var EventEmitter = /** @class */ (function (_super) {
@@ -49471,7 +49476,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.0+28.sha-038d823.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.0+33.sha-a48376b.with-local-changes');
 
     /**
      * @license
