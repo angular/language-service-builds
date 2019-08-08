3e4aea94ae30fafb2ed71c4bd91cf55cdbb8cfd4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 21:32:12 +0000

    2abbe98e3 release: cut the v9.0.0-next.1 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+73.sha-7613f13.with-local-changes
+ * @license Angular v9.0.0-next.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18111,7 +18111,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.0+73.sha-7613f13.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.1.with-local-changes');
 
     /**
      * @license
@@ -38628,7 +38628,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.0+73.sha-7613f13.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.1.with-local-changes');
 
     /**
      * @license
@@ -49458,7 +49458,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-next.0+73.sha-7613f13.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.1.with-local-changes');
 
     /**
      * @license
