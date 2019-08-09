3dd6415038c2007e03ad8802609fe743e25ec48d
Kara Erickson <karakara@google.com>
2019-08-09 18:07:57 +0000

    695f322dc docs: clarify pipe naming (#31806)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+7.sha-9896d43.with-local-changes
+ * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18096,7 +18096,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.1+7.sha-9896d43.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.1+5.sha-695f322.with-local-changes');
 
     /**
      * @license
@@ -46856,7 +46856,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.1+7.sha-9896d43.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.1+5.sha-695f322.with-local-changes');
 
     /**
      * @license
@@ -56836,8 +56836,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -60601,7 +60600,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-next.1+7.sha-9896d43.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.1+5.sha-695f322.with-local-changes');
 
     /**
      * @license
