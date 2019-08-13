8b2cc9a2fa5a1c8a24725e3cdf1f19c692e0b8c4
Kara Erickson <karakara@google.com>
2019-08-13 18:38:33 +0000

    a95f860a9 docs: correct description of animation example (#32009)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+6.sha-6215636.with-local-changes
+ * @license Angular v9.0.0-next.2+7.sha-a95f860.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18107,7 +18107,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+6.sha-6215636.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+7.sha-a95f860.with-local-changes');
 
     /**
      * @license
@@ -46799,7 +46799,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.2+6.sha-6215636.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.2+7.sha-a95f860.with-local-changes');
 
     /**
      * @license
@@ -52225,7 +52225,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -56769,7 +56769,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -60533,7 +60534,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-next.2+6.sha-6215636.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.2+7.sha-a95f860.with-local-changes');
 
     /**
      * @license
