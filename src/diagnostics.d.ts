c03bd2d34e21ccb2240c485e07753fc258bd083e
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:14:30 +0000

    f6a1de6b3 build(docs-infra): upgrade cli command docs sources to fe88669c9 (#32147)

--- src/diagnostics.d.ts
+++ src/diagnostics.d.ts
@@ -9,10 +9,13 @@
 import { NgAnalyzedModules } from '@angular/compiler';
 import * as ts from 'typescript';
 import { AstResult } from './common';
-import { Declarations, Diagnostic, Diagnostics, TemplateSource } from './types';
+import { Declarations, Diagnostic, Diagnostics, Span, TemplateSource } from './types';
 export interface AstProvider {
     getTemplateAst(template: TemplateSource, fileName: string): AstResult;
 }
 export declare function getTemplateDiagnostics(template: TemplateSource, ast: AstResult): Diagnostics;
 export declare function getDeclarationDiagnostics(declarations: Declarations, modules: NgAnalyzedModules): Diagnostics;
 export declare function ngDiagnosticToTsDiagnostic(d: Diagnostic, file: ts.SourceFile | undefined): ts.Diagnostic;
+export declare function uniqueBySpan<T extends {
+    span: Span;
+}>(elements: T[]): T[];
