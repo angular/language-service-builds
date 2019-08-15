c77aaefba3058bef4748ee6c116c71a38bdea24c
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:16:36 +0000

    253a1125b test(ivy): add style binding node-based micro benchmark (#32104)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+32.sha-f6a1de6.with-local-changes
+ * @license Angular v9.0.0-next.2+37.sha-253a112.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18107,7 +18107,7 @@ define(['exports', 'path', 'typescript'], function (exports, path, ts) { 'use st
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+32.sha-f6a1de6.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+37.sha-253a112.with-local-changes');
 
     /**
      * @license
@@ -46843,7 +46843,7 @@ define(['exports', 'path', 'typescript'], function (exports, path, ts) { 'use st
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.2+32.sha-f6a1de6.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.2+37.sha-253a112.with-local-changes');
 
     /**
      * @license
@@ -56811,7 +56811,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -60468,7 +60469,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-next.2+32.sha-f6a1de6.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.2+37.sha-253a112.with-local-changes');
 
     /**
      * @license
