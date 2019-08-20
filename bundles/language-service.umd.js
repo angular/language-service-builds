c9b1dad72f096026edc05ffc2886a317913097f1
Andrew Kushnir <akushnir@google.com>
2019-08-20 18:08:57 +0000

    3dbc4ab57 fix(ivy): get name directly from nativeNode (#32198)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+68.sha-cfed0c0.with-local-changes
+ * @license Angular v9.0.0-next.2+69.sha-3dbc4ab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18615,7 +18615,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+68.sha-cfed0c0.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+69.sha-3dbc4ab.with-local-changes');
 
     /**
      * @license
@@ -43257,7 +43257,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.2+68.sha-cfed0c0.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.2+69.sha-3dbc4ab.with-local-changes');
 
     /**
      * @license
@@ -50271,7 +50271,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            var legacyOfflineMode = this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -50631,7 +50631,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             configurable: true
         });
         Object.defineProperty(DebugElement__POST_R3__.prototype, "name", {
-            get: function () { return this.nativeElement.nodeName; },
+            get: function () { return this.nativeNode.nodeName; },
             enumerable: true,
             configurable: true
         });
@@ -53512,7 +53512,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version('9.0.0-next.2+68.sha-cfed0c0.with-local-changes');
+    var VERSION$3 = new Version('9.0.0-next.2+69.sha-3dbc4ab.with-local-changes');
 
     /**
      * @license
@@ -70209,7 +70209,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.2+68.sha-cfed0c0.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.2+69.sha-3dbc4ab.with-local-changes');
 
     /**
      * @license
