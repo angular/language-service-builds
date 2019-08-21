32784c2a1ff86d8bdb6800da1edc9e226daf3dae
Andrew Kushnir <akushnir@google.com>
2019-08-21 17:31:38 +0000

    8e1a72546 build(docs-infra): upgrade cli command docs sources to d2489aeda (#32236)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+78.sha-424ab48.with-local-changes
+ * @license Angular v9.0.0-next.2+79.sha-8e1a725.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18608,7 +18608,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+78.sha-424ab48.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+79.sha-8e1a725.with-local-changes');
 
     /**
      * @license
@@ -51328,7 +51328,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.2+78.sha-424ab48.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.2+79.sha-8e1a725.with-local-changes');
 
     /**
      * @license
@@ -64491,7 +64491,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version('9.0.0-next.2+78.sha-424ab48.with-local-changes');
+    var VERSION$3 = new Version('9.0.0-next.2+79.sha-8e1a725.with-local-changes');
 
     /**
      * @license
@@ -81180,7 +81180,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.2+78.sha-424ab48.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.2+79.sha-8e1a725.with-local-changes');
 
     /**
      * @license
