e712d5f9f8a33470395966ba8b085827743f64cb
atscott <atscott@google.com>
2019-08-22 20:53:02 +0000

    21bb74d16 docs: improve structure (#32227)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+13.sha-6d11154.with-local-changes
+ * @license Angular v9.0.0-next.3+19.sha-21bb74d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18619,7 +18619,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+13.sha-6d11154.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+19.sha-21bb74d.with-local-changes');
 
     /**
      * @license
@@ -51400,7 +51400,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.3+13.sha-6d11154.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.3+19.sha-21bb74d.with-local-changes');
 
     /**
      * @license
@@ -64564,7 +64564,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version('9.0.0-next.3+13.sha-6d11154.with-local-changes');
+    var VERSION$3 = new Version('9.0.0-next.3+19.sha-21bb74d.with-local-changes');
 
     /**
      * @license
@@ -81590,7 +81590,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.3+13.sha-6d11154.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.3+19.sha-21bb74d.with-local-changes');
 
     /**
      * @license
