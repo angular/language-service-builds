71bc5959c9befdcc4bc191738a96aaf715cf0325
atscott <atscott@google.com>
2019-08-27 16:31:37 +0000

    70cf8ed05 docs: Fix typo in singleton-services.md page (#32330)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+47.sha-f57c17d.with-local-changes
+ * @license Angular v9.0.0-next.3+50.sha-70cf8ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18619,7 +18619,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+50.sha-70cf8ed.with-local-changes');
 
     /**
      * @license
@@ -51398,7 +51398,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.3+50.sha-70cf8ed.with-local-changes');
 
     /**
      * @license
@@ -64562,7 +64562,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
+    var VERSION$3 = new Version('9.0.0-next.3+50.sha-70cf8ed.with-local-changes');
 
     /**
      * @license
@@ -81594,7 +81594,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.3+50.sha-70cf8ed.with-local-changes');
 
     /**
      * @license
