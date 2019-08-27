99eccbae0bfecfb5c93cbc63db6b0b794a6e18c5
atscott <atscott@google.com>
2019-08-27 16:28:14 +0000

    f57c17de2 build: remove the deprecated `aio` commit message scope (#32341)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+49.sha-c7c7f9f.with-local-changes
+ * @license Angular v9.0.0-next.3+47.sha-f57c17d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18634,7 +18634,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
     /**
      * @license
@@ -43326,7 +43326,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
     /**
      * @license
@@ -53581,7 +53581,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+    var VERSION$3 = new Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
     /**
      * @license
@@ -70613,7 +70613,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
     /**
      * @license
