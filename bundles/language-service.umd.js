b25584feaaef9789e3dff8505220602e53cbddaa
atscott <atscott@google.com>
2019-08-28 16:22:14 +0000

    125ef0a0e build(docs-infra): upgrade cli command docs sources to 0d56c1f5f (#32367)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+57.sha-ef20475.with-local-changes
+ * @license Angular v9.0.0-next.3+61.sha-125ef0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18670,7 +18670,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+57.sha-ef20475.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+61.sha-125ef0a.with-local-changes');
 
     /**
      * @license
@@ -43372,7 +43372,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-next.3+57.sha-ef20475.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-next.3+61.sha-125ef0a.with-local-changes');
 
     /**
      * @license
@@ -50386,7 +50386,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            var legacyOfflineMode = this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -53638,7 +53638,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version('9.0.0-next.3+57.sha-ef20475.with-local-changes');
+    var VERSION$3 = new Version('9.0.0-next.3+61.sha-125ef0a.with-local-changes');
 
     /**
      * @license
@@ -70699,7 +70699,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.3+57.sha-ef20475.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.3+61.sha-125ef0a.with-local-changes');
 
     /**
      * @license
