ede7f58f60d153463a26f1a45b0ee6c49c494677
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 23:22:43 +0000

    4c3674f66 docs: expand abbreviation in providers guide (#32400)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+17.sha-a9ff48e.with-local-changes
+ * @license Angular v9.0.0-next.5+21.sha-4c3674f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13004,9 +13004,10 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
         };
         StylingBuilder.prototype.registerInputBasedOnName = function (name, expression, sourceSpan) {
             var binding = null;
-            var nameToMatch = name.substring(0, 5); // class | style
-            var isStyle = nameToMatch === 'style';
-            var isClass = isStyle ? false : (nameToMatch === 'class');
+            var prefix = name.substring(0, 6);
+            var isStyle = name === 'style' || prefix === 'style.' || prefix === 'style!';
+            var isClass = !isStyle &&
+                (name === 'class' || name === 'className' || prefix === 'class.' || prefix === 'class!');
             if (isStyle || isClass) {
                 var isMapBased = name.charAt(5) !== '.'; // style.prop or class.prop makes this a no
                 var property = name.substr(isMapBased ? 5 : 6); // the dot explains why there's a +1
@@ -18840,7 +18841,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.5+21.sha-4c3674f.with-local-changes');
 
     /**
      * @license
@@ -34148,7 +34149,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.5+21.sha-4c3674f.with-local-changes');
 
     /**
      * @license
@@ -65530,9 +65531,13 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
                     existingListener = findExistingListener(lView, eventName, tNode.index);
                 }
                 if (existingListener !== null) {
-                    // Attach a new listener at the head of the coalesced listeners list.
-                    listenerFn.__ngNextListenerFn__ = existingListener.__ngNextListenerFn__;
-                    existingListener.__ngNextListenerFn__ = listenerFn;
+                    // Attach a new listener to coalesced listeners list, maintaining the order in which
+                    // listeners are registered. For performance reasons, we keep a reference to the last
+                    // listener in that list (in `__ngLastListenerFn__` field), so we can avoid going through
+                    // the entire set each time we need to add a new listener.
+                    var lastListenerFn = existingListener.__ngLastListenerFn__ || existingListener;
+                    lastListenerFn.__ngNextListenerFn__ = listenerFn;
+                    existingListener.__ngLastListenerFn__ = listenerFn;
                     processOutputs = false;
                 }
                 else {
@@ -68453,7 +68458,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.5+21.sha-4c3674f.with-local-changes');
 
     /**
      * @license
@@ -78417,8 +78422,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -81980,7 +81984,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.5+21.sha-4c3674f.with-local-changes');
 
     /**
      * @license
