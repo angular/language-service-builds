f926536882eb8149f3cfc094d48298ae7dee0d91
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:28:56 +0000

    fed6b25be ci: move to latest lock-closed commit for github action (#32502)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+15.sha-5ab7cb4.with-local-changes
+ * @license Angular v9.0.0-next.5+14.sha-fed6b25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18855,7 +18855,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.5+15.sha-5ab7cb4.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.5+14.sha-fed6b25.with-local-changes');
 
     /**
      * @license
@@ -34163,7 +34163,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.5+15.sha-5ab7cb4.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.5+14.sha-fed6b25.with-local-changes');
 
     /**
      * @license
@@ -59826,14 +59826,6 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-
-    /**
-     * @license
-     * Copyright Google Inc. All Rights Reserved.
-     *
-     * Use of this source code is governed by an MIT-style license that can be
-     * found in the LICENSE file at https://angular.io/license
-     */
     /**
      * Returns the component instance associated with a given DOM host element.
      * Elements which don't represent components return `null`.
@@ -60473,7 +60465,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs'], function (exports, path, t
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.5+15.sha-5ab7cb4.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.5+14.sha-fed6b25.with-local-changes');
 
     /**
      * @license
@@ -67484,7 +67476,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = this._compiler instanceof Compiler;
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -71061,7 +71053,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.5+15.sha-5ab7cb4.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.5+14.sha-fed6b25.with-local-changes');
 
     /**
      * @license
