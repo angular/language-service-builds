5f870e0c59088619bf6065959cae3bfb67affba7
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 16:54:47 +0000

    0a0489d3d ci: run lock closed issues more frequently during backlog clearing, update to latest version from dev-infra (#32637)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+17.sha-fcdd068.with-local-changes
+ * @license Angular v9.0.0-next.6+18.sha-0a0489d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18863,7 +18863,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.6+17.sha-fcdd068.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.6+18.sha-0a0489d.with-local-changes');
 
     /**
      * @license
@@ -34171,7 +34171,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.6+17.sha-fcdd068.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.6+18.sha-0a0489d.with-local-changes');
 
     /**
      * @license
@@ -60501,7 +60501,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.6+17.sha-fcdd068.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.6+18.sha-0a0489d.with-local-changes');
 
     /**
      * @license
@@ -64271,7 +64271,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -67512,7 +67512,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = this._compiler instanceof Compiler;
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -71126,7 +71126,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.6+17.sha-fcdd068.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.6+18.sha-0a0489d.with-local-changes');
 
     /**
      * @license
