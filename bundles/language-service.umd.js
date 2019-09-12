61982d59feb62d13c73e3338fdc8ae178e6e267c
Kara Erickson <karakara@google.com>
2019-09-12 17:57:15 +0000

    73cb58172 perf(ivy): avoid repeat lview reads in pipe instructions (#32633)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+21.sha-1771d6f.with-local-changes
+ * @license Angular v9.0.0-next.6+28.sha-73cb581.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18848,7 +18848,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.6+28.sha-73cb581.with-local-changes');
 
     /**
      * @license
@@ -34156,7 +34156,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.6+28.sha-73cb581.with-local-changes');
 
     /**
      * @license
@@ -53941,12 +53941,6 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
         }
         return null;
     }
-    /**
-     * A helper function that returns `true` if a given `TNode` has any matching directives.
-     */
-    function hasDirectives(tNode) {
-        return tNode.directiveEnd > tNode.directiveStart;
-    }
     function getTNode(index, view) {
         ngDevMode && assertGreaterThan(index, -1, 'wrong index for TNode');
         ngDevMode && assertLessThan(index, view[TVIEW].data.length, 'wrong index for TNode');
@@ -65483,7 +65477,9 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      */
     function ɵɵlistener(eventName, listenerFn, useCapture, eventTargetResolver) {
         if (useCapture === void 0) { useCapture = false; }
-        listenerInternal(eventName, listenerFn, useCapture, eventTargetResolver);
+        var lView = getLView();
+        var tNode = getPreviousOrParentTNode();
+        listenerInternal(lView, lView[RENDERER], tNode, eventName, listenerFn, useCapture, eventTargetResolver);
     }
     /**
     * Registers a synthetic host listener (e.g. `(@foo.start)`) on a component.
@@ -65508,7 +65504,10 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     */
     function ɵɵcomponentHostSyntheticListener(eventName, listenerFn, useCapture, eventTargetResolver) {
         if (useCapture === void 0) { useCapture = false; }
-        listenerInternal(eventName, listenerFn, useCapture, eventTargetResolver, loadComponentRenderer);
+        var lView = getLView();
+        var tNode = getPreviousOrParentTNode();
+        var renderer = loadComponentRenderer(tNode, lView);
+        listenerInternal(lView, renderer, tNode, eventName, listenerFn, useCapture, eventTargetResolver);
     }
     /**
      * A utility function that checks if a given element has already an event handler registered for an
@@ -65541,11 +65540,10 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
         }
         return null;
     }
-    function listenerInternal(eventName, listenerFn, useCapture, eventTargetResolver, loadRendererFn) {
+    function listenerInternal(lView, renderer, tNode, eventName, listenerFn, useCapture, eventTargetResolver) {
         if (useCapture === void 0) { useCapture = false; }
-        var lView = getLView();
-        var tNode = getPreviousOrParentTNode();
         var tView = lView[TVIEW];
+        var isTNodeDirectiveHost = isDirectiveHost(tNode);
         var firstTemplatePass = tView.firstTemplatePass;
         var tCleanup = firstTemplatePass && (tView.cleanup || (tView.cleanup = []));
         ngDevMode && assertNodeOfPossibleTypes(tNode, 3 /* Element */, 0 /* Container */, 4 /* ElementContainer */);
@@ -65555,7 +65553,6 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
             var native = getNativeByTNode(tNode, lView);
             var resolved = eventTargetResolver ? eventTargetResolver(native) : EMPTY_OBJ;
             var target = resolved.target || native;
-            var renderer = loadRendererFn ? loadRendererFn(tNode, lView) : lView[RENDERER];
             var lCleanup = getCleanup(lView);
             var lCleanupIndex = lCleanup.length;
             var idxOrTargetGetter = eventTargetResolver ?
@@ -65581,7 +65578,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
                 // Also, we don't have to search for existing listeners is there are no directives
                 // matching on a given node as we can't register multiple event handlers for the same event in
                 // a template (this would mean having duplicate attributes).
-                if (!eventTargetResolver && hasDirectives(tNode)) {
+                if (!eventTargetResolver && isTNodeDirectiveHost) {
                     existingListener = findExistingListener(lView, eventName, tNode.index);
                 }
                 if (existingListener !== null) {
@@ -65614,30 +65611,32 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
             }
         }
         // subscribe to directive outputs
-        if (tNode.outputs === undefined) {
-            // if we create TNode here, inputs must be undefined so we know they still need to be
-            // checked
-            tNode.outputs = generatePropertyAliases(tView, tNode, 1 /* Output */);
-        }
-        var outputs = tNode.outputs;
-        var props;
-        if (processOutputs && outputs && (props = outputs[eventName])) {
-            var propsLength = props.length;
-            if (propsLength) {
-                var lCleanup = getCleanup(lView);
-                for (var i = 0; i < propsLength; i += 3) {
-                    var index = props[i];
-                    ngDevMode && assertDataInRange(lView, index);
-                    var minifiedName = props[i + 2];
-                    var directiveInstance = lView[index];
-                    var output = directiveInstance[minifiedName];
-                    if (ngDevMode && !isObservable(output)) {
-                        throw new Error("@Output " + minifiedName + " not initialized in '" + directiveInstance.constructor.name + "'.");
+        if (isTNodeDirectiveHost && processOutputs) {
+            var outputs = tNode.outputs;
+            if (outputs === undefined) {
+                // if we create TNode here, inputs must be undefined so we know they still need to be
+                // checked
+                outputs = tNode.outputs = generatePropertyAliases(tView, tNode, 1 /* Output */);
+            }
+            var props = void 0;
+            if (outputs !== null && (props = outputs[eventName])) {
+                var propsLength = props.length;
+                if (propsLength) {
+                    var lCleanup = getCleanup(lView);
+                    for (var i = 0; i < propsLength; i += 3) {
+                        var index = props[i];
+                        ngDevMode && assertDataInRange(lView, index);
+                        var minifiedName = props[i + 2];
+                        var directiveInstance = lView[index];
+                        var output = directiveInstance[minifiedName];
+                        if (ngDevMode && !isObservable(output)) {
+                            throw new Error("@Output " + minifiedName + " not initialized in '" + directiveInstance.constructor.name + "'.");
+                        }
+                        var subscription = output.subscribe(listenerFn);
+                        var idx = lCleanup.length;
+                        lCleanup.push(listenerFn, subscription);
+                        tCleanup && tCleanup.push(eventName, tNode.index, idx, -(idx + 1));
                     }
-                    var subscription = output.subscribe(listenerFn);
-                    var idx = lCleanup.length;
-                    lCleanup.push(listenerFn, subscription);
-                    tCleanup && tCleanup.push(eventName, tNode.index, idx, -(idx + 1));
                 }
             }
         }
@@ -68511,7 +68510,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.6+28.sha-73cb581.with-local-changes');
 
     /**
      * @license
@@ -73253,8 +73252,10 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * @codeGenApi
      */
     function ɵɵpipeBind1(index, slotOffset, v1) {
-        var pipeInstance = load(getLView(), index);
-        return unwrapValue(isPure(index) ? ɵɵpureFunction1(slotOffset, pipeInstance.transform, v1, pipeInstance) :
+        var lView = getLView();
+        var pipeInstance = load(lView, index);
+        return unwrapValue(lView, isPure(lView, index) ?
+            ɵɵpureFunction1(slotOffset, pipeInstance.transform, v1, pipeInstance) :
             pipeInstance.transform(v1));
     }
     /**
@@ -73271,8 +73272,10 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * @codeGenApi
      */
     function ɵɵpipeBind2(index, slotOffset, v1, v2) {
-        var pipeInstance = load(getLView(), index);
-        return unwrapValue(isPure(index) ? ɵɵpureFunction2(slotOffset, pipeInstance.transform, v1, v2, pipeInstance) :
+        var lView = getLView();
+        var pipeInstance = load(lView, index);
+        return unwrapValue(lView, isPure(lView, index) ?
+            ɵɵpureFunction2(slotOffset, pipeInstance.transform, v1, v2, pipeInstance) :
             pipeInstance.transform(v1, v2));
     }
     /**
@@ -73290,8 +73293,9 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * @codeGenApi
      */
     function ɵɵpipeBind3(index, slotOffset, v1, v2, v3) {
-        var pipeInstance = load(getLView(), index);
-        return unwrapValue(isPure(index) ?
+        var lView = getLView();
+        var pipeInstance = load(lView, index);
+        return unwrapValue(lView, isPure(lView, index) ?
             ɵɵpureFunction3(slotOffset, pipeInstance.transform, v1, v2, v3, pipeInstance) :
             pipeInstance.transform(v1, v2, v3));
     }
@@ -73311,8 +73315,9 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * @codeGenApi
      */
     function ɵɵpipeBind4(index, slotOffset, v1, v2, v3, v4) {
-        var pipeInstance = load(getLView(), index);
-        return unwrapValue(isPure(index) ?
+        var lView = getLView();
+        var pipeInstance = load(lView, index);
+        return unwrapValue(lView, isPure(lView, index) ?
             ɵɵpureFunction4(slotOffset, pipeInstance.transform, v1, v2, v3, v4, pipeInstance) :
             pipeInstance.transform(v1, v2, v3, v4));
     }
@@ -73329,12 +73334,14 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * @codeGenApi
      */
     function ɵɵpipeBindV(index, slotOffset, values) {
-        var pipeInstance = load(getLView(), index);
-        return unwrapValue(isPure(index) ? ɵɵpureFunctionV(slotOffset, pipeInstance.transform, values, pipeInstance) :
+        var lView = getLView();
+        var pipeInstance = load(lView, index);
+        return unwrapValue(lView, isPure(lView, index) ?
+            ɵɵpureFunctionV(slotOffset, pipeInstance.transform, values, pipeInstance) :
             pipeInstance.transform.apply(pipeInstance, values));
     }
-    function isPure(index) {
-        return getLView()[TVIEW].data[index + HEADER_OFFSET].pure;
+    function isPure(lView, index) {
+        return lView[TVIEW].data[index + HEADER_OFFSET].pure;
     }
     /**
      * Unwrap the output of a pipe transformation.
@@ -73343,10 +73350,9 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      *
      * @param newValue the pipe transformation output.
      */
-    function unwrapValue(newValue) {
+    function unwrapValue(lView, newValue) {
         if (WrappedValue.isWrapped(newValue)) {
             newValue = WrappedValue.unwrap(newValue);
-            var lView = getLView();
             // The NO_CHANGE value needs to be written at the index where the impacted binding value is
             // stored
             var bindingToInvalidateIdx = lView[BINDING_INDEX];
@@ -78479,7 +78485,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
@@ -82090,7 +82097,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.6+28.sha-73cb581.with-local-changes');
 
     /**
      * @license
