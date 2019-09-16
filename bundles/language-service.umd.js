daa6cbe19a04fc7499d3c14db2a69a95910e7d50
Kara Erickson <karakara@google.com>
2019-09-16 17:05:05 +0000

    947c076ff fix(compiler-cli): fix typo in diagnostic template info. (#32684)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+12.sha-6600bea.with-local-changes
+ * @license Angular v8.2.6+13.sha-947c076.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18111,7 +18111,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.6+12.sha-6600bea.with-local-changes');
+    var VERSION$1 = new Version('8.2.6+13.sha-947c076.with-local-changes');
 
     /**
      * @license
@@ -24865,7 +24865,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
                         this.reportError('The template context does not have an implicit value', spanOf$1(ast.sourceSpan));
                     }
                     else {
-                        this.reportError("The template context does not defined a member called '" + ast.value + "'", spanOf$1(ast.sourceSpan));
+                        this.reportError("The template context does not define a member called '" + ast.value + "'", spanOf$1(ast.sourceSpan));
                     }
                 }
             }
@@ -38627,7 +38627,7 @@ define(['exports', 'path', 'typescript', 'fs'], function (exports, path, ts, fs)
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('8.2.6+12.sha-6600bea.with-local-changes');
+    var VERSION$2 = new Version$1('8.2.6+13.sha-947c076.with-local-changes');
 
     /**
      * @license
@@ -49498,7 +49498,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('8.2.6+12.sha-6600bea.with-local-changes');
+    var VERSION$3 = new Version$1('8.2.6+13.sha-947c076.with-local-changes');
 
     /**
      * @license
