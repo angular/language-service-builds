4a75a3830ec190db2e8943358a2955be64405f89
Kara Erickson <karakara@google.com>
2019-09-16 16:56:54 +0000

    52a6da043 fix(ivy): correct debug array names (#32691)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+55.sha-1748aeb.with-local-changes
+ * @license Angular v9.0.0-next.6+56.sha-52a6da0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18867,7 +18867,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.6+55.sha-1748aeb.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.6+56.sha-52a6da0.with-local-changes');
 
     /**
      * @license
@@ -34192,7 +34192,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.6+55.sha-1748aeb.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.6+56.sha-52a6da0.with-local-changes');
 
     /**
      * @license
@@ -53942,7 +53942,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
             try {
                 // We need to do it this way so that TypeScript does not down-level the below code.
                 var FunctionConstructor = createNamedArrayType.constructor;
-                return (new FunctionConstructor('Array', "return class ABC extends Array{}"))(Array);
+                return (new FunctionConstructor('Array', "return class " + name + " extends Array{}"))(Array);
             }
             catch (e) {
                 // If it does not work just give up and fall back to regular Array.
@@ -60626,7 +60626,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.6+55.sha-1748aeb.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.6+56.sha-52a6da0.with-local-changes');
 
     /**
      * @license
@@ -71324,7 +71324,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.6+55.sha-1748aeb.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.6+56.sha-52a6da0.with-local-changes');
 
     /**
      * @license
