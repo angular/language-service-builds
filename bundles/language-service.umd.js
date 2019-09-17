e594de01fb29a4f17fdac940d9df5c7a6e50fa9a
Andrew Kushnir <akushnir@google.com>
2019-09-17 20:52:23 +0000

    a0d04c628 fix(ivy): ensure that `window.ng` utilities are published when NgModules are used (#32725)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+71.sha-b741a1c.with-local-changes
+ * @license Angular v9.0.0-next.6+72.sha-a0d04c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18842,7 +18842,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.6+71.sha-b741a1c.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.6+72.sha-a0d04c6.with-local-changes');
 
     /**
      * @license
@@ -34167,7 +34167,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.6+71.sha-b741a1c.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.6+72.sha-a0d04c6.with-local-changes');
 
     /**
      * @license
@@ -60782,7 +60782,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.6+71.sha-b741a1c.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.6+72.sha-a0d04c6.with-local-changes');
 
     /**
      * @license
@@ -71480,7 +71480,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.6+71.sha-b741a1c.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.6+72.sha-a0d04c6.with-local-changes');
 
     /**
      * @license
