3092dbdac828834f623c97ac99d3976959c2bd4b
atscott <atscott@google.com>
2019-09-27 18:56:40 +0000

    84feda1db build(docs-infra): do not ngcc the 'module' entry points (#32679)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+15.sha-5ae48d5.with-local-changes
+ * @license Angular v9.0.0-next.8+17.sha-84feda1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18849,7 +18849,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.8+15.sha-5ae48d5.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.8+17.sha-84feda1.with-local-changes');
 
     /**
      * @license
@@ -34174,7 +34174,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.8+15.sha-5ae48d5.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.8+17.sha-84feda1.with-local-changes');
 
     /**
      * @license
@@ -60813,7 +60813,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.8+15.sha-5ae48d5.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.8+17.sha-84feda1.with-local-changes');
 
     /**
      * @license
@@ -67824,7 +67824,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            var legacyOfflineMode = this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -71518,7 +71518,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.8+15.sha-5ae48d5.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.8+17.sha-84feda1.with-local-changes');
 
     /**
      * @license
