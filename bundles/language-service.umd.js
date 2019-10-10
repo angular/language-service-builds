4c2c60105d25629e49ed97183dd2efd364b692e1
Miško Hevery <misko@hevery.com>
2019-10-10 21:24:08 +0000

    8d111da7f perf(ivy): use instanceof operator to check for NodeInjectorFactory instances (#33082)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+3.sha-676a95a.with-local-changes
+ * @license Angular v9.0.0-next.10+16.sha-8d111da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18928,7 +18928,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+3.sha-676a95a.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+16.sha-8d111da.with-local-changes');
 
     /**
      * @license
@@ -34231,7 +34231,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.10+3.sha-676a95a.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.10+16.sha-8d111da.with-local-changes');
 
     /**
      * @license
@@ -53091,9 +53091,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
         return NodeInjectorFactory;
     }());
     function isFactory(obj) {
-        // See: https://jsperf.com/instanceof-vs-getprototypeof
-        return obj !== null && typeof obj == 'object' &&
-            Object.getPrototypeOf(obj) == NodeInjectorFactory.prototype;
+        return obj instanceof NodeInjectorFactory;
     }
 
     /**
@@ -69142,7 +69140,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.10+3.sha-676a95a.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.10+16.sha-8d111da.with-local-changes');
 
     /**
      * @license
@@ -82689,7 +82687,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.10+3.sha-676a95a.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.10+16.sha-8d111da.with-local-changes');
 
     /**
      * @license
