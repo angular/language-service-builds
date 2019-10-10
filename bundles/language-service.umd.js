70a9c80dbef2e03d1e13868244d1d31f146889fb
Miško Hevery <misko@hevery.com>
2019-10-10 22:40:34 +0000

    b0834fe96 refactor(compiler): replace instanceof Array (#33076)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+28.sha-6ab5f36.with-local-changes
+ * @license Angular v9.0.0-next.10+29.sha-b0834fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2624,7 +2624,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
         if (typeof token === 'string') {
             return token;
         }
-        if (token instanceof Array) {
+        if (Array.isArray(token)) {
             return '[' + token.map(stringify).join(', ') + ']';
         }
         if (token == null) {
@@ -18943,7 +18943,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+28.sha-6ab5f36.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+29.sha-b0834fe.with-local-changes');
 
     /**
      * @license
@@ -26539,7 +26539,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
                     if (isPrimitive(expression)) {
                         return expression;
                     }
-                    if (expression instanceof Array) {
+                    if (Array.isArray(expression)) {
                         var result_2 = [];
                         try {
                             for (var _c = __values(expression), _d = _c.next(); !_d.done; _d = _c.next()) {
@@ -34247,7 +34247,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.10+28.sha-6ab5f36.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.10+29.sha-b0834fe.with-local-changes');
 
     /**
      * @license
@@ -61005,7 +61005,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.10+28.sha-6ab5f36.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.10+29.sha-b0834fe.with-local-changes');
 
     /**
      * @license
@@ -71611,7 +71611,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.10+28.sha-6ab5f36.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.10+29.sha-b0834fe.with-local-changes');
 
     /**
      * @license
