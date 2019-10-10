f10143d025447da719f3bb91f94f077239d827d1
Miško Hevery <misko@hevery.com>
2019-10-10 21:23:08 +0000

    a3ef3e11c docs: changed 'function' word in to 'steps' (#32211)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+16.sha-8d111da.with-local-changes
+ * @license Angular v9.0.0-next.10+2.sha-a3ef3e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18943,7 +18943,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+16.sha-8d111da.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+2.sha-a3ef3e1.with-local-changes');
 
     /**
      * @license
@@ -34246,7 +34246,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.10+16.sha-8d111da.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.10+2.sha-a3ef3e1.with-local-changes');
 
     /**
      * @license
@@ -52722,7 +52722,9 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
         return NodeInjectorFactory;
     }());
     function isFactory(obj) {
-        return obj instanceof NodeInjectorFactory;
+        // See: https://jsperf.com/instanceof-vs-getprototypeof
+        return obj !== null && typeof obj == 'object' &&
+            Object.getPrototypeOf(obj) == NodeInjectorFactory.prototype;
     }
 
     /**
@@ -60984,7 +60986,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.10+16.sha-8d111da.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.10+2.sha-a3ef3e1.with-local-changes');
 
     /**
      * @license
@@ -67861,7 +67863,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = this._compiler instanceof Compiler;
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
             return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
@@ -71590,7 +71592,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.10+16.sha-8d111da.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.10+2.sha-a3ef3e1.with-local-changes');
 
     /**
      * @license
