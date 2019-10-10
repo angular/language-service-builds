54d62c714c2a4fddac80dd3eb8fc6debd3a94678
Miško Hevery <misko@hevery.com>
2019-10-10 21:33:31 +0000

    f640a4a49 fix(ivy): i18n - turn on legacy message-id support by default (#33053)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+21.sha-728cd84.with-local-changes
+ * @license Angular v9.0.0-next.10+20.sha-f640a4a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18943,7 +18943,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+21.sha-728cd84.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+20.sha-f640a4a.with-local-changes');
 
     /**
      * @license
@@ -34246,7 +34246,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.10+21.sha-728cd84.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.10+20.sha-f640a4a.with-local-changes');
 
     /**
      * @license
@@ -61005,7 +61005,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.10+21.sha-728cd84.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.10+20.sha-f640a4a.with-local-changes');
 
     /**
      * @license
@@ -71611,7 +71611,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.10+21.sha-728cd84.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.10+20.sha-f640a4a.with-local-changes');
 
     /**
      * @license
