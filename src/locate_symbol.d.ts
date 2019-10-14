89489c3ebb899f0d0ccae81476edeb96bfb0d43a
Miško Hevery <misko@hevery.com>
2019-10-14 21:04:46 +0000

    f433d6604 feat(ivy): i18n - support source locale inlining (#33101)

--- src/locate_symbol.d.ts
+++ src/locate_symbol.d.ts
@@ -6,11 +6,13 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/language-service/src/locate_symbol" />
+import { CompileTypeSummary } from '@angular/compiler';
 import { AstResult } from './common';
 import { Span, Symbol } from './types';
 export interface SymbolInfo {
     symbol: Symbol;
     span: Span;
+    compileTypeSummary: CompileTypeSummary | undefined;
 }
 /**
  * Traverse the template AST and locate the Symbol at the specified `position`.
