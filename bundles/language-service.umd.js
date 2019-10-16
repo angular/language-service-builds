d28058cbd9fa343f36455b17f18a0b49bd09f323
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 21:09:47 +0000

    becd62d4a fix(upgrade): remove unused version export (#33180)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+10.sha-86104b8.with-local-changes
+ * @license Angular v9.0.0-next.11+11.sha-becd62d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18966,7 +18966,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+10.sha-86104b8.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+11.sha-becd62d.with-local-changes');
 
     /**
      * @license
@@ -34324,7 +34324,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.11+10.sha-86104b8.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.11+11.sha-becd62d.with-local-changes');
 
     /**
      * @license
@@ -69445,7 +69445,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.11+10.sha-86104b8.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.11+11.sha-becd62d.with-local-changes');
 
     /**
      * @license
@@ -74738,7 +74738,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
     };
     function getPromiseCtor(promiseCtor) {
         if (!promiseCtor) {
-            promiseCtor = config.Promise || Promise;
+            promiseCtor = Promise;
         }
         if (!promiseCtor) {
             throw new Error('no Promise impl found');
@@ -83056,7 +83056,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.11+10.sha-86104b8.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.11+11.sha-becd62d.with-local-changes');
 
     /**
      * @license
