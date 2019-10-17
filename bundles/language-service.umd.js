87db7c966354150129369aea487737e8dc328746
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:43:11 +0000

    6f203c957 fix(ivy): handling className as an input properly (#33188)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+36.sha-08cb2fa.with-local-changes
+ * @license Angular v9.0.0-next.11+37.sha-6f203c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14722,6 +14722,9 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
             var _a = parseProperty(name), property = _a.property, hasOverrideFlag = _a.hasOverrideFlag;
             var entry = { name: property, value: value, sourceSpan: sourceSpan, hasOverrideFlag: hasOverrideFlag, unit: null };
             if (isMapBased) {
+                if (this._classMapInput) {
+                    throw new Error('[class] and [className] bindings cannot be used on the same element simultaneously');
+                }
                 this._classMapInput = entry;
             }
             else {
@@ -18981,7 +18984,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+37.sha-6f203c9.with-local-changes');
 
     /**
      * @license
@@ -34339,7 +34342,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.11+37.sha-6f203c9.with-local-changes');
 
     /**
      * @license
@@ -61299,7 +61302,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.11+37.sha-6f203c9.with-local-changes');
 
     /**
      * @license
@@ -71905,7 +71908,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.11+37.sha-6f203c9.with-local-changes');
 
     /**
      * @license
