7e1194f4bbf21b1ac3c9979c306ae7ee207a70e2
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:42:47 +0000

    08cb2fa80 fix(ivy): ignore non-property bindings to inputs in template type checker (#33130)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+35.sha-6c3c030.with-local-changes
+ * @license Angular v9.0.0-next.11+36.sha-08cb2fa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18981,7 +18981,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+35.sha-6c3c030.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
 
     /**
      * @license
@@ -34339,7 +34339,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.11+35.sha-6c3c030.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
 
     /**
      * @license
@@ -46906,6 +46906,10 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
          * a matching binding.
          */
         function processAttribute(attr) {
+            // Skip non-property bindings.
+            if (attr instanceof BoundAttribute && attr.type !== 0 /* Property */) {
+                return;
+            }
             // Skip the attribute if the directive does not have an input for it.
             if (!propMatch.has(attr.name)) {
                 return;
@@ -61295,7 +61299,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.11+35.sha-6c3c030.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
 
     /**
      * @license
@@ -71901,7 +71905,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.11+35.sha-6c3c030.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
 
     /**
      * @license
