f884d87dec1f6fccd305512387e8d5782037179b
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 00:01:50 +0000

    5dfbcd563 fix(core): add CLI instructions when localize polyfill is missing (#33199)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+53.sha-de44570.with-local-changes
+ * @license Angular v9.0.0-next.11+54.sha-5dfbcd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18986,7 +18986,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+53.sha-de44570.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
 
     /**
      * @license
@@ -34344,7 +34344,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.11+53.sha-de44570.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
 
     /**
      * @license
@@ -61343,7 +61343,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.11+53.sha-de44570.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
 
     /**
      * @license
@@ -71064,7 +71064,8 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
         _global$1.$localize = _global$1.$localize || function () {
             throw new Error('It looks like your application or one of its dependencies is using i18n.\n' +
                 'Angular 9 introduced a global `$localize()` function that needs to be loaded.\n' +
-                'Please add `import \'@angular/localize/init\';` to your polyfills.ts file.');
+                'Please run `ng add @angular/localize` from the Angular CLI.\n' +
+                '(For non-CLI projects, add `import \'@angular/localize/init\';` to your polyfills.ts file)');
         };
     }
 
@@ -71952,7 +71953,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.11+53.sha-de44570.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
 
     /**
      * @license
