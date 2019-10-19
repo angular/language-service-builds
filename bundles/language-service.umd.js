ec8883dc431d9fa26e53a2678594519303757629
Matias Niemelä <matias@yearofmoo.com>
2019-10-19 00:05:01 +0000

    29bc3a775f build: fix internal Google closure issue with TS3.6 (#33257)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+4.sha-f289411.with-local-changes
+ * @license Angular v9.0.0-next.12+6.sha-29bc3a7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18971,7 +18971,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.12+4.sha-f289411.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.12+6.sha-29bc3a7.with-local-changes');
 
     /**
      * @license
@@ -34356,7 +34356,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.12+4.sha-f289411.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.12+6.sha-29bc3a7.with-local-changes');
 
     /**
      * @license
@@ -62562,7 +62562,13 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
                 ViewContainerRef_.prototype.createComponent = function (componentFactory, index, injector, projectableNodes, ngModuleRef) {
                     var contextInjector = injector || this.parentInjector;
                     if (!ngModuleRef && componentFactory.ngModule == null && contextInjector) {
-                        ngModuleRef = contextInjector.get(NgModuleRef, null) || undefined;
+                        // DO NOT REFACTOR. The code here used to have a `value || undefined` expression
+                        // which seems to cause internal google apps to fail. This is documented in the
+                        // following internal bug issue: go/b/142967802
+                        var result = contextInjector.get(NgModuleRef, null);
+                        if (result) {
+                            ngModuleRef = result;
+                        }
                     }
                     var componentRef = componentFactory.create(contextInjector, projectableNodes, undefined, ngModuleRef);
                     this.insert(componentRef.hostView, index);
@@ -69979,7 +69985,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.12+4.sha-f289411.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.12+6.sha-29bc3a7.with-local-changes');
 
     /**
      * @license
@@ -83536,7 +83542,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.12+4.sha-f289411.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.12+6.sha-29bc3a7.with-local-changes');
 
     /**
      * @license
