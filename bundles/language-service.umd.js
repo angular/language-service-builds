94c6400139d186b734fb1e0c934393a448807860
Kara Erickson <karakara@google.com>
2019-10-22 21:53:36 +0000

    398ff1e7e7 docs: add ModuleWithProviders deprecation (#33266)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+50.sha-b3d6d50.with-local-changes
+ * @license Angular v9.0.0-next.12+51.sha-398ff1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19042,7 +19042,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.12+50.sha-b3d6d50.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.12+51.sha-398ff1e.with-local-changes');
 
     /**
      * @license
@@ -34437,7 +34437,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-next.12+50.sha-b3d6d50.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-next.12+51.sha-398ff1e.with-local-changes');
 
     /**
      * @license
@@ -70264,7 +70264,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-next.12+50.sha-b3d6d50.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-next.12+51.sha-398ff1e.with-local-changes');
 
     /**
      * @license
@@ -83822,7 +83822,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-next.12+50.sha-b3d6d50.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-next.12+51.sha-398ff1e.with-local-changes');
 
     /**
      * @license
