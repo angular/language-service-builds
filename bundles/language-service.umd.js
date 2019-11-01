ec496a1075f0c9998307b6858c06eb33a7b7e678
atscott <atscott01@gmail.com>
2019-11-01 18:13:31 +0000

    d77453d03f test: add cli-hello-world-ivy-i18n integration test (#33510)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+19.sha-88e8023.with-local-changes
+ * @license Angular v9.0.0-rc.0+29.sha-d77453d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19009,7 +19009,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.0+19.sha-88e8023.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.0+29.sha-d77453d.with-local-changes');
 
     /**
      * @license
@@ -33620,7 +33620,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-rc.0+19.sha-88e8023.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-rc.0+29.sha-d77453d.with-local-changes');
 
     /**
      * @license
@@ -50565,14 +50565,6 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-
-    /**
-     * @license
-     * Copyright Google Inc. All Rights Reserved.
-     *
-     * Use of this source code is governed by an MIT-style license that can be
-     * found in the LICENSE file at https://angular.io/license
-     */
     /**
      * Extension of the TypeScript compiler host that supports files added to the Program which
      * were never on disk.
@@ -51959,26 +51951,23 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * @param ast contains HTML and template AST
      */
     function getTemplateDiagnostics(ast) {
-        var results = [];
         var parseErrors = ast.parseErrors, templateAst = ast.templateAst, htmlAst = ast.htmlAst, template = ast.template;
-        if (parseErrors) {
-            results.push.apply(results, __spread(parseErrors.map(function (e) {
+        if (parseErrors && parseErrors.length) {
+            return parseErrors.map(function (e) {
                 return {
                     kind: DiagnosticKind$1.Error,
                     span: offsetSpan$1(spanOf$2(e.span), template.span.start),
                     message: e.msg,
                 };
-            })));
+            });
         }
-        var expressionDiagnostics = getTemplateExpressionDiagnostics({
+        return getTemplateExpressionDiagnostics({
             templateAst: templateAst,
             htmlAst: htmlAst,
             offset: template.span.start,
             query: template.query,
             members: template.members,
         });
-        results.push.apply(results, __spread(expressionDiagnostics));
-        return results;
     }
     /**
      * Generate an error message that indicates a directive is not part of any
@@ -62541,7 +62530,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.0+19.sha-88e8023.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.0+29.sha-d77453d.with-local-changes');
 
     /**
      * @license
@@ -73152,7 +73141,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-rc.0+19.sha-88e8023.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-rc.0+29.sha-d77453d.with-local-changes');
 
     /**
      * @license
