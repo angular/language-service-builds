5a209231add3af6e0b93445791d62d47ade2d770
atscott <atscott01@gmail.com>
2019-11-05 01:28:29 +0000

    25aaff2ee1 docs: Adding Doug Parker to contributors list. (#33516)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes
+ * @license Angular v9.0.0-rc.0+42.sha-25aaff2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18998,7 +18998,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.0+42.sha-25aaff2.with-local-changes');
 
     /**
      * @license
@@ -33609,7 +33609,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-rc.0+42.sha-25aaff2.with-local-changes');
 
     /**
      * @license
@@ -70906,7 +70906,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.0+42.sha-25aaff2.with-local-changes');
 
     /**
      * @license
@@ -84444,7 +84444,7 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-rc.0+42.sha-25aaff2.with-local-changes');
 
     /**
      * @license
