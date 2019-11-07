fb8d84c31bb381b0e52caf7a108c458ecae74d81
Andrew Scott <atscott01@gmail.com>
2019-11-07 22:36:14 +0000

    c77faf566e fix(common): update CLDR generated files to 36.0.0 (#33584)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+18.sha-71b400e.with-local-changes
+ * @license Angular v9.0.0-rc.1+20.sha-c77faf5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19032,7 +19032,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
     /**
      * @license
@@ -33540,7 +33540,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
     /**
      * @license
@@ -61982,7 +61982,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
     /**
      * @license
@@ -72260,7 +72260,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$4;
