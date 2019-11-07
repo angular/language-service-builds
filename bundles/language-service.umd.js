be4e04f88c5e34e601f5fad27b900abfb0136b37
Andrew Scott <atscott01@gmail.com>
2019-11-07 02:15:09 +0000

    39550746f8 fix(ivy): better support for i18n attributes on <ng-container>s (#33599)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+1.sha-c0f69f3.with-local-changes
+ * @license Angular v9.0.0-rc.1+3.sha-3955074.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19015,7 +19015,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+1.sha-c0f69f3.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+3.sha-3955074.with-local-changes');
 
     /**
      * @license
@@ -33535,7 +33535,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-rc.1+1.sha-c0f69f3.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-rc.1+3.sha-3955074.with-local-changes');
 
     /**
      * @license
@@ -70645,7 +70645,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+1.sha-c0f69f3.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+3.sha-3955074.with-local-changes');
 
     /**
      * @license
@@ -74501,9 +74501,13 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
                         }
                     }
                     else {
-                        elementAttributeInternal(previousElementIndex, attrName, value, lView);
-                        // Check if that attribute is a directive input
                         var tNode = getTNode(previousElementIndex, lView);
+                        // Set attributes for Elements only, for other types (like ElementContainer),
+                        // only set inputs below
+                        if (tNode.type === 3 /* Element */) {
+                            elementAttributeInternal(previousElementIndex, attrName, value, lView);
+                        }
+                        // Check if that attribute is a directive input
                         var dataValue = tNode.inputs && tNode.inputs[attrName];
                         if (dataValue) {
                             setInputsForProperty(lView, dataValue, value);
@@ -83924,7 +83928,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-rc.1+1.sha-c0f69f3.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-rc.1+3.sha-3955074.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$4;
