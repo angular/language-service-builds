0c70a7eec2473f18d92714a637a9449eade19c37
Kara Erickson <karakara@google.com>
2019-11-13 21:28:21 +0000

    5ad1e5f5ec release: cut the v9.0.0-rc.2 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+87.sha-b4d7a68.with-local-changes
+ * @license Angular v9.0.0-rc.2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19032,7 +19032,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+87.sha-b4d7a68.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.2.with-local-changes');
 
     /**
      * @license
@@ -33579,7 +33579,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-rc.1+87.sha-b4d7a68.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-rc.2.with-local-changes');
 
     /**
      * @license
@@ -62156,7 +62156,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+87.sha-b4d7a68.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.2.with-local-changes');
 
     /**
      * @license
@@ -72434,7 +72434,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-rc.1+87.sha-b4d7a68.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-rc.2.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$4;
