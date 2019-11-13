a72924e11c5995ba35903ef65627553177ace068
Kara Erickson <karakara@google.com>
2019-11-13 00:27:44 +0000

    9485e16e3b docs: dix typo in `DEVELOPER.md` (#33743)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+77.sha-84a0105.with-local-changes
+ * @license Angular v9.0.0-rc.1+78.sha-9485e16.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19060,7 +19060,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+77.sha-84a0105.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+78.sha-9485e16.with-local-changes');
 
     /**
      * @license
@@ -33607,7 +33607,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$2 = new Version('9.0.0-rc.1+77.sha-84a0105.with-local-changes');
+    var VERSION$2 = new Version('9.0.0-rc.1+78.sha-9485e16.with-local-changes');
 
     /**
      * @license
@@ -62164,7 +62164,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
     /**
      * @publicApi
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+77.sha-84a0105.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+78.sha-9485e16.with-local-changes');
 
     /**
      * @license
@@ -72442,7 +72442,7 @@ define(['exports', 'path', 'typescript', 'os', 'fs', 'typescript/lib/tsserverlib
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$4 = new Version$1('9.0.0-rc.1+77.sha-84a0105.with-local-changes');
+    var VERSION$4 = new Version$1('9.0.0-rc.1+78.sha-9485e16.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$4;
