659ad05919fc5e0566237dab6bc7bc3940b37fc5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 19:14:04 +0000

    eca85d5880 feat(bazel): update ng-add to use bazel v1.1.0 (#33813)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+106.sha-e25b256.with-local-changes
+ * @license Angular v9.0.0-rc.1+108.sha-eca85d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18534,7 +18534,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+106.sha-e25b256.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+108.sha-eca85d5.with-local-changes');
 
     /**
      * @license
@@ -38813,7 +38813,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+106.sha-e25b256.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+108.sha-eca85d5.with-local-changes');
 
     /**
      * @license
@@ -50847,7 +50847,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+106.sha-e25b256.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+108.sha-eca85d5.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
