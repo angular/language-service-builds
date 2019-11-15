ddc2736212ba9de3ba45558b704d461895bd51b4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 00:25:14 +0000

    e51ec671a5 fix(ivy): extend assertion in `directiveInject` function to support IcuContainers (#33832)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+109.sha-96c9ccc.with-local-changes
+ * @license Angular v9.0.0-rc.1+110.sha-e51ec67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18534,7 +18534,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+109.sha-96c9ccc.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+110.sha-e51ec67.with-local-changes');
 
     /**
      * @license
@@ -38805,7 +38805,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+109.sha-96c9ccc.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+110.sha-e51ec67.with-local-changes');
 
     /**
      * @license
@@ -50839,7 +50839,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+109.sha-96c9ccc.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+110.sha-e51ec67.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
