cc3c4c6704ffab28c09d8a33803d2d05a9a4a3ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-18 17:35:40 +0000

    1218ce4f5f docs: add xrefs to update app and deprecations policy (#33767)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+45.sha-7567f2b.with-local-changes
+ * @license Angular v9.0.0-rc.2+61.sha-1218ce4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18517,7 +18517,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.2+45.sha-7567f2b.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.2+61.sha-1218ce4.with-local-changes');
 
     /**
      * @license
@@ -38737,7 +38737,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.2+45.sha-7567f2b.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.2+61.sha-1218ce4.with-local-changes');
 
     /**
      * @license
@@ -50809,7 +50809,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.2+45.sha-7567f2b.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.2+61.sha-1218ce4.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
