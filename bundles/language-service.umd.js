b7c7ef4fe99d10cf1e90038cd1e43bf1b78f6b3e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 18:51:08 +0000

    a959fae66e build: add ngJitMode to ng_rollup_bundle terser config (#33865)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+180.sha-bb290ce.with-local-changes
+ * @license Angular v9.0.0-rc.1+181.sha-a959fae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18576,7 +18576,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+180.sha-bb290ce.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+181.sha-a959fae.with-local-changes');
 
     /**
      * @license
@@ -47598,7 +47598,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+180.sha-bb290ce.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+181.sha-a959fae.with-local-changes');
 
     /**
      * @license
@@ -62678,7 +62678,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+180.sha-bb290ce.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+181.sha-a959fae.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
