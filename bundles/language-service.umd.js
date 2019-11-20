1515502cf24416d0b5ec4973ffc7fefc1ad5ddb3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 20:16:10 +0000

    595375f7a3 fix(ivy): don't infer template context types when in full mode (#33537)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+92.sha-a7e2327.with-local-changes
+ * @license Angular v9.0.0-rc.2+96.sha-595375f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18535,7 +18535,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.2+92.sha-a7e2327.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.2+96.sha-595375f.with-local-changes');
 
     /**
      * @license
@@ -38755,7 +38755,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.2+92.sha-a7e2327.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.2+96.sha-595375f.with-local-changes');
 
     /**
      * @license
@@ -50830,7 +50830,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.2+92.sha-a7e2327.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.2+96.sha-595375f.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
