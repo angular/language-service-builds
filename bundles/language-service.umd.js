6a3be93d06458d6e21264860c81592acf45cfff2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 20:02:10 +0000

    250e6fdd39 fix(service-worker): throw when using the unsupported `versionedFiles` option in config (#33903)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+20.sha-c6695fa.with-local-changes
+ * @license Angular v9.0.0-rc.3+22.sha-250e6fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18556,7 +18556,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.3+20.sha-c6695fa.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.3+22.sha-250e6fd.with-local-changes');
 
     /**
      * @license
@@ -38776,7 +38776,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.3+20.sha-c6695fa.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.3+22.sha-250e6fd.with-local-changes');
 
     /**
      * @license
@@ -50851,7 +50851,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.3+20.sha-c6695fa.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.3+22.sha-250e6fd.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
