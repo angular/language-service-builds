2c91fce0b7c32a93c9adbd634cf767f2214a3775
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 19:25:26 +0000

    99e68d0318 ci: use more descriptive names for published build artifacts (#33957)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+35.sha-ad9b9a3.with-local-changes
+ * @license Angular v9.0.0-rc.3+42.sha-99e68d0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18556,7 +18556,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.3+35.sha-ad9b9a3.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.3+42.sha-99e68d0.with-local-changes');
 
     /**
      * @license
@@ -38776,7 +38776,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.3+35.sha-ad9b9a3.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.3+42.sha-99e68d0.with-local-changes');
 
     /**
      * @license
@@ -50851,7 +50851,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.3+35.sha-ad9b9a3.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.3+42.sha-99e68d0.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
