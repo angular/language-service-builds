86d6fa248234537a702e2bea6512b52a6af0fe83
Miško Hevery <misko@hevery.com>
2019-11-27 00:39:57 +0000

    005c4a1cc5 build: set default ts_devserver serving_path to /app_bundle.js to match g3 (#33996)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+288.sha-005c4a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18612,7 +18612,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+288.sha-005c4a1.with-local-changes');
 
     /**
      * @license
@@ -38885,7 +38885,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+288.sha-005c4a1.with-local-changes');
 
     /**
      * @license
@@ -50868,7 +50868,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+288.sha-005c4a1.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
