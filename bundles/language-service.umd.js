451b1e4ffd7e830469cd92a483ff468bedee39a9
Miško Hevery <misko@hevery.com>
2019-11-27 00:36:27 +0000

    49804fe017 fix(language-service): determine index types accessed using dot notation (#33884)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+286.sha-3f68377.with-local-changes
+ * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18597,7 +18597,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+286.sha-3f68377.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
 
     /**
      * @license
@@ -47670,7 +47670,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+286.sha-3f68377.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
 
     /**
      * @license
@@ -62669,7 +62669,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+286.sha-3f68377.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
