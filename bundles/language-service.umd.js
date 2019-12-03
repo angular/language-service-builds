d1d885593878e16139b50239d93ff4c75fe5db81
Miško Hevery <misko@hevery.com>
2019-12-03 18:54:38 +0000

    6b1a47183d fix(ivy): i18n - trim whitespace when parsing metadata (#34154)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+348.sha-539d8f0.with-local-changes
+ * @license Angular v9.0.0-rc.1+347.sha-6b1a471.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18603,7 +18603,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+348.sha-539d8f0.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+347.sha-6b1a471.with-local-changes');
 
     /**
      * @license
@@ -38883,7 +38883,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+348.sha-539d8f0.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+347.sha-6b1a471.with-local-changes');
 
     /**
      * @license
@@ -50844,7 +50844,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+348.sha-539d8f0.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+347.sha-6b1a471.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
