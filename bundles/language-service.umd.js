0ef259de1884addb867c7c785affa2ca478cd42e
Miško Hevery <misko@hevery.com>
2019-12-04 00:47:34 +0000

    e315215983 docs: add kwinten pisman to GDE resources (#34184)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+360.sha-fcbc38c.with-local-changes
+ * @license Angular v9.0.0-rc.1+362.sha-e315215.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18588,7 +18588,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+360.sha-fcbc38c.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+362.sha-e315215.with-local-changes');
 
     /**
      * @license
@@ -30713,7 +30713,6 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      *                     the directive/component instance lives
      */
     function setActiveHostElement(elementIndex) {
-        if (elementIndex === void 0) { elementIndex = null; }
         if (hasActiveElementFlag(1 /* RunExitFn */)) {
             executeElementExitFn();
         }
@@ -30739,7 +30738,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      */
     function setElementExitFn(fn) {
         setActiveElementFlag(1 /* RunExitFn */);
-        if (instructionState.elementExitFn == null) {
+        if (instructionState.elementExitFn === null) {
             instructionState.elementExitFn = fn;
         }
         ngDevMode &&
@@ -30974,9 +30973,9 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
         instructionState.lFrame = instructionState.lFrame.parent;
     }
     function nextContextImpl(level) {
-        if (level === void 0) { level = 1; }
-        instructionState.lFrame.contextLView = walkUpViews(level, instructionState.lFrame.contextLView);
-        return instructionState.lFrame.contextLView[CONTEXT];
+        var contextLView = instructionState.lFrame.contextLView =
+            walkUpViews(level, instructionState.lFrame.contextLView);
+        return contextLView[CONTEXT];
     }
     function walkUpViews(nestingLevel, currentView) {
         while (nestingLevel > 0) {
@@ -47678,7 +47677,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+360.sha-fcbc38c.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+362.sha-e315215.with-local-changes');
 
     /**
      * @license
@@ -62674,7 +62673,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+360.sha-fcbc38c.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+362.sha-e315215.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
