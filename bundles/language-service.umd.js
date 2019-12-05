75533a86727ddfe5f1a1ec26094c150bae420726
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:26:18 +0000

    0341f3239a build: set upper version limit for yarn to <2 (#34236)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+384.sha-421dba0.with-local-changes
+ * @license Angular v9.0.0-rc.1+385.sha-0341f32.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18607,7 +18607,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+384.sha-421dba0.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+385.sha-0341f32.with-local-changes');
 
     /**
      * @license
@@ -47696,7 +47696,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+384.sha-421dba0.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+385.sha-0341f32.with-local-changes');
 
     /**
      * @license
@@ -62693,7 +62693,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+384.sha-421dba0.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+385.sha-0341f32.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
