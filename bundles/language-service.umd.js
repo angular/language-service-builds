04a2bde29b7d50435519bd115d09b7255e1e74e8
Andrew Kushnir <akushnir@google.com>
2019-12-06 19:22:55 +0000

    6e944acaae build: remove compile build variable (#34280)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+25.sha-7dfd327.with-local-changes
+ * @license Angular v9.0.0-rc.5+28.sha-6e944ac.with-local-changes
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18571,7 +18571,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.5+25.sha-7dfd327.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.5+28.sha-6e944ac.with-local-changes');
 
     /**
      * @license
@@ -38850,7 +38850,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.5+25.sha-7dfd327.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.5+28.sha-6e944ac.with-local-changes');
 
     /**
      * @license
@@ -50765,7 +50765,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.5+25.sha-7dfd327.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.5+28.sha-6e944ac.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
