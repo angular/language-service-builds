3db918d4ce622fbe4e444d421a6717ef99dcf28b
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:42:51 +0000

    8683c0335b docs: Fix links to Webpack docs (#34326)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+414.sha-419b153.with-local-changes
+ * @license Angular v9.0.0-rc.1+415.sha-8683c03.with-local-changes
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18668,7 +18668,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+414.sha-419b153.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+415.sha-8683c03.with-local-changes');
 
     /**
      * @license
@@ -38950,7 +38950,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+414.sha-419b153.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+415.sha-8683c03.with-local-changes');
 
     /**
      * @license
@@ -50889,7 +50889,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+414.sha-419b153.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+415.sha-8683c03.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
