fa8aafd313951e4cacadeeea55797821fea3038f
Andrew Kushnir <akushnir@google.com>
2019-12-11 00:58:49 +0000

    b72c7a89a9 refactor(ivy): include generic type for `ModuleWithProviders` in .d.ts files (#34235)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+423.sha-0984fbc.with-local-changes
+ * @license Angular v9.0.0-rc.1+425.sha-b72c7a8.with-local-changes
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -3638,6 +3638,10 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
         };
         Identifiers.NgModuleDefWithMeta = {
             name: 'ɵɵNgModuleDefWithMeta',
+            moduleName: CORE$1,
+        };
+        Identifiers.ModuleWithProviders = {
+            name: 'ModuleWithProviders',
             moduleName: CORE$1,
         };
         Identifiers.defineNgModule = { name: 'ɵɵdefineNgModule', moduleName: CORE$1 };
@@ -18668,7 +18672,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+423.sha-0984fbc.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
 
     /**
      * @license
@@ -38954,7 +38958,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+423.sha-0984fbc.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
 
     /**
      * @license
@@ -50896,7 +50900,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+423.sha-0984fbc.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
