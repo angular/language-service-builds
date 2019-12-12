05a0267b54e328b0d2e1734f85929fadce3494ca
Kara Erickson <karakara@google.com>
2019-12-12 21:45:11 +0000

    6ba5fdc208 fix(ivy): generate a better error for template var writes (#34339)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+450.sha-6e7ca05.with-local-changes
+ * @license Angular v9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18657,7 +18657,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+450.sha-6e7ca05.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes');
 
     /**
      * @license
@@ -47775,7 +47775,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+450.sha-6e7ca05.with-local-changes');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes');
 
     /**
      * @license
@@ -62752,7 +62752,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+450.sha-6e7ca05.with-local-changes');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
