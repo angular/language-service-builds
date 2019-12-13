1fcb3a35c57f4f76189d6151990d0680366d3cdb
Kara Erickson <karakara@google.com>
2019-12-13 22:02:57 +0000

    17f7f06ca5 Revert "build: update to yarn@1.21.1" (#34402)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+463.sha-ea57587
+ * @license Angular v9.0.0-rc.1+464.sha-17f7f06.with-local-changes
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18706,7 +18706,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+463.sha-ea57587');
+    var VERSION$1 = new Version('9.0.0-rc.1+464.sha-17f7f06.with-local-changes');
 
     /**
      * @license
@@ -38934,7 +38934,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+463.sha-ea57587');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+464.sha-17f7f06.with-local-changes');
 
     /**
      * @license
@@ -50855,7 +50855,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+463.sha-ea57587');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+464.sha-17f7f06.with-local-changes');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
