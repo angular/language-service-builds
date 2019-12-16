16a9e273d179a00a3395b1f7e344b397a1f8e425
Kara Erickson <karakara@google.com>
2019-12-16 16:06:11 +0000

    04ab03664d ci: ensure saucelabs test output is human readable (#34277)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+476.sha-3bbd12d
+ * @license Angular v9.0.0-rc.1+478.sha-04ab036
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18706,7 +18706,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+476.sha-3bbd12d');
+    var VERSION$1 = new Version('9.0.0-rc.1+478.sha-04ab036');
 
     /**
      * @license
@@ -38958,7 +38958,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+476.sha-3bbd12d');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+478.sha-04ab036');
 
     /**
      * @license
@@ -50882,7 +50882,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+476.sha-3bbd12d');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+478.sha-04ab036');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
