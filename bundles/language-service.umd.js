af31efebb7702cb95b1efbe9d6c2672f6e92bddb
Kara Erickson <karakara@google.com>
2019-12-17 23:00:05 +0000

    82dce68e13 refactor(compiler-cli): avoid unnecessarily calling `getSourceFile()` twice in `PartialEvaluator` (#34441)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+67.sha-deeb87d
+ * @license Angular v9.0.0-rc.6+68.sha-82dce68
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18693,7 +18693,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.6+67.sha-deeb87d');
+    var VERSION$1 = new Version('9.0.0-rc.6+68.sha-82dce68');
 
     /**
      * @license
@@ -38944,7 +38944,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.6+67.sha-deeb87d');
+    var VERSION$2 = new Version$1('9.0.0-rc.6+68.sha-82dce68');
 
     /**
      * @license
@@ -50867,7 +50867,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.6+67.sha-deeb87d');
+    var VERSION$3 = new Version$1('9.0.0-rc.6+68.sha-82dce68');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
