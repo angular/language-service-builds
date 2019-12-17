f2783e8b0d58f4ea4504df783d43e0f61cb1be7e
Kara Erickson <karakara@google.com>
2019-12-17 21:53:39 +0000

    b637b9322e ci(docs-infra): fix failure in `aio_monitoring_stable` due to yarn version mismatch (#34451)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+490.sha-5cecd97
+ * @license Angular v9.0.0-rc.1+496.sha-b637b93
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18693,7 +18693,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+490.sha-5cecd97');
+    var VERSION$1 = new Version('9.0.0-rc.1+496.sha-b637b93');
 
     /**
      * @license
@@ -38944,7 +38944,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+490.sha-5cecd97');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+496.sha-b637b93');
 
     /**
      * @license
@@ -50868,7 +50868,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+490.sha-5cecd97');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+496.sha-b637b93');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
