150613db36f90c108e6603050f5e21b9083dc2c2
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:16:30 +0000

    61c805fa1b build(docs-infra): bump handlebars from 4.1.2 to 4.5.3 in /aio

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-22ac0e9
+ * @license Angular v9.0.0-rc.1+522.sha-61c805f
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18693,7 +18693,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+522.sha-22ac0e9');
+    var VERSION$1 = new Version('9.0.0-rc.1+522.sha-61c805f');
 
     /**
      * @license
@@ -38941,7 +38941,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+522.sha-22ac0e9');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+522.sha-61c805f');
 
     /**
      * @license
@@ -50865,7 +50865,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+522.sha-22ac0e9');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+522.sha-61c805f');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
