e80136b1514e49d53b93aa3aa69e1192f9f64678
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:40:11 +0000

    e116816131 refactor(ivy): let `strictTemplates` imply `fullTemplateTypeCheck` (#34195)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+525.sha-1de49ba
+ * @license Angular v9.0.0-rc.1+527.sha-e116816
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18679,7 +18679,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+525.sha-1de49ba');
+    var VERSION$1 = new Version('9.0.0-rc.1+527.sha-e116816');
 
     /**
      * @license
@@ -47842,7 +47842,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+525.sha-1de49ba');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+527.sha-e116816');
 
     /**
      * @license
@@ -62799,7 +62799,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+525.sha-1de49ba');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+527.sha-e116816');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
