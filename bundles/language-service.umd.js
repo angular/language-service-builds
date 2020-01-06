fa2e8f21eb730dff4d7314ee2f2878803b9c8890
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:23:10 +0000

    8410278b6d docs: copy edit getting started step 4 (#32839)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+521.sha-9b9116c
+ * @license Angular v9.0.0-rc.1+522.sha-8410278
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18678,7 +18678,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+521.sha-9b9116c');
+    var VERSION$1 = new Version('9.0.0-rc.1+522.sha-8410278');
 
     /**
      * @license
@@ -47841,7 +47841,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+521.sha-9b9116c');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+522.sha-8410278');
 
     /**
      * @license
@@ -62798,7 +62798,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+521.sha-9b9116c');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+522.sha-8410278');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
