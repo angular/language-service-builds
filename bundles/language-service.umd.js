bca83c2ab37d678ff3c3abaef282f58e3f927e15
Misko Hevery <misko@hevery.com>
2020-01-11 00:00:53 +0000

    142363a95f refactor(ivy): delete `ɵɵallocHostVars` instruction (#34708)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+616.sha-65d36d6
+ * @license Angular v9.0.0-rc.1+636.sha-142363a
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18650,7 +18650,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('0.0.0');
+    var VERSION$1 = new Version('9.0.0-rc.1+636.sha-142363a');
 
     /**
      * @license
@@ -39304,7 +39304,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('0.0.0');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+636.sha-142363a');
 
     /**
      * @license
@@ -51228,7 +51228,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('0.0.0');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+636.sha-142363a');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
