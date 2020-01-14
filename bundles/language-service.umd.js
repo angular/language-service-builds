e5ab26e9036dda5a63c5103105cf05ee0edb55ed
atscott <atscott@google.com>
2020-01-14 23:49:58 +0000

    cb142b6df9 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18663,7 +18663,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+673.sha-c3c72f6');
+    var VERSION$1 = new Version('9.0.0-rc.1+674.sha-cb142b6');
 
     /**
      * @license
@@ -47849,7 +47849,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+673.sha-c3c72f6');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+674.sha-cb142b6');
 
     /**
      * @license
@@ -62938,7 +62938,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+673.sha-c3c72f6');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+674.sha-cb142b6');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
