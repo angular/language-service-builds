a6e0dc2c186754dab2e47fbe2c448b136b192f7e
GitHub <noreply@github.com>
2020-01-15 21:35:29 +0000

    1fd4d4f0d4 fixup! docs: fix public api tool name

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-966cb35
+ * @license Angular v9.0.0-rc.1+693.sha-1fd4d4f
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18678,7 +18678,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+692.sha-966cb35');
+    var VERSION$1 = new Version('9.0.0-rc.1+693.sha-1fd4d4f');
 
     /**
      * @license
@@ -38927,7 +38927,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+692.sha-966cb35');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+693.sha-1fd4d4f');
 
     /**
      * @license
@@ -50915,7 +50915,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+692.sha-966cb35');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+693.sha-1fd4d4f');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
