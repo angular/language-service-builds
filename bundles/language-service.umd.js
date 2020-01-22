90c1320d233ec6224a598aa93ae19022903498f8
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:17:39 +0000

    61ad50d40a docs: add message on click of hero in toh-4 (#34496)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
+ * @license Angular v9.0.0-rc.1+747.sha-61ad50d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18710,7 +18710,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+746.sha-e7cf37d');
+    var VERSION$1 = new Version('9.0.0-rc.1+747.sha-61ad50d');
 
     /**
      * @license
@@ -38941,7 +38941,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+746.sha-e7cf37d');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+747.sha-61ad50d');
 
     /**
      * @license
@@ -50944,7 +50944,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+746.sha-e7cf37d');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+747.sha-61ad50d');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
