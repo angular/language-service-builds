f7c6584ba50aa230cf67aff0e4fd97125d5e1c44
Andrew Kushnir <akushnir@google.com>
2020-01-22 21:57:17 +0000

    13ce3756c2 docs: add differential loading term to glossary (#34884)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+748.sha-32489c7
+ * @license Angular v9.0.0-rc.1+749.sha-13ce375
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18719,7 +18719,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+748.sha-32489c7');
+    var VERSION$1 = new Version('9.0.0-rc.1+749.sha-13ce375');
 
     /**
      * @license
@@ -38950,7 +38950,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+748.sha-32489c7');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+749.sha-13ce375');
 
     /**
      * @license
@@ -50953,7 +50953,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+748.sha-32489c7');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+749.sha-13ce375');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
