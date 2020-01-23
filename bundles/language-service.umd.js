d8162748d45f4b17d9a3a7a4b164a0ab5a8625e0
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:33:54 +0000

    68025ce09a test(language-service): recognize inputs/ouputs declared in decorator (#34875)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
+ * @license Angular v9.0.0-rc.1+764.sha-68025ce
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18719,7 +18719,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+763.sha-1ea04ff');
+    var VERSION$1 = new Version('9.0.0-rc.1+764.sha-68025ce');
 
     /**
      * @license
@@ -38991,7 +38991,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+763.sha-1ea04ff');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+764.sha-68025ce');
 
     /**
      * @license
@@ -51006,7 +51006,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+763.sha-1ea04ff');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+764.sha-68025ce');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
