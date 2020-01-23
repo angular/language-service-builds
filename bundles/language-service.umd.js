95a61393cfc87cdc246499b7f0cae741765ee884
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:45:22 +0000

    3049878aa9 ci: bump release version of ts-api-guardian (#34933)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+778.sha-055533b
+ * @license Angular v9.0.0-rc.1+779.sha-3049878
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18719,7 +18719,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+778.sha-055533b');
+    var VERSION$1 = new Version('9.0.0-rc.1+779.sha-3049878');
 
     /**
      * @license
@@ -38992,7 +38992,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+778.sha-055533b');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+779.sha-3049878');
 
     /**
      * @license
@@ -51007,7 +51007,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+778.sha-055533b');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+779.sha-3049878');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
