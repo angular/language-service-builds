13721659ce349ba56a648abb80ecc8eba3dca69d
Andrew Kushnir <akushnir@google.com>
2020-01-24 17:27:42 +0000

    24b2f1da2b refactor(ivy): introduce the 'core' package and split apart NgtscProgram (#34887)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+790.sha-31e9dda
+ * @license Angular v9.0.0-rc.1+791.sha-24b2f1d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18704,7 +18704,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+790.sha-31e9dda');
+    var VERSION$1 = new Version('9.0.0-rc.1+791.sha-24b2f1d');
 
     /**
      * @license
@@ -47912,7 +47912,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+790.sha-31e9dda');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+791.sha-24b2f1d');
 
     /**
      * @license
@@ -63023,7 +63023,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+790.sha-31e9dda');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+791.sha-24b2f1d');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
