061621d995839c3619e12c0dd66d8365bb049b62
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:28:18 +0000

    6e5cfd2cd2 fix(ivy): catch FatalDiagnosticError thrown from preanalysis phase (#34801)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+824.sha-549ab2e
+ * @license Angular v9.0.0-rc.1+827.sha-6e5cfd2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18740,7 +18740,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+824.sha-549ab2e');
+    var VERSION$1 = new Version('9.0.0-rc.1+827.sha-6e5cfd2');
 
     /**
      * @license
@@ -47022,7 +47022,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+824.sha-549ab2e');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+827.sha-6e5cfd2');
 
     /**
      * @license
@@ -62114,7 +62114,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+824.sha-549ab2e');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+827.sha-6e5cfd2');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
