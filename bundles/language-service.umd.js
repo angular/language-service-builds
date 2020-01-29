15d7dae6763573663f1e628de3662b479a5dcc7c
Andrew Kushnir <akushnir@google.com>
2020-01-29 23:36:46 +0000

    0390d20d60 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+58.sha-9b7d703
+ * @license Angular v9.0.0-rc.11+59.sha-0390d20
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.11+58.sha-9b7d703');
+    var VERSION$1 = new Version('9.0.0-rc.11+59.sha-0390d20');
 
     /**
      * @license
@@ -38435,7 +38435,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.11+58.sha-9b7d703');
+    var VERSION$2 = new Version$1('9.0.0-rc.11+59.sha-0390d20');
 
     /**
      * @license
@@ -50432,7 +50432,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.11+58.sha-9b7d703');
+    var VERSION$3 = new Version$1('9.0.0-rc.11+59.sha-0390d20');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
