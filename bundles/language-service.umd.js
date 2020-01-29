901eedc06881a2e4cdd120ff07fd6d9b5b6cbdd6
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:41:09 +0000

    53c4c8de8f test: ensure global options for benchmark tests can be set in bazel (#34753)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+844.sha-669df70
+ * @license Angular v9.0.0-rc.1+847.sha-53c4c8d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+844.sha-669df70');
+    var VERSION$1 = new Version('9.0.0-rc.1+847.sha-53c4c8d');
 
     /**
      * @license
@@ -38421,7 +38421,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+844.sha-669df70');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+847.sha-53c4c8d');
 
     /**
      * @license
@@ -50419,7 +50419,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+844.sha-669df70');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+847.sha-53c4c8d');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
