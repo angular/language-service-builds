3fdfa020636ff8252a5cc2f0e66b1d1f6d9c9116
Misko Hevery <misko@hevery.com>
2020-02-01 03:04:38 +0000

    ab12529941 release: cut the v9.0.0-rc.13 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.12+24.sha-58dbe66
+ * @license Angular v9.0.0-rc.13
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.12+24.sha-58dbe66');
+    var VERSION$1 = new Version('9.0.0-rc.13');
 
     /**
      * @license
@@ -38438,7 +38438,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.12+24.sha-58dbe66');
+    var VERSION$2 = new Version$1('9.0.0-rc.13');
 
     /**
      * @license
@@ -50435,7 +50435,7 @@ define(['exports', 'typescript', 'path', 'typescript/lib/tsserverlibrary'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.12+24.sha-58dbe66');
+    var VERSION$3 = new Version$1('9.0.0-rc.13');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
