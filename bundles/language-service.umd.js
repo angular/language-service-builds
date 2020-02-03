3a57b719857cd7fd61875c160bfb7b58062023d3
Miško Hevery <misko@hevery.com>
2020-02-03 22:24:09 +0000

    9601b5d18a refactor(ngcc): remove unused function (#35122)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+903.sha-ab931cf
+ * @license Angular v9.0.0-rc.1+904.sha-9601b5d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+903.sha-ab931cf');
+    var VERSION$1 = new Version('9.0.0-rc.1+904.sha-9601b5d');
 
     /**
      * @license
@@ -38403,7 +38403,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+903.sha-ab931cf');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+904.sha-9601b5d');
 
     /**
      * @license
@@ -50401,7 +50401,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+903.sha-ab931cf');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+904.sha-9601b5d');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
