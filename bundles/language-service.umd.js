9d819ecb40a340b24e5cd306b05b98ccde4f6890
Miško Hevery <misko@hevery.com>
2020-02-04 17:20:00 +0000

    7369dd6f24 docs: fix animations example/remove 1st person (#35046)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+907.sha-128d4ac
+ * @license Angular v9.0.0-rc.1+909.sha-7369dd6
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+907.sha-128d4ac');
+    var VERSION$1 = new Version('9.0.0-rc.1+909.sha-7369dd6');
 
     /**
      * @license
@@ -38403,7 +38403,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+907.sha-128d4ac');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+909.sha-7369dd6');
 
     /**
      * @license
@@ -50401,7 +50401,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+907.sha-128d4ac');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+909.sha-7369dd6');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
