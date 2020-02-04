5ebda55bc12f7f176a6854ae96dd4c223ec02143
Miško Hevery <misko@hevery.com>
2020-02-04 19:58:02 +0000

    7fb66eca25 docs: changes AoT to AOT for consistency (#35112)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+916.sha-6ddf550
+ * @license Angular v9.0.0-rc.1+919.sha-7fb66ec
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+916.sha-6ddf550');
+    var VERSION$1 = new Version('9.0.0-rc.1+919.sha-7fb66ec');
 
     /**
      * @license
@@ -38403,7 +38403,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+916.sha-6ddf550');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+919.sha-7fb66ec');
 
     /**
      * @license
@@ -50401,7 +50401,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+916.sha-6ddf550');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+919.sha-7fb66ec');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
