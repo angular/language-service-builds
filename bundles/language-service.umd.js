0f1056b67915bfa0f6287d19b600572450dbaaf4
Kara Erickson <karakara@google.com>
2020-02-06 22:51:25 +0000

    bcb539f6de docs: remove the release schedule from docs (#34474)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+7.sha-f4b9d66
+ * @license Angular v9.0.0+9.sha-bcb539f
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0+7.sha-f4b9d66');
+    var VERSION$1 = new Version('9.0.0+9.sha-bcb539f');
 
     /**
      * @license
@@ -38404,7 +38404,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0+7.sha-f4b9d66');
+    var VERSION$2 = new Version$1('9.0.0+9.sha-bcb539f');
 
     /**
      * @license
@@ -50401,7 +50401,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0+7.sha-f4b9d66');
+    var VERSION$3 = new Version$1('9.0.0+9.sha-bcb539f');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
