c2c34c72dab52e6314e85f64f1e87ebffcb9370e
Misko Hevery <misko@hevery.com>
2020-02-06 22:13:37 +0000

    86db592b5c docs(ivy): add a not about compile-time constants to the ivy compatibility guide (#35194)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+938.sha-053dabb
+ * @license Angular v9.0.0-rc.1+939.sha-86db592
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+938.sha-053dabb');
+    var VERSION$1 = new Version('9.0.0-rc.1+939.sha-86db592');
 
     /**
      * @license
@@ -38404,7 +38404,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+938.sha-053dabb');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+939.sha-86db592');
 
     /**
      * @license
@@ -50402,7 +50402,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+938.sha-053dabb');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+939.sha-86db592');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
