afd9ea23e86a80816fb044280ff445fe009443b2
Misko Hevery <misko@hevery.com>
2020-02-06 23:56:40 +0000

    3c69442dbd feat(compiler-cli): implement NgTscPlugin on top of the NgCompiler API (#34792)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+942.sha-49dd7b6
+ * @license Angular v9.0.0-rc.1+944.sha-3c69442
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18735,7 +18735,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+942.sha-49dd7b6');
+    var VERSION$1 = new Version('9.0.0-rc.1+944.sha-3c69442');
 
     /**
      * @license
@@ -47424,7 +47424,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+942.sha-49dd7b6');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+944.sha-3c69442');
 
     /**
      * @license
@@ -62515,7 +62515,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+942.sha-49dd7b6');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+944.sha-3c69442');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
