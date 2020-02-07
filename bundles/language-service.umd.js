11c3a0730b6d0352fecd2af315bf1a14a188928d
Kara Erickson <karakara@google.com>
2020-02-07 21:28:00 +0000

    5de5b52beb fix(ivy): repeat template guards to narrow types in event handlers (#35193)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+966.sha-daac33c
+ * @license Angular v9.0.0-rc.1+967.sha-5de5b52
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+966.sha-daac33c');
+    var VERSION$1 = new Version('9.0.0-rc.1+967.sha-5de5b52');
 
     /**
      * @license
@@ -38405,7 +38405,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+966.sha-daac33c');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+967.sha-5de5b52');
 
     /**
      * @license
@@ -50403,7 +50403,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+966.sha-daac33c');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+967.sha-5de5b52');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
