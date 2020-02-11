61a03404e05701a69ca832ce2543cbb1fa5af2a3
Kara Erickson <karakara@google.com>
2020-02-11 20:13:34 +0000

    7c9735a995 fix(elements): schematics fails with schema.json not found error (#35211)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+984.sha-332937e
+ * @license Angular v9.0.0-rc.1+985.sha-7c9735a
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18756,7 +18756,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+984.sha-332937e');
+    var VERSION$1 = new Version('9.0.0-rc.1+985.sha-7c9735a');
 
     /**
      * @license
@@ -47447,7 +47447,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.0-rc.1+984.sha-332937e');
+    var VERSION$2 = new Version$1('9.0.0-rc.1+985.sha-7c9735a');
 
     /**
      * @license
@@ -62542,7 +62542,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.0-rc.1+984.sha-332937e');
+    var VERSION$3 = new Version$1('9.0.0-rc.1+985.sha-7c9735a');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
