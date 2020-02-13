92a58fad34901dfb6c987ce6e1457add2cf4fcb2
Kara Erickson <karakara@google.com>
2020-02-13 01:37:08 +0000

    a30fd2993b fix(ivy): error if directive with synthetic property binding is on same node as directive that injects ViewContainerRef (#35343)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+6.sha-a84093a
+ * @license Angular v9.0.1+7.sha-a30fd29
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18771,7 +18771,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.1+6.sha-a84093a');
+    var VERSION$1 = new Version('9.0.1+7.sha-a30fd29');
 
     /**
      * @license
@@ -38427,7 +38427,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.1+6.sha-a84093a');
+    var VERSION$2 = new Version$1('9.0.1+7.sha-a30fd29');
 
     /**
      * @license
@@ -50428,7 +50428,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.1+6.sha-a84093a');
+    var VERSION$3 = new Version$1('9.0.1+7.sha-a30fd29');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
