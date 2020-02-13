530b36298a48529a7ec7d5b4e4800dbf93180008
Kara Erickson <karakara@google.com>
2020-02-13 18:35:53 +0000

    84c9c6ecc2 docs: minor typo fix (#35423) PR Close #35423

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+7.sha-a30fd29
+ * @license Angular v9.0.1+16.sha-84c9c6e
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18771,7 +18771,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.1+7.sha-a30fd29');
+    var VERSION$1 = new Version('9.0.1+16.sha-84c9c6e');
 
     /**
      * @license
@@ -38427,7 +38427,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.1+7.sha-a30fd29');
+    var VERSION$2 = new Version$1('9.0.1+16.sha-84c9c6e');
 
     /**
      * @license
@@ -50428,7 +50428,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.1+7.sha-a30fd29');
+    var VERSION$3 = new Version$1('9.0.1+16.sha-84c9c6e');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
