a669d8b83a17c73c237d0cc26f770cd6427c3b7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:42:36 +0000

    2ca7984d55 fix(core): remove support for `Map`/`Set` in `[class]`/`[style]` bindings (#35392)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+21.sha-b9b512f
+ * @license Angular v9.1.0-next.0+23.sha-2ca7984
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18771,7 +18771,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.0+21.sha-b9b512f');
+    var VERSION$1 = new Version('9.1.0-next.0+23.sha-2ca7984');
 
     /**
      * @license
@@ -38455,7 +38455,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.0+21.sha-b9b512f');
+    var VERSION$2 = new Version$1('9.1.0-next.0+23.sha-2ca7984');
 
     /**
      * @license
@@ -50457,7 +50457,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.0+21.sha-b9b512f');
+    var VERSION$3 = new Version$1('9.1.0-next.0+23.sha-2ca7984');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
