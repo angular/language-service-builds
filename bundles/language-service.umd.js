b14c8c4b3d4320c1c1d32001988f8a2697b4c3f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:10:16 +0000

    7b56daffe6 fix(docs-infra): add fallback to web fonts so downloading does not block rendering (#35352)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+29.sha-d7c4f40
+ * @license Angular v9.1.0-next.0+33.sha-7b56daf
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18771,7 +18771,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.0+29.sha-d7c4f40');
+    var VERSION$1 = new Version('9.1.0-next.0+33.sha-7b56daf');
 
     /**
      * @license
@@ -38455,7 +38455,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.0+29.sha-d7c4f40');
+    var VERSION$2 = new Version$1('9.1.0-next.0+33.sha-7b56daf');
 
     /**
      * @license
@@ -50457,7 +50457,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.0+29.sha-d7c4f40');
+    var VERSION$3 = new Version$1('9.1.0-next.0+33.sha-7b56daf');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
