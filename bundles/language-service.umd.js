ab2547502ccfcd49d9db5d1cdbe73eda61fd5af0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 21:24:41 +0000

    1997b86a00 fix(docs-infra): improve focus styles in topnav and footer (#33255)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+2.sha-a756161
+ * @license Angular v9.1.0-next.1+3.sha-1997b86
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18771,7 +18771,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+2.sha-a756161');
+    var VERSION$1 = new Version('9.1.0-next.1+3.sha-1997b86');
 
     /**
      * @license
@@ -38488,7 +38488,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.1+2.sha-a756161');
+    var VERSION$2 = new Version$1('9.1.0-next.1+3.sha-1997b86');
 
     /**
      * @license
@@ -50506,7 +50506,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.1+2.sha-a756161');
+    var VERSION$3 = new Version$1('9.1.0-next.1+3.sha-1997b86');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
