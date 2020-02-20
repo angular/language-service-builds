05dffe1d400580200eb2dcedc1ba60d65935afb5
Miško Hevery <misko@hevery.com>
2020-02-20 19:14:42 +0000

    1b72fc10eb docs: edit displaying-data page to introduce section (#35507)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+1.sha-8e12707
+ * @license Angular v9.1.0-next.1+5.sha-1b72fc1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18771,7 +18771,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+1.sha-8e12707');
+    var VERSION$1 = new Version('9.1.0-next.1+5.sha-1b72fc1');
 
     /**
      * @license
@@ -38488,7 +38488,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.1+1.sha-8e12707');
+    var VERSION$2 = new Version$1('9.1.0-next.1+5.sha-1b72fc1');
 
     /**
      * @license
@@ -50506,7 +50506,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.1+1.sha-8e12707');
+    var VERSION$3 = new Version$1('9.1.0-next.1+5.sha-1b72fc1');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
