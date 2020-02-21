d1b03df306d48f9e839c7038625910947715b7f8
Miško Hevery <misko@hevery.com>
2020-02-21 21:09:25 +0000

    4253662231 fix(ivy): add strictLiteralTypes to align Ivy + VE checking of literals (#35462)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+28.sha-049f118
+ * @license Angular v9.1.0-next.1+30.sha-4253662
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18813,7 +18813,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+28.sha-049f118');
+    var VERSION$1 = new Version('9.1.0-next.1+30.sha-4253662');
 
     /**
      * @license
@@ -47934,7 +47934,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.1+28.sha-049f118');
+    var VERSION$2 = new Version$1('9.1.0-next.1+30.sha-4253662');
 
     /**
      * @license
@@ -63086,7 +63086,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.1+28.sha-049f118');
+    var VERSION$3 = new Version$1('9.1.0-next.1+30.sha-4253662');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
