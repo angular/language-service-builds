5ff89f1651ef21aa639829ce6724f0c5fabf7d3f
Miško Hevery <misko@hevery.com>
2020-02-25 21:39:10 +0000

    8e354dae00 fix(language-service): get the right 'ElementAst' in the nested HTML tag (#35317)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+61.sha-1e20b2c
+ * @license Angular v9.1.0-next.1+59.sha-8e354da
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18828,7 +18828,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+61.sha-1e20b2c');
+    var VERSION$1 = new Version('9.1.0-next.1+59.sha-8e354da');
 
     /**
      * @license
@@ -38573,7 +38573,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.1+61.sha-1e20b2c');
+    var VERSION$2 = new Version$1('9.1.0-next.1+59.sha-8e354da');
 
     /**
      * @license
@@ -50591,7 +50591,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.1+61.sha-1e20b2c');
+    var VERSION$3 = new Version$1('9.1.0-next.1+59.sha-8e354da');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
