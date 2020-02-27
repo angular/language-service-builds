23c04ee662153ee2e5ae4e8e71c24109da3166d2
atscott <atscott@google.com>
2020-02-27 19:16:49 +0000

    20b0c80b0b fix(ngcc): allow deep-import warnings to be ignored (#35683)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+4.sha-30f29ee
+ * @license Angular v9.1.0-next.2+3.sha-20b0c80
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18813,7 +18813,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+4.sha-30f29ee');
+    var VERSION$1 = new Version('9.1.0-next.2+3.sha-20b0c80');
 
     /**
      * @license
@@ -47937,7 +47937,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+4.sha-30f29ee');
+    var VERSION$2 = new Version$1('9.1.0-next.2+3.sha-20b0c80');
 
     /**
      * @license
@@ -63072,7 +63072,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+4.sha-30f29ee');
+    var VERSION$3 = new Version$1('9.1.0-next.2+3.sha-20b0c80');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
