cce4286a8f218f7ade6f0aba36dfa6feb6eac277
atscott <atscott@google.com>
2020-02-27 19:14:14 +0000

    30f29ee8e7 docs: correct spelling of 'detection' (#35723)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+2.sha-0b97d07
+ * @license Angular v9.1.0-next.2+4.sha-30f29ee
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18813,7 +18813,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+2.sha-0b97d07');
+    var VERSION$1 = new Version('9.1.0-next.2+4.sha-30f29ee');
 
     /**
      * @license
@@ -47937,7 +47937,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+2.sha-0b97d07');
+    var VERSION$2 = new Version$1('9.1.0-next.2+4.sha-30f29ee');
 
     /**
      * @license
@@ -63072,7 +63072,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+2.sha-0b97d07');
+    var VERSION$3 = new Version$1('9.1.0-next.2+4.sha-30f29ee');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
