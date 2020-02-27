c7136f9b71c68e2a5de1f60d01f61d89c25ea63f
atscott <atscott@google.com>
2020-02-27 22:28:54 +0000

    287d841486 build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3+9.sha-9b53054
+ * @license Angular v9.0.4+3.sha-287d841
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18828,7 +18828,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.3+9.sha-9b53054');
+    var VERSION$1 = new Version('9.0.4+3.sha-287d841');
 
     /**
      * @license
@@ -38571,7 +38571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.3+9.sha-9b53054');
+    var VERSION$2 = new Version$1('9.0.4+3.sha-287d841');
 
     /**
      * @license
@@ -50577,7 +50577,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.3+9.sha-9b53054');
+    var VERSION$3 = new Version$1('9.0.4+3.sha-287d841');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
