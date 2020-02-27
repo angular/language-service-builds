1c06e4238ea3ecee3099e0e765e18644289f2ea7
atscott <atscott@google.com>
2020-02-27 22:22:46 +0000

    8fed1fe792 perf(core): adding micro benchmark for host bindings (#35705)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+10.sha-bc7b589
+ * @license Angular v9.1.0-next.2+11.sha-8fed1fe
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18828,7 +18828,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+10.sha-bc7b589');
+    var VERSION$1 = new Version('9.1.0-next.2+11.sha-8fed1fe');
 
     /**
      * @license
@@ -38574,7 +38574,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+10.sha-bc7b589');
+    var VERSION$2 = new Version$1('9.1.0-next.2+11.sha-8fed1fe');
 
     /**
      * @license
@@ -50581,7 +50581,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+10.sha-bc7b589');
+    var VERSION$3 = new Version$1('9.1.0-next.2+11.sha-8fed1fe');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
