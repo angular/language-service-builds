dd674fa62624d8ba044258a6776178140027fa3c
Joey Perrott <josephperrott.github@gmail.com>
2020-02-28 20:32:42 +0000

    3b32bc7b41 ci: move public-api goldens to goldens directory

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+22.sha-7bfcc35
+ * @license Angular v9.1.0-next.2+22.sha-3b32bc7
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18747,7 +18747,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+22.sha-7bfcc35');
+    var VERSION$1 = new Version('9.1.0-next.2+22.sha-3b32bc7');
 
     /**
      * @license
@@ -38545,7 +38545,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+22.sha-7bfcc35');
+    var VERSION$2 = new Version$1('9.1.0-next.2+22.sha-3b32bc7');
 
     /**
      * @license
@@ -50552,7 +50552,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+22.sha-7bfcc35');
+    var VERSION$3 = new Version$1('9.1.0-next.2+22.sha-3b32bc7');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
