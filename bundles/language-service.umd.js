ccfb03473dc043ad1a8c47a3b9ffe04b08685f71
atscott <atscott@google.com>
2020-02-28 16:03:29 +0000

    40039d8068 fix(ivy): narrow `NgIf` context variables in template type checker (#35125)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+20.sha-208ef7b
+ * @license Angular v9.1.0-next.2+21.sha-40039d8
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18732,7 +18732,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+20.sha-208ef7b');
+    var VERSION$1 = new Version('9.1.0-next.2+21.sha-40039d8');
 
     /**
      * @license
@@ -47908,7 +47908,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+20.sha-208ef7b');
+    var VERSION$2 = new Version$1('9.1.0-next.2+21.sha-40039d8');
 
     /**
      * @license
@@ -63043,7 +63043,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+20.sha-208ef7b');
+    var VERSION$3 = new Version$1('9.1.0-next.2+21.sha-40039d8');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
