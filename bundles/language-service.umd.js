0bc59517712c294c8f8685431b19a724d0fcb396
atscott <atscott@google.com>
2020-03-02 17:53:34 +0000

    44e47da4cf docs: reword explanation for using singleton services decorative to be more understandable (#35002)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+35.sha-a153b61
+ * @license Angular v9.1.0-next.2+36.sha-44e47da
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18736,7 +18736,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+35.sha-a153b61');
+    var VERSION$1 = new Version('9.1.0-next.2+36.sha-44e47da');
 
     /**
      * @license
@@ -47907,7 +47907,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+35.sha-a153b61');
+    var VERSION$2 = new Version$1('9.1.0-next.2+36.sha-44e47da');
 
     /**
      * @license
@@ -63043,7 +63043,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+35.sha-a153b61');
+    var VERSION$3 = new Version$1('9.1.0-next.2+36.sha-44e47da');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
