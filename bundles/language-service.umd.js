a492e07cc28628c75bfcc33e69dd8faca59e3dcd
atscott <atscott@google.com>
2020-03-02 16:44:30 +0000

    35c9f0dc2f fix(animations): process shorthand `margin` and `padding` styles correctly (#35701)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+28.sha-746e313
+ * @license Angular v9.1.0-next.2+32.sha-35c9f0d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18732,7 +18732,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+28.sha-746e313');
+    var VERSION$1 = new Version('9.1.0-next.2+32.sha-35c9f0d');
 
     /**
      * @license
@@ -47904,7 +47904,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+28.sha-746e313');
+    var VERSION$2 = new Version$1('9.1.0-next.2+32.sha-35c9f0d');
 
     /**
      * @license
@@ -63040,7 +63040,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+28.sha-746e313');
+    var VERSION$3 = new Version$1('9.1.0-next.2+32.sha-35c9f0d');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
