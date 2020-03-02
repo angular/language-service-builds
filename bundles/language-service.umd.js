fe70937b5b35e8ca8c7a88fcea4837420bd571c0
atscott <atscott@google.com>
2020-03-02 19:54:16 +0000

    710fca2b8e style: Remove `ivy` scope, and add `ve` scope (#35799)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+23.sha-1503408
+ * @license Angular v9.0.4+25.sha-710fca2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18751,7 +18751,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.4+23.sha-1503408');
+    var VERSION$1 = new Version('9.0.4+25.sha-710fca2');
 
     /**
      * @license
@@ -38544,7 +38544,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.4+23.sha-1503408');
+    var VERSION$2 = new Version$1('9.0.4+25.sha-710fca2');
 
     /**
      * @license
@@ -50550,7 +50550,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.4+23.sha-1503408');
+    var VERSION$3 = new Version$1('9.0.4+25.sha-710fca2');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
