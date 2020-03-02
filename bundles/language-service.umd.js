d30d15a06bb7ac964d5341fb9ceac0109f169ea9
atscott <atscott@google.com>
2020-03-02 16:41:40 +0000

    746e313a76 docs: Fix typo in template syntax (#35767)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+29.sha-0a5a841
+ * @license Angular v9.1.0-next.2+28.sha-746e313
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18747,7 +18747,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+29.sha-0a5a841');
+    var VERSION$1 = new Version('9.1.0-next.2+28.sha-746e313');
 
     /**
      * @license
@@ -38543,7 +38543,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+29.sha-0a5a841');
+    var VERSION$2 = new Version$1('9.1.0-next.2+28.sha-746e313');
 
     /**
      * @license
@@ -50550,7 +50550,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+29.sha-0a5a841');
+    var VERSION$3 = new Version$1('9.1.0-next.2+28.sha-746e313');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
