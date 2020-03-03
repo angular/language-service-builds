7b9445102e2b01f8ddd71377beaebfac9714637d
atscott <atscott@google.com>
2020-03-03 18:05:16 +0000

    c7d0567d37 Revert "docs: add comment markers for internal goog.define replacement (#32795)" (#35830)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+54.sha-5615928
+ * @license Angular v9.1.0-next.2+55.sha-c7d0567
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+54.sha-5615928');
+    var VERSION$1 = new Version('9.1.0-next.2+55.sha-c7d0567');
 
     /**
      * @license
@@ -38654,7 +38654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+54.sha-5615928');
+    var VERSION$2 = new Version$1('9.1.0-next.2+55.sha-c7d0567');
 
     /**
      * @license
@@ -50661,7 +50661,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+54.sha-5615928');
+    var VERSION$3 = new Version$1('9.1.0-next.2+55.sha-c7d0567');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
