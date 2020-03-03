263b1398d3787f8c8637f1f604923b0e2f14eac8
atscott <atscott@google.com>
2020-03-03 22:17:36 +0000

    2c41bb8490 fix(compiler): type-checking error for duplicate variables in templates (#35674)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+57.sha-1f8a243
+ * @license Angular v9.1.0-next.2+58.sha-2c41bb8
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18735,7 +18735,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+57.sha-1f8a243');
+    var VERSION$1 = new Version('9.1.0-next.2+58.sha-2c41bb8');
 
     /**
      * @license
@@ -48012,7 +48012,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+57.sha-1f8a243');
+    var VERSION$2 = new Version$1('9.1.0-next.2+58.sha-2c41bb8');
 
     /**
      * @license
@@ -63150,7 +63150,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+57.sha-1f8a243');
+    var VERSION$3 = new Version$1('9.1.0-next.2+58.sha-2c41bb8');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
