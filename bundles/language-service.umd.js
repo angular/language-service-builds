2bf6fa149e58e14ae5343bd06ea072256e3de019
atscott <atscott@google.com>
2020-03-03 17:20:32 +0000

    bf42807aa6 docs(common): switch HTTP guide examples to `TestBed.inject()` (#35777)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+46.sha-c195d22
+ * @license Angular v9.1.0-next.2+47.sha-bf42807
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+46.sha-c195d22');
+    var VERSION$1 = new Version('9.1.0-next.2+47.sha-bf42807');
 
     /**
      * @license
@@ -38654,7 +38654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+46.sha-c195d22');
+    var VERSION$2 = new Version$1('9.1.0-next.2+47.sha-bf42807');
 
     /**
      * @license
@@ -50661,7 +50661,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+46.sha-c195d22');
+    var VERSION$3 = new Version$1('9.1.0-next.2+47.sha-bf42807');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
