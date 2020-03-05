a9576e22431a175cf43c85ca575fcea13cc9a1b5
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 23:42:42 +0000

    c55f900081 fix(ngcc): a new LockFile implementation that uses a child-process (#35861)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+80.sha-98a9daf
+ * @license Angular v9.1.0-next.2+84.sha-c55f900
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18871,7 +18871,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+80.sha-98a9daf');
+    var VERSION$1 = new Version('9.1.0-next.2+84.sha-c55f900');
 
     /**
      * @license
@@ -48148,7 +48148,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+80.sha-98a9daf');
+    var VERSION$2 = new Version$1('9.1.0-next.2+84.sha-c55f900');
 
     /**
      * @license
@@ -63286,7 +63286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+80.sha-98a9daf');
+    var VERSION$3 = new Version$1('9.1.0-next.2+84.sha-c55f900');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
