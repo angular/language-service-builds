d60d32a2b0a9ef90e36cccbe11b47e1d44c49b6b
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:14:52 +0000

    28d2bf7d7c docs(zone.js): update comment and chapter format of ngzone doc. (#35738)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+10.sha-eaf5b58
+ * @license Angular v9.0.5+12.sha-28d2bf7
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18887,7 +18887,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.5+10.sha-eaf5b58');
+    var VERSION$1 = new Version('9.0.5+12.sha-28d2bf7');
 
     /**
      * @license
@@ -38764,7 +38764,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.5+10.sha-eaf5b58');
+    var VERSION$2 = new Version$1('9.0.5+12.sha-28d2bf7');
 
     /**
      * @license
@@ -50774,7 +50774,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.5+10.sha-eaf5b58');
+    var VERSION$3 = new Version$1('9.0.5+12.sha-28d2bf7');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
