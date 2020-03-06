eeaaf613608e4fa6bf3d824e184a8eba08cdce38
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:07:48 +0000

    4e1d780554 fix(language-service): resolve the real path for symlink (#35895)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+89.sha-59078c4
+ * @license Angular v9.1.0-next.2+88.sha-4e1d780
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18871,7 +18871,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+89.sha-59078c4');
+    var VERSION$1 = new Version('9.1.0-next.2+88.sha-4e1d780');
 
     /**
      * @license
@@ -48148,7 +48148,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+89.sha-59078c4');
+    var VERSION$2 = new Version$1('9.1.0-next.2+88.sha-4e1d780');
 
     /**
      * @license
@@ -63289,7 +63289,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+89.sha-59078c4');
+    var VERSION$3 = new Version$1('9.1.0-next.2+88.sha-4e1d780');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
