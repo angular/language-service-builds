dd13d7397745da5386f7a76e2fe9441f80b172de
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:25:59 +0000

    86a45ff561 docs(zone.js): update coments of zone configuration API definition (#35849)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+94.sha-84c2458
+ * @license Angular v9.1.0-next.2+95.sha-86a45ff
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18871,7 +18871,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+94.sha-84c2458');
+    var VERSION$1 = new Version('9.1.0-next.2+95.sha-86a45ff');
 
     /**
      * @license
@@ -48148,7 +48148,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+94.sha-84c2458');
+    var VERSION$2 = new Version$1('9.1.0-next.2+95.sha-86a45ff');
 
     /**
      * @license
@@ -63289,7 +63289,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+94.sha-84c2458');
+    var VERSION$3 = new Version$1('9.1.0-next.2+95.sha-86a45ff');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
