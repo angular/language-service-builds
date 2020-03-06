8426bf7fb4c4457e24a0d545c494e46fb52dcbbc
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 00:17:37 +0000

    2cccf59415 build: create dev-infra-private npm package (#35862)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+5.sha-2a9b254
+ * @license Angular v9.0.5+6.sha-2cccf59
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18887,7 +18887,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.5+5.sha-2a9b254');
+    var VERSION$1 = new Version('9.0.5+6.sha-2cccf59');
 
     /**
      * @license
@@ -38764,7 +38764,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.5+5.sha-2a9b254');
+    var VERSION$2 = new Version$1('9.0.5+6.sha-2cccf59');
 
     /**
      * @license
@@ -50774,7 +50774,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.5+5.sha-2a9b254');
+    var VERSION$3 = new Version$1('9.0.5+6.sha-2cccf59');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
