c59d7bd7023740a55654088fe3569295735168ec
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:02:49 +0000

    a1b90619ba test(zone.js): sets rollup_bundle to use the --silent flag in karma tests (#35881)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+87.sha-44f9fa6
+ * @license Angular v9.1.0-next.2+86.sha-a1b9061
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18871,7 +18871,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+87.sha-44f9fa6');
+    var VERSION$1 = new Version('9.1.0-next.2+86.sha-a1b9061');
 
     /**
      * @license
@@ -48148,7 +48148,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+87.sha-44f9fa6');
+    var VERSION$2 = new Version$1('9.1.0-next.2+86.sha-a1b9061');
 
     /**
      * @license
@@ -63286,7 +63286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+87.sha-44f9fa6');
+    var VERSION$3 = new Version$1('9.1.0-next.2+86.sha-a1b9061');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
