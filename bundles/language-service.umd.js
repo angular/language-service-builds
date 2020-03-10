898bf57ee2f549c8e52f7e0a2cd21a5b54e3f9d2
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:59:55 +0000

    65a6848ed7 build: creates a script to get contributor stats from across the angular org (#35834)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+123.sha-9368053
+ * @license Angular v9.1.0-next.2+126.sha-65a6848
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18886,7 +18886,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+123.sha-9368053');
+    var VERSION$1 = new Version('9.1.0-next.2+126.sha-65a6848');
 
     /**
      * @license
@@ -38801,7 +38801,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+123.sha-9368053');
+    var VERSION$2 = new Version$1('9.1.0-next.2+126.sha-65a6848');
 
     /**
      * @license
@@ -50815,7 +50815,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+123.sha-9368053');
+    var VERSION$3 = new Version$1('9.1.0-next.2+126.sha-65a6848');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
