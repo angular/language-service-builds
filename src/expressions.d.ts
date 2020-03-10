42d2f0ea41e55bea608af9b90491e85960bbde4b
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:08:51 +0000

    a73e125c04 feat(dev-infra): add dev-infra to the commit message scopes (#35992)

--- src/expressions.d.ts
+++ src/expressions.d.ts
@@ -9,14 +9,6 @@
 import { AST } from '@angular/compiler';
 import { Span, Symbol, SymbolQuery, SymbolTable } from './types';
 export declare function getExpressionCompletions(scope: SymbolTable, ast: AST, position: number, query: SymbolQuery): Symbol[] | undefined;
-/**
- * Retrieves the expression symbol at a particular position in a template.
- *
- * @param scope symbols in scope of the template
- * @param ast template AST
- * @param position absolute location in template to retrieve symbol at
- * @param query type symbol query for the template scope
- */
 export declare function getExpressionSymbol(scope: SymbolTable, ast: AST, position: number, query: SymbolQuery): {
     symbol: Symbol;
     span: Span;
