2f58582c958b98d4634eb16284dfd39c7d10b758
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:00:27 +0000

    191e4d15b5 build: fix elements test failures on IE (#35940)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+125.sha-0bf6e58
+ * @license Angular v9.1.0-next.2+124.sha-191e4d1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18886,7 +18886,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+125.sha-0bf6e58');
+    var VERSION$1 = new Version('9.1.0-next.2+124.sha-191e4d1');
 
     /**
      * @license
@@ -38801,7 +38801,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.2+125.sha-0bf6e58');
+    var VERSION$2 = new Version$1('9.1.0-next.2+124.sha-191e4d1');
 
     /**
      * @license
@@ -50815,7 +50815,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.2+125.sha-0bf6e58');
+    var VERSION$3 = new Version$1('9.1.0-next.2+124.sha-191e4d1');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
