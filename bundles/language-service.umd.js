e2c4c300909517b43a1d9b4ad08d3c0111b971ef
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:13:04 +0000

    10f1e94f18 build(bazel): remove hacky fake @angular/bazel npm package under tools/npm/@angular_bazel (#36009)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+15.sha-5f7d066
+ * @license Angular v9.1.0-next.4+12.sha-10f1e94
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18884,7 +18884,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+15.sha-5f7d066');
+    var VERSION$1 = new Version('9.1.0-next.4+12.sha-10f1e94');
 
     /**
      * @license
@@ -38799,7 +38799,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+15.sha-5f7d066');
+    var VERSION$2 = new Version$1('9.1.0-next.4+12.sha-10f1e94');
 
     /**
      * @license
@@ -50813,7 +50813,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+15.sha-5f7d066');
+    var VERSION$3 = new Version$1('9.1.0-next.4+12.sha-10f1e94');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
