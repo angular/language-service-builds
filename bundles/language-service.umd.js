45eb41b58b41701439a71dbe4e267ec6c01a8d9d
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:12:51 +0000

    5f7d06668e fix(compiler-cli): TypeScript peer dependency range (#36008)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+14.sha-5e3a898
+ * @license Angular v9.1.0-next.4+15.sha-5f7d066
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18884,7 +18884,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+14.sha-5e3a898');
+    var VERSION$1 = new Version('9.1.0-next.4+15.sha-5f7d066');
 
     /**
      * @license
@@ -38799,7 +38799,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+14.sha-5e3a898');
+    var VERSION$2 = new Version$1('9.1.0-next.4+15.sha-5f7d066');
 
     /**
      * @license
@@ -50813,7 +50813,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+14.sha-5e3a898');
+    var VERSION$3 = new Version$1('9.1.0-next.4+15.sha-5f7d066');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
