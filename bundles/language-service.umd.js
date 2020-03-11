1462aa6db3b8987934aedea9320ef945cba52f62
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:17:03 +0000

    d8f9ec5291 refactor(docs-infra): merge `.api-body` style blocks (#31574)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+18.sha-9a12403
+ * @license Angular v9.1.0-next.4+22.sha-d8f9ec5
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18869,7 +18869,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+18.sha-9a12403');
+    var VERSION$1 = new Version('9.1.0-next.4+22.sha-d8f9ec5');
 
     /**
      * @license
@@ -48155,7 +48155,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+18.sha-9a12403');
+    var VERSION$2 = new Version$1('9.1.0-next.4+22.sha-d8f9ec5');
 
     /**
      * @license
@@ -63308,7 +63308,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+18.sha-9a12403');
+    var VERSION$3 = new Version$1('9.1.0-next.4+22.sha-d8f9ec5');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
