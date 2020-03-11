21289ae8cdcaca400474e745537eca21e7dbc023
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:38:40 +0000

    19cfaf7f4c fix(core): don't re-invoke pure pipes that throw and arguments are the same (#35827)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+1.sha-3aa5ae9
+ * @license Angular v9.1.0-next.4+4.sha-19cfaf7
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18869,7 +18869,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+1.sha-3aa5ae9');
+    var VERSION$1 = new Version('9.1.0-next.4+4.sha-19cfaf7');
 
     /**
      * @license
@@ -48153,7 +48153,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+1.sha-3aa5ae9');
+    var VERSION$2 = new Version$1('9.1.0-next.4+4.sha-19cfaf7');
 
     /**
      * @license
@@ -52750,6 +52750,18 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         return pureFunctionVInternal(getLView(), getBindingRoot(), slotOffset, pureFn, exps, thisArg);
     }
     /**
+     * Results of a pure function invocation are stored in LView in a dedicated slot that is initialized
+     * to NO_CHANGE. In rare situations a pure pipe might throw an exception on the very first
+     * invocation and not produce any valid results. In this case LView would keep holding the NO_CHANGE
+     * value. The NO_CHANGE is not something that we can use in expressions / bindings thus we convert
+     * it to `undefined`.
+     */
+    function getPureFunctionReturnValue(lView, returnValueIndex) {
+        ngDevMode && assertDataInRange(lView, returnValueIndex);
+        var lastReturnValue = lView[returnValueIndex];
+        return lastReturnValue === NO_CHANGE ? undefined : lastReturnValue;
+    }
+    /**
      * If the value of the provided exp has changed, calls the pure function to return
      * an updated value. Or if the value has not changed, returns cached value.
      *
@@ -52765,7 +52777,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var bindingIndex = bindingRoot + slotOffset;
         return bindingUpdated(lView, bindingIndex, exp) ?
             updateBinding(lView, bindingIndex + 1, thisArg ? pureFn.call(thisArg, exp) : pureFn(exp)) :
-            getBinding(lView, bindingIndex + 1);
+            getPureFunctionReturnValue(lView, bindingIndex + 1);
     }
     /**
      * If the value of any provided exp has changed, calls the pure function to return
@@ -52784,7 +52796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var bindingIndex = bindingRoot + slotOffset;
         return bindingUpdated2(lView, bindingIndex, exp1, exp2) ?
             updateBinding(lView, bindingIndex + 2, thisArg ? pureFn.call(thisArg, exp1, exp2) : pureFn(exp1, exp2)) :
-            getBinding(lView, bindingIndex + 2);
+            getPureFunctionReturnValue(lView, bindingIndex + 2);
     }
     /**
      * If the value of any provided exp has changed, calls the pure function to return
@@ -52804,7 +52816,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var bindingIndex = bindingRoot + slotOffset;
         return bindingUpdated3(lView, bindingIndex, exp1, exp2, exp3) ?
             updateBinding(lView, bindingIndex + 3, thisArg ? pureFn.call(thisArg, exp1, exp2, exp3) : pureFn(exp1, exp2, exp3)) :
-            getBinding(lView, bindingIndex + 3);
+            getPureFunctionReturnValue(lView, bindingIndex + 3);
     }
     /**
      * If the value of any provided exp has changed, calls the pure function to return
@@ -52826,7 +52838,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var bindingIndex = bindingRoot + slotOffset;
         return bindingUpdated4(lView, bindingIndex, exp1, exp2, exp3, exp4) ?
             updateBinding(lView, bindingIndex + 4, thisArg ? pureFn.call(thisArg, exp1, exp2, exp3, exp4) : pureFn(exp1, exp2, exp3, exp4)) :
-            getBinding(lView, bindingIndex + 4);
+            getPureFunctionReturnValue(lView, bindingIndex + 4);
     }
     /**
      * pureFunction instruction that can support any number of bindings.
@@ -52850,7 +52862,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             bindingUpdated(lView, bindingIndex++, exps[i]) && (different = true);
         }
         return different ? updateBinding(lView, bindingIndex, pureFn.apply(thisArg, exps)) :
-            getBinding(lView, bindingIndex);
+            getPureFunctionReturnValue(lView, bindingIndex);
     }
 
     /**
@@ -63294,7 +63306,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+1.sha-3aa5ae9');
+    var VERSION$3 = new Version$1('9.1.0-next.4+4.sha-19cfaf7');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
