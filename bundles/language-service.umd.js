9f70bfda23caf1e2714c0cca2655426d985005b9
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:29:04 +0000

    8ea61a19cd feat(ngcc): support invalidating the entry-point manifest (#35931)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+28.sha-5af0e75
+ * @license Angular v9.1.0-next.4+33.sha-8ea61a1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18930,7 +18930,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+28.sha-5af0e75');
+    var VERSION$1 = new Version('9.1.0-next.4+33.sha-8ea61a1');
 
     /**
      * @license
@@ -38886,7 +38886,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+28.sha-5af0e75');
+    var VERSION$2 = new Version$1('9.1.0-next.4+33.sha-8ea61a1');
 
     /**
      * @license
@@ -50900,7 +50900,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+28.sha-5af0e75');
+    var VERSION$3 = new Version$1('9.1.0-next.4+33.sha-8ea61a1');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
