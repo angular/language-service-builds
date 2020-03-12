518d17637a8bde557d0334387ca30e28112f09c4
Andrew Kushnir <akushnir@google.com>
2020-03-12 19:37:30 +0000

    63815b54f3 test(core): Add benchpress test for transplanted view change detection (#36001)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+40.sha-372a56a
+ * @license Angular v9.1.0-next.4+41.sha-63815b5
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18930,7 +18930,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+40.sha-372a56a');
+    var VERSION$1 = new Version('9.1.0-next.4+41.sha-63815b5');
 
     /**
      * @license
@@ -38886,7 +38886,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+40.sha-372a56a');
+    var VERSION$2 = new Version$1('9.1.0-next.4+41.sha-63815b5');
 
     /**
      * @license
@@ -50900,7 +50900,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+40.sha-372a56a');
+    var VERSION$3 = new Version$1('9.1.0-next.4+41.sha-63815b5');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
