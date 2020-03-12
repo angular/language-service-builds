376358496e27b5890e0c6b23b73dea45dae28b54
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:12:04 +0000

    37a48391f2 refactor(ngcc): remove unused `LockFileWithSignalHandlers` (#35938)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18915,7 +18915,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+36.sha-0d806c8');
+    var VERSION$1 = new Version('9.1.0-next.4+37.sha-37a4839');
 
     /**
      * @license
@@ -48242,7 +48242,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+36.sha-0d806c8');
+    var VERSION$2 = new Version$1('9.1.0-next.4+37.sha-37a4839');
 
     /**
      * @license
@@ -63395,7 +63395,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+36.sha-0d806c8');
+    var VERSION$3 = new Version$1('9.1.0-next.4+37.sha-37a4839');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
