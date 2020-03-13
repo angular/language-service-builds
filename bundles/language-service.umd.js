286acdcc29d9954fa0cdb226450a8d6843366c32
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:20:34 +0000

    4a9514ec4e docs: remove speeding update ngcc section (#36049)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+41.sha-63815b5
+ * @license Angular v9.1.0-next.4+42.sha-4a9514e
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18930,7 +18930,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+41.sha-63815b5');
+    var VERSION$1 = new Version('9.1.0-next.4+42.sha-4a9514e');
 
     /**
      * @license
@@ -38886,7 +38886,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+41.sha-63815b5');
+    var VERSION$2 = new Version$1('9.1.0-next.4+42.sha-4a9514e');
 
     /**
      * @license
@@ -50900,7 +50900,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+41.sha-63815b5');
+    var VERSION$3 = new Version$1('9.1.0-next.4+42.sha-4a9514e');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
