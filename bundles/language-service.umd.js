0cdd66a39271cacd858f3df6ed2afbc4ca84afe1
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:22:09 +0000

    0f8e710c7c feat(zone.js): Monkey patch MessagePort.prototype onproperties (#34610)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+54.sha-8456c5e
+ * @license Angular v9.1.0-next.4+53.sha-0f8e710
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18930,7 +18930,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+54.sha-8456c5e');
+    var VERSION$1 = new Version('9.1.0-next.4+53.sha-0f8e710');
 
     /**
      * @license
@@ -38886,7 +38886,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+54.sha-8456c5e');
+    var VERSION$2 = new Version$1('9.1.0-next.4+53.sha-0f8e710');
 
     /**
      * @license
@@ -50900,7 +50900,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+54.sha-8456c5e');
+    var VERSION$3 = new Version$1('9.1.0-next.4+53.sha-0f8e710');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
