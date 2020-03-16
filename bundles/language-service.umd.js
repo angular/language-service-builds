9491cb1e87022d784b1c30e441f48b06d6c3723d
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:21:53 +0000

    8456c5ec60 feat(zone.js): add a zone config to allow user disable wrapping uncaught promise rejection (#35873)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+52.sha-05d0586
+ * @license Angular v9.1.0-next.4+54.sha-8456c5e
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18915,7 +18915,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+52.sha-05d0586');
+    var VERSION$1 = new Version('9.1.0-next.4+54.sha-8456c5e');
 
     /**
      * @license
@@ -48242,7 +48242,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+52.sha-05d0586');
+    var VERSION$2 = new Version$1('9.1.0-next.4+54.sha-8456c5e');
 
     /**
      * @license
@@ -63401,7 +63401,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+52.sha-05d0586');
+    var VERSION$3 = new Version$1('9.1.0-next.4+54.sha-8456c5e');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
