c06ec56f057fd7d9551ffb228df28608370761d5
Andrew Kushnir <akushnir@google.com>
2020-03-17 17:59:32 +0000

    35d94e9d08 build: fix ts-api-guardian golden approval not working on windows (#36096)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+44.sha-8da4adc
+ * @license Angular v9.0.6+46.sha-35d94e9
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18896,7 +18896,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.6+44.sha-8da4adc');
+    var VERSION$1 = new Version('9.0.6+46.sha-35d94e9');
 
     /**
      * @license
@@ -38805,7 +38805,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.6+44.sha-8da4adc');
+    var VERSION$2 = new Version$1('9.0.6+46.sha-35d94e9');
 
     /**
      * @license
@@ -50815,7 +50815,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.6+44.sha-8da4adc');
+    var VERSION$3 = new Version$1('9.0.6+46.sha-35d94e9');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
