d562de3102dcab14b2a8ee683529293eb174e0a9
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:03 +0000

    6d4f437973 build: ng_rollup_bundle internal rule should support ngcc (#36044)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.7
+ * @license Angular v9.0.7+2.sha-6d4f437
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18896,7 +18896,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.7');
+    var VERSION$1 = new Version('9.0.7+2.sha-6d4f437');
 
     /**
      * @license
@@ -38805,7 +38805,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.0.7');
+    var VERSION$2 = new Version$1('9.0.7+2.sha-6d4f437');
 
     /**
      * @license
@@ -50815,7 +50815,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.0.7');
+    var VERSION$3 = new Version$1('9.0.7+2.sha-6d4f437');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
