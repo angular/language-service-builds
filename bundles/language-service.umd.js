047ca53e4676ac4716a79ca8cc3b855272e1fcfc
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:21:58 +0000

    67b9adfd1a docs(upgrade): remove outdated/non-applicable AOT section from upgrade guide (#36015)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+80.sha-9e70bcb
+ * @license Angular v9.1.0-next.4+101.sha-67b9adf
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18960,7 +18960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+80.sha-9e70bcb');
+    var VERSION$1 = new Version('9.1.0-next.4+101.sha-67b9adf');
 
     /**
      * @license
@@ -48301,7 +48301,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+80.sha-9e70bcb');
+    var VERSION$2 = new Version$1('9.1.0-next.4+101.sha-67b9adf');
 
     /**
      * @license
@@ -63460,7 +63460,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+80.sha-9e70bcb');
+    var VERSION$3 = new Version$1('9.1.0-next.4+101.sha-67b9adf');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
