85e3c5a45f9397aa40b81ae548a44d71f0c9a46c
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:48 +0000

    c9f554cda7 fix(ngcc): do not crash on overlapping entry-points (#36083)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5
+ * @license Angular v9.1.0-next.5+6.sha-c9f554c
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18960,7 +18960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.5');
+    var VERSION$1 = new Version('9.1.0-next.5+6.sha-c9f554c');
 
     /**
      * @license
@@ -48301,7 +48301,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.5');
+    var VERSION$2 = new Version$1('9.1.0-next.5+6.sha-c9f554c');
 
     /**
      * @license
@@ -63460,7 +63460,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.5');
+    var VERSION$3 = new Version$1('9.1.0-next.5+6.sha-c9f554c');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
