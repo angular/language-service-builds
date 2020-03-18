617b55ef337082c314723f638500076489a1dbe1
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:29:04 +0000

    1aa5783a45 docs: adding a note on extra text interpolations and HTML tags in translations to Ivy guide (#36029)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+101.sha-67b9adf
+ * @license Angular v9.1.0-next.4+102.sha-1aa5783
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18960,7 +18960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+101.sha-67b9adf');
+    var VERSION$1 = new Version('9.1.0-next.4+102.sha-1aa5783');
 
     /**
      * @license
@@ -48301,7 +48301,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.4+101.sha-67b9adf');
+    var VERSION$2 = new Version$1('9.1.0-next.4+102.sha-1aa5783');
 
     /**
      * @license
@@ -63460,7 +63460,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.4+101.sha-67b9adf');
+    var VERSION$3 = new Version$1('9.1.0-next.4+102.sha-1aa5783');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
