71d91eb6cfdc1ca137d648083fc6e7b152679ea6
Andrew Kushnir <akushnir@google.com>
2020-03-19 20:26:13 +0000

    b4972fa165 release: cut the v9.1.0-rc.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+8.sha-e342ffd
+ * @license Angular v9.1.0-rc.0
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18960,7 +18960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.5+8.sha-e342ffd');
+    var VERSION$1 = new Version('9.1.0-rc.0');
 
     /**
      * @license
@@ -48301,7 +48301,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.5+8.sha-e342ffd');
+    var VERSION$2 = new Version$1('9.1.0-rc.0');
 
     /**
      * @license
@@ -63516,7 +63516,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.5+8.sha-e342ffd');
+    var VERSION$3 = new Version$1('9.1.0-rc.0');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
