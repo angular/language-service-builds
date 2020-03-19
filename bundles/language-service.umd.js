8134a6312c5dd72a40a1df7da2d62c3353740257
Andrew Kushnir <akushnir@google.com>
2020-03-19 16:28:35 +0000

    e342ffd855 fix(core): adhere to bootstrap options for JIT compiled components (#35534)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+9.sha-fb92f5d
+ * @license Angular v9.1.0-next.5+8.sha-e342ffd
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18960,7 +18960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.5+9.sha-fb92f5d');
+    var VERSION$1 = new Version('9.1.0-next.5+8.sha-e342ffd');
 
     /**
      * @license
@@ -48301,7 +48301,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-next.5+9.sha-fb92f5d');
+    var VERSION$2 = new Version$1('9.1.0-next.5+8.sha-e342ffd');
 
     /**
      * @license
@@ -63516,7 +63516,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-next.5+9.sha-fb92f5d');
+    var VERSION$3 = new Version$1('9.1.0-next.5+8.sha-e342ffd');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
