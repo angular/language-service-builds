88a733b61b9f796eae19528839a1bb32dc09a7f8
Misko Hevery <misko@hevery.com>
2020-03-23 19:00:30 +0000

    1cb7b88505 fix(dev-infra): change circular deps positional params to camelCase (#36165)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+19.sha-fced8ee
+ * @license Angular v9.1.0-rc.0+18.sha-1cb7b88
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -9822,7 +9822,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         return code == $SEMICOLON || code == $EOF || !isAsciiLetter(code);
     }
     function isExpansionCaseStart(peek) {
-        return peek !== $RBRACE;
+        return peek === $EQ || isAsciiLetter(peek) || isDigit(peek);
     }
     function compareCharCodeCaseInsensitive(code1, code2) {
         return toUpperCaseCharCode(code1) == toUpperCaseCharCode(code2);
@@ -18978,7 +18978,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+19.sha-fced8ee');
+    var VERSION$1 = new Version('9.1.0-rc.0+18.sha-1cb7b88');
 
     /**
      * @license
@@ -38955,7 +38955,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+19.sha-fced8ee');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+18.sha-1cb7b88');
 
     /**
      * @license
@@ -50969,7 +50969,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+19.sha-fced8ee');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+18.sha-1cb7b88');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
