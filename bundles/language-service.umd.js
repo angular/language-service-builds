c6773dd93ea8acd31e74484e565bfc28c35ffbc9
Misko Hevery <misko@hevery.com>
2020-03-23 18:56:23 +0000

    e81ad3a1bc docs: Add asterisk info in template syntax guide (#36176)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+13.sha-0ce8ad3
+ * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -18978,7 +18978,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+13.sha-0ce8ad3');
+    var VERSION$1 = new Version('9.1.0-rc.0+16.sha-e81ad3a');
 
     /**
      * @license
@@ -38955,7 +38955,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+13.sha-0ce8ad3');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+16.sha-e81ad3a');
 
     /**
      * @license
@@ -50969,7 +50969,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+13.sha-0ce8ad3');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+16.sha-e81ad3a');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
