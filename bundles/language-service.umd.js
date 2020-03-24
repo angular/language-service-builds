974ddce3980766a9d0d75e1987c6ba43e8033c5b
Misko Hevery <misko@hevery.com>
2020-03-24 22:07:48 +0000

    407fa42679 fix(common): let `KeyValuePipe` accept type unions with `null` (#36093)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.1+10.sha-c9c2408
+ * @license Angular v9.1.0-rc.1+13.sha-407fa42
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -5364,6 +5364,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     function compileFactoryFunction(meta) {
         var t = variable('t');
         var statements = [];
+        var ctorDepsType = NONE_TYPE;
         // The type to instantiate via constructor invocation. If there is no delegated factory, meaning
         // this type is always created by constructor invocation, then this is the type-to-create
         // parameter provided by the user (t) if specified, or the current type if not. If there is a
@@ -5377,6 +5378,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             // There is a constructor (either explicitly or implicitly defined).
             if (meta.deps !== 'invalid') {
                 ctorExpr = new InstantiateExpr(typeForCtor, injectDependencies(meta.deps, meta.injectFn, meta.target === R3FactoryTarget.Pipe));
+                ctorDepsType = createCtorDepsType(meta.deps);
             }
         }
         else {
@@ -5442,7 +5444,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         return {
             factory: fn([new FnParam('t', DYNAMIC_TYPE)], body, INFERRED_TYPE, undefined, meta.name + "_Factory"),
             statements: statements,
-            type: expressionType(importExpr(Identifiers$1.FactoryDef, [typeWithParameters(meta.type.type, meta.typeArgumentCount)]))
+            type: expressionType(importExpr(Identifiers$1.FactoryDef, [typeWithParameters(meta.type.type, meta.typeArgumentCount), ctorDepsType]))
         };
     }
     function injectDependencies(deps, injectFn, isPipe) {
@@ -5479,6 +5481,46 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             default:
                 return unsupported("Unknown R3ResolvedDependencyType: " + R3ResolvedDependencyType[dep.resolved]);
         }
+    }
+    function createCtorDepsType(deps) {
+        var hasTypes = false;
+        var attributeTypes = deps.map(function (dep) {
+            var type = createCtorDepType(dep);
+            if (type !== null) {
+                hasTypes = true;
+                return type;
+            }
+            else {
+                return literal(null);
+            }
+        });
+        if (hasTypes) {
+            return expressionType(literalArr(attributeTypes));
+        }
+        else {
+            return NONE_TYPE;
+        }
+    }
+    function createCtorDepType(dep) {
+        var entries = [];
+        if (dep.resolved === R3ResolvedDependencyType.Attribute) {
+            if (dep.attribute !== null) {
+                entries.push({ key: 'attribute', value: dep.attribute, quoted: false });
+            }
+        }
+        if (dep.optional) {
+            entries.push({ key: 'optional', value: literal(true), quoted: false });
+        }
+        if (dep.host) {
+            entries.push({ key: 'host', value: literal(true), quoted: false });
+        }
+        if (dep.self) {
+            entries.push({ key: 'self', value: literal(true), quoted: false });
+        }
+        if (dep.skipSelf) {
+            entries.push({ key: 'skipSelf', value: literal(true), quoted: false });
+        }
+        return entries.length > 0 ? literalMap(entries) : null;
     }
     function isDelegatedMetadata(meta) {
         return meta.delegateType !== undefined;
@@ -15144,6 +15186,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             errors: allErrors,
             styleUrls: transformer.styleUrls,
             styles: transformer.styles,
+            ngContentSelectors: transformer.ngContentSelectors,
         };
     }
     var HtmlAstToIvyAst = /** @class */ (function () {
@@ -15152,6 +15195,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             this.errors = [];
             this.styles = [];
             this.styleUrls = [];
+            this.ngContentSelectors = [];
             this.inI18nBlock = false;
         }
         // HTML visitor
@@ -15250,6 +15294,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 var selector = preparsedElement.selectAttr;
                 var attrs = element.attrs.map(function (attr) { return _this.visitAttribute(attr); });
                 parsedElement = new Content(selector, attrs, element.sourceSpan, element.i18n);
+                this.ngContentSelectors.push(selector);
             }
             else if (isTemplateElement) {
                 // `<ng-template>`
@@ -17970,7 +18015,13 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var htmlParser = new HtmlParser();
         var parseResult = htmlParser.parse(template, templateUrl, __assign(__assign({ leadingTriviaChars: LEADING_TRIVIA_CHARS }, options), { tokenizeExpansionForms: true }));
         if (parseResult.errors && parseResult.errors.length > 0) {
-            return { errors: parseResult.errors, nodes: [], styleUrls: [], styles: [] };
+            return {
+                errors: parseResult.errors,
+                nodes: [],
+                styleUrls: [],
+                styles: [],
+                ngContentSelectors: []
+            };
         }
         var rootNodes = parseResult.rootNodes;
         // process i18n meta information (scan attributes, generate ids)
@@ -17989,11 +18040,11 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 rootNodes = visitAll$1(new I18nMetaVisitor(interpolationConfig, /* keepI18nAttrs */ false), rootNodes);
             }
         }
-        var _a = htmlAstToRender3Ast(rootNodes, bindingParser), nodes = _a.nodes, errors = _a.errors, styleUrls = _a.styleUrls, styles = _a.styles;
+        var _a = htmlAstToRender3Ast(rootNodes, bindingParser), nodes = _a.nodes, errors = _a.errors, styleUrls = _a.styleUrls, styles = _a.styles, ngContentSelectors = _a.ngContentSelectors;
         if (errors && errors.length > 0) {
-            return { errors: errors, nodes: [], styleUrls: [], styles: [] };
+            return { errors: errors, nodes: [], styleUrls: [], styles: [], ngContentSelectors: [] };
         }
-        return { nodes: nodes, styleUrls: styleUrls, styles: styles };
+        return { nodes: nodes, styleUrls: styleUrls, styles: styles, ngContentSelectors: ngContentSelectors };
     }
     var elementRegistry = new DomElementSchemaRegistry();
     /**
@@ -18156,7 +18207,8 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var definitionMap = baseDirectiveFields(meta, constantPool, bindingParser);
         addFeatures(definitionMap, meta);
         var expression = importExpr(Identifiers$1.defineDirective).callFn([definitionMap.toLiteralMap()]);
-        var type = createTypeForDef(meta, Identifiers$1.DirectiveDefWithMeta);
+        var typeParams = createDirectiveTypeParams(meta);
+        var type = expressionType(importExpr(Identifiers$1.DirectiveDefWithMeta, typeParams));
         return { expression: expression, type: type };
     }
     /**
@@ -18265,7 +18317,9 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             definitionMap.set('changeDetection', literal(changeDetection));
         }
         var expression = importExpr(Identifiers$1.defineComponent).callFn([definitionMap.toLiteralMap()]);
-        var type = createTypeForDef(meta, Identifiers$1.ComponentDefWithMeta);
+        var typeParams = createDirectiveTypeParams(meta);
+        typeParams.push(stringArrayAsType(meta.template.ngContentSelectors));
+        var type = expressionType(importExpr(Identifiers$1.ComponentDefWithMeta, typeParams));
         return { expression: expression, type: type };
     }
     function prepareQueryParams(query, constantPool) {
@@ -18352,18 +18406,18 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         return arr.length > 0 ? expressionType(literalArr(arr.map(function (value) { return literal(value); }))) :
             NONE_TYPE;
     }
-    function createTypeForDef(meta, typeBase) {
+    function createDirectiveTypeParams(meta) {
         // On the type side, remove newlines from the selector as it will need to fit into a TypeScript
         // string literal, which must be on one line.
         var selectorForType = meta.selector !== null ? meta.selector.replace(/\n/g, '') : null;
-        return expressionType(importExpr(typeBase, [
+        return [
             typeWithParameters(meta.type.type, meta.typeArgumentCount),
             selectorForType !== null ? stringAsType(selectorForType) : NONE_TYPE,
             meta.exportAs !== null ? stringArrayAsType(meta.exportAs) : NONE_TYPE,
             stringMapAsType(meta.inputs),
             stringMapAsType(meta.outputs),
             stringArrayAsType(meta.queries.map(function (q) { return q.propertyName; })),
-        ]));
+        ];
     }
     // Define and update any view queries
     function createViewQueriesFunction(viewQueries, constantPool, name) {
@@ -18911,11 +18965,12 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         }
         return {
             token: tokenExpr,
+            attribute: null,
             resolved: facade.resolved,
             host: facade.host,
             optional: facade.optional,
             self: facade.self,
-            skipSelf: facade.skipSelf
+            skipSelf: facade.skipSelf,
         };
     }
     function convertR3DependencyMetadataArray(facades) {
@@ -18978,7 +19033,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.1+10.sha-c9c2408');
+    var VERSION$1 = new Version('9.1.0-rc.1+13.sha-407fa42');
 
     /**
      * @license
@@ -38955,7 +39010,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.1+10.sha-c9c2408');
+    var VERSION$2 = new Version$1('9.1.0-rc.1+13.sha-407fa42');
 
     /**
      * @license
@@ -50969,7 +51024,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.1+10.sha-c9c2408');
+    var VERSION$3 = new Version$1('9.1.0-rc.1+13.sha-407fa42');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
