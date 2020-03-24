fb9b12f20b3688446685799366350e3ff642a1f4
Misko Hevery <misko@hevery.com>
2020-03-24 21:48:52 +0000

    32ce8b1326 feat(compiler): add dependency info and ng-content selectors to metadata (#35695)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+33.sha-b8e9a30
+ * @license Angular v9.1.0-rc.0+32.sha-32ce8b1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19033,7 +19033,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+33.sha-b8e9a30');
+    var VERSION$1 = new Version('9.1.0-rc.0+32.sha-32ce8b1');
 
     /**
      * @license
@@ -39010,7 +39010,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+33.sha-b8e9a30');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+32.sha-32ce8b1');
 
     /**
      * @license
@@ -51024,7 +51024,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+33.sha-b8e9a30');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+32.sha-32ce8b1');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
