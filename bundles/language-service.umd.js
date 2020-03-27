746a37827ab025f51eee43ee4e8a646e4899a3f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 21:38:00 +0000

    5cee709266 fix(ngcc): do not spawn more processes than intended in parallel mode (#36280)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+53.sha-2d0847c
+ * @license Angular v9.1.0-rc.0+54.sha-5cee709
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19018,7 +19018,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+53.sha-2d0847c');
+    var VERSION$1 = new Version('9.1.0-rc.0+54.sha-5cee709');
 
     /**
      * @license
@@ -48361,7 +48361,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+53.sha-2d0847c');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+54.sha-5cee709');
 
     /**
      * @license
@@ -63580,7 +63580,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+53.sha-2d0847c');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+54.sha-5cee709');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
