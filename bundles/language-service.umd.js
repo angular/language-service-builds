87a25e80923ed1b26a206f31947e08f4ac0af954
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:55:16 +0000

    0ab244f39e docs: fix typo in Schematics guide (#36328)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+20.sha-15cffa2
+ * @license Angular v9.1.0+22.sha-0ab244f
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19033,7 +19033,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+20.sha-15cffa2');
+    var VERSION$1 = new Version('9.1.0+22.sha-0ab244f');
 
     /**
      * @license
@@ -39010,7 +39010,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0+20.sha-15cffa2');
+    var VERSION$2 = new Version$1('9.1.0+22.sha-0ab244f');
 
     /**
      * @license
@@ -51024,7 +51024,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0+20.sha-15cffa2');
+    var VERSION$3 = new Version$1('9.1.0+22.sha-0ab244f');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
