b386506d12d4f04b41eb471608f92a6fb4eb8b34
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 18:57:17 +0000

    c6dd900f60 fix(ngcc): do not write entry-point manifest outside node_modules (#36299)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+55.sha-c94a33c
+ * @license Angular v9.1.0-rc.0+58.sha-c6dd900
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19018,7 +19018,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+55.sha-c94a33c');
+    var VERSION$1 = new Version('9.1.0-rc.0+58.sha-c6dd900');
 
     /**
      * @license
@@ -48361,7 +48361,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+55.sha-c94a33c');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+58.sha-c6dd900');
 
     /**
      * @license
@@ -63580,7 +63580,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+55.sha-c94a33c');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+58.sha-c6dd900');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
