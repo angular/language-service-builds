10fe3a98615a503042f9679b5e76aa310fa1d76c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:24:52 +0000

    63fbc71439 build: don't use deprecated $(location) pre-declared variable (#36308)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+63.sha-c5df9ce
+ * @license Angular v9.1.0-rc.0+65.sha-63fbc71
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19018,7 +19018,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+63.sha-c5df9ce');
+    var VERSION$1 = new Version('9.1.0-rc.0+65.sha-63fbc71');
 
     /**
      * @license
@@ -48361,7 +48361,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+63.sha-c5df9ce');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+65.sha-63fbc71');
 
     /**
      * @license
@@ -63580,7 +63580,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+63.sha-c5df9ce');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+65.sha-63fbc71');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
