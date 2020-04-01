c640e37efc6a229be42059795778859e58ce9d23
Kara Erickson <karakara@google.com>
2020-04-01 23:05:37 +0000

    75afd80ae8 refactor(compiler): add `@nocollapse` annotation using a synthetic comment (#35932)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+90.sha-e9de281
+ * @license Angular v9.1.0-rc.0+89.sha-75afd80
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19033,7 +19033,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+90.sha-e9de281');
+    var VERSION$1 = new Version('9.1.0-rc.0+89.sha-75afd80');
 
     /**
      * @license
@@ -39012,7 +39012,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+90.sha-e9de281');
+    var VERSION$2 = new Version$1('9.1.0-rc.0+89.sha-75afd80');
 
     /**
      * @license
@@ -51026,7 +51026,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+90.sha-e9de281');
+    var VERSION$3 = new Version$1('9.1.0-rc.0+89.sha-75afd80');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
