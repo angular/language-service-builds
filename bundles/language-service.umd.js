1e65a77906b2b4083ffed05d57d28931b687519d
Kara Erickson <karakara@google.com>
2020-04-08 18:26:20 +0000

    52ab9397a0 build: update matching regex for bazel stamping (#36523)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+106.sha-fd2ef74
+ * @license Angular v9.1.1+1.sha-52ab939
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19032,7 +19032,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+106.sha-fd2ef74');
+    var VERSION$1 = new Version('9.1.1+1.sha-52ab939');
 
     /**
      * @license
@@ -39285,7 +39285,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0+106.sha-fd2ef74');
+    var VERSION$2 = new Version$1('9.1.1+1.sha-52ab939');
 
     /**
      * @license
@@ -51327,7 +51327,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0+106.sha-fd2ef74');
+    var VERSION$3 = new Version$1('9.1.1+1.sha-52ab939');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
