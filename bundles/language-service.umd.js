03806efe240b6a6c23a535675bde7f57a2e47b8f
Kara Erickson <karakara@google.com>
2020-04-08 18:45:57 +0000

    db4a448439 release: cut the v10.0.0-next.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+151.sha-7549c65
+ * @license Angular v10.0.0-next.0
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19017,7 +19017,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+151.sha-7549c65');
+    var VERSION$1 = new Version('10.0.0-next.0');
 
     /**
      * @license
@@ -48659,7 +48659,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.0-rc.0+151.sha-7549c65');
+    var VERSION$2 = new Version$1('10.0.0-next.0');
 
     /**
      * @license
@@ -63906,7 +63906,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.0-rc.0+151.sha-7549c65');
+    var VERSION$3 = new Version$1('10.0.0-next.0');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
