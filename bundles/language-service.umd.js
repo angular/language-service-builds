5c108d40c449a7b4c7034b648bd308edd2e8af7b
atscott <atscott@google.com>
2020-04-09 18:59:33 +0000

    3bedfdac9d perf(ngcc): only load  if it is needed (#36486)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+12.sha-13ae2d7
+ * @license Angular v10.0.0-next.1+17.sha-3bedfda
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.1+12.sha-13ae2d7');
+    var VERSION$1 = new Version('10.0.0-next.1+17.sha-3bedfda');
 
     /**
      * @license
@@ -39888,7 +39888,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.1+12.sha-13ae2d7');
+    var VERSION$2 = new Version$1('10.0.0-next.1+17.sha-3bedfda');
 
     /**
      * @license
@@ -51939,7 +51939,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('10.0.0-next.1+12.sha-13ae2d7');
+    var VERSION$3 = new Version$1('10.0.0-next.1+17.sha-3bedfda');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
