4f98de6ad4a4777e3d1406286e89965b06ce74c5
atscott <atscott01@gmail.com>
2020-04-09 01:24:02 +0000

    1bfa908ab3 style: typescript lint fix (#36531)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+13.sha-333b867
+ * @license Angular v9.1.1+15.sha-1bfa908
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19556,7 +19556,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.1+13.sha-333b867');
+    var VERSION$1 = new Version('9.1.1+15.sha-1bfa908');
 
     /**
      * @license
@@ -39895,7 +39895,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.1+13.sha-333b867');
+    var VERSION$2 = new Version$1('9.1.1+15.sha-1bfa908');
 
     /**
      * @license
@@ -51946,7 +51946,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('9.1.1+13.sha-333b867');
+    var VERSION$3 = new Version$1('9.1.1+15.sha-1bfa908');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
