df5334e03f1bf9a7fa5894aa1f279080fd41f09b
atscott <atscott@google.com>
2020-04-10 21:16:02 +0000

    ca677481a2 fix(core): undecorated-classes-with-decorated-fields migration should avoid error if base class has no value declaration (#36543)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+20.sha-6ab43d7
+ * @license Angular v10.0.0-next.1+21.sha-ca67748
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.1+20.sha-6ab43d7');
+    var VERSION$1 = new Version('10.0.0-next.1+21.sha-ca67748');
 
     /**
      * @license
@@ -39888,7 +39888,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.1+20.sha-6ab43d7');
+    var VERSION$2 = new Version$1('10.0.0-next.1+21.sha-ca67748');
 
     /**
      * @license
@@ -51939,7 +51939,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('10.0.0-next.1+20.sha-6ab43d7');
+    var VERSION$3 = new Version$1('10.0.0-next.1+21.sha-ca67748');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
