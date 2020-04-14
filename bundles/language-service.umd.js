9ba510bbc0d592f89a15a0299aabce1560036008
atscott <atscott@google.com>
2020-04-14 17:36:53 +0000

    29122c518b style: lint (#36580)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19534,7 +19534,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.1+27.sha-0cc53fb');
+    var VERSION$1 = new Version('10.0.0-next.1+30.sha-29122c5');
 
     /**
      * @license
@@ -49262,7 +49262,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.1+27.sha-0cc53fb');
+    var VERSION$2 = new Version$1('10.0.0-next.1+30.sha-29122c5');
 
     /**
      * @license
@@ -64518,7 +64518,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$3 = new Version$1('10.0.0-next.1+27.sha-0cc53fb');
+    var VERSION$3 = new Version$1('10.0.0-next.1+30.sha-29122c5');
 
     exports.TypeScriptServiceHost = TypeScriptServiceHost;
     exports.VERSION = VERSION$3;
