e482996a9729c9ee41262ba0dc7c521323adaf3c
atscott <atscott@google.com>
2020-04-14 21:21:14 +0000

    0147de9044 build: fix circular deps failure (#36629)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+34.sha-b010849
+ * @license Angular v10.0.0-next.1+35.sha-0147de9
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19534,7 +19534,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.1+34.sha-b010849');
+    var VERSION$1 = new Version('10.0.0-next.1+35.sha-0147de9');
 
     /**
      * @license
@@ -49378,7 +49378,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.1+34.sha-b010849');
+    var VERSION$2 = new Version$1('10.0.0-next.1+35.sha-0147de9');
 
     /**
      * @license
