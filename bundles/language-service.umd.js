5c8fd2cf0ca211dc75a7c5ffbd6cad369c299252
atscott <atscott@google.com>
2020-04-15 23:38:46 +0000

    074266b896 release: cut the v10.0.0-next.2 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+39.sha-c4dc642
+ * @license Angular v10.0.0-next.2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19534,7 +19534,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.1+39.sha-c4dc642');
+    var VERSION$1 = new Version('10.0.0-next.2');
 
     /**
      * @license
@@ -49378,7 +49378,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.1+39.sha-c4dc642');
+    var VERSION$2 = new Version$1('10.0.0-next.2');
 
     /**
      * @license
