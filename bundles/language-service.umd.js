6bbdd589cd31f96cecf701c5803a31637aff0953
atscott <atscott@google.com>
2020-04-16 17:55:59 +0000

    297b7da166 docs: add string-union type note (#35859)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+13.sha-88b0985
+ * @license Angular v10.0.0-next.2+17.sha-297b7da
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+13.sha-88b0985');
+    var VERSION$1 = new Version('10.0.0-next.2+17.sha-297b7da');
 
     /**
      * @license
@@ -39996,7 +39996,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+13.sha-88b0985');
+    var VERSION$2 = new Version$1('10.0.0-next.2+17.sha-297b7da');
 
     /**
      * @license
