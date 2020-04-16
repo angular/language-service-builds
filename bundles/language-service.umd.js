dee92e05ea6ae9dae69d55025f23989a774c18a6
atscott <atscott@google.com>
2020-04-16 17:06:04 +0000

    b1f1d3ffd2 fix(core): handle empty translations correctly (#36499)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
+ * @license Angular v10.0.0-next.2+3.sha-b1f1d3f
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+12.sha-1e3b8a1');
+    var VERSION$1 = new Version('10.0.0-next.2+3.sha-b1f1d3f');
 
     /**
      * @license
@@ -39979,7 +39979,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+12.sha-1e3b8a1');
+    var VERSION$2 = new Version$1('10.0.0-next.2+3.sha-b1f1d3f');
 
     /**
      * @license
