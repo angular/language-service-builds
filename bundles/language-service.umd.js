b226cf4a365132de0c14afeb6071f791f32e1ff4
atscott <atscott@google.com>
2020-04-16 00:09:51 +0000

    793a001d7c release: cut the v9.1.2 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.1+46.sha-5c3774c');
+    var VERSION$1 = new Version('9.1.2');
 
     /**
      * @license
@@ -39979,7 +39979,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.1+46.sha-5c3774c');
+    var VERSION$2 = new Version$1('9.1.2');
 
     /**
      * @license
