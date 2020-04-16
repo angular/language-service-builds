0752d409ed78d81ceb5045464a97244f58328fb2
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:26:35 +0000

    c332d4d916 refactor(ngcc): moved shared setup into a single function (#36637)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+19.sha-bfda7ca
+ * @license Angular v10.0.0-next.2+27.sha-c332d4d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+19.sha-bfda7ca');
+    var VERSION$1 = new Version('10.0.0-next.2+27.sha-c332d4d');
 
     /**
      * @license
@@ -39996,7 +39996,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+19.sha-bfda7ca');
+    var VERSION$2 = new Version$1('10.0.0-next.2+27.sha-c332d4d');
 
     /**
      * @license
