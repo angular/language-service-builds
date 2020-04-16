e3997b1d5840d83436781c993f86f5afaf9038c2
atscott <atscott@google.com>
2020-04-16 18:30:20 +0000

    bfda7ca5bd docs(changelog): Add new line between changes (#36653)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+18.sha-305b059
+ * @license Angular v10.0.0-next.2+19.sha-bfda7ca
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+18.sha-305b059');
+    var VERSION$1 = new Version('10.0.0-next.2+19.sha-bfda7ca');
 
     /**
      * @license
@@ -39996,7 +39996,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+18.sha-305b059');
+    var VERSION$2 = new Version$1('10.0.0-next.2+19.sha-bfda7ca');
 
     /**
      * @license
