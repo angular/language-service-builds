5e2b7b9fd6d03218c1726929d7d43c22b616ba09
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:59:23 +0000

    351759906b refactor(compiler): remove unused CachedFileSystem (#36687)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+41.sha-81d23b3
+ * @license Angular v10.0.0-next.2+44.sha-3517599
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19551,7 +19551,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+41.sha-81d23b3');
+    var VERSION$1 = new Version('10.0.0-next.2+44.sha-3517599');
 
     /**
      * @license
@@ -49383,7 +49383,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+41.sha-81d23b3');
+    var VERSION$2 = new Version$1('10.0.0-next.2+44.sha-3517599');
 
     /**
      * @license
