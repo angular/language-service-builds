bb35bdf454559aaf07dc6196637af2186ba5aeb6
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 17:57:52 +0000

    f4ef8a92f8 docs: fix typo (#36665) PR Close #36665

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+45.sha-d7e9d87
+ * @license Angular v10.0.0-next.2+46.sha-f4ef8a9
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19551,7 +19551,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+45.sha-d7e9d87');
+    var VERSION$1 = new Version('10.0.0-next.2+46.sha-f4ef8a9');
 
     /**
      * @license
@@ -49383,7 +49383,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+45.sha-d7e9d87');
+    var VERSION$2 = new Version$1('10.0.0-next.2+46.sha-f4ef8a9');
 
     /**
      * @license
