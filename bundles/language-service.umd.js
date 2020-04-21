3c9ccb5aef04ea046636a1555ad35afc43b70d4a
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:14:32 +0000

    51b09244ff fix(dev-infra): extract commit headers before checking commit message validity (#36733)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+53.sha-f186c32
+ * @license Angular v10.0.0-next.2+51.sha-51b0924
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19551,7 +19551,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+53.sha-f186c32');
+    var VERSION$1 = new Version('10.0.0-next.2+51.sha-51b0924');
 
     /**
      * @license
@@ -49383,7 +49383,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.2+53.sha-f186c32');
+    var VERSION$2 = new Version$1('10.0.0-next.2+51.sha-51b0924');
 
     /**
      * @license
