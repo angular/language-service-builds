8892e274d6a9956532cad7ee6f7b1151bccd2716
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:23:51 +0000

    0bd50e2e50 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+47.sha-254b9ea
+ * @license Angular v9.1.2+51.sha-0bd50e2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.2+47.sha-254b9ea');
+    var VERSION$1 = new Version('9.1.2+51.sha-0bd50e2');
 
     /**
      * @license
@@ -39973,7 +39973,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.2+47.sha-254b9ea');
+    var VERSION$2 = new Version$1('9.1.2+51.sha-0bd50e2');
 
     /**
      * @license
