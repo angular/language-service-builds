4def638f20f88f2991b52d2889e259552c08e0bd
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:17:18 +0000

    a6e10ef869 build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+52.sha-c0ed57d
+ * @license Angular v9.1.2+54.sha-a6e10ef
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.2+52.sha-c0ed57d');
+    var VERSION$1 = new Version('9.1.2+54.sha-a6e10ef');
 
     /**
      * @license
@@ -39973,7 +39973,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.2+52.sha-c0ed57d');
+    var VERSION$2 = new Version$1('9.1.2+54.sha-a6e10ef');
 
     /**
      * @license
