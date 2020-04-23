2479a235dacf5ed9998a12890a40c0f884bc685e
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:56:16 +0000

    e6161ca307 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+21.sha-e37f266
+ * @license Angular v10.0.0-next.3+24.sha-e6161ca
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19551,7 +19551,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+21.sha-e37f266');
+    var VERSION$1 = new Version('10.0.0-next.3+24.sha-e6161ca');
 
     /**
      * @license
@@ -49397,7 +49397,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+21.sha-e37f266');
+    var VERSION$2 = new Version$1('10.0.0-next.3+24.sha-e6161ca');
 
     /**
      * @license
