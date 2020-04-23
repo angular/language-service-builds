7dc6eedd0ce66440509cc6d3f717f4593c1ab9ce
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:27:25 +0000

    113c75dd27 refactor(localize): remove unused code (#36745)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+7.sha-4275b34
+ * @license Angular v10.0.0-next.3+12.sha-113c75d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+7.sha-4275b34');
+    var VERSION$1 = new Version('10.0.0-next.3+12.sha-113c75d');
 
     /**
      * @license
@@ -39973,7 +39973,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+7.sha-4275b34');
+    var VERSION$2 = new Version$1('10.0.0-next.3+12.sha-113c75d');
 
     /**
      * @license
