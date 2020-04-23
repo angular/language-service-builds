731f17d6e3210541dcbf4065e02d14cff09ae387
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:36:44 +0000

    7743c43529 test(router): fix router test failing on IE (#36742)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+6.sha-d40bcce
+ * @license Angular v9.1.3+9.sha-7743c43
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.3+6.sha-d40bcce');
+    var VERSION$1 = new Version('9.1.3+9.sha-7743c43');
 
     /**
      * @license
@@ -39973,7 +39973,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.3+6.sha-d40bcce');
+    var VERSION$2 = new Version$1('9.1.3+9.sha-7743c43');
 
     /**
      * @license
