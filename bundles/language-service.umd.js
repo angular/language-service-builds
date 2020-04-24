5baf2745cf2ca82c07feef58488055bb4f57625a
Andrew Kushnir <akushnir@google.com>
2020-04-24 18:26:34 +0000

    ad8c4cdd75 revert: "feat(dev-infra): exposed new rule 'component_benchmark' via dev_infra (#36434)" (#36798)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+27.sha-76190c7
+ * @license Angular v10.0.0-next.3+30.sha-ad8c4cd
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19551,7 +19551,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+27.sha-76190c7');
+    var VERSION$1 = new Version('10.0.0-next.3+30.sha-ad8c4cd');
 
     /**
      * @license
@@ -49397,7 +49397,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+27.sha-76190c7');
+    var VERSION$2 = new Version$1('10.0.0-next.3+30.sha-ad8c4cd');
 
     /**
      * @license
