b9c4a3c1ba50f46708c0ae74fdd6c37aaeb966b8
Andrew Kushnir <akushnir@google.com>
2020-04-24 19:54:42 +0000

    2365bb89d7 build: migrate from gulp to ng-dev for running formatting (#36726)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+30.sha-ad8c4cd
+ * @license Angular v10.0.0-next.3+32.sha-2365bb8
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+30.sha-ad8c4cd');
+    var VERSION$1 = new Version('10.0.0-next.3+32.sha-2365bb8');
 
     /**
      * @license
@@ -39973,7 +39973,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+30.sha-ad8c4cd');
+    var VERSION$2 = new Version$1('10.0.0-next.3+32.sha-2365bb8');
 
     /**
      * @license
