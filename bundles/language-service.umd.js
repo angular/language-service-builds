b65260adc0b2c094cd139c413c9a8e17869eb2a6
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:36:16 +0000

    1d9375d115 ci: fix bad reference to `head` property in rebase-pr script (#36825)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+36.sha-f27deea
+ * @license Angular v10.0.0-next.3+37.sha-1d9375d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19551,7 +19551,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+36.sha-f27deea');
+    var VERSION$1 = new Version('10.0.0-next.3+37.sha-1d9375d');
 
     /**
      * @license
@@ -49418,7 +49418,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+36.sha-f27deea');
+    var VERSION$2 = new Version$1('10.0.0-next.3+37.sha-1d9375d');
 
     /**
      * @license
