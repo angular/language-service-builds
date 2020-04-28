990743c77d3f526d247a251356f6ff07c48dc2ba
Andrew Kushnir <akushnir@google.com>
2020-04-28 23:24:36 +0000

    89c589085d fix(ngcc): recognize enum declarations emitted in JavaScript (#36550)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+44.sha-a6a7e1b
+ * @license Angular v10.0.0-next.3+45.sha-89c5890
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+44.sha-a6a7e1b');
+    var VERSION$1 = new Version('10.0.0-next.3+45.sha-89c5890');
 
     /**
      * @license
@@ -40001,7 +40001,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+44.sha-a6a7e1b');
+    var VERSION$2 = new Version$1('10.0.0-next.3+45.sha-89c5890');
 
     /**
      * @license
