c55afec91a35de6180f1dfec6276dffbe64055cc
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:39:07 +0000

    52d2e46700 release: cut the v10.0.0-next.4 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+48.sha-c89421b
+ * @license Angular v10.0.0-next.4
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+48.sha-c89421b');
+    var VERSION$1 = new Version('10.0.0-next.4');
 
     /**
      * @license
@@ -40020,7 +40020,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.3+48.sha-c89421b');
+    var VERSION$2 = new Version$1('10.0.0-next.4');
 
     /**
      * @license
