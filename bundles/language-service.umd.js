22468b64efc555769b688f45c37c956496e953f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:26:09 +0000

    12fcc7cafe build(language-service): make test project a filegroup (#36865)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+15.sha-49be32c
+ * @license Angular v10.0.0-next.4+31.sha-12fcc7c
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+15.sha-49be32c');
+    var VERSION$1 = new Version('10.0.0-next.4+31.sha-12fcc7c');
 
     /**
      * @license
@@ -49535,7 +49535,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.4+15.sha-49be32c');
+    var VERSION$2 = new Version$1('10.0.0-next.4+31.sha-12fcc7c');
 
     /**
      * @license
