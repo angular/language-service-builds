9ed59795a01e2d9502f86d3e221cdc0be630c9da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:09:34 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+34.sha-0f3831b');
+    var VERSION$1 = new Version('10.0.0-next.4+36.sha-297dab8');
 
     /**
      * @license
@@ -49535,7 +49535,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.4+34.sha-0f3831b');
+    var VERSION$2 = new Version$1('10.0.0-next.4+36.sha-297dab8');
 
     /**
      * @license
