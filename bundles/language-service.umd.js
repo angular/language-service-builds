5eec0e2af7b70c4543ff2f0a05b973a6cf24b12c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:18:37 +0000

    75370107c2 docs(dev-infra): update format commands described in DEVELOPER.md (#36910)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+17.sha-a034276
+ * @license Angular v9.1.4+26.sha-7537010
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.4+17.sha-a034276');
+    var VERSION$1 = new Version('9.1.4+26.sha-7537010');
 
     /**
      * @license
@@ -40079,7 +40079,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.4+17.sha-a034276');
+    var VERSION$2 = new Version$1('9.1.4+26.sha-7537010');
 
     /**
      * @license
