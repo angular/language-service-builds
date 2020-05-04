a8d21e6d19b6650e11ba46062ad9a5f27d75d83f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:21:58 +0000

    b3713a112f test(language-service): Add method to override inline template (#36890)

--- bundles/ivy.umd.js
+++ bundles/ivy.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+37.sha-6046e86
+ * @license Angular v10.0.0-next.4+42.sha-b3713a1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+37.sha-6046e86
+ * @license Angular v10.0.0-next.4+42.sha-b3713a1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+37.sha-6046e86');
+    var VERSION$1 = new Version('10.0.0-next.4+42.sha-b3713a1');
 
     /**
      * @license
@@ -49535,7 +49535,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.4+37.sha-6046e86');
+    var VERSION$2 = new Version$1('10.0.0-next.4+42.sha-b3713a1');
 
     /**
      * @license
