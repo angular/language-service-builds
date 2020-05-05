fd34451b8bb1ccc92df8ec9c1c9cb5b1d23c162f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:26:25 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+7.sha-306f46c
+ * @license Angular v10.0.0-next.5+18.sha-b95a336
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+7.sha-306f46c');
+    var VERSION$1 = new Version('10.0.0-next.5+18.sha-b95a336');
 
     /**
      * @license
@@ -49535,7 +49535,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+7.sha-306f46c');
+    var VERSION$2 = new Version$1('10.0.0-next.5+18.sha-b95a336');
 
     /**
      * @license
@@ -52601,25 +52601,14 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var pluralMapping = ['zero', 'one', 'two', 'few', 'many'];
     /**
      * Returns the plural case based on the locale
      */
     function getPluralCase(value, locale) {
-        var plural = getLocalePluralCase(locale)(value);
-        switch (plural) {
-            case 0:
-                return 'zero';
-            case 1:
-                return 'one';
-            case 2:
-                return 'two';
-            case 3:
-                return 'few';
-            case 4:
-                return 'many';
-            default:
-                return 'other';
-        }
+        var plural = getLocalePluralCase(locale)(parseInt(value, 10));
+        var result = pluralMapping[plural];
+        return (result !== undefined) ? result : 'other';
     }
     /**
      * The locale id that the application is using by default (for translations and ICU expressions).
