5de677ff2a2f4824b76e656e04cfcc66f307d50f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 19:11:38 +0000

    c9e0db55f7 refactor(core): deprecate `WrappedValue` (#36819)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+42.sha-ecffc35
+ * @license Angular v10.0.0-next.5+43.sha-c9e0db5
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+42.sha-ecffc35');
+    var VERSION$1 = new Version('10.0.0-next.5+43.sha-c9e0db5');
 
     /**
      * @license
@@ -39369,6 +39369,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * ```
      *
      * @publicApi
+     * @deprecated from v10 stop using. (No replacement, deemed unnecessary.)
      */
     var WrappedValue = /** @class */ (function () {
         function WrappedValue(value) {
@@ -40107,7 +40108,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+42.sha-ecffc35');
+    var VERSION$2 = new Version$1('10.0.0-next.5+43.sha-c9e0db5');
 
     /**
      * @license
