5dfe3a6d4dc521cefd46440380a2def55c3a666a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:04:26 +0000

    1b8752e595 refactor(docs-infra): take advantage of latest Jasmine features in preview server tests (#36837)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+24.sha-fbd281c
+ * @license Angular v10.0.0-next.5+37.sha-1b8752e
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+24.sha-fbd281c');
+    var VERSION$1 = new Version('10.0.0-next.5+37.sha-1b8752e');
 
     /**
      * @license
@@ -40107,7 +40107,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+24.sha-fbd281c');
+    var VERSION$2 = new Version$1('10.0.0-next.5+37.sha-1b8752e');
 
     /**
      * @license
