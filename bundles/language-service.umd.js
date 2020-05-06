8f5d7eccc04de5483a045f0fb89cec74b3c29187
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 21:16:16 +0000

    3c6c00d1d4 docs: remove the note about @angular/http from the deprecations guide (#36944)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+45.sha-fafa50d
+ * @license Angular v10.0.0-next.5+53.sha-3c6c00d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+45.sha-fafa50d');
+    var VERSION$1 = new Version('10.0.0-next.5+53.sha-3c6c00d');
 
     /**
      * @license
@@ -49544,7 +49544,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+45.sha-fafa50d');
+    var VERSION$2 = new Version$1('10.0.0-next.5+53.sha-3c6c00d');
 
     /**
      * @license
