a2abb31671f595339d3c2685f573aa38d8d206ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 20:57:05 +0000

    fafa50d97f fix(ngcc): support ModuleWithProviders functions that delegate (#36948)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+43.sha-c9e0db5
+ * @license Angular v10.0.0-next.5+45.sha-fafa50d
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+43.sha-c9e0db5');
+    var VERSION$1 = new Version('10.0.0-next.5+45.sha-fafa50d');
 
     /**
      * @license
@@ -40108,7 +40108,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+43.sha-c9e0db5');
+    var VERSION$2 = new Version$1('10.0.0-next.5+45.sha-fafa50d');
 
     /**
      * @license
