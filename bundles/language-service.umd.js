2f835ec94cfbfed41e7c690699fb166a4411c770
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 00:27:10 +0000

    fbd281c26e build: remove typescript 3.6 and 3.7 support (#36329)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+22.sha-35d61c1
+ * @license Angular v10.0.0-next.5+24.sha-fbd281c
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+22.sha-35d61c1');
+    var VERSION$1 = new Version('10.0.0-next.5+24.sha-fbd281c');
 
     /**
      * @license
@@ -40107,7 +40107,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+22.sha-35d61c1');
+    var VERSION$2 = new Version$1('10.0.0-next.5+24.sha-fbd281c');
 
     /**
      * @license
