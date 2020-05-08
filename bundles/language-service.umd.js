40e07c0044d2322ff3e9dc1a782a78411ae59942
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:29:20 +0000

    d1b3af697c release: cut the v9.1.5 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+67.sha-dcf1dcb
+ * @license Angular v9.1.5
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.4+67.sha-dcf1dcb');
+    var VERSION$1 = new Version('9.1.5');
 
     /**
      * @license
@@ -40087,7 +40087,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.4+67.sha-dcf1dcb');
+    var VERSION$2 = new Version$1('9.1.5');
 
     /**
      * @license
