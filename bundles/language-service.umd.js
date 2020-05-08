367d49e727d8cc3122521c587ca3d778cf0ba823
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:29:45 +0000

    b6bc0aeb61 release: cut the v10.0.0-next.6 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+74.sha-42d1091
+ * @license Angular v10.0.0-next.6
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+74.sha-42d1091');
+    var VERSION$1 = new Version('10.0.0-next.6');
 
     /**
      * @license
@@ -49451,7 +49451,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.5+74.sha-42d1091');
+    var VERSION$2 = new Version$1('10.0.0-next.6');
 
     /**
      * @license
