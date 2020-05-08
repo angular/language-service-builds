ebdb8afc9e29d0a621c5301fb31a0e36c44b2831
Misko Hevery <misko@hevery.com>
2020-05-08 22:00:37 +0000

    c184de175d build: migrate to typescript file for ng-dev config (#37017)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+8.sha-de0c71c
+ * @license Angular v10.0.0-next.6+10.sha-c184de1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19571,7 +19571,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+8.sha-de0c71c');
+    var VERSION$1 = new Version('10.0.0-next.6+10.sha-c184de1');
 
     /**
      * @license
@@ -49451,7 +49451,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.6+8.sha-de0c71c');
+    var VERSION$2 = new Version$1('10.0.0-next.6+10.sha-c184de1');
 
     /**
      * @license
