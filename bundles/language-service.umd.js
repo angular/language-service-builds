d9dd4feacd0fed04b2ef91a986763960e0bc4b83
Misko Hevery <misko@hevery.com>
2020-05-08 22:04:32 +0000

    bd30c37040 fix(docs-infra): add visual signal for external links (#36601)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+16.sha-eb34aa5
+ * @license Angular v10.0.0-next.6+17.sha-bd30c37
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19622,7 +19622,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+16.sha-eb34aa5');
+    var VERSION$1 = new Version('10.0.0-next.6+17.sha-bd30c37');
 
     /**
      * @license
@@ -40150,7 +40150,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.6+16.sha-eb34aa5');
+    var VERSION$2 = new Version$1('10.0.0-next.6+17.sha-bd30c37');
 
     /**
      * @license
