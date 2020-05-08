ab6a4dc3aed667c3be7853da7a2b2b540d24cc33
Misko Hevery <misko@hevery.com>
2020-05-08 22:04:08 +0000

    df71c7c617 build(docs-infra): upgrade cli command docs sources to 15edb0231 (#37005)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+15.sha-1142c37
+ * @license Angular v10.0.0-next.6+11.sha-df71c7c
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19586,7 +19586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+15.sha-1142c37');
+    var VERSION$1 = new Version('10.0.0-next.6+11.sha-df71c7c');
 
     /**
      * @license
@@ -40105,7 +40105,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.6+15.sha-1142c37');
+    var VERSION$2 = new Version$1('10.0.0-next.6+11.sha-df71c7c');
 
     /**
      * @license
