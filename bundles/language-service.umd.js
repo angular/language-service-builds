6beef957ac083dd35a6c561c766e7cf925e84070
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:12:42 +0000

    99b63b677e Revert "fix(compiler-cli): fix case-sensitivity issues in NgtscCompilerHost (#36968)" (#37003)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.5
+ * @license Angular v9.1.5+16.sha-99b63b6
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19566,7 +19566,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.5');
+    var VERSION$1 = new Version('9.1.5+16.sha-99b63b6');
 
     /**
      * @license
@@ -40087,7 +40087,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.5');
+    var VERSION$2 = new Version$1('9.1.5+16.sha-99b63b6');
 
     /**
      * @license
