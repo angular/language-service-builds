45780b1d66bb928ff93d462cf1a16d6f51425474
Misko Hevery <misko@hevery.com>
2020-05-11 18:18:58 +0000

    7eb1a58b26 docs: refactor form validation doc to be task-oriented (#36374)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+20.sha-28f3c1c
+ * @license Angular v10.0.0-next.6+21.sha-7eb1a58
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19621,7 +19621,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+20.sha-28f3c1c');
+    var VERSION$1 = new Version('10.0.0-next.6+21.sha-7eb1a58');
 
     /**
      * @license
@@ -40149,7 +40149,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.6+20.sha-28f3c1c');
+    var VERSION$2 = new Version$1('10.0.0-next.6+21.sha-7eb1a58');
 
     /**
      * @license
