967b1a2722888699483b330adb6d24bee014aef0
Misko Hevery <misko@hevery.com>
2020-05-12 22:06:07 +0000

    d7c70caa4c docs(platform-webworker): remove mention of version 10 (#37052)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+31.sha-2cddeab
+ * @license Angular v10.0.0-next.6+32.sha-d7c70ca
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19606,7 +19606,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+31.sha-2cddeab');
+    var VERSION$1 = new Version('10.0.0-next.6+32.sha-d7c70ca');
 
     /**
      * @license
@@ -49275,7 +49275,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.6+31.sha-2cddeab');
+    var VERSION$2 = new Version$1('10.0.0-next.6+32.sha-d7c70ca');
 
     /**
      * @license
