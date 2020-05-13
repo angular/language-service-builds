69a2c61042a360d8eb2d26d8035487b47f4f93cd
Misko Hevery <misko@hevery.com>
2020-05-13 23:25:05 +0000

    ce39755937 docs: Remove duplicate `and` (#37067)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+14.sha-2cacab9
+ * @license Angular v10.0.0-next.7+10.sha-ce39755
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+14.sha-2cacab9');
+    var VERSION$1 = new Version('10.0.0-next.7+10.sha-ce39755');
 
     /**
      * @license
@@ -31698,15 +31698,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * without having to remember the specific indices.
      * Uglify will inline these when minifying so there shouldn't be a cost.
      */
-    /**
-     * Flag to signify that this `LContainer` may have transplanted views which need to be change
-     * detected. (see: `LView[DECLARATION_COMPONENT_VIEW])`.
-     *
-     * This flag, once set, is never unset for the `LContainer`. This means that when unset we can skip
-     * a lot of work in `refreshDynamicEmbeddedViews`. But when set we still need to verify
-     * that the `MOVED_VIEWS` are transplanted and on-push.
-     */
-    var HAS_TRANSPLANTED_VIEWS = 2;
+    var ACTIVE_INDEX = 2;
     // PARENT, NEXT, TRANSPLANTED_VIEWS_TO_REFRESH are indices 3, 4, and 5
     // As we already have these constants in LView, we don't need to re-create them.
     // T_HOST is index 6
@@ -31998,6 +31990,9 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      */
     function resetPreOrderHookFlags(lView) {
         lView[PREORDER_HOOK_FLAGS] = 0;
+    }
+    function getLContainerActiveIndex(lContainer) {
+        return lContainer[ACTIVE_INDEX] >> 1 /* SHIFT */;
     }
     /**
      * Updates the `TRANSPLANTED_VIEWS_TO_REFRESH` counter on the `LContainer` as well as the parents
@@ -34111,24 +34106,6 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var parent = lView[PARENT];
         return isLContainer(parent) ? parent[PARENT] : parent;
     }
-    /**
-     * Gets the first `LContainer` in the LView or `null` if none exists.
-     */
-    function getFirstLContainer(lView) {
-        return getNearestLContainer(lView[CHILD_HEAD]);
-    }
-    /**
-     * Gets the next `LContainer` that is a sibling of the given container.
-     */
-    function getNextLContainer(container) {
-        return getNearestLContainer(container[NEXT]);
-    }
-    function getNearestLContainer(viewOrContainer) {
-        while (viewOrContainer !== null && !isLContainer(viewOrContainer)) {
-            viewOrContainer = viewOrContainer[NEXT];
-        }
-        return viewOrContainer;
-    }
 
     /**
      * @license
@@ -34834,9 +34811,17 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         function LContainerDebug(_raw_lContainer) {
             this._raw_lContainer = _raw_lContainer;
         }
+        Object.defineProperty(LContainerDebug.prototype, "activeIndex", {
+            get: function () {
+                return getLContainerActiveIndex(this._raw_lContainer);
+            },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(LContainerDebug.prototype, "hasTransplantedViews", {
             get: function () {
-                return this._raw_lContainer[HAS_TRANSPLANTED_VIEWS];
+                return (this._raw_lContainer[ACTIVE_INDEX] & 1 /* HAS_TRANSPLANTED_VIEWS */) ===
+                    1 /* HAS_TRANSPLANTED_VIEWS */;
             },
             enumerable: true,
             configurable: true
@@ -35873,30 +35858,59 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         }
     }
     /**
+     * Gets the first `LContainer` in the LView or `null` if none exists.
+     */
+    function getFirstLContainer(lView) {
+        var viewOrContainer = lView[CHILD_HEAD];
+        while (viewOrContainer !== null &&
+            !(isLContainer(viewOrContainer) &&
+                viewOrContainer[ACTIVE_INDEX] >> 1 /* SHIFT */ ===
+                    -1 /* DYNAMIC_EMBEDDED_VIEWS_ONLY */)) {
+            viewOrContainer = viewOrContainer[NEXT];
+        }
+        return viewOrContainer;
+    }
+    /**
+     * Gets the next `LContainer` that is a sibling of the given container.
+     */
+    function getNextLContainer(container) {
+        var viewOrContainer = container[NEXT];
+        while (viewOrContainer !== null &&
+            !(isLContainer(viewOrContainer) &&
+                viewOrContainer[ACTIVE_INDEX] >> 1 /* SHIFT */ ===
+                    -1 /* DYNAMIC_EMBEDDED_VIEWS_ONLY */)) {
+            viewOrContainer = viewOrContainer[NEXT];
+        }
+        return viewOrContainer;
+    }
+    /**
      * Mark transplanted views as needing to be refreshed at their insertion points.
+     *
+     * See: `ActiveIndexFlag.HAS_TRANSPLANTED_VIEWS` and `LView[DECLARATION_COMPONENT_VIEW]` for
+     * explanation of transplanted views.
      *
      * @param lView The `LView` that may have transplanted views.
      */
     function markTransplantedViewsForRefresh(lView) {
         for (var lContainer = getFirstLContainer(lView); lContainer !== null; lContainer = getNextLContainer(lContainer)) {
-            if (!lContainer[HAS_TRANSPLANTED_VIEWS])
-                continue;
-            var movedViews = lContainer[MOVED_VIEWS];
-            ngDevMode && assertDefined(movedViews, 'Transplanted View flags set but missing MOVED_VIEWS');
-            for (var i = 0; i < movedViews.length; i++) {
-                var movedLView = movedViews[i];
-                var insertionLContainer = movedLView[PARENT];
-                ngDevMode && assertLContainer(insertionLContainer);
-                // We don't want to increment the counter if the moved LView was already marked for
-                // refresh.
-                if ((movedLView[FLAGS] & 1024 /* RefreshTransplantedView */) === 0) {
-                    updateTransplantedViewCount(insertionLContainer, 1);
+            if ((lContainer[ACTIVE_INDEX] & 1 /* HAS_TRANSPLANTED_VIEWS */) !== 0) {
+                var movedViews = lContainer[MOVED_VIEWS];
+                ngDevMode && assertDefined(movedViews, 'Transplanted View flags set but missing MOVED_VIEWS');
+                for (var i = 0; i < movedViews.length; i++) {
+                    var movedLView = movedViews[i];
+                    var insertionLContainer = movedLView[PARENT];
+                    ngDevMode && assertLContainer(insertionLContainer);
+                    // We don't want to increment the counter if the moved LView was already marked for
+                    // refresh.
+                    if ((movedLView[FLAGS] & 1024 /* RefreshTransplantedView */) === 0) {
+                        updateTransplantedViewCount(insertionLContainer, 1);
+                    }
+                    // Note, it is possible that the `movedViews` is tracking views that are transplanted *and*
+                    // those that aren't (declaration component === insertion component). In the latter case,
+                    // it's fine to add the flag, as we will clear it immediately in
+                    // `refreshDynamicEmbeddedViews` for the view currently being refreshed.
+                    movedLView[FLAGS] |= 1024 /* RefreshTransplantedView */;
                 }
-                // Note, it is possible that the `movedViews` is tracking views that are transplanted *and*
-                // those that aren't (declaration component === insertion component). In the latter case,
-                // it's fine to add the flag, as we will clear it immediately in
-                // `refreshDynamicEmbeddedViews` for the view currently being refreshed.
-                movedLView[FLAGS] |= 1024 /* RefreshTransplantedView */;
             }
         }
     }
@@ -40110,7 +40124,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.7+14.sha-2cacab9');
+    var VERSION$2 = new Version$1('10.0.0-next.7+10.sha-ce39755');
 
     /**
      * @license
