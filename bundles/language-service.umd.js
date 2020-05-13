e3be4849d58c54ea4ccca74ce5a7cad07e3b2c96
Misko Hevery <misko@hevery.com>
2020-05-13 23:10:15 +0000

    255c2b2ff0 docs: refactor lifecycle hooks doc topic for task orientation (#36353)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7
+ * @license Angular v10.0.0-next.7+1.sha-255c2b2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19606,7 +19606,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7');
+    var VERSION$1 = new Version('10.0.0-next.7+1.sha-255c2b2');
 
     /**
      * @license
@@ -49275,7 +49275,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.7');
+    var VERSION$2 = new Version$1('10.0.0-next.7+1.sha-255c2b2');
 
     /**
      * @license
