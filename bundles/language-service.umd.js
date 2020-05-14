16ad15fac8eff647b36716cd9713bfa2f804ccd2
Kara Erickson <karakara@google.com>
2020-05-14 19:22:26 +0000

    77c0ef38be docs(router): filter the event to subscribe (#37027)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+46.sha-5f64e52
+ * @license Angular v10.0.0-next.7+47.sha-77c0ef3
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+46.sha-5f64e52');
+    var VERSION$1 = new Version('10.0.0-next.7+47.sha-77c0ef3');
 
     /**
      * @license
@@ -40110,7 +40110,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.7+46.sha-5f64e52');
+    var VERSION$2 = new Version$1('10.0.0-next.7+47.sha-77c0ef3');
 
     /**
      * @license
