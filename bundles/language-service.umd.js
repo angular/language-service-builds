c8d29f61b89d59db2905a8633b7794a51954e9a9
Kara Erickson <karakara@google.com>
2020-05-14 18:07:07 +0000

    2418c6aca1 refactor(docs-infra): refactors `addImageDimensionsImpl` (#37046)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+16.sha-9020507
+ * @license Angular v10.0.0-next.7+17.sha-2418c6a
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+16.sha-9020507');
+    var VERSION$1 = new Version('10.0.0-next.7+17.sha-2418c6a');
 
     /**
      * @license
@@ -40110,7 +40110,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.7+16.sha-9020507');
+    var VERSION$2 = new Version$1('10.0.0-next.7+17.sha-2418c6a');
 
     /**
      * @license
