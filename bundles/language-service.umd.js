c55052aed131d8b3b8fb68d4717f3c26ae846697
Kara Erickson <karakara@google.com>
2020-05-15 17:34:40 +0000

    5ffa4f12e5 refactor(docs-infra): refactors `checkUnbalancedBackTicks` (#37065)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+41.sha-d808333
+ * @license Angular v9.1.6+47.sha-5ffa4f1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19601,7 +19601,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.6+41.sha-d808333');
+    var VERSION$1 = new Version('9.1.6+47.sha-5ffa4f1');
 
     /**
      * @license
@@ -40131,7 +40131,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.6+41.sha-d808333');
+    var VERSION$2 = new Version$1('9.1.6+47.sha-5ffa4f1');
 
     /**
      * @license
