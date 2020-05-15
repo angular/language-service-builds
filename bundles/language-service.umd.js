1795403b79ff82c7f818e66a09198a48e44af579
Kara Erickson <karakara@google.com>
2020-05-15 00:59:07 +0000

    42a9e5ad7d ci: increase payload size limits for integration tests (#37123)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+53.sha-aaa89bb
+ * @license Angular v10.0.0-next.7+55.sha-42a9e5a
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+53.sha-aaa89bb');
+    var VERSION$1 = new Version('10.0.0-next.7+55.sha-42a9e5a');
 
     /**
      * @license
@@ -36554,8 +36554,10 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         parent, //
         projection, //
         styles, //
+        stylesWithoutHost, //
         residualStyles, //
         classes, //
+        classesWithoutHost, //
         residualClasses, //
         classBindings, //
         styleBindings) {
@@ -36583,8 +36585,10 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             this.parent = parent;
             this.projection = projection;
             this.styles = styles;
+            this.stylesWithoutHost = stylesWithoutHost;
             this.residualStyles = residualStyles;
             this.classes = classes;
+            this.classesWithoutHost = classesWithoutHost;
             this.residualClasses = residualClasses;
             this.classBindings = classBindings;
             this.styleBindings = styleBindings;
@@ -37946,8 +37950,10 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         tParent, // parent: TElementNode|TContainerNode|null
         null, // projection: number|(ITNode|RNode[])[]|null
         null, // styles: string|null
+        null, // stylesWithoutHost: string|null
         undefined, // residualStyles: string|null
         null, // classes: string|null
+        null, // classesWithoutHost: string|null
         undefined, // residualClasses: string|null
         0, // classBindings: TStylingRange;
         0) :
@@ -37975,8 +37981,10 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 parent: tParent,
                 projection: null,
                 styles: null,
+                stylesWithoutHost: null,
                 residualStyles: undefined,
                 classes: null,
+                classesWithoutHost: null,
                 residualClasses: undefined,
                 classBindings: 0,
                 styleBindings: 0,
@@ -42902,29 +42910,34 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      *
      * @param tNode The `TNode` into which the styling information should be loaded.
      * @param attrs `TAttributes` containing the styling information.
+     * @param writeToHost Where should the resulting static styles be written?
+     *   - `false` Write to `TNode.stylesWithoutHost` / `TNode.classesWithoutHost`
+     *   - `true` Write to `TNode.styles` / `TNode.classes`
      */
-    function computeStaticStyling(tNode, attrs) {
+    function computeStaticStyling(tNode, attrs, writeToHost) {
         ngDevMode &&
             assertFirstCreatePass(getTView(), 'Expecting to be called in first template pass only');
-        var styles = tNode.styles;
-        var classes = tNode.classes;
+        var styles = writeToHost ? tNode.styles : null;
+        var classes = writeToHost ? tNode.classes : null;
         var mode = 0;
-        for (var i = 0; i < attrs.length; i++) {
-            var value = attrs[i];
-            if (typeof value === 'number') {
-                mode = value;
-            }
-            else if (mode == 1 /* Classes */) {
-                classes = concatStringsWithSpace(classes, value);
-            }
-            else if (mode == 2 /* Styles */) {
-                var style = value;
-                var styleValue = attrs[++i];
-                styles = concatStringsWithSpace(styles, style + ': ' + styleValue + ';');
+        if (attrs !== null) {
+            for (var i = 0; i < attrs.length; i++) {
+                var value = attrs[i];
+                if (typeof value === 'number') {
+                    mode = value;
+                }
+                else if (mode == 1 /* Classes */) {
+                    classes = concatStringsWithSpace(classes, value);
+                }
+                else if (mode == 2 /* Styles */) {
+                    var style = value;
+                    var styleValue = attrs[++i];
+                    styles = concatStringsWithSpace(styles, style + ': ' + styleValue + ';');
+                }
             }
         }
-        styles !== null && (tNode.styles = styles);
-        classes !== null && (tNode.classes = classes);
+        writeToHost ? tNode.styles = styles : tNode.stylesWithoutHost = styles;
+        writeToHost ? tNode.classes = classes : tNode.classesWithoutHost = classes;
     }
 
     /**
@@ -43934,8 +43947,11 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var tNode = getOrCreateTNode(tView, lView[T_HOST], index, 3 /* Element */, name, attrs);
         var hasDirectives = resolveDirectives(tView, lView, tNode, getConstant(tViewConsts, localRefsIndex));
         ngDevMode && logUnknownElementError(tView, lView, native, tNode, hasDirectives);
+        if (tNode.attrs !== null) {
+            computeStaticStyling(tNode, tNode.attrs, false);
+        }
         if (tNode.mergedAttrs !== null) {
-            computeStaticStyling(tNode, tNode.mergedAttrs);
+            computeStaticStyling(tNode, tNode.mergedAttrs, true);
         }
         if (tView.queries !== null) {
             tView.queries.elementStart(tView, tNode);
@@ -44024,11 +44040,11 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 tView.queries.elementEnd(previousOrParentTNode);
             }
         }
-        if (tNode.classes !== null && hasClassInput(tNode)) {
-            setDirectiveInputsWhichShadowsStyling(tView, tNode, getLView(), tNode.classes, true);
+        if (tNode.classesWithoutHost != null && hasClassInput(tNode)) {
+            setDirectiveInputsWhichShadowsStyling(tView, tNode, getLView(), tNode.classesWithoutHost, true);
         }
-        if (tNode.styles !== null && hasStyleInput(tNode)) {
-            setDirectiveInputsWhichShadowsStyling(tView, tNode, getLView(), tNode.styles, false);
+        if (tNode.stylesWithoutHost != null && hasStyleInput(tNode)) {
+            setDirectiveInputsWhichShadowsStyling(tView, tNode, getLView(), tNode.stylesWithoutHost, false);
         }
     }
     /**
@@ -44096,7 +44112,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         // While ng-container doesn't necessarily support styling, we use the style context to identify
         // and execute directives on the ng-container.
         if (attrs !== null) {
-            computeStaticStyling(tNode, attrs);
+            computeStaticStyling(tNode, attrs, true);
         }
         var localRefs = getConstant(tViewConsts, localRefsIndex);
         resolveDirectives(tView, lView, tNode, localRefs);
@@ -45786,7 +45802,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 // the binding has removed it. This would confuse `[ngStyle]`/`[ngClass]` to do the wrong
                 // thing as it would think that the static portion was removed. For this reason we
                 // concatenate it so that `[ngStyle]`/`[ngClass]`  can continue to work on changed.
-                var staticPrefix = isClassBased ? tNode.classes : tNode.styles;
+                var staticPrefix = isClassBased ? tNode.classesWithoutHost : tNode.stylesWithoutHost;
                 ngDevMode && isClassBased === false && staticPrefix !== null &&
                     assertEqual(staticPrefix.endsWith(';'), true, 'Expecting static portion to end with \';\'');
                 if (staticPrefix !== null) {
@@ -48157,7 +48173,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
         var tNode = getOrCreateTNode(tView, null, 0, 3 /* Element */, null, null);
         var mergedAttrs = tNode.mergedAttrs = def.hostAttrs;
         if (mergedAttrs !== null) {
-            computeStaticStyling(tNode, mergedAttrs);
+            computeStaticStyling(tNode, mergedAttrs, true);
             if (rNode !== null) {
                 setUpAttributes(hostRenderer, rNode, mergedAttrs);
                 if (tNode.classes !== null) {
@@ -49136,7 +49152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.7+53.sha-aaa89bb');
+    var VERSION$2 = new Version$1('10.0.0-next.7+55.sha-42a9e5a');
 
     /**
      * @license
