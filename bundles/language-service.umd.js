48be9a947a387a56ea923120d53626f3a6a52d7f
Kara Erickson <karakara@google.com>
2020-05-15 18:50:04 +0000

    3d7c85b2aa feat(dev-infra): create tool to determine conflicts created by a PR (#37051)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+62.sha-55c2433
+ * @license Angular v10.0.0-next.7+63.sha-3d7c85b2
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+62.sha-55c2433');
+    var VERSION$1 = new Version('10.0.0-next.7+63.sha-3d7c85b2');
 
     /**
      * @license
@@ -40119,7 +40119,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.7+62.sha-55c2433');
+    var VERSION$2 = new Version$1('10.0.0-next.7+63.sha-3d7c85b2');
 
     /**
      * @license
