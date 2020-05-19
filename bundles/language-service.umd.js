6bc77fde17b160c63963305eed5b5ac32747dd89
Kara Erickson <karakara@google.com>
2020-05-19 22:56:13 +0000

    362fba4bde fix(dev-infra): make commit-message-filter script executable for merge (#37209)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+30.sha-211d33a
+ * @license Angular v10.0.0-next.8+31.sha-362fba4
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.8+30.sha-211d33a');
+    var VERSION$1 = new Version('10.0.0-next.8+31.sha-362fba4');
 
     /**
      * @license
@@ -40119,7 +40119,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.8+30.sha-211d33a');
+    var VERSION$2 = new Version$1('10.0.0-next.8+31.sha-362fba4');
 
     /**
      * @license
