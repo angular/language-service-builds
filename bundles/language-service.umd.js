e121deecd69cfd3c8b240bce9ad4a7454a99ff4d
Kara Erickson <karakara@google.com>
2020-05-19 22:44:25 +0000

    383f04b96d feat(dev-infra): register ts-node when reading configuration (#37196)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+27.sha-772c5b8
+ * @license Angular v10.0.0-next.8+29.sha-383f04b
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.8+27.sha-772c5b8');
+    var VERSION$1 = new Version('10.0.0-next.8+29.sha-383f04b');
 
     /**
      * @license
@@ -49152,7 +49152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.8+27.sha-772c5b8');
+    var VERSION$2 = new Version$1('10.0.0-next.8+29.sha-383f04b');
 
     /**
      * @license
