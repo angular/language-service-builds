8a06d7aa67f593b44b7c9a9cffd58660102370f7
Kara Erickson <karakara@google.com>
2020-05-20 18:31:49 +0000

    19313f7dad Revert "fix(elements): correctly handle getting/setting properties before connecting the element (#36114)"

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+80.sha-9ae3e11
+ * @license Angular v9.1.8+7.sha-19313f7
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19601,7 +19601,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.6+80.sha-9ae3e11');
+    var VERSION$1 = new Version('9.1.8+7.sha-19313f7');
 
     /**
      * @license
@@ -40127,7 +40127,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.6+80.sha-9ae3e11');
+    var VERSION$2 = new Version$1('9.1.8+7.sha-19313f7');
 
     /**
      * @license
