715717a9e7c7503f4a393225090238ce8eb882bf
Kara Erickson <karakara@google.com>
2020-05-20 22:11:51 +0000

    1465372a0e fix(elements): do not break when the constructor of an Angular Element is not called (#36114) (#37226)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+12.sha-3eee536
+ * @license Angular v9.1.8+19.sha-1465372
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19601,7 +19601,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.8+12.sha-3eee536');
+    var VERSION$1 = new Version('9.1.8+19.sha-1465372');
 
     /**
      * @license
@@ -40127,7 +40127,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.8+12.sha-3eee536');
+    var VERSION$2 = new Version$1('9.1.8+19.sha-1465372');
 
     /**
      * @license
