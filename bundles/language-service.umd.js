6c246e3f749392d104b72143714c5b9f9a1a9d05
Kara Erickson <karakara@google.com>
2020-05-20 17:58:42 +0000

    aedac71491 docs: release notes for the v9.1.8 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+11.sha-89b44d1
+ * @license Angular v10.0.0-next.9+12.sha-aedac71
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.9+11.sha-89b44d1');
+    var VERSION$1 = new Version('10.0.0-next.9+12.sha-aedac71');
 
     /**
      * @license
@@ -49152,7 +49152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-next.9+11.sha-89b44d1');
+    var VERSION$2 = new Version$1('10.0.0-next.9+12.sha-aedac71');
 
     /**
      * @license
