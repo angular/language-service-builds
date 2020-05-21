2016e08329321755f528f8ae307643f33e825403
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:59:49 +0000

    540c29cd6b feat(dev-infra): migrate release tool to use new logging system (#37232)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+3.sha-93be1f1
+ * @license Angular v10.0.0-rc.0+14.sha-540c29c
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+3.sha-93be1f1');
+    var VERSION$1 = new Version('10.0.0-rc.0+14.sha-540c29c');
 
     /**
      * @license
@@ -40119,7 +40119,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+3.sha-93be1f1');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+14.sha-540c29c');
 
     /**
      * @license
