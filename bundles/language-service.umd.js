1afe3fb055a751fd9f100dec3a63a9ba553b3dae
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:42:00 +0000

    93be1f1543 build: use static patch value for targetting branches in merge config (#37245)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+4.sha-87b1aea
+ * @license Angular v10.0.0-rc.0+3.sha-93be1f1
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+4.sha-87b1aea');
+    var VERSION$1 = new Version('10.0.0-rc.0+3.sha-93be1f1');
 
     /**
      * @license
@@ -49152,7 +49152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+4.sha-87b1aea');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+3.sha-93be1f1');
 
     /**
      * @license
