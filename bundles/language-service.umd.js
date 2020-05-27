7abb781b1a66a622522155e6abcfd624f68fb516
Matias Niemelä <matias@yearofmoo.com>
2020-05-27 21:46:42 +0000

    64fc8b8ee5 docs: add IE mobile to deprecated browsers (#37313)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+19.sha-a62c1c4
+ * @license Angular v10.0.0-rc.0+20.sha-64fc8b8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+19.sha-a62c1c4');
+    var VERSION$1 = new Version('10.0.0-rc.0+20.sha-64fc8b8');
 
     /**
      * @license
@@ -40119,7 +40119,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+19.sha-a62c1c4');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+20.sha-64fc8b8');
 
     /**
      * @license
