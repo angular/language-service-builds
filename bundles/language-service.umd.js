be8f2f34942d4e9c574b3370e8b7f3d926355f7d
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 20:26:58 +0000

    98d820030c docs(platform-server): fix renderModule usage guidance with Ivy (#37296)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+20.sha-64fc8b8
+ * @license Angular v10.0.0-rc.0+22.sha-98d8200
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+20.sha-64fc8b8');
+    var VERSION$1 = new Version('10.0.0-rc.0+22.sha-98d8200');
 
     /**
      * @license
@@ -40119,7 +40119,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+20.sha-64fc8b8');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+22.sha-98d8200');
 
     /**
      * @license
