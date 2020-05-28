27b5bc94a9b737198025d99fc2cff17631e44cfc
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 23:09:55 +0000

    df082307b9 docs: fix typo in committer.md (#37171)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+21.sha-f001aef
+ * @license Angular v10.0.0-rc.0+23.sha-df08230
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+21.sha-f001aef');
+    var VERSION$1 = new Version('10.0.0-rc.0+23.sha-df08230');
 
     /**
      * @license
@@ -49152,7 +49152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+21.sha-f001aef');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+23.sha-df08230');
 
     /**
      * @license
