ed21e790dab6e59ef1673e5ade980894b703cf8d
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 01:53:24 +0000

    82761ec50e docs: Mention Bazel builder and schematics in Deprecations section (#37190)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+11.sha-270da1f
+ * @license Angular v10.0.0-rc.0+22.sha-82761ec
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19598,7 +19598,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+11.sha-270da1f');
+    var VERSION$1 = new Version('10.0.0-rc.0+22.sha-82761ec');
 
     /**
      * @license
@@ -40119,7 +40119,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+11.sha-270da1f');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+22.sha-82761ec');
 
     /**
      * @license
