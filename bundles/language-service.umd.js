47ea573b719f2dc2daa4d5e00fba6b586066d285
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 18:43:44 +0000

    cf86f72eb7 release: cut the v10.0.0-rc.2 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+23.sha-d16a7f3
+ * @license Angular v10.0.0-rc.2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+23.sha-d16a7f3');
+    var VERSION$1 = new Version('10.0.0-rc.2');
 
     /**
      * @license
@@ -40104,7 +40104,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+23.sha-d16a7f3');
+    var VERSION$2 = new Version$1('10.0.0-rc.2');
 
     /**
      * @license
