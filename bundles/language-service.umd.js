d9ed91d734f083c1b6790f1c2e9d8c368dcfd0f5
atscott <atscott@google.com>
2020-06-03 22:50:15 +0000

    e35269dd87 docs: update file header to be correct (#37425)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+21.sha-60a03b7
+ * @license Angular v10.0.0-rc.2+22.sha-e35269d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+21.sha-60a03b7');
+    var VERSION$1 = new Version('10.0.0-rc.2+22.sha-e35269d');
 
     /**
      * @license
@@ -40092,7 +40092,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.2+21.sha-60a03b7');
+    var VERSION$2 = new Version$1('10.0.0-rc.2+22.sha-e35269d');
 
     /**
      * @license
