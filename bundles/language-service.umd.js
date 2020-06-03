a11867220f12690ed8cde614093eab7623fe759a
atscott <atscott@google.com>
2020-06-03 20:45:49 +0000

    965a688c97 fix(compiler-cli): use ModuleWithProviders type if static eval fails (#37126)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
+ * @license Angular v10.0.0-rc.0+56.sha-965a688
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+55.sha-ab3f4c9');
+    var VERSION$1 = new Version('10.0.0-rc.0+56.sha-965a688');
 
     /**
      * @license
@@ -40092,7 +40092,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+55.sha-ab3f4c9');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+56.sha-965a688');
 
     /**
      * @license
