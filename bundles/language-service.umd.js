c13b629c55436f48e79923b535a39460dcb787d2
atscott <atscott@google.com>
2020-06-03 23:13:19 +0000

    34827559b9 docs: update the stackblitz in the GitHub Issue template (#37219)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+58.sha-65c3888
+ * @license Angular v10.0.0-rc.0+59.sha-3482755
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+58.sha-65c3888');
+    var VERSION$1 = new Version('10.0.0-rc.0+59.sha-3482755');
 
     /**
      * @license
@@ -40092,7 +40092,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+58.sha-65c3888');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+59.sha-3482755');
 
     /**
      * @license
