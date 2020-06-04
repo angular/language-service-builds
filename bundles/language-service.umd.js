f7cbd6d291eae6866ca458ebb1bc350a8aecd4c9
atscott <atscott@google.com>
2020-06-04 16:54:31 +0000

    5f0be3cb2e feat(dev-infra): Add oauth scope check to ensure necessary permissions for merge tooling (#37421)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+24.sha-2c1f35e
+ * @license Angular v10.0.0-rc.2+43.sha-5f0be3c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+24.sha-2c1f35e');
+    var VERSION$1 = new Version('10.0.0-rc.2+43.sha-5f0be3c');
 
     /**
      * @license
@@ -40092,7 +40092,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.2+24.sha-2c1f35e');
+    var VERSION$2 = new Version$1('10.0.0-rc.2+43.sha-5f0be3c');
 
     /**
      * @license
