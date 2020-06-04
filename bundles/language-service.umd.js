503c6624e5cab9b58d5da08d64ab1c185615b7e7
atscott <atscott@google.com>
2020-06-04 16:40:02 +0000

    2021ad12cf ci(docs-infra): skip deploying RC version when lexicographically smaller than stable (#37426)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
+ * @license Angular v10.0.0-rc.0+61.sha-2021ad1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+62.sha-6e7bd93');
+    var VERSION$1 = new Version('10.0.0-rc.0+61.sha-2021ad1');
 
     /**
      * @license
@@ -40092,7 +40092,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+62.sha-6e7bd93');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+61.sha-2021ad1');
 
     /**
      * @license
