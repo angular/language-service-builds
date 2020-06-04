0bab0aa8dfe6c2af971b6dd0a114168b6ef1f68a
atscott <atscott@google.com>
2020-06-04 20:07:21 +0000

    dffcca73e4 fix(dev-infra): clean up usages within pullapprove tooling (#37338)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+69.sha-b2bd386
+ * @license Angular v10.0.0-rc.0+71.sha-dffcca7
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+69.sha-b2bd386');
+    var VERSION$1 = new Version('10.0.0-rc.0+71.sha-dffcca7');
 
     /**
      * @license
@@ -49150,7 +49150,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+69.sha-b2bd386');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+71.sha-dffcca7');
 
     /**
      * @license
