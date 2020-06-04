9ae7b5d058c99639bcdd966a38d0aaeffbb5f8df
atscott <atscott@google.com>
2020-06-04 16:39:50 +0000

    6e7bd939f6 perf(ngcc): cache parsed tsconfig between runs (#37417)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+60.sha-c4f4675
+ * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+60.sha-c4f4675');
+    var VERSION$1 = new Version('10.0.0-rc.0+62.sha-6e7bd93');
 
     /**
      * @license
@@ -49150,7 +49150,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+60.sha-c4f4675');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+62.sha-6e7bd93');
 
     /**
      * @license
