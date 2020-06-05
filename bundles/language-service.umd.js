75bf0217b3ee385a1e424fd5cb81e548fad92986
atscott <atscott@google.com>
2020-06-05 18:29:37 +0000

    17996bf719 fix(dev-infra): properly determine oauth scopes for git client token (#37462)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+79.sha-85b6c94
+ * @license Angular v10.0.0-rc.0+82.sha-17996bf
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+79.sha-85b6c94');
+    var VERSION$1 = new Version('10.0.0-rc.0+82.sha-17996bf');
 
     /**
      * @license
@@ -49150,7 +49150,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+79.sha-85b6c94');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+82.sha-17996bf');
 
     /**
      * @license
