6be04d8aa5153f6de8cab6c467db6d3c73bb4a83
atscott <atscott@google.com>
2020-06-06 02:48:54 +0000

    75294e7dad ci: special case tooling-cli-shared-api review group (#37467)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+57.sha-2349143
+ * @license Angular v10.0.0-rc.2+59.sha-75294e7
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+57.sha-2349143');
+    var VERSION$1 = new Version('10.0.0-rc.2+59.sha-75294e7');
 
     /**
      * @license
@@ -40092,7 +40092,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.2+57.sha-2349143');
+    var VERSION$2 = new Version$1('10.0.0-rc.2+59.sha-75294e7');
 
     /**
      * @license
