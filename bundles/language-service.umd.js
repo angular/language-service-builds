8a5f6e3bda801445a3b21531a38abd6ed6ce81b0
atscott <atscott@google.com>
2020-06-08 17:34:26 +0000

    ed3c549063 ci: remove IgorMinar from reviewers list for pullapprove fallback group (#36456)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+90.sha-3a11617
+ * @license Angular v10.0.0-rc.0+91.sha-ed3c549
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+90.sha-3a11617');
+    var VERSION$1 = new Version('10.0.0-rc.0+91.sha-ed3c549');
 
     /**
      * @license
@@ -49150,7 +49150,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+90.sha-3a11617');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+91.sha-ed3c549');
 
     /**
      * @license
