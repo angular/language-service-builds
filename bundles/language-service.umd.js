eca3d0df8942855b684e5e0a9e0e1ddff3b35770
atscott <atscott@google.com>
2020-06-08 22:24:55 +0000

    87fdd23157 docs: add note on publishing libraries in ivy (#36556)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+96.sha-713fb3b
+ * @license Angular v10.0.0-rc.0+97.sha-87fdd23
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+96.sha-713fb3b');
+    var VERSION$1 = new Version('10.0.0-rc.0+97.sha-87fdd23');
 
     /**
      * @license
@@ -40098,7 +40098,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+96.sha-713fb3b');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+97.sha-87fdd23');
 
     /**
      * @license
