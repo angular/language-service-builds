4e7015a65e1d8a6a003ce2911bc95660c31d6d33
atscott <atscott@google.com>
2020-06-09 20:55:04 +0000

    d494f7bd5e docs(dev-infra): add comment about what the requiredBaseCommit is (#37509)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+11.sha-ec6a7ab
+ * @license Angular v10.0.0-rc.3+12.sha-d494f7b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.3+11.sha-ec6a7ab');
+    var VERSION$1 = new Version('10.0.0-rc.3+12.sha-d494f7b');
 
     /**
      * @license
@@ -40132,7 +40132,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.3+11.sha-ec6a7ab');
+    var VERSION$2 = new Version$1('10.0.0-rc.3+12.sha-d494f7b');
 
     /**
      * @license
