1ee1ec54970c5f28f7fb4c233081696798dc1be9
atscott <atscott@google.com>
2020-06-09 00:34:23 +0000

    8d817daf78 fix(router): Fix relative link generation from empty path components (#37446)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+97.sha-87fdd23
+ * @license Angular v10.0.0-rc.0+99.sha-8d817da
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+97.sha-87fdd23');
+    var VERSION$1 = new Version('10.0.0-rc.0+99.sha-8d817da');
 
     /**
      * @license
@@ -40098,7 +40098,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+97.sha-87fdd23');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+99.sha-8d817da');
 
     /**
      * @license
