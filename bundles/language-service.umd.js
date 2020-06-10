c220ed9a952538db14ea4571f6a60bad167fceda
atscott <atscott@google.com>
2020-06-10 20:22:07 +0000

    20346ff192 build: remove ngcontainer Docker (#36421)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+123.sha-bd9c1e6
+ * @license Angular v10.0.0-rc.0+124.sha-20346ff
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+123.sha-bd9c1e6');
+    var VERSION$1 = new Version('10.0.0-rc.0+124.sha-20346ff');
 
     /**
      * @license
@@ -49166,7 +49166,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+123.sha-bd9c1e6');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+124.sha-20346ff');
 
     /**
      * @license
