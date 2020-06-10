e0e95828ff469885813f7b785c8fee137f74e7cf
atscott <atscott@google.com>
2020-06-10 19:12:41 +0000

    ae364864f6 fix(ngcc): do not scan import expressions in d.ts files (#37503)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+122.sha-d76a872
+ * @license Angular v10.0.0-rc.0+119.sha-ae36486
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+122.sha-d76a872');
+    var VERSION$1 = new Version('10.0.0-rc.0+119.sha-ae36486');
 
     /**
      * @license
@@ -40099,7 +40099,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+122.sha-d76a872');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+119.sha-ae36486');
 
     /**
      * @license
