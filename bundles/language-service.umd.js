6cacf4af5ae72523f8c7e6c7ed18bf974e2d6468
atscott <atscott@google.com>
2020-06-10 19:24:40 +0000

    bd9c1e6cfc build: add commit message scope for migration changes (#36390)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+120.sha-e836eaf
+ * @license Angular v10.0.0-rc.0+123.sha-bd9c1e6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+120.sha-e836eaf');
+    var VERSION$1 = new Version('10.0.0-rc.0+123.sha-bd9c1e6');
 
     /**
      * @license
@@ -40099,7 +40099,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+120.sha-e836eaf');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+123.sha-bd9c1e6');
 
     /**
      * @license
