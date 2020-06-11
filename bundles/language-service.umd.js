22b403e38dc6623f5bb4b6a7942c72ce0f47672c
Misko Hevery <misko@hevery.com>
2020-06-11 19:30:44 +0000

    38c48beddd refactor(elements): add accessor workaround for build-optimizer (#37456)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+6.sha-c2f4a9b
+ * @license Angular v10.0.0-rc.4+14.sha-38c48be
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.4+6.sha-c2f4a9b');
+    var VERSION$1 = new Version('10.0.0-rc.4+14.sha-38c48be');
 
     /**
      * @license
@@ -40099,7 +40099,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.4+6.sha-c2f4a9b');
+    var VERSION$2 = new Version$1('10.0.0-rc.4+14.sha-38c48be');
 
     /**
      * @license
