02f8eea6320087c6b9c8448a83aa881e784acfeb
Misko Hevery <misko@hevery.com>
2020-06-12 22:28:49 +0000

    1bc807c81a style(common): enforce format on newly included files (#36940)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+161.sha-8dcf618
+ * @license Angular v10.0.0-rc.0+164.sha-1bc807c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+161.sha-8dcf618');
+    var VERSION$1 = new Version('10.0.0-rc.0+164.sha-1bc807c');
 
     /**
      * @license
@@ -49166,7 +49166,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+161.sha-8dcf618');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+164.sha-1bc807c');
 
     /**
      * @license
