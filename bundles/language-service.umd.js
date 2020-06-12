24c37e17d5c4b6ca37b37e9525a4181fb86581cc
Misko Hevery <misko@hevery.com>
2020-06-12 16:10:42 +0000

    aba33de5f5 build: fix outdated payload size (#37551)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+159.sha-a075260
+ * @license Angular v10.0.0-rc.0+158.sha-aba33de
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+159.sha-a075260');
+    var VERSION$1 = new Version('10.0.0-rc.0+158.sha-aba33de');
 
     /**
      * @license
@@ -40099,7 +40099,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+159.sha-a075260');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+158.sha-aba33de');
 
     /**
      * @license
