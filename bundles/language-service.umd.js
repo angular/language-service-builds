f4052efe968f9c0e49af8d69085b1a4beac5d0f5
Misko Hevery <misko@hevery.com>
2020-06-15 21:54:48 +0000

    2a145f2463 perf(forms): optimize internal method _anyControls in FormGroup (#32534)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+184.sha-e0dfa42
+ * @license Angular v10.0.0-rc.0+185.sha-2a145f2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+184.sha-e0dfa42');
+    var VERSION$1 = new Version('10.0.0-rc.0+185.sha-2a145f2');
 
     /**
      * @license
@@ -49175,7 +49175,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+184.sha-e0dfa42');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+185.sha-2a145f2');
 
     /**
      * @license
