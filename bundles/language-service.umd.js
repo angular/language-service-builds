f8a47fd6d1885d169d626374758e8c2e49c640b8
Misko Hevery <misko@hevery.com>
2020-06-15 16:27:44 +0000

    c1fdfd4f14 docs: update CONTRIBUTING.md with info about major docs changes (#35238)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+167.sha-1091ddb
+ * @license Angular v10.0.0-rc.0+169.sha-c1fdfd4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+167.sha-1091ddb');
+    var VERSION$1 = new Version('10.0.0-rc.0+169.sha-c1fdfd4');
 
     /**
      * @license
@@ -49166,7 +49166,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+167.sha-1091ddb');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+169.sha-c1fdfd4');
 
     /**
      * @license
