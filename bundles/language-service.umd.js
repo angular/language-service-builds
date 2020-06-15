c09394eaa5e744848eff57bfa0e42ab1370a1ea9
Misko Hevery <misko@hevery.com>
2020-06-15 17:15:18 +0000

    300c2fec9c refactor(compiler-cli): make IncrementalBuild strategy configurable (#37339)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+177.sha-a7faa6b
+ * @license Angular v10.0.0-rc.0+178.sha-300c2fe
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+177.sha-a7faa6b');
+    var VERSION$1 = new Version('10.0.0-rc.0+178.sha-300c2fe');
 
     /**
      * @license
@@ -40108,7 +40108,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+177.sha-a7faa6b');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+178.sha-300c2fe');
 
     /**
      * @license
