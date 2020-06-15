7cb703acdfed831da8fc12e364359b4c86683dc2
Misko Hevery <misko@hevery.com>
2020-06-15 21:54:36 +0000

    e0dfa42d6e ci: include PR author as an approver of all PRs (#36915)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+181.sha-a7359d4
+ * @license Angular v10.0.0-rc.0+184.sha-e0dfa42
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+181.sha-a7359d4');
+    var VERSION$1 = new Version('10.0.0-rc.0+184.sha-e0dfa42');
 
     /**
      * @license
@@ -49175,7 +49175,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+181.sha-a7359d4');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+184.sha-e0dfa42');
 
     /**
      * @license
