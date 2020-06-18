805992f584e84c35a2ae1b656fe7061ab3c0ce83
Misko Hevery <misko@hevery.com>
2020-06-18 23:30:01 +0000

    1d844b9bd8 docs(core): correct type for opts.read (#37626)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+195.sha-53e95233
+ * @license Angular v10.0.0-rc.0+196.sha-1d844b9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+195.sha-53e95233');
+    var VERSION$1 = new Version('10.0.0-rc.0+196.sha-1d844b9');
 
     /**
      * @license
@@ -49175,7 +49175,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+195.sha-53e95233');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+196.sha-1d844b9');
 
     /**
      * @license
