49172a717dc3a58e575d78bd7476955d8dc58e55
Misko Hevery <misko@hevery.com>
2020-06-18 23:27:29 +0000

    bd7f440357 perf(ngcc): shortcircuit tokenizing in ESM dependency host (#37639)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+193.sha-71f008f
+ * @license Angular v10.0.0-rc.0+198.sha-bd7f440
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+193.sha-71f008f');
+    var VERSION$1 = new Version('10.0.0-rc.0+198.sha-bd7f440');
 
     /**
      * @license
@@ -49175,7 +49175,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.0-rc.0+193.sha-71f008f');
+    var VERSION$2 = new Version$1('10.0.0-rc.0+198.sha-bd7f440');
 
     /**
      * @license
