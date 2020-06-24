a65a66fadfc2bd864c3558545ff5b68c27d88fb6
Misko Hevery <misko@hevery.com>
2020-06-24 23:33:16 +0000

    41ce5240f2 docs: release notes for the v10.0.0 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+225.sha-243824e
+ * @license Angular v10.0.0-rc.0+226.sha-41ce524
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17616,7 +17616,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+225.sha-243824e');
+    const VERSION$1 = new Version('10.0.0-rc.0+226.sha-41ce524');
 
     /**
      * @license
@@ -43777,7 +43777,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+225.sha-243824e');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+226.sha-41ce524');
 
     /**
      * @license
