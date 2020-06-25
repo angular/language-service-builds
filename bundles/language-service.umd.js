5b3d5b9e27fa99efba0aaa0769633fe587555afa
Igor Minar <igor@angular.io>
2020-06-25 22:57:46 +0000

    20c44a6240 fix(docs-infra): fix typo in the deploy-to-firebase.sh script

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+3.sha-0feb560
+ * @license Angular v9.1.11+4.sha-20c44a6
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19601,7 +19601,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.11+3.sha-0feb560');
+    var VERSION$1 = new Version('9.1.11+4.sha-20c44a6');
 
     /**
      * @license
@@ -40127,7 +40127,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.11+3.sha-0feb560');
+    var VERSION$2 = new Version$1('9.1.11+4.sha-20c44a6');
 
     /**
      * @license
