0e1bbc55854ce6c03206759515c99301db959471
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:02:25 +0000

    8924ec1474 test(docs-infra): add end to end tests for api reference (#37612)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+231.sha-bedc045
+ * @license Angular v10.0.0-rc.0+229.sha-8924ec1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17616,7 +17616,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+231.sha-bedc045');
+    const VERSION$1 = new Version('10.0.0-rc.0+229.sha-8924ec1');
 
     /**
      * @license
@@ -43777,7 +43777,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+231.sha-bedc045');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+229.sha-8924ec1');
 
     /**
      * @license
