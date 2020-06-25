e8e076f6e90e17492bdb89d2eef051c2fb60a581
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:34:46 +0000

    5103d908c8 perf(compiler-cli): fix regressions in incremental program reuse (#37641)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+255.sha-9318e23
+ * @license Angular v10.0.0-rc.0+256.sha-5103d90
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+255.sha-9318e23');
+    const VERSION$1 = new Version('10.0.0-rc.0+256.sha-5103d90');
 
     /**
      * @license
@@ -33376,7 +33376,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+255.sha-9318e23');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+256.sha-5103d90');
 
     /**
      * @license
