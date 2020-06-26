54ae25f46e8e887d7ec2189ee8f49268fea0938e
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:28:15 +0000

    9c668e091f docs(elements): fixed command that adds the package @angular/elements (#37681)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+265.sha-4c7f32f
+ * @license Angular v10.0.0-rc.0+268.sha-9c668e0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+265.sha-4c7f32f');
+    const VERSION$1 = new Version('10.0.0-rc.0+268.sha-9c668e0');
 
     /**
      * @license
@@ -33376,7 +33376,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+265.sha-4c7f32f');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+268.sha-9c668e0');
 
     /**
      * @license
