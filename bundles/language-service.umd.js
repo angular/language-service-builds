b49d55ad84654eef3c6841d5b87661702ecbbcce
Andrew Kushnir <akushnir@google.com>
2020-06-26 20:57:10 +0000

    8fd8143ab8 docs: release notes for the v10.0.1 release

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+278.sha-0879d2e
+ * @license Angular v10.0.0-rc.0+282.sha-8fd8143
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+278.sha-0879d2e');
+    const VERSION$1 = new Version('10.0.0-rc.0+282.sha-8fd8143');
 
     /**
      * @license
@@ -33380,7 +33380,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+278.sha-0879d2e');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+282.sha-8fd8143');
 
     /**
      * @license
