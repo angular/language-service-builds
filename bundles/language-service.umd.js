dec9ac0b409369e5fa5b8fdb14bb3a2e50beec87
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:19:07 +0000

    2038568f3e ci: decrease expected AIO and integration payload sizes (#36578) (#36578)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+283.sha-ae5257c
+ * @license Angular v10.0.0-rc.0+287.sha-2038568
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+283.sha-ae5257c');
+    const VERSION$1 = new Version('10.0.0-rc.0+287.sha-2038568');
 
     /**
      * @license
@@ -33381,7 +33381,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+283.sha-ae5257c');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+287.sha-2038568');
 
     /**
      * @license
