b5fa1bf8853c3a30b18fa2f524a6ec00f5fb43ab
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:01:32 +0000

    eba9dd364e fix(elements): fire custom element output events during component initialization (#37570)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+291.sha-78a4476
+ * @license Angular v10.0.0-rc.0+292.sha-eba9dd3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+291.sha-78a4476');
+    const VERSION$1 = new Version('10.0.0-rc.0+292.sha-eba9dd3');
 
     /**
      * @license
@@ -33389,7 +33389,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+291.sha-78a4476');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+292.sha-eba9dd3');
 
     /**
      * @license
