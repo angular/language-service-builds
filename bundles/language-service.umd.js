1cac819ffa87f76a77121bf87afd9db56712f564
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:44:28 +0000

    c29f830c4b docs: correct left nav toc to remove duplicated page links (#37827)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+295.sha-cde4653
+ * @license Angular v10.0.0-rc.0+296.sha-c29f830
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.0-rc.0+295.sha-cde4653');
+    const VERSION$1 = new Version('10.0.0-rc.0+296.sha-c29f830');
 
     /**
      * @license
@@ -33389,7 +33389,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.0-rc.0+295.sha-cde4653');
+    const VERSION$2 = new Version$1('10.0.0-rc.0+296.sha-c29f830');
 
     /**
      * @license
