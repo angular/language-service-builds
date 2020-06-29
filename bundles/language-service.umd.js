15cceb101ff5f01da0a79647cbe2fdffb8f2f94c
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:44:08 +0000

    7a91b23cb5 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19583,7 +19583,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.1+10.sha-89e16ed');
+    var VERSION$1 = new Version('10.0.1+16.sha-7a91b23');
 
     /**
      * @license
@@ -40121,7 +40121,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.1+10.sha-89e16ed');
+    var VERSION$2 = new Version$1('10.0.1+16.sha-7a91b23');
 
     /**
      * @license
