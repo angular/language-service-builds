cce3763aeb7f1cd04cb171b861b2023aa45633b6
atscott <atscott01@gmail.com>
2020-07-09 22:34:49 +0000

    b76a2dc2cb fix(bazel): ng_module rule does not expose flat module information in Ivy (#36971)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+18.sha-8ee23ba
+ * @license Angular v10.0.3+20.sha-b76a2dc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19590,7 +19590,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.3+18.sha-8ee23ba');
+    var VERSION$1 = new Version('10.0.3+20.sha-b76a2dc');
 
     /**
      * @license
@@ -40131,7 +40131,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('10.0.3+18.sha-8ee23ba');
+    var VERSION$2 = new Version$1('10.0.3+20.sha-b76a2dc');
 
     /**
      * @license
