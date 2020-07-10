00a23dd9bd99b0f98973809d8eb9e7f748586e3e
Andrew Scott <atscott01@gmail.com>
2020-07-10 22:30:46 +0000

    0d38288078 docs: Move router tutorial (toh) from router.md to new file (#37979)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+37.sha-2e9b953
+ * @license Angular v10.0.3+38.sha-0d38288
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.3+37.sha-2e9b953');
+    const VERSION$1 = new Version('10.0.3+38.sha-0d38288');
 
     /**
      * @license
@@ -33392,7 +33392,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.3+37.sha-2e9b953');
+    const VERSION$2 = new Version$1('10.0.3+38.sha-0d38288');
 
     /**
      * @license
