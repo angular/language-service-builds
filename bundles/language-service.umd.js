429913e5a906ec7b5b9470efda0e568bc09db82a
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:53:04 +0000

    2e9b953e9d docs(router): fix routerLink docs (#37997) The current content for the routerLink commands input does not make it to aio. fixes #35414

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+36.sha-a94383f
+ * @license Angular v10.0.3+37.sha-2e9b953
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17623,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.3+36.sha-a94383f');
+    const VERSION$1 = new Version('10.0.3+37.sha-2e9b953');
 
     /**
      * @license
@@ -33392,7 +33392,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.3+36.sha-a94383f');
+    const VERSION$2 = new Version$1('10.0.3+37.sha-2e9b953');
 
     /**
      * @license
