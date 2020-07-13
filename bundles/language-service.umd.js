891fd60a5ff6afd024f094c6d275d15de311ea3b
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:53:59 +0000

    66947cf9fa docs: fix typo in router.md (#37227)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+30.sha-d1aa467
+ * @license Angular v10.1.0-next.0+32.sha-66947cf
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17630,7 +17630,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.0+30.sha-d1aa467');
+    const VERSION$1 = new Version('10.1.0-next.0+32.sha-66947cf');
 
     /**
      * @license
@@ -43755,7 +43755,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.0+30.sha-d1aa467');
+    const VERSION$2 = new Version$1('10.1.0-next.0+32.sha-66947cf');
 
     /**
      * @license
