8aa51361031085f6f12a7cb25360d9a5ba8ec879
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:49:38 +0000

    a259a84d3f docs(http): Remove extra semicolons in the http guide (#37228)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+28.sha-72d6032
+ * @license Angular v10.1.0-next.0+31.sha-a259a84
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17630,7 +17630,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.0+28.sha-72d6032');
+    const VERSION$1 = new Version('10.1.0-next.0+31.sha-a259a84');
 
     /**
      * @license
@@ -33399,7 +33399,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.0+28.sha-72d6032');
+    const VERSION$2 = new Version$1('10.1.0-next.0+31.sha-a259a84');
 
     /**
      * @license
