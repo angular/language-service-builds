8f962ad9389b3aae1ab1331381d33dafdc591fec
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:45:29 +0000

    f01b0337d2 docs(router): fix typo in "spotlight on pathmatch" (#38039)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+43.sha-4ec3026
+ * @license Angular v10.1.0-next.0+44.sha-f01b033
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17630,7 +17630,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.0+43.sha-4ec3026');
+    const VERSION$1 = new Version('10.1.0-next.0+44.sha-f01b033');
 
     /**
      * @license
@@ -33408,7 +33408,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.0+43.sha-4ec3026');
+    const VERSION$2 = new Version$1('10.1.0-next.0+44.sha-f01b033');
 
     /**
      * @license
