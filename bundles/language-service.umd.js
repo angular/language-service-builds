aeeb3735e8c6e10fb81b4cb8b29ab976c6ec5c15
Andrew Kushnir <akushnir@google.com>
2020-07-15 21:14:20 +0000

    469aba0140 docs: add Emma Twersky to DevRel Contributor page (#38084)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+12.sha-d8c47b9
+ * @license Angular v10.1.0-next.1+13.sha-469aba0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+12.sha-d8c47b9');
+    const VERSION$1 = new Version('10.1.0-next.1+13.sha-469aba0');
 
     /**
      * @license
@@ -33409,7 +33409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+12.sha-d8c47b9');
+    const VERSION$2 = new Version$1('10.1.0-next.1+13.sha-469aba0');
 
     /**
      * @license
