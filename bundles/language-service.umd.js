a103e5150c6c7bdc8a21c54b8d296c88885c955a
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:35:13 +0000

    d8c47b971c docs: fix typo in "Creating libraries" guide (`by publishing...ensures` --> `publishing...ensures`) (#38032)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+11.sha-24b4205
+ * @license Angular v10.1.0-next.1+12.sha-d8c47b9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+11.sha-24b4205');
+    const VERSION$1 = new Version('10.1.0-next.1+12.sha-d8c47b9');
 
     /**
      * @license
@@ -43764,7 +43764,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+11.sha-24b4205');
+    const VERSION$2 = new Version$1('10.1.0-next.1+12.sha-d8c47b9');
 
     /**
      * @license
