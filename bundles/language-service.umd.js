febe17c807fb76e3c719d8ed88d5c584a3dff2cb
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:01:06 +0000

    45d79efc3f docs: remove all references to Angular Console (#37608)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+6.sha-09eee5b
+ * @license Angular v10.1.0-next.1+2.sha-45d79ef
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17630,7 +17630,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+6.sha-09eee5b');
+    const VERSION$1 = new Version('10.1.0-next.1+2.sha-45d79ef');
 
     /**
      * @license
@@ -33408,7 +33408,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+6.sha-09eee5b');
+    const VERSION$2 = new Version$1('10.1.0-next.1+2.sha-45d79ef');
 
     /**
      * @license
