be7270f87729ccbd92dcd7c94d004c1d0b20cb9f
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:27:07 +0000

    6c9401c338 docs: update router api documentation (#37980)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+23.sha-e382632
+ * @license Angular v10.1.0-next.1+20.sha-6c9401c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+23.sha-e382632');
+    const VERSION$1 = new Version('10.1.0-next.1+20.sha-6c9401c');
 
     /**
      * @license
@@ -43762,7 +43762,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+23.sha-e382632');
+    const VERSION$2 = new Version$1('10.1.0-next.1+20.sha-6c9401c');
 
     /**
      * @license
