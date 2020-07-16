4494791644f0cb0286052cd9a3c3d7a9cd523720
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:25:50 +0000

    99960a98d2 docs: update router api documentation (#37980)

--- bundles/ivy.umd.js
+++ bundles/ivy.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+15.sha-9ac3383
+ * @license Angular v10.0.4+16.sha-99960a9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18845,7 +18845,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.4+15.sha-9ac3383');
+    const VERSION$1 = new Version('10.0.4+16.sha-99960a9');
 
     /**
      * @license
@@ -19434,7 +19434,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.4+15.sha-9ac3383');
+    const VERSION$2 = new Version('10.0.4+16.sha-99960a9');
 
     /**
      * @license
