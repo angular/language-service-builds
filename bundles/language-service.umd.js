330bfb8aaec726146d453aaf6cd222cd1e827ee1
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:25:56 +0000

    c1539b16ae docs: add Ajit Singh to the collaborators (#37792)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+14.sha-737506e
+ * @license Angular v10.1.0-next.1+15.sha-c1539b1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+14.sha-737506e');
+    const VERSION$1 = new Version('10.1.0-next.1+15.sha-c1539b1');
 
     /**
      * @license
@@ -33502,7 +33502,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+14.sha-737506e');
+    const VERSION$2 = new Version$1('10.1.0-next.1+15.sha-c1539b1');
 
     /**
      * @license
