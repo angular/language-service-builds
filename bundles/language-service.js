96f26baf7147e50eeee1152755ab8d8fc5386612
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:28:38 +0000

    cf9a47ba53 feat(localize): allow duplicate messages to be handled during extraction (#38082)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+20.sha-6c9401c
+ * @license Angular v10.1.0-next.1+22.sha-cf9a47b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+20.sha-6c9401c');
+    const VERSION$1 = new Version('10.1.0-next.1+22.sha-cf9a47b');
 
     /**
      * @license
@@ -43762,7 +43762,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+20.sha-6c9401c');
+    const VERSION$2 = new Version$1('10.1.0-next.1+22.sha-cf9a47b');
 
     /**
      * @license
