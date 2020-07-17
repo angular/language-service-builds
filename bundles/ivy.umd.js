59aa22280d860cf19089d5493a7cbfd9c24fa925
Andrew Kushnir <akushnir@google.com>
2020-07-17 00:05:20 +0000

    e9cb6fbe87 build(language-service): add script to build package locally (#38103)

--- bundles/ivy.umd.js
+++ bundles/ivy.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+16.sha-99960a9
+ * @license Angular v10.0.4+17.sha-e9cb6fb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18845,7 +18845,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.4+16.sha-99960a9');
+    const VERSION$1 = new Version('10.0.4+17.sha-e9cb6fb');
 
     /**
      * @license
@@ -19434,7 +19434,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.4+16.sha-99960a9');
+    const VERSION$2 = new Version('10.0.4+17.sha-e9cb6fb');
 
     /**
      * @license
