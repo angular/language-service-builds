3cef29131afc32e3856112f1019ae22075e838ea
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:10:57 +0000

    fd45f7485d docs(docs-infra): reformat redundant sentence (#38109)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+24.sha-58f6079
+ * @license Angular v10.1.0-next.1+25.sha-fd45f74
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+24.sha-58f6079');
+    const VERSION$1 = new Version('10.1.0-next.1+25.sha-fd45f74');
 
     /**
      * @license
@@ -43762,7 +43762,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+24.sha-58f6079');
+    const VERSION$2 = new Version$1('10.1.0-next.1+25.sha-fd45f74');
 
     /**
      * @license
