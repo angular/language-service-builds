0e8c9f4a4ba11fc3b969260894815323dd56a211
Andrew Kushnir <akushnir@google.com>
2020-07-17 00:32:30 +0000

    58f6079873 build(language-service): add script to build package locally (#38103)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+22.sha-cf9a47b
+ * @license Angular v10.1.0-next.1+24.sha-58f6079
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+22.sha-cf9a47b');
+    const VERSION$1 = new Version('10.1.0-next.1+24.sha-58f6079');
 
     /**
      * @license
@@ -43762,7 +43762,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+22.sha-cf9a47b');
+    const VERSION$2 = new Version$1('10.1.0-next.1+24.sha-58f6079');
 
     /**
      * @license
