660bea02df5f1d3b26450522dd7a30649197bb4b
Andrew Kushnir <akushnir@google.com>
2020-07-20 20:59:35 +0000

    d6d3984524 docs(common): fix selector field in NgIfAs example component (#35854)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+31.sha-9cb318f
+ * @license Angular v10.0.4+33.sha-d6d3984
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18807,7 +18807,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.4+31.sha-9cb318f');
+    const VERSION$1 = new Version('10.0.4+33.sha-d6d3984');
 
     /**
      * @license
@@ -19396,7 +19396,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.4+31.sha-9cb318f');
+    const VERSION$2 = new Version('10.0.4+33.sha-d6d3984');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+31.sha-9cb318f
+ * @license Angular v10.0.4+33.sha-d6d3984
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17586,7 +17586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.4+31.sha-9cb318f');
+    const VERSION$1 = new Version('10.0.4+33.sha-d6d3984');
 
     /**
      * @license
@@ -33459,7 +33459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.4+31.sha-9cb318f');
+    const VERSION$2 = new Version$1('10.0.4+33.sha-d6d3984');
 
     /**
      * @license
