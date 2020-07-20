9c353f545a53b15601b31e03164da8bdd45b01eb
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:42:42 +0000

    5b31a0a294 docs: separate template syntax into multiple docs (#36954)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
+ * @license Angular v10.1.0-next.1+36.sha-5b31a0a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+35.sha-5d3d8b8');
+    const VERSION$1 = new Version('10.1.0-next.1+36.sha-5b31a0a');
 
     /**
      * @license
@@ -43726,7 +43726,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+35.sha-5d3d8b8');
+    const VERSION$2 = new Version$1('10.1.0-next.1+36.sha-5b31a0a');
 
     /**
      * @license
