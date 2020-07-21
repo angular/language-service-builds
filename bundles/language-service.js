cc234e2e108cb950fd876f7c478bdaa3c9886a7b
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:56:59 +0000

    f5eeb1a714 docs: fix DOCUMENT import path (#38158) (#38159)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+36.sha-2af3d9c
+ * @license Angular v10.0.4+37.sha-f5eeb1a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17586,7 +17586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.4+36.sha-2af3d9c');
+    const VERSION$1 = new Version('10.0.4+37.sha-f5eeb1a');
 
     /**
      * @license
@@ -33459,7 +33459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.4+36.sha-2af3d9c');
+    const VERSION$2 = new Version$1('10.0.4+37.sha-f5eeb1a');
 
     /**
      * @license
