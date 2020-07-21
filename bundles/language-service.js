45acb79418658227368fb5c020482ebfdb669306
Andrew Kushnir <akushnir@google.com>
2020-07-21 21:53:52 +0000

    7f82a2c1c8 docs: fix type in lazy-load callout (#38153)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+44.sha-a9910f5
+ * @license Angular v10.1.0-next.1+45.sha-7f82a2c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.1+44.sha-a9910f5');
+    const VERSION$1 = new Version('10.1.0-next.1+45.sha-7f82a2c');
 
     /**
      * @license
@@ -33466,7 +33466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.1+44.sha-a9910f5');
+    const VERSION$2 = new Version$1('10.1.0-next.1+45.sha-7f82a2c');
 
     /**
      * @license
