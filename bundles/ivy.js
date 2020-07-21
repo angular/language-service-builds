b39c2104660e6df4647e347ab3c3052fa8dce1b0
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:32:36 +0000

    4664acce50 docs: add routing terms to glossary (#38053)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+34.sha-3797861
+ * @license Angular v10.0.4+35.sha-4664acc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18807,7 +18807,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.4+34.sha-3797861');
+    const VERSION$1 = new Version('10.0.4+35.sha-4664acc');
 
     /**
      * @license
@@ -19396,7 +19396,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.4+34.sha-3797861');
+    const VERSION$2 = new Version('10.0.4+35.sha-4664acc');
 
     /**
      * @license
