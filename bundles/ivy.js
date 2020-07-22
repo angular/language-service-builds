6fb0019981195d6da11c69960a861c4034a5e9fa
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:38:14 +0000

    5a733629b5 build(docs-infra): remove boilerplate file listings in `example-boilerplate.js` (#38173)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+1.sha-5e742d2
+ * @license Angular v10.1.0-next.2+8.sha-5a73362
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18814,7 +18814,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+1.sha-5e742d2');
+    const VERSION$1 = new Version('10.1.0-next.2+8.sha-5a73362');
 
     /**
      * @license
@@ -19403,7 +19403,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.2+1.sha-5e742d2');
+    const VERSION$2 = new Version('10.1.0-next.2+8.sha-5a73362');
 
     /**
      * @license
