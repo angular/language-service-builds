126a250b01383d18ad8074b47cd2e67326967f80
Misko Hevery <misko@hevery.com>
2020-07-23 18:20:37 +0000

    8bb8f98c28 docs: remove duplicate `https://` (#38199)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+12.sha-3c1866b
+ * @license Angular v10.1.0-next.2+13.sha-8bb8f98
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+12.sha-3c1866b');
+    const VERSION$1 = new Version('10.1.0-next.2+13.sha-8bb8f98');
 
     /**
      * @license
@@ -33466,7 +33466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.2+12.sha-3c1866b');
+    const VERSION$2 = new Version$1('10.1.0-next.2+13.sha-8bb8f98');
 
     /**
      * @license
