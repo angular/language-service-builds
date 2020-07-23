b871d811d211046d4927757771b88a4418ee4d2c
Misko Hevery <misko@hevery.com>
2020-07-23 18:25:08 +0000

    51aa4aa1c3 docs: update dynamic-component loading guide (#36959)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+13.sha-8bb8f98
+ * @license Angular v10.1.0-next.2+14.sha-51aa4aa
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+13.sha-8bb8f98');
+    const VERSION$1 = new Version('10.1.0-next.2+14.sha-51aa4aa');
 
     /**
      * @license
@@ -33466,7 +33466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.2+13.sha-8bb8f98');
+    const VERSION$2 = new Version$1('10.1.0-next.2+14.sha-51aa4aa');
 
     /**
      * @license
