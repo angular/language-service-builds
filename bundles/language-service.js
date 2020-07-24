0d8f399ff80374080907b8fc25c4147cb84685d5
Misko Hevery <misko@hevery.com>
2020-07-24 17:22:18 +0000

    aca01985fd docs: Fix link by removing a space (#38214) PR Close #38214

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+16.sha-b8af109
+ * @license Angular v10.0.5+18.sha-aca0198
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17586,7 +17586,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.5+16.sha-b8af109');
+    const VERSION$1 = new Version('10.0.5+18.sha-aca0198');
 
     /**
      * @license
@@ -33459,7 +33459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.5+16.sha-b8af109');
+    const VERSION$2 = new Version$1('10.0.5+18.sha-aca0198');
 
     /**
      * @license
