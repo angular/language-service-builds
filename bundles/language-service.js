316e0c515169468d0821e2ccbeeb12d0bb344001
Misko Hevery <misko@hevery.com>
2020-07-24 15:46:55 +0000

    307db74e93 docs: fixed that class attribute is not closed (#38219)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+17.sha-d0060dc
+ * @license Angular v10.1.0-next.2+21.sha-307db74
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+17.sha-d0060dc');
+    const VERSION$1 = new Version('10.1.0-next.2+21.sha-307db74');
 
     /**
      * @license
@@ -33466,7 +33466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.2+17.sha-d0060dc');
+    const VERSION$2 = new Version$1('10.1.0-next.2+21.sha-307db74');
 
     /**
      * @license
