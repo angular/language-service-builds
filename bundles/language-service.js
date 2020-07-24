58cf33df84c16ad3350e95fec6ec8f2b97a19e29
Misko Hevery <misko@hevery.com>
2020-07-24 17:13:09 +0000

    cce5583e75 docs(core): Fix incorrectly rendered code example in structural directives guide (#38207)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+21.sha-307db74
+ * @license Angular v10.1.0-next.2+22.sha-cce5583
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+21.sha-307db74');
+    const VERSION$1 = new Version('10.1.0-next.2+22.sha-cce5583');
 
     /**
      * @license
@@ -33466,7 +33466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.2+21.sha-307db74');
+    const VERSION$2 = new Version$1('10.1.0-next.2+22.sha-cce5583');
 
     /**
      * @license
