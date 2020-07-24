0ec4be41f8acc841f9498ac2feaff4fb13e2a738
Misko Hevery <misko@hevery.com>
2020-07-24 22:36:55 +0000

    253337dc0a feat(zone.js): move MutationObserver/FileReader to different module (#31657)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+23.sha-6e2db22
+ * @license Angular v10.1.0-next.2+24.sha-253337d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18814,7 +18814,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+23.sha-6e2db22');
+    const VERSION$1 = new Version('10.1.0-next.2+24.sha-253337d');
 
     /**
      * @license
@@ -19403,7 +19403,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.2+23.sha-6e2db22');
+    const VERSION$2 = new Version('10.1.0-next.2+24.sha-253337d');
 
     /**
      * @license
