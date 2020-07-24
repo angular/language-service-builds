eec58b702552bbb535e63a08649b8a4246bf6075
Misko Hevery <misko@hevery.com>
2020-07-24 22:34:35 +0000

    253337dc0a feat(zone.js): move MutationObserver/FileReader to different module (#31657)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+23.sha-6e2db22
+ * @license Angular v10.1.0-next.2+24.sha-253337d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+23.sha-6e2db22');
+    const VERSION$1 = new Version('10.1.0-next.2+24.sha-253337d');
 
     /**
      * @license
@@ -33466,7 +33466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.2+23.sha-6e2db22');
+    const VERSION$2 = new Version$1('10.1.0-next.2+24.sha-253337d');
 
     /**
      * @license
