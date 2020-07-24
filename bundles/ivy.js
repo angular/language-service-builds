a62c4a5c9a8b110d009455f0044d091c27655f0e
Misko Hevery <misko@hevery.com>
2020-07-24 17:13:46 +0000

    6e2db228af docs: Fix link by removing a space (#38214) PR Close #38214

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+22.sha-cce5583
+ * @license Angular v10.1.0-next.2+23.sha-6e2db22
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18814,7 +18814,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+22.sha-cce5583');
+    const VERSION$1 = new Version('10.1.0-next.2+23.sha-6e2db22');
 
     /**
      * @license
@@ -19403,7 +19403,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.2+22.sha-cce5583');
+    const VERSION$2 = new Version('10.1.0-next.2+23.sha-6e2db22');
 
     /**
      * @license
