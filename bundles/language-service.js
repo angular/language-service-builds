7e53b7a0141738bfa05251c30fae62ab051b5fe3
Misko Hevery <misko@hevery.com>
2020-07-27 17:45:25 +0000

    f74cfadf64 docs: clarify the description of pipes (#37950)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+35.sha-8e5969b
+ * @license Angular v10.1.0-next.2+36.sha-f74cfad
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17604,7 +17604,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.2+35.sha-8e5969b');
+    const VERSION$1 = new Version('10.1.0-next.2+36.sha-f74cfad');
 
     /**
      * @license
@@ -33477,7 +33477,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.2+35.sha-8e5969b');
+    const VERSION$2 = new Version$1('10.1.0-next.2+36.sha-f74cfad');
 
     /**
      * @license
