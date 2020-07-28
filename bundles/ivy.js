ce1da0e20c0df6fa13e7266d95dca58865b4ffe8
Misko Hevery <misko@hevery.com>
2020-07-28 22:28:32 +0000

    cdda60a430 release: cut the v10.0.6 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+51.sha-7570356
+ * @license Angular v10.0.6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18818,7 +18818,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.5+51.sha-7570356');
+    const VERSION$1 = new Version('10.0.6');
 
     /**
      * @license
@@ -19407,7 +19407,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.5+51.sha-7570356');
+    const VERSION$2 = new Version('10.0.6');
 
     /**
      * @license
