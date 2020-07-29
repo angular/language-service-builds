c4797f2615feb87f38980df853be687d86f9785e
Misko Hevery <misko@hevery.com>
2020-07-29 16:54:52 +0000

    0469d9240a Revert "refactor(platform-browser): specify return type of parseEventName (#38089)"

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6
+ * @license Angular v10.0.6+1.sha-0469d92
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18818,7 +18818,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.6');
+    const VERSION$1 = new Version('10.0.6+1.sha-0469d92');
 
     /**
      * @license
@@ -19407,7 +19407,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.6');
+    const VERSION$2 = new Version('10.0.6+1.sha-0469d92');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6
+ * @license Angular v10.0.6+1.sha-0469d92
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17597,7 +17597,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.6');
+    const VERSION$1 = new Version('10.0.6+1.sha-0469d92');
 
     /**
      * @license
@@ -33471,7 +33471,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.6');
+    const VERSION$2 = new Version$1('10.0.6+1.sha-0469d92');
 
     /**
      * @license
