85674f7c0eeeaedcc5102cb52411e77d85dd62cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:18:47 +0000

    87baa06cc6 revert: docs(core): correct SomeService to SomeComponent (#38325)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+41.sha-e49b053
+ * @license Angular v10.1.0-next.3+40.sha-87baa06
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17647,7 +17647,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.3+41.sha-e49b053');
+    const VERSION$1 = new Version('10.1.0-next.3+40.sha-87baa06');
 
     /**
      * @license
@@ -43783,7 +43783,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.3+41.sha-e49b053');
+    const VERSION$2 = new Version$1('10.1.0-next.3+40.sha-87baa06');
 
     /**
      * @license
