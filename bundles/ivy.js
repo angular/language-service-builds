98eeafbd59a2f05ebcf5d831da30206b4464a7cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:29:33 +0000

    192ef42304 docs: release notes for the v10.0.8 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+43.sha-8fbf40b
+ * @license Angular v10.1.0-next.3+44.sha-192ef42
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18868,7 +18868,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.3+43.sha-8fbf40b');
+    const VERSION$1 = new Version('10.1.0-next.3+44.sha-192ef42');
 
     /**
      * @license
@@ -19457,7 +19457,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.3+43.sha-8fbf40b');
+    const VERSION$2 = new Version('10.1.0-next.3+44.sha-192ef42');
 
     /**
      * @license
