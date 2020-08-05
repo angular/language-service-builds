d71ebb047583a37f859c86eccd27c4b5e70f0958
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:25:00 +0000

    ba175be41f fix(compiler-cli): match wrapHost parameter types within plugin interface (#38004)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+6.sha-7630234
+ * @license Angular v10.1.0-next.4+5.sha-ba175be
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18868,7 +18868,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.4+6.sha-7630234');
+    const VERSION$1 = new Version('10.1.0-next.4+5.sha-ba175be');
 
     /**
      * @license
@@ -19457,7 +19457,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.4+6.sha-7630234');
+    const VERSION$2 = new Version('10.1.0-next.4+5.sha-ba175be');
 
     /**
      * @license
