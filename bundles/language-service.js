5406601d3910a2ff7b4d65483421ecf71abc03e4
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:37:19 +0000

    db897f4f7a docs: add a page with the Angular roadmap (#38356)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+6.sha-4151314
+ * @license Angular v10.0.8+8.sha-db897f4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17640,7 +17640,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.8+6.sha-4151314');
+    const VERSION$1 = new Version('10.0.8+8.sha-db897f4');
 
     /**
      * @license
@@ -33516,7 +33516,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.8+6.sha-4151314');
+    const VERSION$2 = new Version$1('10.0.8+8.sha-db897f4');
 
     /**
      * @license
