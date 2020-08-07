c8a48ac562be3fea9948369722fd6e53475fe8ad
Andrew Kushnir <akushnir@google.com>
2020-08-07 18:40:44 +0000

    4107abba2d refactor(common): use getElementById in ViewportScroller.scrollToAnchor (#38372)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+20.sha-ef7b70a
+ * @license Angular v10.0.8+22.sha-4107abb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17640,7 +17640,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.8+20.sha-ef7b70a');
+    const VERSION$1 = new Version('10.0.8+22.sha-4107abb');
 
     /**
      * @license
@@ -33556,7 +33556,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.8+20.sha-ef7b70a');
+    const VERSION$2 = new Version$1('10.0.8+22.sha-4107abb');
 
     /**
      * @license
