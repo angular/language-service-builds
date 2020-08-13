8a0c92b6de0ce160cce857f02f46b3e45dcfcf08
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:01:50 +0000

    d5e09f4d62 fix(core): fix multiple nested views removal from ViewContainerRef (#38317)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+14.sha-692e34d
+ * @license Angular v10.0.9+16.sha-d5e09f4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18868,7 +18868,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.9+14.sha-692e34d');
+    const VERSION$1 = new Version('10.0.9+16.sha-d5e09f4');
 
     /**
      * @license
@@ -19457,7 +19457,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.0.9+14.sha-692e34d');
+    const VERSION$2 = new Version('10.0.9+16.sha-d5e09f4');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+14.sha-692e34d
+ * @license Angular v10.0.9+16.sha-d5e09f4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17647,7 +17647,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.9+14.sha-692e34d');
+    const VERSION$1 = new Version('10.0.9+16.sha-d5e09f4');
 
     /**
      * @license
@@ -33581,7 +33581,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.9+14.sha-692e34d');
+    const VERSION$2 = new Version$1('10.0.9+16.sha-d5e09f4');
 
     /**
      * @license
