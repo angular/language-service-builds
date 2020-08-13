c236dd18f65f276de25ef7affaf19777c4e4aff3
Andrew Scott <atscott01@gmail.com>
2020-08-13 18:56:23 +0000

    b769771d60 refactor(router): Add annotations to correct Router documentation (#38448)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+10.sha-a80f654
+ * @license Angular v10.1.0-next.5+11.sha-b769771
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17654,7 +17654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.5+10.sha-a80f654');
+    const VERSION$1 = new Version('10.1.0-next.5+11.sha-b769771');
 
     /**
      * @license
@@ -33588,7 +33588,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.5+10.sha-a80f654');
+    const VERSION$2 = new Version$1('10.1.0-next.5+11.sha-b769771');
 
     /**
      * @license
