b271c25461c296e337bf346d440790fd3b17a92c
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:10:30 +0000

    f4ced74e3a feat(dev-infra): save invalid commit message attempts to be restored on next commit attempt (#38304)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+4.sha-8366eff
+ * @license Angular v10.1.0-next.5+5.sha-f4ced74
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17654,7 +17654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.5+4.sha-8366eff');
+    const VERSION$1 = new Version('10.1.0-next.5+5.sha-f4ced74');
 
     /**
      * @license
@@ -43648,7 +43648,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.5+4.sha-8366eff');
+    const VERSION$2 = new Version$1('10.1.0-next.5+5.sha-f4ced74');
 
     /**
      * @license
