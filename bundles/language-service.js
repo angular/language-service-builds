3dd5018ad157fae4f0f662f5d652335b89efdad9
Andrew Scott <atscott01@gmail.com>
2020-08-13 17:04:02 +0000

    aa847cb014 build: run browsers tests on chromium locally (#38435)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+8.sha-8763d82
+ * @license Angular v10.1.0-next.5+9.sha-aa847cb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17654,7 +17654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.5+8.sha-8763d82');
+    const VERSION$1 = new Version('10.1.0-next.5+9.sha-aa847cb');
 
     /**
      * @license
@@ -33588,7 +33588,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.5+8.sha-8763d82');
+    const VERSION$2 = new Version$1('10.1.0-next.5+9.sha-aa847cb');
 
     /**
      * @license
