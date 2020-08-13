639203f093935591cdd6b8f0f118a08389d18e65
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:11:57 +0000

    945751e2e8 ci: disable closure size tracking test (#38449)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+11.sha-b769771
+ * @license Angular v10.1.0-next.5+12.sha-945751e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18875,7 +18875,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.5+11.sha-b769771');
+    const VERSION$1 = new Version('10.1.0-next.5+12.sha-945751e');
 
     /**
      * @license
@@ -19464,7 +19464,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.5+11.sha-b769771');
+    const VERSION$2 = new Version('10.1.0-next.5+12.sha-945751e');
 
     /**
      * @license
