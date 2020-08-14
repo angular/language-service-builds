af59d9a152e1eb1fb5d01f72dd78f60891f79134
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:05:04 +0000

    71079ce47e fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+19.sha-ca79880
+ * @license Angular v10.1.0-next.5+20.sha-71079ce
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17654,7 +17654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.5+19.sha-ca79880');
+    const VERSION$1 = new Version('10.1.0-next.5+20.sha-71079ce');
 
     /**
      * @license
@@ -43648,7 +43648,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.5+19.sha-ca79880');
+    const VERSION$2 = new Version$1('10.1.0-next.5+20.sha-71079ce');
 
     /**
      * @license
