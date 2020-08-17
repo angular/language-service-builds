cbdc43041474345ecc6423da70c20dfb648ccb17
Andrew Scott <atscott@google.com>
2020-08-17 18:22:42 +0000

    3b9c802dee fix(ngcc): detect synthesized delegate constructors for downleveled ES2015 classes (#38463)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+24.sha-81c3e80
+ * @license Angular v10.1.0-next.5+26.sha-3b9c802
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18893,7 +18893,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.5+24.sha-81c3e80');
+    const VERSION$1 = new Version('10.1.0-next.5+26.sha-3b9c802');
 
     /**
      * @license
@@ -19482,7 +19482,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.5+24.sha-81c3e80');
+    const VERSION$2 = new Version('10.1.0-next.5+26.sha-3b9c802');
 
     /**
      * @license
