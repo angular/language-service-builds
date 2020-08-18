ef53adabfa9645f40faaf5dbe3f89920d13e68e1
Andrew Scott <atscott@google.com>
2020-08-18 23:44:55 +0000

    63ba74fe4e feat(dev-infra): tooling to check out pending PR (#38474)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+7.sha-aaa1d8e
+ * @license Angular v10.1.0-next.6+8.sha-63ba74f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.6+7.sha-aaa1d8e');
+    const VERSION$1 = new Version('10.1.0-next.6+8.sha-63ba74f');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.6+7.sha-aaa1d8e');
+    const VERSION$2 = new Version$1('10.1.0-next.6+8.sha-63ba74f');
 
     /**
      * @license
