df754a001311a88919785292911f2455603ba7da
Andrew Scott <atscott@google.com>
2020-08-18 17:44:24 +0000

    dbfb50e9f4 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+4.sha-723a9ff
+ * @license Angular v10.1.0-next.6+6.sha-dbfb50e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18884,7 +18884,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.6+4.sha-723a9ff');
+    const VERSION$1 = new Version('10.1.0-next.6+6.sha-dbfb50e');
 
     /**
      * @license
@@ -19473,7 +19473,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.6+4.sha-723a9ff');
+    const VERSION$2 = new Version('10.1.0-next.6+6.sha-dbfb50e');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+4.sha-723a9ff
+ * @license Angular v10.1.0-next.6+6.sha-dbfb50e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.6+4.sha-723a9ff');
+    const VERSION$1 = new Version('10.1.0-next.6+6.sha-dbfb50e');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.6+4.sha-723a9ff');
+    const VERSION$2 = new Version$1('10.1.0-next.6+6.sha-dbfb50e');
 
     /**
      * @license
