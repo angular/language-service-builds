cbe4410c48fa948c7eb0dc45718bbbee8c5fc07d
Andrew Scott <atscott@google.com>
2020-08-19 15:50:26 +0000

    55fd725e74 docs: Fix typo in the inputs and outputs guide (#38524)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+9.sha-f77fd5e
+ * @license Angular v10.1.0-next.6+10.sha-55fd725
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.6+9.sha-f77fd5e');
+    const VERSION$1 = new Version('10.1.0-next.6+10.sha-55fd725');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.6+9.sha-f77fd5e');
+    const VERSION$2 = new Version$1('10.1.0-next.6+10.sha-55fd725');
 
     /**
      * @license
