5de8253b1443b8f8acd4f43fa8914571ec9ade39
Andrew Scott <atscott@google.com>
2020-08-19 00:27:06 +0000

    398118f708 feat(dev-infra): create a wizard for building commit messages (#38457)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+7.sha-dcc3f6d
+ * @license Angular v10.0.10+8.sha-398118f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17647,7 +17647,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.0.10+7.sha-dcc3f6d');
+    const VERSION$1 = new Version('10.0.10+8.sha-398118f');
 
     /**
      * @license
@@ -33611,7 +33611,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.0.10+7.sha-dcc3f6d');
+    const VERSION$2 = new Version$1('10.0.10+8.sha-398118f');
 
     /**
      * @license
