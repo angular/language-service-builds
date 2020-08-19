96daeeba20328f55338cdb2d3cb4b6b9aa732e46
Misko Hevery <misko@hevery.com>
2020-08-19 21:42:57 +0000

    f245c6bb15 fix(core): remove closing body tag from inert DOM builder (#38454)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+6.sha-68a9a01
+ * @license Angular v10.1.0-next.7+7.sha-f245c6b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18884,7 +18884,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+6.sha-68a9a01');
+    const VERSION$1 = new Version('10.1.0-next.7+7.sha-f245c6b');
 
     /**
      * @license
@@ -19473,7 +19473,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.7+6.sha-68a9a01');
+    const VERSION$2 = new Version('10.1.0-next.7+7.sha-f245c6b');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+6.sha-68a9a01
+ * @license Angular v10.1.0-next.7+7.sha-f245c6b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+6.sha-68a9a01');
+    const VERSION$1 = new Version('10.1.0-next.7+7.sha-f245c6b');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.7+6.sha-68a9a01');
+    const VERSION$2 = new Version$1('10.1.0-next.7+7.sha-f245c6b');
 
     /**
      * @license
