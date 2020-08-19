7ba9f1d650f5349e32609e93e78db76ddd37b070
Misko Hevery <misko@hevery.com>
2020-08-19 19:02:57 +0000

    7ad32649c0 fix(router): support lazy loading for empty path named outlets (#38379)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+12.sha-0270020
+ * @license Angular v10.1.0-next.7+2.sha-7ad3264
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18884,7 +18884,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.6+12.sha-0270020');
+    const VERSION$1 = new Version('10.1.0-next.7+2.sha-7ad3264');
 
     /**
      * @license
@@ -19473,7 +19473,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.6+12.sha-0270020');
+    const VERSION$2 = new Version('10.1.0-next.7+2.sha-7ad3264');
 
     /**
      * @license
