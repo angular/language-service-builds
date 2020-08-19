8452b209f8b927ca94c1619009b6e74fc6a4bda4
Andrew Scott <atscott@google.com>
2020-08-19 16:29:27 +0000

    6b662d10c1 fix(zone.js): zone.js package.json should not include files/directories field (#38528)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+11.sha-6b662d1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.6+10.sha-55fd725');
+    const VERSION$1 = new Version('10.1.0-next.6+11.sha-6b662d1');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.6+10.sha-55fd725');
+    const VERSION$2 = new Version$1('10.1.0-next.6+11.sha-6b662d1');
 
     /**
      * @license
