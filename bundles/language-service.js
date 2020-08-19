bb0218fc22783a07b5c1779cfd17b6ff1f987553
Misko Hevery <misko@hevery.com>
2020-08-19 21:44:43 +0000

    ac461e1efd fix(localize): extract the correct message ids (#38498)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+7.sha-f245c6b
+ * @license Angular v10.1.0-next.7+8.sha-ac461e1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+7.sha-f245c6b');
+    const VERSION$1 = new Version('10.1.0-next.7+8.sha-ac461e1');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.7+7.sha-f245c6b');
+    const VERSION$2 = new Version$1('10.1.0-next.7+8.sha-ac461e1');
 
     /**
      * @license
