c52bcd52d71531775487a8b286e4fe526b3e697c
Misko Hevery <misko@hevery.com>
2020-08-19 21:29:21 +0000

    1ec609946f docs: Typos fixes in the binding syntax guide (#38519)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+2.sha-7ad3264
+ * @license Angular v10.1.0-next.7+3.sha-1ec6099
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+2.sha-7ad3264');
+    const VERSION$1 = new Version('10.1.0-next.7+3.sha-1ec6099');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.7+2.sha-7ad3264');
+    const VERSION$2 = new Version$1('10.1.0-next.7+3.sha-1ec6099');
 
     /**
      * @license
