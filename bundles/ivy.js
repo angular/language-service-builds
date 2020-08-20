a289ad2623271fd38ca9d11ec8e6d0ac46ee5cc3
Misko Hevery <misko@hevery.com>
2020-08-20 20:23:33 +0000

    956b25a100 docs: apply code styling in template reference variables guide (#38522)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+12.sha-22f1ac3
+ * @license Angular v10.1.0-next.7+14.sha-956b25a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18884,7 +18884,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+12.sha-22f1ac3');
+    const VERSION$1 = new Version('10.1.0-next.7+14.sha-956b25a');
 
     /**
      * @license
@@ -19473,7 +19473,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.7+12.sha-22f1ac3');
+    const VERSION$2 = new Version('10.1.0-next.7+14.sha-956b25a');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+12.sha-22f1ac3
+ * @license Angular v10.1.0-next.7+14.sha-956b25a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+12.sha-22f1ac3');
+    const VERSION$1 = new Version('10.1.0-next.7+14.sha-956b25a');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.7+12.sha-22f1ac3');
+    const VERSION$2 = new Version$1('10.1.0-next.7+14.sha-956b25a');
 
     /**
      * @license
