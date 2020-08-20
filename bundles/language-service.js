9e13db03e03b2bf7c6b306e6d18e8ec628cfe2c8
Misko Hevery <misko@hevery.com>
2020-08-20 16:55:18 +0000

    6442875c99 docs(core): Fix typo in JSDoc for AbstractType<T> (#38541)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+11.sha-4ee5e73
+ * @license Angular v10.1.0-next.7+10.sha-6442875
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+11.sha-4ee5e73');
+    const VERSION$1 = new Version('10.1.0-next.7+10.sha-6442875');
 
     /**
      * @license
@@ -33637,7 +33637,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.7+11.sha-4ee5e73');
+    const VERSION$2 = new Version$1('10.1.0-next.7+10.sha-6442875');
 
     /**
      * @license
