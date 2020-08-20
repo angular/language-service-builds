0c4f70626063295f5ceada9cfd7435c53a2b6fa9
Misko Hevery <misko@hevery.com>
2020-08-20 17:04:18 +0000

    8017ca4db3 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+10.sha-6442875
+ * @license Angular v10.1.0-next.7+13.sha-8017ca4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17672,7 +17672,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+10.sha-6442875');
+    const VERSION$1 = new Version('10.1.0-next.7+13.sha-8017ca4');
 
     /**
      * @license
@@ -43698,7 +43698,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-next.7+10.sha-6442875');
+    const VERSION$2 = new Version$1('10.1.0-next.7+13.sha-8017ca4');
 
     /**
      * @license
