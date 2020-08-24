866fa159280eb0843f0afc51937f831f1fd2c040
Misko Hevery <misko@hevery.com>
2020-08-24 22:52:41 +0000

    f18e2d5898 docs: release notes for the v10.0.12 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+25.sha-375f0a6
+ * @license Angular v10.1.0-next.7+26.sha-f18e2d5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19008,7 +19008,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+25.sha-375f0a6');
+    const VERSION$1 = new Version('10.1.0-next.7+26.sha-f18e2d5');
 
     /**
      * @license
@@ -19597,7 +19597,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.7+25.sha-375f0a6');
+    const VERSION$2 = new Version('10.1.0-next.7+26.sha-f18e2d5');
 
     /**
      * @license
