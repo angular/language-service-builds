ed8ff2fdfa9333a73fc8dd853741b839e86aeb93
Misko Hevery <misko@hevery.com>
2020-08-25 17:25:09 +0000

    94a3e0e81d docs: add correct path for karma.conf.js file (#38571)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+30.sha-9cbde86
+ * @license Angular v10.1.0-next.7+31.sha-94a3e0e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19008,7 +19008,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-next.7+30.sha-9cbde86');
+    const VERSION$1 = new Version('10.1.0-next.7+31.sha-94a3e0e');
 
     /**
      * @license
@@ -19597,7 +19597,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-next.7+30.sha-9cbde86');
+    const VERSION$2 = new Version('10.1.0-next.7+31.sha-94a3e0e');
 
     /**
      * @license
