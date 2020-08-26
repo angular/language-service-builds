9abe8c290004729a9a9f2d3977eedfa28547bc9a
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:13:45 +0000

    0681a20d28 fix(docs-infra): add guard to not render anchor link for private api (#38589)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0
+ * @license Angular v10.1.0-rc.0+1.sha-0681a20
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17796,7 +17796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0');
+    const VERSION$1 = new Version('10.1.0-rc.0+1.sha-0681a20');
 
     /**
      * @license
@@ -33776,7 +33776,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-rc.0');
+    const VERSION$2 = new Version$1('10.1.0-rc.0+1.sha-0681a20');
 
     /**
      * @license
