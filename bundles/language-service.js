d9a1f7cb3342b73709a1537075916c0313cdc1c4
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:03:11 +0000

    ebc0e46501 refactor(dev-infra): improve error message for unexpected version branches (#38622)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+23.sha-d5fabc3
+ * @license Angular v10.1.0-rc.0+22.sha-ebc0e46
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17796,7 +17796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0+23.sha-d5fabc3');
+    const VERSION$1 = new Version('10.1.0-rc.0+22.sha-ebc0e46');
 
     /**
      * @license
@@ -43837,7 +43837,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-rc.0+23.sha-d5fabc3');
+    const VERSION$2 = new Version$1('10.1.0-rc.0+22.sha-ebc0e46');
 
     /**
      * @license
