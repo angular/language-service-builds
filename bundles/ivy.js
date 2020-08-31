04c106ba43cb859e69315db92785adb760d69952
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:50:17 +0000

    cbbf8b542f docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+14.sha-91dfb18
+ * @license Angular v10.1.0-rc.0+15.sha-cbbf8b5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19008,7 +19008,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0+14.sha-91dfb18');
+    const VERSION$1 = new Version('10.1.0-rc.0+15.sha-cbbf8b5');
 
     /**
      * @license
@@ -19597,7 +19597,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-rc.0+14.sha-91dfb18');
+    const VERSION$2 = new Version('10.1.0-rc.0+15.sha-cbbf8b5');
 
     /**
      * @license
