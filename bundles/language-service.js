c1e207975fe677e5037deadeda2fd37b9448f6b6
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:12:10 +0000

    c1bc070b40 refactor(dev-infra): remove `style` type from commit style guide (#38639)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+18.sha-b2857b4
+ * @license Angular v10.1.0-rc.0+16.sha-c1bc070
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17796,7 +17796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0+18.sha-b2857b4');
+    const VERSION$1 = new Version('10.1.0-rc.0+16.sha-c1bc070');
 
     /**
      * @license
@@ -43837,7 +43837,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-rc.0+18.sha-b2857b4');
+    const VERSION$2 = new Version$1('10.1.0-rc.0+16.sha-c1bc070');
 
     /**
      * @license
