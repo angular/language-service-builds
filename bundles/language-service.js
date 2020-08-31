095ae4fefd3704d9f54319813666a6e95294b965
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:08:01 +0000

    5d5caf21b8 docs: fix broken markdown in start/start-data (#38644) PR Close #38644

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+15.sha-930eeaf
+ * @license Angular v10.1.0-rc.0+17.sha-5d5caf2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17796,7 +17796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0+15.sha-930eeaf');
+    const VERSION$1 = new Version('10.1.0-rc.0+17.sha-5d5caf2');
 
     /**
      * @license
@@ -43837,7 +43837,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-rc.0+15.sha-930eeaf');
+    const VERSION$2 = new Version$1('10.1.0-rc.0+17.sha-5d5caf2');
 
     /**
      * @license
