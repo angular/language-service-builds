76010715b176007204de03ea1af1bfebe4ee6c52
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:09:32 +0000

    c1bc070b40 refactor(dev-infra): remove `style` type from commit style guide (#38639)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+20.sha-52c7a4b
+ * @license Angular v10.1.0-rc.0+16.sha-c1bc070
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19008,7 +19008,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0+20.sha-52c7a4b');
+    const VERSION$1 = new Version('10.1.0-rc.0+16.sha-c1bc070');
 
     /**
      * @license
@@ -19597,7 +19597,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.0-rc.0+20.sha-52c7a4b');
+    const VERSION$2 = new Version('10.1.0-rc.0+16.sha-c1bc070');
 
     /**
      * @license
