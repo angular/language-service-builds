1c744330fc29a32196cec2378708b802d70d6f82
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:39:21 +0000

    e6ee7c2aeb release: cut the v11.0.0-next.0 release

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+37.sha-59c234c
+ * @license Angular v11.0.0-next.0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17815,7 +17815,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0-rc.0+37.sha-59c234c');
+    const VERSION$1 = new Version('11.0.0-next.0');
 
     /**
      * @license
@@ -43857,7 +43857,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0-rc.0+37.sha-59c234c');
+    const VERSION$2 = new Version$1('11.0.0-next.0');
 
     /**
      * @license
