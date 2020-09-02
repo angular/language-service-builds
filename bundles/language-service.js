1e9f32cfbb937b9a73f99bf941d685ae69298ae6
Andrew Scott <atscott@google.com>
2020-09-02 22:11:29 +0000

    32f33f095f fix(localize): render context of translation file parse errors (#38673)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0
+ * @license Angular v10.1.0+2.sha-32f33f0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17815,7 +17815,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0');
+    const VERSION$1 = new Version('10.1.0+2.sha-32f33f0');
 
     /**
      * @license
@@ -33796,7 +33796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0');
+    const VERSION$2 = new Version$1('10.1.0+2.sha-32f33f0');
 
     /**
      * @license
