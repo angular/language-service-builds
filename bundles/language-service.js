00b4c6dad00add28259512aa91a2b341924cb9f4
atscott <atscott01@gmail.com>
2020-09-03 19:21:58 +0000

    1c7e5cef3e docs: add dayjs date adapter to resources page (#38031)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+8.sha-44bb85a
+ * @license Angular v11.0.0-next.0+10.sha-1c7e5ce
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17814,7 +17814,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+8.sha-44bb85a');
+    const VERSION$1 = new Version('11.0.0-next.0+10.sha-1c7e5ce');
 
     /**
      * @license
@@ -33795,7 +33795,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.0+8.sha-44bb85a');
+    const VERSION$2 = new Version$1('11.0.0-next.0+10.sha-1c7e5ce');
 
     /**
      * @license
