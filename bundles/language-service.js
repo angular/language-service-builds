f3b699b7403ec75a567b92447d23bcb3d2d1b20c
atscott <atscott01@gmail.com>
2020-09-08 17:29:53 +0000

    5863537575 build: upgrade all preview-server JS dependencies to latest versions (#38736)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+10.sha-0a277c6
+ * @license Angular v10.1.0+20.sha-5863537
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17810,7 +17810,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.0+10.sha-0a277c6');
+    const VERSION$1 = new Version('10.1.0+20.sha-5863537');
 
     /**
      * @license
@@ -33791,7 +33791,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.0+10.sha-0a277c6');
+    const VERSION$2 = new Version$1('10.1.0+20.sha-5863537');
 
     /**
      * @license
