afaf611924f8507c30a9f53a26927d1f831808af
atscott <atscott01@gmail.com>
2020-09-08 21:33:28 +0000

    a32a317ea1 fix(compiler-cli): ensure that a declaration is available in type-to-value conversion (#38684)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+41.sha-b0c79f2
+ * @license Angular v11.0.0-next.0+40.sha-a32a317
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19064,7 +19064,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+41.sha-b0c79f2');
+    const VERSION$1 = new Version('11.0.0-next.0+40.sha-a32a317');
 
     /**
      * @license
@@ -19654,7 +19654,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.0+41.sha-b0c79f2');
+    const VERSION$2 = new Version('11.0.0-next.0+40.sha-a32a317');
 
     /**
      * @license
