6ecec1fc5d9e96f92cbfa6589ce08e1b809b8ba9
atscott <atscott01@gmail.com>
2020-09-08 17:33:10 +0000

    d9da7e5a18 docs: fix result of sanitization example (#38724)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+24.sha-97475d7
+ * @license Angular v11.0.0-next.0+19.sha-d9da7e5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19012,7 +19012,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+24.sha-97475d7');
+    const VERSION$1 = new Version('11.0.0-next.0+19.sha-d9da7e5');
 
     /**
      * @license
@@ -19602,7 +19602,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.0+24.sha-97475d7');
+    const VERSION$2 = new Version('11.0.0-next.0+19.sha-d9da7e5');
 
     /**
      * @license
