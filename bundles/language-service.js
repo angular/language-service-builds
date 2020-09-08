88c81cacdcecf49b2ede2eefbfea9bfa4cec0136
atscott <atscott01@gmail.com>
2020-09-08 17:27:45 +0000

    c2d017de83 docs: word correction (#38729) PR Close #38729

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+23.sha-a29f9a9
+ * @license Angular v11.0.0-next.0+16.sha-c2d017d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17809,7 +17809,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+23.sha-a29f9a9');
+    const VERSION$1 = new Version('11.0.0-next.0+16.sha-c2d017d');
 
     /**
      * @license
@@ -33790,7 +33790,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.0+23.sha-a29f9a9');
+    const VERSION$2 = new Version$1('11.0.0-next.0+16.sha-c2d017d');
 
     /**
      * @license
