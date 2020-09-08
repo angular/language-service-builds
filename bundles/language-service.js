4d257db7f323f26813f4ffb4fe9f08614c9626b9
atscott <atscott01@gmail.com>
2020-09-08 17:30:36 +0000

    d9da7e5a18 docs: fix result of sanitization example (#38724)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+24.sha-97475d7
+ * @license Angular v11.0.0-next.0+19.sha-d9da7e5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17809,7 +17809,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+24.sha-97475d7');
+    const VERSION$1 = new Version('11.0.0-next.0+19.sha-d9da7e5');
 
     /**
      * @license
@@ -33790,7 +33790,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.0+24.sha-97475d7');
+    const VERSION$2 = new Version$1('11.0.0-next.0+19.sha-d9da7e5');
 
     /**
      * @license
