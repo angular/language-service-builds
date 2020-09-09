36c33ea7f590e4c3af8fe5719e34266ce259a9a2
atscott <atscott01@gmail.com>
2020-09-09 20:47:33 +0000

    886f58d4fe docs: release notes for the v10.1.1 release

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+55.sha-18f84a0
+ * @license Angular v11.0.0-next.0+56.sha-886f58d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17861,7 +17861,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+55.sha-18f84a0');
+    const VERSION$1 = new Version('11.0.0-next.0+56.sha-886f58d');
 
     /**
      * @license
@@ -43895,7 +43895,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.0+55.sha-18f84a0');
+    const VERSION$2 = new Version$1('11.0.0-next.0+56.sha-886f58d');
 
     /**
      * @license
