7e6eab2bd7de4b94dcce736306848049dfd8eed9
atscott <atscott01@gmail.com>
2020-09-09 16:30:19 +0000

    6d9bfb8368 docs(router): fixed PreloadAllModules comment typo (#38758) PR Close #38758

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+47.sha-19a4843
+ * @license Angular v11.0.0-next.0+51.sha-6d9bfb8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17861,7 +17861,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.0+47.sha-19a4843');
+    const VERSION$1 = new Version('11.0.0-next.0+51.sha-6d9bfb8');
 
     /**
      * @license
@@ -33842,7 +33842,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.0+47.sha-19a4843');
+    const VERSION$2 = new Version$1('11.0.0-next.0+51.sha-6d9bfb8');
 
     /**
      * @license
