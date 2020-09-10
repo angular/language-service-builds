97592e05ffc81e7e58d9d91604cc95df8305bff4
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:18:05 +0000

    ce1efc1af2 fix(localize): ensure that `formatOptions` is optional (#38787)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+11.sha-d141516
+ * @license Angular v11.0.0-next.1+12.sha-ce1efc1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17861,7 +17861,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.1+11.sha-d141516');
+    const VERSION$1 = new Version('11.0.0-next.1+12.sha-ce1efc1');
 
     /**
      * @license
@@ -33843,7 +33843,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.1+11.sha-d141516');
+    const VERSION$2 = new Version$1('11.0.0-next.1+12.sha-ce1efc1');
 
     /**
      * @license
