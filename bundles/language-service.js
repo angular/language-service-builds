6a8a60eac0db3fbbb62a1f1c9d8f2bc98c8e596b
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:12 +0000

    aa43cbf8c5 fix(dev-infra): correct build setup for dev-infra (#38815)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+18.sha-b05d79d
+ * @license Angular v10.1.1+19.sha-aa43cbf
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17841,7 +17841,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.1+18.sha-b05d79d');
+    const VERSION$1 = new Version('10.1.1+19.sha-aa43cbf');
 
     /**
      * @license
@@ -33823,7 +33823,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.1+18.sha-b05d79d');
+    const VERSION$2 = new Version$1('10.1.1+19.sha-aa43cbf');
 
     /**
      * @license
