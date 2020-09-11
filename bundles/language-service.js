ac9aead17c1c081419a94f362267f58cd01957b0
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:06:05 +0000

    8096c63c66 docs: clarify what fixes are merged to LTS versions (#38788)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+13.sha-31d0ee4
+ * @license Angular v10.1.1+15.sha-8096c63
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17841,7 +17841,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.1+13.sha-31d0ee4');
+    const VERSION$1 = new Version('10.1.1+15.sha-8096c63');
 
     /**
      * @license
@@ -33823,7 +33823,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.1+13.sha-31d0ee4');
+    const VERSION$2 = new Version$1('10.1.1+15.sha-8096c63');
 
     /**
      * @license
