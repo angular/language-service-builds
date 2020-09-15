85c13b8a648620da5cc2f8768dde507d2d86b7ab
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:45:24 +0000

    a1c1c450dc test(ngcc): load standard files only once (#38840)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+40.sha-b9ca6d2
+ * @license Angular v11.0.0-next.1+43.sha-a1c1c45
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19066,7 +19066,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.1+40.sha-b9ca6d2');
+    const VERSION$1 = new Version('11.0.0-next.1+43.sha-a1c1c45');
 
     /**
      * @license
@@ -19659,7 +19659,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.1+40.sha-b9ca6d2');
+    const VERSION$2 = new Version('11.0.0-next.1+43.sha-a1c1c45');
 
     /**
      * @license
