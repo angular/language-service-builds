3df5f175db967c593104a92db0870c5b7c8c3f9b
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:48:56 +0000

    722699fb0c fix(dev-infra): handle no caretaker config being defined (#38862)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+8.sha-49ee90b
+ * @license Angular v11.0.0-next.2+11.sha-722699f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17863,7 +17863,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+8.sha-49ee90b');
+    const VERSION$1 = new Version('11.0.0-next.2+11.sha-722699f');
 
     /**
      * @license
@@ -43898,7 +43898,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.2+8.sha-49ee90b');
+    const VERSION$2 = new Version$1('11.0.0-next.2+11.sha-722699f');
 
     /**
      * @license
