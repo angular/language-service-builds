22a7eeff0030a5f621ca87c8887fc2907d446159
Misko Hevery <misko@hevery.com>
2020-09-18 15:29:57 +0000

    d795a00137 refactor(compiler): replace Comment nodes with leadingComments property (#38811)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+20.sha-e442486
+ * @license Angular v11.0.0-next.2+19.sha-d795a00
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19088,7 +19088,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+20.sha-e442486');
+    const VERSION$1 = new Version('11.0.0-next.2+19.sha-d795a00');
 
     /**
      * @license
@@ -19681,7 +19681,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.2+20.sha-e442486');
+    const VERSION$2 = new Version('11.0.0-next.2+19.sha-d795a00');
 
     /**
      * @license
