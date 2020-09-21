6dc4d0019c74146387b3190877d9be741c41c802
Misko Hevery <misko@hevery.com>
2020-09-21 19:51:00 +0000

    fdd4fa0009 docs: change wrong default app module by ng new (#38549)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+32.sha-f3f6a42
+ * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17885,7 +17885,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+32.sha-f3f6a42');
+    const VERSION$1 = new Version('11.0.0-next.2+36.sha-fdd4fa0');
 
     /**
      * @license
@@ -43920,7 +43920,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.2+32.sha-f3f6a42');
+    const VERSION$2 = new Version$1('11.0.0-next.2+36.sha-fdd4fa0');
 
     /**
      * @license
