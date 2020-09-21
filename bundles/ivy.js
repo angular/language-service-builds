934e169563887d4b4b59cb85e3741c8d1fba81ee
Misko Hevery <misko@hevery.com>
2020-09-21 19:45:30 +0000

    f3f6a42342 test(docs-infra): replace deprecated `ReflectiveInjector` with `Injector` (#38897)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+31.sha-e498ea9
+ * @license Angular v11.0.0-next.2+32.sha-f3f6a42
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19088,7 +19088,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+31.sha-e498ea9');
+    const VERSION$1 = new Version('11.0.0-next.2+32.sha-f3f6a42');
 
     /**
      * @license
@@ -19681,7 +19681,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.2+31.sha-e498ea9');
+    const VERSION$2 = new Version('11.0.0-next.2+32.sha-f3f6a42');
 
     /**
      * @license
