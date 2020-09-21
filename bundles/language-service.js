282e63537ff789bc02df1daf0db0a83ba3308a52
Misko Hevery <misko@hevery.com>
2020-09-21 19:49:21 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
+ * @license Angular v11.0.0-next.2+37.sha-2b1b718
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17885,7 +17885,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+36.sha-fdd4fa0');
+    const VERSION$1 = new Version('11.0.0-next.2+37.sha-2b1b718');
 
     /**
      * @license
@@ -33867,7 +33867,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.2+36.sha-fdd4fa0');
+    const VERSION$2 = new Version$1('11.0.0-next.2+37.sha-2b1b718');
 
     /**
      * @license
