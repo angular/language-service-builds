bebf6476e4c1ed7134069b7e00b2b8061fb1d0b7
Misko Hevery <misko@hevery.com>
2020-09-21 19:50:14 +0000

    984ed39195 feat(common): Add ISO week-numbering year formats support to formatDate (#38828)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+37.sha-2b1b718
+ * @license Angular v11.0.0-next.2+35.sha-984ed39
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19088,7 +19088,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+37.sha-2b1b718');
+    const VERSION$1 = new Version('11.0.0-next.2+35.sha-984ed39');
 
     /**
      * @license
@@ -19681,7 +19681,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.2+37.sha-2b1b718');
+    const VERSION$2 = new Version('11.0.0-next.2+35.sha-984ed39');
 
     /**
      * @license
