875187f2dcdf9be2c2ce74525f2e06c8bd155538
Misko Hevery <misko@hevery.com>
2020-09-21 16:56:14 +0000

    e498ea9b5a revert: feat(router): better warning message when a router outlet has not been instantiated (#38920)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
+ * @license Angular v11.0.0-next.2+31.sha-e498ea9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17885,7 +17885,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+29.sha-d5ddb9f');
+    const VERSION$1 = new Version('11.0.0-next.2+31.sha-e498ea9');
 
     /**
      * @license
@@ -33867,7 +33867,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.2+29.sha-d5ddb9f');
+    const VERSION$2 = new Version$1('11.0.0-next.2+31.sha-e498ea9');
 
     /**
      * @license
