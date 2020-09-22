76633af7e807b7dfe34fde5a7b5f49806bd6baed
Misko Hevery <misko@hevery.com>
2020-09-22 22:37:45 +0000

    75610505c6 refactor(zone.js): remove usages of blacklist related to UNPATCHED_EVENTS (#38930)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+54.sha-b168252
+ * @license Angular v11.0.0-next.2+53.sha-7561050
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19145,7 +19145,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+54.sha-b168252');
+    const VERSION$1 = new Version('11.0.0-next.2+53.sha-7561050');
 
     /**
      * @license
@@ -19738,7 +19738,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.2+54.sha-b168252');
+    const VERSION$2 = new Version('11.0.0-next.2+53.sha-7561050');
 
     /**
      * @license
