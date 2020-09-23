501933d37d17cece0efca8c351aa1c4a513ab644
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 19:41:45 +0000

    73c7882629 release: cut the v10.1.3 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+33.sha-32e32e5
+ * @license Angular v10.1.3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19066,7 +19066,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.2+33.sha-32e32e5');
+    const VERSION$1 = new Version('10.1.3');
 
     /**
      * @license
@@ -19659,7 +19659,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.2+33.sha-32e32e5');
+    const VERSION$2 = new Version('10.1.3');
 
     /**
      * @license
