b791e2ec72934bac7d2262dad1243c1b56c5d636
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:04:48 +0000

    4baabf9cd3 release: cut the v11.0.0-next.3 release

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+55.sha-7244e1b
+ * @license Angular v11.0.0-next.3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17942,7 +17942,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.2+55.sha-7244e1b');
+    const VERSION$1 = new Version('11.0.0-next.3');
 
     /**
      * @license
@@ -33924,7 +33924,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.2+55.sha-7244e1b');
+    const VERSION$2 = new Version$1('11.0.0-next.3');
 
     /**
      * @license
