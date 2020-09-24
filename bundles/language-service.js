81261ca1a5a4f0fab609f9570936c6b8c15956aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:06:09 +0000

    4c8766573d refactor(language-service): Update hybrid visitor to use keySpan for bound attributes (#38955)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+7.sha-8422633
+ * @license Angular v11.0.0-next.3+8.sha-4c87665
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17942,7 +17942,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+7.sha-8422633');
+    const VERSION$1 = new Version('11.0.0-next.3+8.sha-4c87665');
 
     /**
      * @license
@@ -43977,7 +43977,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+7.sha-8422633');
+    const VERSION$2 = new Version$1('11.0.0-next.3+8.sha-4c87665');
 
     /**
      * @license
