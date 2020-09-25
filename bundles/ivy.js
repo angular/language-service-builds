e5e38e2fad31dd4878fd7d53a11314adc9256569
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:54:11 +0000

    4ca1c736bb fix(packaging): remove polyfills needed to run tests on IE9 and IE 10 (#38931)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+19.sha-cf9176e
+ * @license Angular v11.0.0-next.3+17.sha-4ca1c73
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19145,7 +19145,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+19.sha-cf9176e');
+    const VERSION$1 = new Version('11.0.0-next.3+17.sha-4ca1c73');
 
     /**
      * @license
@@ -19738,7 +19738,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.3+19.sha-cf9176e');
+    const VERSION$2 = new Version('11.0.0-next.3+17.sha-4ca1c73');
 
     /**
      * @license
