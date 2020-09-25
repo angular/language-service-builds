39075c7f278134e0f1911fc39ca89c790795c0ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:56:47 +0000

    4ca1c736bb fix(packaging): remove polyfills needed to run tests on IE9 and IE 10 (#38931)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+19.sha-cf9176e
+ * @license Angular v11.0.0-next.3+17.sha-4ca1c73
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17942,7 +17942,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+19.sha-cf9176e');
+    const VERSION$1 = new Version('11.0.0-next.3+17.sha-4ca1c73');
 
     /**
      * @license
@@ -43977,7 +43977,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+19.sha-cf9176e');
+    const VERSION$2 = new Version$1('11.0.0-next.3+17.sha-4ca1c73');
 
     /**
      * @license
