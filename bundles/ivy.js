6a625f81a70f3ebd7d590449110ccf7345079c00
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:55:31 +0000

    2f6777df18 docs(docs-infra): add upcoming conference (#38956)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+12.sha-7f6f8bb
+ * @license Angular v11.0.0-next.3+18.sha-2f6777d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19145,7 +19145,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+12.sha-7f6f8bb');
+    const VERSION$1 = new Version('11.0.0-next.3+18.sha-2f6777d');
 
     /**
      * @license
@@ -19738,7 +19738,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.3+12.sha-7f6f8bb');
+    const VERSION$2 = new Version('11.0.0-next.3+18.sha-2f6777d');
 
     /**
      * @license
