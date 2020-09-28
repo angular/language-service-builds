8bda0f3e44361acf0da781d2b86c594aa2cb8873
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:52:21 +0000

    4fd635a03a build(docs-infra): enable AOT in development mode for all docs examples (#39001)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+63.sha-4a9f452
+ * @license Angular v11.0.0-next.3+69.sha-4fd635a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+63.sha-4a9f452');
+    const VERSION$1 = new Version('11.0.0-next.3+69.sha-4fd635a');
 
     /**
      * @license
@@ -33979,7 +33979,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+63.sha-4a9f452');
+    const VERSION$2 = new Version$1('11.0.0-next.3+69.sha-4fd635a');
 
     /**
      * @license
