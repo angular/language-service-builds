625fad16e8b2f22588494087ac89ecfa7cc5ff77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:41:46 +0000

    c8958d76db build(docs-infra): simplify updating dependencies in docs examples boilerplate (#38992)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+81.sha-b16a69c
+ * @license Angular v11.0.0-next.3+86.sha-c8958d7
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+81.sha-b16a69c');
+    const VERSION$1 = new Version('11.0.0-next.3+86.sha-c8958d7');
 
     /**
      * @license
@@ -33979,7 +33979,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+81.sha-b16a69c');
+    const VERSION$2 = new Version$1('11.0.0-next.3+86.sha-c8958d7');
 
     /**
      * @license
