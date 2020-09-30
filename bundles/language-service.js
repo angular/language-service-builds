a49b229d40aec4003d80bd6cf7d8b03d8b16f260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:59:45 +0000

    8b7acc4f8f refactor(compiler): Binding parser sets binding span as source span in Ivy (#39036)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+93.sha-e8084ff
+ * @license Angular v11.0.0-next.3+91.sha-8b7acc4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17945,7 +17945,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+93.sha-e8084ff');
+    const VERSION$1 = new Version('11.0.0-next.3+91.sha-8b7acc4');
 
     /**
      * @license
@@ -41077,7 +41077,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * Determines if two `TStylingKey`s are a match.
      *
-     * When computing whether a binding contains a duplicate, we need to compare if the instruction
+     * When computing weather a binding contains a duplicate, we need to compare if the instruction
      * `TStylingKey` has a match.
      *
      * Here are examples of `TStylingKey`s which match given `tStylingKeyCursor` is:
@@ -43996,7 +43996,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+93.sha-e8084ff');
+    const VERSION$2 = new Version$1('11.0.0-next.3+91.sha-8b7acc4');
 
     /**
      * @license
