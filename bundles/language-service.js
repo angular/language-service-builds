e65d8622adddc98227d78cdf9e66220242f8745d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:42:08 +0000

    1b70dc931d docs: edit template-statements doc (#38742)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+80.sha-a6971ba
+ * @license Angular v11.0.0-next.3+78.sha-1b70dc9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+80.sha-a6971ba');
+    const VERSION$1 = new Version('11.0.0-next.3+78.sha-1b70dc9');
 
     /**
      * @license
@@ -43995,7 +43995,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+80.sha-a6971ba');
+    const VERSION$2 = new Version$1('11.0.0-next.3+78.sha-1b70dc9');
 
     /**
      * @license
