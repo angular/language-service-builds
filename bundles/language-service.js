6f044f62221d6321f6a39cdac3014dac47ff5881
Joey Perrott <josephperrott@gmail.com>
2020-09-30 22:25:05 +0000

    ea1968317e fix(router): update getRouteGuards to check if the context outlet is activated (#39049)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+10.sha-5b15e5e
+ * @license Angular v11.0.0-next.4+11.sha-ea19683
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17945,7 +17945,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.4+10.sha-5b15e5e');
+    const VERSION$1 = new Version('11.0.0-next.4+11.sha-ea19683');
 
     /**
      * @license
@@ -43996,7 +43996,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.4+10.sha-5b15e5e');
+    const VERSION$2 = new Version$1('11.0.0-next.4+11.sha-ea19683');
 
     /**
      * @license
