877f93dfba90df0df36ca88098fb6d02cbb65867
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:44:23 +0000

    ded9aeb447 refactor(language-service): Move two-way binding logic to visitBoundEvent (#38985)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+86.sha-c8958d7
+ * @license Angular v11.0.0-next.3+82.sha-ded9aeb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+86.sha-c8958d7');
+    const VERSION$1 = new Version('11.0.0-next.3+82.sha-ded9aeb');
 
     /**
      * @license
@@ -43995,7 +43995,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+86.sha-c8958d7');
+    const VERSION$2 = new Version$1('11.0.0-next.3+82.sha-ded9aeb');
 
     /**
      * @license
