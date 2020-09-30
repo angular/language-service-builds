b54871b7f962fae09ea1f13835845e1ccd1f9b8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:51:03 +0000

    8f66540152 test(language-service): Update AppComponent test project to include more fields (#39033)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+82.sha-ded9aeb
+ * @license Angular v11.0.0-next.3+88.sha-8f66540
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+82.sha-ded9aeb');
+    const VERSION$1 = new Version('11.0.0-next.3+88.sha-8f66540');
 
     /**
      * @license
@@ -43995,7 +43995,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.3+82.sha-ded9aeb');
+    const VERSION$2 = new Version$1('11.0.0-next.3+88.sha-8f66540');
 
     /**
      * @license
