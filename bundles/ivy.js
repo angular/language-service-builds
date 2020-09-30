7a416d86e871266c76d72b0adcee7cef57ce3cf8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:56:50 +0000

    2aeaedc519 docs(docs-infra): replace a comma with a period at the end of a sentence (#39034)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+87.sha-8f11b51
+ * @license Angular v11.0.0-next.3+89.sha-2aeaedc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19147,7 +19147,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.3+87.sha-8f11b51');
+    const VERSION$1 = new Version('11.0.0-next.3+89.sha-2aeaedc');
 
     /**
      * @license
@@ -19740,7 +19740,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.3+87.sha-8f11b51');
+    const VERSION$2 = new Version('11.0.0-next.3+89.sha-2aeaedc');
 
     /**
      * @license
