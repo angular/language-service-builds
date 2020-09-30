a7e1e411e4a302014deb47f74d4420d2af8dcde3
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:14:24 +0000

    8e3710349d build(docs-infra): upgrade cli command docs sources to e526da605 (#39059)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+3.sha-eec9f4a
+ * @license Angular v11.0.0-next.4+2.sha-8e37103
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19148,7 +19148,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.4+3.sha-eec9f4a');
+    const VERSION$1 = new Version('11.0.0-next.4+2.sha-8e37103');
 
     /**
      * @license
@@ -19741,7 +19741,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.4+3.sha-eec9f4a');
+    const VERSION$2 = new Version('11.0.0-next.4+2.sha-8e37103');
 
     /**
      * @license
