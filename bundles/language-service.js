520238dd9efd3f2450acaccca25dccaf5e7d91c8
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:57:54 +0000

    7dd0db6d4f refactor(compiler-cli): implement `BabelAstFactory` and `AstHost`s (#38866)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+13.sha-21213e1
+ * @license Angular v11.0.0-next.4+16.sha-7dd0db6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17945,7 +17945,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.4+13.sha-21213e1');
+    const VERSION$1 = new Version('11.0.0-next.4+16.sha-7dd0db6');
 
     /**
      * @license
@@ -43996,7 +43996,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.4+13.sha-21213e1');
+    const VERSION$2 = new Version$1('11.0.0-next.4+16.sha-7dd0db6');
 
     /**
      * @license
