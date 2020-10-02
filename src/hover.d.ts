6eae420d124d81886f14ad5bf1e7264d0ce0fe60
Joey Perrott <josephperrott@gmail.com>
2020-10-02 19:11:38 +0000

    3bbbf39b58 feat(compiler): Recover on malformed keyed reads and keyed writes (#39004)

--- src/hover.d.ts
+++ src/hover.d.ts
@@ -9,6 +9,11 @@
 import { NgAnalyzedModules } from '@angular/compiler';
 import * as ts from 'typescript';
 import * as ng from './types';
+export declare const SYMBOL_SPACE: string;
+export declare const SYMBOL_PUNC: string;
+export declare const SYMBOL_TEXT: string;
+export declare const SYMBOL_INTERFACE: string;
+export declare const ALIAS_NAME: string;
 /**
  * Traverse the template AST and look for the symbol located at `position`, then
  * return the corresponding quick info.
@@ -24,3 +29,13 @@ export declare function getTemplateHover(info: ng.AstResult, position: number, a
  * @param analyzedModules all NgModules in the program.
  */
 export declare function getTsHover(position: number, declarations: ng.Declaration[], analyzedModules: NgAnalyzedModules): ts.QuickInfo | undefined;
+/**
+ * Construct a QuickInfo object taking into account its container and type.
+ * @param name Name of the QuickInfo target
+ * @param kind component, directive, pipe, etc.
+ * @param textSpan span of the target
+ * @param containerName either the Symbol's container or the NgModule that contains the directive
+ * @param type user-friendly name of the type
+ * @param documentation docstring or comment
+ */
+export declare function createQuickInfo(name: string, kind: string, textSpan: ts.TextSpan, containerName?: string, type?: string, documentation?: ts.SymbolDisplayPart[]): ts.QuickInfo;
