0412ffa343fc7cea564f7333debd3fa6439ea187
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:20:36 +0000

    072b707b38 ci: update to latest version of sauce-connect (#39073)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+24.sha-878e2f0
+ * @license Angular v10.1.4+27.sha-072b707
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19147,7 +19147,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.4+24.sha-878e2f0');
+    const VERSION$1 = new Version('10.1.4+27.sha-072b707');
 
     /**
      * @license
@@ -19740,7 +19740,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.4+24.sha-878e2f0');
+    const VERSION$2 = new Version('10.1.4+27.sha-072b707');
 
     /**
      * @license
