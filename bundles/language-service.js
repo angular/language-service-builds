77caf759af52e24bcfe67e455499899cbff71fc4
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:00:48 +0000

    07a66fc4c2 docs: remove explicit boolean type in examples as TS infers it automatically (#39081)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+52.sha-6e0a8eb
+ * @license Angular v11.0.0-next.4+53.sha-07a66fc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18026,7 +18026,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.4+52.sha-6e0a8eb');
+    const VERSION$1 = new Version('11.0.0-next.4+53.sha-07a66fc');
 
     /**
      * @license
@@ -34070,7 +34070,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.4+52.sha-6e0a8eb');
+    const VERSION$2 = new Version$1('11.0.0-next.4+53.sha-07a66fc');
 
     /**
      * @license
