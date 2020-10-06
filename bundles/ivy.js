b89df6b04eca9a32a2f7fd1c6a976b26222cf463
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:00:04 +0000

    6e0a8eb268 docs: add info about working with fixup commits (#39110)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+51.sha-21dfb33
+ * @license Angular v11.0.0-next.4+52.sha-6e0a8eb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19229,7 +19229,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.4+51.sha-21dfb33');
+    const VERSION$1 = new Version('11.0.0-next.4+52.sha-6e0a8eb');
 
     /**
      * @license
@@ -19822,7 +19822,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.4+51.sha-21dfb33');
+    const VERSION$2 = new Version('11.0.0-next.4+52.sha-6e0a8eb');
 
     /**
      * @license
