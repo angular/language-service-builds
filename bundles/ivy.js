1f2fac6d99873fc7fffff8659fe9a209e4658c45
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:58:06 +0000

    cc13b37446 build(docs-infra): render optional decorator options with a ? (#39167)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+33.sha-9f132d0
+ * @license Angular v10.1.4+36.sha-cc13b37
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19147,7 +19147,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.4+33.sha-9f132d0');
+    const VERSION$1 = new Version('10.1.4+36.sha-cc13b37');
 
     /**
      * @license
@@ -19740,7 +19740,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.4+33.sha-9f132d0');
+    const VERSION$2 = new Version('10.1.4+36.sha-cc13b37');
 
     /**
      * @license
