715bce894b938ef99cc853aac2ae1b3857974878
atscott <atscott01@gmail.com>
2020-10-08 19:18:46 +0000

    42a164f522 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#39182)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+7.sha-049b453
+ * @license Angular v11.0.0-next.5+8.sha-42a164f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19229,7 +19229,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+7.sha-049b453');
+    const VERSION$1 = new Version('11.0.0-next.5+8.sha-42a164f');
 
     /**
      * @license
@@ -19864,7 +19864,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.5+7.sha-049b453');
+    const VERSION$2 = new Version('11.0.0-next.5+8.sha-42a164f');
 
     /**
      * @license
