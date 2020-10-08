8eafff0ba74db8dffd7a99a8e9b61cc65d96dfc7
atscott <atscott01@gmail.com>
2020-10-08 16:09:55 +0000

    8cf16b4815 feat(docs-infra): add short url for strict mode guide (#39129)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+2.sha-eb34037
+ * @license Angular v10.1.5+4.sha-8cf16b4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.5+2.sha-eb34037');
+    const VERSION$1 = new Version('10.1.5+4.sha-8cf16b4');
 
     /**
      * @license
@@ -33979,7 +33979,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.5+2.sha-eb34037');
+    const VERSION$2 = new Version$1('10.1.5+4.sha-8cf16b4');
 
     /**
      * @license
