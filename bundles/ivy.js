5ede4ae0177273de5267dbc8199a1248edf0518b
atscott <atscott01@gmail.com>
2020-10-09 17:01:52 +0000

    b2342d4116 feat(dev-infra): Add github links to caretaker checks (#39185)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+15.sha-7f689a2
+ * @license Angular v11.0.0-next.5+16.sha-b2342d4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+15.sha-7f689a2');
+    const VERSION$1 = new Version('11.0.0-next.5+16.sha-b2342d4');
 
     /**
      * @license
@@ -19926,7 +19926,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.5+15.sha-7f689a2');
+    const VERSION$2 = new Version('11.0.0-next.5+16.sha-b2342d4');
 
     /**
      * @license
