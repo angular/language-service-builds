f276387cb69c6d74c3ec0481facdb548f8af790a
atscott <atscott01@gmail.com>
2020-10-09 15:22:32 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+6.sha-7628c36
+ * @license Angular v10.1.5+7.sha-8913aee
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.5+6.sha-7628c36');
+    const VERSION$1 = new Version('10.1.5+7.sha-8913aee');
 
     /**
      * @license
@@ -33979,7 +33979,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.5+6.sha-7628c36');
+    const VERSION$2 = new Version$1('10.1.5+7.sha-8913aee');
 
     /**
      * @license
