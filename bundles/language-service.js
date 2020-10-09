edd734803401b095665634119aed7116a6703eb8
atscott <atscott01@gmail.com>
2020-10-09 17:04:12 +0000

    b2342d4116 feat(dev-infra): Add github links to caretaker checks (#39185)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+15.sha-7f689a2
+ * @license Angular v11.0.0-next.5+16.sha-b2342d4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+15.sha-7f689a2');
+    const VERSION$1 = new Version('11.0.0-next.5+16.sha-b2342d4');
 
     /**
      * @license
@@ -44140,7 +44140,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.5+15.sha-7f689a2');
+    const VERSION$2 = new Version$1('11.0.0-next.5+16.sha-b2342d4');
 
     /**
      * @license
