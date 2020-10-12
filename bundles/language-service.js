446771c9b025bf866b2df290f77aa7a15caef620
atscott <atscott01@gmail.com>
2020-10-12 16:04:49 +0000

    2d37e47e95 build(docs-infra): support passing args to `ng serve` via `serve-and-sync` (#39201)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+10.sha-952fd86
+ * @license Angular v10.1.5+12.sha-2d37e47
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17960,7 +17960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.5+10.sha-952fd86');
+    const VERSION$1 = new Version('10.1.5+12.sha-2d37e47');
 
     /**
      * @license
@@ -33995,7 +33995,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.5+10.sha-952fd86');
+    const VERSION$2 = new Version$1('10.1.5+12.sha-2d37e47');
 
     /**
      * @license
