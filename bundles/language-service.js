64fcb4a03729bf6c9e7c1a9b7612208574a1133d
atscott <atscott01@gmail.com>
2020-10-12 20:04:00 +0000

    3487d5a13a docs: move Inputs and Outputs topic to Components section (#38984)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+17.sha-1efc75c
+ * @license Angular v10.1.5+18.sha-3487d5a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17960,7 +17960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.5+17.sha-1efc75c');
+    const VERSION$1 = new Version('10.1.5+18.sha-3487d5a');
 
     /**
      * @license
@@ -33995,7 +33995,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.5+17.sha-1efc75c');
+    const VERSION$2 = new Version$1('10.1.5+18.sha-3487d5a');
 
     /**
      * @license
