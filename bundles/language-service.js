3819262008ccc05f37c9af2f7c99dc1bdb717fbf
atscott <atscott01@gmail.com>
2020-10-12 15:32:39 +0000

    a84976fdfc feat(language-service): Add getTypeDefinitionAtPosition (go to type definition) (#39145)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+17.sha-9f3388e
+ * @license Angular v11.0.0-next.5+19.sha-a84976f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+17.sha-9f3388e');
+    const VERSION$1 = new Version('11.0.0-next.5+19.sha-a84976f');
 
     /**
      * @license
@@ -34124,7 +34124,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.5+17.sha-9f3388e');
+    const VERSION$2 = new Version$1('11.0.0-next.5+19.sha-a84976f');
 
     /**
      * @license
