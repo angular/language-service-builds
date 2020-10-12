b34980d0b4d172604f42fed58eeea625f82fde3b
atscott <atscott01@gmail.com>
2020-10-12 20:19:57 +0000

    b0b4953fd6 fix(router): Allow undefined inputs on routerLink (#39151)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+46.sha-72f4497
+ * @license Angular v11.0.0-next.5+47.sha-b0b4953
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+46.sha-72f4497');
+    const VERSION$1 = new Version('11.0.0-next.5+47.sha-b0b4953');
 
     /**
      * @license
@@ -19926,7 +19926,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.5+46.sha-72f4497');
+    const VERSION$2 = new Version('11.0.0-next.5+47.sha-b0b4953');
 
     /**
      * @license
