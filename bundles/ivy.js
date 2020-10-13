0fdc0f644f63a97c0a893ee854958d92414c8162
atscott <atscott01@gmail.com>
2020-10-13 22:22:14 +0000

    b642f0bf45 docs(zone.js): update changelog to add jest useFakeTimers feature (#39126)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+51.sha-0875fd2
+ * @license Angular v11.0.0-next.5+53.sha-b642f0b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+51.sha-0875fd2');
+    const VERSION$1 = new Version('11.0.0-next.5+53.sha-b642f0b');
 
     /**
      * @license
@@ -19926,7 +19926,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.5+51.sha-0875fd2');
+    const VERSION$2 = new Version('11.0.0-next.5+53.sha-b642f0b');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+51.sha-0875fd2
+ * @license Angular v11.0.0-next.5+53.sha-b642f0b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+51.sha-0875fd2');
+    const VERSION$1 = new Version('11.0.0-next.5+53.sha-b642f0b');
 
     /**
      * @license
@@ -44140,7 +44140,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.5+51.sha-0875fd2');
+    const VERSION$2 = new Version$1('11.0.0-next.5+53.sha-b642f0b');
 
     /**
      * @license
