670ee8c2c5bb86916ce11945b27df907bf1c1c91
atscott <atscott01@gmail.com>
2020-10-14 22:03:56 +0000

    822b838fbc fix(ngcc): ensure that "inline exports" can be interpreted correctly (#39267)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+5.sha-a83693d
+ * @license Angular v11.0.0-next.6+10.sha-822b838
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+5.sha-a83693d');
+    const VERSION$1 = new Version('11.0.0-next.6+10.sha-822b838');
 
     /**
      * @license
@@ -45985,7 +45985,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+5.sha-a83693d');
+    const VERSION$2 = new Version$1('11.0.0-next.6+10.sha-822b838');
 
     /**
      * @license
