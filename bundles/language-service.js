deb3375dd017f4bbea5a74490c8b3612d5f5449d
atscott <atscott01@gmail.com>
2020-10-14 18:13:41 +0000

    c6350116b5 docs: add Discord as an official communication channel (#39149)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+67.sha-f6d5cdf
+ * @license Angular v11.0.0-next.5+69.sha-c635011
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+67.sha-f6d5cdf');
+    const VERSION$1 = new Version('11.0.0-next.5+69.sha-c635011');
 
     /**
      * @license
@@ -44219,7 +44219,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.5+67.sha-f6d5cdf');
+    const VERSION$2 = new Version$1('11.0.0-next.5+69.sha-c635011');
 
     /**
      * @license
