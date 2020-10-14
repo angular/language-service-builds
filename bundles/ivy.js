6cb9a7742ddc5074c5caaea17af3a6be4f0e31d4
atscott <atscott01@gmail.com>
2020-10-14 21:24:29 +0000

    a5d8d03edb release: cut the v11.0.0-next.6 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+74.sha-0ec7043
+ * @license Angular v11.0.0-next.6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.5+74.sha-0ec7043');
+    const VERSION$1 = new Version('11.0.0-next.6');
 
     /**
      * @license
@@ -19926,7 +19926,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.5+74.sha-0ec7043');
+    const VERSION$2 = new Version('11.0.0-next.6');
 
     /**
      * @license
