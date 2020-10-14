fee95b9d265ca2a9549300a09729cbbe4518f40b
atscott <atscott01@gmail.com>
2020-10-14 16:58:32 +0000

    5221df8e92 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+20.sha-23fc2b4
+ * @license Angular v10.1.5+22.sha-5221df8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17960,7 +17960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.5+20.sha-23fc2b4');
+    const VERSION$1 = new Version('10.1.5+22.sha-5221df8');
 
     /**
      * @license
@@ -33995,7 +33995,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.5+20.sha-23fc2b4');
+    const VERSION$2 = new Version$1('10.1.5+22.sha-5221df8');
 
     /**
      * @license
