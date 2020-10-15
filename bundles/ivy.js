ba43611daa19308bd6b797b895e81b69b2d731e5
atscott <atscott01@gmail.com>
2020-10-15 17:18:15 +0000

    72cdd42278 docs: Update output directory name in Deployment guide (#39247)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6
+ * @license Angular v10.1.6+7.sha-72cdd42
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19163,7 +19163,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.6');
+    const VERSION$1 = new Version('10.1.6+7.sha-72cdd42');
 
     /**
      * @license
@@ -19756,7 +19756,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('10.1.6');
+    const VERSION$2 = new Version('10.1.6+7.sha-72cdd42');
 
     /**
      * @license
