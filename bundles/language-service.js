65e36ff2db3e552748449b163c4d075f8ac54559
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:28:05 +0000

    79d67dc1f4 refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+42.sha-b5ec5a7
+ * @license Angular v11.0.0-next.6+43.sha-79d67dc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+42.sha-b5ec5a7');
+    const VERSION$1 = new Version('11.0.0-next.6+43.sha-79d67dc');
 
     /**
      * @license
@@ -34458,7 +34458,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+42.sha-b5ec5a7');
+    const VERSION$2 = new Version$1('11.0.0-next.6+43.sha-79d67dc');
 
     /**
      * @license
