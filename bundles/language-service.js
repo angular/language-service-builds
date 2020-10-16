81031355dff92b3f9c59168263d6eed33294b488
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:41:34 +0000

    34b74cecb6 build(docs-infra): add a tool to create new examples (#39283)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+33.sha-81aa119
+ * @license Angular v11.0.0-next.6+34.sha-34b74ce
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18190,7 +18190,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+33.sha-81aa119');
+    const VERSION$1 = new Version('11.0.0-next.6+34.sha-34b74ce');
 
     /**
      * @license
@@ -34408,7 +34408,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+33.sha-81aa119');
+    const VERSION$2 = new Version$1('11.0.0-next.6+34.sha-34b74ce');
 
     /**
      * @license
