e2784d818a45c1adc991d11bd28c956989a9a46a
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:41:23 +0000

    81aa119739 fix(core): convert legacy-sanitized values to Trusted Types (#39218)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+29.sha-765fa33
+ * @license Angular v11.0.0-next.6+33.sha-81aa119
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18190,7 +18190,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+29.sha-765fa33');
+    const VERSION$1 = new Version('11.0.0-next.6+33.sha-81aa119');
 
     /**
      * @license
@@ -34408,7 +34408,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+29.sha-765fa33');
+    const VERSION$2 = new Version$1('11.0.0-next.6+33.sha-81aa119');
 
     /**
      * @license
