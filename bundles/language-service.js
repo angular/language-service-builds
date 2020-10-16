9fe0e28fb31d5834db28bc55a680f39a3bc04429
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:10:50 +0000

    8fd59dabba ci: separate the windows CI tests into build and test (#39289)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+20.sha-d8e313b
+ * @license Angular v10.1.6+21.sha-8fd59da
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17960,7 +17960,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.1.6+20.sha-d8e313b');
+    const VERSION$1 = new Version('10.1.6+21.sha-8fd59da');
 
     /**
      * @license
@@ -34101,7 +34101,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.1.6+20.sha-d8e313b');
+    const VERSION$2 = new Version$1('10.1.6+21.sha-8fd59da');
 
     /**
      * @license
