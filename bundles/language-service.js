4f74d6300fd8423e6f04fb40159d0cfbb496ac32
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:57:47 +0000

    563fb6cdbe feat(language-service): Implement go to definition for style and template urls (#39202)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+41.sha-1b21350
+ * @license Angular v11.0.0-next.6+39.sha-563fb6c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+41.sha-1b21350');
+    const VERSION$1 = new Version('11.0.0-next.6+39.sha-563fb6c');
 
     /**
      * @license
@@ -34458,7 +34458,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+41.sha-1b21350');
+    const VERSION$2 = new Version$1('11.0.0-next.6+39.sha-563fb6c');
 
     /**
      * @license
