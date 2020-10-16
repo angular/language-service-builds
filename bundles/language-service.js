efb111ec4f37c3c04cbfc37c7e9dab5802975c4b
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:30:27 +0000

    492e236ae6 docs: Typos fixes in built-in directives guide (#38520)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+36.sha-4dc8d83
+ * @license Angular v11.0.0-next.6+35.sha-492e236
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18190,7 +18190,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+36.sha-4dc8d83');
+    const VERSION$1 = new Version('11.0.0-next.6+35.sha-492e236');
 
     /**
      * @license
@@ -34408,7 +34408,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+36.sha-4dc8d83');
+    const VERSION$2 = new Version$1('11.0.0-next.6+35.sha-492e236');
 
     /**
      * @license
