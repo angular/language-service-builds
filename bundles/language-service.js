f0fba89a10944bd827bc18919559f84e1fab04eb
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:44:34 +0000

    a38293d213 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+35.sha-492e236
+ * @license Angular v11.0.0-next.6+37.sha-a38293d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18190,7 +18190,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+35.sha-492e236');
+    const VERSION$1 = new Version('11.0.0-next.6+37.sha-a38293d');
 
     /**
      * @license
@@ -46219,7 +46219,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+35.sha-492e236');
+    const VERSION$2 = new Version$1('11.0.0-next.6+37.sha-a38293d');
 
     /**
      * @license
