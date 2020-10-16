1ad555b949f7c3717b8972fe9a040e09c2fa2ef6
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:23:36 +0000

    96f59f64a0 docs: Very minor spelling mistake (#39299)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+24.sha-5bda62c
+ * @license Angular v11.0.0-next.6+25.sha-96f59f6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18104,7 +18104,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+24.sha-5bda62c');
+    const VERSION$1 = new Version('11.0.0-next.6+25.sha-96f59f6');
 
     /**
      * @license
@@ -34320,7 +34320,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+24.sha-5bda62c');
+    const VERSION$2 = new Version$1('11.0.0-next.6+25.sha-96f59f6');
 
     /**
      * @license
