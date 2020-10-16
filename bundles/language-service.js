38e1995210bf1a1b4834a722cc1d6d481ed1b005
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:56:57 +0000

    1b21350e17 test(language-service): Make project service a singleton (#39308)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+40.sha-0c01c4a
+ * @license Angular v11.0.0-next.6+41.sha-1b21350
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+40.sha-0c01c4a');
+    const VERSION$1 = new Version('11.0.0-next.6+41.sha-1b21350');
 
     /**
      * @license
@@ -34458,7 +34458,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+40.sha-0c01c4a');
+    const VERSION$2 = new Version$1('11.0.0-next.6+41.sha-1b21350');
 
     /**
      * @license
