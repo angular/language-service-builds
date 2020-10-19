4ccb76b4fe5877cb1922c5c05212e1cf48b74f8c
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:48:04 +0000

    b304bd0535 docs: change definition of NgModules in Angular concepts guide (#36179)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+48.sha-0268b72
+ * @license Angular v11.0.0-next.6+49.sha-b304bd0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19393,7 +19393,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+48.sha-0268b72');
+    const VERSION$1 = new Version('11.0.0-next.6+49.sha-b304bd0');
 
     /**
      * @license
@@ -20028,7 +20028,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+48.sha-0268b72');
+    const VERSION$2 = new Version('11.0.0-next.6+49.sha-b304bd0');
 
     /**
      * @license
