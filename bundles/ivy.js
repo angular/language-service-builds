0f2278add8cd03469a7ff3ee1dcf2f912115cf2d
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:30:47 +0000

    767fdccf59 feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+50.sha-7e742ae
+ * @license Angular v11.0.0-next.6+51.sha-767fdcc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19393,7 +19393,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+50.sha-7e742ae');
+    const VERSION$1 = new Version('11.0.0-next.6+51.sha-767fdcc');
 
     /**
      * @license
@@ -20028,7 +20028,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+50.sha-7e742ae');
+    const VERSION$2 = new Version('11.0.0-next.6+51.sha-767fdcc');
 
     /**
      * @license
