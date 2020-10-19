0968e54ec09b728eedfd3e356e884e499a5d67a8
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:57:15 +0000

    41c2228d49 docs: clarify grammatical error (#39279)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+53.sha-f5a3e44
+ * @license Angular v11.0.0-next.6+52.sha-41c2228
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+53.sha-f5a3e44');
+    const VERSION$1 = new Version('11.0.0-next.6+52.sha-41c2228');
 
     /**
      * @license
@@ -34458,7 +34458,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+53.sha-f5a3e44');
+    const VERSION$2 = new Version$1('11.0.0-next.6+52.sha-41c2228');
 
     /**
      * @license
