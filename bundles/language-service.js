483323419956e7c0672653db2f0f11637444efa1
Andrew Kushnir <akushnir@google.com>
2020-10-20 23:53:03 +0000

    cfac8e0764 build: upgrade karma to version 4.4.0 (#39180)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+60.sha-9fb7bde
+ * @license Angular v11.0.0-next.6+61.sha-cfac8e0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+60.sha-9fb7bde');
+    const VERSION$1 = new Version('11.0.0-next.6+61.sha-cfac8e0');
 
     /**
      * @license
@@ -34458,7 +34458,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+60.sha-9fb7bde');
+    const VERSION$2 = new Version$1('11.0.0-next.6+61.sha-cfac8e0');
 
     /**
      * @license
