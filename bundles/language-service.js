0725a4a90333533ec89f23b9cff3f941986daa01
Andrew Kushnir <akushnir@google.com>
2020-10-20 00:10:49 +0000

    7040f1714b docs: add Jessica Janiuk to contributors.json (#39253)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+52.sha-41c2228
+ * @license Angular v11.0.0-next.6+54.sha-7040f17
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+52.sha-41c2228');
+    const VERSION$1 = new Version('11.0.0-next.6+54.sha-7040f17');
 
     /**
      * @license
@@ -46269,7 +46269,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+52.sha-41c2228');
+    const VERSION$2 = new Version$1('11.0.0-next.6+54.sha-7040f17');
 
     /**
      * @license
