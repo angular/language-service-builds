e263ca08c76e3410c811beeb2dc3e47035ae69a9
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:22:00 +0000

    595062e415 docs: Add Component Overview topic to angular.io (#39186)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+43.sha-5167eea
+ * @license Angular v11.0.0-next.6+46.sha-595062e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18104,7 +18104,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+43.sha-5167eea');
+    const VERSION$1 = new Version('11.0.0-next.6+46.sha-595062e');
 
     /**
      * @license
@@ -34322,7 +34322,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+43.sha-5167eea');
+    const VERSION$2 = new Version$1('11.0.0-next.6+46.sha-595062e');
 
     /**
      * @license
