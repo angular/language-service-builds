bd6c09011254bac807bb56d34f663250d7d6282f
Andrew Kushnir <akushnir@google.com>
2020-10-20 20:49:39 +0000

    a7964f4eca fix(common): update locales using new CLDR data (#39343)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+57.sha-a67895c
+ * @license Angular v11.0.0-next.6+59.sha-a7964f4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+57.sha-a67895c');
+    const VERSION$1 = new Version('11.0.0-next.6+59.sha-a7964f4');
 
     /**
      * @license
@@ -46269,7 +46269,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+57.sha-a67895c');
+    const VERSION$2 = new Version$1('11.0.0-next.6+59.sha-a7964f4');
 
     /**
      * @license
