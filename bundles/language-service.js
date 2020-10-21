3793f251a5486469201f6cc6a9d443213879ed0d
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:02:12 +0000

    b4e8399144 fix(platform-server): Resolve absolute URL from baseUrl (#39334)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+54.sha-d8c9f42
+ * @license Angular v11.0.0-next.6+55.sha-b4e8399
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18104,7 +18104,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+54.sha-d8c9f42');
+    const VERSION$1 = new Version('11.0.0-next.6+55.sha-b4e8399');
 
     /**
      * @license
@@ -34322,7 +34322,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+54.sha-d8c9f42');
+    const VERSION$2 = new Version$1('11.0.0-next.6+55.sha-b4e8399');
 
     /**
      * @license
