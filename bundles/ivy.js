970544fa812b73962131a4c7295856fcde3f0dd6
Andrew Kushnir <akushnir@google.com>
2020-10-21 18:43:21 +0000

    3525b2fa28 docs: release notes for the v10.2.0 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+66.sha-7768aeb
+ * @license Angular v11.0.0-next.6+67.sha-3525b2f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19393,7 +19393,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+66.sha-7768aeb');
+    const VERSION$1 = new Version('11.0.0-next.6+67.sha-3525b2f');
 
     /**
      * @license
@@ -20028,7 +20028,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+66.sha-7768aeb');
+    const VERSION$2 = new Version('11.0.0-next.6+67.sha-3525b2f');
 
     /**
      * @license
