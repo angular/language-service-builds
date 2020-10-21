9c03cfd682546fff65b452d61f027bd7a0a5c35f
Andrew Kushnir <akushnir@google.com>
2020-10-21 19:26:52 +0000

    eb4c05d97a build: update bazelversion (#39351)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+67.sha-3525b2f
+ * @license Angular v11.0.0-next.6+69.sha-eb4c05d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18354,7 +18354,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+67.sha-3525b2f');
+    const VERSION$1 = new Version('11.0.0-next.6+69.sha-eb4c05d');
 
     /**
      * @license
@@ -34458,7 +34458,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+67.sha-3525b2f');
+    const VERSION$2 = new Version$1('11.0.0-next.6+69.sha-eb4c05d');
 
     /**
      * @license
