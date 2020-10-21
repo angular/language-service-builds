a6cff42ce44e20a6c77287f7a668b7164c766049
Andrew Kushnir <akushnir@google.com>
2020-10-21 21:15:39 +0000

    7f79e77c66 build: update bazelversion (#39351)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+55.sha-b4e8399
+ * @license Angular v11.0.0-rc.0+1.sha-7f79e77
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19307,7 +19307,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+55.sha-b4e8399');
+    const VERSION$1 = new Version('11.0.0-rc.0+1.sha-7f79e77');
 
     /**
      * @license
@@ -19942,7 +19942,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+55.sha-b4e8399');
+    const VERSION$2 = new Version('11.0.0-rc.0+1.sha-7f79e77');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+55.sha-b4e8399
+ * @license Angular v11.0.0-rc.0+1.sha-7f79e77
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18104,7 +18104,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+55.sha-b4e8399');
+    const VERSION$1 = new Version('11.0.0-rc.0+1.sha-7f79e77');
 
     /**
      * @license
@@ -34322,7 +34322,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+55.sha-b4e8399');
+    const VERSION$2 = new Version$1('11.0.0-rc.0+1.sha-7f79e77');
 
     /**
      * @license
