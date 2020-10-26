0fb04c54f2fe9c6e774043caf50afaf7ee152edc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 17:38:54 +0000

    90c0772743 fix(zone.js): remove global declaration (#37861)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+95.sha-b989ba2
+ * @license Angular v11.0.0-next.6+99.sha-90c0772
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+95.sha-b989ba2');
+    const VERSION$1 = new Version('11.0.0-next.6+99.sha-90c0772');
 
     /**
      * @license
@@ -34639,7 +34639,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+95.sha-b989ba2');
+    const VERSION$2 = new Version$1('11.0.0-next.6+99.sha-90c0772');
 
     /**
      * @license
