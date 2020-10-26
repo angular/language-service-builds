90a4da7a66bb8a286580fea22dac6057d8622e50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:17:52 +0000

    3236ae0ee1 docs: fix typo in component overview (#39425)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+103.sha-35fdc6d
+ * @license Angular v11.0.0-next.6+105.sha-3236ae0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+103.sha-35fdc6d');
+    const VERSION$1 = new Version('11.0.0-next.6+105.sha-3236ae0');
 
     /**
      * @license
@@ -46762,7 +46762,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+103.sha-35fdc6d');
+    const VERSION$2 = new Version$1('11.0.0-next.6+105.sha-3236ae0');
 
     /**
      * @license
