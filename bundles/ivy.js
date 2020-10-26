b496039a8a49aa7b4b64ce2d30a91fb484212e49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:11:35 +0000

    cf60e0e889 docs: use SimpleChanges in component interaction guide (#39342)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+99.sha-90c0772
+ * @license Angular v11.0.0-next.6+100.sha-cf60e0e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19402,7 +19402,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+99.sha-90c0772');
+    const VERSION$1 = new Version('11.0.0-next.6+100.sha-cf60e0e');
 
     /**
      * @license
@@ -20037,7 +20037,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+99.sha-90c0772');
+    const VERSION$2 = new Version('11.0.0-next.6+100.sha-cf60e0e');
 
     /**
      * @license
