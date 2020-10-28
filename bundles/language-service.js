16664595dc386113dc120297baece2b193e5dc60
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:25:47 +0000

    20c26cd49b build: add PullApprove: disable as a caretaker note label (#39430)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+137.sha-1508fd2
+ * @license Angular v11.0.0-next.6+140.sha-20c26cd
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+137.sha-1508fd2');
+    const VERSION$1 = new Version('11.0.0-next.6+140.sha-20c26cd');
 
     /**
      * @license
@@ -46748,7 +46748,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+137.sha-1508fd2');
+    const VERSION$2 = new Version$1('11.0.0-next.6+140.sha-20c26cd');
 
     /**
      * @license
