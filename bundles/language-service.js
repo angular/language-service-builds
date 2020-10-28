e994c888c7a309c925117e9c66012514c8fda818
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:36:00 +0000

    ddc070e778 docs(core): add jessicajaniuk to pullapprove list (#39473)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+60.sha-366be11
+ * @license Angular v11.0.0-rc.0+70.sha-ddc070e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18105,7 +18105,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.0+60.sha-366be11');
+    const VERSION$1 = new Version('11.0.0-rc.0+70.sha-ddc070e');
 
     /**
      * @license
@@ -34503,7 +34503,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-rc.0+60.sha-366be11');
+    const VERSION$2 = new Version$1('11.0.0-rc.0+70.sha-ddc070e');
 
     /**
      * @license
