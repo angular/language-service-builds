3de8aaeace37c009f171754504e15059803edb2f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:39:59 +0000

    bf05c979a7 release: cut the v11.0.0-rc.1 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+70.sha-ddc070e
+ * @license Angular v11.0.0-rc.1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19308,7 +19308,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.0+70.sha-ddc070e');
+    const VERSION$1 = new Version('11.0.0-rc.1');
 
     /**
      * @license
@@ -19943,7 +19943,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-rc.0+70.sha-ddc070e');
+    const VERSION$2 = new Version('11.0.0-rc.1');
 
     /**
      * @license
