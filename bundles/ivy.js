02fa2f20631ded348026b0878de9d98916652ba1
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:02:31 +0000

    60e377ed77 docs: archive user-input topic (#39309)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+147.sha-bdec818
+ * @license Angular v11.0.0-next.6+148.sha-60e377e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19400,7 +19400,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+147.sha-bdec818');
+    const VERSION$1 = new Version('11.0.0-next.6+148.sha-60e377e');
 
     /**
      * @license
@@ -20035,7 +20035,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+147.sha-bdec818');
+    const VERSION$2 = new Version('11.0.0-next.6+148.sha-60e377e');
 
     /**
      * @license
