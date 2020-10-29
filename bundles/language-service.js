23a878b6c8e7896d3e9046746acdc0697553ef69
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:18:20 +0000

    e45da28ed5 build: update to CLI 11.0.0-rc.1 (#39432)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+10.sha-ec416d0
+ * @license Angular v11.0.0-rc.1+12.sha-e45da28
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18105,7 +18105,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.1+10.sha-ec416d0');
+    const VERSION$1 = new Version('11.0.0-rc.1+12.sha-e45da28');
 
     /**
      * @license
@@ -34503,7 +34503,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-rc.1+10.sha-ec416d0');
+    const VERSION$2 = new Version$1('11.0.0-rc.1+12.sha-e45da28');
 
     /**
      * @license
