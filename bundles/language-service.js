fea0760a17798b0462883f61d3667d1b965f00cd
Joey Perrott <josephperrott@gmail.com>
2020-10-29 19:46:53 +0000

    8d324ec314 ci: update NgBot config to include more labels that indicate that ticket is triaged (#39494)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+157.sha-f54662e
+ * @license Angular v11.0.0-next.6+158.sha-8d324ec
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+157.sha-f54662e');
+    const VERSION$1 = new Version('11.0.0-next.6+158.sha-8d324ec');
 
     /**
      * @license
@@ -34645,7 +34645,7 @@ define(['exports', 'path', 'typescript/lib/tsserverlibrary', 'typescript'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+157.sha-f54662e');
+    const VERSION$2 = new Version$1('11.0.0-next.6+158.sha-8d324ec');
 
     /**
      * @license
