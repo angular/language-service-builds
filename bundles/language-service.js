910464522d3474d6e8da3f605adae79d5458bb89
Joey Perrott <josephperrott@gmail.com>
2020-10-29 16:05:37 +0000

    eccf9d68b8 docs: Match browser name with custom launcher name (#39480)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+3.sha-b8c71c4
+ * @license Angular v11.0.0-rc.1+4.sha-eccf9d6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18105,7 +18105,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.1+3.sha-b8c71c4');
+    const VERSION$1 = new Version('11.0.0-rc.1+4.sha-eccf9d6');
 
     /**
      * @license
@@ -34503,7 +34503,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-rc.1+3.sha-b8c71c4');
+    const VERSION$2 = new Version$1('11.0.0-rc.1+4.sha-eccf9d6');
 
     /**
      * @license
