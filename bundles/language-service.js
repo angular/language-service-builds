bae148010ee9cb056758c50c7ce1ac70a2e30546
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:59:02 +0000

    5f1e9758f5 build(docs-infra): ensure that deployment works on CI (#39535)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+187.sha-ad62eda
+ * @license Angular v11.0.0-next.6+188.sha-5f1e975
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18257,7 +18257,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+187.sha-ad62eda');
+    const VERSION$1 = new Version('11.0.0-next.6+188.sha-5f1e975');
 
     /**
      * @license
@@ -34666,7 +34666,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+187.sha-ad62eda');
+    const VERSION$2 = new Version$1('11.0.0-next.6+188.sha-5f1e975');
 
     /**
      * @license
