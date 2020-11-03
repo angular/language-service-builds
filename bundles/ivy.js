79f30b94ab0514531e3c930d3dde3dac34c045d3
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:02:41 +0000

    b6064c2782 fix(dev-infra): run fetches in the git client in quiet mode (#39503)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+35.sha-a0b6548
+ * @license Angular v11.0.0-rc.1+37.sha-b6064c2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19345,7 +19345,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.1+35.sha-a0b6548');
+    const VERSION$1 = new Version('11.0.0-rc.1+37.sha-b6064c2');
 
     /**
      * @license
@@ -19980,7 +19980,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-rc.1+35.sha-a0b6548');
+    const VERSION$2 = new Version('11.0.0-rc.1+37.sha-b6064c2');
 
     /**
      * @license
