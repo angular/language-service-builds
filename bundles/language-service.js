f3b0256edde47cb9e732f85784004743d88bdd1b
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:12:12 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+15.sha-71d0063
+ * @license Angular v10.2.1+29.sha-aa3bd27
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17963,7 +17963,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.2.1+15.sha-71d0063');
+    const VERSION$1 = new Version('10.2.1+29.sha-aa3bd27');
 
     /**
      * @license
@@ -34115,7 +34115,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.2.1+15.sha-71d0063');
+    const VERSION$2 = new Version$1('10.2.1+29.sha-aa3bd27');
 
     /**
      * @license
