9627e81769f269ae9a8b7f503152bc0c7ad7953f
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:12:16 +0000

    d34e36cc95 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+46.sha-96ad439
+ * @license Angular v11.0.0-rc.2+1.sha-d34e36c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19345,7 +19345,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.1+46.sha-96ad439');
+    const VERSION$1 = new Version('11.0.0-rc.2+1.sha-d34e36c');
 
     /**
      * @license
@@ -19980,7 +19980,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-rc.1+46.sha-96ad439');
+    const VERSION$2 = new Version('11.0.0-rc.2+1.sha-d34e36c');
 
     /**
      * @license
