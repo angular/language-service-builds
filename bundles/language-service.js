830d93b475a4ab684eadf334a1bf8f1d8f57a8e5
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:43:08 +0000

    1e91f84697 docs: fix Android 10 codename (#39399)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+193.sha-d0dd0e8
+ * @license Angular v11.0.0-next.6+194.sha-1e91f84
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18257,7 +18257,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+193.sha-d0dd0e8');
+    const VERSION$1 = new Version('11.0.0-next.6+194.sha-1e91f84');
 
     /**
      * @license
@@ -46771,7 +46771,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+193.sha-d0dd0e8');
+    const VERSION$2 = new Version$1('11.0.0-next.6+194.sha-1e91f84');
 
     /**
      * @license
