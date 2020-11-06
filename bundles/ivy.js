912278485ab36d74cb30dc9ee9ce2447e5cb35d6
Misko Hevery <misko@hevery.com>
2020-11-06 23:58:50 +0000

    c68ca49d0b fix(compiler-cli): avoid duplicate diagnostics about unknown pipes (#39517)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.2+14.sha-dd28855
+ * @license Angular v11.0.0-rc.2+15.sha-c68ca49
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19375,7 +19375,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.2+14.sha-dd28855');
+    const VERSION$1 = new Version('11.0.0-rc.2+15.sha-c68ca49');
 
     /**
      * @license
@@ -20010,7 +20010,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-rc.2+14.sha-dd28855');
+    const VERSION$2 = new Version('11.0.0-rc.2+15.sha-c68ca49');
 
     /**
      * @license
@@ -32813,6 +32813,11 @@ Either add the @Injectable() decorator to '${provider.node.name
         constructor(resolver) {
             this.resolver = resolver;
             this._diagnostics = [];
+            /**
+             * Tracks which `BindingPipe` nodes have already been recorded as invalid, so only one diagnostic
+             * is ever produced per node.
+             */
+            this.recordedPipes = new Set();
         }
         get diagnostics() {
             return this._diagnostics;
@@ -32824,6 +32829,9 @@ Either add the @Injectable() decorator to '${provider.node.name
             this._diagnostics.push(makeTemplateDiagnostic(templateId, mapping, ref.valueSpan || ref.sourceSpan, ts.DiagnosticCategory.Error, ngErrorCode(ErrorCode.MISSING_REFERENCE_TARGET), errorMsg));
         }
         missingPipe(templateId, ast) {
+            if (this.recordedPipes.has(ast)) {
+                return;
+            }
             const mapping = this.resolver.getSourceMapping(templateId);
             const errorMsg = `No pipe found with name '${ast.name}'.`;
             const sourceSpan = this.resolver.toParseSourceSpan(templateId, ast.nameSpan);
@@ -32831,6 +32839,7 @@ Either add the @Injectable() decorator to '${provider.node.name
                 throw new Error(`Assertion failure: no SourceLocation found for usage of pipe '${ast.name}'.`);
             }
             this._diagnostics.push(makeTemplateDiagnostic(templateId, mapping, sourceSpan, ts.DiagnosticCategory.Error, ngErrorCode(ErrorCode.MISSING_PIPE), errorMsg));
+            this.recordedPipes.add(ast);
         }
         illegalAssignmentToTemplateVar(templateId, assignment, target) {
             const mapping = this.resolver.getSourceMapping(templateId);
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.2+14.sha-dd28855
+ * @license Angular v11.0.0-rc.2+15.sha-c68ca49
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18172,7 +18172,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-rc.2+14.sha-dd28855');
+    const VERSION$1 = new Version('11.0.0-rc.2+15.sha-c68ca49');
 
     /**
      * @license
@@ -34686,7 +34686,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-rc.2+14.sha-dd28855');
+    const VERSION$2 = new Version$1('11.0.0-rc.2+15.sha-c68ca49');
 
     /**
      * @license
