80f4f450d7eb9edb44552ee84aba4828c125eb75
Misko Hevery <misko@hevery.com>
2020-11-06 17:53:05 +0000

    b9b9178458 fix(compiler-cli): do not drop non-Angular decorators when downleveling (#39577)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+216.sha-3a1d36c
+ * @license Angular v11.0.0-next.6+217.sha-b9b9178
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19489,7 +19489,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+216.sha-3a1d36c');
+    const VERSION$1 = new Version('11.0.0-next.6+217.sha-b9b9178');
 
     /**
      * @license
@@ -20240,7 +20240,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+216.sha-3a1d36c');
+    const VERSION$2 = new Version('11.0.0-next.6+217.sha-b9b9178');
 
     /**
      * @license
