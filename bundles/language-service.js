8cab408089cf59f97d3e31346489897332db381e
Misko Hevery <misko@hevery.com>
2020-11-09 16:11:14 +0000

    48d15fc760 docs: update cli deprecations (#39547)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+229.sha-0edf6fc
+ * @license Angular v11.0.0-next.6+227.sha-48d15fc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18286,7 +18286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+229.sha-0edf6fc');
+    const VERSION$1 = new Version('11.0.0-next.6+227.sha-48d15fc');
 
     /**
      * @license
@@ -46891,7 +46891,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+229.sha-0edf6fc');
+    const VERSION$2 = new Version$1('11.0.0-next.6+227.sha-48d15fc');
 
     /**
      * @license
