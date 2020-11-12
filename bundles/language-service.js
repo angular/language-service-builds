e6acc06944d51231a7fae3a077fd7f902a3ea941
atscott <atscott01@gmail.com>
2020-11-12 17:29:52 +0000

    3be344cdc2 docs: correct appearance of numbering (#39638)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+234.sha-6d13473
+ * @license Angular v11.0.0-next.6+235.sha-3be344c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18286,7 +18286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+234.sha-6d13473');
+    const VERSION$1 = new Version('11.0.0-next.6+235.sha-3be344c');
 
     /**
      * @license
@@ -46891,7 +46891,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+234.sha-6d13473');
+    const VERSION$2 = new Version$1('11.0.0-next.6+235.sha-3be344c');
 
     /**
      * @license
