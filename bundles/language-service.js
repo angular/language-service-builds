4400a21c7a596fd183315d21703d418e3fe970d7
atscott <atscott01@gmail.com>
2020-11-12 17:28:19 +0000

    d1355caa74 docs: added missing slash to close tag (#39626) PR Close #39626

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+235.sha-3be344c
+ * @license Angular v11.0.0-next.6+237.sha-d1355ca
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18286,7 +18286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+235.sha-3be344c');
+    const VERSION$1 = new Version('11.0.0-next.6+237.sha-d1355ca');
 
     /**
      * @license
@@ -34792,7 +34792,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+235.sha-3be344c');
+    const VERSION$2 = new Version$1('11.0.0-next.6+237.sha-d1355ca');
 
     /**
      * @license
