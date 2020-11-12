5853dcf43b61383847ae9a3f4147782cd7f2395e
atscott <atscott01@gmail.com>
2020-11-12 22:24:20 +0000

    8e17dc0278 fix(core): remove deprecated wtfZoneSpec from NgZone (#37864)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+243.sha-7ce8250
+ * @license Angular v11.0.0-next.6+245.sha-8e17dc0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18286,7 +18286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+243.sha-7ce8250');
+    const VERSION$1 = new Version('11.0.0-next.6+245.sha-8e17dc0');
 
     /**
      * @license
@@ -46885,7 +46885,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+243.sha-7ce8250');
+    const VERSION$2 = new Version$1('11.0.0-next.6+245.sha-8e17dc0');
 
     /**
      * @license
@@ -52838,9 +52838,6 @@ Please check that 1) the type for the parameter at index ${index} is correct and
             const self = this;
             self._nesting = 0;
             self._outer = self._inner = Zone.current;
-            if (Zone['wtfZoneSpec']) {
-                self._inner = self._inner.fork(Zone['wtfZoneSpec']);
-            }
             if (Zone['TaskTrackingZoneSpec']) {
                 self._inner = self._inner.fork(new Zone['TaskTrackingZoneSpec']);
             }
