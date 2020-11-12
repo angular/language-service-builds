b6f70a691c5e6d1d57776dd0f55b54b36dd3d386
atscott <atscott01@gmail.com>
2020-11-12 22:17:09 +0000

    f8956adb73 build: update zone.js version to 0.11.3 (#39317)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+241.sha-a872362
+ * @license Angular v11.0.0-next.6+242.sha-f8956ad
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18286,7 +18286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+241.sha-a872362');
+    const VERSION$1 = new Version('11.0.0-next.6+242.sha-f8956ad');
 
     /**
      * @license
@@ -34792,7 +34792,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+241.sha-a872362');
+    const VERSION$2 = new Version$1('11.0.0-next.6+242.sha-f8956ad');
 
     /**
      * @license
