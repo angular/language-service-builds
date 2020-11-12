6b4607a38577b65f6c27e931da5e73b0c5a3f9b0
atscott <atscott01@gmail.com>
2020-11-12 20:02:39 +0000

    b015d3e950 fix(core): not inserting ViewContainerRef nodes when inside root of a component (#39599)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+239.sha-1bc53eb
+ * @license Angular v11.0.0-next.6+240.sha-b015d3e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18286,7 +18286,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+239.sha-1bc53eb');
+    const VERSION$1 = new Version('11.0.0-next.6+240.sha-b015d3e');
 
     /**
      * @license
@@ -34792,7 +34792,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+239.sha-1bc53eb');
+    const VERSION$2 = new Version$1('11.0.0-next.6+240.sha-b015d3e');
 
     /**
      * @license
