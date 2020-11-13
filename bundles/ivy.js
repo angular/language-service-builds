7590f034b58897254751d8b8e8dc9fd9627bfc28
atscott <atscott01@gmail.com>
2020-11-13 22:04:50 +0000

    d68cac69ed ci: update angular-robot config for L2 triage of PRs (#39655)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+261.sha-24e13e0
+ * @license Angular v11.0.0-next.6+262.sha-d68cac6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19513,7 +19513,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+261.sha-24e13e0');
+    const VERSION$1 = new Version('11.0.0-next.6+262.sha-d68cac6');
 
     /**
      * @license
@@ -20264,7 +20264,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+261.sha-24e13e0');
+    const VERSION$2 = new Version('11.0.0-next.6+262.sha-d68cac6');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+261.sha-24e13e0
+ * @license Angular v11.0.0-next.6+262.sha-d68cac6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18310,7 +18310,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+261.sha-24e13e0');
+    const VERSION$1 = new Version('11.0.0-next.6+262.sha-d68cac6');
 
     /**
      * @license
@@ -46915,7 +46915,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+261.sha-24e13e0');
+    const VERSION$2 = new Version$1('11.0.0-next.6+262.sha-d68cac6');
 
     /**
      * @license
