02fe7aec02938341823c966302c2ba9754cf11e2
atscott <atscott01@gmail.com>
2020-11-13 19:58:59 +0000

    25d6fcacc4 docs: add doc with list of example apps currently in aio (#39295)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+251.sha-9e53c6e
+ * @license Angular v11.0.0-next.6+259.sha-25d6fca
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18310,7 +18310,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+251.sha-9e53c6e');
+    const VERSION$1 = new Version('11.0.0-next.6+259.sha-25d6fca');
 
     /**
      * @license
@@ -46909,7 +46909,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+251.sha-9e53c6e');
+    const VERSION$2 = new Version$1('11.0.0-next.6+259.sha-25d6fca');
 
     /**
      * @license
