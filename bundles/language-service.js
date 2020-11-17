80933b4d0d9e9eee92c3f485e4c749b6c4916b48
atscott <atscott01@gmail.com>
2020-11-17 20:20:21 +0000

    806d7aa22c docs: add documentation for ngNonBindable (#36560)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+283.sha-209a18c
+ * @license Angular v11.0.0-next.6+284.sha-806d7aa
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+283.sha-209a18c');
+    const VERSION$1 = new Version('11.0.0-next.6+284.sha-806d7aa');
 
     /**
      * @license
@@ -34579,7 +34579,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+283.sha-209a18c');
+    const VERSION$2 = new Version$1('11.0.0-next.6+284.sha-806d7aa');
 
     /**
      * @license
