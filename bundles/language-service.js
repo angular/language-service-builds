cb2a5dbda24dc7a534c2c15371e5c23f058fd3e5
atscott <atscott01@gmail.com>
2020-11-17 20:26:40 +0000

    b6893d23c5 test(language-service): introduce new virtual testing environment (#39594)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+284.sha-806d7aa
+ * @license Angular v11.0.0-next.6+288.sha-b6893d2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+284.sha-806d7aa');
+    const VERSION$1 = new Version('11.0.0-next.6+288.sha-b6893d2');
 
     /**
      * @license
@@ -46383,7 +46383,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+284.sha-806d7aa');
+    const VERSION$2 = new Version$1('11.0.0-next.6+288.sha-b6893d2');
 
     /**
      * @license
