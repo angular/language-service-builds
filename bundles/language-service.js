dfcd266155074ec4628fb9a859f2b425e1075c17
atscott <atscott01@gmail.com>
2020-11-18 00:42:13 +0000

    00291ae054 docs: fix dead CSRF link (#35929)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+292.sha-d39c4bb
+ * @license Angular v11.0.0-next.6+293.sha-00291ae
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+292.sha-d39c4bb');
+    const VERSION$1 = new Version('11.0.0-next.6+293.sha-00291ae');
 
     /**
      * @license
@@ -46383,7 +46383,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+292.sha-d39c4bb');
+    const VERSION$2 = new Version$1('11.0.0-next.6+293.sha-00291ae');
 
     /**
      * @license
