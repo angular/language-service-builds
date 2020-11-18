81e062668c6e96bdc52efaf368d79ae5e6ba2c19
atscott <atscott01@gmail.com>
2020-11-18 19:06:37 +0000

    153b42b01e release: cut the v11.0.1 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+46.sha-5d8b5eb
+ * @license Angular v11.0.1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19543,7 +19543,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0+46.sha-5d8b5eb');
+    const VERSION$1 = new Version('11.0.1');
 
     /**
      * @license
@@ -20178,7 +20178,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0+46.sha-5d8b5eb');
+    const VERSION$2 = new Version('11.0.1');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+46.sha-5d8b5eb
+ * @license Angular v11.0.1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18340,7 +18340,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0+46.sha-5d8b5eb');
+    const VERSION$1 = new Version('11.0.1');
 
     /**
      * @license
@@ -34449,7 +34449,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0+46.sha-5d8b5eb');
+    const VERSION$2 = new Version$1('11.0.1');
 
     /**
      * @license
