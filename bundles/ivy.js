d9c81a890969ee1c42739fb2a715dc6a6b705102
atscott <atscott01@gmail.com>
2020-11-18 17:33:07 +0000

    0ab833f5c2 docs: meetup url to https (#33713)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+293.sha-00291ae
+ * @license Angular v11.0.0-next.6+294.sha-0ab833f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19684,7 +19684,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+293.sha-00291ae');
+    const VERSION$1 = new Version('11.0.0-next.6+294.sha-0ab833f');
 
     /**
      * @license
@@ -20435,7 +20435,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.0-next.6+293.sha-00291ae');
+    const VERSION$2 = new Version('11.0.0-next.6+294.sha-0ab833f');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+293.sha-00291ae
+ * @license Angular v11.0.0-next.6+294.sha-0ab833f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.0-next.6+293.sha-00291ae');
+    const VERSION$1 = new Version('11.0.0-next.6+294.sha-0ab833f');
 
     /**
      * @license
@@ -34579,7 +34579,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.0-next.6+293.sha-00291ae');
+    const VERSION$2 = new Version$1('11.0.0-next.6+294.sha-0ab833f');
 
     /**
      * @license
