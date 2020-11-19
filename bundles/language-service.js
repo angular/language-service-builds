be08577a5053caa69cb546fa1be44f438d6b6101
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:44:56 +0000

    a965589eb8 feat(core): adds get method to QueryList (#36907)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+24.sha-1eb4066
+ * @license Angular v11.1.0-next.0+31.sha-a965589
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+24.sha-1eb4066');
+    const VERSION$1 = new Version('11.1.0-next.0+31.sha-a965589');
 
     /**
      * @license
@@ -46426,7 +46426,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+24.sha-1eb4066');
+    const VERSION$2 = new Version$1('11.1.0-next.0+31.sha-a965589');
 
     /**
      * @license
@@ -50771,6 +50771,12 @@ Please check that 1) the type for the parameter at index ${index} is correct and
             const proto = QueryList.prototype;
             if (!proto[symbol])
                 proto[symbol] = symbolIterator;
+        }
+        /**
+         * Returns the QueryList entry at `index`.
+         */
+        get(index) {
+            return this._results[index];
         }
         /**
          * See
