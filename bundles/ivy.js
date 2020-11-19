10f5f0b65541aa9c9d3684579c421da3c8e965d1
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:07:40 +0000

    33639db911 release: cut the v11.0.2 release

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+17.sha-2fe2104
+ * @license Angular v11.0.2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19543,7 +19543,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.1+17.sha-2fe2104');
+    const VERSION$1 = new Version('11.0.2');
 
     /**
      * @license
@@ -20178,7 +20178,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.1+17.sha-2fe2104');
+    const VERSION$2 = new Version('11.0.2');
 
     /**
      * @license
