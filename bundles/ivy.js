6d13278702b72b74b51ba1ce882d49ad977453b0
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:04:40 +0000

    dc697d33d0 docs(http): fix em-dashes not displaying correctly (#38665)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+4.sha-3273856
+ * @license Angular v11.1.0-next.0+5.sha-dc697d3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19684,7 +19684,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+4.sha-3273856');
+    const VERSION$1 = new Version('11.1.0-next.0+5.sha-dc697d3');
 
     /**
      * @license
@@ -20435,7 +20435,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+4.sha-3273856');
+    const VERSION$2 = new Version('11.1.0-next.0+5.sha-dc697d3');
 
     /**
      * @license
