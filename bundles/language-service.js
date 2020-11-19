7fdc1b1b0cf8916df5f336ba386c2a07afb0cd46
Andrew Kushnir <akushnir@google.com>
2020-11-19 18:19:39 +0000

    81d72a1e37 fix(router): migration incorrectly replacing deprecated key (#39763)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+16.sha-6150640
+ * @license Angular v11.1.0-next.0+17.sha-81d72a1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+16.sha-6150640');
+    const VERSION$1 = new Version('11.1.0-next.0+17.sha-81d72a1');
 
     /**
      * @license
@@ -46426,7 +46426,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+16.sha-6150640');
+    const VERSION$2 = new Version$1('11.1.0-next.0+17.sha-81d72a1');
 
     /**
      * @license
