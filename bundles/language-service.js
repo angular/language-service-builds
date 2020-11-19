3c836c05ef045ce57ce9cb8648d57764a88b65d4
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:12:04 +0000

    b521b5eb39 build(docs-infra): update @angular/material to 11.0.0 (#39600)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+5.sha-dc697d3
+ * @license Angular v11.1.0-next.0+11.sha-b521b5e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+5.sha-dc697d3');
+    const VERSION$1 = new Version('11.1.0-next.0+11.sha-b521b5e');
 
     /**
      * @license
@@ -34603,7 +34603,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+5.sha-dc697d3');
+    const VERSION$2 = new Version$1('11.1.0-next.0+11.sha-b521b5e');
 
     /**
      * @license
