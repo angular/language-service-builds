1b7faea2285f22165c6d63d0729eddc5d24c3ace
Andrew Kushnir <akushnir@google.com>
2020-11-19 17:22:19 +0000

    61506404a9 docs: move di-in-action doc to conceptual ref section (#39544)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+15.sha-1951342
+ * @license Angular v11.1.0-next.0+16.sha-6150640
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19684,7 +19684,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+15.sha-1951342');
+    const VERSION$1 = new Version('11.1.0-next.0+16.sha-6150640');
 
     /**
      * @license
@@ -20435,7 +20435,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+15.sha-1951342');
+    const VERSION$2 = new Version('11.1.0-next.0+16.sha-6150640');
 
     /**
      * @license
