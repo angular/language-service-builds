2d87cf6ef664563a5548b98800ead834dbd8c11c
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:12:26 +0000

    0e859381bc docs: release notes for the v11.0.2 release

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+17.sha-81d72a1
+ * @license Angular v11.1.0-next.0+18.sha-0e85938
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+17.sha-81d72a1');
+    const VERSION$1 = new Version('11.1.0-next.0+18.sha-0e85938');
 
     /**
      * @license
@@ -34603,7 +34603,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+17.sha-81d72a1');
+    const VERSION$2 = new Version$1('11.1.0-next.0+18.sha-0e85938');
 
     /**
      * @license
