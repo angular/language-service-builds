3da0456818a5707138d5cffdab8b838b697673c3
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:26:16 +0000

    d001ecc5a0 docs: fix confusing description for strictTemplates (#39745)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+12.sha-f8c6230
+ * @license Angular v11.1.0-next.0+13.sha-d001ecc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18478,7 +18478,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+12.sha-f8c6230');
+    const VERSION$1 = new Version('11.1.0-next.0+13.sha-d001ecc');
 
     /**
      * @license
@@ -46426,7 +46426,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+12.sha-f8c6230');
+    const VERSION$2 = new Version$1('11.1.0-next.0+13.sha-d001ecc');
 
     /**
      * @license
