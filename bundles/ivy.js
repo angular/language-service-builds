06002103da015a7baf626c7dc81ed17bde541854
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:56:50 +0000

    66d863febe fix(animations): getAnimationStyle causes exceptions in older browsers (#29709)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+36.sha-fe1ada8
+ * @license Angular v11.1.0-next.0+38.sha-66d863f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19684,7 +19684,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+36.sha-fe1ada8');
+    const VERSION$1 = new Version('11.1.0-next.0+38.sha-66d863f');
 
     /**
      * @license
@@ -20435,7 +20435,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+36.sha-fe1ada8');
+    const VERSION$2 = new Version('11.1.0-next.0+38.sha-66d863f');
 
     /**
      * @license
