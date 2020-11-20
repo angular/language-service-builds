84e16248c8a298cf17884c1dcd001e986f97cfed
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:53:27 +0000

    ff0a90e4a8 fix(core): meta addTag() adds incorrect attribute for httpEquiv (#32531)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+44.sha-9aaa869
+ * @license Angular v11.1.0-next.0+46.sha-ff0a90e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18544,7 +18544,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+44.sha-9aaa869');
+    const VERSION$1 = new Version('11.1.0-next.0+46.sha-ff0a90e');
 
     /**
      * @license
@@ -46512,7 +46512,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+44.sha-9aaa869');
+    const VERSION$2 = new Version$1('11.1.0-next.0+46.sha-ff0a90e');
 
     /**
      * @license
