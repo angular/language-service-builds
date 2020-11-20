cca3c9d9d1acc8d319e2d10e2c7600fedfbbb346
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:47:01 +0000

    9aaa86967d build(language-service): include sources for Ivy LS (#39748)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+45.sha-156f9f3
+ * @license Angular v11.1.0-next.0+44.sha-9aaa869
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18544,7 +18544,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+45.sha-156f9f3');
+    const VERSION$1 = new Version('11.1.0-next.0+44.sha-9aaa869');
 
     /**
      * @license
@@ -34680,7 +34680,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+45.sha-156f9f3');
+    const VERSION$2 = new Version$1('11.1.0-next.0+44.sha-9aaa869');
 
     /**
      * @license
