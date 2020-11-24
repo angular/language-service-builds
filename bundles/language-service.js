646eaebb7379ebee7bceb6e1a830e75dab61de58
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:32:23 +0000

    75b53690f6 docs: explain foreign function/value constraints in Ivy compatibility examples (#36092)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+49.sha-9cb12ca
+ * @license Angular v11.0.2+51.sha-75b5369
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18406,7 +18406,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.2+49.sha-9cb12ca');
+    const VERSION$1 = new Version('11.0.2+51.sha-75b5369');
 
     /**
      * @license
@@ -34550,7 +34550,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.2+49.sha-9cb12ca');
+    const VERSION$2 = new Version$1('11.0.2+51.sha-75b5369');
 
     /**
      * @license
