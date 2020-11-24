e04a5658656e36c30df2300b0be086eac7dc5815
Andrew Kushnir <akushnir@google.com>
2020-11-24 23:19:54 +0000

    23c36a24ed test(docs-infra): disable the Selenium Promise Manager in docs examples e2e tests (#39818)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+84.sha-8758006
+ * @license Angular v11.1.0-next.0+89.sha-23c36a2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18659,7 +18659,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+84.sha-8758006');
+    const VERSION$1 = new Version('11.1.0-next.0+89.sha-23c36a2');
 
     /**
      * @license
@@ -34795,7 +34795,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+84.sha-8758006');
+    const VERSION$2 = new Version$1('11.1.0-next.0+89.sha-23c36a2');
 
     /**
      * @license
