09aa92505a44725fb4ca3af8939987b1b4329665
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:07:32 +0000

    b5c0f9d7d1 feat(platform-browser): add doubletap HammerJS support (#26362)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+66.sha-88fbc06
+ * @license Angular v11.1.0-next.0+69.sha-b5c0f9d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18631,7 +18631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+66.sha-88fbc06');
+    const VERSION$1 = new Version('11.1.0-next.0+69.sha-b5c0f9d');
 
     /**
      * @license
@@ -34767,7 +34767,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+66.sha-88fbc06');
+    const VERSION$2 = new Version$1('11.1.0-next.0+69.sha-b5c0f9d');
 
     /**
      * @license
