b5c191e78d3c603437c6be2f31c9a6b209e546e5
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:16:19 +0000

    453b32f4b9 fix(compiler-cli): report error when a reference target is missing instead of crashing (#39805)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+69.sha-b5c0f9d
+ * @license Angular v11.1.0-next.0+70.sha-453b32f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18631,7 +18631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+69.sha-b5c0f9d');
+    const VERSION$1 = new Version('11.1.0-next.0+70.sha-453b32f');
 
     /**
      * @license
@@ -46590,7 +46590,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+69.sha-b5c0f9d');
+    const VERSION$2 = new Version$1('11.1.0-next.0+70.sha-453b32f');
 
     /**
      * @license
