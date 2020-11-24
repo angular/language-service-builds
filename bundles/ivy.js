35d5349211d87669df1c0d27bc23ab0f40febf89
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:06:50 +0000

    a1b6ad07a8 fix(core): Allow passing AbstractType to the inject function (#37958)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+70.sha-453b32f
+ * @license Angular v11.1.0-next.0+71.sha-a1b6ad0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19837,7 +19837,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+70.sha-453b32f');
+    const VERSION$1 = new Version('11.1.0-next.0+71.sha-a1b6ad0');
 
     /**
      * @license
@@ -20588,7 +20588,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+70.sha-453b32f');
+    const VERSION$2 = new Version('11.1.0-next.0+71.sha-a1b6ad0');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+70.sha-453b32f
+ * @license Angular v11.1.0-next.0+71.sha-a1b6ad0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18631,7 +18631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+70.sha-453b32f');
+    const VERSION$1 = new Version('11.1.0-next.0+71.sha-a1b6ad0');
 
     /**
      * @license
@@ -34767,7 +34767,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+70.sha-453b32f');
+    const VERSION$2 = new Version$1('11.1.0-next.0+71.sha-a1b6ad0');
 
     /**
      * @license
