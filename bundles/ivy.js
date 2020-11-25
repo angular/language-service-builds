a1b9f842075df57e100c0d7865d6b48e5a5a396f
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 23:58:03 +0000

    01c1bfdbdd fix(core): Ensure OnPush ancestors are marked dirty when events occur (#39833)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+61.sha-4cfa209
+ * @license Angular v11.0.2+63.sha-01c1bfd
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.2+61.sha-4cfa209');
+    const VERSION$1 = new Version('11.0.2+63.sha-01c1bfd');
 
     /**
      * @license
@@ -20245,7 +20245,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.2+61.sha-4cfa209');
+    const VERSION$2 = new Version('11.0.2+63.sha-01c1bfd');
 
     /**
      * @license
