49ddc69e9fb83be4acfc7b3604c12737c224026b
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:41:15 +0000

    4def19f39f test(compiler-cli): add TypeScript 4.1 typings integration test (#39571)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+96.sha-2786292
+ * @license Angular v11.1.0-next.0+99.sha-4def19f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18660,7 +18660,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+96.sha-2786292');
+    const VERSION$1 = new Version('11.1.0-next.0+99.sha-4def19f');
 
     /**
      * @license
@@ -46619,7 +46619,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+96.sha-2786292');
+    const VERSION$2 = new Version$1('11.1.0-next.0+99.sha-4def19f');
 
     /**
      * @license
