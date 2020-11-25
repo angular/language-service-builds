65f4405d6b2881c894680a1b96ba7dceb39dfd37
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:32:06 +0000

    6653028863 docs: make MDN link locale-indepenent (#39842)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+93.sha-bf12f51
+ * @license Angular v11.1.0-next.0+94.sha-6653028
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19866,7 +19866,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+93.sha-bf12f51');
+    const VERSION$1 = new Version('11.1.0-next.0+94.sha-6653028');
 
     /**
      * @license
@@ -20729,7 +20729,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+93.sha-bf12f51');
+    const VERSION$2 = new Version('11.1.0-next.0+94.sha-6653028');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+93.sha-bf12f51
+ * @license Angular v11.1.0-next.0+94.sha-6653028
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18660,7 +18660,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+93.sha-bf12f51');
+    const VERSION$1 = new Version('11.1.0-next.0+94.sha-6653028');
 
     /**
      * @license
@@ -46619,7 +46619,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+93.sha-bf12f51');
+    const VERSION$2 = new Version$1('11.1.0-next.0+94.sha-6653028');
 
     /**
      * @license
