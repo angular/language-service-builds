7450ebee813aca4fc4340bddc4591662699e9451
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:27:25 +0000

    1539c64fb0 docs(common): change HTTPResponse to HttpResponse (#39860) PR Close #39860

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+110.sha-c622b17
+ * @license Angular v11.1.0-next.0+112.sha-1539c64
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19866,7 +19866,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+110.sha-c622b17');
+    const VERSION$1 = new Version('11.1.0-next.0+112.sha-1539c64');
 
     /**
      * @license
@@ -20729,7 +20729,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+110.sha-c622b17');
+    const VERSION$2 = new Version('11.1.0-next.0+112.sha-1539c64');
 
     /**
      * @license
