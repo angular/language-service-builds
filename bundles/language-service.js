f1197373605f0ecc91bd1cf4356fa5563a530033
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:33:28 +0000

    dd3ed3cb2b docs: fix builder section of generated angular.json file for libs (#39888)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+113.sha-71a5314
+ * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18660,7 +18660,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+113.sha-71a5314');
+    const VERSION$1 = new Version('11.1.0-next.0+114.sha-dd3ed3c');
 
     /**
      * @license
@@ -34796,7 +34796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+113.sha-71a5314');
+    const VERSION$2 = new Version$1('11.1.0-next.0+114.sha-dd3ed3c');
 
     /**
      * @license
