af146dcb7dff59a3c2345426cb216071173475a9
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:57:46 +0000

    9a2f83b826 docs: edit Getting Started start/index.md copy and headers (#39583)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+111.sha-c5619ca
+ * @license Angular v11.1.0-next.0+106.sha-9a2f83b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19866,7 +19866,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+111.sha-c5619ca');
+    const VERSION$1 = new Version('11.1.0-next.0+106.sha-9a2f83b');
 
     /**
      * @license
@@ -20729,7 +20729,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+111.sha-c5619ca');
+    const VERSION$2 = new Version('11.1.0-next.0+106.sha-9a2f83b');
 
     /**
      * @license
