6adf2daa7ddbd39fe570fee9eb4264104edc143a
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:55:17 +0000

    9a2f83b826 docs: edit Getting Started start/index.md copy and headers (#39583)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+111.sha-c5619ca
+ * @license Angular v11.1.0-next.0+106.sha-9a2f83b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18660,7 +18660,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+111.sha-c5619ca');
+    const VERSION$1 = new Version('11.1.0-next.0+106.sha-9a2f83b');
 
     /**
      * @license
@@ -34796,7 +34796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+111.sha-c5619ca');
+    const VERSION$2 = new Version$1('11.1.0-next.0+106.sha-9a2f83b');
 
     /**
      * @license
