f9e5019ebe0138a8db475dd0150e8ac8d23e18a7
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:30:15 +0000

    71a5314335 test(compiler-cli): migrate providers compliance tests (#39878)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+112.sha-1539c64
+ * @license Angular v11.1.0-next.0+113.sha-71a5314
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19866,7 +19866,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+112.sha-1539c64');
+    const VERSION$1 = new Version('11.1.0-next.0+113.sha-71a5314');
 
     /**
      * @license
@@ -20729,7 +20729,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.0+112.sha-1539c64');
+    const VERSION$2 = new Version('11.1.0-next.0+113.sha-71a5314');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+112.sha-1539c64
+ * @license Angular v11.1.0-next.0+113.sha-71a5314
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18660,7 +18660,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.0+112.sha-1539c64');
+    const VERSION$1 = new Version('11.1.0-next.0+113.sha-71a5314');
 
     /**
      * @license
@@ -34796,7 +34796,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.0+112.sha-1539c64');
+    const VERSION$2 = new Version$1('11.1.0-next.0+113.sha-71a5314');
 
     /**
      * @license
