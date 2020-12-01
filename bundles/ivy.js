04a65a1eb9008ca7d6b9b09a60e3c682149b3f35
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:26:21 +0000

    3d19645208 docs: edit binding-syntax doc (#38773)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+76.sha-518fbea
+ * @license Angular v11.0.2+77.sha-3d19645
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.2+76.sha-518fbea');
+    const VERSION$1 = new Version('11.0.2+77.sha-3d19645');
 
     /**
      * @license
@@ -20245,7 +20245,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.2+76.sha-518fbea');
+    const VERSION$2 = new Version('11.0.2+77.sha-3d19645');
 
     /**
      * @license
