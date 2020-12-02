3be659291861c48b68780005d30c5e658f3b4839
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 19:59:03 +0000

    943ae37f99 docs: archive svg-in-templates.md (#39896)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+5.sha-b596366
+ * @license Angular v11.1.0-next.1+2.sha-943ae37
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19866,7 +19866,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+5.sha-b596366');
+    const VERSION$1 = new Version('11.1.0-next.1+2.sha-943ae37');
 
     /**
      * @license
@@ -20729,7 +20729,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.1+5.sha-b596366');
+    const VERSION$2 = new Version('11.1.0-next.1+2.sha-943ae37');
 
     /**
      * @license
