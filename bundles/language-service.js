4537c0c46ab3e67b255d254ceaeaef8858296819
Misko Hevery <misko@hevery.com>
2020-12-07 21:44:22 +0000

    e92d8a8e8f test(compiler-cli): Add test for checkTypeOfDomReferences = false (#39539)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+54.sha-5fc4508
+ * @license Angular v11.1.0-next.1+56.sha-e92d8a8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18663,7 +18663,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+54.sha-5fc4508');
+    const VERSION$1 = new Version('11.1.0-next.1+56.sha-e92d8a8');
 
     /**
      * @license
@@ -34550,7 +34550,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+54.sha-5fc4508');
+    const VERSION$2 = new Version$1('11.1.0-next.1+56.sha-e92d8a8');
 
     /**
      * @license
