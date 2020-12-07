b00a433aba1a7f87a2547f223693d9b206d8dbdc
Misko Hevery <misko@hevery.com>
2020-12-07 18:16:28 +0000

    6d19c16d32 build: remove check-env script (#39980)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+51.sha-d9356f2
+ * @license Angular v11.1.0-next.1+52.sha-6d19c16
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18663,7 +18663,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+51.sha-d9356f2');
+    const VERSION$1 = new Version('11.1.0-next.1+52.sha-6d19c16');
 
     /**
      * @license
@@ -46378,7 +46378,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+51.sha-d9356f2');
+    const VERSION$2 = new Version$1('11.1.0-next.1+52.sha-6d19c16');
 
     /**
      * @license
