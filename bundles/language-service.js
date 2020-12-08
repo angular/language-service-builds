4115e2ee429530209ea2820663a7500ebd76ef1f
Misko Hevery <misko@hevery.com>
2020-12-08 17:49:14 +0000

    8ab1231908 docs(animations): a small typo (#37164)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+42.sha-5631daa
+ * @license Angular v11.0.3+54.sha-8ab1231
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18449,7 +18449,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.3+42.sha-5631daa');
+    const VERSION$1 = new Version('11.0.3+54.sha-8ab1231');
 
     /**
      * @license
@@ -34344,7 +34344,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.3+42.sha-5631daa');
+    const VERSION$2 = new Version$1('11.0.3+54.sha-8ab1231');
 
     /**
      * @license
