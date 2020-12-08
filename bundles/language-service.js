d7d59a73a04f4f30b63bb23fc7041606ba8fa370
Misko Hevery <misko@hevery.com>
2020-12-08 17:19:28 +0000

    e8d2348e21 docs:  how to create a custom Attribute decorator in a child component and use it from a parent component (#38721)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+66.sha-f408af3
+ * @license Angular v11.1.0-next.1+65.sha-e8d2348
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18702,7 +18702,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+66.sha-f408af3');
+    const VERSION$1 = new Version('11.1.0-next.1+65.sha-e8d2348');
 
     /**
      * @license
@@ -46417,7 +46417,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+66.sha-f408af3');
+    const VERSION$2 = new Version$1('11.1.0-next.1+65.sha-e8d2348');
 
     /**
      * @license
