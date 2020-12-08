3d21a6ecabc4a7c54302858e82621cf31a2cfde9
Misko Hevery <misko@hevery.com>
2020-12-08 17:40:48 +0000

    86fdc77ddf docs: replace image with one containing correct text (#40022)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+71.sha-2ffd739
+ * @license Angular v11.1.0-next.1+72.sha-86fdc77
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18702,7 +18702,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+71.sha-2ffd739');
+    const VERSION$1 = new Version('11.1.0-next.1+72.sha-86fdc77');
 
     /**
      * @license
@@ -46417,7 +46417,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+71.sha-2ffd739');
+    const VERSION$2 = new Version$1('11.1.0-next.1+72.sha-86fdc77');
 
     /**
      * @license
