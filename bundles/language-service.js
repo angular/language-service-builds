cd56afc75da53bfaa0d6dd9d1fb755c13c11b4d5
Misko Hevery <misko@hevery.com>
2020-12-08 20:30:28 +0000

    6dc43a475b fix(upgrade): avoid memory leak when removing downgraded components (#39965)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+83.sha-e692884
+ * @license Angular v11.1.0-next.1+85.sha-6dc43a4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18707,7 +18707,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+83.sha-e692884');
+    const VERSION$1 = new Version('11.1.0-next.1+85.sha-6dc43a4');
 
     /**
      * @license
@@ -34594,7 +34594,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+83.sha-e692884');
+    const VERSION$2 = new Version$1('11.1.0-next.1+85.sha-6dc43a4');
 
     /**
      * @license
