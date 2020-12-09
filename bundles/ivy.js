ebe9183d603f576ac74786fa6e0688a4ec37b249
Misko Hevery <misko@hevery.com>
2020-12-09 00:36:54 +0000

    61b9adb1b1 docs: add Pato Vargas to GDE resources (#40029)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+89.sha-55c21fa
+ * @license Angular v11.1.0-next.1+90.sha-61b9adb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19913,7 +19913,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+89.sha-55c21fa');
+    const VERSION$1 = new Version('11.1.0-next.1+90.sha-61b9adb');
 
     /**
      * @license
@@ -20595,7 +20595,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+89.sha-55c21fa'));
+        definitionMap.set('version', literal('11.1.0-next.1+90.sha-61b9adb'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -20776,7 +20776,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.1+89.sha-55c21fa');
+    const VERSION$2 = new Version('11.1.0-next.1+90.sha-61b9adb');
 
     /**
      * @license
