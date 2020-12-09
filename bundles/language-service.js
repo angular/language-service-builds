9bb0888e2bfbcf3c9cf692eb7d92be44b70275f4
Misko Hevery <misko@hevery.com>
2020-12-09 17:28:45 +0000

    e39f923ff9 ci: update pullapprove config to add comp labels (#39962)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+98.sha-fc323d0
+ * @license Angular v11.1.0-next.1+100.sha-e39f923
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18707,7 +18707,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+98.sha-fc323d0');
+    const VERSION$1 = new Version('11.1.0-next.1+100.sha-e39f923');
 
     /**
      * @license
@@ -46422,7 +46422,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+98.sha-fc323d0');
+    const VERSION$2 = new Version$1('11.1.0-next.1+100.sha-e39f923');
 
     /**
      * @license
