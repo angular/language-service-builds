3fbe6d1d6c44759830a6177d1569e137e28714c2
Misko Hevery <misko@hevery.com>
2020-12-09 17:35:01 +0000

    42d478b16a docs(common): use table layout for Pre-defined format options (#40036)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+101.sha-13126d2
+ * @license Angular v11.1.0-next.1+102.sha-42d478b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18707,7 +18707,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.1+101.sha-13126d2');
+    const VERSION$1 = new Version('11.1.0-next.1+102.sha-42d478b');
 
     /**
      * @license
@@ -34594,7 +34594,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.1+101.sha-13126d2');
+    const VERSION$2 = new Version$1('11.1.0-next.1+102.sha-42d478b');
 
     /**
      * @license
