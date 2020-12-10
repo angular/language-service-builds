336a8db39f2d18770eb2253951096db5fda0df4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:56:49 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+21.sha-79a42d9
+ * @license Angular v11.1.0-next.2+23.sha-91fb830
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19948,7 +19948,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.2+21.sha-79a42d9');
+    const VERSION$1 = new Version('11.1.0-next.2+23.sha-91fb830');
 
     /**
      * @license
@@ -20630,7 +20630,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.2+21.sha-79a42d9'));
+        definitionMap.set('version', literal('11.1.0-next.2+23.sha-91fb830'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -20811,7 +20811,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.2+21.sha-79a42d9');
+    const VERSION$2 = new Version('11.1.0-next.2+23.sha-91fb830');
 
     /**
      * @license
