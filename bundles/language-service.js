acaf986e195fb21120b3f914e50f1cb8418a6b07
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:42:22 +0000

    fea95059d5 build(docs-infra): upgrade cli command docs sources to f6f0cde41 (#40064)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+20.sha-19b9ec8
+ * @license Angular v11.1.0-next.2+22.sha-fea9505
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18739,7 +18739,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.2+20.sha-19b9ec8');
+    const VERSION$1 = new Version('11.1.0-next.2+22.sha-fea9505');
 
     /**
      * @license
@@ -34626,7 +34626,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.2+20.sha-19b9ec8');
+    const VERSION$2 = new Version$1('11.1.0-next.2+22.sha-fea9505');
 
     /**
      * @license
