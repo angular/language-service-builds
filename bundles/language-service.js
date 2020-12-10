d6fd85b75369aeb85ab70dda7966181776073372
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:10:37 +0000

    b4b21bdff4 fix(upgrade): fix HMR for hybrid applications (#40045)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+10.sha-76e3de2
+ * @license Angular v11.1.0-next.2+13.sha-b4b21bd
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18739,7 +18739,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.2+10.sha-76e3de2');
+    const VERSION$1 = new Version('11.1.0-next.2+13.sha-b4b21bd');
 
     /**
      * @license
@@ -46454,7 +46454,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.2+10.sha-76e3de2');
+    const VERSION$2 = new Version$1('11.1.0-next.2+13.sha-b4b21bd');
 
     /**
      * @license
