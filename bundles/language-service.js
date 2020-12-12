036d961048c0cf2f999aec5b0ff745068a59a9e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-12 00:44:44 +0000

    2b74a05a65 refactor(compiler-cli): Enable pipe information when checkTypeOfPipes=false (#39555)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+26.sha-6e4e68c
+ * @license Angular v11.1.0-next.2+27.sha-2b74a05
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18739,7 +18739,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.2+26.sha-6e4e68c');
+    const VERSION$1 = new Version('11.1.0-next.2+27.sha-2b74a05');
 
     /**
      * @license
@@ -46454,7 +46454,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.2+26.sha-6e4e68c');
+    const VERSION$2 = new Version$1('11.1.0-next.2+27.sha-2b74a05');
 
     /**
      * @license
