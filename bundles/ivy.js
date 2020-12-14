bc65810bec7d77f28eec2766e228fc27398a3475
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:14:19 +0000

    524d581dfd docs: fix typo (#40106)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+36.sha-c18a9d5
+ * @license Angular v11.1.0-next.2+38.sha-524d581
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19948,7 +19948,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.2+36.sha-c18a9d5');
+    const VERSION$1 = new Version('11.1.0-next.2+38.sha-524d581');
 
     /**
      * @license
@@ -20630,7 +20630,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.2+36.sha-c18a9d5'));
+        definitionMap.set('version', literal('11.1.0-next.2+38.sha-524d581'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -20811,7 +20811,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.2+36.sha-c18a9d5');
+    const VERSION$2 = new Version('11.1.0-next.2+38.sha-524d581');
 
     /**
      * @license
