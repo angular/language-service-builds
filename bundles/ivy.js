314d47d8d18e0005881e32e3558e11711a9bb89a
Joey Perrott <josephperrott@gmail.com>
2020-12-16 23:42:31 +0000

    e0e2157a28 release: cut the v11.0.5 release (#40164)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+27.sha-922f492
+ * @license Angular v11.0.4+28.sha-e0e2157
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19639,7 +19639,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.4+27.sha-922f492');
+    const VERSION$1 = new Version('11.0.4+28.sha-e0e2157');
 
     /**
      * @license
@@ -20274,7 +20274,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.4+27.sha-922f492');
+    const VERSION$2 = new Version('11.0.4+28.sha-e0e2157');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+27.sha-922f492
+ * @license Angular v11.0.4+28.sha-e0e2157
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18436,7 +18436,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.4+27.sha-922f492');
+    const VERSION$1 = new Version('11.0.4+28.sha-e0e2157');
 
     /**
      * @license
@@ -34331,7 +34331,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.4+27.sha-922f492');
+    const VERSION$2 = new Version$1('11.0.4+28.sha-e0e2157');
 
     /**
      * @license
