3444c58cfd2abbf271d96ea9ef709c45e7140de2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 21:43:12 +0000

    70b4816cd5 fix(core): set `ngDevMode` to `false` when calling `enableProdMode()` (#40124)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+55.sha-47d9b6d
+ * @license Angular v11.1.0-next.2+56.sha-70b4816
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18767,7 +18767,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.2+55.sha-47d9b6d');
+    const VERSION$1 = new Version('11.1.0-next.2+56.sha-70b4816');
 
     /**
      * @license
@@ -34654,7 +34654,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.2+55.sha-47d9b6d');
+    const VERSION$2 = new Version$1('11.1.0-next.2+56.sha-70b4816');
 
     /**
      * @license
