74065e0e9f8d5683b6d482c57c02244ae60b13da
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:31:28 +0000

    4a11226be7 release: cut the v10.2.4 release

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.3+3.sha-90570c0
+ * @license Angular v10.2.4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17995,7 +17995,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('10.2.3+3.sha-90570c0');
+    const VERSION$1 = new Version('10.2.4');
 
     /**
      * @license
@@ -34147,7 +34147,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('10.2.3+3.sha-90570c0');
+    const VERSION$2 = new Version$1('10.2.4');
 
     /**
      * @license
