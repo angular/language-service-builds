b068799b2604af2a0596532794827372e60388ef
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:43:49 +0000

    eae07e10aa docs: fix styles in binding guides (#40182)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+9.sha-f7642ce
+ * @license Angular v11.1.0-next.3+10.sha-eae07e1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16757,7 +16757,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+9.sha-f7642ce');
+    const VERSION$1 = new Version('11.1.0-next.3+10.sha-eae07e1');
 
     /**
      * @license
@@ -17439,7 +17439,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+9.sha-f7642ce'));
+        definitionMap.set('version', literal('11.1.0-next.3+10.sha-eae07e1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -20839,7 +20839,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.3+9.sha-f7642ce');
+    const VERSION$2 = new Version('11.1.0-next.3+10.sha-eae07e1');
 
     /**
      * @license
