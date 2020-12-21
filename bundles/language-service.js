e9c8e5375221184c6c7d8c4dcd7a01cfe2732b9e
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:45:33 +0000

    f7642ceff3 docs: update App Shell guide to reflect its actual behavior (#40210) PR Close #40210

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+7.sha-10a468a
+ * @license Angular v11.1.0-next.3+9.sha-f7642ce
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18767,7 +18767,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+7.sha-10a468a');
+    const VERSION$1 = new Version('11.1.0-next.3+9.sha-f7642ce');
 
     /**
      * @license
@@ -46518,7 +46518,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.3+7.sha-10a468a');
+    const VERSION$2 = new Version$1('11.1.0-next.3+9.sha-f7642ce');
 
     /**
      * @license
