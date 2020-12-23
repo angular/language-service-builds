31ac04c58b6f950d0f02a30262ca7f18e452f79b
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:26:35 +0000

    c83163ecfe docs(router): fix typo (#40244)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+24.sha-9a149d4
+ * @license Angular v11.0.5+26.sha-c83163e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19639,7 +19639,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.5+24.sha-9a149d4');
+    const VERSION$1 = new Version('11.0.5+26.sha-c83163e');
 
     /**
      * @license
@@ -20274,7 +20274,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.5+24.sha-9a149d4');
+    const VERSION$2 = new Version('11.0.5+26.sha-c83163e');
 
     /**
      * @license
