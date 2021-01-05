d6d54da8769dc9ee119c11d0672e86431f1f4e4d
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:25:26 +0000

    ec5b09b6f3 build(docs-infra): upgrade cli command docs sources to 5dd818279 (#40251)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+26.sha-c83163e
+ * @license Angular v11.0.5+31.sha-ec5b09b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18436,7 +18436,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.5+26.sha-c83163e');
+    const VERSION$1 = new Version('11.0.5+31.sha-ec5b09b');
 
     /**
      * @license
@@ -34331,7 +34331,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.5+26.sha-c83163e');
+    const VERSION$2 = new Version$1('11.0.5+31.sha-ec5b09b');
 
     /**
      * @license
