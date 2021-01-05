3f462db6fa69a6f7628f12a0ca0f26e100f46c18
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:20:22 +0000

    c1d7805acc docs(core): Fix spelling (#40296) PR Close #40296

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+50.sha-e43f7e2
+ * @license Angular v11.1.0-next.3+52.sha-c1d7805
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18837,7 +18837,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+50.sha-e43f7e2');
+    const VERSION$1 = new Version('11.1.0-next.3+52.sha-c1d7805');
 
     /**
      * @license
@@ -46591,7 +46591,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.3+50.sha-e43f7e2');
+    const VERSION$2 = new Version$1('11.1.0-next.3+52.sha-c1d7805');
 
     /**
      * @license
@@ -53869,7 +53869,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     }
     /**
      * Creates a factory for a platform. Can be used to provide or override `Providers` specific to
-     * your applciation's runtime needs, such as `PLATFORM_INITIALIZER` and `PLATFORM_ID`.
+     * your application's runtime needs, such as `PLATFORM_INITIALIZER` and `PLATFORM_ID`.
      * @param parentPlatformFactory Another platform factory to modify. Allows you to compose factories
      * to build up configurations that might be required by different libraries or parts of the
      * application.
