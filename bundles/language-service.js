a49b4f0ce8d4abe5a5b1610289c59ed2c73ad4fc
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:22:01 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+52.sha-c1d7805
+ * @license Angular v11.1.0-next.3+53.sha-8cdfd77
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18837,7 +18837,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+52.sha-c1d7805');
+    const VERSION$1 = new Version('11.1.0-next.3+53.sha-8cdfd77');
 
     /**
      * @license
@@ -34727,7 +34727,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.3+52.sha-c1d7805');
+    const VERSION$2 = new Version$1('11.1.0-next.3+53.sha-8cdfd77');
 
     /**
      * @license
