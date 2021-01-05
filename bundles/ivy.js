d23d0e9049f54a38a589df9eed2d37e64bfdf43b
Joey Perrott <josephperrott@gmail.com>
2021-01-05 20:56:24 +0000

    b473bc226c ci: remove autolabeling from pullapprove config (#40314)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+43.sha-a384961
+ * @license Angular v11.1.0-next.3+44.sha-b473bc2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16827,7 +16827,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+43.sha-a384961');
+    const VERSION$1 = new Version('11.1.0-next.3+44.sha-b473bc2');
 
     /**
      * @license
@@ -17509,7 +17509,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+43.sha-a384961'));
+        definitionMap.set('version', literal('11.1.0-next.3+44.sha-b473bc2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -20908,7 +20908,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.3+43.sha-a384961');
+    const VERSION$2 = new Version('11.1.0-next.3+44.sha-b473bc2');
 
     /**
      * @license
