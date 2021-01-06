974b311173cde05b7f96412034152535d7b0c6c2
atscott <atscott01@gmail.com>
2021-01-06 22:16:47 +0000

    48526ccdf1 docs: fix instructions for setting up `ProductDetailsComponent` in `start-routing.md` (#40197)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+56.sha-5b9c981
+ * @license Angular v11.0.6+5.sha-48526cc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19649,7 +19649,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.5+56.sha-5b9c981');
+    const VERSION$1 = new Version('11.0.6+5.sha-48526cc');
 
     /**
      * @license
@@ -20284,7 +20284,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.5+56.sha-5b9c981');
+    const VERSION$2 = new Version('11.0.6+5.sha-48526cc');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+56.sha-5b9c981
+ * @license Angular v11.0.6+5.sha-48526cc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18446,7 +18446,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.5+56.sha-5b9c981');
+    const VERSION$1 = new Version('11.0.6+5.sha-48526cc');
 
     /**
      * @license
@@ -34344,7 +34344,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.5+56.sha-5b9c981');
+    const VERSION$2 = new Version$1('11.0.6+5.sha-48526cc');
 
     /**
      * @license
