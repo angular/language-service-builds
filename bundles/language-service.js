0fed991774a930037f7905d2506a428904cb69e2
atscott <atscott01@gmail.com>
2021-01-06 22:22:42 +0000

    1e151f9fe4 docs: fix instructions for setting up `ProductDetailsComponent` in `start-routing.md` (#40197)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+3.sha-989b4a9
+ * @license Angular v11.1.0-next.4+7.sha-1e151f9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18942,7 +18942,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+3.sha-989b4a9');
+    const VERSION$1 = new Version('11.1.0-next.4+7.sha-1e151f9');
 
     /**
      * @license
@@ -46728,7 +46728,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+3.sha-989b4a9');
+    const VERSION$2 = new Version$1('11.1.0-next.4+7.sha-1e151f9');
 
     /**
      * @license
