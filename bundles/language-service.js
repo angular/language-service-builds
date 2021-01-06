a1f913f8d0974b3e0cb941394bebf3199e3fa036
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:26:12 +0000

    9e6b3cffea docs: update homepage headline (#40056)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+55.sha-6abc133
+ * @license Angular v11.1.0-next.3+56.sha-9e6b3cf
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18875,7 +18875,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+55.sha-6abc133');
+    const VERSION$1 = new Version('11.1.0-next.3+56.sha-9e6b3cf');
 
     /**
      * @license
@@ -34765,7 +34765,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.3+55.sha-6abc133');
+    const VERSION$2 = new Version$1('11.1.0-next.3+56.sha-9e6b3cf');
 
     /**
      * @license
