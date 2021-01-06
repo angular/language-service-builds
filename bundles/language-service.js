c5e37294a5ec218c8065656418be3cbcfe02ff02
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:53:53 +0000

    7eadf2e6da docs: correct some spellings (#40322)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+60.sha-7c16762
+ * @license Angular v11.1.0-next.3+62.sha-7eadf2e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18875,7 +18875,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+60.sha-7c16762');
+    const VERSION$1 = new Version('11.1.0-next.3+62.sha-7eadf2e');
 
     /**
      * @license
@@ -34765,7 +34765,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.3+60.sha-7c16762');
+    const VERSION$2 = new Version$1('11.1.0-next.3+62.sha-7eadf2e');
 
     /**
      * @license
