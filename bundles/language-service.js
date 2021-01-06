a792007a6dbb4a2c1c3b4895918e1c4eb36a92e6
GitHub <noreply@github.com>
2021-01-06 21:49:03 +0000

    bff48a0885 release: cut the v11.1.0-next.4 release (#40330)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+72.sha-c81d5bf
+ * @license Angular v11.1.0-next.3+73.sha-bff48a0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18942,7 +18942,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.3+72.sha-c81d5bf');
+    const VERSION$1 = new Version('11.1.0-next.3+73.sha-bff48a0');
 
     /**
      * @license
@@ -46728,7 +46728,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.3+72.sha-c81d5bf');
+    const VERSION$2 = new Version$1('11.1.0-next.3+73.sha-bff48a0');
 
     /**
      * @license
