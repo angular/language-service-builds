23cbff3b7edbd78a5abaf6dc9244f3a8a0ba29af
atscott <atscott01@gmail.com>
2021-01-07 21:50:39 +0000

    15b15be259 fix(compiler): recover event parse when animation event name is empty (#39925)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+29.sha-5704372
+ * @license Angular v11.1.0-next.4+30.sha-15b15be
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -9929,16 +9929,14 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
             const matches = splitAtPeriod(name, [name, '']);
             const eventName = matches[0];
             const phase = matches[1].toLowerCase();
+            const ast = this._parseAction(expression, handlerSpan);
+            targetEvents.push(new ParsedEvent(eventName, phase, 1 /* Animation */, ast, sourceSpan, handlerSpan, keySpan));
+            if (eventName.length === 0) {
+                this._reportError(`Animation event name is missing in binding`, sourceSpan);
+            }
             if (phase) {
-                switch (phase) {
-                    case 'start':
-                    case 'done':
-                        const ast = this._parseAction(expression, handlerSpan);
-                        targetEvents.push(new ParsedEvent(eventName, phase, 1 /* Animation */, ast, sourceSpan, handlerSpan, keySpan));
-                        break;
-                    default:
-                        this._reportError(`The provided animation output phase value "${phase}" for "@${eventName}" is not supported (use start or done)`, sourceSpan);
-                        break;
+                if (phase !== 'start' && phase !== 'done') {
+                    this._reportError(`The provided animation output phase value "${phase}" for "@${eventName}" is not supported (use start or done)`, sourceSpan);
                 }
             }
             else {
@@ -16943,7 +16941,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+29.sha-5704372');
+    const VERSION$1 = new Version('11.1.0-next.4+30.sha-15b15be');
 
     /**
      * @license
@@ -17625,7 +17623,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+29.sha-5704372'));
+        definitionMap.set('version', literal('11.1.0-next.4+30.sha-15b15be'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21065,7 +21063,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+29.sha-5704372');
+    const VERSION$2 = new Version('11.1.0-next.4+30.sha-15b15be');
 
     /**
      * @license
