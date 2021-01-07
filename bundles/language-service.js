bb8b8b505b003b435ca0c9ce107cc92c9fbf5bf3
atscott <atscott01@gmail.com>
2021-01-07 21:48:02 +0000

    15b15be259 fix(compiler): recover event parse when animation event name is empty (#39925)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+29.sha-5704372
+ * @license Angular v11.1.0-next.4+30.sha-15b15be
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -11255,16 +11255,14 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             const matches = splitAtPeriod(name, [name, '']);
             const eventName = matches[0];
             const phase = matches[1].toLowerCase();
+            const ast = this._parseAction(expression, handlerSpan);
+            targetEvents.push(new ParsedEvent(eventName, phase, 1 /* Animation */, ast, sourceSpan, handlerSpan, keySpan));
+            if (eventName.length === 0) {
+                this._reportError(`Animation event name is missing in binding`, sourceSpan);
+            }
             if (phase) {
-                switch (phase) {
-                    case 'start':
-                    case 'done':
-                        const ast = this._parseAction(expression, handlerSpan);
-                        targetEvents.push(new ParsedEvent(eventName, phase, 1 /* Animation */, ast, sourceSpan, handlerSpan, keySpan));
-                        break;
-                    default:
-                        this._reportError(`The provided animation output phase value "${phase}" for "@${eventName}" is not supported (use start or done)`, sourceSpan);
-                        break;
+                if (phase !== 'start' && phase !== 'done') {
+                    this._reportError(`The provided animation output phase value "${phase}" for "@${eventName}" is not supported (use start or done)`, sourceSpan);
                 }
             }
             else {
@@ -18953,7 +18951,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+29.sha-5704372');
+    const VERSION$1 = new Version('11.1.0-next.4+30.sha-15b15be');
 
     /**
      * @license
@@ -34843,7 +34841,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+29.sha-5704372');
+    const VERSION$2 = new Version$1('11.1.0-next.4+30.sha-15b15be');
 
     /**
      * @license
