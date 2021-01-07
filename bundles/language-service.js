22ceab39d2f1f47bb900a0f8818385cb48970392
atscott <atscott01@gmail.com>
2021-01-07 21:43:27 +0000

    266cc9b162 refactor(compiler-cli): support external template source-mapping when linking (#40237)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+11.sha-335c1ab
+ * @license Angular v11.1.0-next.4+21.sha-266cc9b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17801,6 +17801,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 interpolationConfig,
                 preserveWhitespaces,
                 template,
+                templateUrl,
                 isInline,
                 errors: parseResult.errors,
                 nodes: [],
@@ -17821,6 +17822,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 interpolationConfig,
                 preserveWhitespaces,
                 template,
+                templateUrl,
                 isInline,
                 errors: i18nMetaResult.errors,
                 nodes: [],
@@ -17846,6 +17848,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
             preserveWhitespaces,
             errors: errors.length > 0 ? errors : null,
             template,
+            templateUrl,
             isInline,
             nodes,
             styleUrls,
@@ -18942,7 +18945,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+11.sha-335c1ab');
+    const VERSION$1 = new Version('11.1.0-next.4+21.sha-266cc9b');
 
     /**
      * @license
@@ -46728,7 +46731,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+11.sha-335c1ab');
+    const VERSION$2 = new Version$1('11.1.0-next.4+21.sha-266cc9b');
 
     /**
      * @license
