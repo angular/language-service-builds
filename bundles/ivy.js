1ad1948204af49aaea3e87394cf28d1add5fe683
atscott <atscott01@gmail.com>
2021-01-07 21:34:43 +0000

    40a03f0f28 docs: lifecycle-hooks example - extract things (components, directives) into their own files (#40212)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.6+7.sha-64d94e8
+ * @license Angular v11.0.7+4.sha-40a03f0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19649,7 +19649,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.6+7.sha-64d94e8');
+    const VERSION$1 = new Version('11.0.7+4.sha-40a03f0');
 
     /**
      * @license
@@ -20284,7 +20284,7 @@ define(['exports', 'os', 'typescript', 'fs', 'constants', 'stream', 'util', 'ass
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.0.6+7.sha-64d94e8');
+    const VERSION$2 = new Version('11.0.7+4.sha-40a03f0');
 
     /**
      * @license
