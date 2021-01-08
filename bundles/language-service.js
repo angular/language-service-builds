370ece5057ea23cf7c520dbe6ceba990359d6bad
Andrew Scott <atscott@google.com>
2021-01-08 18:09:24 +0000

    fdbd3cae8a fix(forms): handle standalone `<form>` tag correctly in `NgControlStatusGroup` directive (#40344)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+37.sha-7a819ca
+ * @license Angular v11.1.0-next.4+38.sha-fdbd3ca
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18951,7 +18951,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+37.sha-7a819ca');
+    const VERSION$1 = new Version('11.1.0-next.4+38.sha-fdbd3ca');
 
     /**
      * @license
@@ -34841,7 +34841,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+37.sha-7a819ca');
+    const VERSION$2 = new Version$1('11.1.0-next.4+38.sha-fdbd3ca');
 
     /**
      * @license
