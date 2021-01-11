78b85f127480ee66d6447101dcfe85cb7ba08ea3
atscott <atscott01@gmail.com>
2021-01-11 19:32:37 +0000

    c9de7daae4 build: update bazel rules_nodejs to 2.3.2 (#40367)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+58.sha-4bb067d
+ * @license Angular v11.1.0-next.4+65.sha-c9de7da
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18951,7 +18951,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+58.sha-4bb067d');
+    const VERSION$1 = new Version('11.1.0-next.4+65.sha-c9de7da');
 
     /**
      * @license
@@ -34904,7 +34904,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+58.sha-4bb067d');
+    const VERSION$2 = new Version$1('11.1.0-next.4+65.sha-c9de7da');
 
     /**
      * @license
