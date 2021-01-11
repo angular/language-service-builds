eb049630d3d7a317068d81171f16900fc69de822
atscott <atscott01@gmail.com>
2021-01-11 18:21:00 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+50.sha-1e46b73
+ * @license Angular v11.1.0-next.4+53.sha-3d7e207
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18951,7 +18951,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+50.sha-1e46b73');
+    const VERSION$1 = new Version('11.1.0-next.4+53.sha-3d7e207');
 
     /**
      * @license
@@ -34904,7 +34904,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+50.sha-1e46b73');
+    const VERSION$2 = new Version$1('11.1.0-next.4+53.sha-3d7e207');
 
     /**
      * @license
