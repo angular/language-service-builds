fdb723351d4278554ec5e05643fd43c1bb90d84e
atscott <atscott01@gmail.com>
2021-01-11 19:19:54 +0000

    4bb067d738 docs: update JSDOC comment for the ArbTranslationSerializer (#40354)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+64.sha-f8aad11
+ * @license Angular v11.1.0-next.4+58.sha-4bb067d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18951,7 +18951,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+64.sha-f8aad11');
+    const VERSION$1 = new Version('11.1.0-next.4+58.sha-4bb067d');
 
     /**
      * @license
@@ -34904,7 +34904,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+64.sha-f8aad11');
+    const VERSION$2 = new Version$1('11.1.0-next.4+58.sha-4bb067d');
 
     /**
      * @license
