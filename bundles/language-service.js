254d39d8a78bfba9b31595f8e680833d4a3ace39
atscott <atscott01@gmail.com>
2021-01-11 18:19:58 +0000

    6438a29eb4 docs(router): fix minor grammatical issue (#40380)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.7+13.sha-c44dd84
+ * @license Angular v11.0.8+6.sha-6438a29
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18446,7 +18446,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.7+13.sha-c44dd84');
+    const VERSION$1 = new Version('11.0.8+6.sha-6438a29');
 
     /**
      * @license
@@ -34407,7 +34407,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.7+13.sha-c44dd84');
+    const VERSION$2 = new Version$1('11.0.8+6.sha-6438a29');
 
     /**
      * @license
