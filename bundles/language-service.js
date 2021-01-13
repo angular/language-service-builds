8cad1833e345a70b91373060fc9605a3f032c648
GitHub <noreply@github.com>
2021-01-13 19:14:32 +0000

    c6ca7b4a12 release: cut the v11.0.9 release (#40414)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+30.sha-642c45b
+ * @license Angular v11.0.8+31.sha-c6ca7b4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18457,7 +18457,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.0.8+30.sha-642c45b');
+    const VERSION$1 = new Version('11.0.8+31.sha-c6ca7b4');
 
     /**
      * @license
@@ -34393,7 +34393,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.0.8+30.sha-642c45b');
+    const VERSION$2 = new Version$1('11.0.8+31.sha-c6ca7b4');
 
     /**
      * @license
