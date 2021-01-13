d62229816b7b439f9706fec6085511e6d5dd22c9
atscott <atscott01@gmail.com>
2021-01-13 22:36:19 +0000

    0664d750b6 perf(core): make DI decorators tree-shakable when used for `useFactory` deps config (#40145)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.5+1.sha-ebd94ef
+ * @license Angular v11.1.0-rc.0+7.sha-0664d75
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16952,7 +16952,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.5+1.sha-ebd94ef');
+    const VERSION$1 = new Version('11.1.0-rc.0+7.sha-0664d75');
 
     /**
      * @license
@@ -17609,7 +17609,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.5+1.sha-ebd94ef'));
+        definitionMap.set('version', literal('11.1.0-rc.0+7.sha-0664d75'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21052,7 +21052,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.5+1.sha-ebd94ef');
+    const VERSION$2 = new Version('11.1.0-rc.0+7.sha-0664d75');
 
     /**
      * @license
