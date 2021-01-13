10ecf3b9a2ee5206624d9a2127d3a7885d1433bd
atscott <atscott01@gmail.com>
2021-01-13 17:31:59 +0000

    625d2c252b fix(language-service): diagnostic and definition should work for absolute url (#40406)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+80.sha-b48eabd
+ * @license Angular v11.1.0-next.4+81.sha-625d2c2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16952,7 +16952,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+80.sha-b48eabd');
+    const VERSION$1 = new Version('11.1.0-next.4+81.sha-625d2c2');
 
     /**
      * @license
@@ -17609,7 +17609,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+80.sha-b48eabd'));
+        definitionMap.set('version', literal('11.1.0-next.4+81.sha-625d2c2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21052,7 +21052,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+80.sha-b48eabd');
+    const VERSION$2 = new Version('11.1.0-next.4+81.sha-625d2c2');
 
     /**
      * @license
