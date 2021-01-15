0d576191ecb7796cf37da2cef2045f2362892277
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:39:09 +0000

    b37a9eba1a fix(router): lazy loaded modules without RouterModule.forChild() won't cause an infinite loop (#36605)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+108.sha-5457936
+ * @license Angular v11.1.0-next.4+111.sha-b37a9eb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16962,7 +16962,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+108.sha-5457936');
+    const VERSION$1 = new Version('11.1.0-next.4+111.sha-b37a9eb');
 
     /**
      * @license
@@ -17619,7 +17619,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+108.sha-5457936'));
+        definitionMap.set('version', literal('11.1.0-next.4+111.sha-b37a9eb'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21067,7 +21067,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+108.sha-5457936');
+    const VERSION$2 = new Version('11.1.0-next.4+111.sha-b37a9eb');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+108.sha-5457936
+ * @license Angular v11.1.0-next.4+111.sha-b37a9eb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18972,7 +18972,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+108.sha-5457936');
+    const VERSION$1 = new Version('11.1.0-next.4+111.sha-b37a9eb');
 
     /**
      * @license
@@ -34952,7 +34952,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+108.sha-5457936');
+    const VERSION$2 = new Version$1('11.1.0-next.4+111.sha-b37a9eb');
 
     /**
      * @license
