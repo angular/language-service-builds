1bfe097d18e3590856fdc78b221d1f8dea8c4598
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:42:35 +0000

    96690ed3a4 feat(docs-infra): add shortcuts for the angular.io PWA (#40393)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+111.sha-b37a9eb
+ * @license Angular v11.1.0-next.4+110.sha-96690ed
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18972,7 +18972,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+111.sha-b37a9eb');
+    const VERSION$1 = new Version('11.1.0-next.4+110.sha-96690ed');
 
     /**
      * @license
@@ -46818,7 +46818,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+111.sha-b37a9eb');
+    const VERSION$2 = new Version$1('11.1.0-next.4+110.sha-96690ed');
 
     /**
      * @license
