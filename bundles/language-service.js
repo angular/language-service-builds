941eca2aea93dcb7dcc236b375b2d3c2a1b62913
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:13:47 +0000

    61792cc6f5 refactor(compiler): remove unused files in css_parser/ and corresponding spec tests (#37463)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+110.sha-96690ed
+ * @license Angular v11.1.0-next.4+113.sha-61792cc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18972,7 +18972,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+110.sha-96690ed');
+    const VERSION$1 = new Version('11.1.0-next.4+113.sha-61792cc');
 
     /**
      * @license
@@ -34952,7 +34952,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+110.sha-96690ed');
+    const VERSION$2 = new Version$1('11.1.0-next.4+113.sha-61792cc');
 
     /**
      * @license
