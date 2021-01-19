7476865d2c93bc1d5d31371a514e4e1198f32d87
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:48:31 +0000

    0568c7387f docs: add Angular Builders to community resources (#40475)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+120.sha-2c95f57
+ * @license Angular v11.1.0-next.4+121.sha-0568c73
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+120.sha-2c95f57');
+    const VERSION$1 = new Version('11.1.0-next.4+121.sha-0568c73');
 
     /**
      * @license
@@ -34964,7 +34964,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+120.sha-2c95f57');
+    const VERSION$2 = new Version$1('11.1.0-next.4+121.sha-0568c73');
 
     /**
      * @license
