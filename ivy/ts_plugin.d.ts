f2553b83c2eabc1a62b270c1425f7cc98017f04d
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:28:41 +0000

    2b2a847ad7 refactor(migrations): remove rxjs usage within static queries migration (#38657)

--- ivy/ts_plugin.d.ts
+++ ivy/ts_plugin.d.ts
@@ -7,9 +7,5 @@
  */
 /// <amd-module name="@angular/language-service/ivy/ts_plugin" />
 import * as ts from 'typescript/lib/tsserverlibrary';
-import { GetTcbResponse } from './language_service';
-export interface NgLanguageService extends ts.LanguageService {
-    getTcb(fileName: string, position: number): GetTcbResponse;
-}
-export declare function create(info: ts.server.PluginCreateInfo): NgLanguageService;
+export declare function create(info: ts.server.PluginCreateInfo): ts.LanguageService;
 export declare function getExternalFiles(project: ts.server.Project): string[];
