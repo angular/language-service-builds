1e5ce7c394c57b84cdab4ce13fc5ccdad0f9e6df
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:53:23 +0000

    3cf4e3c7c5 fix(router): always stringify matrix parameters (#25095)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+137.sha-315b272
+ * @license Angular v11.1.0-next.4+139.sha-3cf4e3c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+137.sha-315b272');
+    const VERSION$1 = new Version('11.1.0-next.4+139.sha-3cf4e3c');
 
     /**
      * @license
@@ -34989,7 +34989,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+137.sha-315b272');
+    const VERSION$2 = new Version$1('11.1.0-next.4+139.sha-3cf4e3c');
 
     /**
      * @license
