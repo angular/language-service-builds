3623e75cf4c7cf178bf173fa69af4373c9bb3007
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 20:50:31 +0000

    69fd94220e fix(router): Fix occasional error when creating url tree in IE 11 and Edge (#40488)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+11.sha-e995d85
+ * @license Angular v11.1.0+14.sha-69fd942
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0+11.sha-e995d85');
+    const VERSION$1 = new Version('11.1.0+14.sha-69fd942');
 
     /**
      * @license
@@ -34974,7 +34974,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0+11.sha-e995d85');
+    const VERSION$2 = new Version$1('11.1.0+14.sha-69fd942');
 
     /**
      * @license
