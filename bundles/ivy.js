1c57a98f1167ee01037bf434d34fc9982ba608c5
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:50:19 +0000

    a8a27efb77 fix(router): always stringify matrix parameters (#25095)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0
+ * @license Angular v11.1.0+9.sha-a8a27ef
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16974,7 +16974,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0');
+    const VERSION$1 = new Version('11.1.0+9.sha-a8a27ef');
 
     /**
      * @license
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0'));
+        definitionMap.set('version', literal('11.1.0+9.sha-a8a27ef'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21079,7 +21079,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0');
+    const VERSION$2 = new Version('11.1.0+9.sha-a8a27ef');
 
     /**
      * @license
