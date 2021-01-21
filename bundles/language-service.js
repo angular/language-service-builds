df66c961f81f993c73fb1ac76885e3d0a4f406a9
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:21:58 +0000

    575a2d162c feat(common): implement `appendAll()` method on `HttpParams` (#20930)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+149.sha-c9033d6
+ * @license Angular v11.1.0-next.4+150.sha-575a2d1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+149.sha-c9033d6');
+    const VERSION$1 = new Version('11.1.0-next.4+150.sha-575a2d1');
 
     /**
      * @license
@@ -34974,7 +34974,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+149.sha-c9033d6');
+    const VERSION$2 = new Version$1('11.1.0-next.4+150.sha-575a2d1');
 
     /**
      * @license
