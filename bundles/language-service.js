0668e960d4f73a91c5061d545aaf8bb83a237754
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 22:41:39 +0000

    bb3b315eee fix(core): fix possible XSS attack in development through SSR (#40525)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+157.sha-c5599cc
+ * @license Angular v11.1.0-next.4+158.sha-bb3b315
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19032,7 +19032,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+157.sha-c5599cc');
+    const VERSION$1 = new Version('11.1.0-next.4+158.sha-bb3b315');
 
     /**
      * @license
@@ -35039,7 +35039,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+157.sha-c5599cc');
+    const VERSION$2 = new Version$1('11.1.0-next.4+158.sha-bb3b315');
 
     /**
      * @license
