84f1ff6235aacd9040b044d42ee7e80d0e5762eb
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:37:11 +0000

    37710b9be3 fix(service-worker): handle error with console.error (#40236)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+26.sha-dfe4200
+ * @license Angular v11.1.0+30.sha-37710b9
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0+26.sha-dfe4200');
+    const VERSION$1 = new Version('11.1.0+30.sha-37710b9');
 
     /**
      * @license
@@ -34991,7 +34991,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0+26.sha-dfe4200');
+    const VERSION$2 = new Version$1('11.1.0+30.sha-37710b9');
 
     /**
      * @license
