1bd8ffe0e127155061e17fad6c72ed2ffb583c11
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:34:31 +0000

    cffb00ec11 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+182.sha-39b3cbb
+ * @license Angular v11.1.0-next.4+183.sha-cffb00e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -16974,7 +16974,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+182.sha-39b3cbb');
+    const VERSION$1 = new Version('11.1.0-next.4+183.sha-cffb00e');
 
     /**
      * @license
@@ -17631,7 +17631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+182.sha-39b3cbb'));
+        definitionMap.set('version', literal('11.1.0-next.4+183.sha-cffb00e'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21079,7 +21079,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+182.sha-39b3cbb');
+    const VERSION$2 = new Version('11.1.0-next.4+183.sha-cffb00e');
 
     /**
      * @license
