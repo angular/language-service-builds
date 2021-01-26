9a1d659b2c5f6de4433162a92772e00cdf512fdf
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:47:41 +0000

    365ac5e68e docs: fixed issue with missing code sample by rearranging import to proper docregion (#40565)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+177.sha-956f75f
+ * @license Angular v11.1.0-next.4+178.sha-365ac5e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+177.sha-956f75f');
+    const VERSION$1 = new Version('11.1.0-next.4+178.sha-365ac5e');
 
     /**
      * @license
@@ -34991,7 +34991,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+177.sha-956f75f');
+    const VERSION$2 = new Version$1('11.1.0-next.4+178.sha-365ac5e');
 
     /**
      * @license
