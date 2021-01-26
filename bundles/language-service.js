417acfb65397169c598d3080f70c3c995b4c4d61
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 19:25:53 +0000

    ffad040a96 build: update to yarn v1.22.10 (#40562)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+179.sha-6bf99e0
+ * @license Angular v11.1.0-next.4+181.sha-ffad040
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+179.sha-6bf99e0');
+    const VERSION$1 = new Version('11.1.0-next.4+181.sha-ffad040');
 
     /**
      * @license
@@ -34991,7 +34991,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+179.sha-6bf99e0');
+    const VERSION$2 = new Version$1('11.1.0-next.4+181.sha-ffad040');
 
     /**
      * @license
