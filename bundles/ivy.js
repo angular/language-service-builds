4302c2f861da08fc5fac3e9e7f86f85af88bf2f3
GitHub <noreply@github.com>
2021-01-27 20:21:48 +0000

    e75a4f261e docs: release notes for the v11.1.1 release (#40599)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+194.sha-be979c9
+ * @license Angular v11.1.0-next.4+195.sha-e75a4f2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17008,7 +17008,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+194.sha-be979c9');
+    const VERSION$1 = new Version('11.1.0-next.4+195.sha-e75a4f2');
 
     /**
      * @license
@@ -17665,7 +17665,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+194.sha-be979c9'));
+        definitionMap.set('version', literal('11.1.0-next.4+195.sha-e75a4f2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21113,7 +21113,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+194.sha-be979c9');
+    const VERSION$2 = new Version('11.1.0-next.4+195.sha-e75a4f2');
 
     /**
      * @license
