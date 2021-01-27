3acfab75ff7b26f6068305b2effa70481b71fabc
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:07:43 +0000

    d703d21656 fix(dev-infra): fix substitution string for component_benchmark (#40591)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+186.sha-f1cf875
+ * @license Angular v11.1.0-next.4+190.sha-d703d21
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -18984,7 +18984,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+186.sha-f1cf875');
+    const VERSION$1 = new Version('11.1.0-next.4+190.sha-d703d21');
 
     /**
      * @license
@@ -34991,7 +34991,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+186.sha-f1cf875');
+    const VERSION$2 = new Version$1('11.1.0-next.4+190.sha-d703d21');
 
     /**
      * @license
