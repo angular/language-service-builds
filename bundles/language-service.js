39fd2103ba28e1057e39140ce1c6d976805ad2fe
GitHub <noreply@github.com>
2021-01-27 21:22:48 +0000

    8772fbd4bb release: bump the version to 12.0.0-next.0 (#40604)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+195.sha-e75a4f2
+ * @license Angular v11.1.0-next.4+196.sha-8772fbd
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19018,7 +19018,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+195.sha-e75a4f2');
+    const VERSION$1 = new Version('11.1.0-next.4+196.sha-8772fbd');
 
     /**
      * @license
@@ -35025,7 +35025,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+195.sha-e75a4f2');
+    const VERSION$2 = new Version$1('11.1.0-next.4+196.sha-8772fbd');
 
     /**
      * @license
