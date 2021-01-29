6923ddd5e7f8e2e908e480dd60349e06d2ab0520
Misko Hevery <misko@hevery.com>
2021-01-29 00:15:08 +0000

    22f9e454a4 fix(core): NgZone coaleascing options should trigger onStable correctly (#40540)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+208.sha-88f8ddd
+ * @license Angular v11.1.0-next.4+209.sha-22f9e45
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+208.sha-88f8ddd');
+    const VERSION$1 = new Version('11.1.0-next.4+209.sha-22f9e45');
 
     /**
      * @license
@@ -35028,7 +35028,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+208.sha-88f8ddd');
+    const VERSION$2 = new Version$1('11.1.0-next.4+209.sha-22f9e45');
 
     /**
      * @license
@@ -39127,6 +39127,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                 !shouldCoalesceRunChangeDetection && shouldCoalesceEventChangeDetection;
             self.shouldCoalesceRunChangeDetection = shouldCoalesceRunChangeDetection;
             self.lastRequestAnimationFrameId = -1;
+            self.isCheckStableRunning = false;
             self.nativeRequestAnimationFrame = getNativeRequestAnimationFrame().nativeRequestAnimationFrame;
             forkInnerZoneWithAngularBehavior(self);
         }
@@ -39206,7 +39207,9 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     }
     const EMPTY_PAYLOAD = {};
     function checkStable(zone) {
-        if (zone._nesting == 0 && !zone.hasPendingMicrotasks && !zone.isStable) {
+        if (!zone.isCheckStableRunning && zone._nesting == 0 && !zone.hasPendingMicrotasks &&
+            !zone.isStable) {
+            zone.isCheckStableRunning = true;
             try {
                 zone._nesting++;
                 zone.onMicrotaskEmpty.emit(null);
@@ -39221,11 +39224,25 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
                         zone.isStable = true;
                     }
                 }
+                zone.isCheckStableRunning = false;
             }
         }
     }
     function delayChangeDetectionForEvents(zone) {
-        if (zone.lastRequestAnimationFrameId !== -1) {
+        /**
+         * We also need to check isCheckStableRunning here
+         * Consider the following case with shouldCoalesceRunChangeDetection = true
+         *
+         * ngZone.run(() => {});
+         * ngZone.run(() => {});
+         *
+         * We want the two `ngZone.run()` only trigger one change detection
+         * when shouldCoalesceRunChangeDetection is true.
+         * And because in this case, change detection run in async way(requestAnimationFrame),
+         * so we also need to check the isCheckStableRunning here to prevent multiple
+         * change detections.
+         */
+        if (zone.isCheckStableRunning || zone.lastRequestAnimationFrameId !== -1) {
             return;
         }
         zone.lastRequestAnimationFrameId = zone.nativeRequestAnimationFrame.call(_global$1, () => {
