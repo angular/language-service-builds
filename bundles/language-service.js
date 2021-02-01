c39e5e095446da5579048f6ec8da46ca89e9638c
Misko Hevery <misko@hevery.com>
2021-02-01 17:38:09 +0000

    caafac2248 fix(compiler-cli): preserve user line endings in diagnostic template parse (#40597)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+15.sha-4c5f18d
+ * @license Angular v11.2.0-next.0+18.sha-caafac2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.0-next.0+15.sha-4c5f18d');
+    const VERSION$1 = new Version('11.2.0-next.0+18.sha-caafac2');
 
     /**
      * @license
@@ -35029,7 +35029,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.0-next.0+15.sha-4c5f18d');
+    const VERSION$2 = new Version$1('11.2.0-next.0+18.sha-caafac2');
 
     /**
      * @license
