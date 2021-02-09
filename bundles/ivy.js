6074af392e4f732f792cd1f82eac911b09fbc72c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:43:46 +0000

    c0d1e34d53 docs: update docs-style-guide (#36281)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-rc.0+63.sha-415ad8d
+ * @license Angular v11.2.0-rc.0+65.sha-c0d1e34
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.0-rc.0+63.sha-415ad8d');
+    const VERSION$1 = new Version('11.2.0-rc.0+65.sha-c0d1e34');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.0-rc.0+63.sha-415ad8d'));
+        definitionMap.set('version', literal('11.2.0-rc.0+65.sha-c0d1e34'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21116,7 +21116,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.2.0-rc.0+63.sha-415ad8d');
+    const VERSION$2 = new Version('11.2.0-rc.0+65.sha-c0d1e34');
 
     /**
      * @license
--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-rc.0+63.sha-415ad8d
+ * @license Angular v11.2.0-rc.0+65.sha-c0d1e34
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.0-rc.0+63.sha-415ad8d');
+    const VERSION$1 = new Version('11.2.0-rc.0+65.sha-c0d1e34');
 
     /**
      * @license
@@ -35029,7 +35029,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.0-rc.0+63.sha-415ad8d');
+    const VERSION$2 = new Version$1('11.2.0-rc.0+65.sha-c0d1e34');
 
     /**
      * @license
