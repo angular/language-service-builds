65d87c23634111d1a512780a63e1bc9358b93142
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:10:11 +0000

    089a634053 refactor(docs-infra): use a single CSS class to target the API list container (#40704)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+249.sha-0652b29
+ * @license Angular v11.1.0-next.4+284.sha-089a634
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+249.sha-0652b29');
+    const VERSION$1 = new Version('11.1.0-next.4+284.sha-089a634');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+249.sha-0652b29'));
+        definitionMap.set('version', literal('11.1.0-next.4+284.sha-089a634'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21116,7 +21116,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+249.sha-0652b29');
+    const VERSION$2 = new Version('11.1.0-next.4+284.sha-089a634');
 
     /**
      * @license
