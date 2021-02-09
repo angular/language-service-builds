291dc61379558ff4e1436a1326792ba80c019ef1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:15:13 +0000

    ce44a3d1dc fix(localize): support downleveled inlined helper localize calls (#40754)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+307.sha-46ab859
+ * @license Angular v11.1.0-next.4+306.sha-ce44a3d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+307.sha-46ab859');
+    const VERSION$1 = new Version('11.1.0-next.4+306.sha-ce44a3d');
 
     /**
      * @license
@@ -46908,7 +46908,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+307.sha-46ab859');
+    const VERSION$2 = new Version$1('11.1.0-next.4+306.sha-ce44a3d');
 
     /**
      * @license
