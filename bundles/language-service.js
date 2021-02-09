1f66645dd4b7f13c2db3500f258ed77354f641ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:11:09 +0000

    4ce44eac33 fix(animations): properly track listeners for a removed element (#40712)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+284.sha-089a634
+ * @license Angular v11.1.0-next.4+302.sha-4ce44ea
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+284.sha-089a634');
+    const VERSION$1 = new Version('11.1.0-next.4+302.sha-4ce44ea');
 
     /**
      * @license
@@ -35029,7 +35029,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+284.sha-089a634');
+    const VERSION$2 = new Version$1('11.1.0-next.4+302.sha-4ce44ea');
 
     /**
      * @license
