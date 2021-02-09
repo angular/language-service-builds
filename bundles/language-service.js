1653becea03ce1b28cf5131773d2f993d1c3d4f4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:09:14 +0000

    0652b29f62 fix(zone.js): setTimeout patch should clean tasksByHandleId cache. (#40586)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+250.sha-2282069
+ * @license Angular v11.1.0-next.4+249.sha-0652b29
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+250.sha-2282069');
+    const VERSION$1 = new Version('11.1.0-next.4+249.sha-0652b29');
 
     /**
      * @license
@@ -46908,7 +46908,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+250.sha-2282069');
+    const VERSION$2 = new Version$1('11.1.0-next.4+249.sha-0652b29');
 
     /**
      * @license
