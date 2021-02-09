11ad3bf3f1d3e27faacf31dae79784e15d629d57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:13:49 +0000

    9dada8a2dd fix(localize): improve invalid syntax in extraction error message (#40724)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+302.sha-4ce44ea
+ * @license Angular v11.1.0-next.4+303.sha-9dada8a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+302.sha-4ce44ea');
+    const VERSION$1 = new Version('11.1.0-next.4+303.sha-9dada8a');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+302.sha-4ce44ea'));
+        definitionMap.set('version', literal('11.1.0-next.4+303.sha-9dada8a'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21116,7 +21116,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.1.0-next.4+302.sha-4ce44ea');
+    const VERSION$2 = new Version('11.1.0-next.4+303.sha-9dada8a');
 
     /**
      * @license
