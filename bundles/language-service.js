48fe43b1e77dcc831a423713d75b6002751dd01b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:47:41 +0000

    ddff6b63d7 docs: fix absent documentation for digitsInfo parameter in the API docs (#40746)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+321.sha-7a8821c
+ * @license Angular v11.1.0-next.4+322.sha-ddff6b6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+321.sha-7a8821c');
+    const VERSION$1 = new Version('11.1.0-next.4+322.sha-ddff6b6');
 
     /**
      * @license
@@ -46928,7 +46928,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+321.sha-7a8821c');
+    const VERSION$2 = new Version$1('11.1.0-next.4+322.sha-ddff6b6');
 
     /**
      * @license
