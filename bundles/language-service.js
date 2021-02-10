b8ebcc7de6ec2513b62fae41916a97bf05b350c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:25:58 +0000

    378da71f27 fix(compiler-cli): don't crash when we can't resolve a resource (#40660)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+312.sha-bc5a694
+ * @license Angular v11.1.0-next.4+313.sha-378da71
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+312.sha-bc5a694');
+    const VERSION$1 = new Version('11.1.0-next.4+313.sha-378da71');
 
     /**
      * @license
@@ -35029,7 +35029,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+312.sha-bc5a694');
+    const VERSION$2 = new Version$1('11.1.0-next.4+313.sha-378da71');
 
     /**
      * @license
