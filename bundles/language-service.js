4957155b6559d2f801182db834c9475d1d6a4ffc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:10:09 +0000

    bc5a694b82 release: update 11.2.0-rc.0 release notes (#40706)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+309.sha-61a0b6d
+ * @license Angular v11.1.0-next.4+312.sha-bc5a694
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.1.0-next.4+309.sha-61a0b6d');
+    const VERSION$1 = new Version('11.1.0-next.4+312.sha-bc5a694');
 
     /**
      * @license
@@ -35029,7 +35029,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.1.0-next.4+309.sha-61a0b6d');
+    const VERSION$2 = new Version$1('11.1.0-next.4+312.sha-bc5a694');
 
     /**
      * @license
