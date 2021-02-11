cf5eb0277e2a540f20e99f6a563c1f7307d1792f
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:33:42 +0000

    c22ae5d9b9 fix(dev-infra): update npm package subsitutions to reflect no longer having an imported workspace (#40785)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+7.sha-2d7bc1d
+ * @license Angular v12.0.0-next.0+8.sha-c22ae5d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+7.sha-2d7bc1d');
+    const VERSION$1 = new Version('12.0.0-next.0+8.sha-c22ae5d');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.0+7.sha-2d7bc1d'));
+        definitionMap.set('version', literal('12.0.0-next.0+8.sha-c22ae5d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21116,7 +21116,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('12.0.0-next.0+7.sha-2d7bc1d');
+    const VERSION$2 = new Version('12.0.0-next.0+8.sha-c22ae5d');
 
     /**
      * @license
