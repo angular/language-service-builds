37aec0b6c4c87faea15eeadb631bb0755c3af1d7
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:35:45 +0000

    dfc9f36dd8 build: update browserstacktunnel-wrapper (#40810)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+13.sha-e27787a
+ * @license Angular v12.0.0-next.0+16.sha-dfc9f36
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+13.sha-e27787a');
+    const VERSION$1 = new Version('12.0.0-next.0+16.sha-dfc9f36');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.0+13.sha-e27787a'));
+        definitionMap.set('version', literal('12.0.0-next.0+16.sha-dfc9f36'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21126,7 +21126,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('12.0.0-next.0+13.sha-e27787a');
+    const VERSION$2 = new Version('12.0.0-next.0+16.sha-dfc9f36');
 
     /**
      * @license
