fac619764bfe4d4a2de27631d1601e99440ab595
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:22:28 +0000

    2d7bc1d301 ci: Remove language-service from testing group (#40795)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+6.sha-7ca244a
+ * @license Angular v12.0.0-next.0+7.sha-2d7bc1d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+6.sha-7ca244a');
+    const VERSION$1 = new Version('12.0.0-next.0+7.sha-2d7bc1d');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.0+6.sha-7ca244a'));
+        definitionMap.set('version', literal('12.0.0-next.0+7.sha-2d7bc1d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21116,7 +21116,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('12.0.0-next.0+6.sha-7ca244a');
+    const VERSION$2 = new Version('12.0.0-next.0+7.sha-2d7bc1d');
 
     /**
      * @license
