799805f752b0852ae70e450054cee98d9cf3692d
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:29:38 +0000

    c56ecab515 feat(common): support ICU standard "stand alone day of week" with `DatePipe` (#40766)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+2.sha-6f017a4
+ * @license Angular v12.0.0-next.0+3.sha-c56ecab
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+2.sha-6f017a4');
+    const VERSION$1 = new Version('12.0.0-next.0+3.sha-c56ecab');
 
     /**
      * @license
@@ -35047,7 +35047,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+2.sha-6f017a4');
+    const VERSION$2 = new Version$1('12.0.0-next.0+3.sha-c56ecab');
 
     /**
      * @license
