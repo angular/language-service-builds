ef150609eeed477836243dc5b158b04d6bf2a033
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:33:43 +0000

    4c6d557ca9 build: update browserstacktunnel-wrapper (#40810)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+13.sha-925e2b7
+ * @license Angular v11.2.0+16.sha-4c6d557
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.0+13.sha-925e2b7');
+    const VERSION$1 = new Version('11.2.0+16.sha-4c6d557');
 
     /**
      * @license
@@ -35047,7 +35047,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.0+13.sha-925e2b7');
+    const VERSION$2 = new Version$1('11.2.0+16.sha-4c6d557');
 
     /**
      * @license
