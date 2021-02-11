915c66435a9282efe7bca748071a952d962c0c4a
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:31:47 +0000

    c56ecab515 feat(common): support ICU standard "stand alone day of week" with `DatePipe` (#40766)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+2.sha-6f017a4
+ * @license Angular v12.0.0-next.0+3.sha-c56ecab
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17011,7 +17011,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+2.sha-6f017a4');
+    const VERSION$1 = new Version('12.0.0-next.0+3.sha-c56ecab');
 
     /**
      * @license
@@ -17668,7 +17668,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.0+2.sha-6f017a4'));
+        definitionMap.set('version', literal('12.0.0-next.0+3.sha-c56ecab'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21116,7 +21116,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('12.0.0-next.0+2.sha-6f017a4');
+    const VERSION$2 = new Version('12.0.0-next.0+3.sha-c56ecab');
 
     /**
      * @license
