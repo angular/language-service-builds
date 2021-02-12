752853a6d18677fd9714026e74eaae90a90425a0
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:14:18 +0000

    43ecf8a77b feat(platform-server): allow shimming the global env sooner (#40559)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+20.sha-80f4ff3
+ * @license Angular v12.0.0-next.0+24.sha-43ecf8a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19021,7 +19021,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+20.sha-80f4ff3');
+    const VERSION$1 = new Version('12.0.0-next.0+24.sha-43ecf8a');
 
     /**
      * @license
@@ -46928,7 +46928,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+20.sha-80f4ff3');
+    const VERSION$2 = new Version$1('12.0.0-next.0+24.sha-43ecf8a');
 
     /**
      * @license
