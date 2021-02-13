413b2a0fb6ddfa32e33653edbbf4bef58ab17784
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:10:47 +0000

    5554a3f174 docs: add NgRx book to resources (#39841)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+34.sha-9661873
+ * @license Angular v12.0.0-next.0+33.sha-5554a3f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17031,7 +17031,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+34.sha-9661873');
+    const VERSION$1 = new Version('12.0.0-next.0+33.sha-5554a3f');
 
     /**
      * @license
@@ -17688,7 +17688,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.0+34.sha-9661873'));
+        definitionMap.set('version', literal('12.0.0-next.0+33.sha-5554a3f'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -17909,7 +17909,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.0+34.sha-9661873'));
+        definitionMap.set('version', literal('12.0.0-next.0+33.sha-5554a3f'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -21181,7 +21181,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('12.0.0-next.0+34.sha-9661873');
+    const VERSION$2 = new Version('12.0.0-next.0+33.sha-5554a3f');
 
     /**
      * @license
