6b065035b5cd4caa481d3374a4d911a95a423608
Joey Perrott <josephperrott@gmail.com>
2021-02-13 00:30:42 +0000

    ccd10ea315 docs: Clarify new ng workspace options (#40816)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+30.sha-44b8b0f
+ * @license Angular v12.0.0-next.0+31.sha-ccd10ea
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19041,7 +19041,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+30.sha-44b8b0f');
+    const VERSION$1 = new Version('12.0.0-next.0+31.sha-ccd10ea');
 
     /**
      * @license
@@ -35067,7 +35067,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+30.sha-44b8b0f');
+    const VERSION$2 = new Version$1('12.0.0-next.0+31.sha-ccd10ea');
 
     /**
      * @license
