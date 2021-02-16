97255f3d46f38f8e08f0a7d15405fc7378cc74eb
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:03:04 +0000

    f2ee9d5679 refactor(docs-infra): use CDK clipboard service (#40840)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+37.sha-67d85b3
+ * @license Angular v12.0.0-next.0+39.sha-f2ee9d5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19046,7 +19046,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+37.sha-67d85b3');
+    const VERSION$1 = new Version('12.0.0-next.0+39.sha-f2ee9d5');
 
     /**
      * @license
@@ -46960,7 +46960,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+37.sha-67d85b3');
+    const VERSION$2 = new Version$1('12.0.0-next.0+39.sha-f2ee9d5');
 
     /**
      * @license
