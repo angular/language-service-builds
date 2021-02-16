864701aa43320cf81f4ad081599952067b052032
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:00:01 +0000

    5f0c219883 docs: fix schematics' package name for `ng generate` sub-commands (#40846)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+36.sha-72a00dc
+ * @license Angular v12.0.0-next.0+38.sha-5f0c219
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19046,7 +19046,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+36.sha-72a00dc');
+    const VERSION$1 = new Version('12.0.0-next.0+38.sha-5f0c219');
 
     /**
      * @license
@@ -35079,7 +35079,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+36.sha-72a00dc');
+    const VERSION$2 = new Version$1('12.0.0-next.0+38.sha-5f0c219');
 
     /**
      * @license
