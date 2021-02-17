261143344822d2e5dd8474c550b4de5438e1fe2b
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:22:28 +0000

    322951af49 refactor(compiler-cli): error on cyclic imports in partial compilation (#40782)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+75.sha-0807d13
+ * @license Angular v12.0.0-next.0+73.sha-322951a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19156,7 +19156,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+75.sha-0807d13');
+    const VERSION$1 = new Version('12.0.0-next.0+73.sha-322951a');
 
     /**
      * @license
@@ -35189,7 +35189,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+75.sha-0807d13');
+    const VERSION$2 = new Version$1('12.0.0-next.0+73.sha-322951a');
 
     /**
      * @license
