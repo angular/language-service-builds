ca360765d9e918717677136e0698f9efa947cb80
GitHub <noreply@github.com>
2021-02-17 19:05:45 +0000

    a9936c22ba docs: release notes for the v11.2.1 release (#40888)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+73.sha-322951a
+ * @license Angular v12.0.0-next.0+77.sha-a9936c2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19156,7 +19156,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.0+73.sha-322951a');
+    const VERSION$1 = new Version('12.0.0-next.0+77.sha-a9936c2');
 
     /**
      * @license
@@ -35189,7 +35189,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.0+73.sha-322951a');
+    const VERSION$2 = new Version$1('12.0.0-next.0+77.sha-a9936c2');
 
     /**
      * @license
