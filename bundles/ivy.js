57027fd6478b52a059589b618890306e83675360
Zach Arend <zachzach@google.com>
2021-02-18 01:28:57 +0000

    170692c18c refactor(compiler): add process title to Angular node binaries (#40648)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+3.sha-a970c19
+ * @license Angular v11.2.1+9.sha-170692c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17146,7 +17146,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.1+3.sha-a970c19');
+    const VERSION$1 = new Version('11.2.1+9.sha-170692c');
 
     /**
      * @license
@@ -17803,7 +17803,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.1+3.sha-a970c19'));
+        definitionMap.set('version', literal('11.2.1+9.sha-170692c'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -18024,7 +18024,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.1+3.sha-a970c19'));
+        definitionMap.set('version', literal('11.2.1+9.sha-170692c'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -21296,7 +21296,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.2.1+3.sha-a970c19');
+    const VERSION$2 = new Version('11.2.1+9.sha-170692c');
 
     /**
      * @license
