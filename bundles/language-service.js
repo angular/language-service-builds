1c495c74a9512d5a4b308c3661b469564094af9a
Zach Arend <zachzach@google.com>
2021-02-18 01:29:28 +0000

    542ba1fc00 refactor(compiler): add process title to Angular node binaries (#40648)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+9.sha-65baac6
+ * @license Angular v12.0.0-next.1+10.sha-542ba1f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19182,7 +19182,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.1+9.sha-65baac6');
+    const VERSION$1 = new Version('12.0.0-next.1+10.sha-542ba1f');
 
     /**
      * @license
@@ -47098,7 +47098,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.1+9.sha-65baac6');
+    const VERSION$2 = new Version$1('12.0.0-next.1+10.sha-542ba1f');
 
     /**
      * @license
