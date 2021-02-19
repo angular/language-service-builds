44ff179f5f5e9e9d1b6c2f7963cf1c39aa8d8e7e
atscott <atscott01@gmail.com>
2021-02-19 17:34:58 +0000

    09e1e1935a refactor(docs-infra): convert Sass mixin from camelCase to kebab-case to follow Sass conventions (#40881)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+14.sha-b4301c3
+ * @license Angular v12.0.0-next.1+32.sha-09e1e19
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19182,7 +19182,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.1+14.sha-b4301c3');
+    const VERSION$1 = new Version('12.0.0-next.1+32.sha-09e1e19');
 
     /**
      * @license
@@ -35215,7 +35215,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.1+14.sha-b4301c3');
+    const VERSION$2 = new Version$1('12.0.0-next.1+32.sha-09e1e19');
 
     /**
      * @license
