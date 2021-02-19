3cc38dcd9c2f3d9d2aaa90dc3903fbba8b440906
Andrew Scott <atscott@google.com>
2021-02-19 16:52:57 +0000

    4e84b2f3ac docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+9.sha-170692c
+ * @license Angular v11.2.1+10.sha-4e84b2f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19156,7 +19156,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.1+9.sha-170692c');
+    const VERSION$1 = new Version('11.2.1+10.sha-4e84b2f');
 
     /**
      * @license
@@ -35189,7 +35189,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.1+9.sha-170692c');
+    const VERSION$2 = new Version$1('11.2.1+10.sha-4e84b2f');
 
     /**
      * @license
