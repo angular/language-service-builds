37ac4b242123cfe4cb3f9258768021a1bbfabed5
atscott <atscott01@gmail.com>
2021-02-22 17:00:35 +0000

    53c65f468f test(language-service): update compiler_spec to use the new testing env (#40679)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+35.sha-d1d1dad
+ * @license Angular v12.0.0-next.1+37.sha-53c65f4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19182,7 +19182,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.1+35.sha-d1d1dad');
+    const VERSION$1 = new Version('12.0.0-next.1+37.sha-53c65f4');
 
     /**
      * @license
@@ -47098,7 +47098,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.1+35.sha-d1d1dad');
+    const VERSION$2 = new Version$1('12.0.0-next.1+37.sha-53c65f4');
 
     /**
      * @license
