d0c95376ebac4a1bd76a1c6b4d09a8b85bbf79ac
Andrew Scott <atscott@google.com>
2021-02-22 20:55:22 +0000

    b84f719747 docs: update year (#40925)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+41.sha-dc9fd1a
+ * @license Angular v12.0.0-next.1+42.sha-b84f719
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19182,7 +19182,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.1+41.sha-dc9fd1a');
+    const VERSION$1 = new Version('12.0.0-next.1+42.sha-b84f719');
 
     /**
      * @license
@@ -35231,7 +35231,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.1+41.sha-dc9fd1a');
+    const VERSION$2 = new Version$1('12.0.0-next.1+42.sha-b84f719');
 
     /**
      * @license
