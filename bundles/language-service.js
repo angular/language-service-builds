1a1c0a41e546b57f48d4e3c6b920962c4aed9eb1
atscott <atscott01@gmail.com>
2021-02-23 19:05:50 +0000

    bd6f377db2 docs: improve accessibility of tohpt4 (#40622)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+50.sha-596dfb8
+ * @license Angular v12.0.0-next.1+53.sha-bd6f377
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19182,7 +19182,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.1+50.sha-596dfb8');
+    const VERSION$1 = new Version('12.0.0-next.1+53.sha-bd6f377');
 
     /**
      * @license
@@ -35231,7 +35231,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.1+50.sha-596dfb8');
+    const VERSION$2 = new Version$1('12.0.0-next.1+53.sha-bd6f377');
 
     /**
      * @license
