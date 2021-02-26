84458c57dd86c4dd53852d27ab07e4e9f17178b9
Zach Arend <zachzach@google.com>
2021-02-26 16:51:10 +0000

    89ac771652 fix(docs-infra): add links to glossary for terms on the "Press kit" page (#40945)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+4.sha-55eb7b5
+ * @license Angular v11.2.3+10.sha-89ac771
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19168,7 +19168,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.3+4.sha-55eb7b5');
+    const VERSION$1 = new Version('11.2.3+10.sha-89ac771');
 
     /**
      * @license
@@ -35201,7 +35201,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.3+4.sha-55eb7b5');
+    const VERSION$2 = new Version$1('11.2.3+10.sha-89ac771');
 
     /**
      * @license
