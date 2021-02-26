3188c4f241f1b2022d8118a23da507d570a36482
Zach Arend <zachzach@google.com>
2021-02-26 16:50:11 +0000

    284af7308b fix(ngcc): do not fail hard when a format-path points to a non-existing or empty file (#40985)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+8.sha-6206d99
+ * @license Angular v12.0.0-next.2+10.sha-284af73
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19183,7 +19183,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.2+8.sha-6206d99');
+    const VERSION$1 = new Version('12.0.0-next.2+10.sha-284af73');
 
     /**
      * @license
@@ -47099,7 +47099,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.2+8.sha-6206d99');
+    const VERSION$2 = new Version$1('12.0.0-next.2+10.sha-284af73');
 
     /**
      * @license
