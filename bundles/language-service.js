0d1a1464b7630f0274709081bdb4d56d2bcb3835
Zach Arend <zachzach@google.com>
2021-03-03 18:25:33 +0000

    7765b648e7 docs: update Angular Global Summit on event pages (#41050)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+54.sha-d105994
+ * @license Angular v12.0.0-next.2+55.sha-7765b64
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19180,7 +19180,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.2+54.sha-d105994');
+    const VERSION$1 = new Version('12.0.0-next.2+55.sha-7765b64');
 
     /**
      * @license
@@ -47091,7 +47091,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.2+54.sha-d105994');
+    const VERSION$2 = new Version$1('12.0.0-next.2+55.sha-7765b64');
 
     /**
      * @license
