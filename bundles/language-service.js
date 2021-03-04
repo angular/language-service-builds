e1ac7b74065d4a8f9a85b71f2d6450a05fe8c17b
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:20:54 +0000

    d9acaa8547 docs: remove Codelyzer recommendation from style guide (#40992)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+3.sha-ad40fca
+ * @license Angular v12.0.0-next.3+4.sha-d9acaa8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19181,7 +19181,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.3+3.sha-ad40fca');
+    const VERSION$1 = new Version('12.0.0-next.3+4.sha-d9acaa8');
 
     /**
      * @license
@@ -47092,7 +47092,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.3+3.sha-ad40fca');
+    const VERSION$2 = new Version$1('12.0.0-next.3+4.sha-d9acaa8');
 
     /**
      * @license
