b1661e3fa9e19fe9dcbd27cd8f2e96d82b3c82b0
Andrew Kushnir <akushnir@google.com>
2021-03-04 23:09:35 +0000

    45216ccc0d fix(language-service): Only provide dom completions for inline templates (#41078)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+13.sha-38524c4
+ * @license Angular v12.0.0-next.3+14.sha-45216cc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19164,7 +19164,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.3+13.sha-38524c4');
+    const VERSION$1 = new Version('12.0.0-next.3+14.sha-45216cc');
 
     /**
      * @license
@@ -35186,7 +35186,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.3+13.sha-38524c4');
+    const VERSION$2 = new Version$1('12.0.0-next.3+14.sha-45216cc');
 
     /**
      * @license
