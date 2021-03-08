57cb0ffd0740ec62eadb2538981bf17421db1f9f
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:51:18 +0000

    ed342f4e53 test(dev-infra): fix test order failure in tests for the release command (#41023)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+26.sha-925746b
+ * @license Angular v11.2.4+27.sha-ed342f4
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19149,7 +19149,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.4+26.sha-925746b');
+    const VERSION$1 = new Version('11.2.4+27.sha-ed342f4');
 
     /**
      * @license
@@ -35181,7 +35181,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.4+26.sha-925746b');
+    const VERSION$2 = new Version$1('11.2.4+27.sha-ed342f4');
 
     /**
      * @license
