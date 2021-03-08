8e105ee265ee8ec34c4ba7814d65782344a9af98
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:13:36 +0000

    7854c4ddbe test(docs-infra): run a11y tests against `/tutorial` and update a11y scores (#41103)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+25.sha-681c341
+ * @license Angular v12.0.0-next.3+23.sha-7854c4d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19164,7 +19164,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.3+25.sha-681c341');
+    const VERSION$1 = new Version('12.0.0-next.3+23.sha-7854c4d');
 
     /**
      * @license
@@ -47034,7 +47034,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.3+25.sha-681c341');
+    const VERSION$2 = new Version$1('12.0.0-next.3+23.sha-7854c4d');
 
     /**
      * @license
