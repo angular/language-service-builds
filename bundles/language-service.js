6b400b7978980c34c4107d871d2abe66b57b3090
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:50:33 +0000

    c4ea0ab4e0 fix(docs-infra): print info to help debugging SW cache issue (#41106) (#41139)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+51.sha-16f90ca
+ * @license Angular v11.2.4+52.sha-c4ea0ab
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19137,7 +19137,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.4+51.sha-16f90ca');
+    const VERSION$1 = new Version('11.2.4+52.sha-c4ea0ab');
 
     /**
      * @license
@@ -35162,7 +35162,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.4+51.sha-16f90ca');
+    const VERSION$2 = new Version$1('11.2.4+52.sha-c4ea0ab');
 
     /**
      * @license
