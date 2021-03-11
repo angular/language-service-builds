3a12aade664b2b091eac1da9b595fede42050b84
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:11:33 +0000

    94e8f5d4a4 docs: add stepan suvorov to GDE resources (#41178)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+8.sha-f2ca29c
+ * @license Angular v12.0.0-next.4+9.sha-94e8f5d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19153,7 +19153,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.4+8.sha-f2ca29c');
+    const VERSION$1 = new Version('12.0.0-next.4+9.sha-94e8f5d');
 
     /**
      * @license
@@ -46998,7 +46998,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.4+8.sha-f2ca29c');
+    const VERSION$2 = new Version$1('12.0.0-next.4+9.sha-94e8f5d');
 
     /**
      * @license
