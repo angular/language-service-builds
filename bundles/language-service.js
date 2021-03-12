a4e750a64c3fff5874e19ffd0d952e4b63c4bc59
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 21:46:12 +0000

    cfaf425314 docs: add book about testing to resources (#41159)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+12.sha-7096246
+ * @license Angular v12.0.0-next.4+19.sha-cfaf425
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19152,7 +19152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.4+12.sha-7096246');
+    const VERSION$1 = new Version('12.0.0-next.4+19.sha-cfaf425');
 
     /**
      * @license
@@ -46996,7 +46996,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.4+12.sha-7096246');
+    const VERSION$2 = new Version$1('12.0.0-next.4+19.sha-cfaf425');
 
     /**
      * @license
