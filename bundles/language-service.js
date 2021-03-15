798cd13373eea4b9b8bad430bb0204f001b6db6b
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:37:14 +0000

    3bd1992218 perf(forms): avoid direct references to the `Validators` class (#41189)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+24.sha-1644d64
+ * @license Angular v12.0.0-next.4+25.sha-3bd1992
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19152,7 +19152,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.4+24.sha-1644d64');
+    const VERSION$1 = new Version('12.0.0-next.4+25.sha-3bd1992');
 
     /**
      * @license
@@ -35159,7 +35159,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.4+24.sha-1644d64');
+    const VERSION$2 = new Version$1('12.0.0-next.4+25.sha-3bd1992');
 
     /**
      * @license
