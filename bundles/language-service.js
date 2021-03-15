dd4db1326d2ffad6bb611692015b73d5631763e2
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:52:44 +0000

    b4484125d6 docs: document `help wanted` and `good first issue` labels (#41195)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19137,7 +19137,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.5+18.sha-5908eda');
+    const VERSION$1 = new Version('11.2.5+19.sha-b448412');
 
     /**
      * @license
@@ -35159,7 +35159,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.5+18.sha-5908eda');
+    const VERSION$2 = new Version$1('11.2.5+19.sha-b448412');
 
     /**
      * @license
