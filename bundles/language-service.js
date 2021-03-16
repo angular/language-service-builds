c0fb233c351a54129bf43cb53eb9d090df810b55
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:56:21 +0000

    6414590f8d perf(forms): make `RadioControlRegistry` class tree-shakable (#41126)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+22.sha-bec919a
+ * @license Angular v11.2.5+25.sha-6414590
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19137,7 +19137,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.5+22.sha-bec919a');
+    const VERSION$1 = new Version('11.2.5+25.sha-6414590');
 
     /**
      * @license
@@ -35159,7 +35159,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.5+22.sha-bec919a');
+    const VERSION$2 = new Version$1('11.2.5+25.sha-6414590');
 
     /**
      * @license
