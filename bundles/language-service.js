c572b6fdd1b9529a7647cf4309705d42ec7e5ca3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 22:07:46 +0000

    18cd7bbd9b docs: remove unused attribute (#41211)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+27.sha-ca9af40
+ * @license Angular v11.2.5+28.sha-18cd7bb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19137,7 +19137,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.5+27.sha-ca9af40');
+    const VERSION$1 = new Version('11.2.5+28.sha-18cd7bb');
 
     /**
      * @license
@@ -35159,7 +35159,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.5+27.sha-ca9af40');
+    const VERSION$2 = new Version$1('11.2.5+28.sha-18cd7bb');
 
     /**
      * @license
