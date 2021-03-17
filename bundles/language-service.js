f73383818b3eeae73ea45b1a2e9ff13e31dd18d3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:28:37 +0000

    98de4cb1bf docs: add deprecation note next to IE 11 in browser-support page (#41147)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+44.sha-0f0f34f
+ * @license Angular v12.0.0-next.4+47.sha-98de4cb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19202,7 +19202,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.4+44.sha-0f0f34f');
+    const VERSION$1 = new Version('12.0.0-next.4+47.sha-98de4cb');
 
     /**
      * @license
@@ -35209,7 +35209,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.4+44.sha-0f0f34f');
+    const VERSION$2 = new Version$1('12.0.0-next.4+47.sha-98de4cb');
 
     /**
      * @license
