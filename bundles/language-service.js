c53aefb5a33422b0cd150d31fffce60868d889c2
Misko Hevery <misko@hevery.com>
2021-03-18 17:36:47 +0000

    ac9a250fc4 docs: update link to schematics schema (#41258)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+6.sha-ac9c428
+ * @license Angular v12.0.0-next.5+7.sha-ac9a250
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19203,7 +19203,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.5+6.sha-ac9c428');
+    const VERSION$1 = new Version('12.0.0-next.5+7.sha-ac9a250');
 
     /**
      * @license
@@ -47049,7 +47049,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.5+6.sha-ac9c428');
+    const VERSION$2 = new Version$1('12.0.0-next.5+7.sha-ac9a250');
 
     /**
      * @license
