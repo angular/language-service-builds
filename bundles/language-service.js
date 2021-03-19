1d813f4d4d60e83af4bcfdb9cf4e515859b6c6b7
Misko Hevery <misko@hevery.com>
2021-03-19 20:23:02 +0000

    5eb7f3491f fix(dev-infra): remove commit message wizard and builder (#41280)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+12.sha-aa039a1
+ * @license Angular v12.0.0-next.5+15.sha-5eb7f34
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19201,7 +19201,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.5+12.sha-aa039a1');
+    const VERSION$1 = new Version('12.0.0-next.5+15.sha-5eb7f34');
 
     /**
      * @license
@@ -47047,7 +47047,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.5+12.sha-aa039a1');
+    const VERSION$2 = new Version$1('12.0.0-next.5+15.sha-5eb7f34');
 
     /**
      * @license
