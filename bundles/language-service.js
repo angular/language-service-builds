d1198be13d8a3f132702155fd23be23fdb41965e
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:50 +0000

    ed5bfe8173 docs: simplify deleteHero method in tutorial (#41303)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+18.sha-5bc0a47
+ * @license Angular v12.0.0-next.5+16.sha-ed5bfe8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19201,7 +19201,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.5+18.sha-5bc0a47');
+    const VERSION$1 = new Version('12.0.0-next.5+16.sha-ed5bfe8');
 
     /**
      * @license
@@ -47047,7 +47047,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.5+18.sha-5bc0a47');
+    const VERSION$2 = new Version$1('12.0.0-next.5+16.sha-ed5bfe8');
 
     /**
      * @license
