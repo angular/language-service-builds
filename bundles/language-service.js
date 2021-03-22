4ccd232755d41b5d2571dba1acaef868d5f86f0f
Misko Hevery <misko@hevery.com>
2021-03-22 20:05:37 +0000

    b5551609fe fix(router): fragment can be null (#37336)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+23.sha-e7b1d43
+ * @license Angular v12.0.0-next.5+24.sha-b555160
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19199,7 +19199,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.5+23.sha-e7b1d43');
+    const VERSION$1 = new Version('12.0.0-next.5+24.sha-b555160');
 
     /**
      * @license
@@ -35206,7 +35206,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.5+23.sha-e7b1d43');
+    const VERSION$2 = new Version$1('12.0.0-next.5+24.sha-b555160');
 
     /**
      * @license
