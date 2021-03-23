fc60e4aa48bec5e3960b17970b8375c8bc432b86
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:13:18 +0000

    1eba57eb00 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+29.sha-80f11a9
+ * @license Angular v12.0.0-next.5+30.sha-1eba57e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19199,7 +19199,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.5+29.sha-80f11a9');
+    const VERSION$1 = new Version('12.0.0-next.5+30.sha-1eba57e');
 
     /**
      * @license
@@ -47045,7 +47045,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.5+29.sha-80f11a9');
+    const VERSION$2 = new Version$1('12.0.0-next.5+30.sha-1eba57e');
 
     /**
      * @license
