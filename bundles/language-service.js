cd358794ee359c5dc3d4e8fef7d189c438f46ee5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-24 20:55:47 +0000

    dd82c8e9f4 docs(docs-infra): add Zach Arend to contributors.json (#41325)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+41.sha-9da7ea9
+ * @license Angular v12.0.0-next.6+1.sha-dd82c8e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19199,7 +19199,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.5+41.sha-9da7ea9');
+    const VERSION$1 = new Version('12.0.0-next.6+1.sha-dd82c8e');
 
     /**
      * @license
@@ -35213,7 +35213,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.5+41.sha-9da7ea9');
+    const VERSION$2 = new Version$1('12.0.0-next.6+1.sha-dd82c8e');
 
     /**
      * @license
