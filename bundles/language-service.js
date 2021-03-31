3e1c7c359c85fa55299b6973d8d732344b4b8ecf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:26:03 +0000

    e112e320bf fix(compiler): handle case-sensitive CSS custom properties (#41380)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+25.sha-a371646
+ * @license Angular v12.0.0-next.6+23.sha-e112e32
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19244,7 +19244,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.6+25.sha-a371646');
+    const VERSION$1 = new Version('12.0.0-next.6+23.sha-e112e32');
 
     /**
      * @license
@@ -35260,7 +35260,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.6+25.sha-a371646');
+    const VERSION$2 = new Version$1('12.0.0-next.6+23.sha-e112e32');
 
     /**
      * @license
