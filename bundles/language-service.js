c4ab53b0f6eff72703be1c550e0766ee44a396af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 22:05:44 +0000

    154129b9de release: cut the v11.2.8 release (#41419)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+32.sha-4bac297
+ * @license Angular v11.2.7+33.sha-154129b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19180,7 +19180,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.7+32.sha-4bac297');
+    const VERSION$1 = new Version('11.2.7+33.sha-154129b');
 
     /**
      * @license
@@ -35202,7 +35202,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.7+32.sha-4bac297');
+    const VERSION$2 = new Version$1('11.2.7+33.sha-154129b');
 
     /**
      * @license
