656ef7bc7ba98f54b0c6d35d8aeb1c0ac4da3ce1
atscott <atscott01@gmail.com>
2021-04-02 18:11:54 +0000

    d4f739fb70 docs: Including Visual Studio (#41414)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+2.sha-dc8a081
+ * @license Angular v11.2.8+3.sha-d4f739f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -20399,7 +20399,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.8+2.sha-dc8a081');
+    const VERSION$1 = new Version('11.2.8+3.sha-d4f739f');
 
     /**
      * @license
@@ -21056,7 +21056,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.8+2.sha-dc8a081'));
+        definitionMap.set('version', literal('11.2.8+3.sha-d4f739f'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21277,7 +21277,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.8+2.sha-dc8a081'));
+        definitionMap.set('version', literal('11.2.8+3.sha-d4f739f'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -21309,7 +21309,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.2.8+2.sha-dc8a081');
+    const VERSION$2 = new Version('11.2.8+3.sha-d4f739f');
 
     /**
      * @license
