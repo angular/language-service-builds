1e125ad0ba049f26775bd4fbfe4cf9e56b3262ac
atscott <atscott01@gmail.com>
2021-04-05 15:37:50 +0000

    fcd190aa9c docs: fix grammar mistake in `glossary.md` ("can exported" --> "can be exported") (#41440)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+9.sha-1de04b1
+ * @license Angular v12.0.0-next.7+10.sha-fcd190a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19246,7 +19246,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.7+9.sha-1de04b1');
+    const VERSION$1 = new Version('12.0.0-next.7+10.sha-fcd190a');
 
     /**
      * @license
@@ -35292,7 +35292,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.7+9.sha-1de04b1');
+    const VERSION$2 = new Version$1('12.0.0-next.7+10.sha-fcd190a');
 
     /**
      * @license
