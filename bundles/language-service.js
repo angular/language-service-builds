3d49c6a3d8fedf6737b248d83710f686930b7193
atscott <atscott01@gmail.com>
2021-04-07 19:03:40 +0000

    4b8bbe2cf5 docs: cherry-pick the v11.2.9 release changelog

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+41.sha-eda27c9
+ * @license Angular v12.0.0-next.7+40.sha-4b8bbe2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19246,7 +19246,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.7+41.sha-eda27c9');
+    const VERSION$1 = new Version('12.0.0-next.7+40.sha-4b8bbe2');
 
     /**
      * @license
@@ -47154,7 +47154,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.7+41.sha-eda27c9');
+    const VERSION$2 = new Version$1('12.0.0-next.7+40.sha-4b8bbe2');
 
     /**
      * @license
