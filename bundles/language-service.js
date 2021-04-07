74180f6e35e76fd42116b30008b063349a7e8297
atscott <atscott01@gmail.com>
2021-04-07 19:10:26 +0000

    5a81490554 docs: use the right tag in the HeroesComponent's template snippet (#41473)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+40.sha-4b8bbe2
+ * @license Angular v12.0.0-next.8+1.sha-5a81490
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19246,7 +19246,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.7+40.sha-4b8bbe2');
+    const VERSION$1 = new Version('12.0.0-next.8+1.sha-5a81490');
 
     /**
      * @license
@@ -35292,7 +35292,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.7+40.sha-4b8bbe2');
+    const VERSION$2 = new Version$1('12.0.0-next.8+1.sha-5a81490');
 
     /**
      * @license
