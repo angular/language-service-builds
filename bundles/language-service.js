383dd30c81cac5b6a8f5cefb89347c6aa5d7747e
atscott <atscott01@gmail.com>
2021-04-07 16:58:12 +0000

    dd66da1864 release: bump angular-in-web-memory-api package to 0.12.0 (#41487)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+33.sha-4a68e13
+ * @license Angular v12.0.0-next.7+34.sha-dd66da1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19246,7 +19246,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.7+33.sha-4a68e13');
+    const VERSION$1 = new Version('12.0.0-next.7+34.sha-dd66da1');
 
     /**
      * @license
@@ -47154,7 +47154,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.7+33.sha-4a68e13');
+    const VERSION$2 = new Version$1('12.0.0-next.7+34.sha-dd66da1');
 
     /**
      * @license
