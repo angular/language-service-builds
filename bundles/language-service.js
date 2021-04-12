9dd092640d46170b28c138f3e63d55c26fb3636f
Zach Arend <zachzach@google.com>
2021-04-12 17:56:33 +0000

    42e3a5241d test(compiler-cli): add integration test for relative rootDir (#41359)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+32.sha-aa0e54f
+ * @license Angular v12.0.0-next.8+35.sha-42e3a52
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19377,7 +19377,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+32.sha-aa0e54f');
+    const VERSION$1 = new Version('12.0.0-next.8+35.sha-42e3a52');
 
     /**
      * @license
@@ -47283,7 +47283,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+32.sha-aa0e54f');
+    const VERSION$2 = new Version$1('12.0.0-next.8+35.sha-42e3a52');
 
     /**
      * @license
