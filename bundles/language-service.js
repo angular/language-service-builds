1a6472316e1b05e28307857a6e5872f3e3e26a31
Zach Arend <zachzach@google.com>
2021-04-12 17:42:12 +0000

    ebc80b3b5c build: add breakingChangesLabel to merge configuration (#41546)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+29.sha-4810f5c
+ * @license Angular v12.0.0-next.8+31.sha-ebc80b3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19377,7 +19377,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+29.sha-4810f5c');
+    const VERSION$1 = new Version('12.0.0-next.8+31.sha-ebc80b3');
 
     /**
      * @license
@@ -47283,7 +47283,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+29.sha-4810f5c');
+    const VERSION$2 = new Version$1('12.0.0-next.8+31.sha-ebc80b3');
 
     /**
      * @license
