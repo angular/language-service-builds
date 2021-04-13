a6c8a4eaca06d8e7e07da50a1e78c8a06683284d
Zach Arend <zachzach@google.com>
2021-04-13 20:23:00 +0000

    71b8c9ab29 refactor(bazel): remove old Angular CLI schematics and builder (#41575)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+64.sha-c7f9516
+ * @license Angular v12.0.0-next.8+65.sha-71b8c9a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19394,7 +19394,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+64.sha-c7f9516');
+    const VERSION$1 = new Version('12.0.0-next.8+65.sha-71b8c9a');
 
     /**
      * @license
@@ -35452,7 +35452,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+64.sha-c7f9516');
+    const VERSION$2 = new Version$1('12.0.0-next.8+65.sha-71b8c9a');
 
     /**
      * @license
