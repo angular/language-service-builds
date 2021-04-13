26f2ce9402663dbb7b6c0a12741d359283a26723
Zach Arend <zachzach@google.com>
2021-04-13 20:29:04 +0000

    78236bfdca fix(language-service): use script versions for incremental compilations (#41475)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+65.sha-71b8c9a
+ * @license Angular v12.0.0-next.8+69.sha-78236bf
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19394,7 +19394,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+65.sha-71b8c9a');
+    const VERSION$1 = new Version('12.0.0-next.8+69.sha-78236bf');
 
     /**
      * @license
@@ -35452,7 +35452,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+65.sha-71b8c9a');
+    const VERSION$2 = new Version$1('12.0.0-next.8+69.sha-78236bf');
 
     /**
      * @license
