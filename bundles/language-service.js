1a9a82967fdea12607d8f76de9a6fb5ff62fda7a
Zach Arend <zachzach@google.com>
2021-04-13 04:28:48 +0000

    bfbdb8f84d refactor(compiler-cli): cleanup redundant storage of reuse `ts.Program` (#41289)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+49.sha-319da89
+ * @license Angular v12.0.0-next.8+52.sha-bfbdb8f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19378,7 +19378,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+49.sha-319da89');
+    const VERSION$1 = new Version('12.0.0-next.8+52.sha-bfbdb8f');
 
     /**
      * @license
@@ -47284,7 +47284,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+49.sha-319da89');
+    const VERSION$2 = new Version$1('12.0.0-next.8+52.sha-bfbdb8f');
 
     /**
      * @license
