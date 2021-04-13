1961329302921318e5e95ffed80715a16cc2d85a
Zach Arend <zachzach@google.com>
2021-04-13 21:14:32 +0000

    ba84fa6f42 refactor(compiler-cli): remove i18n options from `LinkerOptions` (#41554)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+69.sha-78236bf
+ * @license Angular v12.0.0-next.8+71.sha-ba84fa6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19394,7 +19394,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+69.sha-78236bf');
+    const VERSION$1 = new Version('12.0.0-next.8+71.sha-ba84fa6');
 
     /**
      * @license
@@ -47310,7 +47310,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+69.sha-78236bf');
+    const VERSION$2 = new Version$1('12.0.0-next.8+71.sha-ba84fa6');
 
     /**
      * @license
