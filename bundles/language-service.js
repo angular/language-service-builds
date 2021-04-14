358b44e24fbeb0acc67592028085af52262d78c4
Zach Arend <zachzach@google.com>
2021-04-14 23:21:29 +0000

    4329ccae2c docs: add rx-angular to resources.json (#39291)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+106.sha-dde81ba
+ * @license Angular v12.0.0-next.8+108.sha-4329cca
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19392,7 +19392,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+106.sha-dde81ba');
+    const VERSION$1 = new Version('12.0.0-next.8+108.sha-4329cca');
 
     /**
      * @license
@@ -35450,7 +35450,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+106.sha-dde81ba');
+    const VERSION$2 = new Version$1('12.0.0-next.8+108.sha-4329cca');
 
     /**
      * @license
