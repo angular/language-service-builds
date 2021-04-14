49e38460fb183a22a2c7d522e253e33b3693b46e
Zach Arend <zachzach@google.com>
2021-04-14 17:11:57 +0000

    beafa22de9 feat(upgrade): update supported range of node versions (#41544)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+78.sha-0bc539a
+ * @license Angular v12.0.0-next.8+95.sha-beafa22
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19394,7 +19394,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+78.sha-0bc539a');
+    const VERSION$1 = new Version('12.0.0-next.8+95.sha-beafa22');
 
     /**
      * @license
@@ -35452,7 +35452,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+78.sha-0bc539a');
+    const VERSION$2 = new Version$1('12.0.0-next.8+95.sha-beafa22');
 
     /**
      * @license
