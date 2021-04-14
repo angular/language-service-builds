d2a5240dfa96b37d31e88a33447b42d66da8cdcd
Zach Arend <zachzach@google.com>
2021-04-14 23:15:03 +0000

    a1b2718b92 fix(router): recursively merge empty path matches (#41584)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+102.sha-1043aa4
+ * @license Angular v12.0.0-next.8+105.sha-a1b2718
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19394,7 +19394,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+102.sha-1043aa4');
+    const VERSION$1 = new Version('12.0.0-next.8+105.sha-a1b2718');
 
     /**
      * @license
@@ -47358,7 +47358,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+102.sha-1043aa4');
+    const VERSION$2 = new Version$1('12.0.0-next.8+105.sha-a1b2718');
 
     /**
      * @license
