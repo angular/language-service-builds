d561daadd42f1febc882e7bd6e9d2e99646deb9f
Zach Arend <zachzach@google.com>
2021-04-14 23:17:03 +0000

    1b43158af6 fix(compiler-cli): do not error with prepocessing if component has no inline styles (#41602)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+105.sha-a1b2718
+ * @license Angular v12.0.0-next.8+104.sha-1b43158
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19394,7 +19394,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+105.sha-a1b2718');
+    const VERSION$1 = new Version('12.0.0-next.8+104.sha-1b43158');
 
     /**
      * @license
@@ -35452,7 +35452,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+105.sha-a1b2718');
+    const VERSION$2 = new Version$1('12.0.0-next.8+104.sha-1b43158');
 
     /**
      * @license
