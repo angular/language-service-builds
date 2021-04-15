5399084ab1adbdad94713721a90175e5f67d0372
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:43:31 +0000

    3aa92355ef fix(common): viewport scroller not finding elements inside the shadow DOM (#41644)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+6.sha-f4376fc
+ * @license Angular v11.2.10+7.sha-3aa9235
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -20399,7 +20399,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.10+6.sha-f4376fc');
+    const VERSION$1 = new Version('11.2.10+7.sha-3aa9235');
 
     /**
      * @license
@@ -21056,7 +21056,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.10+6.sha-f4376fc'));
+        definitionMap.set('version', literal('11.2.10+7.sha-3aa9235'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21277,7 +21277,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.10+6.sha-f4376fc'));
+        definitionMap.set('version', literal('11.2.10+7.sha-3aa9235'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -21309,7 +21309,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.2.10+6.sha-f4376fc');
+    const VERSION$2 = new Version('11.2.10+7.sha-3aa9235');
 
     /**
      * @license
