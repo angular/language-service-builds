01337575d999925a69ef6b3f6e123e08bbd037ae
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:22:50 +0000

    6a153a2a6f build: update diff to version 5.0.0 (#41684)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+147.sha-1391323
+ * @license Angular v12.0.0-next.8+149.sha-6a153a2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19381,7 +19381,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+147.sha-1391323');
+    const VERSION$1 = new Version('12.0.0-next.8+149.sha-6a153a2');
 
     /**
      * @license
@@ -35439,7 +35439,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+147.sha-1391323');
+    const VERSION$2 = new Version$1('12.0.0-next.8+149.sha-6a153a2');
 
     /**
      * @license
