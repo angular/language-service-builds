7b056feb89cfb0728a61f76070a89ab905647278
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:04:16 +0000

    364ff96f28 build(docs-infra): upgrade cli command docs sources to 79d820c92 (#41719)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+184.sha-c2011c0
+ * @license Angular v12.0.0-next.8+185.sha-364ff96
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19381,7 +19381,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+184.sha-c2011c0');
+    const VERSION$1 = new Version('12.0.0-next.8+185.sha-364ff96');
 
     /**
      * @license
@@ -35439,7 +35439,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+184.sha-c2011c0');
+    const VERSION$2 = new Version$1('12.0.0-next.8+185.sha-364ff96');
 
     /**
      * @license
