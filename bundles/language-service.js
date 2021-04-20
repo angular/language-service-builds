3bdfe70fec8acfda2369be7531c5bdb75730177d
Andrew Kushnir <akushnir@google.com>
2021-04-20 18:59:31 +0000

    20e09177b7 docs: add margin to button boilerplate CSS for examples (#41413)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+38.sha-7459f6d
+ * @license Angular v11.2.10+39.sha-20e0917
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19180,7 +19180,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.10+38.sha-7459f6d');
+    const VERSION$1 = new Version('11.2.10+39.sha-20e0917');
 
     /**
      * @license
@@ -35242,7 +35242,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.10+38.sha-7459f6d');
+    const VERSION$2 = new Version$1('11.2.10+39.sha-20e0917');
 
     /**
      * @license
