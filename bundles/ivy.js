f2db8c3db6d633b4cec3bac946e1e67a8cd2d20f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:27:36 +0000

    1d1fa1a8f3 test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+42.sha-03d2fc2
+ * @license Angular v11.2.11+2.sha-1d1fa1a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -20399,7 +20399,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.10+42.sha-03d2fc2');
+    const VERSION$1 = new Version('11.2.11+2.sha-1d1fa1a');
 
     /**
      * @license
@@ -21056,7 +21056,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.10+42.sha-03d2fc2'));
+        definitionMap.set('version', literal('11.2.11+2.sha-1d1fa1a'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -21277,7 +21277,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      */
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.10+42.sha-03d2fc2'));
+        definitionMap.set('version', literal('11.2.11+2.sha-1d1fa1a'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -21309,7 +21309,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('11.2.10+42.sha-03d2fc2');
+    const VERSION$2 = new Version('11.2.11+2.sha-1d1fa1a');
 
     /**
      * @license
