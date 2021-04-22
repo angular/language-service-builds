7ec6cfdd9cf547f1c162d68be8822078bb1aa36e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:51:00 +0000

    a4a55f0687 fix(dev-infra): verify python3 is available before release (#41753)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+195.sha-8bfd9e8
+ * @license Angular v12.0.0-next.8+196.sha-a4a55f0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19381,7 +19381,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+195.sha-8bfd9e8');
+    const VERSION$1 = new Version('12.0.0-next.8+196.sha-a4a55f0');
 
     /**
      * @license
@@ -35439,7 +35439,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+195.sha-8bfd9e8');
+    const VERSION$2 = new Version$1('12.0.0-next.8+196.sha-a4a55f0');
 
     /**
      * @license
