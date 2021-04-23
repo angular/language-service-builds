25a1b3f95b73f3bf88a05edd6249795e81df29ee
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 17:04:27 +0000

    87dc8511cc fix(upgrade): preserve $interval.flush when ngMocks is being used (#30229)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+16.sha-5895ddb
+ * @license Angular v12.0.0-rc.0+18.sha-87dc851
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19381,7 +19381,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.0+16.sha-5895ddb');
+    const VERSION$1 = new Version('12.0.0-rc.0+18.sha-87dc851');
 
     /**
      * @license
@@ -35439,7 +35439,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.0+16.sha-5895ddb');
+    const VERSION$2 = new Version$1('12.0.0-rc.0+18.sha-87dc851');
 
     /**
      * @license
