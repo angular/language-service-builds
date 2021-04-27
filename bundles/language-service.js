ccdca88c4f13d2f5f3e7706590df0570bd70bd11
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 21:23:12 +0000

    3b7d4ebbd6 fix(docs-infra): do not redirect disambiguated URLs (#41842)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+259.sha-d580b27
+ * @license Angular v12.0.0-next.8+265.sha-3b7d4eb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+259.sha-d580b27');
+    const VERSION$1 = new Version('12.0.0-next.8+265.sha-3b7d4eb');
 
     /**
      * @license
@@ -35508,7 +35508,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+259.sha-d580b27');
+    const VERSION$2 = new Version$1('12.0.0-next.8+265.sha-3b7d4eb');
 
     /**
      * @license
