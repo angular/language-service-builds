5ff7a225cd8976f77e13683c4114955dfad7b04c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:36:18 +0000

    3783976543 docs: remove the section "Using components vs services from other modules" (#41835)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+253.sha-f978976
+ * @license Angular v12.0.0-next.8+255.sha-3783976
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+253.sha-f978976');
+    const VERSION$1 = new Version('12.0.0-next.8+255.sha-3783976');
 
     /**
      * @license
@@ -35508,7 +35508,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+253.sha-f978976');
+    const VERSION$2 = new Version$1('12.0.0-next.8+255.sha-3783976');
 
     /**
      * @license
