c76a2417ceff1d5ffe7dcfcf2b7b26a2822bff9f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:42:14 +0000

    1aed2f3234 fix(dev-infra): replace `chalk` require with namespace import (#41847)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+265.sha-3b7d4eb
+ * @license Angular v12.0.0-next.8+268.sha-1aed2f3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+265.sha-3b7d4eb');
+    const VERSION$1 = new Version('12.0.0-next.8+268.sha-1aed2f3');
 
     /**
      * @license
@@ -47450,7 +47450,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+265.sha-3b7d4eb');
+    const VERSION$2 = new Version$1('12.0.0-next.8+268.sha-1aed2f3');
 
     /**
      * @license
