1616e68438890d765b8277be4f567a5c2abd9dc3
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:59:38 +0000

    336fe9124e Revert "ci: re-enable the `@angular/components` unit tests (#41816)" (#41863)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+36.sha-7219d1b
+ * @license Angular v11.2.11+38.sha-336fe91
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19208,7 +19208,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.11+36.sha-7219d1b');
+    const VERSION$1 = new Version('11.2.11+38.sha-336fe91');
 
     /**
      * @license
@@ -35270,7 +35270,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.11+36.sha-7219d1b');
+    const VERSION$2 = new Version$1('11.2.11+38.sha-336fe91');
 
     /**
      * @license
