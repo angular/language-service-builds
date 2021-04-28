48ad9ff1dd96349d4df242cb6c25579d263642f4
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:55:01 +0000

    c31d6ea24e docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+279.sha-3ccad85
+ * @license Angular v12.0.0-next.8+281.sha-c31d6ea
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+279.sha-3ccad85');
+    const VERSION$1 = new Version('12.0.0-next.8+281.sha-c31d6ea');
 
     /**
      * @license
@@ -47450,7 +47450,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+279.sha-3ccad85');
+    const VERSION$2 = new Version$1('12.0.0-next.8+281.sha-c31d6ea');
 
     /**
      * @license
