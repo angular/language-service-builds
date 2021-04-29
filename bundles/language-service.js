bf740e65081ca1cdfcece6c081bed2edb80a56a6
Misko Hevery <misko@hevery.com>
2021-04-29 21:17:10 +0000

    5bae16da48 docs(docs-infra): fix handling of client-side errors in networking (#36608)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+7.sha-6ba5921
+ * @license Angular v11.2.12+9.sha-5bae16d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19208,7 +19208,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.12+7.sha-6ba5921');
+    const VERSION$1 = new Version('11.2.12+9.sha-5bae16d');
 
     /**
      * @license
@@ -35270,7 +35270,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.12+7.sha-6ba5921');
+    const VERSION$2 = new Version$1('11.2.12+9.sha-5bae16d');
 
     /**
      * @license
