bb71a32a9a4626a4d2258d2e5f8db5cec43682f4
Misko Hevery <misko@hevery.com>
2021-04-30 21:47:24 +0000

    9b4b281c52 fix(core): handle multiple i18n attributes with expression bindings (#41882)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+298.sha-35450c7
+ * @license Angular v12.0.0-next.8+301.sha-9b4b281
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+298.sha-35450c7');
+    const VERSION$1 = new Version('12.0.0-next.8+301.sha-9b4b281');
 
     /**
      * @license
@@ -35467,7 +35467,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+298.sha-35450c7');
+    const VERSION$2 = new Version$1('12.0.0-next.8+301.sha-9b4b281');
 
     /**
      * @license
