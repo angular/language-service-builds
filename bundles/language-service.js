a058d80782a308694f7bb6878a2f80c703283483
Misko Hevery <misko@hevery.com>
2021-04-30 20:48:53 +0000

    be9b19e820 Revert "build: update dependency karma to v6 (#41696)" (#41904)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+20.sha-0255a2c
+ * @license Angular v12.0.0-rc.1+21.sha-be9b19e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.1+20.sha-0255a2c');
+    const VERSION$1 = new Version('12.0.0-rc.1+21.sha-be9b19e');
 
     /**
      * @license
@@ -35508,7 +35508,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.1+20.sha-0255a2c');
+    const VERSION$2 = new Version$1('12.0.0-rc.1+21.sha-be9b19e');
 
     /**
      * @license
