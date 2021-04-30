351ff9f62ae8df678a377f86039868677eee7c45
Misko Hevery <misko@hevery.com>
2021-04-30 21:55:07 +0000

    7e9ef4d257 docs: Update link to Testing Angular online book (#41906)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+9.sha-5bae16d
+ * @license Angular v11.2.12+12.sha-7e9ef4d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19208,7 +19208,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.12+9.sha-5bae16d');
+    const VERSION$1 = new Version('11.2.12+12.sha-7e9ef4d');
 
     /**
      * @license
@@ -35270,7 +35270,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.12+9.sha-5bae16d');
+    const VERSION$2 = new Version$1('11.2.12+12.sha-7e9ef4d');
 
     /**
      * @license
