029be597338f841a0d077f0ec05e39e370fc5e73
Misko Hevery <misko@hevery.com>
2021-05-03 21:49:34 +0000

    3777cf55aa Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+313.sha-3b58903
+ * @license Angular v12.0.0-next.8+316.sha-3777cf5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+313.sha-3b58903');
+    const VERSION$1 = new Version('12.0.0-next.8+316.sha-3777cf5');
 
     /**
      * @license
@@ -47395,7 +47395,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+313.sha-3b58903');
+    const VERSION$2 = new Version$1('12.0.0-next.8+316.sha-3777cf5');
 
     /**
      * @license
