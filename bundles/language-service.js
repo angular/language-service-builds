bd15ea27fecf4b1ee82d484af25ed5ba16cd39a8
Misko Hevery <misko@hevery.com>
2021-05-03 21:43:31 +0000

    253ed5d484 docs: improve accessibility of form-validation example (#41283)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+311.sha-6967f3c
+ * @license Angular v12.0.0-next.8+312.sha-253ed5d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+311.sha-6967f3c');
+    const VERSION$1 = new Version('12.0.0-next.8+312.sha-253ed5d');
 
     /**
      * @license
@@ -35467,7 +35467,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+311.sha-6967f3c');
+    const VERSION$2 = new Version$1('12.0.0-next.8+312.sha-253ed5d');
 
     /**
      * @license
