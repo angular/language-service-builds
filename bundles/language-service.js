9b8ccae1e16b53ece0b0771f8d7596574a2a7c21
Misko Hevery <misko@hevery.com>
2021-05-04 15:38:22 +0000

    6d7d659919 docs: add new migration guide to v12 documentation that describes how to migrate older localization ids to new ones (#41828)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+318.sha-b7db98f
+ * @license Angular v12.0.0-next.8+319.sha-6d7d659
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+318.sha-b7db98f');
+    const VERSION$1 = new Version('12.0.0-next.8+319.sha-6d7d659');
 
     /**
      * @license
@@ -47395,7 +47395,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+318.sha-b7db98f');
+    const VERSION$2 = new Version$1('12.0.0-next.8+319.sha-6d7d659');
 
     /**
      * @license
