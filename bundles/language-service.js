4984a25a2daf4135929ca5c05ff971d3fe37fc40
Misko Hevery <misko@hevery.com>
2021-05-04 15:35:38 +0000

    b7db98fd92 build: remove emojis from github issue templates (#41902)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+314.sha-741c295
+ * @license Angular v12.0.0-next.8+318.sha-b7db98f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+314.sha-741c295');
+    const VERSION$1 = new Version('12.0.0-next.8+318.sha-b7db98f');
 
     /**
      * @license
@@ -35467,7 +35467,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+314.sha-741c295');
+    const VERSION$2 = new Version$1('12.0.0-next.8+318.sha-b7db98f');
 
     /**
      * @license
