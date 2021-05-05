7a90a45dc42d4982dba4cd7e60f992ad35b48e83
Misko Hevery <misko@hevery.com>
2021-05-05 23:18:04 +0000

    768d4ac94d release: cut the v12.0.0-rc.2 release

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+64.sha-df2ff04
+ * @license Angular v12.0.0-rc.2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.1+64.sha-df2ff04');
+    const VERSION$1 = new Version('12.0.0-rc.2');
 
     /**
      * @license
@@ -35467,7 +35467,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.1+64.sha-df2ff04');
+    const VERSION$2 = new Version$1('12.0.0-rc.2');
 
     /**
      * @license
