191521f801383d2f31155f664c5cb75f1c797fe7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:05:47 +0000

    568a69cfda feat(dev-infra): include release notes in Github release entries when publishing (#41964)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+8.sha-cbe01a3
+ * @license Angular v12.0.0-rc.2+13.sha-568a69c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19412,7 +19412,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.2+8.sha-cbe01a3');
+    const VERSION$1 = new Version('12.0.0-rc.2+13.sha-568a69c');
 
     /**
      * @license
@@ -35470,7 +35470,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.2+8.sha-cbe01a3');
+    const VERSION$2 = new Version$1('12.0.0-rc.2+13.sha-568a69c');
 
     /**
      * @license
