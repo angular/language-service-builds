e453248a0d3e8c96ed83ba613af176e5357c934b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 22:00:43 +0000

    8dd5216c9e docs: use relative path for the feature request process diagram (#41970)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+13.sha-568a69c
+ * @license Angular v12.0.0-rc.2+15.sha-8dd5216
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19412,7 +19412,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.2+13.sha-568a69c');
+    const VERSION$1 = new Version('12.0.0-rc.2+15.sha-8dd5216');
 
     /**
      * @license
@@ -35470,7 +35470,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.2+13.sha-568a69c');
+    const VERSION$2 = new Version$1('12.0.0-rc.2+15.sha-8dd5216');
 
     /**
      * @license
