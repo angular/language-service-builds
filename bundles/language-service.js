21db53d4d0ef52f3cebc30be183ceb31b8f29db8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:01:03 +0000

    35fbd4f411 build: update dependency fs-extra to v10 (#41937)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+344.sha-abcd4bb
+ * @license Angular v12.0.0-next.8+345.sha-35fbd4f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+344.sha-abcd4bb');
+    const VERSION$1 = new Version('12.0.0-next.8+345.sha-35fbd4f');
 
     /**
      * @license
@@ -47439,7 +47439,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+344.sha-abcd4bb');
+    const VERSION$2 = new Version$1('12.0.0-next.8+345.sha-35fbd4f');
 
     /**
      * @license
