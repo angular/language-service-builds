1534ac5f175db31bb51c1971452bf2b70899d0aa
Andrew Scott <atscott@google.com>
2021-05-07 01:31:29 +0000

    51cc14292d docs: reword a few phrases in the GITHUB_PROCESS.md doc (#41977)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+15.sha-8dd5216
+ * @license Angular v12.0.0-rc.2+16.sha-51cc142
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19412,7 +19412,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.2+15.sha-8dd5216');
+    const VERSION$1 = new Version('12.0.0-rc.2+16.sha-51cc142');
 
     /**
      * @license
@@ -35470,7 +35470,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.2+15.sha-8dd5216');
+    const VERSION$2 = new Version$1('12.0.0-rc.2+16.sha-51cc142');
 
     /**
      * @license
