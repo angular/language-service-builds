fc50f3664a44b9245dc427c666e5ef7e675b7c0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 22:07:25 +0000

    1f84ac1d5f release: cut the v12.0.0-rc.3 release (#42031)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+45.sha-d555555
+ * @license Angular v12.0.0-rc.2+46.sha-1f84ac1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19412,7 +19412,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.2+45.sha-d555555');
+    const VERSION$1 = new Version('12.0.0-rc.2+46.sha-1f84ac1');
 
     /**
      * @license
@@ -35481,7 +35481,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.2+45.sha-d555555');
+    const VERSION$2 = new Version$1('12.0.0-rc.2+46.sha-1f84ac1');
 
     /**
      * @license
