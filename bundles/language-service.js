0f0d6514b2b62d35b5fae0162a51bd006d8dfd24
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 18:53:58 +0000

    0a2191f8e7 Revert "refactor(core): optimize calls to `split` and `slice` while computing version parts (#41208)"

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.3+1.sha-52cd8c9
+ * @license Angular v12.0.0-rc.3+2.sha-0a2191f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -2567,10 +2567,10 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     class Version {
         constructor(full) {
             this.full = full;
-            const [major, minor, ...rest] = full.split('.');
-            this.major = major;
-            this.minor = minor;
-            this.patch = rest.join('.');
+            const splits = full.split('.');
+            this.major = splits[0];
+            this.minor = splits[1];
+            this.patch = splits.slice(2).join('.');
         }
     }
     const __window = typeof window !== 'undefined' && window;
@@ -19412,7 +19412,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-rc.3+1.sha-52cd8c9');
+    const VERSION$1 = new Version('12.0.0-rc.3+2.sha-0a2191f');
 
     /**
      * @license
@@ -35472,16 +35472,15 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     class Version$1 {
         constructor(full) {
             this.full = full;
-            const [major, minor, ...rest] = full.split('.');
-            this.major = major;
-            this.minor = minor;
-            this.patch = rest.join('.');
+            this.major = full.split('.')[0];
+            this.minor = full.split('.')[1];
+            this.patch = full.split('.').slice(2).join('.');
         }
     }
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-rc.3+1.sha-52cd8c9');
+    const VERSION$2 = new Version$1('12.0.0-rc.3+2.sha-0a2191f');
 
     /**
      * @license
