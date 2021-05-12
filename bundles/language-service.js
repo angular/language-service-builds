c650643ee6b0098a4d070fc443a474a0cfd4adc4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:29:52 +0000

    dad205e502 docs: add release notes for v12.0.0 to CHANGELOG

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+396.sha-a74eb52
+ * @license Angular v12.0.0-next.8+397.sha-dad205e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+396.sha-a74eb52');
+    const VERSION$1 = new Version('12.0.0-next.8+397.sha-dad205e');
 
     /**
      * @license
@@ -35521,7 +35521,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+396.sha-a74eb52');
+    const VERSION$2 = new Version$1('12.0.0-next.8+397.sha-dad205e');
 
     /**
      * @license
