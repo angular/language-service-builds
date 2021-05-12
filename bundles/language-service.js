2ebe42ff4955fe7d7b7b1457af4d33be3b0375e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:56:05 +0000

    d34ce6b76a build(docs-infra): update docs examples to Angular v12.0.0 (#42071)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+398.sha-65ee1c3
+ * @license Angular v12.0.0-next.8+399.sha-d34ce6b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+398.sha-65ee1c3');
+    const VERSION$1 = new Version('12.0.0-next.8+399.sha-d34ce6b');
 
     /**
      * @license
@@ -35521,7 +35521,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+398.sha-65ee1c3');
+    const VERSION$2 = new Version$1('12.0.0-next.8+399.sha-d34ce6b');
 
     /**
      * @license
