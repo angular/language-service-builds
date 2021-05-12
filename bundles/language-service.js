06b6547fb97e812cbba76f1596bcfcbc884a5fee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:27:35 +0000

    5f7a401fef release: cut the v11.2.14 release (#42070)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+24.sha-b075481
+ * @license Angular v11.2.13+29.sha-5f7a401
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -2527,10 +2527,10 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     class Version {
         constructor(full) {
             this.full = full;
-            const [major, minor, ...rest] = full.split('.');
-            this.major = major;
-            this.minor = minor;
-            this.patch = rest.join('.');
+            const splits = full.split('.');
+            this.major = splits[0];
+            this.minor = splits[1];
+            this.patch = splits.slice(2).join('.');
         }
     }
     const __window = typeof window !== 'undefined' && window;
@@ -19208,7 +19208,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.13+24.sha-b075481');
+    const VERSION$1 = new Version('11.2.13+29.sha-5f7a401');
 
     /**
      * @license
@@ -35272,16 +35272,15 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     class Version$1 {
         constructor(full) {
             this.full = full;
-            const [major, minor, ...rest] = full.split('.');
-            this.major = major;
-            this.minor = minor;
-            this.patch = rest.join('.');
+            this.major = full.split('.')[0];
+            this.minor = full.split('.')[1];
+            this.patch = full.split('.').slice(2).join('.');
         }
     }
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.13+24.sha-b075481');
+    const VERSION$2 = new Version$1('11.2.13+29.sha-5f7a401');
 
     /**
      * @license
