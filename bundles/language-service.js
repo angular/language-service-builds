d9c7cd7a277e48a9dbdd7f5a3c257923ffd8f287
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 01:05:01 +0000

    53cd7e5eb5 docs: update the angular roadmap (#42050)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+399.sha-d34ce6b
+ * @license Angular v12.0.0-next.8+401.sha-53cd7e5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+399.sha-d34ce6b');
+    const VERSION$1 = new Version('12.0.0-next.8+401.sha-53cd7e5');
 
     /**
      * @license
@@ -35521,7 +35521,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+399.sha-d34ce6b');
+    const VERSION$2 = new Version$1('12.0.0-next.8+401.sha-53cd7e5');
 
     /**
      * @license
