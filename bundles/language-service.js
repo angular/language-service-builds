370a32e351ed03e27c364ad54d93f02ae4bf6cfc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:26:17 +0000

    04f41554dc build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#41991) (#42041)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.14
+ * @license Angular v11.2.14+1.sha-04f4155
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19208,7 +19208,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('11.2.14');
+    const VERSION$1 = new Version('11.2.14+1.sha-04f4155');
 
     /**
      * @license
@@ -35280,7 +35280,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('11.2.14');
+    const VERSION$2 = new Version$1('11.2.14+1.sha-04f4155');
 
     /**
      * @license
