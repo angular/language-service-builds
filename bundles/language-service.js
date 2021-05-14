e80fb70458cc27befb970faa6a14dd32a50bf7a1
Zach Arend <zachzach@google.com>
2021-05-14 15:12:12 +0000

    4bc5b4d93b fix(core): global listeners not being bound on non-node host elements (#42014)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+410.sha-2e7eb27
+ * @license Angular v12.0.0-next.8+411.sha-4bc5b4d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+410.sha-2e7eb27');
+    const VERSION$1 = new Version('12.0.0-next.8+411.sha-4bc5b4d');
 
     /**
      * @license
@@ -35521,7 +35521,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+410.sha-2e7eb27');
+    const VERSION$2 = new Version$1('12.0.0-next.8+411.sha-4bc5b4d');
 
     /**
      * @license
