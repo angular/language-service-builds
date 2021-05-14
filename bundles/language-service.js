217632e95639fa4b292fa20f0689e6eac102cfb9
Zach Arend <zachzach@google.com>
2021-05-14 21:27:56 +0000

    6d87895ffd test(dev-infra): add testing for integration of ReleaseNotes in publishing tooling (#41967)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+414.sha-e89e3cb
+ * @license Angular v12.0.0-next.8+415.sha-6d87895
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+414.sha-e89e3cb');
+    const VERSION$1 = new Version('12.0.0-next.8+415.sha-6d87895');
 
     /**
      * @license
@@ -47456,7 +47456,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+414.sha-e89e3cb');
+    const VERSION$2 = new Version$1('12.0.0-next.8+415.sha-6d87895');
 
     /**
      * @license
