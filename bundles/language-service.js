02127980cade4d5379d9e5be8547e795d7ef469f
Andrew Scott <atscott@google.com>
2021-05-18 23:00:06 +0000

    ec78e68a36 fix(dev-infra): correct FormatConfig interface (#42154)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+435.sha-b7d1614
+ * @license Angular v12.0.0-next.8+437.sha-ec78e68
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+435.sha-b7d1614');
+    const VERSION$1 = new Version('12.0.0-next.8+437.sha-ec78e68');
 
     /**
      * @license
@@ -47503,7 +47503,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+435.sha-b7d1614');
+    const VERSION$2 = new Version$1('12.0.0-next.8+437.sha-ec78e68');
 
     /**
      * @license
