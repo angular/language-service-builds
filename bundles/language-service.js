642fb7e2f67d6ef11a4b11e5d8d1aee32a805800
Andrew Scott <atscott@google.com>
2021-05-18 22:59:32 +0000

    aa9a86cfd5 build: remove publishConfig entry from package.json entries (#42104)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+34.sha-a20ef16
+ * @license Angular v12.0.0+37.sha-aa9a86c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0+34.sha-a20ef16');
+    const VERSION$1 = new Version('12.0.0+37.sha-aa9a86c');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0+34.sha-a20ef16');
+    const VERSION$2 = new Version$1('12.0.0+37.sha-aa9a86c');
 
     /**
      * @license
