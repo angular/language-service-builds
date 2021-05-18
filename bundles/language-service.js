f5f8c1803316db2f66098f7190602791f77e2c22
atscott <atscott01@gmail.com>
2021-05-18 15:57:18 +0000

    2dfa010763 docs: update bazel migration doc link (#42142) PR Close #42142

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+422.sha-5885cf5
+ * @license Angular v12.0.0-next.8+425.sha-2dfa010
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19453,7 +19453,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0-next.8+422.sha-5885cf5');
+    const VERSION$1 = new Version('12.0.0-next.8+425.sha-2dfa010');
 
     /**
      * @license
@@ -47456,7 +47456,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0-next.8+422.sha-5885cf5');
+    const VERSION$2 = new Version$1('12.0.0-next.8+425.sha-2dfa010');
 
     /**
      * @license
