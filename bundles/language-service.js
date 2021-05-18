26e9674a966ed3919052982c5e545aa5e13580ef
atscott <atscott01@gmail.com>
2021-05-18 21:24:30 +0000

    a20ef16d82 feat(dev-infra): publish major versions to "next" NPM dist tag (#42133)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+32.sha-c58e15a
+ * @license Angular v12.0.0+34.sha-a20ef16
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0+32.sha-c58e15a');
+    const VERSION$1 = new Version('12.0.0+34.sha-a20ef16');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0+32.sha-c58e15a');
+    const VERSION$2 = new Version$1('12.0.0+34.sha-a20ef16');
 
     /**
      * @license
