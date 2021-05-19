31c97ebc5dde24863e55a91fda143e879a2daf8b
GitHub <noreply@github.com>
2021-05-19 18:11:12 +0000

    43525adab1 release: cut the v12.0.1 release (#42175)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+41.sha-d0214e7
+ * @license Angular v12.0.0+42.sha-43525ad
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0+41.sha-d0214e7');
+    const VERSION$1 = new Version('12.0.0+42.sha-43525ad');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0+41.sha-d0214e7');
+    const VERSION$2 = new Version$1('12.0.0+42.sha-43525ad');
 
     /**
      * @license
