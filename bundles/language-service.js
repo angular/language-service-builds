39d32fff5ead63bc9519b442c933e11c0874f9ec
Zach Arend <zachzach@google.com>
2021-05-20 23:23:44 +0000

    ce6d49fc97 docs: edits to remove jargon (#41282)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+4.sha-640578f
+ * @license Angular v12.0.1+10.sha-ce6d49f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.1+4.sha-640578f');
+    const VERSION$1 = new Version('12.0.1+10.sha-ce6d49f');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.1+4.sha-640578f');
+    const VERSION$2 = new Version$1('12.0.1+10.sha-ce6d49f');
 
     /**
      * @license
