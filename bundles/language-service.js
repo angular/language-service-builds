e00cb963aab2ceadd3218cc54ff9c1a95232d579
Zach Arend <zachzach@google.com>
2021-05-20 16:27:04 +0000

    390d24b75f test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+42.sha-43525ad
+ * @license Angular v12.0.1+3.sha-390d24b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.0+42.sha-43525ad');
+    const VERSION$1 = new Version('12.0.1+3.sha-390d24b');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.0+42.sha-43525ad');
+    const VERSION$2 = new Version$1('12.0.1+3.sha-390d24b');
 
     /**
      * @license
