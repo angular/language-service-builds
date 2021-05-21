874f646a016978026afc8671fdac706ae7909dbe
Zach Arend <zachzach@google.com>
2021-05-21 18:25:06 +0000

    62becc88f9 fix(docs-infra): fix example compilation issues (#42199)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+14.sha-8c158ba
+ * @license Angular v12.1.0-next.2+15.sha-62becc8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+14.sha-8c158ba');
+    const VERSION$1 = new Version('12.1.0-next.2+15.sha-62becc8');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+14.sha-8c158ba');
+    const VERSION$2 = new Version$1('12.1.0-next.2+15.sha-62becc8');
 
     /**
      * @license
