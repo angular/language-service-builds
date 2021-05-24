6d06310d9cad9e28c7f627eafa4eed14f87f5066
Zach Arend <zachzach@google.com>
2021-05-24 23:00:20 +0000

    1040697787 docs: remove sentence about how the not reloading the page. (#42290)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+33.sha-ad12b31
+ * @license Angular v12.1.0-next.2+37.sha-1040697
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+33.sha-ad12b31');
+    const VERSION$1 = new Version('12.1.0-next.2+37.sha-1040697');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+33.sha-ad12b31');
+    const VERSION$2 = new Version$1('12.1.0-next.2+37.sha-1040697');
 
     /**
      * @license
