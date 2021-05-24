9d76f48edbbdee3f060405acef6453fda24a4213
Zach Arend <zachzach@google.com>
2021-05-24 16:18:11 +0000

    4ac55ca676 fix(dev-infra): use template strings for release note templates (#42224)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+22.sha-d7a1da9
+ * @license Angular v12.1.0-next.2+24.sha-4ac55ca
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+22.sha-d7a1da9');
+    const VERSION$1 = new Version('12.1.0-next.2+24.sha-4ac55ca');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+22.sha-d7a1da9');
+    const VERSION$2 = new Version$1('12.1.0-next.2+24.sha-4ac55ca');
 
     /**
      * @license
