d7654349da62c032639db48ae473c446812bc916
Zach Arend <zachzach@google.com>
2021-05-24 19:43:59 +0000

    c820066d98 docs: align code in `product-example.component.*.ts` files (#42155)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+25.sha-bf505ad
+ * @license Angular v12.1.0-next.2+31.sha-c820066
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+25.sha-bf505ad');
+    const VERSION$1 = new Version('12.1.0-next.2+31.sha-c820066');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+25.sha-bf505ad');
+    const VERSION$2 = new Version$1('12.1.0-next.2+31.sha-c820066');
 
     /**
      * @license
