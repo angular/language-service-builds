bc959cebb082d13537124f359d6b4725a3b059cc
Zach Arend <zachzach@google.com>
2021-05-25 18:27:33 +0000

    d7768c61ad docs: edit copy for tick() in testing and api docs (#35697)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+67.sha-594e633
+ * @license Angular v12.1.0-next.2+68.sha-d7768c6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+67.sha-594e633');
+    const VERSION$1 = new Version('12.1.0-next.2+68.sha-d7768c6');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+67.sha-594e633');
+    const VERSION$2 = new Version$1('12.1.0-next.2+68.sha-d7768c6');
 
     /**
      * @license
