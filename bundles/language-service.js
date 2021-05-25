9175522e456369524bac4bed39c010857b185e24
Zach Arend <zachzach@google.com>
2021-05-25 22:32:14 +0000

    bdd3cca9ab docs: update wording for angular libraries mapping peer dependencies (#42322)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+70.sha-3d9062d
+ * @license Angular v12.1.0-next.2+72.sha-bdd3cca
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+70.sha-3d9062d');
+    const VERSION$1 = new Version('12.1.0-next.2+72.sha-bdd3cca');
 
     /**
      * @license
@@ -47503,7 +47503,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+70.sha-3d9062d');
+    const VERSION$2 = new Version$1('12.1.0-next.2+72.sha-bdd3cca');
 
     /**
      * @license
