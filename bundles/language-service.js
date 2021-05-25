52fffa877b4c3155d00cda899d9fda39e88e3809
Zach Arend <zachzach@google.com>
2021-05-25 22:33:54 +0000

    4a2cc3e451 docs: add explanation of how to use locally created build artifacts in a local project (#42319)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+71.sha-063d178
+ * @license Angular v12.1.0-next.2+77.sha-4a2cc3e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+71.sha-063d178');
+    const VERSION$1 = new Version('12.1.0-next.2+77.sha-4a2cc3e');
 
     /**
      * @license
@@ -47503,7 +47503,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+71.sha-063d178');
+    const VERSION$2 = new Version$1('12.1.0-next.2+77.sha-4a2cc3e');
 
     /**
      * @license
