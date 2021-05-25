67154c14f4136fabb62802cc91d7a5e4460bad9c
Zach Arend <zachzach@google.com>
2021-05-25 18:20:44 +0000

    f74fd64523 fix(docs-infra): increase the opacity of background-color for inline `code` blocks (#42297)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+62.sha-ef33806
+ * @license Angular v12.1.0-next.2+65.sha-f74fd64
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+62.sha-ef33806');
+    const VERSION$1 = new Version('12.1.0-next.2+65.sha-f74fd64');
 
     /**
      * @license
@@ -47503,7 +47503,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+62.sha-ef33806');
+    const VERSION$2 = new Version$1('12.1.0-next.2+65.sha-f74fd64');
 
     /**
      * @license
