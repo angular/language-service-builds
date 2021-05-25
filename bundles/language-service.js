120c88f4272daf20a038074b48e5ffdbfc8489d8
Zach Arend <zachzach@google.com>
2021-05-25 18:15:16 +0000

    1955dbb048 docs: remove references to CheckAlways and CheckOnce (#42300)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+57.sha-29e5d50
+ * @license Angular v12.1.0-next.2+59.sha-1955dbb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+57.sha-29e5d50');
+    const VERSION$1 = new Version('12.1.0-next.2+59.sha-1955dbb');
 
     /**
      * @license
@@ -47503,7 +47503,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+57.sha-29e5d50');
+    const VERSION$2 = new Version$1('12.1.0-next.2+59.sha-1955dbb');
 
     /**
      * @license
