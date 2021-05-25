fab17a33cd3070cfa2f1e66bb9eabc5b2adb0093
Zach Arend <zachzach@google.com>
2021-05-25 18:15:09 +0000

    ef33806480 docs: add small clarity around navigationCancel (#42282)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+61.sha-2ff5bfe
+ * @license Angular v12.1.0-next.2+62.sha-ef33806
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+61.sha-2ff5bfe');
+    const VERSION$1 = new Version('12.1.0-next.2+62.sha-ef33806');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+61.sha-2ff5bfe');
+    const VERSION$2 = new Version$1('12.1.0-next.2+62.sha-ef33806');
 
     /**
      * @license
