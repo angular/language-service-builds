df7031b40f284d4ab08b613ae0b3b0aac759d376
Zach Arend <zachzach@google.com>
2021-05-25 18:08:55 +0000

    9f09c3b9b1 build(core): update supported range of node versions to be less restrictive (#42205)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+40.sha-1c3fe69
+ * @license Angular v12.1.0-next.2+56.sha-9f09c3b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+40.sha-1c3fe69');
+    const VERSION$1 = new Version('12.1.0-next.2+56.sha-9f09c3b');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+40.sha-1c3fe69');
+    const VERSION$2 = new Version$1('12.1.0-next.2+56.sha-9f09c3b');
 
     /**
      * @license
