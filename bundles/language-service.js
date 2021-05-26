f99d85b02aa6a0996adf2001d73c3e4682392d68
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:38:38 +0000

    4e9b61aaf9 docs: remove/update broken links in resources appearing on aio (#42232)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+13.sha-9193ea2
+ * @license Angular v12.1.0-next.3+14.sha-4e9b61a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+13.sha-9193ea2');
+    const VERSION$1 = new Version('12.1.0-next.3+14.sha-4e9b61a');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+13.sha-9193ea2');
+    const VERSION$2 = new Version$1('12.1.0-next.3+14.sha-4e9b61a');
 
     /**
      * @license
