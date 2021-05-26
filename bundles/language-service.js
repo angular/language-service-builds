13df9bc5c5e3234ecba6709453a047b3adb5dcb1
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:44:55 +0000

    516923e3dc docs: clarify when the `ngDoBootstrap` hook is invoked (#42362)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+16.sha-1572659
+ * @license Angular v12.1.0-next.3+18.sha-516923e
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+16.sha-1572659');
+    const VERSION$1 = new Version('12.1.0-next.3+18.sha-516923e');
 
     /**
      * @license
@@ -47509,7 +47509,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+16.sha-1572659');
+    const VERSION$2 = new Version$1('12.1.0-next.3+18.sha-516923e');
 
     /**
      * @license
