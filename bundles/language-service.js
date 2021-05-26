0c5c08bfb0e99620c4058ea7d254f211e3aaa6f6
Zach Arend <zachzach@google.com>
2021-05-26 20:32:49 +0000

    a787f78074 test: clean up internal testing utilities (#42177)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+10.sha-bd51762
+ * @license Angular v12.1.0-next.3+11.sha-a787f78
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+10.sha-bd51762');
+    const VERSION$1 = new Version('12.1.0-next.3+11.sha-a787f78');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+10.sha-bd51762');
+    const VERSION$2 = new Version$1('12.1.0-next.3+11.sha-a787f78');
 
     /**
      * @license
