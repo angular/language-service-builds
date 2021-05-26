66558a123c52671635909ece3c9ace7ce904a8bf
GitHub <noreply@github.com>
2021-05-26 20:05:37 +0000

    c110d050f2 release: cut the v12.1.0-next.3 release (#42360)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+85.sha-8228170
+ * @license Angular v12.1.0-next.3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.2+85.sha-8228170');
+    const VERSION$1 = new Version('12.1.0-next.3');
 
     /**
      * @license
@@ -47509,7 +47509,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.2+85.sha-8228170');
+    const VERSION$2 = new Version$1('12.1.0-next.3');
 
     /**
      * @license
