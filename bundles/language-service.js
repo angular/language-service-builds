dbf1e7a7daf47147c7dd15c1100b0b37e97060e6
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:39:11 +0000

    4cb11cc4e7 docs: add note about why module import order is important (#42323)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+14.sha-4e9b61a
+ * @license Angular v12.1.0-next.3+15.sha-4cb11cc
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+14.sha-4e9b61a');
+    const VERSION$1 = new Version('12.1.0-next.3+15.sha-4cb11cc');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+14.sha-4e9b61a');
+    const VERSION$2 = new Version$1('12.1.0-next.3+15.sha-4cb11cc');
 
     /**
      * @license
