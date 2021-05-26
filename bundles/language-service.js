b29896297ac52d7f509558d0738bbffd82341f1d
Zach Arend <zachzach@google.com>
2021-05-26 20:26:41 +0000

    40bbcaa12d docs: move and update browser compatibility docs to browser-support guide (#42334)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+2.sha-0166391
+ * @license Angular v12.1.0-next.3+5.sha-40bbcaa
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+2.sha-0166391');
+    const VERSION$1 = new Version('12.1.0-next.3+5.sha-40bbcaa');
 
     /**
      * @license
@@ -47509,7 +47509,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+2.sha-0166391');
+    const VERSION$2 = new Version$1('12.1.0-next.3+5.sha-40bbcaa');
 
     /**
      * @license
