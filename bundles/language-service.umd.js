db149bf59374a3f1f0cc3cf939a97c58f613cdd0
Zach Arend <zachzach@google.com>
2021-05-26 21:02:02 +0000

    fc25f94f2f build: mark //integration:bazel-schematics_test as manual (#42042)

--- bundles/language-service.umd.js
+++ bundles/language-service.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.13
+ * @license Angular v9.1.13+2.sha-fc25f94
  * Copyright Google Inc. All Rights Reserved.
  * License: MIT
  */
@@ -19601,7 +19601,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.13');
+    var VERSION$1 = new Version('9.1.13+2.sha-fc25f94');
 
     /**
      * @license
@@ -40163,7 +40163,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    var VERSION$2 = new Version$1('9.1.13');
+    var VERSION$2 = new Version$1('9.1.13+2.sha-fc25f94');
 
     /**
      * @license
