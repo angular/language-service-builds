b2e42656bc098a550b225952dba07e1e3ad5f524
Zach Arend <zachzach@google.com>
2021-05-26 15:24:12 +0000

    54b23aef42 docs: document shallow equality emit for ActivatedRoute (#42280)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+66.sha-c01da21
+ * @license Angular v12.0.1+83.sha-54b23ae
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.1+66.sha-c01da21');
+    const VERSION$1 = new Version('12.0.1+83.sha-54b23ae');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.1+66.sha-c01da21');
+    const VERSION$2 = new Version$1('12.0.1+83.sha-54b23ae');
 
     /**
      * @license
@@ -36727,7 +36727,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
          *
          * ```typescript
          * @Component({
-         *   selector: 'my-app',
+         *   selector: 'app-root',
          *   template: `Number of ticks: {{numberOfTicks}}`
          *   changeDetection: ChangeDetectionStrategy.OnPush,
          * })
@@ -36847,7 +36847,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
          * }
          *
          * @Component({
-         *   selector: 'my-app',
+         *   selector: 'app-root',
          *   providers: [DataProvider],
          *   template: `
          *     Live Update: <input type="checkbox" [(ngModel)]="live">
