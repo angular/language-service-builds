954916b6558b7d450a0723751476953393198e1a
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:05:11 +0000

    ee0e3fbf89 docs: edits to remove jargon (#42025)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+42.sha-635aaaa
+ * @license Angular v12.1.0-next.3+45.sha-ee0e3fb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+42.sha-635aaaa');
+    const VERSION$1 = new Version('12.1.0-next.3+45.sha-ee0e3fb');
 
     /**
      * @license
@@ -47509,7 +47509,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+42.sha-635aaaa');
+    const VERSION$2 = new Version$1('12.1.0-next.3+45.sha-ee0e3fb');
 
     /**
      * @license
