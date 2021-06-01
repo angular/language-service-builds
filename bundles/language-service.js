39b8dfe439e08dc0d3c170fd09d4932fe2dc112d
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:41:38 +0000

    b0293c656e docs: clarify how declaration files can be installed (#42417)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+42.sha-b60c9c1
+ * @license Angular v12.0.2+62.sha-b0293c6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.2+42.sha-b60c9c1');
+    const VERSION$1 = new Version('12.0.2+62.sha-b0293c6');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.2+42.sha-b60c9c1');
+    const VERSION$2 = new Version$1('12.0.2+62.sha-b0293c6');
 
     /**
      * @license
