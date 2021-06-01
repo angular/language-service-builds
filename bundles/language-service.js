eefbe5f9ad4f663c78b95ed0b877026ac8434f25
Andrew Kushnir <akushnir@google.com>
2021-06-01 20:26:58 +0000

    5babdb2284 build: update angular (#42379)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+62.sha-e039075
+ * @license Angular v12.1.0-next.3+63.sha-5babdb2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19500,7 +19500,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.3+62.sha-e039075');
+    const VERSION$1 = new Version('12.1.0-next.3+63.sha-5babdb2');
 
     /**
      * @license
@@ -35568,7 +35568,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.3+62.sha-e039075');
+    const VERSION$2 = new Version$1('12.1.0-next.3+63.sha-5babdb2');
 
     /**
      * @license
