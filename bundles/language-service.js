555fed19e930d4fc4754596143f35b24e931d00d
Andrew Kushnir <akushnir@google.com>
2021-06-02 21:08:16 +0000

    0a1d8e5102 refactor(docs-infra): clean up AIO Sass files (#42442)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3
+ * @license Angular v12.0.3+11.sha-0a1d8e5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.3');
+    const VERSION$1 = new Version('12.0.3+11.sha-0a1d8e5');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.3');
+    const VERSION$2 = new Version$1('12.0.3+11.sha-0a1d8e5');
 
     /**
      * @license
