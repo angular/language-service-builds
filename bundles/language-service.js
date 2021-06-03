07f1bc0d7df16b3d0f81effd7f3831c56d8c21b6
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 23:25:06 +0000

    ae858c0504 ci: extend default timeout of karma tests for legacy saucelab tests (#42473)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+34.sha-ba08485
+ * @license Angular v12.1.0-next.4+39.sha-ae858c0
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19550,7 +19550,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+34.sha-ba08485');
+    const VERSION$1 = new Version('12.1.0-next.4+39.sha-ae858c0');
 
     /**
      * @license
@@ -47567,7 +47567,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+34.sha-ba08485');
+    const VERSION$2 = new Version$1('12.1.0-next.4+39.sha-ae858c0');
 
     /**
      * @license
