1ac2e9e82ca99e71cd6aa47166ec74bc0c3007ec
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 21:03:52 +0000

    a6dec05e82 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+22.sha-50c87e8
+ * @license Angular v12.0.3+26.sha-a6dec05
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.3+22.sha-50c87e8');
+    const VERSION$1 = new Version('12.0.3+26.sha-a6dec05');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.3+22.sha-50c87e8');
+    const VERSION$2 = new Version$1('12.0.3+26.sha-a6dec05');
 
     /**
      * @license
