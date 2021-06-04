5f49c8abd1c592a201a5b82158a3e380881a92db
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 22:39:44 +0000

    30059173f4 docs: add node.js version info into the universal guide (#42375)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+46.sha-4fc24ad
+ * @license Angular v12.1.0-next.4+47.sha-3005917
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+46.sha-4fc24ad');
+    const VERSION$1 = new Version('12.1.0-next.4+47.sha-3005917');
 
     /**
      * @license
@@ -35628,7 +35628,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+46.sha-4fc24ad');
+    const VERSION$2 = new Version$1('12.1.0-next.4+47.sha-3005917');
 
     /**
      * @license
