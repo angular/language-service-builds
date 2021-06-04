9e032c5e11fe44541db403e1bfc94622cad1ea57
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:52:31 +0000

    6080db1839 build: suppress errors in git hooks (#42484)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+31.sha-beae425
+ * @license Angular v12.0.3+33.sha-6080db1
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.3+31.sha-beae425');
+    const VERSION$1 = new Version('12.0.3+33.sha-6080db1');
 
     /**
      * @license
@@ -35527,7 +35527,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.3+31.sha-beae425');
+    const VERSION$2 = new Version$1('12.0.3+33.sha-6080db1');
 
     /**
      * @license
