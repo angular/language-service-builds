54ed88f3580b6f6932aad6b3c91716f21d9253d9
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:44:29 +0000

    34dd3c360b fix(language-service): fix autocomplete info display for some cases (#42472)

--- bundles/ivy.js
+++ bundles/ivy.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+42.sha-8bb6fb7
+ * @license Angular v12.0.3+43.sha-34dd3c3
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -17918,7 +17918,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.3+42.sha-8bb6fb7');
+    const VERSION$1 = new Version('12.0.3+43.sha-34dd3c3');
 
     /**
      * @license
@@ -18557,7 +18557,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function compileDeclareClassMetadata(metadata) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         definitionMap.set('ngImport', importExpr(Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
@@ -18597,7 +18597,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$1));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -18814,7 +18814,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function compileDeclareFactoryFunction(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$2));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         definitionMap.set('ngImport', importExpr(Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', compileDependencies(meta.deps));
@@ -18856,7 +18856,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createInjectableDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$3));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         definitionMap.set('ngImport', importExpr(Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
@@ -18935,7 +18935,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createInjectorDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$4));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         definitionMap.set('ngImport', importExpr(Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
@@ -18972,7 +18972,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createNgModuleDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$5));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         definitionMap.set('ngImport', importExpr(Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
@@ -19030,7 +19030,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$6));
-        definitionMap.set('version', literal('12.0.3+42.sha-8bb6fb7'));
+        definitionMap.set('version', literal('12.0.3+43.sha-34dd3c3'));
         definitionMap.set('ngImport', importExpr(Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -19062,7 +19062,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$2 = new Version('12.0.3+42.sha-8bb6fb7');
+    const VERSION$2 = new Version('12.0.3+43.sha-34dd3c3');
 
     /**
      * @license
@@ -38924,7 +38924,9 @@ https://v9.angular.io/guide/template-typecheck#template-type-checking`,
     }
     function getTsSymbolDisplayInfo(tsLS, checker, symbol, kind, ownerName) {
         const decl = symbol.valueDeclaration;
-        if (decl === undefined || (!ts$1.isPropertyDeclaration(decl) && !ts$1.isMethodDeclaration(decl)) ||
+        if (decl === undefined ||
+            (!ts$1.isPropertyDeclaration(decl) && !ts$1.isMethodDeclaration(decl) &&
+                !isNamedClassDeclaration(decl)) ||
             !ts$1.isIdentifier(decl.name)) {
             return null;
         }
@@ -40696,14 +40698,24 @@ https://v9.angular.io/guide/template-typecheck#template-type-checking`,
                     displayParts = info.displayParts;
                     documentation = info.documentation;
                     break;
+                case AttributeCompletionKind.StructuralDirectiveAttribute:
                 case AttributeCompletionKind.DirectiveInput:
                 case AttributeCompletionKind.DirectiveOutput:
                     const propertySymbol = getAttributeCompletionSymbol(completion, this.typeChecker);
                     if (propertySymbol === null) {
                         return undefined;
                     }
-                    info = getTsSymbolDisplayInfo(this.tsLS, this.typeChecker, propertySymbol, completion.kind === AttributeCompletionKind.DirectiveInput ? DisplayInfoKind.PROPERTY :
-                        DisplayInfoKind.EVENT, completion.directive.tsSymbol.name);
+                    let kind;
+                    if (completion.kind === AttributeCompletionKind.DirectiveInput) {
+                        kind = DisplayInfoKind.PROPERTY;
+                    }
+                    else if (completion.kind === AttributeCompletionKind.DirectiveOutput) {
+                        kind = DisplayInfoKind.EVENT;
+                    }
+                    else {
+                        kind = DisplayInfoKind.DIRECTIVE;
+                    }
+                    info = getTsSymbolDisplayInfo(this.tsLS, this.typeChecker, propertySymbol, kind, completion.directive.tsSymbol.name);
                     if (info === null) {
                         return undefined;
                     }
@@ -40714,7 +40726,7 @@ https://v9.angular.io/guide/template-typecheck#template-type-checking`,
                 name: entryName,
                 kind: unsafeCastDisplayInfoKindToScriptElementKind(kind),
                 kindModifiers: ts$1.ScriptElementKindModifier.none,
-                displayParts: [],
+                displayParts,
                 documentation,
             };
         }
