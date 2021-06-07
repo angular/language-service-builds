a38b520945122d6b2423732258d73b3cbe2251c7
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:52:53 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+47.sha-3005917
+ * @license Angular v12.1.0-next.4+48.sha-cd22c7a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+47.sha-3005917');
+    const VERSION$1 = new Version('12.1.0-next.4+48.sha-cd22c7a');
 
     /**
      * @license
@@ -47569,7 +47569,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+47.sha-3005917');
+    const VERSION$2 = new Version$1('12.1.0-next.4+48.sha-cd22c7a');
 
     /**
      * @license
