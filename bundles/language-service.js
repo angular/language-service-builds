666bec9d03a4cc6d2d6af1618ad8f705b796c772
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 17:21:00 +0000

    cc904b5226 docs(core): clarify deprecation of `entryComponents` (#42248)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+50.sha-b061e5b
+ * @license Angular v12.1.0-next.4+51.sha-cc904b5
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+50.sha-b061e5b');
+    const VERSION$1 = new Version('12.1.0-next.4+51.sha-cc904b5');
 
     /**
      * @license
@@ -35628,7 +35628,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+50.sha-b061e5b');
+    const VERSION$2 = new Version$1('12.1.0-next.4+51.sha-cc904b5');
 
     /**
      * @license
