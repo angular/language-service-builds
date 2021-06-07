4a03a65e232f9dcc7fd1414f341a549e1d9b2238
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 22:53:21 +0000

    b762820485 build: simplify generation of closure locale file (#42230)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+57.sha-a493ea9
+ * @license Angular v12.1.0-next.4+63.sha-b762820
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+57.sha-a493ea9');
+    const VERSION$1 = new Version('12.1.0-next.4+63.sha-b762820');
 
     /**
      * @license
@@ -35628,7 +35628,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+57.sha-a493ea9');
+    const VERSION$2 = new Version$1('12.1.0-next.4+63.sha-b762820');
 
     /**
      * @license
