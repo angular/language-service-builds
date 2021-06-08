6b34130919386c060319a322b6bacb1a429c6ccc
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:03:42 +0000

    4cf442c879 docs: add links to examples for ComponentFactoryResolver (#42496)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+76.sha-c44ab4f
+ * @license Angular v12.1.0-next.4+74.sha-4cf442c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+76.sha-c44ab4f');
+    const VERSION$1 = new Version('12.1.0-next.4+74.sha-4cf442c');
 
     /**
      * @license
@@ -35631,7 +35631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+76.sha-c44ab4f');
+    const VERSION$2 = new Version$1('12.1.0-next.4+74.sha-4cf442c');
 
     /**
      * @license
