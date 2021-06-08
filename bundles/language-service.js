6dfb3f6c6693311cead610115dc2dd3253080fd5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:04:45 +0000

    c44ab4f6da fix(router): fix `serializeQueryParams` logic (#42481)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+72.sha-d10c38a
+ * @license Angular v12.1.0-next.4+76.sha-c44ab4f
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+72.sha-d10c38a');
+    const VERSION$1 = new Version('12.1.0-next.4+76.sha-c44ab4f');
 
     /**
      * @license
@@ -47400,6 +47400,9 @@ Please check that 1) the type for the parameter at index ${index} is correct and
      * then use the factory's `create()` method to create a component of that type.
      *
      * @see [Dynamic Components](guide/dynamic-component-loader)
+     * @see [Usage Example](guide/dynamic-component-loader#resolving-components)
+     * @see <live-example name="dynamic-component-loader" noDownload></live-example>
+    of the code in this cookbook
      * @publicApi
      */
     class ComponentFactoryResolver {
@@ -47573,7 +47576,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+72.sha-d10c38a');
+    const VERSION$2 = new Version$1('12.1.0-next.4+76.sha-c44ab4f');
 
     /**
      * @license
