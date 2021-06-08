50cc5ba31ba145b22760f8b8658b2b91aa61c1f2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:07:24 +0000

    43b4a8c07c docs: Edited to remove jargon in Reference Errors (#42186)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+86.sha-f3fde4b
+ * @license Angular v12.1.0-next.4+85.sha-43b4a8c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.4+86.sha-f3fde4b');
+    const VERSION$1 = new Version('12.1.0-next.4+85.sha-43b4a8c');
 
     /**
      * @license
@@ -47576,7 +47576,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.4+86.sha-f3fde4b');
+    const VERSION$2 = new Version$1('12.1.0-next.4+85.sha-43b4a8c');
 
     /**
      * @license
