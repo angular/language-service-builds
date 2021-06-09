9d6a9a525296f5ad1a8e37b13be709be76c9096b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 19:33:11 +0000

    3961b3c360 fix(core): ensure that autoRegisterModuleById registration in ɵɵdefineNgModule is not DCE-ed by closure (#42529)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+1.sha-e36c5b4
+ * @license Angular v12.1.0-next.5+2.sha-3961b3c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+1.sha-e36c5b4');
+    const VERSION$1 = new Version('12.1.0-next.5+2.sha-3961b3c');
 
     /**
      * @license
@@ -28254,22 +28254,22 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * @codeGenApi
      */
     function ɵɵdefineNgModule(def) {
-        const res = {
-            type: def.type,
-            bootstrap: def.bootstrap || EMPTY_ARRAY,
-            declarations: def.declarations || EMPTY_ARRAY,
-            imports: def.imports || EMPTY_ARRAY,
-            exports: def.exports || EMPTY_ARRAY,
-            transitiveCompileScopes: null,
-            schemas: def.schemas || null,
-            id: def.id || null,
-        };
-        if (def.id != null) {
-            noSideEffects(() => {
+        return noSideEffects(() => {
+            const res = {
+                type: def.type,
+                bootstrap: def.bootstrap || EMPTY_ARRAY,
+                declarations: def.declarations || EMPTY_ARRAY,
+                imports: def.imports || EMPTY_ARRAY,
+                exports: def.exports || EMPTY_ARRAY,
+                transitiveCompileScopes: null,
+                schemas: def.schemas || null,
+                id: def.id || null,
+            };
+            if (def.id != null) {
                 autoRegisterModuleById[def.id] = def.type;
-            });
-        }
-        return res;
+            }
+            return res;
+        });
     }
     /**
      * Adds the module metadata that is necessary to compute the module's transitive scope to an
@@ -47576,7 +47576,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+1.sha-e36c5b4');
+    const VERSION$2 = new Version$1('12.1.0-next.5+2.sha-3961b3c');
 
     /**
      * @license
