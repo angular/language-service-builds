3ae4540be82ad38c4394d332d5b149187c2c88a4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:35:51 +0000

    665b986896 docs: CLI users polyfills (#42263)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+3.sha-22bda22
+ * @license Angular v12.1.0-next.5+4.sha-665b986
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+3.sha-22bda22');
+    const VERSION$1 = new Version('12.1.0-next.5+4.sha-665b986');
 
     /**
      * @license
@@ -35631,7 +35631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+3.sha-22bda22');
+    const VERSION$2 = new Version$1('12.1.0-next.5+4.sha-665b986');
 
     /**
      * @license
