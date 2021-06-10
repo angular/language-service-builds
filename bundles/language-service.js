c283bf65a7844ccac90fda4f698a191eb2915e38
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:22:36 +0000

    ec90d8d5e5 docs: change field name from id to $id in schema.json file (#42528)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+9.sha-cddb3a8
+ * @license Angular v12.1.0-next.5+8.sha-ec90d8d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+9.sha-cddb3a8');
+    const VERSION$1 = new Version('12.1.0-next.5+8.sha-ec90d8d');
 
     /**
      * @license
@@ -35631,7 +35631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+9.sha-cddb3a8');
+    const VERSION$2 = new Version$1('12.1.0-next.5+8.sha-ec90d8d');
 
     /**
      * @license
