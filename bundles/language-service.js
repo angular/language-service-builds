201c8d029a0eb0bdd2d0b7c11c4cfe8c7add02d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:44:41 +0000

    299f92c3b6 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+4.sha-665b986
+ * @license Angular v12.1.0-next.5+5.sha-299f92c
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+4.sha-665b986');
+    const VERSION$1 = new Version('12.1.0-next.5+5.sha-299f92c');
 
     /**
      * @license
@@ -35631,7 +35631,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+4.sha-665b986');
+    const VERSION$2 = new Version$1('12.1.0-next.5+5.sha-299f92c');
 
     /**
      * @license
