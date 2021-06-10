f83b46540a4744898db97925f9705a395e91c845
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:28:07 +0000

    013d1cb010 refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+6.sha-5185795
+ * @license Angular v12.0.4+11.sha-013d1cb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.4+6.sha-5185795');
+    const VERSION$1 = new Version('12.0.4+11.sha-013d1cb');
 
     /**
      * @license
@@ -35530,7 +35530,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.4+6.sha-5185795');
+    const VERSION$2 = new Version$1('12.0.4+11.sha-013d1cb');
 
     /**
      * @license
