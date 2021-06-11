845f9434c644bbe725870bba70fcbd91f8b9c63d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 19:03:56 +0000

    cee5c100af docs: remove section on tables from docs style guide (#42330)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+11.sha-013d1cb
+ * @license Angular v12.0.4+12.sha-cee5c10
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19459,7 +19459,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.0.4+11.sha-013d1cb');
+    const VERSION$1 = new Version('12.0.4+12.sha-cee5c10');
 
     /**
      * @license
@@ -35530,7 +35530,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.0.4+11.sha-013d1cb');
+    const VERSION$2 = new Version$1('12.0.4+12.sha-cee5c10');
 
     /**
      * @license
