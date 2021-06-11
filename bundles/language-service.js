5dd445d2d0f22a841b320988e81d7f0df3abd42b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 15:20:55 +0000

    0c13e2bf97 build(packaging): remove rehype (#42557)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+12.sha-efb440e
+ * @license Angular v12.1.0-next.5+13.sha-0c13e2b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19549,7 +19549,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+12.sha-efb440e');
+    const VERSION$1 = new Version('12.1.0-next.5+13.sha-0c13e2b');
 
     /**
      * @license
@@ -47576,7 +47576,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+12.sha-efb440e');
+    const VERSION$2 = new Version$1('12.1.0-next.5+13.sha-0c13e2b');
 
     /**
      * @license
