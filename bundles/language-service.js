b8e23d7e563a39d1e71aaf0b983656221a8990f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:35 +0000

    3b81528ddd fix(docs-infra): improve card layout for different browser font sizes (#42533)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+42.sha-f3a7987
+ * @license Angular v12.1.0-next.5+43.sha-3b81528
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19553,7 +19553,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+42.sha-f3a7987');
+    const VERSION$1 = new Version('12.1.0-next.5+43.sha-3b81528');
 
     /**
      * @license
@@ -35635,7 +35635,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+42.sha-f3a7987');
+    const VERSION$2 = new Version$1('12.1.0-next.5+43.sha-3b81528');
 
     /**
      * @license
