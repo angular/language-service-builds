a8704af77aa27f4c45d20dc1359771959544bcca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:52:56 +0000

    0e33521705 Revert "release: bump the next branch to v12.2.0-next.0"

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+49.sha-7961e2f
+ * @license Angular v12.1.0-next.5+50.sha-0e33521
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19553,7 +19553,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.0-next.5+49.sha-7961e2f');
+    const VERSION$1 = new Version('12.1.0-next.5+50.sha-0e33521');
 
     /**
      * @license
@@ -35635,7 +35635,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.0-next.5+49.sha-7961e2f');
+    const VERSION$2 = new Version$1('12.1.0-next.5+50.sha-0e33521');
 
     /**
      * @license
