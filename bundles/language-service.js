898115a1b591256b24ec07c52752db4010407e96
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:53:16 +0000

    56fb4eb08d docs(compiler): interally typo fixed (#42676)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+3.sha-37a740c
+ * @license Angular v12.2.0-next.0+4.sha-56fb4eb
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19594,7 +19594,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.0+3.sha-37a740c');
+    const VERSION$1 = new Version('12.2.0-next.0+4.sha-56fb4eb');
 
     /**
      * @license
@@ -47621,7 +47621,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.0+3.sha-37a740c');
+    const VERSION$2 = new Version$1('12.2.0-next.0+4.sha-56fb4eb');
 
     /**
      * @license
