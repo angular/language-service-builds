b06035bedc7c1ea8a3e1c8b0f4caeff5577c9c63
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:54:42 +0000

    4bea630baa docs(core): add context jsdoc param to createEmbeddedView (#42675) PR Close #42675

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+9.sha-2a260dc
+ * @license Angular v12.2.0-next.0+10.sha-4bea630
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19594,7 +19594,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.0+9.sha-2a260dc');
+    const VERSION$1 = new Version('12.2.0-next.0+10.sha-4bea630');
 
     /**
      * @license
@@ -47621,7 +47621,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.0+9.sha-2a260dc');
+    const VERSION$2 = new Version$1('12.2.0-next.0+10.sha-4bea630');
 
     /**
      * @license
