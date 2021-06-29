062f66a4ea94fd19e1ebedb17e7449da73cbd003
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:37:41 +0000

    8528073f6c fix(dev-infra): remove bots from special thanks section (#42697)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+19.sha-f29fe5c
+ * @license Angular v12.2.0-next.0+20.sha-8528073
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19594,7 +19594,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.0+19.sha-f29fe5c');
+    const VERSION$1 = new Version('12.2.0-next.0+20.sha-8528073');
 
     /**
      * @license
@@ -35676,7 +35676,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.0+19.sha-f29fe5c');
+    const VERSION$2 = new Version$1('12.2.0-next.0+20.sha-8528073');
 
     /**
      * @license
