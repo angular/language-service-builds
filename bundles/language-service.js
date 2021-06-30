0a024b3196747456ec90d53de1dc382d98946dce
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 16:34:19 +0000

    f3965ff834 docs: setting context value inside an interceptor (#42699)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+20.sha-8528073
+ * @license Angular v12.2.0-next.0+21.sha-f3965ff
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19594,7 +19594,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.0+20.sha-8528073');
+    const VERSION$1 = new Version('12.2.0-next.0+21.sha-f3965ff');
 
     /**
      * @license
@@ -35676,7 +35676,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.0+20.sha-8528073');
+    const VERSION$2 = new Version$1('12.2.0-next.0+21.sha-f3965ff');
 
     /**
      * @license
