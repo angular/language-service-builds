a72fc33d10faaaf22c64ecc3db9dc6256198881b
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:54:11 +0000

    bfa1b5d9eb fix(docs-infra): move dots inside Usage Notes anchor tag (#42725)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+2.sha-d997303
+ * @license Angular v12.2.0-next.1+3.sha-bfa1b5d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+2.sha-d997303');
+    const VERSION$1 = new Version('12.2.0-next.1+3.sha-bfa1b5d');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+2.sha-d997303');
+    const VERSION$2 = new Version$1('12.2.0-next.1+3.sha-bfa1b5d');
 
     /**
      * @license
