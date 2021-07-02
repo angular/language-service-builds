7d836b29de1fb3f679554395b7cc81df20d061c9
atscott <atscott01@gmail.com>
2021-07-02 22:33:32 +0000

    4c78984ad2 fix(compiler-cli): support reflecting namespace declarations (#42728)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+12.sha-e99af18
+ * @license Angular v12.2.0-next.1+15.sha-4c78984
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+12.sha-e99af18');
+    const VERSION$1 = new Version('12.2.0-next.1+15.sha-4c78984');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+12.sha-e99af18');
+    const VERSION$2 = new Version$1('12.2.0-next.1+15.sha-4c78984');
 
     /**
      * @license
