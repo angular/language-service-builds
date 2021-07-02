400d596a3ffeef31700bee916d5141b6a85aece8
atscott <atscott01@gmail.com>
2021-07-02 17:09:50 +0000

    e81efe6667 docs: convert commit SHAs and PR numbers in changelog to links (#42732)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+7.sha-19bcdfb
+ * @license Angular v12.2.0-next.1+9.sha-e81efe6
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+7.sha-19bcdfb');
+    const VERSION$1 = new Version('12.2.0-next.1+9.sha-e81efe6');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+7.sha-19bcdfb');
+    const VERSION$2 = new Version$1('12.2.0-next.1+9.sha-e81efe6');
 
     /**
      * @license
