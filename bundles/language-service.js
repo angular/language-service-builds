fdabe8a095a6e8e2f5ecbf6889a89210dcce7231
atscott <atscott01@gmail.com>
2021-07-03 00:34:53 +0000

    c9b47f2182 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+15.sha-4c78984
+ * @license Angular v12.2.0-next.1+16.sha-c9b47f2
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+15.sha-4c78984');
+    const VERSION$1 = new Version('12.2.0-next.1+16.sha-c9b47f2');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+15.sha-4c78984');
+    const VERSION$2 = new Version$1('12.2.0-next.1+16.sha-c9b47f2');
 
     /**
      * @license
