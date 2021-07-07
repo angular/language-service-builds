6312c3f3fde630f655982eee14dedbb2994a35c9
Andrew Scott <atscott@google.com>
2021-07-07 20:57:21 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+18.sha-e064f17
+ * @license Angular v12.2.0-next.1+19.sha-24ac378
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+18.sha-e064f17');
+    const VERSION$1 = new Version('12.2.0-next.1+19.sha-24ac378');
 
     /**
      * @license
@@ -47637,7 +47637,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+18.sha-e064f17');
+    const VERSION$2 = new Version$1('12.2.0-next.1+19.sha-24ac378');
 
     /**
      * @license
