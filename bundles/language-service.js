17345c1ac840e9b9d984973880bdcc7af565eda4
atscott <atscott01@gmail.com>
2021-07-08 17:50:29 +0000

    cb2ca9a66e fix(service-worker): correctly handle unrecoverable state when a client no longer exists (#42736)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+29.sha-ad08b17
+ * @license Angular v12.2.0-next.1+39.sha-cb2ca9a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+29.sha-ad08b17');
+    const VERSION$1 = new Version('12.2.0-next.1+39.sha-cb2ca9a');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+29.sha-ad08b17');
+    const VERSION$2 = new Version$1('12.2.0-next.1+39.sha-cb2ca9a');
 
     /**
      * @license
