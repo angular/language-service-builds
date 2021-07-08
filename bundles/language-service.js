147256e7285f89080c0dde44751b623176aaad8e
atscott <atscott01@gmail.com>
2021-07-08 22:19:06 +0000

    dfe9e1d077 build: lock file maintenance (#42770)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+45.sha-1f3747b
+ * @license Angular v12.2.0-next.1+46.sha-dfe9e1d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+45.sha-1f3747b');
+    const VERSION$1 = new Version('12.2.0-next.1+46.sha-dfe9e1d');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+45.sha-1f3747b');
+    const VERSION$2 = new Version$1('12.2.0-next.1+46.sha-dfe9e1d');
 
     /**
      * @license
