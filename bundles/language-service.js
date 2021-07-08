7d5be7fa56512966f667165898118be461011487
atscott <atscott01@gmail.com>
2021-07-08 22:11:46 +0000

    1f3747b583 ci: configure renovate to ignore `@microsoft/api-extractor` (#42797)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+43.sha-e7832a6
+ * @license Angular v12.2.0-next.1+45.sha-1f3747b
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+43.sha-e7832a6');
+    const VERSION$1 = new Version('12.2.0-next.1+45.sha-1f3747b');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+43.sha-e7832a6');
+    const VERSION$2 = new Version$1('12.2.0-next.1+45.sha-1f3747b');
 
     /**
      * @license
@@ -39240,11 +39240,11 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * through DI.
      *
      * ```
-     *  function initializeApp(httpClient: HttpClient): Observable<any> {
-     *   return httpClient.get("https://someUrl.com/api/user")
+     *  function initializeAppFactory(httpClient: HttpClient): () => Observable<any> {
+     *   return () => httpClient.get("https://someUrl.com/api/user")
      *     .pipe(
      *        tap(user => { ... })
-     *     )
+     *     );
      *  }
      *
      *  @NgModule({
@@ -39253,7 +39253,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      *    bootstrap: [AppComponent],
      *    providers: [{
      *      provide: APP_INITIALIZER,
-     *      useFactory: initializeApp,
+     *      useFactory: initializeAppFactory,
      *      deps: [HttpClient],
      *      multi: true
      *    }]
