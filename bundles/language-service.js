d79910509a1c2102608ab8ad050ede922bb3e5b7
Andrew Kushnir <akushnir@google.com>
2021-07-09 22:08:49 +0000

    762e057e85 build: update to rules_nodejs v4.0.0-beta.0 (#42760)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+53.sha-9d58ebf
+ * @license Angular v12.2.0-next.1+58.sha-762e057
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+53.sha-9d58ebf');
+    const VERSION$1 = new Version('12.2.0-next.1+58.sha-762e057');
 
     /**
      * @license
@@ -35692,7 +35692,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+53.sha-9d58ebf');
+    const VERSION$2 = new Version$1('12.2.0-next.1+58.sha-762e057');
 
     /**
      * @license
