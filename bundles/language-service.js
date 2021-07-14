9f385927e5882900da732bfd5c1db1426e68e1ec
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:33:50 +0000

    e6f01d777d revert: docs: remove references to Trusted Types due to incompatibility with Webpack 5 (#42796)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+97.sha-2a61728
+ * @license Angular v12.1.2+3.sha-e6f01d7
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21466,7 +21466,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.1.1+97.sha-2a61728');
+    const VERSION$1 = new Version('12.1.2+3.sha-e6f01d7');
 
     /**
      * @license
@@ -37544,7 +37544,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.1.1+97.sha-2a61728');
+    const VERSION$2 = new Version$1('12.1.2+3.sha-e6f01d7');
 
     /**
      * @license
