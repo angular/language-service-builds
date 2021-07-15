c44ba3b6c22a47a58e5688281a86ea21df32b337
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:40:27 +0000

    31593db489 refactor(core): expand error logging when the JIT compiler is not available (#42693)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+4.sha-07d7e60
+ * @license Angular v12.2.0-next.2+5.sha-31593db
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+4.sha-07d7e60');
+    const VERSION$1 = new Version('12.2.0-next.2+5.sha-31593db');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+4.sha-07d7e60');
+    const VERSION$2 = new Version$1('12.2.0-next.2+5.sha-31593db');
 
     /**
      * @license
