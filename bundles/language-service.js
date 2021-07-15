ebe49801b978dcd151545c35dfdd9016527c0c50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:55:19 +0000

    642362a9c7 build: add configuration for caretaker handoff (#42859)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+12.sha-f5baa55
+ * @license Angular v12.2.0-next.2+15.sha-642362a
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+12.sha-f5baa55');
+    const VERSION$1 = new Version('12.2.0-next.2+15.sha-642362a');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+12.sha-f5baa55');
+    const VERSION$2 = new Version$1('12.2.0-next.2+15.sha-642362a');
 
     /**
      * @license
