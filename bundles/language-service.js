62c15ffddbda2161cf5ad6e4914da11f47012595
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:56:39 +0000

    74228215a1 build: update dependency @microsoft/api-extractor to v7.18.4 (#42864)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+15.sha-642362a
+ * @license Angular v12.2.0-next.2+13.sha-7422821
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+15.sha-642362a');
+    const VERSION$1 = new Version('12.2.0-next.2+13.sha-7422821');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+15.sha-642362a');
+    const VERSION$2 = new Version$1('12.2.0-next.2+13.sha-7422821');
 
     /**
      * @license
