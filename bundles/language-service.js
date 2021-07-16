5735ee3c865fc952a9db0ef51753ec2666405073
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 17:22:43 +0000

    3da71467e9 build(docs-infra): upgrade Lighthouse to version 8.1.0 (#42846)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+13.sha-7422821
+ * @license Angular v12.2.0-next.2+16.sha-3da7146
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+13.sha-7422821');
+    const VERSION$1 = new Version('12.2.0-next.2+16.sha-3da7146');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+13.sha-7422821');
+    const VERSION$2 = new Version$1('12.2.0-next.2+16.sha-3da7146');
 
     /**
      * @license
