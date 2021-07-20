a7e10d692dc9d03111d6b7f3cbaf3aab3b574398
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:52:17 +0000

    7eddd12bf0 feat(dev-infra): add option to setup global types in API golden test (#42876)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+34.sha-30165f9
+ * @license Angular v12.2.0-next.2+36.sha-7eddd12
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+34.sha-30165f9');
+    const VERSION$1 = new Version('12.2.0-next.2+36.sha-7eddd12');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+34.sha-30165f9');
+    const VERSION$2 = new Version$1('12.2.0-next.2+36.sha-7eddd12');
 
     /**
      * @license
