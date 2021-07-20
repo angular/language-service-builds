073be4eea630356117ab160419180f2d3101ca08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:58:33 +0000

    52dc65dd1b build(docs-infra): improve @usageNotes message (#42888)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+39.sha-307dac6
+ * @license Angular v12.2.0-next.2+40.sha-52dc65d
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+39.sha-307dac6');
+    const VERSION$1 = new Version('12.2.0-next.2+40.sha-52dc65d');
 
     /**
      * @license
@@ -49497,7 +49497,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+39.sha-307dac6');
+    const VERSION$2 = new Version$1('12.2.0-next.2+40.sha-52dc65d');
 
     /**
      * @license
