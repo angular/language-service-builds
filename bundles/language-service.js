b41c930327e2b2f2efd60a25109ad88f99099801
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:35:47 +0000

    96d31d8c9f fix(dev-infra): prevent verbose messaging of git checkout and commit for release (#42871)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+28.sha-0ce8f6e
+ * @license Angular v12.2.0-next.2+32.sha-96d31d8
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+28.sha-0ce8f6e');
+    const VERSION$1 = new Version('12.2.0-next.2+32.sha-96d31d8');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+28.sha-0ce8f6e');
+    const VERSION$2 = new Version$1('12.2.0-next.2+32.sha-96d31d8');
 
     /**
      * @license
