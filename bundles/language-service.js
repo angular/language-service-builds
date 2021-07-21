0ba53204cfeb1d4a68b8bf8102012c8e66872ba8
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:58:56 +0000

    a502279592 feat(forms): allow minLength/maxLength validator to be bound to `null` (#42565)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+4.sha-eefe168.with-local-changes
+ * @license Angular v12.2.0-next.3+5.sha-a502279.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.3+4.sha-eefe168.with-local-changes');
+    const VERSION$1 = new Version('12.2.0-next.3+5.sha-a502279.with-local-changes');
 
     /**
      * @license
@@ -49497,7 +49497,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.3+4.sha-eefe168.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.0-next.3+5.sha-a502279.with-local-changes');
 
     /**
      * @license
