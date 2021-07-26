990d264e327bc5a1c2134925eb46c566e8d61a6e
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 19:24:07 +0000

    dbae00195e feat(router): ability to provide custom route reuse strategy via DI for `RouterTestingModule` (#42434)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+79.sha-ff87da3.with-local-changes
+ * @license Angular v12.2.0-next.1+80.sha-22290af.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+79.sha-ff87da3.with-local-changes');
+    const VERSION$1 = new Version('12.2.0-next.1+80.sha-22290af.with-local-changes');
 
     /**
      * @license
@@ -49497,7 +49497,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+79.sha-ff87da3.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.0-next.1+80.sha-22290af.with-local-changes');
 
     /**
      * @license
