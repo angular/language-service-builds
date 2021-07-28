5b983ad46fb94d60e77da4c4211e4be9786ad127
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:10:16 +0000

    047994b048 feat(dev-infra): introduce release action for directly branching-off into RC (#42973)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+86.sha-5b5868d.with-local-changes
+ * @license Angular v12.2.0-next.1+87.sha-1dffa51.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.1+86.sha-5b5868d.with-local-changes');
+    const VERSION$1 = new Version('12.2.0-next.1+87.sha-1dffa51.with-local-changes');
 
     /**
      * @license
@@ -37560,7 +37560,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.1+86.sha-5b5868d.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.0-next.1+87.sha-1dffa51.with-local-changes');
 
     /**
      * @license
