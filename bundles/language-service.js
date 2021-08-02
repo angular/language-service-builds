a9eb7fab5d3100ec96e9a5e1b284e39c8f499819
atscott <atscott01@gmail.com>
2021-08-02 22:58:03 +0000

    f62366fd16 docs: amend links in content-projection guide (#43009)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+12.sha-f5baa55.with-local-changes
+ * @license Angular v12.2.0-next.2+13.sha-7422821.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21591,7 +21591,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+12.sha-f5baa55.with-local-changes');
+    const VERSION$1 = new Version('12.2.0-next.2+13.sha-7422821.with-local-changes');
 
     /**
      * @license
@@ -49606,7 +49606,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+12.sha-f5baa55.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.0-next.2+13.sha-7422821.with-local-changes');
 
     /**
      * @license
