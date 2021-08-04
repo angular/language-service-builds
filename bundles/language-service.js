894663c4b7ff910dd63a731147bf1b7c9ff4575e
GitHub <noreply@github.com>
2021-08-04 17:27:21 +0000

    82c8393d37 docs: release notes for the v12.1.5 release (#43046)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+29.sha-5327926.with-local-changes
+ * @license Angular v12.2.0-next.2+30.sha-89509df.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+29.sha-5327926.with-local-changes');
+    const VERSION$1 = new Version('12.2.0-next.2+30.sha-89509df.with-local-changes');
 
     /**
      * @license
@@ -49498,7 +49498,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+29.sha-5327926.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.0-next.2+30.sha-89509df.with-local-changes');
 
     /**
      * @license
