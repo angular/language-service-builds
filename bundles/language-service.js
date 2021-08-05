f402921eb83f755c48336ab61e4fabd15ffd639e
Andrew Kushnir <akushnir@google.com>
2021-08-05 23:25:03 +0000

    a26b0dcc72 ci: stop publishing snapshots to angular/dev-infra-private-builds (#43059)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+27.sha-b41a2b3.with-local-changes
+ * @license Angular v12.2.0-next.2+28.sha-0ce8f6e.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21482,7 +21482,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.0-next.2+27.sha-b41a2b3.with-local-changes');
+    const VERSION$1 = new Version('12.2.0-next.2+28.sha-0ce8f6e.with-local-changes');
 
     /**
      * @license
@@ -37561,7 +37561,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.0-next.2+27.sha-b41a2b3.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.0-next.2+28.sha-0ce8f6e.with-local-changes');
 
     /**
      * @license
