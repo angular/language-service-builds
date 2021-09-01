61655116f6def3380cf94fe51b7e9c2230eded4a
Andrew Scott <atscott01@gmail.com>
2021-09-01 21:40:27 +0000

    a0bd6e90f9 fix(router): add more context to `Unhandled Navigation Error` (#43291)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+17.sha-caecd18.with-local-changes
+ * @license Angular v12.2.4+1.sha-a0bd6e9.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21475,7 +21475,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.3+17.sha-caecd18.with-local-changes');
+    const VERSION$1 = new Version('12.2.4+1.sha-a0bd6e9.with-local-changes');
 
     /**
      * @license
@@ -37554,7 +37554,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.3+17.sha-caecd18.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.4+1.sha-a0bd6e9.with-local-changes');
 
     /**
      * @license
