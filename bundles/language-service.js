c14f80717d9e4278605899939bca9569d4cf8fb1
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 16:59:07 +0000

    5a888f1b30 docs: Edits to remove jargon (#43045)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+30.sha-ed235bd.with-local-changes
+ * @license Angular v12.2.6+31.sha-5a888f1.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21610,7 +21610,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.6+30.sha-ed235bd.with-local-changes');
+    const VERSION$1 = new Version('12.2.6+31.sha-5a888f1.with-local-changes');
 
     /**
      * @license
@@ -36825,7 +36825,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.6+30.sha-ed235bd.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.6+31.sha-5a888f1.with-local-changes');
 
     /**
      * @license
