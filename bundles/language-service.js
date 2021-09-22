58e47246381884f8b608db7c612d7dea06429598
GitHub <noreply@github.com>
2021-09-22 19:38:10 +0000

    49d29e1337 release: cut the v13.0.0-next.7 release (#43539)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+64.sha-d2a03b1.with-local-changes
+ * @license Angular v13.0.0-next.6+65.sha-49d29e1.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('13.0.0-next.6+64.sha-d2a03b1.with-local-changes');
+    const VERSION$1 = new Version('13.0.0-next.6+65.sha-49d29e1.with-local-changes');
 
     /**
      * @license
@@ -48607,7 +48607,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('13.0.0-next.6+64.sha-d2a03b1.with-local-changes');
+    const VERSION$2 = new Version$1('13.0.0-next.6+65.sha-49d29e1.with-local-changes');
 
     /**
      * @license
