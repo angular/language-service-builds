b26c485b2c0d4b186c9a2e115af6ac43878889cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:17:00 +0000

    9d290bd012 ci: correctly handle commit message with carriage returns in `payload-size.sh` (#43569)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+38.sha-e131540.with-local-changes
+ * @license Angular v13.0.0-next.7+40.sha-9d290bd.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21474,7 +21474,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('13.0.0-next.7+38.sha-e131540.with-local-changes');
+    const VERSION$1 = new Version('13.0.0-next.7+40.sha-9d290bd.with-local-changes');
 
     /**
      * @license
@@ -36675,7 +36675,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('13.0.0-next.7+38.sha-e131540.with-local-changes');
+    const VERSION$2 = new Version$1('13.0.0-next.7+40.sha-9d290bd.with-local-changes');
 
     /**
      * @license
