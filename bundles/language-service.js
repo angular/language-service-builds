6de424985f292749072696620f3c38a4a08846e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 21:42:00 +0000

    988cca7ef5 fix(ngcc): do not fail for packages which correspond with `Object` members (#43589)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+8.sha-bdf5887.with-local-changes
+ * @license Angular v13.0.0-next.8+10.sha-988cca7.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21484,7 +21484,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('13.0.0-next.8+8.sha-bdf5887.with-local-changes');
+    const VERSION$1 = new Version('13.0.0-next.8+10.sha-988cca7.with-local-changes');
 
     /**
      * @license
@@ -48622,7 +48622,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('13.0.0-next.8+8.sha-bdf5887.with-local-changes');
+    const VERSION$2 = new Version$1('13.0.0-next.8+10.sha-988cca7.with-local-changes');
 
     /**
      * @license
