45dcaa8e3653026de5c85499786d096605c7252f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:39:19 +0000

    8d34f86edb docs: deprecate `PlatformRef.bootstrapModuleFactory` and factory-based signature of `ApplicationRef.bootstrap` (#43560)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+14.sha-4f3beff.with-local-changes
+ * @license Angular v13.0.0-next.8+15.sha-8d34f86.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21484,7 +21484,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('13.0.0-next.8+14.sha-4f3beff.with-local-changes');
+    const VERSION$1 = new Version('13.0.0-next.8+15.sha-8d34f86.with-local-changes');
 
     /**
      * @license
@@ -48622,7 +48622,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('13.0.0-next.8+14.sha-4f3beff.with-local-changes');
+    const VERSION$2 = new Version$1('13.0.0-next.8+15.sha-8d34f86.with-local-changes');
 
     /**
      * @license
@@ -56066,6 +56066,9 @@ Please check that 1) the type for the parameter at index ${index} is correct and
          *
          * let moduleRef = platformBrowser().bootstrapModuleFactory(MyModuleNgFactory);
          * ```
+         *
+         * @deprecated Passing NgModule factories as the `PlatformRef.bootstrapModuleFactory` function
+         *     argument is deprecated. Use the `PlatformRef.bootstrapModule` API instead.
          */
         bootstrapModuleFactory(moduleFactory, options) {
             // Note: We need to create the NgZone _before_ we instantiate the module,
