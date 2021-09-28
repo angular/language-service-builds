3b07dfe83f5aff46a60777fd1cce043b30bcf9ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:38:28 +0000

    4f3beffdbf feat(router): emit activate/deactivate events when an outlet gets attached/detached (#43333)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+13.sha-8d2b6af.with-local-changes
+ * @license Angular v13.0.0-next.8+14.sha-4f3beff.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21484,7 +21484,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('13.0.0-next.8+13.sha-8d2b6af.with-local-changes');
+    const VERSION$1 = new Version('13.0.0-next.8+14.sha-4f3beff.with-local-changes');
 
     /**
      * @license
@@ -48622,7 +48622,7 @@ Please check that 1) the type for the parameter at index ${index} is correct and
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('13.0.0-next.8+13.sha-8d2b6af.with-local-changes');
+    const VERSION$2 = new Version$1('13.0.0-next.8+14.sha-4f3beff.with-local-changes');
 
     /**
      * @license
