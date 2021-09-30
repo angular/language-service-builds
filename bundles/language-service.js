7cdca87584a4c491368631bb6b12ed9b1b1c15b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-30 20:36:27 +0000

    e5b0c39a77 docs: release notes for the v12.2.8 release (#43655)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+35.sha-a07cb09.with-local-changes
+ * @license Angular v13.0.0-next.8+36.sha-e5b0c39.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21484,7 +21484,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('13.0.0-next.8+35.sha-a07cb09.with-local-changes');
+    const VERSION$1 = new Version('13.0.0-next.8+36.sha-e5b0c39.with-local-changes');
 
     /**
      * @license
@@ -36685,7 +36685,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('13.0.0-next.8+35.sha-a07cb09.with-local-changes');
+    const VERSION$2 = new Version$1('13.0.0-next.8+36.sha-e5b0c39.with-local-changes');
 
     /**
      * @license
