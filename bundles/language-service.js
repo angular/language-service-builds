9ca54629d2d3d642f7280396a5a89bfd65d36121
Andrew Kushnir <akushnir@google.com>
2021-10-19 04:09:54 +0000

    9f350a8473 ci: update payload size limit (#43885)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.9+17.sha-c7c285c.with-local-changes
+ * @license Angular v12.2.10+15.sha-9f350a8.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21611,7 +21611,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.9+17.sha-c7c285c.with-local-changes');
+    const VERSION$1 = new Version('12.2.10+15.sha-9f350a8.with-local-changes');
 
     /**
      * @license
@@ -36833,7 +36833,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.9+17.sha-c7c285c.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.10+15.sha-9f350a8.with-local-changes');
 
     /**
      * @license
