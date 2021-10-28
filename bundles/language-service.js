bcf71ad52efee529f9463ba4bc48cc610e3c2cfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-28 18:53:46 +0000

    f225bfde80 docs(common): describe behavior when reference changes (#42863) PR Close #42863

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.2+1.sha-8d89b28.with-local-changes
+ * @license Angular v13.0.0-rc.2+5.sha-f225bfd.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -20062,7 +20062,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    new Version('13.0.0-rc.2+1.sha-8d89b28.with-local-changes');
+    new Version('13.0.0-rc.2+5.sha-f225bfd.with-local-changes');
 
     /**
      * @license
@@ -20691,7 +20691,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function compileDeclareClassMetadata(metadata) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION$6));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
@@ -20731,7 +20731,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createDirectiveDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION$5));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -20948,7 +20948,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function compileDeclareFactoryFunction(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION$4));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', compileDependencies(meta.deps));
@@ -20990,7 +20990,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createInjectableDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION$3));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
@@ -21069,7 +21069,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createInjectorDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION$2));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
@@ -21106,7 +21106,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createNgModuleDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION$1));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
@@ -21164,7 +21164,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
     function createPipeDefinitionMap(meta) {
         const definitionMap = new DefinitionMap();
         definitionMap.set('minVersion', literal$1(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', literal$1('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+        definitionMap.set('version', literal$1('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
@@ -21196,7 +21196,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'os', 'typescript', 'fs', '
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    new Version('13.0.0-rc.2+1.sha-8d89b28.with-local-changes');
+    new Version('13.0.0-rc.2+5.sha-f225bfd.with-local-changes');
 
     /**
      * @license
@@ -29807,7 +29807,7 @@ Either add the @Injectable() decorator to '${provider.node.name
      * Compiles @NgModule annotations to ngModuleDef fields.
      */
     class NgModuleDecoratorHandler {
-        constructor(reflector, evaluator, metaReader, metaRegistry, scopeRegistry, referencesRegistry, isCore, refEmitter, factoryTracker, annotateForClosureCompiler, injectableRegistry, perf, localeId) {
+        constructor(reflector, evaluator, metaReader, metaRegistry, scopeRegistry, referencesRegistry, isCore, refEmitter, factoryTracker, annotateForClosureCompiler, injectableRegistry, perf) {
             this.reflector = reflector;
             this.evaluator = evaluator;
             this.metaReader = metaReader;
@@ -29820,7 +29820,6 @@ Either add the @Injectable() decorator to '${provider.node.name
             this.annotateForClosureCompiler = annotateForClosureCompiler;
             this.injectableRegistry = injectableRegistry;
             this.perf = perf;
-            this.localeId = localeId;
             this.precedence = HandlerPrecedence.PRIMARY;
             this.name = NgModuleDecoratorHandler.name;
         }
@@ -30147,15 +30146,6 @@ Either add the @Injectable() decorator to '${provider.node.name
                     type: injectorDef.type,
                 },
             ];
-            if (this.localeId) {
-                // QUESTION: can this stuff be removed?
-                res.push({
-                    name: 'ɵloc',
-                    initializer: new LiteralExpr(this.localeId),
-                    statements: [],
-                    type: STRING_TYPE
-                });
-            }
             return res;
         }
         _toR3Reference(valueRef, valueContext, typeContext) {
@@ -40995,7 +40985,7 @@ Either add the @Injectable() decorator to '${provider.node.name
                 // before injectable factories (so injectable factories can delegate to them)
                 new PipeDecoratorHandler(reflector, evaluator, metaRegistry, scopeRegistry, injectableRegistry, isCore, this.delegatingPerfRecorder),
                 new InjectableDecoratorHandler(reflector, isCore, this.options.strictInjectionParameters || false, injectableRegistry, this.delegatingPerfRecorder),
-                new NgModuleDecoratorHandler(reflector, evaluator, metaReader, metaRegistry, scopeRegistry, referencesRegistry, isCore, refEmitter, this.adapter.factoryTracker, this.closureCompilerEnabled, injectableRegistry, this.delegatingPerfRecorder, this.options.i18nInLocale),
+                new NgModuleDecoratorHandler(reflector, evaluator, metaReader, metaRegistry, scopeRegistry, referencesRegistry, isCore, refEmitter, this.adapter.factoryTracker, this.closureCompilerEnabled, injectableRegistry, this.delegatingPerfRecorder),
             ];
             const traitCompiler = new TraitCompiler(handlers, reflector, this.delegatingPerfRecorder, this.incrementalCompilation, this.options.compileNonExportedClasses !== false, compilationMode, dtsTransforms, semanticDepGraphUpdater);
             // Template type-checking may use the `ProgramDriver` to produce new `ts.Program`(s). If this
