89bc5e50345075a64355aa7542f0f7c16b3d4700
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:31 +0000

    b6554d75cd fix(ngcc): correctly resolve UMD dependencies (#44382)

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.13+10.sha-4cf1a81.with-local-changes
+ * @license Angular v12.2.14+1.sha-b6554d7.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21634,7 +21634,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.13+10.sha-4cf1a81.with-local-changes');
+    const VERSION$1 = new Version('12.2.14+1.sha-b6554d7.with-local-changes');
 
     /**
      * @license
@@ -36868,7 +36868,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.13+10.sha-4cf1a81.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.14+1.sha-b6554d7.with-local-changes');
 
     /**
      * @license
