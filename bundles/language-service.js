5e3394225c7fef8c4da5e266edec5c9b08e13b50
Joey Perrott <josephperrott.github@gmail.com>
2022-03-23 22:29:38 +0000

    68901bf0f3 fix(docs-infra): prevent framing of AIO with X-Frame-Options

--- bundles/language-service.js
+++ bundles/language-service.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.15+3.sha-b2a081a.with-local-changes
+ * @license Angular v12.2.16+1.sha-68901bf.with-local-changes
  * Copyright Google LLC All Rights Reserved.
  * License: MIT
  */
@@ -21634,7 +21634,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const VERSION$1 = new Version('12.2.15+3.sha-b2a081a.with-local-changes');
+    const VERSION$1 = new Version('12.2.16+1.sha-68901bf.with-local-changes');
 
     /**
      * @license
@@ -36868,7 +36868,7 @@ define(['exports', 'typescript/lib/tsserverlibrary', 'typescript', 'path'], func
     /**
      * @publicApi
      */
-    const VERSION$2 = new Version$1('12.2.15+3.sha-b2a081a.with-local-changes');
+    const VERSION$2 = new Version$1('12.2.16+1.sha-68901bf.with-local-changes');
 
     /**
      * @license
