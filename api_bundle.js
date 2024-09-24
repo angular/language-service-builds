297a2742a8eaec762484557878af7520b7d599a9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 13:37:46 +0000

    03ac3c2 refactor: update license text to point to angular.dev (#57902)

--- api_bundle.js
+++ api_bundle.js
@@ -30,6 +30,6 @@ function isNgLanguageService(ls) {
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 //# sourceMappingURL=api_bundle.js.map
