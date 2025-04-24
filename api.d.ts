013395c893d993616191a80ecd1b96151c72579b
Miles Malerba <mmalerba@google.com>
2025-04-24 20:15:48 +0000

    e711f99 feat(core): move `provideExperimentalCheckNoChangesForDebug` to `provideCheckNoChangesConfig` (#60906)

--- api.d.ts
+++ api.d.ts
@@ -36,6 +36,10 @@ export interface PluginConfig {
      */
     enableLetSyntax?: false;
     /**
+     * Whether selectorless is enabled.
+     */
+    enableSelectorless?: true;
+    /**
      * A list of diagnostic codes that should be supressed in the language service.
      */
     suppressAngularDiagnosticCodes?: number[];
